3304df10a9116a29424181c01cc549c906797dcd
Bidcar <bidcar.herrera@forzalatam.com>
2020-07-11 02:57:21 -0600

    form edit guide

---
+++ DeliveryBackOffice/Tables/ALTER TABLE dbo.DeliveryOrder - ADD Columns of guide editing.sql
@@ -0,0 +1,17 @@
+USE DeliveryBackOffice
+ALTER TABLE dbo.DeliveryOrder ADD
+	DateUpdated datetime NULL,
+	TokenUpdated nvarchar(50) NULL,
+	AlteredInformation bit NULL
+GO
+DECLARE @v sql_variant 
+SET @v = N'Date editing guide'
+EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'dbo', N'TABLE', N'DeliveryOrder', N'COLUMN', N'DateUpdated'
+GO
+DECLARE @v2 sql_variant 
+SET @v2 = N'User editing guide'
+EXECUTE sp_addextendedproperty N'MS_Description', @v2, N'SCHEMA', N'dbo', N'TABLE', N'DeliveryOrder', N'COLUMN', N'TokenUpdated'
+GO
+DECLARE @v3 sql_variant 
+SET @v3 = N'altered guide, yes or not'
+EXECUTE sp_addextendedproperty N'MS_Description', @v3, N'SCHEMA', N'dbo', N'TABLE', N'DeliveryOrder', N'COLUMN', N'AlteredInformation'
