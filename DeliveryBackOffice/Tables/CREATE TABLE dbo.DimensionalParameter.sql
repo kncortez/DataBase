d180aca6dfa30ba64e29d65f721b3f5daacfe30e
eramirez <edwin.ramirez@forzalatam.com>
2020-07-21 15:29:59 -0600

    creacion tabla dimensional parameter

---
+++ DeliveryBackOffice/Tables/CREATE TABLE dbo.DimensionalParameter.sql
@@ -0,0 +1,54 @@
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.Unit SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
+BEGIN TRANSACTION
+GO
+CREATE TABLE dbo.DimensionalParameter
+	(
+	IdDimensional bigint NOT NULL IDENTITY (1, 1),
+	IdUnit int NULL,
+	ByRange bit NULL,
+	ByUnity bit NULL,
+	MinimunValue int NULL,
+	MaximunValue int NULL,
+	DimensionalStatus bit NULL,
+	IdCountry nvarchar(2) NULL,
+	TokenCreated nvarchar(50) NULL,
+	DateCreated datetime NULL,
+	TokenUpdated nvarchar(50) NULL,
+	UpdatedCreated datetime NULL
+	)  ON [PRIMARY]
+GO
+ALTER TABLE dbo.DimensionalParameter ADD CONSTRAINT
+	PK_DimensionalParameter PRIMARY KEY CLUSTERED 
+	(
+	IdDimensional
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+ALTER TABLE dbo.DimensionalParameter ADD CONSTRAINT
+	FK_DimensionalParameters_Unit FOREIGN KEY
+	(
+	IdUnit
+	) REFERENCES dbo.Unit
+	(
+	IdUnit
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+ALTER TABLE dbo.DimensionalParameter SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
