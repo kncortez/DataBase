577eecfb8d9a80c8234d4be6ec13d063a7c2b71f
eramirez <edwin.ramirez@forzalatam.com>
2020-08-07 18:33:52 -0600

    actualizacion de llaves a tabla rate estimate

--- DeliveryBackOffice/Tables/ALTER TABLE dbo.RateEstimate.sql
+++ DeliveryBackOffice/Tables/ALTER TABLE dbo.RateEstimate.sql
@@ -10,10 +10,140 @@ SET ANSI_WARNINGS ON
 COMMIT
 BEGIN TRANSACTION
 GO
+ALTER TABLE dbo.RateEstimate
+	DROP CONSTRAINT FK_RateEstimate_RateCategory
+GO
 ALTER TABLE dbo.RateCategory SET (LOCK_ESCALATION = TABLE)
 GO
 COMMIT
 BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.RateEstimate
+	DROP CONSTRAINT FK_RateEstimate_Settlement
+GO
+ALTER TABLE dbo.Settlement SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.RateEstimate
+	DROP CONSTRAINT FK_RateEstimate_Ecommerce
+GO
+ALTER TABLE dbo.Ecommerce SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.RateEstimate
+	DROP CONSTRAINT FK_RateEstimate_Customer
+GO
+ALTER TABLE dbo.Customer SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
+BEGIN TRANSACTION
+GO
+CREATE TABLE dbo.Tmp_RateEstimate
+	(
+	IdRateEstimated bigint NOT NULL IDENTITY (1, 1),
+	IdSource bigint NULL,
+	IdDestiny bigint NULL,
+	ObjectType nvarchar(50) NULL,
+	CountPieces int NULL,
+	UnitValue decimal(5, 2) NULL,
+	IdUnit int NULL,
+	CodeCredit nvarchar(10) NULL,
+	IdRate int NULL,
+	BaseRate decimal(18, 2) NULL,
+	EstimateTotalAmount decimal(18, 2) NULL,
+	IdCustomer int NULL,
+	IdEcommerce int NULL,
+	DateService datetime NULL,
+	EstimatedStatus bit NULL,
+	TokenCreated nvarchar(50) NULL,
+	DateCreated datetime NULL,
+	TokenUpdated nvarchar(50) NULL,
+	DateUpdated datetime NULL,
+	DateEcommerceSale datetime NULL,
+	DateEcommercePickUp datetime NULL,
+	DateCarrierDelivery datetime NULL,
+	IdCurrency int NULL,
+	IdCountry nvarchar(2) NULL,
+	IdRateCategory int NULL,
+	RatePlanDescription nvarchar(50) NULL
+	)  ON [PRIMARY]
+GO
+ALTER TABLE dbo.Tmp_RateEstimate SET (LOCK_ESCALATION = TABLE)
+GO
+DECLARE @v sql_variant 
+SET @v = N'Description of the object to Transport'
+EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'dbo', N'TABLE', N'Tmp_RateEstimate', N'COLUMN', N'ObjectType'
+GO
+DECLARE @v sql_variant 
+SET @v = N'Value of Unit Mass Lbs Weight'
+EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'dbo', N'TABLE', N'Tmp_RateEstimate', N'COLUMN', N'UnitValue'
+GO
+DECLARE @v sql_variant 
+SET @v = N'Mass Lbs'
+EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'dbo', N'TABLE', N'Tmp_RateEstimate', N'COLUMN', N'IdUnit'
+GO
+DECLARE @v sql_variant 
+SET @v = N'date when created the estimate rate'
+EXECUTE sp_addextendedproperty N'MS_Description', @v, N'SCHEMA', N'dbo', N'TABLE', N'Tmp_RateEstimate', N'COLUMN', N'DateService'
+GO
+SET IDENTITY_INSERT dbo.Tmp_RateEstimate ON
+GO
+IF EXISTS(SELECT * FROM dbo.RateEstimate)
+	 EXEC('INSERT INTO dbo.Tmp_RateEstimate (IdRateEstimated, IdSource, IdDestiny, ObjectType, CountPieces, UnitValue, IdUnit, CodeCredit, IdRate, EstimateTotalAmount, IdCustomer, IdEcommerce, DateService, EstimatedStatus, TokenCreated, DateCreated, TokenUpdated, DateUpdated, DateEcommerceSale, DateEcommercePickUp, DateCarrierDelivery, IdCurrency, IdCountry, IdRateCategory, RatePlanDescription)
+		SELECT IdRateEstimated, IdSource, IdDestiny, ObjectType, CountPieces, UnitValue, IdUnit, CodeCredit, IdRate, EstimateTotalAmount, IdCustomer, IdEcommerce, DateService, EstimatedStatus, TokenCreated, DateCreated, TokenUpdated, DateUpdated, DateEcommerceSale, DateEcommercePickUp, DateCarrierDelivery, IdCurrency, IdCountry, IdRateCategory, RatePlanDescription FROM dbo.RateEstimate WITH (HOLDLOCK TABLOCKX)')
+GO
+SET IDENTITY_INSERT dbo.Tmp_RateEstimate OFF
+GO
+ALTER TABLE dbo.RateEstimateDetail
+	DROP CONSTRAINT FK_RateEstimateDetail_RateEstimate
+GO
+DROP TABLE dbo.RateEstimate
+GO
+EXECUTE sp_rename N'dbo.Tmp_RateEstimate', N'RateEstimate', 'OBJECT' 
+GO
+ALTER TABLE dbo.RateEstimate ADD CONSTRAINT
+	PK_RateEstimate PRIMARY KEY CLUSTERED 
+	(
+	IdRateEstimated
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+ALTER TABLE dbo.RateEstimate ADD CONSTRAINT
+	FK_RateEstimate_Customer FOREIGN KEY
+	(
+	IdCustomer
+	) REFERENCES dbo.Customer
+	(
+	IdCustomer
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+ALTER TABLE dbo.RateEstimate ADD CONSTRAINT
+	FK_RateEstimate_Ecommerce FOREIGN KEY
+	(
+	IdEcommerce
+	) REFERENCES dbo.Ecommerce
+	(
+	IdEcommerce
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+ALTER TABLE dbo.RateEstimate ADD CONSTRAINT
+	FK_RateEstimate_Settlement FOREIGN KEY
+	(
+	IdDestiny
+	) REFERENCES dbo.Settlement
+	(
+	IdSettlement
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
 GO
 ALTER TABLE dbo.RateEstimate ADD CONSTRAINT
 	FK_RateEstimate_RateCategory FOREIGN KEY
@@ -26,6 +156,20 @@ ALTER TABLE dbo.RateEstimate ADD CONSTRAINT
 	 ON DELETE  NO ACTION 
 	
 GO
-ALTER TABLE dbo.RateEstimate SET (LOCK_ESCALATION = TABLE)
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.RateEstimateDetail ADD CONSTRAINT
+	FK_RateEstimateDetail_RateEstimate FOREIGN KEY
+	(
+	IdRateEstimate
+	) REFERENCES dbo.RateEstimate
+	(
+	IdRateEstimated
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+ALTER TABLE dbo.RateEstimateDetail SET (LOCK_ESCALATION = TABLE)
 GO
 COMMIT
