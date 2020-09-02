8db437a4482ff1d3f4bf602c885c6942ff59e9cc
ccano <carlos.cano@forzalatam.com>
2020-09-02 17:51:22 -0600

    NEW: reports and scripts according to user history

--- DeliveryBackOffice/Script/Reportes Operativos/Reporte 01 - Producto con tiempo de entrega vencido.sql
+++ DeliveryBackOffice/Script/Reportes Operativos/Reporte 01 - Producto con tiempo de entrega vencido.sql
@@ -57,4 +57,5 @@ FROM
 join deliverybackoffice.dbo.deliveryorderdetail dod with(nolock) on dod.Guide_Serie = SUBQ.Guide_Serie and dod.Guide_Number = SUBQ.Guide_Number and dod.StatusOrderId = 11
 join DeliveryBackOffice.dbo.DeliveryOrder do with(nolock) on do.Guide_Serie = SUBQ.Guide_Serie and do.Guide_Number = SUBQ.Guide_Number
 where dod.DateCreated <=  GETDATE() - 1
+AND (SUBQ.Rack_Position NOT LIKE '%(92)%' AND SUBQ.Rack_Position NOT LIKE '%(93)%')
 order by Days_Overdue
