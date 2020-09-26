21ea375ac34929a98837ab7784f2d93d577afd90
ccano <carlos.cano@forzalatam.com>
2020-09-26 13:03:44 -0600

    FIX: Customer and VisitPoint information

--- DeliveryBackOffice/Script/Script Insertar nuevo cliente(FLOR HERNANDEZ).sql
+++ DeliveryBackOffice/Script/Script Insertar nuevo cliente(FLOR HERNANDEZ).sql
@@ -4,7 +4,7 @@ SELECT * FROM DeliveryBackOffice.dbo.VisitPointClient WHERE CodeOfReference = 13
 SELECT * FROM DeliveryBackOffice.dbo.Customer WHERE IdCustomer = 35;
 
 INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject], [RegexEmail], [RegexFilename], [Abbreviation])
-VALUES ('FLOR DE MARIA HERNANDEZ RIVERA', 'FLOR HERNANDEZ', 'florh49@gmail.com','^.*solicitud.*$','^florh49@gmail.com$','^envios_.*\.xls$',' FLOR HERNANDEZ')
+VALUES ('FLOR DE MARIA HERNANDEZ RIVERA', 'FLOR HERNANDEZ', '@gmail.com','^.*solicitud.*$','^florh49@gmail.com$','^envios_.*\.xls$',' FLOR HERNANDEZ')
  
 INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-VALUES (13425,'FLOR HERNANDEZ',1,'GT', 35,'CCANO',GETDATE(),NULL,NULL,33,NULL,NULL,NULL,NULL,NULL,NULL)
+VALUES (13425,'FLOR HERNANDEZ',1,'GT', 138084,'CCANO',GETDATE(),NULL,NULL,35,NULL,NULL,NULL,NULL,NULL,NULL)
