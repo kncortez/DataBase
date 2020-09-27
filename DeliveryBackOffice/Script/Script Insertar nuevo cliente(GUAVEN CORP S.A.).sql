94fa346b132436e694c45823308e0a87eb4694f5
ccano <carlos.cano@forzalatam.com>
2020-09-26 18:01:06 -0600

    FIX: customer and visitpoint info

--- DeliveryBackOffice/Script/Script Insertar nuevo cliente(GUAVEN CORP S.A.).sql
+++ DeliveryBackOffice/Script/Script Insertar nuevo cliente(GUAVEN CORP S.A.).sql
@@ -5,9 +5,9 @@ SELECT * FROM DeliveryBackOffice.dbo.VisitPointClient WHERE CodeOfReference = 13
 
 INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], 
 [Domain], [RegexSubject], [RegexEmail], [RegexFilename], [Abbreviation])  
- VALUES ('GUAVEN CORP S.A.', 'GUAVEN CORP S.A.', '@guavencorp.com.gt', '^.*solicitud.*$',
- '^administracion@guavencorp.com$', '^envios_.*\.xls$','GUAVEN CORP S.A.');
+ VALUES ('GUAVEN CORP S.A.', 'GUAVEN CORP S.A.', '@guavencorp.com', '^.*solicitud.*$',
+ '^([\w\.\-]+)@.guavencorp.com$', '^envios_.*\.xls$','GUAVEN CORP');
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ( [CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],
  [VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13440,'GUAVEN CORP S.A.', 42,'GT', NULL,'APAZ',GETDATE(),NULL,NULL, 42,NULL,NULL,NULL,NULL,NULL,NULL)
+ VALUES (13440,'GUAVEN CORP S.A.', 138097,'GT', NULL,'CCANO',GETDATE(),NULL,NULL, 38,NULL,NULL,NULL,NULL,NULL,NULL)
