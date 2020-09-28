4828e961a6948a855ee8ef69693f81956cb2d4dc
dmolina <david.molina@forzalatam.com>
2020-09-28 16:05:57 -0600

    FP-20 ALTA DE 4 CLIENTES: Customers added successfully.

---
+++ DeliveryBackOffice/Script/Script Insertar nuevos clientes(INGRID DE DOMINGUEZ, EVELYN SANCHEZ, NATURA PHARMAKA, B&E DISTRIBUCIONES).sql
@@ -0,0 +1,42 @@
+INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], 
+[Domain], [RegexSubject], [RegexEmail], [RegexFilename], [Abbreviation])  
+ VALUES ('INGRID ELIZABETH RODRIGUEZ GARICA DE DOMINGUEZ', 'INGRID DE DOMINGUEZ', '@gmail.com', '^.*solicitud.*$',
+ '^dedominguez.ingrid@gmail.com$', '^envios_.*\.xls$','Ingrid de Domínguez');
+
+ INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ( [CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],
+ [VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+ VALUES (13450,'INGRID ELIZABETH RODRIGUEZ GARICA DE DOMINGUEZ', 1,'GT', 138106,'APAZ', GETDATE(), NULL, NULL, 44, '7 Calle 13-32 Quinta Samayoa Apto. C', 7, 'Guatemala', 'Guatemala', '40315208',
+ 'INGRID DE DOMINGUEZ');
+
+ INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], 
+[Domain], [RegexSubject], [RegexEmail], [RegexFilename], [Abbreviation])  
+ VALUES ('EVELYN YOHANA SANCHEZ MERIDA', 'ANASANZ816', '@gmail.com', '^.*solicitud.*$',
+ '^anasanz816@gmail.com$', '^envios_.*\.xls$', 'ANASANZ816');
+
+ INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ( [CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],
+ [VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+ VALUES (13455,'EVELYN YOHANA SANCHEZ MERIDA', 1,'GT', 138107,'APAZ', GETDATE(), NULL, NULL, 45, 
+ '21 Av. final Alameda Norte (extremo de buses transurbanos)', 18, 'Guatemala', 'Guatemala', '59793970',
+ 'ANASANZ816');
+
+  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], 
+[Domain], [RegexSubject], [RegexEmail], [RegexFilename], [Abbreviation])  
+ VALUES ('NATURA PHARMAKA', 'ANASANZ816', '@gmail.com', '^.*solicitud.*$',
+ '^evelynpaniaguag@gmail.com$', '^envios_.*\.xls$', 'NATURA PHARMAKA');
+
+ INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ( [CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],
+ [VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+ VALUES (13460,'NATURA PHARMAKA', 1,'GT', 138108,'APAZ', GETDATE(), NULL, NULL, 46, 
+ 'Km16.5 Carr. El Salvador, Crece Olmeca, Res. Rancho Verde, Manz.C Sec.I Casa 24', 0, 'Fraijanes', 'Guatemala', '51751300',
+ 'NATURA PHARMAKA');
+
+   INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], 
+[Domain], [RegexSubject], [RegexEmail], [RegexFilename], [Abbreviation])  
+ VALUES ('B&E DISTRIBUCIOHNES', 'B&E DISTRIBUCIOHNES', '@gmail.com', '^.*solicitud.*$',
+ '^samuel@distribucionesbye.com$', '^envios_.*\.xls$', 'B&E DISTRIBUCIOHNES');
+
+ INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ( [CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],
+ [VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+ VALUES (13465,'B&E DISTRIBUCIOHNES', 1,'GT', 138109,'APAZ', GETDATE(), NULL, NULL, 47, 
+ '6 Calle A 13-62 Colonia Quinta Samayoa', 7, 'Guatemala', 'Guatemala', '47397152',
+ 'B&E DISTRIBUCIOHNES');
