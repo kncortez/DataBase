a562a14a58ebb048d1c182a3b3a08193cf5f85b0
ccano <carlos.cano@forzalatam.com>
2020-10-13 17:11:48 -0600

    FIX: data truncate and values

--- DeliveryBackOffice/Script/Script alta de cliente y punto de visita.sql
+++ DeliveryBackOffice/Script/Script alta de cliente y punto de visita.sql
@@ -1,152 +1,130 @@
   -- JOSE LUIS PUAC GARCÍA
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('JOSE LUIS PUAC GARCIA', 'JOSE LUIS PUAC GARCIA', '@gmail.com','^.*solicitud.*$','^jlpuac11@gmail.com$'
- ,'^envios_.*\.xls$','JOSE LUIS PUAC GARCIA')
+ VALUES ('JOSE LUIS PUAC GARCIA', 'JOSE LUIS PUAC GARCIA', '@gmail.com','^.*solicitud.*$','^jlpuac11@gmail.com$' ,'^envios_.*\.xls$','JOSE LUIS PUAC GARCIA')
+ 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13501,'502 MOTO SPORT',8,'GT',138183,'ERIVAS',GETDATE(),NULL,NULL,50
- ,'1a calle 1-95 San Cristóbal, Zona 8 de Mixco',8,'MIXCO','GUATEMALA',24725358,'JOSE LUIS PUAC GARCIA')
+ VALUES (13501,'502 MOTO SPORT',1,'GT',138183,'CCANO',GETDATE(),NULL,NULL,50,'1a calle 1-95 San Cristóbal',1,'MIXCO','GUATEMALA',24725358,'JOSE LUIS PUAC GARCIA')
+
  
    -- RODOLFO DAVID IZAGUIRRE OLIVEROS
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('RODOLFO DAVID IZAGUIRRE OLIVEROS', 'RODOLFO DAVID IZAGUIRRE OLIVEROS', '@gmail.com','^.*solicitud.*$','^izaguirre.rodolfo@gmail.com$'
- ,'^envios_.*\.xls$','RODOLFO DAVID IZAGUIRRE OLIVEROS')
+ VALUES ('RODOLFO DAVID IZAGUIRRE OLIVEROS', 'RODOLFO DAVID IZAGUIRRE OLIVEROS', '@gmail.com','^.*solicitud.*$','^izaguirre.rodolfo@gmail.com$','^envios_.*\.xls$','RODOLFO DAVID')
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13506,'FRAGANCIAS WOW',1,'GT',138184,'ERIVAS',GETDATE(),NULL,NULL,51
- ,'14 avenida 3-51 zona 1',1,'GUATEMALA','GUATEMALA',35372097,'RODOLFO DAVID IZAGUIRRE OLIVEROS')
+ VALUES (13506,'FRAGANCIAS WOW',1,'GT',138184,'ERIVAS',GETDATE(),NULL,NULL,51,'14 avenida 3-51 zona 1',1,'GUATEMALA','GUATEMALA',35372097,'RODOLFO DAVID IZAGUIRRE OLIVEROS')
  
+
  -- HECTOR RONALDO MORALES ESPINOZA
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('HECTOR RONALDO MORALES ESPINOZA', 'HECTOR RONALDO MORALES ESPINOZA', '@gmail.com','^.*solicitud.*$','^tete.morales26.rm@gmail.com$'
- ,'^envios_.*\.xls$','HECTOR RONALDO MORALES ESPINOZA')
+ VALUES ('HECTOR RONALDO MORALES ESPINOZA', 'HECTOR RONALDO MORALES ESPINOZA', '@gmail.com','^.*solicitud.*$','^tete.morales26.rm@gmail.com$','^envios_.*\.xls$','HECTOR RONALDO')
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13511,'GUMMY MONSTER',1,'GT',138187,'ERIVAS',GETDATE(),NULL,NULL,52
- ,'Boca del Monte, Guatemala',0,'VILLA CANALES','GUATEMALA',55796895,'HECTOR RONALDO MORALES ESPINOZA')
+ VALUES (13511,'GUMMY MONSTER',1,'GT',138187,'ERIVAS',GETDATE(),NULL,NULL,52,'Boca del Monte, Guatemala',0,'VILLA CANALES','GUATEMALA',55796895,'HECTOR RONALDO MORALES ESPINOZA')
  
+
  -- SEMBRADIO
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('SEMBRADIO', 'SEMBRADIO', '@gmail.com','^.*solicitud.*$','^sembradioshop@gmail.com$'
- ,'^envios_.*\.xls$','SEMBRADIO')
+ VALUES ('SEMBRADIO', 'SEMBRADIO', '@gmail.com','^.*solicitud.*$','^sembradioshop@gmail.com$','^envios_.*\.xls$','SEMBRADIO')
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13516,'SEMBRADIO GT',1,'GT',138188,'ERIVAS',GETDATE(),NULL,NULL,53
- ,'12 calle 12-36 Zona 2 Ciudad Nueva',2,'GUATEMALA','GUATEMALA',55167852,'SEMBRADIO')
+ VALUES (13516,'SEMBRADIO GT',1,'GT',138188,'ERIVAS',GETDATE(),NULL,NULL,53,'12 calle 12-36 Zona 2 Ciudad Nueva',2,'GUATEMALA','GUATEMALA',55167852,'SEMBRADIO')
+
  
  -- KARIN JULISSA GARCIA PALENCIA
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('KARIN JULISSA GARCIA PALENCIA', 'KARIN JULISSA GARCIA PALENCIA', '@gmail.com','^.*solicitud.*$','^karingapa@gmail.com$'
- ,'^envios_.*\.xls$','KARIN JULISSA GARCIA PALENCIA')
+ VALUES ('KARIN JULISSA GARCIA PALENCIA', 'KARIN JULISSA GARCIA PALENCIA', '@gmail.com','^.*solicitud.*$','^karingapa@gmail.com$','^envios_.*\.xls$','KARIN JULISSA')
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13521,'CIENCIA Y PASIÓN SHOP',1,'GT',138189,'ERIVAS',GETDATE(),NULL,NULL,54
- ,'19 calle 47-48 Zona 5 Colonia Saravia',5,'GUATEMALA','GUATEMALA',54738279,'KARIN JULISSA GARCIA PALENCIA')
+ VALUES (13521,'CIENCIA Y PASIÓN SHOP',1,'GT',138189,'ERIVAS',GETDATE(),NULL,NULL,54,'19 calle 47-48 Zona 5 Colonia Saravia',5,'GUATEMALA','GUATEMALA',54738279,'KARIN JULISSA GARCIA PALENCIA')
  
   -- WORLD OF THE FASHION
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('WORLD OF THE FASHION', 'WORLD OF THE FASHION', '@hotmail.com','^.*solicitud.*$','^worldfashion-00@hotmail.com$'
- ,'^envios_.*\.xls$','WORLD OF THE FASHION')
+ VALUES ('WORLD OF THE FASHION', 'WORLD OF THE FASHION', '@hotmail.com','^.*solicitud.*$','^worldfashion-00@hotmail.com$','^envios_.*\.xls$','WORLD FASHION')
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13526,'WORLD OF THE FASHION',1,'GT',138196,'ERIVAS',GETDATE(),NULL,NULL,55
- ,'2 Avenida 10-35',1,'ESQUIPULAS','CHIQUIMULA',59084557,'WORLD OF THE FASHION')
+ VALUES (13526,'WORLD OF THE FASHION',1,'GT',138196,'ERIVAS',GETDATE(),NULL,NULL,55,'2 Avenida 10-35',1,'ESQUIPULAS','CHIQUIMULA',59084557,'WORLD OF THE FASHION')
  
+
   -- 502 AUTOTECH
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('502 AUTOTECH', '502 AUTOTECH', '@gmail.com','^.*solicitud.*$','^robertopuacgaria@gmail.com$'
- ,'^envios_.*\.xls$','502 AUTOTECH')
+ VALUES ('502 AUTOTECH', '502 AUTOTECH', '@gmail.com','^.*solicitud.*$','^robertopuacgaria@gmail.com$','^envios_.*\.xls$','502 AUTOTECH')
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13531,'502 AUTOTECH',1,'GT',138194,'ERIVAS',GETDATE(),NULL,NULL,56
- ,'Calle Salida a Caminos frente a hotel ejecutivo',2,'JUTIAPA','JUTIAPA',41351334,'502 AUTOTECH')
+ VALUES (13531,'502 AUTOTECH',1,'GT',138194,'ERIVAS',GETDATE(),NULL,NULL,56,'Calle Salida a Caminos frente a hotel ejecutivo',2,'JUTIAPA','JUTIAPA',41351334,'502 AUTOTECH')
  
+
    -- JUAN JOSE CASTILLO
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('JUAN JOSE CASTILLO', 'JUAN JOSE CASTILLO', '@gmail.com','^.*solicitud.*$','^chapinmallpublicidad@gmail.com$'
- ,'^envios_.*\.xls$','JUAN JOSE CASTILLO')
+ VALUES ('JUAN JOSE CASTILLO', 'JUAN JOSE CASTILLO', '@gmail.com','^.*solicitud.*$','^chapinmallpublicidad@gmail.com$','^envios_.*\.xls$','JUAN JOSE')
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13536,'GUATEMAYOREO',1,'GT',138191,'ERIVAS',GETDATE(),NULL,NULL,57
- ,'Condominio Valles de Navarra, Residencial haciendo navarra #39B San Jose Pinula',0,'GUATEMALA','GUATEMALA',66759725,'JUAN JOSE CASTILLO')
+ VALUES (13536,'GUATEMAYOREO',1,'GT',138191,'ERIVAS',GETDATE(),NULL,NULL,57,'Condominio Valles de Navarra, Residencial haciendo navarra #39B San Jose Pinula',0,'GUATEMALA','GUATEMALA',66759725,'JUAN JOSE CASTILLO')
  
+
    -- ALVISA
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('ALVISA', 'ALVISA', '@gmail.com','^.*solicitud.*$','^vielmannjuanmarcos@gmail.com$'
- ,'^envios_.*\.xls$','ALVISA')
+ VALUES ('ALVISA', 'ALVISA', '@gmail.com','^.*solicitud.*$','^vielmannjuanmarcos@gmail.com$','^envios_.*\.xls$','ALVISA')
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13541,'ALVISA',1,'GT',138182,'ERIVAS',GETDATE(),NULL,NULL,58
- ,'3 Calle 22-84 Colonia los Calistemos',8,'JUTIAPA','JUTIAPA',42379749,'ALVISA')
+ VALUES (13541,'ALVISA',1,'GT',138182,'ERIVAS',GETDATE(),NULL,NULL,58,'3 Calle 22-84 Colonia los Calistemos',8,'JUTIAPA','JUTIAPA',42379749,'ALVISA')
  
+
     -- E MARKET
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('E MARKET', 'E MARKET', '@gmail.com','^.*solicitud.*$','^ferdyoliva93@gmail.com$'
- ,'^envios_.*\.xls$','E MARKET')
+ VALUES ('E MARKET', 'E MARKET', '@gmail.com','^.*solicitud.*$','^ferdyoliva93@gmail.com$','^envios_.*\.xls$','E MARKET')
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13546,'E MARKET',1,'GT',138193,'ERIVAS',GETDATE(),NULl,NULL,59
- ,'Casa P 27 Sector uno vistas del Prado',18,'GUATEMALA','GUATEMALA',41770345 / 59748357,'E MARKET')
+ VALUES (13546,'E MARKET',1,'GT',138193,'ERIVAS',GETDATE(),NULl,NULL,59,'Casa P 27 Sector uno vistas del Prado',18,'GUATEMALA','GUATEMALA',41770345 / 59748357,'E MARKET')
  
+
     -- GRUPO MCV, S.A.
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('GRUPO MCV, S.A.', 'GRUPO MCV, S.A.', '@guateclic.com','^.*solicitud.*$','^victor.fernandez@guateclic.com$'
- ,'^envios_.*\.xls$','GRUPO MCV, S.A.')
+ VALUES ('GRUPO MCV, S.A.', 'GRUPO MCV, S.A.', '@guateclic.com','^.*solicitud.*$','^victor.fernandez@guateclic.com$','^envios_.*\.xls$','GRUPO MCV, S.A.')
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13551,'GUATECLIC',1,'GT',138195,'ERIVAS',GETDATE(),NULl,NULL,60
- ,'25 Avenida 1-89 Vista Hermosa 2 Edificio Insigne',15,'GUATEMALA','GUATEMALA',48294492 / 22967777,'GRUPO MCV, S.A.')
+ VALUES (13551,'GUATECLIC',1,'GT',138195,'ERIVAS',GETDATE(),NULl,NULL,60,'25 Avenida 1-89 Vista Hermosa 2 Edificio Insigne',15,'GUATEMALA','GUATEMALA',48294492 / 22967777,'GRUPO MCV, S.A.')
  
+
   -- GRUPO MCV, S.A.
  INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
- VALUES ('SARANE GT', 'SARANE GT', '@gmail.com','^.*solicitud.*$','^sanare.gt@gmail.com$'
- ,'^envios_.*\.xls$','SARANE GT')
+ VALUES ('SARANE GT', 'SARANE GT', '@gmail.com','^.*solicitud.*$','^sanare.gt@gmail.com$','^envios_.*\.xls$','SARANE GT')
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
- VALUES (13561,'SANARE GT',1,'GT',138197,'ERIVAS',GETDATE(),NULl,NULL,61
- ,'9 Calle 6-78 Residenciales del Norte',17,'GUATEMALA','GUATEMALA',59370854,'GRUPO MCV, S.A.')
- 
- 
- 
- 
- 
- 
- 
- 
- 
+ VALUES (13556,'SANARE GT',1,'GT',138197,'ERIVAS',GETDATE(),NULl,NULL,61,'9 Calle 6-78 Residenciales del Norte',17,'GUATEMALA','GUATEMALA',59370854,'SANARE GT')
  
