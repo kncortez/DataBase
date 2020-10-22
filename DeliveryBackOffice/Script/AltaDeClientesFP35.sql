1b526555e09b58373474bf4f8912cf60be0f588e
ccano <carlos.cano@forzalatam.com>
2020-10-22 17:19:40 -0600

    FIX: values according to history FP-35

--- DeliveryBackOffice/Script/AltaDeClientesFP35.sql
+++ DeliveryBackOffice/Script/AltaDeClientesFP35.sql
@@ -2,12 +2,12 @@
 
 -- DIVINA'S SHOE
   INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject], [RegexEmail], [RegexFilename], [Abbreviation]) 
-  VALUES ('DIVINAS SHOE', 'DIVINAS SHOE', '@hotmail.com','^.*solicitud.*$','^rosamaru1_1994@hotmail.com$','^envios_.*\.xls$','DIVINAS SHOE')
+  VALUES ('DIVINA''S SHOE', 'DIVINA''S SHOE', '@hotmail.com','^.*solicitud.*$','^rosamaru1_1994@hotmail.com$','^envios_.*\.xls$','DIVINAS SHOE')
   
   INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],
   [DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-  VALUES (13657,'DIVINAS SHOE',1,'GT',138265,'ERIVAS',GETDATE(),NULL,NULL,81,''
-  ,3,'GUATEMALA','GUATEMALA',59880333,'DIVINAS SHOE')
+  VALUES (13657,'DIVINA''S SHOE',1,'GT',138265,'ERIVAS',GETDATE(),NULL,NULL,85,''
+  ,3,'GUATEMALA','GUATEMALA','59880333','DIVINAS SHOE')
   
   
   -- CARLOS ALEJANDRO MORALES VEGA
@@ -16,8 +16,8 @@
   
   INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],
   [DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-  VALUES (13662,'CARLOS ALEJANDRO MORALES VEGA',1,'GT',138267,'ERIVAS',GETDATE(),NULL,NULL,82,''
-  ,6,'VILLA NUEVA','GUATEMALA',52589407,'CARLOS MORALES')
+  VALUES (13662,'CARLOS ALEJANDRO MORALES VEGA',1,'GT',138267,'ERIVAS',GETDATE(),NULL,NULL,86,''
+  ,6,'VILLA NUEVA','GUATEMALA','52589407','CARLOS MORALES')
   
   
   -- HECHO A MANO
@@ -26,18 +26,18 @@
   
   INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],
   [DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-  VALUES (13667,'HECHO A MANO',1,'GT',138266,'ERIVAS',GETDATE(),NULL,NULL,83,''
-  ,8,'MIXCO','GUATEMALA',55950914,'HECHO A MANO')
+  VALUES (13667,'HECHO A MANO',1,'GT',138266,'ERIVAS',GETDATE(),NULL,NULL,87,''
+  ,8,'MIXCO','GUATEMALA','55950914','HECHO A MANO')
   
   
    -- TIENDA 365
   INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject], [RegexEmail], [RegexFilename], [Abbreviation]) 
-  VALUES ('TIENDA 365', 'TIENDA 365', '@gmail.com','^.*solicitud.*$','^tienda365@guatemala@gmail.com$','^envios_.*\.xls$','TIENDA 365')
+  VALUES ('TIENDA 365', 'TIENDA 365', '@gmail.com','^.*solicitud.*$','^tienda365guatemala@gmail.com$','^envios_.*\.xls$','TIENDA 365')
   
   INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],
   [DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-  VALUES (13672,'TIENDA 365',1,'GT',138263,'ERIVAS',GETDATE(),NULL,NULL,84,'San Miguel Petapa, Guatemala'
-  ,0,'GUATEMALA','GUATEMALA',37611661,'TIENDA 365')
+  VALUES (13672,'TIENDA 365',1,'GT',138263,'ERIVAS',GETDATE(),NULL,NULL,88,'San Miguel Petapa, Guatemala'
+  ,0,'GUATEMALA','GUATEMALA','37611661','TIENDA 365')
   
   
    -- BYRON ESTUARDO MARROQUIN CASTRO
@@ -46,8 +46,8 @@
   
   INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],
   [DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-  VALUES (13677,'CAS MAR',1,'GT',138264,'ERIVAS',GETDATE(),NULL,NULL,85,''
-  ,11,'GUATEMALA','GUATEMALA',54370130,'BYRON ESTUARDO')
+  VALUES (13677,'CAS MAR',1,'GT',138264,'ERIVAS',GETDATE(),NULL,NULL,89,''
+  ,11,'GUATEMALA','GUATEMALA','54370130','BYRON ESTUARDO')
   
   
    -- TOMAX, S.A.
@@ -56,8 +56,8 @@
   
   INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],
   [DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-  VALUES (13682,'TOMAX, S.A.',1,'GT',138268,'ERIVAS',GETDATE(),NULL,NULL,86,''
-  ,12,'GUATEMALA','GUATEMALA',56917239,'TOMAX, S.A.')
+  VALUES (13682,'TOMAX, S.A.',1,'GT',138268,'ERIVAS',GETDATE(),NULL,NULL,90,''
+  ,12,'GUATEMALA','GUATEMALA','56917239','TOMAX, S.A.')
   
   
   -- TEXTILES LO NUESTRO
@@ -66,8 +66,8 @@
   
   INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],
   [DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-  VALUES (13687,'TEXTILES LO NUESTRO',1,'GT',138256,'ERIVAS',GETDATE(),NULL,NULL,87,''
-  ,8,'HUEHUETENANGO','HUEHUETENANGO',45408065,'TEXTILES LO NUESTRO')
+  VALUES (13687,'TEXTILES LO NUESTRO',1,'GT',138256,'ERIVAS',GETDATE(),NULL,NULL,91,''
+  ,8,'HUEHUETENANGO','HUEHUETENANGO','45408065','TEXTILES LO NUESTRO')
   
   
   -- D-ART GUATEMALA
@@ -76,8 +76,8 @@
   
   INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],
   [DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-  VALUES (13692,'D-ART GUATEMALA',1,'GT',138258,'ERIVAS',GETDATE(),NULL,NULL,88,''
-  ,9,'MIXCO','GUATEMALA',58806014 / 30193411,'D-ART GUATEMALA')
+  VALUES (13692,'D-ART GUATEMALA',1,'GT',138258,'ERIVAS',GETDATE(),NULL,NULL,92,''
+  ,9,'MIXCO','GUATEMALA','58806014 / 30193411','D-ART GUATEMALA')
   
   
    -- ANDRES OCHOA
@@ -86,8 +86,8 @@
   
   INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],
   [DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-  VALUES (13697,'MULTI SERVICIOS SPEED NET',1,'GT',138257,'ERIVAS',GETDATE(),NULL,NULL,89,'Puerto San José'
-  ,0,'SAN JOSÉ','ESCUINTLA',56304405,'ANDRES OCHOA')
+  VALUES (13697,'MULTI SERVICIOS SPEED NET',1,'GT',138257,'ERIVAS',GETDATE(),NULL,NULL,93,'Puerto San José'
+  ,0,'SAN JOSÉ','ESCUINTLA','56304405','ANDRES OCHOA')
   
   
   -- SANDY PEREZ
@@ -96,8 +96,8 @@
   
   INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],
   [DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-  VALUES (13702,'SANDALIAS A LA MODA',1,'GT',138244,'ERIVAS',GETDATE(),NULL,NULL,90,'Colonia Quinta Samayoa, Ciudad de Guatemala'
-  ,7,'GUATEMALA','GUATEMALA',59330027,'SANDY PEREZ')
+  VALUES (13702,'SANDALIAS A LA MODA',1,'GT',138244,'ERIVAS',GETDATE(),NULL,NULL,94,'Colonia Quinta Samayoa, Ciudad de Guatemala'
+  ,7,'GUATEMALA','GUATEMALA','59330027','SANDY PEREZ')
   
   
     -- ARGO INVERSIONES INDUSTRIALES
@@ -106,6 +106,6 @@
   
   INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],
   [DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-  VALUES (13707,'ARGO INVERSIONES INDUSTRIALES',1,'GT',138245,'ERIVAS',GETDATE(),NULL,NULL,91,''
-  ,15,'GUATEMALA','GUATEMALA',41582046,'ARGO INVERSIONES INDUSTRIALES')
+  VALUES (13707,'ARGO INVERSIONES INDUSTRIALES',1,'GT',138245,'ERIVAS',GETDATE(),NULL,NULL,95,''
+  ,15,'GUATEMALA','GUATEMALA','41582046','ARGO INVERSIONES INDUSTRIALES')
   
