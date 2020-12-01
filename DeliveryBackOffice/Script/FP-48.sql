489adfb43802b4e5aae906b480079142f2deb279
jhernandez <josselyn.hernandez@forzalatam.com>
2020-12-01 17:02:28 -0600

    Fixed

--- DeliveryBackOffice/Script/FP-48.sql
+++ DeliveryBackOffice/Script/FP-48.sql
@@ -4,4 +4,4 @@
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])  
  VALUES (13262,'DISTRIBUIDORA 502',1,'GT',138697,'JHERNANDEZ',GETDATE(),NULL,NULL,13,NULL,NULL,NULL,NULL,NULL,NULL)
  
- update [DeliveryBackOffice].[dbo].[Customer] SET RegexEmail = '^guatedepot@icloud.com|aurumyforcia@icloud.com|DISTRIBUIDORA502@ICLOUD.COM$$' WHERE IdCustomer = 13
+ update [DeliveryBackOffice].[dbo].[Customer] SET RegexEmail = '^guatedepot@icloud.com|aurumyforcia@icloud.com|distribuidora502@icloud.com$' WHERE IdCustomer = 13
