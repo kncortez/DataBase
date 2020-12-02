cc7ce38c62a67ad090978154e75275858a19b519
jhernandez <josselyn.hernandez@forzalatam.com>
2020-12-01 18:59:20 -0600

    Added name changed

--- DeliveryBackOffice/Script/FP-48.sql
+++ DeliveryBackOffice/Script/FP-48.sql
@@ -1,7 +1,9 @@
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])  
- VALUES (13261,'AURUM & FORCIA',1,'GT',138695'JHERNANDEZ',GETDATE(),NULL,NULL,13,NULL,NULL,NULL,NULL,NULL,NULL)
+ VALUES (13261,'AURUM & FORCIA',1,'GT',138695,'JHERNANDEZ',GETDATE(),NULL,NULL,13,NULL,NULL,NULL,NULL,NULL,NULL)
 
  INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])  
  VALUES (13262,'DISTRIBUIDORA 502',1,'GT',138697,'JHERNANDEZ',GETDATE(),NULL,NULL,13,NULL,NULL,NULL,NULL,NULL,NULL)
  
  update [DeliveryBackOffice].[dbo].[Customer] SET RegexEmail = '^guatedepot@icloud.com|aurumyforcia@icloud.com|distribuidora502@icloud.com$' WHERE IdCustomer = 13
+ update [DeliveryBackOffice].[dbo].[Customer] SET Name, Description = 'NEGOCIOS CORPORATIVOS HR, S.A.' WHERE IdCustomer = 13
+ update [DeliveryBackOffice].[dbo].[Customer] SET Abbreviation = 'NEGOCIOS CORPORATIVOS HR' WHERE IdCustomer = 13
