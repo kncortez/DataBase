465d8102c5dff88ab3cd053175e6e8deae8c3edb
jhernandez <josselyn.hernandez@forzalatam.com>
2020-12-01 19:09:10 -0600

    Fixed and Tested

--- DeliveryBackOffice/Script/FP-48.sql
+++ DeliveryBackOffice/Script/FP-48.sql
@@ -5,5 +5,6 @@
  VALUES (13262,'DISTRIBUIDORA 502',1,'GT',138697,'JHERNANDEZ',GETDATE(),NULL,NULL,13,NULL,NULL,NULL,NULL,NULL,NULL)
  
  update [DeliveryBackOffice].[dbo].[Customer] SET RegexEmail = '^guatedepot@icloud.com|aurumyforcia@icloud.com|distribuidora502@icloud.com$' WHERE IdCustomer = 13
- update [DeliveryBackOffice].[dbo].[Customer] SET Name, Description = 'NEGOCIOS CORPORATIVOS HR, S.A.' WHERE IdCustomer = 13
+ update [DeliveryBackOffice].[dbo].[Customer] SET Name = 'NEGOCIOS CORPORATIVOS HR, S.A.' WHERE IdCustomer = 13
+ update [DeliveryBackOffice].[dbo].[Customer] SET Description = 'NEGOCIOS CORPORATIVOS HR, S.A.' WHERE IdCustomer = 13 
  update [DeliveryBackOffice].[dbo].[Customer] SET Abbreviation = 'NEGOCIOS CORPORATIVOS HR' WHERE IdCustomer = 13
