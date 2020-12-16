be0118c2e8d734584d67a6f78d242444e6f5f7fa
caquino <cesar.aquino@forzalatam.com>
2020-12-15 23:48:47 -0600

    ##ADD nueva tabla para relacionar hubs con vpc

---
+++ DeliveryBackOffice/Script/1. SCRIPT-INSERT- KindOfVPClient.sql
@@ -0,0 +1,9 @@
+INSERT INTO [DeliveryBackOffice].[dbo].[KindOfVPClient]
+           ([KindOfVPName]
+           ,[KindOfVPStatus]
+           ,[TokenCreated]
+           ,[DateCreated]
+           ,[TokenUpdate]
+           ,[DateUpdated])
+     VALUES
+           ('HUB',1,'SYS-CAQUINO',GETDATE(),NULL,NULL)
