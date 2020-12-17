49f3c950908d53711030444d0a93d99eaf76e941
ccano <carlos.cano@forzalatam.com>
2020-12-17 17:50:54 -0600

    #FP-52 DELETE: extra field NEW: customer and visitpoint FDE LATIN VOX S.A

--- DeliveryBackOffice/Script/FP-52 Alta de 4 Clientes y 4 Punto de venta.sql
+++ DeliveryBackOffice/Script/FP-52 Alta de 4 Clientes y 4 Punto de venta.sql
@@ -1,18 +1,22 @@
 INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
-[RegexEmail], [RegexFilename], [Abbreviation], [COD])
-VALUES ('ESTEFANY ALEJANDRA ALDANA MOLINA', 'ESTEFANY ALEJANDRA ALDANA MOLINA', '@gmail.com','^.*solicitud.*$','^estef963almo@gmail.com$','^envios_.*\.xls$','ESTEFANY ALDANA', 1)	
+[RegexEmail], [RegexFilename], [Abbreviation])
+VALUES ('ESTEFANY ALEJANDRA ALDANA MOLINA', 'ESTEFANY ALEJANDRA ALDANA MOLINA', '@gmail.com','^.*solicitud.*$','^estef963almo@gmail.com$','^envios_.*\.xls$','ESTEFANY ALDANA')	
 
 INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
-[RegexEmail], [RegexFilename], [Abbreviation], [COD])
-VALUES ('IVANIA CATALINA DE LEON GONZALEZ', 'IVANIA CATALINA DE LEON GONZALEZ', '@gmail.com','^.*solicitud.*$','^margothtrend@gmail.com$','^envios_.*\.xls$','IVANIA DE LEON', 1)
+[RegexEmail], [RegexFilename], [Abbreviation])
+VALUES ('IVANIA CATALINA DE LEON GONZALEZ', 'IVANIA CATALINA DE LEON GONZALEZ', '@gmail.com','^.*solicitud.*$','^margothtrend@gmail.com$','^envios_.*\.xls$','IVANIA DE LEON')
 
 INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
-[RegexEmail], [RegexFilename], [Abbreviation], [COD])
-VALUES ('GERARDO STIVEN SERMEÑO CONSTANZA', 'GERARDO STIVEN SERMEÑO CONSTANZA', '@gmail.com','^.*solicitud.*$','^gs4181@gmail.com$','^envios_.*\.xls$','GERARDO SERMEÑO', 1)
+[RegexEmail], [RegexFilename], [Abbreviation])
+VALUES ('GERARDO STIVEN SERMEÑO CONSTANZA', 'GERARDO STIVEN SERMEÑO CONSTANZA', '@gmail.com','^.*solicitud.*$','^gs4181@gmail.com$','^envios_.*\.xls$','GERARDO SERMEÑO')
 
 INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
-[RegexEmail], [RegexFilename], [Abbreviation], [COD])
-VALUES ('ADELAS FASHIONS', 'ADELAS FASHIONS', '@gmail.com','^.*solicitud.*$','^adelasfachiohn3@gmail.com$','^envios_.*\.xls$','ADELAS FASHION', 1)
+[RegexEmail], [RegexFilename], [Abbreviation])
+VALUES ('ADELAS FASHIONS', 'ADELAS FASHIONS', '@gmail.com','^.*solicitud.*$','^adelasfachiohn3@gmail.com$','^envios_.*\.xls$','ADELAS FASHION')
+
+INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
+[RegexEmail], [RegexFilename], [Abbreviation])
+VALUES ('FDE LATIN VOX S.A', 'FDE LATIN VOX', '@gmail.com','^.*solicitud.*$','^vardon2@gmail.com$','^envios_.*\.xls$','FDE LATIN VOX')
 
 INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
@@ -33,3 +37,8 @@ INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[De
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
 VALUES (14633,'ADELAS FASHION',1,'GT',138783,'JHERNANDEZ',GETDATE(),NULL,NULL,275,NULL,NULL,NULL,NULL,NULL,NULL)
+
+INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
+[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
+[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+VALUES (14638,'LATIN VOX',1,'GT',138809,'CCANO',GETDATE(),NULL,NULL,276,NULL,NULL,NULL,NULL,NULL,NULL)
