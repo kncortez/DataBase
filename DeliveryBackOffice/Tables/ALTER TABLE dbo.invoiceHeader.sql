5ff78e69df69a8473a9cc2e4daebb96765b5b804
JorgeMurilloForza <jorge.murillo@forzalatam.com>
2021-01-20 11:16:58 -0600

    Alter tabla invoice

--- DeliveryBackOffice/Tables/ALTER TABLE dbo.invoiceHeader.sql
+++ DeliveryBackOffice/Tables/ALTER TABLE dbo.invoiceHeader.sql
@@ -1,3 +1,16 @@
+Use DeliveryBackOffice
+go
+
+IF (select ISNULL(c.COLUMN_NAME,'')
+FROM INFORMATION_SCHEMA.COLUMNS c
+WHERE COLUMN_NAME = 'systemOperation' AND TABLE_NAME = 'invoiceHeader') <> ''
+BEGIN
+	ALTER TABLE DeliveryBackOffice.dbo.[invoiceHeader]
+	DROP COLUMN systemOperation;
+
+END 
+
+
 IF (select ISNULL(c.COLUMN_NAME,'')
 FROM INFORMATION_SCHEMA.COLUMNS c
 WHERE COLUMN_NAME = 'systemOperation' AND TABLE_NAME = 'invoiceHeader') = ''
