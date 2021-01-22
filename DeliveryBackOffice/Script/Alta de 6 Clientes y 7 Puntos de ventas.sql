26de64784d451209a593dbe3c39acf6764987cdd
ccano <carlos.cano@forzalatam.com>
2021-01-21 20:36:46 -0600

    #FP-56 NEW: customers and visitpoints

---
+++ DeliveryBackOffice/Script/Alta de 6 Clientes y 7 Puntos de ventas.sql
@@ -0,0 +1,64 @@
+-- JORGE ARMANDO MEDRANO GRAJEDA
+INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
+[RegexEmail], [RegexFilename], [Abbreviation])
+VALUES ('JORGE ARMANDO MEDRANO GRAJEDA', 'JORGE MEDRANO', '@hotmail.com','^.*solicitud.*$','^jorgemedrano29@hotmail.com$','^envios_.*\.xls$','JORGE MEDRANO')
+
+INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
+[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
+[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+VALUES (14715,'JORMEGA',1,'GT',138948,'CCANO',GETDATE(),NULL,NULL,288,NULL,NULL,NULL,NULL,NULL,NULL)
+
+-- SHARON NATALY HUB FLORIAN
+INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
+[RegexEmail], [RegexFilename], [Abbreviation])
+VALUES ('SHARON NATALY HUB FLORIAN', 'SHARON HUB', '@hotmail.com','^.*solicitud.*$','^sharon97_@hotmail.com$','^envios_.*\.xls$','SHARON HUB')
+
+INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
+[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
+[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+VALUES (14720,'DRIA_SHOP.GT',1,'GT',138955,'CCANO',GETDATE(),NULL,NULL,289,NULL,NULL,NULL,NULL,NULL,NULL)
+
+-- CORPORACION MAGNAGRO S.A.
+INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
+[RegexEmail], [RegexFilename], [Abbreviation])
+VALUES ('CORPORACION MAGNAGRO S.A.', 'CORPORACION MAGNAGRO', '@gmail.com','^.*solicitud.*$','^magnagrocontable@gmail.com$','^envios_.*\.xls$','CORPORACION MAGNAGRO')
+
+INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
+[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
+[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+VALUES (14725,'MAGNAGRO S.A.',1,'GT',138960,'CCANO',GETDATE(),NULL,NULL,290,NULL,NULL,NULL,NULL,NULL,NULL)
+
+-- ITEMS INDUSTRIALES S.A.
+INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
+[RegexEmail], [RegexFilename], [Abbreviation])
+VALUES ('ITEMS INDUSTRIALES S.A.', 'ITEMS INDUSTRIALES', '@itemsind.com','^.*solicitud.*$','^([\w\.\-]+)@itemsind.com$','^envios_.*\.xls$','ITEMS INDUSTRIALES')
+
+INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
+[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
+[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+VALUES (14730,'ITEMS INDUSTRIALES CARR. EL SALVADOR',1,'GT',138961,'CCANO',GETDATE(),NULL,NULL,291,NULL,NULL,NULL,NULL,NULL,NULL)
+
+INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
+[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
+[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+VALUES (14735,'ITEMS INDUSTRIALES ATANASIO',1,'GT',138962,'CCANO',GETDATE(),NULL,NULL,291,NULL,NULL,NULL,NULL,NULL,NULL)
+
+-- ANDREA MARIA VIDES RODRIGUEZ
+INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
+[RegexEmail], [RegexFilename], [Abbreviation])
+VALUES ('ANDREA MARIA VIDES RODRIGUEZ', 'ANDREA VIDES', '@hotmail.com','^.*solicitud.*$','^tallerarana@hotmail.com$','^envios_.*\.xls$','ANDREA VIDES')
+
+INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
+[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
+[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+VALUES (14740,'LUNA LUNERA JUGUETES ONLINE',1,'GT',138963,'CCANO',GETDATE(),NULL,NULL,292,NULL,NULL,NULL,NULL,NULL,NULL)
+
+-- FDE DIVISION DIGITAL, SOCIEDAD ANONIMA
+INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
+[RegexEmail], [RegexFilename], [Abbreviation])
+VALUES ('FDE DIVISION DIGITAL, SOCIEDAD ANONIMA', 'DIVISION DIGITAL', '@digitalcentroameica.com','^.*solicitud.*$','^([\w\.\-]+)@digitalcentroameica.com$','^envios_.*\.xls$','DIVISION DIGITAL')
+
+INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
+[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
+[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+VALUES (14745,'DIVISIÓN DIGITAL',1,'GT',138964,'CCANO',GETDATE(),NULL,NULL,293,NULL,NULL,NULL,NULL,NULL,NULL)
