916e4dd0d3d72480cb5919d59c114327a20704b0
ccano <carlos.cano@forzalatam.com>
2021-01-27 12:34:08 -0600

    FIX: information

--- DeliveryBackOffice/Script/INSERT_FP_57.sql
+++ DeliveryBackOffice/Script/INSERT_FP_57.sql
@@ -1,58 +1,56 @@
 -- EJEMPLO DE NUEVO CLIENTE 
 INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
-VALUES ('VICTORIA ALEJANDRA FERNANDEZ CARRILLO', 'VICTORIA ALEJANDRA FERNANDEZ CARRILLO', '@gmail.com','^.*solicitud.*$','^vickyfercar90@gmail.com$','^envios_.*\.xls$','KEITECH');
+VALUES ('VICTORIA ALEJANDRA FERNANDEZ CARRILLO', 'VICTORIA FERNANDEZ', '@gmail.com','^.*solicitud.*$','^vickyfercar90@gmail.com$','^envios_.*\.xls$','VICTORIA FERNANDEZ');
 
 INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
-VALUES ('CORFASE, S.A', 'CORFASE, S.A', '@gmail.com','^.*solicitud.*$','^bodega2.corfasesa@gmail.com$','^envios_.*\.xls$','CORFASESA')
-
-
-INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
-[RegexEmail], [RegexFilename], [Abbreviation])
-VALUES ('JUSTO MANUEL SAJBÍN RODAS', 'JUSTO MANUEL SAJBÍN RODAS', '@outlook.com','^.*solicitud.*$','^corporacionjar@outlook.com$','^envios_.*\.xls$','ISHOES OUTLET')
-
+VALUES ('CORFASE, S.A', 'CORFASE, S.A', '@gmail.com','^.*solicitud.*$','^bodega2.corfasesa@gmail.com$','^envios_.*\.xls$','CORFASE')
 
 INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
-VALUES ('JUSTO MANUEL SAJBÍN RODAS', 'JUSTO MANUEL SAJBÍN RODAS', '@outlook.com','^.*solicitud.*$','^corporacionjar@outlook.com$','^envios_.*\.xls$','RUBEAUTIFUL')
+VALUES ('JUSTO MANUEL SAJBÍN RODAS', 'JUSTO SAJBÍN', '@outlook.com','^.*solicitud.*$','^corporacionjar@outlook.com$','^envios_.*\.xls$','JUSTO SAJBÍN')
 
 INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
-VALUES ('JUSTO MANUEL SAJBÍN RODAS', 'JUSTO MANUEL SAJBÍN RODAS', '@outlook.com','^.*solicitud.*$','^corporacionjar@outlook.com$','^envios_.*\.xls$','PETRA')
+VALUES ('BANCO INTERNACIONAL S.A.', 'BANCO INTERNACIONAL', '@outlook.com','^.*solicitud.*$','^fernandoblanco.mejia@outlook.com$','^envios_.*\.xls$','BANCO INTERNACIONAL')
 
 INSERT INTO [DeliveryBackOffice].[dbo].[Customer] ([Name], [Description], [Domain], [RegexSubject],
 [RegexEmail], [RegexFilename], [Abbreviation])
-VALUES ('BANCO INTERNACIONAL S.A.', 'BANCO INTERNACIONAL S.A.', '@outlook.com','^.*solicitud.*$','^fernandoblanco.mejia@outlook.com$','^envios_.*\.xls$','INTERBANCO')
-
+VALUES ('FOUR-S FASHION, S.A.', 'FOUR-S FASHION', '@gmail.com','^.*solicitud.*$','^four.sfashioncjm@gmail.com$','^envios_.*\.xls$','FOUR-S FASHION')
 
 -- EJEMPLO DE NUEVO PUNTO
 INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-VALUES (138845,'CASH LOGISTICS CENTRAL',1,'GT',14770,'CCANO',GETDATE(),NULL,NULL,283,NULL,NULL,NULL,NULL,NULL,NULL);
+VALUES (14750,'KEITECH',1,'GT',139038,'CCANO',GETDATE(),NULL,NULL,294,NULL,NULL,NULL,NULL,NULL,NULL);
 
 INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-VALUES (138840,'CASH LOGISTICS AGENCIA TECULUTAN',1,'GT',14771,'CCANO',GETDATE(),NULL,NULL,283,NULL,NULL,NULL,NULL,NULL,NULL);
+VALUES (14755,'CORFASESA',1,'GT',139037,'CCANO',GETDATE(),NULL,NULL,295,NULL,NULL,NULL,NULL,NULL,NULL);
 
 INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-VALUES (138841,'CASH LOGISTICS AGENCIA ESCUINTLA',1,'GT',14772,'CCANO',GETDATE(),NULL,NULL,283,NULL,NULL,NULL,NULL,NULL,NULL);
+VALUES (14760,'ISHOES OUTLET',1,'GT',139034,'CCANO',GETDATE(),NULL,NULL,296,NULL,NULL,NULL,NULL,NULL,NULL);
 
 INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-VALUES (138842,'CASH LOGISTICS AGENCIA MAZATENANGO',1,'GT',14773,'CCANO',GETDATE(),NULL,NULL,283,NULL,NULL,NULL,NULL,NULL,NULL);
+VALUES (14761,'RUBEAUTIFUL',1,'GT',139035,'CCANO',GETDATE(),NULL,NULL,296,NULL,NULL,NULL,NULL,NULL,NULL);
 
 INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-VALUES (138843,'CASH LOGISTICS AGENCIA PETÉN',1,'GT',14774,'CCANO',GETDATE(),NULL,NULL,283,NULL,NULL,NULL,NULL,NULL,NULL);
+VALUES (14762,'PETRA',1,'GT',139036,'CCANO',GETDATE(),NULL,NULL,296,NULL,NULL,NULL,NULL,NULL,NULL);
 
 INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
 [StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
 [CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
-VALUES (138844,'CASH LOGISTICS AGENCIA XELA',1,'GT',14775,'CCANO',GETDATE(),NULL,NULL,283,NULL,NULL,NULL,NULL,NULL,NULL);
+VALUES (14765,'INTERBANCO',1,'GT',139032,'CCANO',GETDATE(),NULL,NULL,297,NULL,NULL,NULL,NULL,NULL,NULL);
+
+INSERT INTO [DeliveryBackOffice].[dbo].[VisitPointClient] ([CodeOfReference],[DescriptionOfClient],
+[StatusClient],[CountryId],[VisitPointId],[TokenCreated],[DateCreated],[TokenUpdated],[DateUpdated],
+[CustomerID],[Address],[Zone],[Town],[Department],[Phone],[ContactName])
+VALUES (14770,'FOUR-S FASHION',1,'GT',139039,'CCANO',GETDATE(),NULL,NULL,298,NULL,NULL,NULL,NULL,NULL,NULL);
