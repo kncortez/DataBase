f3adef0d9b5b24c0ff4145d8f178f9c8e6f71279
Hugo Gomez <hugo.gomez@forzalatam.com>
2021-02-04 12:14:51 -0600

    ###CHANGED Ya no se utlizara la tabla [CatWayToPay] porque se reutilizara la tabla [ctgTypeOfInOutOfMoney] para saber el tipo de pago del usuario adicional se renombro una columna  de DeliveryOrderPaymentDetail llamada WayPayId y ahora su nombe es TypeofInOutMoneyId que contendra el id de la tabla ctgTypeOfInOutOfMoney

--- DeliveryBackOffice/Script/-----Script Para la Insercion de catalogos de WAYPAY,Tipe,Time.sql
+++ DeliveryBackOffice/Script/-----Script Para la Insercion de catalogos de WAYPAY,Tipe,Time.sql
@@ -1,21 +1,23 @@
------Script Para la Insercion de catalogos de WAYPAY
---Efectivo
-insert into [dbo].[CatWayToPay] (WayPayName, WayPayDescription, WayPayAbrev, WayPayStatus, TokenCreated, DateCreated) 
- values('Efectivo', 'El pago es en efectivo', 'EFECT', 1,  'SYS-SYSTEM', GETDATE());
- ---Tarjeta
- insert into [dbo].[CatWayToPay] (WayPayName, WayPayDescription, WayPayAbrev, WayPayStatus, TokenCreated, DateCreated) 
- values('Tarjeta', 'EL pago es con tarjeta de credito o debito', 'TARJT', 1,  'SYS-SYSTEM', GETDATE());
- ---Cupon
- insert into [dbo].[CatWayToPay] (WayPayName, WayPayDescription, WayPayAbrev, WayPayStatus, TokenCreated, DateCreated) 
- values('Cupon', 'El pago es con un cupon promocional', 'CUPN', 1,  'SYS-SYSTEM', GETDATE());
- ---Wallet
- insert into [dbo].[CatWayToPay] (WayPayName, WayPayDescription, WayPayAbrev, WayPayStatus, TokenCreated, DateCreated) 
- values('Wallet', 'El pago es con billetera electronica', 'WALLT', 1,  'SYS-SYSTEM', GETDATE());
- --Credito
- insert into [dbo].[CatWayToPay] (WayPayName, WayPayDescription, WayPayAbrev, WayPayStatus, TokenCreated, DateCreated) 
- values('Credito', 'El servico sera al credito', 'CREDIT', 1,  'SYS-SYSTEM', GETDATE());
+-------Script Para la Insercion de catalogos de WAYPAY
+----Efectivo
+--insert into [dbo].[CatWayToPay] (WayPayName, WayPayDescription, WayPayAbrev, WayPayStatus, TokenCreated, DateCreated) 
+-- values('Efectivo', 'El pago es en efectivo', 'EFECT', 1,  'SYS-SYSTEM', GETDATE());
+-- ---Tarjeta
+-- insert into [dbo].[CatWayToPay] (WayPayName, WayPayDescription, WayPayAbrev, WayPayStatus, TokenCreated, DateCreated) 
+-- values('Tarjeta', 'EL pago es con tarjeta de credito o debito', 'TARJT', 1,  'SYS-SYSTEM', GETDATE());
+-- ---Cupon
+-- insert into [dbo].[CatWayToPay] (WayPayName, WayPayDescription, WayPayAbrev, WayPayStatus, TokenCreated, DateCreated) 
+-- values('Cupon', 'El pago es con un cupon promocional', 'CUPN', 1,  'SYS-SYSTEM', GETDATE());
+-- ---Wallet
+-- insert into [dbo].[CatWayToPay] (WayPayName, WayPayDescription, WayPayAbrev, WayPayStatus, TokenCreated, DateCreated) 
+-- values('Wallet', 'El pago es con billetera electronica', 'WALLT', 1,  'SYS-SYSTEM', GETDATE());
+-- --Credito
+-- insert into [dbo].[CatWayToPay] (WayPayName, WayPayDescription, WayPayAbrev, WayPayStatus, TokenCreated, DateCreated) 
+-- values('Credito', 'El servico sera al credito', 'CREDIT', 1,  'SYS-SYSTEM', GETDATE());
+--
+--- ya no ejecutar el scrip de CatWayToPay
 
-
+--- de aqui para abajo si
 
  -----Script Para la Insercion de catalogos de PAYTYPE
 --Contado
