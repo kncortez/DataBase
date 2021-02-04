d7bcf6d8cc0667ae5ac09c82f9a9685c8510862f
JorgeMurilloForza <jorge.murillo@forzalatam.com>
2021-02-04 17:52:51 -0600

    ##CHANGED calculo de servicos Factura Correo

--- DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE dbo.ServiceNotification.sql
+++ DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE dbo.ServiceNotification.sql
@@ -1,6 +1,6 @@
 use DeliveryBackOffice 
 go
-CREATE PROCEDURE [dbo].[ServiceNotification] 
+CREATE PROCEDURE [dbo].[ServiceNotification]
   @SystemId int = 1, 
   @Guide_Serie varchar(2) = 'FD',  
   @Guide_Number int = 89750, 
@@ -39,9 +39,9 @@ SELECT  distinct
 		  ) 
 		  + '",'+
           '"TypeService":"' + COALESCE(cts.CtsDescription,'') + '",'+     
-          '"AmmountCOD":' + COALESCE(dev.Collect_OnDelivery,'0.00') + ','+        
-          '"AmmountCollect":' + COALESCE(convert(varchar,dev.PriceShippment),'0.00') + ','+     
-            '"GrandTotal":' + COALESCE(Convert(varchar,(isnull(dev.PriceShippment,0)+isnull(dev.Collect_OnDelivery,0))) ,'0.00') +		'}'
+          '"AmmountCOD":' + COALESCE(convert(varchar,isnull(dev.Collect_OnDelivery,'0.00')),'0.00') + ','+        
+          '"AmmountCollect":' + COALESCE(convert(varchar,isnull(dev.PriceShippment,'0.00')),'0.00') + ','+     
+            '"GrandTotal":' + COALESCE(Convert(varchar,(isnull(dev.PriceShippment+dev.Collect_OnDelivery,0))) ,'0.00') + '}'
       from DeliveryBackOffice.dbo.DeliveryOrder dev 
       left join DeliveryBackOffice.dbo.VisitPointClient vp on vp.CodeOfReference = dev.Sender_ID 
       left join DeliveryBackOffice.dbo.Customer ctm on ctm.IdCustomer = vp.CustomerID 
@@ -52,7 +52,7 @@ SELECT  distinct
       left join DeliveryBackOffice.dbo.Person prs on prs.PerIdPerson = rgu.UsrIdPerson 
       left join DeliveryBackOffice.dbo.Township tws on tws.IdTownship = dev.SenderIdTownship 
       left join DeliveryBackOffice.dbo.Township tws2 on tws2.IdTownship = dev.ReceiverIdTownship 
-	  left join DeliveryBackOffice.dbo.CatTypeService cts on  cts.CtsShortName = Rtrim(ltrim(dev.TypeService))				
+	  left join DeliveryBackOffice.dbo.CatTypeService cts on  cts.CtsShortName = Rtrim(ltrim(dev.TypeService)) 
 	  where dev.Guide_Number =@Guide_Number
       and dev.Guide_Serie = @Guide_Serie  
   FOR XML PATH(''), TYPE 
@@ -63,4 +63,4 @@ SELECT  distinct
   select  
    + @jsonOutput 
   FormatJson 
-END
+END 
