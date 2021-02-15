fd85fbec6b0b3a2e94e89ddf35e2719b527b1252
Hugo Gomez <hugo.gomez@forzalatam.com>
2021-02-15 13:07:17 -0600

    ###Changes### Se agregaron mas columnas a la tabla SchedulePickup

--- DeliveryBackOffice/Tables/ALTER TABLE SchedulePickup.sql
+++ DeliveryBackOffice/Tables/ALTER TABLE SchedulePickup.sql
@@ -1,21 +1,21 @@
-	ALTER TABLE SchedulePickup  
+	ALTER TABLE SchedulePickup
 	ADD SenderId int null
 	, SenderName varchar(200) null
 	, SenderPhone varchar(50) null
 	,IdHubLogistics int null
 	, AmountPickup decimal(12,2) null
 	,IdSourcePlataform int null
-
-	ALTER TABLE SchedulePickup 
+	,AddressPickup [varchar](500) NULL,
+	,AssigmentStatus [bit] NULL
+	ALTER TABLE SchedulePickup
 	ADD FOREIGN KEY (SenderId) REFERENCES VisitPointClient(CodeOfReference);
-
-	ALTER TABLE SchedulePickup 
+	ALTER TABLE SchedulePickup
 	ADD FOREIGN KEY (IdHubLogistics) REFERENCES HubLogistics(IdHubLogistic);
-
-	ALTER TABLE SchedulePickup 
+	ALTER TABLE SchedulePickup
 	ADD FOREIGN KEY (IdSourcePlataform) REFERENCES CatSystem(SysIdSystem);
-
 	ALTER TABLE SchedulePickup ALTER COLUMN [AccountId] bigint  NULL
 
 	ALTER TABLE SchedulePickup  
 	ADD AddressPickup varchar(500) null
+	
+	
