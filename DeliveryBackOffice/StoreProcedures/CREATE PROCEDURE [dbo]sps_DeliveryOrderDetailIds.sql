ae0ee7ec439495afbe68dab86066839a528dc0da
JorgeMurilloForza <jorge.murillo@forzalatam.com>
2021-02-16 16:35:25 -0600

    ###CHANGED Campos de No. de ticket y correo remitente al crear una guia portal

--- DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE [dbo]sps_DeliveryOrderDetailIds.sql
+++ DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE [dbo]sps_DeliveryOrderDetailIds.sql
@@ -1,12 +1,14 @@
 USE DeliveryBackOffice
 GO
-CREATE PROCEDURE [dbo].[sps_DeliveryOrderDetailIds]
+ALTER PROCEDURE [dbo].[sps_DeliveryOrderDetailIds]
  @GuideSerie						varchar(2) = 'FD'
 ,@GuideNumber						int = 0
 ,@IdCustomer						int = 0 
 ,@TypeService						varchar(3) = 'EXP'
 ,@IndicationsOrigin					varchar(1500) = ''
 ,@IndicationsDestination			varchar(1500) = ''
+,@Sender_Mail						varchar(200) = ''
+,@Ticket_Number						varchar(300) = ''
 AS 
 BEGIN
 
@@ -15,6 +17,8 @@ BEGIN
 	, TypeService = @TypeService
 	,IndicationsToSendOrigin = @IndicationsOrigin
 	, IndicationsToSendDestination = @IndicationsDestination
+	,Ticket_Number = @Ticket_Number
+	,Sender_Mail = @Sender_Mail
 	where Guide_Number = @GuideNumber and Guide_Serie = @GuideSerie;
 
 	select 1;
