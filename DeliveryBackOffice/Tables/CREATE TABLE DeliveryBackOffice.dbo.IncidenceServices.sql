0289b9dd85546bb7ce3bbd96c36e822c682b7692
caquino <cesar.aquino@forzalatam.com>
2021-02-16 17:04:58 -0600

    Date: 2021-02-16 Author: César Aquino
    ### ADD
    * Tablas Incidencias POD

--- DeliveryBackOffice/Tables/CREATE TABLE DeliveryBackOffice.dbo.IncidenceServices.sql
+++ DeliveryBackOffice/Tables/CREATE TABLE DeliveryBackOffice.dbo.IncidenceServices.sql
@@ -1,19 +1,18 @@
------------//////////// Tabla para el registro de cada incidencia Pickup------------------------------------------/////////////////////
 USE [DeliveryBackOffice]
 GO
 CREATE TABLE DeliveryBackOffice.dbo.IncidenceServices
    (IdIncidence int IDENTITY(1,1) PRIMARY KEY NOT NULL,  
     ServiceManagementId int  NULL,
     IncidenceTypeId int  NULL,
-    DescriptionIncidence varchar (300) NULL,
-    Latitude varchar (30) null,
-    Longitude varchar (30) null,
-    Precision varchar (30) null,
+    DescriptionIncidence varchar (200) NULL,
+    Accurancy varchar(200) null,
+    Latitude varchar (200) null,
+    Longitude varchar (200) null,
     RowStatus bit not NULL,
     TokenCreated varchar(50)  NOT NULL,
     DateCreated datetime  NOT NULL,
     TokenUpdated varchar(50)   NULL,
     DateUpdated datetime   NULL,
     CONSTRAINT FKIncidentRecolection FOREIGN KEY (ServiceManagementId) REFERENCES ServiceManagement(IdServiceManagement),
-    CONSTRAINT FKIncidenTypProduct FOREIGN KEY (IncidenceTypeId) REFERENCES CatTypeIncidence(IdIncidenceType))
-GO 
+    CONSTRAINT FKIncidenTypProduct FOREIGN KEY (IncidenceTypeId) REFERENCES CatTypeIncidence(IdIncidenceType)
+GO
