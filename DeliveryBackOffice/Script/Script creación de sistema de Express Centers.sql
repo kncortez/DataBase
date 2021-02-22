2456eea549cf47f59b050992c96492509075e690
Bidcar <bidcar.herrera@forzalatam.com>
2021-02-21 20:13:43 -0600

    Por defecto al facturar cambie sistema 0 por el del express center.

---
+++ DeliveryBackOffice/Script/Script creación de sistema de Express Centers.sql
@@ -0,0 +1,11 @@
+insert into CatSystem
+(SysNameSystem,SysPlataform,SysDescription,SysRowStatus,SysTokenCreated,SysDateCreated)
+values('FDExpressCenter'
+,'Forza Delivery Express Center'
+,'FD Express Center'
+,1
+,'SYS-BHERRERA'
+,getdate()
+)
+select * from DeliveryBackOffice.dbo.CatSystem
+
