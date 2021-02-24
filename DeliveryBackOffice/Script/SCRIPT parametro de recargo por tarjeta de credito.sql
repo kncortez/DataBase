11e87e986426d300138a4dad15dffa2728c7e2e7
caquino <cesar.aquino@forzalatam.com>
2021-02-24 16:58:33 -0600

    Date: 2021-02-23 Author: César Aquino ### CHANGED * Nuevo parametro cotizador para cobrar pago con tarjeta de crédito

---
+++ DeliveryBackOffice/Script/SCRIPT parametro de recargo por tarjeta de credito.sql
@@ -0,0 +1,3 @@
+insert into dbo.CatToCharge values ('CreditCardRate','Recargo por trajeta de crédito','Fac_CreditCardRate', 2,6,'GT',1,1,'SYS-CAQUINO', GETDATE(), NULL, NULL)
+
+select * from dbo.CatToCharge
