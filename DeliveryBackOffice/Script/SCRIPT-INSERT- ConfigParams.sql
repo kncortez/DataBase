066d1897c7fd7bc39e587b2f71d3a50be2f375fe
ajuarez <abner.juarez@forzalatam.com>
2021-03-23 17:26:52 -0600

    MODIFICADO  - Cambio en el SP GetDynamicCatalog  - Nuevo registro en la tabala ConfigParams

---
+++ DeliveryBackOffice/Script/SCRIPT-INSERT- ConfigParams.sql
@@ -0,0 +1,2 @@
+INSERT INTO DeliveryBackOffice.dbo.ConfigParams (Name,Description,Value,Status,CreateDate)
+VALUES ('ValidateHourPickUp','Validación de hora para la recolección', 'True', 1, GETDATE())
