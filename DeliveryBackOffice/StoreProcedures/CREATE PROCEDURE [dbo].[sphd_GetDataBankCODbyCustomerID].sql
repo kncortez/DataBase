f09f7682b73ba06bfdd61a4220f7241b1f0db4e4
eramirez <edwin.ramirez@forzalatam.com>
2021-06-25 18:51:23 -0600

    Date: 2021-06-25 Author: Edwin Ramirez ### CHANGED * Homologacion de nombres store procedures para gestion de modulos

--- DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE [dbo].[sphd_GetDataBankCODbyCustomerID].sql
+++ DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE [dbo].[sphd_GetDataBankCODbyCustomerID].sql
@@ -1,24 +1,19 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
-SET ANSI_NULLS ON;
+USE [DeliveryBackOffice]
 GO
-SET QUOTED_IDENTIFIER ON;
+
+/****** Object:  StoredProcedure [dbo].[sphd_GetDataBankCODbyCustomerID]    Script Date: 6/25/2021 6:48:44 PM ******/
+SET ANSI_NULLS ON
 GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
 -- =============================================
 -- Author:		<Edwin,Ramirez>
 -- Create date: <2021-05-25>
 -- Description:	<Get COD DataMaster by CustomerID>
 -- =============================================
-CREATE PROCEDURE sphd_GetDataBankCODbyCustomerID
+CREATE PROCEDURE [dbo].[sphd_GetDataBankCODbyCustomerID]
     -- Add the parameters for the stored procedure here
     @IdCustomer AS INT
 AS
@@ -39,3 +34,5 @@ BEGIN
 
 END;
 GO
+
+
