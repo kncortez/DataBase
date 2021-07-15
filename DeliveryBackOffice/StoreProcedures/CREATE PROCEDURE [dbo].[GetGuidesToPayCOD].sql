f2b0931f1b0446635669b2e3c302a1799f033c86
Oscar Morales <oscar.morales@forzalatam.com>
2021-07-15 11:18:43 -0600

    Date: 2021-07-15 Author: Oscar Morales
    ### CHANGED
    * En sp que se utiliza en guías por pagar COD se filtra por banco (Banrural y BI)

--- DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE [dbo].[GetGuidesToPayCOD].sql
+++ DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE [dbo].[GetGuidesToPayCOD].sql
@@ -1,6 +1,6 @@
 USE [DeliveryBackOffice]
 GO
-/****** Object:  StoredProcedure [dbo].[GetGuidesToPayCOD]    Script Date: 22/06/2021 17:18:39 ******/
+/****** Object:  StoredProcedure [dbo].[GetGuidesToPayCOD]    Script Date: 15/07/2021 10:25:23 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -26,14 +26,16 @@ BEGIN
 	btd.IdBatchDetailCOD, btd.GuideSerie, btd.GuideNumber, ISNULL(do.Receiver_FirstName,'')+' '+ISNULL(do.Receiver_LastName, '') AS Receiver, 
 	cu.Name As Client, (SELECT TOP 1 dsc.[Hub] FROM [dbo].[DumpServiceCoverage] AS dsc WHERE twn.[HeaderCode] = dsc.[HeaderCode]) AS Hub, btd.[Commission], 
 	do.[Collect_OnDelivery], btd.[Amount], ISNULL(sr.First_Name,'')+' '+ISNULL(sr.Last_Name,'') AS Courier, btd.[Excluded]
-	FROM [dbo].[BatchDetailCOD] AS btd
-	INNER JOIN [dbo].[BatchCOD] AS bt ON btd.[BatchCODId] = bt.[IdBatchCOD]
-	INNER JOIN [dbo].[DeliveryBank] AS db ON btd.[BankId] = db.[Id_bank]
-	INNER JOIN [dbo].[DeliveryOrder] AS do ON btd.[GuideSerie] = do.[Guide_Serie] AND btd.[GuideNumber] = do.[Guide_Number]
-	INNER JOIN [dbo].[Customer] AS cu ON do.[IdCustomer] = cu.[IdCustomer]
-	INNER JOIN [dbo].[Township] AS twn ON do.[ReceiverIdTownship] = twn.[IdTownship]
-	INNER JOIN [dbo].[ProcessedGuideCOD] AS pg ON btd.[GuideSerie] = pg.[GuideSerie] AND btd.[GuideNumber] = pg.[GuideNumber]
-	INNER JOIN [dbo].[SenderReceiver] AS sr ON pg.[CourierManId] = sr.[ID]
-	WHERE CONVERT(DATE,bt.[Date]) = @Date
+	FROM [dbo].[BatchDetailCOD] btd
+	INNER JOIN [dbo].[BatchCOD] bt ON btd.[BatchCODId] = bt.[IdBatchCOD]
+	INNER JOIN [dbo].[DeliveryBank] db ON btd.[BankId] = db.[Id_bank]
+	INNER JOIN [dbo].[DeliveryOrder] do ON btd.[GuideSerie] = do.[Guide_Serie] AND btd.[GuideNumber] = do.[Guide_Number]
+	INNER JOIN [dbo].[Customer] cu ON do.[IdCustomer] = cu.[IdCustomer]
+	INNER JOIN [dbo].[Township] twn ON do.[ReceiverIdTownship] = twn.[IdTownship]
+	INNER JOIN [dbo].[ProcessedGuideCOD] pg ON btd.[GuideSerie] = pg.[GuideSerie] AND btd.[GuideNumber] = pg.[GuideNumber]
+	INNER JOIN [dbo].[SenderReceiver] sr ON pg.[CourierManId] = sr.[ID]
+	WHERE db.[Id_bank] IN (5,33) --Banrural y BI
+		AND CONVERT(DATE,bt.[Date]) = @Date
 
+	SET NOCOUNT OFF;
 END
