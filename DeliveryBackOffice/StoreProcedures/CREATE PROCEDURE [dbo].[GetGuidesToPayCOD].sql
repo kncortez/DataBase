e263d224126c9d997490b813c1ae417d8a173d96
Oscar Morales <oscar.morales@forzalatam.com>
2021-07-20 12:40:57 -0600

    Date: 2021-07-20 Author: Oscar Morales
    ### CHANGED
    * Se modificó sp para que se muestre información del número de cuenta.

--- DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE [dbo].[GetGuidesToPayCOD].sql
+++ DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE [dbo].[GetGuidesToPayCOD].sql
@@ -23,17 +23,17 @@ BEGIN
 	SET NOCOUNT ON;
 
 	SELECT bt.IdBatchCOD,bt.Name, bt.BatchNumber, db.[Acronym] AS Bank, bt.TotalAmountIncluded, btd.AuthorizationNumber, btd.AuthorizationDate, 
-	btd.IdBatchDetailCOD, btd.GuideSerie, btd.GuideNumber, ISNULL(do.Receiver_FirstName,'')+' '+ISNULL(do.Receiver_LastName, '') AS Receiver, 
+	btd.IdBatchDetailCOD, btd.GuideSerie, btd.GuideNumber, CONCAT(do.Receiver_FirstName, ' ',do.Receiver_LastName) AS Receiver, 
 	cu.Name As Client, (SELECT TOP 1 dsc.[Hub] FROM [dbo].[DumpServiceCoverage] AS dsc WHERE twn.[HeaderCode] = dsc.[HeaderCode]) AS Hub, btd.[Commission], 
-	do.[Collect_OnDelivery], btd.[Amount], ISNULL(sr.First_Name,'')+' '+ISNULL(sr.Last_Name,'') AS Courier, btd.[Excluded]
+	do.[Collect_OnDelivery], btd.[Amount], dcb.DCBA_Num_account As NumAccount, btd.[Excluded]
 	FROM [dbo].[BatchDetailCOD] btd
-	INNER JOIN [dbo].[BatchCOD] bt ON btd.[BatchCODId] = bt.[IdBatchCOD]
-	INNER JOIN [dbo].[DeliveryBank] db ON btd.[BankId] = db.[Id_bank]
-	INNER JOIN [dbo].[DeliveryOrder] do ON btd.[GuideSerie] = do.[Guide_Serie] AND btd.[GuideNumber] = do.[Guide_Number]
-	INNER JOIN [dbo].[Customer] cu ON do.[IdCustomer] = cu.[IdCustomer]
-	INNER JOIN [dbo].[Township] twn ON do.[ReceiverIdTownship] = twn.[IdTownship]
-	INNER JOIN [dbo].[ProcessedGuideCOD] pg ON btd.[GuideSerie] = pg.[GuideSerie] AND btd.[GuideNumber] = pg.[GuideNumber]
-	INNER JOIN [dbo].[SenderReceiver] sr ON pg.[CourierManId] = sr.[ID]
+	LEFT JOIN [dbo].[BatchCOD] bt ON btd.[BatchCODId] = bt.[IdBatchCOD]
+	LEFT JOIN [dbo].[DeliveryBank] db ON btd.[BankId] = db.[Id_bank]
+	LEFT JOIN [dbo].[DeliveryOrder] do ON btd.[GuideSerie] = do.[Guide_Serie] AND btd.[GuideNumber] = do.[Guide_Number]
+	LEFT JOIN [dbo].[Customer] cu ON do.[IdCustomer] = cu.[IdCustomer]
+	LEFT JOIN [dbo].[Township] twn ON do.[ReceiverIdTownship] = twn.[IdTownship]
+	LEFT JOIN [dbo].[ProcessedGuideCOD] pg ON btd.[GuideSerie] = pg.[GuideSerie] AND btd.[GuideNumber] = pg.[GuideNumber]
+	LEFT JOIN [dbo].[DeliveryCustomerBankAccount] dcb ON do.DCBA_ID = dcb.DCBA_Id
 	WHERE db.[Id_bank] IN (5,33) --Banrural y BI
 		AND CONVERT(DATE,bt.[Date]) = @Date
 
