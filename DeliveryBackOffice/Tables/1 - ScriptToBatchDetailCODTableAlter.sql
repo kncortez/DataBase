ec2b28038e114aff6a86a2ba131b8fcf3448cfc2
ajortizp <alvaro.ortiz@forzalatam.com>
2021-08-16 17:54:22 -0600

    MOD: se agrega un comentario a la columna que se agregó a la tabla de BatchDetailCOD.

--- DeliveryBackOffice/Tables/1 - ScriptToBatchDetailCODTableAlter.sql
+++ DeliveryBackOffice/Tables/1 - ScriptToBatchDetailCODTableAlter.sql
@@ -3,7 +3,17 @@ USE [DeliveryBackOffice]
 BEGIN TRAN
 
 --AGREGAR COLUMNAS
-ALTER TABLE [dbo].[BatchDetailCOD] ADD [Comments] [nvarchar](2000) NULL
+ALTER TABLE [dbo].[BatchDetailCOD] ADD [Comments] [nvarchar](2000) NULL;
+
+--AGREGAR COMENTARIO A LA COLUMNA AGREGADA
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', 
+								@value=N'Se almacena el motivo por el que se excluye el registro.' , 
+								@level0type=N'SCHEMA',
+								@level0name=N'dbo', 
+								@level1type=N'TABLE',
+								@level1name=N'BatchDetailCOD', 
+								@level2type=N'COLUMN',
+								@level2name=N'Comments';
 
 --COMMIT
 
