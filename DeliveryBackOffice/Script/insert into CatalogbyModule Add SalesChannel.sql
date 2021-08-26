3ce1777b45909aa0744230c8be2344282531204b
Bidcar <bidcar.herrera@forzalatam.com>
2021-08-26 15:19:36 -0600

    Agregar campo canal de venta

---
+++ DeliveryBackOffice/Script/insert into CatalogbyModule Add SalesChannel.sql
@@ -0,0 +1,4 @@
+INSERT INTO DeliveryBackOffice.dbo.CatalogbyModule
+([NameCatalog], [ModuleID], [SystemID], [RowStatus], [TokenCreated], [DateCreated], [TokenUpdated], [DateUpdated])
+VALUES 
+('SalesChannel',20,2,1,'SYS-BHERRERA',GETDATE(),NULL,null)
