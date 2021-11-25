cb6ed269ee0b984fd51119105598afcb14836856
ixchop98 <alberto.ixchop@forzalatam.com>
2021-11-25 17:18:15 -0600

    [20211125] Modificiaciones:
    Se modificó el sp dbo.[sphdGetModuleAccess] que trae los permisos que posee el usuario logueado para poder traer el nuevo campo de la tabla InternalUser (SaleAdvisorId)
    Se modificó el sp dbo.[sphdSetCustomer] el cual se le agrego el campo @IdUser, para que por medio de esto traer el SaleAdvisorId y compararlo con e SaleAdvisorId del customer que se quiere modificar.
    Se modificó el sp dbo.[sphdSetVisitPointClient] el cual se le agrego el campo@idUser, para poder aplicar la misma regla que sphdSetCustomer.
    Se moodifico la talba Inetrnal User para poder agregar el nuevo campo SaleAdvisorId el cual relaciona las tablas de InternaUser y Customers

---
+++ DeliveryBackOffice/StoreProcedures/ALTER PROCEDURE [dbo].[sphdGetModuleAccess].sql
@@ -0,0 +1,50 @@
+USE [DeliveryBackOffice]
+GO
+/****** Object:  StoredProcedure [dbo].[sphdGetModuleAccess]    Script Date: 25/11/2021 11:51:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Edwin,Ramirez>
+-- Create date: <2021-04-21>
+-- Description:	<Devuelve los datos del usuario logueado>
+-- =============================================
+ALTER PROCEDURE [dbo].[sphdGetModuleAccess]
+	-- Add the parameters for the stored procedure here
+	@CodeUser as bigint,
+	@UserName as nvarchar(50),
+	@IdSystem as int 
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	SELECT RUS.[RusIdRol]
+		  ,RUS.[RusIdSystem]
+		  ,RUS.[RusIdUser]
+		  ,INU.IdEmployee
+		  ,INU.IdUser
+		  ,INU.Username
+		  ,RMS.RmsIdModule
+		  ,MDL.ModIdModuleParent
+		  ,MDL.ModName
+		  ,MDL.ModPath
+		  ,MDL.ModOrder
+		  ,MDL.ModMetadata
+		  ,INU.SaleAdvisorID
+	  FROM [DeliveryBackOffice].[dbo].InternalUser INU 
+	  JOIN [DeliveryBackOffice].[dbo].[RolByUserBySystem] RUS
+									ON INU.RegisterUserID = RUS.RusIdUser AND INU.RowStatus = 'TRUE'
+	  JOIN [DeliveryBackOffice].[dbo].RolByModuleBySystem RMS  ON RMS.RmsIdRol = RUS.RusIdRol 
+									AND RUS.RusIdSystem = RMS.RmsIdSystem 
+								    AND RUS.RusRowStatus  = 'TRUE'
+	  JOIN CatModule MDL			ON RMS.RmsIdModule = MDL.ModIdModule
+									AND MDL.ModRowStatus = 'TRUE'
+	  WHERE INU.IdUser = @CodeUser
+	  AND INU.Username = @UserName
+	  AND RUS.RusIdSystem = @IdSystem  
+
+	  	   
+END
---
+++ DeliveryBackOffice/StoreProcedures/ALTER PROCEDURE [dbo].[sphdSetCustomer].sql
@@ -0,0 +1,333 @@
+USE [DeliveryBackOffice]
+GO
+/****** Object:  StoredProcedure [dbo].[sphdSetCustomer]    Script Date: 24/11/2021 09:34:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Edwin,Ramirez>
+-- Create date: <2021-04-27>
+-- Description:	<inserta o actualiza registros a clientes en base a flag @option 1 insert 2 update>
+-- =============================================
+ ALTER PROCEDURE [dbo].[sphdSetCustomer]
+	-- Add the parameters for the stored procedure here
+	@IdCustomer int,
+	@NameCustomer nvarchar(100),
+	@Description  nvarchar(100) = '',
+	@Domain nvarchar(50)='',
+	@RegexSubject nvarchar(100) = '',
+	@RegexEmail nvarchar(100) = '',
+	@RegexFilename nvarchar(100) = '',
+	@Abbreviation nvarchar(25) = '',
+	@IdCustomerType int = 1,    -- 1	CORPORATIVO; 2	REDISTRIBUIDOR;  3	INDIVIDUAL
+	@CountryID varchar(2) =  'GT',
+	@CommercialName nvarchar(50),
+	@CustomerPhone nvarchar(50),
+	@WebsiteURI nvarchar(50) = '',
+	@ContactName nvarchar(50), 
+	@ContactEmail nvarchar(50),
+	@NotificationAddress nvarchar(200),
+	@SaleAdvisorID int = -1,
+	@DateUpService datetime = NULL,
+	@DateDownService datetime = NULL,
+	@TypeOfBusinessID int,
+	@BusinessSegmentID int,
+	@BusinessActivityID int,
+	@CommercialSegmentID int,
+	@OperationContactName nvarchar(50) = '',
+	@OperationContactPhone nvarchar(50) = '',
+	@OperationContactEmail nvarchar(50) = '',
+	@LegalSponsorName nvarchar(50) = '',
+	@LegalSponsorLastName nvarchar(50) = '', 
+	@LegalSponsorDPI nvarchar(50) = '',
+	@HasAgreement bit  = 'FALSE', 
+	@AgreementNumber nvarchar(50) = '', 
+	@AgreementDateStart datetime = NULL,
+	@AgreementDateEnd datetime = NULL,
+	@InvoiceName nvarchar(100) = '',
+	@TaxIdentificationNumber nvarchar(50) = '',
+	@FiscalAddress  nvarchar(200)  = '',
+	@InvoiceEmail nvarchar(50) = '',
+	@ConditionOfPaymentID int  = 1,
+	@InvoiceContactName nvarchar(50) = '',
+	@InvoiceContactPhone nvarchar(50) = '',
+	@InvoiceContactEmail nvarchar(50) = '',
+	@CODAccountBankID int = NULL,
+	@CODAccountNumber nvarchar(50) = '',
+	@CODAccountName nvarchar(50) = '',
+	@CODAccountTypeID int = NULL,
+	@CODCurrencyID int = NULL,
+	@CODContactName nvarchar(50) = '',
+	@CODContactPhone nvarchar(50) = '',
+	@CODContactEmail nvarchar(50) = '',
+	@RowSatus bit = 'TRUE',
+	@Token nvarchar(50) = 'SYS-FDADMIN',
+	@Option as int,  -- 1 create record, 2 update record
+	@ExcludePriceShippingCOD bit = 'FALSE',
+	@ExcludeCommissionCOD bit = 'FALSE',
+	@CatBatchTypeCODId BIGINT,
+	@CatBatchFrequencyCODId BIGINT
+	,@IdUser BIGINT=NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	BEGIN TRANSACTION
+	BEGIN TRY
+		
+		
+
+
+		
+		IF (@Option = 1)
+		BEGIN 
+			print 'insert record'
+
+			IF NOT EXISTS ( SELECT cli.IdCustomer FROM Customer cli where cli.Name = @NameCustomer ) 
+			BEGIN
+			INSERT INTO [DeliveryBackOffice].[dbo].[Customer]
+				   ([Name]
+				   ,[Description]
+				   ,[Domain]
+				   ,[RegexSubject]
+				   ,[RegexEmail]
+				   ,[RegexFilename]
+				   ,[Abbreviation]
+				   ,[IdCustomerType]
+				   ,[CountryID]
+				   ,[CommercialName]
+				   ,[CustomerPhone]
+				   ,[WebsiteURI]
+				   ,[ContactName]
+				   ,[ContactEmail]
+				   ,[NotificationAddress]
+				   ,[SaleAdvisorID]
+				   ,[DateUpService]
+				   ,[DateDownService]
+				   ,[TypeOfBusinessID]
+				   ,[BusinessSegmentID]
+				   ,[BusinessActivityID]
+				   ,[CommercialSegmentID]
+				   ,[OperationContactName]
+				   ,[OperationContactPhone]
+				   ,[OperationContactEmail]
+				   ,[LegalSponsorName]
+				   ,[LegalSponsorLastName]
+				   ,[LegalSponsorDPI]
+				   ,[HasAgreement]
+				   ,[AgreementNumber]
+				   ,[AgreementDateStart]
+				   ,[AgreementDateEnd]
+				   ,[InvoiceName]
+				   ,[TaxIdentificationNumber]
+				   ,[FiscalAddress]
+				   ,[InvoiceEmail]
+				   ,[ConditionOfPaymentID]
+				   ,[InvoiceContactName]
+				   ,[InvoiceContactPhone]
+				   ,[InvoiceContactEmail]
+				   ,[CODAccountBankID]
+				   ,[CODAccountNumber]
+				   ,[CODAccountName]
+				   ,[CODAccountTypeID]
+				   ,[CODCurrencyID]
+				   ,[CODContactName]
+				   ,[CODContactPhone]
+				   ,[CODContactEmail]
+				   ,[RowSatus]
+				   ,[TokenCreated]
+				   ,[DateCreated]
+				   ,[TokenUpdated]
+				   ,[DateUpdated]
+				   ,[SAPCardCode]
+				   ,[ExcludePriceShippingCOD]
+				   ,[ExcludeCommissionCOD]
+				   ,[CatBatchTypeCODId]
+				   ,[CatBatchFrequencyCODId]
+
+				   )
+			 VALUES
+				   (@NameCustomer
+				   ,@Description
+				   ,@Domain
+				   ,@RegexSubject
+				   ,@RegexEmail
+				   ,@RegexFilename
+				   ,@Abbreviation
+				   ,@IdCustomerType
+				   ,@CountryID
+				   ,@CommercialName
+				   ,@CustomerPhone
+				   ,@WebsiteURI
+				   ,@ContactName
+				   ,@ContactEmail
+				   ,@NotificationAddress
+				   ,@SaleAdvisorID
+				   ,@DateUpService
+				   ,@DateDownService
+				   ,@TypeOfBusinessID
+				   ,@BusinessSegmentID
+				   ,@BusinessActivityID
+				   ,@CommercialSegmentID
+				   ,@OperationContactName
+				   ,@OperationContactPhone
+				   ,@OperationContactEmail
+				   ,@LegalSponsorName
+				   ,@LegalSponsorLastName
+				   ,@LegalSponsorDPI
+				   ,@HasAgreement
+				   ,@AgreementNumber
+				   ,@AgreementDateStart
+				   ,@AgreementDateEnd
+				   ,@InvoiceName
+				   ,@TaxIdentificationNumber
+				   ,@FiscalAddress
+				   ,@InvoiceEmail
+				   ,@ConditionOfPaymentID
+				   ,@InvoiceContactName
+				   ,@InvoiceContactPhone
+				   ,@InvoiceContactEmail
+				   ,@CODAccountBankID
+				   ,@CODAccountNumber
+				   ,@CODAccountName
+				   ,@CODAccountTypeID
+				   ,@CODCurrencyID
+				   ,@CODContactName
+				   ,@CODContactPhone
+				   ,@CODContactEmail
+				   ,@RowSatus --'TRUE'
+				   ,NULL
+				   ,NULL
+				   ,@Token
+				   ,GETDATE()
+				   ,NULL
+				   ,@ExcludePriceShippingCOD
+				   ,@ExcludeCommissionCOD
+				   ,@CatBatchTypeCODId
+				   ,@CatBatchFrequencyCODId				   		
+				   )
+
+				   SELECT	'TRUE'	[blnResult]
+							,CAST(SCOPE_IDENTITY() AS VARCHAR) [IdResult]
+							,'' AS [ErrorNumber]
+							,'' AS [ErrorSeverity]  
+							,'' AS [ErrorState]
+							,'' AS [ErrorProcedure]  
+							,'' AS [ErrorLine]
+							,'Success' AS [Message];  
+				END
+			ELSE
+				BEGIN
+					 SELECT	'FALSE'	[blnResult]
+							,'-1' [IdResult]
+							,'' AS [ErrorNumber]
+							,'' AS [ErrorSeverity]  
+							,'' AS [ErrorState]
+							,'' AS [ErrorProcedure]  
+							,'' AS [ErrorLine]
+							,'El cliente que intenta crear ya existe en base datos' AS [Message];  
+				END
+
+		END
+		ELSE IF (@Option = 2)
+		BEGIN
+				print 'update record'
+				DECLARE @UserSaleAdvisorID int = ((SELECT SaleAdvisorId FROM DBO.InternalUser WHERE IdUser=@IdUser));
+				DECLARE @CurrentSaleAdvisorID int=(SELECT SaleAdvisorID from DBO.Customer WHERE IdCustomer=@IdCustomer);		
+				IF ((NULL in (@UserSaleAdvisorID,@CurrentSaleAdvisorID)) OR (@CurrentSaleAdvisorID<> @UserSaleAdvisorID))
+				BEGIN
+					 SELECT	'FALSE'	[blnResult]
+							,'-1' [IdResult]
+							,'' AS [ErrorNumber]
+							,'' AS [ErrorSeverity]  
+							,'' AS [ErrorState]
+							,'' AS [ErrorProcedure]  
+							,'' AS [ErrorLine]
+							,CONCAT('No tiene permiso para modificar este socio de negocio','') AS [Message];
+				END
+				ELSE
+				UPDATE [DeliveryBackOffice].[dbo].[Customer]
+				   SET [Name] = @NameCustomer
+					  ,[Description] = @Description
+					  --,[Domain] = <Domain, nvarchar(50),>
+					  --,[RegexSubject] = <RegexSubject, nvarchar(100),>
+					  --,[RegexEmail] = <RegexEmail, nvarchar(100),>
+					  --,[RegexFilename] = <RegexFilename, nvarchar(100),>
+					  --,[Abbreviation] = <Abbreviation, nvarchar(25),>
+					  --,[IdCustomerType] = <IdCustomerType, int,>
+					  ,[CountryID] = @CountryID
+					  ,[CommercialName] = @CommercialName
+					  ,[CustomerPhone] = @CustomerPhone
+					  ,[WebsiteURI] = @WebsiteURI
+					  ,[ContactName] = @ContactName
+					  ,[ContactEmail] = @ContactEmail
+					  ,[NotificationAddress] = @NotificationAddress
+					  ,[SaleAdvisorID] = @SaleAdvisorID
+					  ,[DateUpService] = @DateUpService
+					  ,[DateDownService] = @DateDownService
+					  ,[TypeOfBusinessID] = @TypeOfBusinessID
+					  ,[BusinessSegmentID] = @BusinessSegmentID
+					  ,[BusinessActivityID] = @BusinessActivityID
+					  ,[CommercialSegmentID] = @CommercialSegmentID
+					  ,[OperationContactName] = @OperationContactName
+					  ,[OperationContactPhone] = @OperationContactPhone
+					  ,[OperationContactEmail] = @OperationContactEmail
+					  ,[LegalSponsorName] = @LegalSponsorName
+					  ,[LegalSponsorLastName] = @LegalSponsorLastName
+					  ,[LegalSponsorDPI] = @LegalSponsorDPI
+					  ,[HasAgreement] = @HasAgreement
+					  ,[AgreementNumber] = @AgreementNumber
+					  ,[AgreementDateStart] = @AgreementDateStart
+					  ,[AgreementDateEnd] = @AgreementDateEnd
+					  ,[InvoiceName] = @InvoiceName
+					  ,[TaxIdentificationNumber] = @TaxIdentificationNumber
+					  ,[FiscalAddress] = @FiscalAddress
+					  ,[InvoiceEmail] = @InvoiceEmail
+					  ,[ConditionOfPaymentID] = @ConditionOfPaymentID
+					  ,[InvoiceContactName] = @InvoiceContactName
+					  ,[InvoiceContactPhone] = @InvoiceContactPhone
+					  ,[InvoiceContactEmail] = @InvoiceContactEmail
+					  ,[CODAccountBankID] = @CODAccountBankID
+					  ,[CODAccountNumber] = @CODAccountNumber
+					  ,[CODAccountName] = @CODAccountName
+					  ,[CODAccountTypeID] = @CODAccountTypeID
+					  ,[CODCurrencyID] = @CODCurrencyID
+					  ,[CODContactName] = @CODContactName
+					  ,[CODContactPhone] = @CODContactPhone
+					  ,[CODContactEmail] = @CODContactEmail
+					  ,[RowSatus] = @RowSatus
+					  ,[TokenUpdated] = @Token
+					  ,[DateUpdated] = GETDATE()
+					  --[SAPCardCode] = [SAPCardCode]
+					  ,[ExcludePriceShippingCOD] = @ExcludePriceShippingCOD
+					  ,[ExcludeCommissionCOD] = @ExcludeCommissionCOD
+					  ,[CatBatchTypeCODId] = @CatBatchTypeCODId
+					  ,[CatBatchFrequencyCODId] = @CatBatchFrequencyCODId
+				 WHERE IdCustomer = @IdCustomer
+
+				  SELECT	'TRUE'	[blnResult]
+							,CAST(@IdCustomer AS VARCHAR) [IdResult]
+							,'' AS [ErrorNumber]
+							,'' AS [ErrorSeverity]  
+							,'' AS [ErrorState]
+							,'' AS [ErrorProcedure]  
+							,'' AS [ErrorLine]
+							,'Success' AS [Message];  
+
+		END
+
+		COMMIT TRANSACTION;  
+
+	END TRY
+	BEGIN CATCH  
+			SELECT 'FALSE'	[blnResult]
+				,CAST(ERROR_NUMBER() AS VARCHAR) AS [ErrorNumber]
+				,CAST(ERROR_SEVERITY() AS VARCHAR) AS [ErrorSeverity]
+				,CAST(ERROR_STATE() AS VARCHAR) AS [ErrorState]
+				,CAST(ERROR_PROCEDURE() AS VARCHAR) AS [ErrorProcedure]
+				,CAST(ERROR_LINE() AS VARCHAR) AS [ErrorLine]  
+				,CAST(ERROR_MESSAGE() AS NVARCHAR(MAX)) AS [Message];  
+			ROLLBACK TRANSACTION;  
+	END CATCH;  
+END
