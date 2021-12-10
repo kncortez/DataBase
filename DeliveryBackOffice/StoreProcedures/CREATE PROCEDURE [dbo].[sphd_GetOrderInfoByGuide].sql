8723eee9e102ef09893c2eecf18b98b616ab6654
ixchop98 <alberto.ixchop@forzalatam.com>
2021-12-10 16:40:10 -0600

    [20211210] Modificaciones: Se agregaron 4 sp pertenecientes al modulo de alertas

--- DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE [dbo].[sphd_GetOrderInfoByGuide].sql
+++ DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE [dbo].[sphd_GetOrderInfoByGuide].sql
@@ -6,52 +6,60 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
 -- =============================================
 -- Author:		<Alberto, Ixchop>
 -- Create date: <2021-11-29>
--- Description:	<Devuelve todas las guias>
+-- Description:	<Devuelve todas las guias con informacion de sus posibles alertas que posee>
 -- =============================================
 --
 ALTER PROCEDURE [dbo].[sphd_GetOrderInfoByGuide]
 		@GuideToSearch as nvarchar(50)
 AS
 BEGIN
-		
-		SELECT  			 
+		SELECT  			
 			serv.Guide_Serie +  CAST(serv.Guide_Number AS VARCHAR) Guide, 
 			serv.Guide_Serie GuideSerie,
 			serv.Guide_Number GuideNumber,
-			isnull(serv.Receiver_FirstName,'') + ' '+ isnull(serv.Receiver_LastName,'') Name,
-			serv.Receiver_Address Address,
-			serv.Receiver_Zone  Zone,
-			serv.Receiver_Town Town,
-			serv.Receiver_Department Department,
-			serv.Delivery_Max_Date DeliveryMaxDate,
-			serv.Courier_Route CourierRoute,
-			(SELECT DeliveryBackOffice.dbo.fn_get_courier(serv.Guide_Serie, serv.Guide_Number)) CourierName,
-			serv.DateCreated as DateCreated,
-			isnull(serv.Sender_FirstName,'') + ' ' + isnull(serv.Sender_LastName,'') as SenderFullname,
-			(serv.Pieces_Dry +serv.Pieces_Cold ) TotalPieces,
-			(SELECT so.OrderDescription FROM StatusOrder so WHERE so.StatusOrderId = serv.StatusOrderId) as StatusName,
-			StatusOrderId,
-			serv.Receiver_Phone as ReceiverPhone,
-			Talert.AlertTypeId as AlertId,
-			Ttalert.AlertName  as AlertName,
-			Talert.AlertDescription  as AlertDescription,
-			serv.IsCollect,
-			serv.PriceShippment,
-			serv.TypeService,
-			(SELECT p.Package_Name FROM Package p WHERE p.Package_Type = serv.Package_Type) as PackageType,
-			(SELECT DeliveryBackOffice.dbo.fn_get_rackposition(serv.Guide_Serie, serv.Guide_Number)) as RackPosition
+			isnull(serv.Sender_FirstName,'') + ' '+ isnull(serv.Sender_LastName,'') Sender,
+			serv.Sender_Address SenderAddress,
+			serv.Sender_Phone SenderPhone,
+			serv.IndicationsToSendOrigin SenderIndications,
+			isnull(serv.Receiver_FirstName,'') + ' '+ isnull(serv.Receiver_LastName,'') Receiver,
+			serv.Receiver_Address ReceiverAddress,
+			serv.Receiver_Phone ReceiverPhone,
+			serv.IndicationsToSendDestination ReceiverIndications,
+			serv.IsCollect,			
+			Talert.IdDeliveryOrderAlert AlertId,
+			Talert.AlertDescription	AlertDescription,
+			Talert.DateCreated AlertCreated,
+			Talert.AlertTypeId AlertTypeId,
+			Talert.ServiceTypeId AlertServiceTypeId,			
+			serv.StatusOrderId StatusId,
+			SO.OrderDescription StatusDescription,
+			serv.Sender_Department	SenderDepartment,
+			serv.Sender_Town	SenderTown, 
+			serv.SenderIdTownship SenderIdtownship,
+			Receiver_Department ReceiverDepartment,
+			Receiver_Town ReceiverTown,
+			ReceiverIdSettlement,
+			CASE WHEN SO.OrderDescription not in (
+                'Entregado',
+                'Anulado',
+                'Devuelto',
+                'Entregado En Express Center',
+                'Devuelto en Express Center',
+                'COD liquidado',
+                'COD pagado'
+			) then 1 else 0 end AllowCreateAlert
 		FROM DeliveryBackOffice.DBO.DeliveryOrder serv
-			LEFT JOIN DBO.DeliveryOrderAlert Talert ON Talert.GuideSerie=serv.Guide_Serie AND Talert.GuideNumber=serv.Guide_Number
-			LEFT JOIN DBO.CatTypeAlert Ttalert ON Ttalert.IdCatTypeAlert= Talert.AlertTypeId 
+			LEFT JOIN DBO.StatusOrder SO ON serv.StatusOrderId=SO.StatusOrderId 
+		    LEFT JOIN DBO.FinalStatusByModule FSBM ON FSBM.StatusOrderId=SERV.StatusOrderId
+			LEFT JOIN DBO.DeliveryOrderAlert Talert ON Talert.GuideSerie=serv.Guide_Serie AND Talert.GuideNumber=serv.Guide_Number AND Talert.RowStatus=1 
+					AND FSBM.IdFinalStatusByModule IS  NULL			
+			LEFT JOIN DBO.CatTypeAlert Ttalert ON Ttalert.IdCatTypeAlert= Talert.AlertTypeId  
+			
 		WHERE 
-		Talert.RowStatus=1 OR Talert.RowStatus is NULL AND
 		(serv.Guide_Serie +  CAST(Guide_Number AS VARCHAR) = @GuideToSearch)
-
 		
 
 END
