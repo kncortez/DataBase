422b33ee3c73f3d015572d4d656b596b355200c1
ixchop98 <alberto.ixchop@forzalatam.com>
2021-12-14 11:53:03 -0600

    [20211214] Modificaciones: Se modifico el sp sphdGetRolesbyUserId y se agregaron dos nuevos sp

--- DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE sphdGetRolesbyUserId.sql
+++ DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE sphdGetRolesbyUserId.sql
@@ -1,14 +1,3 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -30,10 +19,10 @@ BEGIN
         	cr.RolDescription,
         	cr.RolAdminBrothers,
         	cr.RolAdminClient,
-        	cr.RolAdminInternal	
+        	cr.RolAdminInternal
         from dbo.RolByUserBySystem rus 
         Inner Join InternalUser IU on  rus.RusIdUser= IU.RegisterUserID and IU.RowStatus=1
         inner join CatRol cr on rus.RusIdRol=cr.RolIdRol and cr.RolRowStatus=1
-        where IU.IdUser=@idUser and rus.RusRowStatus=1
+        where rus.RusRowStatus=1 and IU.IdUser=@idUser
 END
 GO
