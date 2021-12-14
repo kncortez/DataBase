89e7321b3f25dd2ce644cd1b83526bcd8d6cb78e
Oscar Morales <oscar.morales@forzalatam.com>
2021-12-14 15:40:29 -0600

    Date: 2021-12-14 Author: Oscar Morales
    ### CHANGED
    * Se en Guías por pagar COD para hacer transaccional el registro de denominaciones y contingencias.
    * Se hicieron mejoras en Facturación para no hacer la búsqueda de nuevo y actualizar solo el valor del FEL.

---
+++ DeliveryBackOffice/StoreProcedures/ALTER PROCEDURE [dbo].[spg_settlement_cod].sql
@@ -0,0 +1,59 @@
+USE [DeliveryBackOffice]
+GO
+/****** Object:  StoredProcedure [dbo].[spg_settlement_cod]    Script Date: 14/12/2021 12:03:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+-- =============================================
+-- Author:		<Carlos, Cano>
+-- Create date: <2020-12-08>
+-- Description:	<Recupera información para generar manifiesto de liquidación (COD)>
+-- =============================================
+ALTER PROCEDURE [dbo].[spg_settlement_cod]
+		@IdManifest INT
+AS
+BEGIN
+	
+	DECLARE @GuideCount INT
+
+	SET NOCOUNT ON;
+
+	SET @GuideCount = (
+		SELECT 
+			COUNT(dobs.Guides_Received_COD)
+		FROM [DeliveryBackOffice].[dbo].[DeliveryOrderBySettlement] dobs
+		JOIN DeliveryBackOffice.dbo.DeliverySettlementDetail dsd ON dsd.ID_DeliveryOrderBySettlement = dobs.ID
+		WHERE dobs.ID = @IdManifest
+		AND dsd.Guide_Settlement = 1 -- guía liquidada en bodega
+		AND dsd.Guide_Discharged = 1  -- guía liquidada vía COD
+	)
+
+	SELECT 
+			dobs.ID, 
+			dobs.Date_Received_COD as Date_Received, 
+			--dobs.Pieces_Dry_Received, 
+			--dobs.Pieces_Cold_Received, 
+			@GuideCount as Guides_Received,
+			isnull(sr.First_Name,'') + ' ' + isnull(sr.Last_Name,'') as Courier_Name,
+			dobs.Route_Received_COD as Route_Received,
+			CONVERT(NVARCHAR,lbt.SSN_IdUser) + ' - ' + lbt.SSN_Username as IdUser_Username_Received,
+			(SELECT SUM(mdos.Quantity*cm.Value) TotalAmountCount
+				FROM MoneyByDeliveryOrderBySettlement mdos
+				INNER JOIN CatMoney cm
+					ON mdos.CatMoneyId = cm.IdCatMoney
+				WHERE DeliveryOrderBySettlementId = @IdManifest) Amount_Received,
+			(SELECT Value
+				FROM Contingency 
+				WHERE DeliveryOrderBySettlementId = @IdManifest) Amount_Difference,
+			cs.StationName Hub
+		FROM [DeliveryBackOffice].[dbo].[DeliveryOrderBySettlement] dobs
+		JOIN DeliveryBackOffice.dbo.SenderReceiver sr ON sr.ID = dobs.ID_Courier
+		JOIN DenariusUser_Dev.dbo.LGN_LogByToken lbt ON lbt.SSN_IdToken = dobs.User_Received_COD
+		LEFT JOIN CatStation cs ON cs.IdStation = dobs.SettlementStationId  
+		WHERE dobs.ID = @IdManifest
+
+END
