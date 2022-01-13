50c8664cec6d9e09f99e2ae264f6b237db437c04
Oscar Morales <oscar.morales@forzalatam.com>
2022-01-13 13:05:35 -0600

    Date: 2022-01-13 Author: Oscar Morales
    ### ADDED
    * Se agrega validación para saber si una guía es permitida en preparación y despacho de entregas.

---
+++ DeliveryBackOffice/Script/INSERT INTO [StatusOrderUpdate].sql
@@ -0,0 +1,17 @@
+--Entregado a Programado para entrega
+INSERT INTO StatusOrderUpdate (StatusOrderId, InvalidIdUpdate)
+	VALUES (5, 3);
+
+--Devuelto a Programado para entrega
+INSERT INTO StatusOrderUpdate (StatusOrderId, InvalidIdUpdate)
+	VALUES (14, 3);
+
+--COD liquidado a Programado para entrega
+INSERT INTO StatusOrderUpdate (StatusOrderId, InvalidIdUpdate)
+	VALUES (24, 3);
+
+--COD pagado a Programado para entrega
+INSERT INTO StatusOrderUpdate (StatusOrderId, InvalidIdUpdate)
+	VALUES (25, 3);
+
+
