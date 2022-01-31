edd71031c020fa2975d4b009cede3d257aaa94ef
Alejandro Rodríguez <oscar.rodriguez@forzalatam.com>
2022-01-31 15:43:36 -0600

    Date: 2022-01-31 Author: Alejandro Rodríguez ### ADDED * Se crearon las funciones FnGetHub y FnClearString * Se agregaron las condiciones para asignar el ID del Township que envía y recibe, por medio del nombre del Township y el nombre del Departmen * Se agregaron las condiciones para asignar el Hub Orige y Destino por medio del ID del Township * Se agregaron los campos para almacenar el SystemSource, SourceSystemID y ModuleSource * Se agregaron los campos IdCustomer, OrderUserCreated y SalePipeLineId
    ### CHANGED
    * Se modificó la estructura que se utiliza para el ingreso de los campos en la tabla DeliveryOrders

--- DeliveryBackOffice/Functions/CREATE FUNCTION FnGetHub.sql
+++ DeliveryBackOffice/Functions/CREATE FUNCTION FnGetHub.sql
@@ -1,22 +1,23 @@
 USE [DeliveryBackOffice]
 GO
-/****** Object:  UserDefinedFunction [dbo].[FnGetHub]    Script Date: 31/01/2022 08:24:23 ******/
+/****** Object:  UserDefinedFunction [dbo].[FnGetHub]    Script Date: 31/01/2022 15:39:04 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 --Devuelve el ID de un HUB dando como entrada el ID del Township(Municipio)
-CREATE FUNCTION [dbo].[FnGetHub](
+ALTER FUNCTION [dbo].[FnGetHub](
     @IdTown INT
 )
-RETURNS VARCHAR(MAX)
+RETURNS INT
 AS
 BEGIN
     
-    DECLARE @Hub NVARCHAR(10) =
-	(SELECT TOP 1 Hub FROM DeliveryBackOffice.dbo.DumpServiceCoverage
+    DECLARE @Hub INT =
+	(SELECT IdHubLogistic FROM DeliveryBackOffice.dbo.HubLogistics
+							WHERE HubAbbreviation = (SELECT TOP 1 Hub FROM DeliveryBackOffice.dbo.DumpServiceCoverage
 							  WHERE HeaderCode = (SELECT HeaderCode FROM DeliveryBackOffice.dbo.Township
-												  WHERE IdTownship = @IdTown))
+												  WHERE IdTownship = @IdTown)))
 
     
     RETURN @Hub
