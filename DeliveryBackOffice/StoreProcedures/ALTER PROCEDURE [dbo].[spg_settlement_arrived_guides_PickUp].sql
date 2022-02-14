e95526f2f8acaedf7b03e5f3f6b67f6fa3a45394
Oscar Morales <oscar.morales@forzalatam.com>
2022-02-14 16:01:56 -0600

    Date: 2022-02-14 Author: Oscar Morales
    ### CHANGED
    * Se cambio el sp spg_Settlement_PickUp para agregar si la pieza es seca o no y devolverla en el SELECT.
    * Se cambio el sp spws_set_route_settlement_status para que retorne si una pieza es seca o no para llevar el control.
    * Se modificó el sp spg_settlement_arrived_guides_PickUp para que muestre correctamente las piezas frías o secas de una guía.
    * Se modificó el sp spg_settlement_not_arrived_guides_PickUp para que muestre correctamente las piezas frías o secas de una guía.

---
+++ DeliveryBackOffice/StoreProcedures/ALTER PROCEDURE [dbo].[spg_settlement_arrived_guides_PickUp].sql
@@ -0,0 +1,100 @@
+USE [DeliveryBackOffice]
+GO
+/****** Object:  StoredProcedure [dbo].[spg_settlement_arrived_guides_PickUp]    Script Date: 14/02/2022 15:04:48 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--drop procedure  [dbo].[spg_settlement_returned_guides_PickUp]
+-- =============================================
+-- Author:		<Hugo, Gomez>
+-- Create date: <2021-03-11>
+-- Description:	<Recupera detalle para generar manifiesto de liquidación (PickUp)>
+-- =============================================
+ALTER PROCEDURE [dbo].[spg_settlement_arrived_guides_PickUp]
+		@IdManifest INT
+AS
+BEGIN
+	
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	DECLARE @temp TABLE (
+		Guide_Code	nvarchar(max),
+		Pieces_Cold int,
+		Pieces_Dry int,
+		Receiver_Fullname nvarchar(201),
+		Receiver_Address nvarchar(600),
+		Receiver_Zone NVARCHAR(50),
+		Receiver_Town nvarchar(100),
+		Receiver_Departament nvarchar(100),
+		Preparation_Date nvarchar(50),
+		Shipping_Date nvarchar(50),
+		Max_Date nvarchar(50),
+		Receiver_Phone nvarchar(100),
+		Rack_Position nvarchar(MAX),
+		Collect_on_Delivery decimal(16,2)
+	)
+
+    -- tablix content
+	INSERT INTO @temp
+	SELECT
+	do.Guide_Serie + isnull(convert(nvarchar,do.Guide_Number),'') as Guide_Code
+	,ISNULL((SELECT
+		COUNT(1)
+	FROM SettlementByPickup sbp
+	JOIN SettlementByPickupDetail sbpd
+		ON (sbp.Id = sbpd.SettlementByPickupId)
+	JOIN DeliveryOrderPiece dop
+		ON dop.GuideSerie = sbpd.GuideSerie
+			AND dop.GuideNumber = sbpd.GuideNumber
+			AND dop.NoPiece = sbpd.NoPiece
+	WHERE Id = @IdManifest
+	AND sbpd.GuideSerie = do.Guide_Serie
+	AND sbpd.GuideNumber = do.Guide_Number
+	AND sbpd.IsPieceLiquidaded = 1
+	AND (dop.IsDry = 0)
+	GROUP BY sbpd.GuideSerie
+			,sbpd.GuideNumber), 0) Pieces_Cold
+	,ISNULL((SELECT
+		COUNT(1)
+	FROM SettlementByPickup sbp
+	JOIN SettlementByPickupDetail sbpd
+		ON (sbp.Id = sbpd.SettlementByPickupId)
+	JOIN DeliveryOrderPiece dop
+		ON dop.GuideSerie = sbpd.GuideSerie
+			AND dop.GuideNumber = sbpd.GuideNumber
+			AND dop.NoPiece = sbpd.NoPiece
+	WHERE Id = @IdManifest
+	AND sbpd.GuideSerie = do.Guide_Serie
+	AND sbpd.GuideNumber = do.Guide_Number
+	AND sbpd.IsPieceLiquidaded = 1
+	AND (dop.IsDry IS NULL OR dop.IsDry = 1)
+	GROUP BY sbpd.GuideSerie
+			,sbpd.GuideNumber),0) Pieces_Dry
+	,isnull(do.Receiver_FirstName,'') + ' ' + isnull(do.Receiver_LastName,'') as Receiver_Fullname
+	,do.Receiver_Address AS Receiver_Address
+	,CONVERT(NVARCHAR, ISNULL(do.Receiver_Zone,0)) AS Receiver_Zone
+	,do.Receiver_Town AS Receiver_Town
+	,do.Receiver_Department AS  Receiver_Departament
+	,CONVERT(varchar, do.Preparation_Date, 103) + ' ' + CONVERT(varchar(5), do.Preparation_Date, 108) as Preparation_Date
+	,CONVERT(varchar, do.Shipping_Date, 103) as Shipping_Date
+	,isnull(CONVERT(varchar, do.Delivery_Max_Date, 103),'') as Max_Date
+	,do.Receiver_Phone as Receiver_Phone
+	,(SELECT DeliveryBackOffice.dbo.fn_get_rackposition(do.Guide_Serie, do.Guide_Number)) as Rack_Position
+	,Collect_OnDelivery
+	FROM [DeliveryBackOffice].[dbo].DeliveryOrder do
+	INNER JOIN (
+		SELECT GuideSerie, GuideNumber 
+		FROM [DeliveryBackOffice].[dbo].SettlementByPickupDetail
+		WHERE SettlementByPickupId = @IdManifest 
+		AND IsPieceLiquidaded = 1 -- Pieza de la guia liquidada
+		GROUP BY GuideSerie, GuideNumber
+	) dsd ON do.Guide_Serie = dsd.GuideSerie AND do.Guide_Number = dsd.GuideNumber
+
+	SELECT * FROM @temp
+	order by Receiver_Departament asc, Receiver_Town asc, Receiver_Zone asc, Receiver_Address asc
+
+END
