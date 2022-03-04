3e186ef1a5d8f2710cbfa98ae81d1e2b0df39f77
Oscar Morales <oscar.morales@forzalatam.com>
2022-03-03 18:49:59 -0600

    Date: 2022-03-03 Author: Oscar Morales
    ### ADDED
    * Se crea sp para obtener los usuarios de los express centers.

---
+++ DeliveryBackOffice/StoreProcedures/CREATE PROCEDURE [dbo].[GetUserByExpressCenter].sql
@@ -0,0 +1,47 @@
+USE [DeliveryBackOffice]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <2022-03-03>
+-- Description:	<Recupera información de Express center y los usuarios de express center para form ClosureReport>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetUserByExpressCenter]
+AS
+BEGIN
+-- SET NOCOUNT ON added to prevent extra result sets from
+-- interfering with SELECT statements.
+SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT
+		IdVisitPointClient
+		,DescriptionOfClient 
+	FROM VisitPointClient
+	WHERE IdKindOfVPClient = 1
+	AND StatusClient = 1
+	ORDER BY DescriptionOfClient
+
+	SELECT
+		vpu.IdVisitPointClient
+	   ,rua.RuaIdAccount
+	   ,CONCAT(vpc.DescriptionOfClient, ' - ', rus.UsrEmail) Usuario
+	FROM dbo.VisitPointByUser vpu
+	JOIN dbo.RegisterUser rus
+		ON rus.UsrIdUser = vpu.RegisterUserID
+			AND vpu.RowStatus = 1
+	JOIN dbo.RolByUserByAccount rua
+		ON rua.RuaIdUser = rus.UsrIdUser
+			AND vpu.RowStatus = 1
+	JOIN dbo.VisitPointClient vpc
+		ON vpc.IdVisitPointClient = vpu.IdVisitPointClient
+			AND IdKindOfVPClient = 1
+			AND StatusClient = 1
+	WHERE vpu.RowStatus = 1
+	ORDER BY vpc.DescriptionOfClient
+END
+GO
