7051ca6d1bbbadd19e35291ae18a0654c257e188
Alejandro Rodríguez <alerod620@gmail.com>
2022-03-22 15:54:54 -0600

    Date: 2022-03-22 Author: Alejandro Rodríguez ### CHANGED * Se modificó la función que obtiene los intentos de entrega.

--- DeliveryBackOffice/Functions/CREATE FnGetCustomerAttempts.sql
+++ DeliveryBackOffice/Functions/CREATE FnGetCustomerAttempts.sql
@@ -1,6 +1,6 @@
 USE [DeliveryBackOffice]
 GO
-
+/****** Object:  UserDefinedFunction [dbo].[FnGetCustomerAttempts]    Script Date: 22/03/2022 09:49:51 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -26,35 +26,55 @@ BEGIN
 	BEGIN 
 
 		--Asigna el IdCustomer
-		SET @IdCustomer = (SELECT CustomerID FROM DeliveryBackOffice.dbo.VisitPointClient
-						   WHERE CodeOfReference = @IdSender)
+		SET @IdCustomer = (SELECT TOP 1
+								CustomerID
+							FROM DeliveryBackOffice.dbo.VisitPointClient
+							WHERE CodeOfReference = @IdSender)
 
 		--Selecciona el tipo de cliente que es
-		SET @TipoCliente = (SELECT IdCustomerType FROM DeliveryBackOffice.dbo.Customer
+		SET @TipoCliente = (SELECT TOP 1
+								IdCustomerType
+							FROM DeliveryBackOffice.dbo.Customer
 							WHERE IdCustomer = @IdCustomer)
 
 		--Cliente corporativo
 		IF (@TipoCliente = 1)
 		BEGIN
 
-			SET @Tarifario = (SELECT RbcIdRate FROM DeliveryBackOffice.dbo.RatebyCustomer
-							  WHERE RbcIdCustomer = @IdCustomer AND RbcRowStatus = 1 AND RbcCodeOfReference = @IdSender)
-
-			--Si no existe tarifario en corporativo, toma el del cliente
-			IF (@Tarifario IS NULL OR @Tarifario = 0)
+			IF EXISTS
+			(
+				SELECT
+					rbc.RbcIdRate
+				FROM dbo.RateByCustomer rbc
+				WHERE rbc.RbcIdCustomer = @IdCustomer
+				AND rbc.RbcRowStatus = 1
+				AND rbc.RbcCodeOfReference = @IdSender
+			)
 			BEGIN
-
-				SET @Tarifario = (SELECT RbcIdRate FROM DeliveryBackOffice.dbo.RatebyCustomer
-								  WHERE RbcIdCustomer = @IdCustomer AND RbcRowStatus = 1)
+				SET @Tarifario = (SELECT
+									  RbcIdRate
+								  FROM DeliveryBackOffice.dbo.RatebyCustomer
+								  WHERE RbcIdCustomer = @IdCustomer
+								  AND RbcRowStatus = 1
+								  AND RbcCodeOfReference = @IdSender)
+			END
+			ELSE
+			BEGIN
+				SET @Tarifario = (SELECT
+									  RbcIdRate
+								  FROM DeliveryBackOffice.dbo.RatebyCustomer
+								  WHERE RbcIdCustomer = @IdCustomer
+								  AND RbcRowStatus = 1
+								  AND RbcCodeOfReference = NULL)
 			END
 
-			SET @Attempts = (SELECT Attempt FROM DeliveryBackOffice.dbo.RateHeader
-							 WHERE RheId = @Tarifario)
+			SET @Attempts = (SELECT
+								Attempt
+							FROM DeliveryBackOffice.dbo.RateHeader
+							WHERE RheId = @Tarifario)
 
-		END
-
-		--Los demás clientes
-		ELSE
+		END		
+		ELSE--Los demás clientes
 		BEGIN
 
 			SET @Attempts = 2
@@ -67,59 +87,50 @@ BEGIN
 	BEGIN
 
 		--Selecciona el tipo de cliente que es
-		SET @TipoCliente = (SELECT IdCustomerType FROM DeliveryBackOffice.dbo.Customer
+		SET @TipoCliente = (SELECT  TOP 1
+								IdCustomerType
+							FROM DeliveryBackOffice.dbo.Customer
 							WHERE IdCustomer = @IdCustomer)
 
 		--Cliente corporativo
 		IF (@TipoCliente = 1)
 		BEGIN
 
-			SET @Tarifario = (SELECT RbcIdRate FROM DeliveryBackOffice.dbo.RatebyCustomer
-							  WHERE RbcIdCustomer = @IdCustomer AND RbcRowStatus = 1 AND RbcCodeOfReference = @IdSender)
-
-			--Si no existe tarifario en corporativo, toma el del cliente
-			IF (@Tarifario IS NULL OR @Tarifario = 0)
+			IF EXISTS
+			(
+				SELECT
+					rbc.RbcIdRate
+				FROM dbo.RateByCustomer rbc
+				WHERE rbc.RbcIdCustomer = @IdCustomer
+				AND rbc.RbcRowStatus = 1
+				AND rbc.RbcCodeOfReference = @IdSender
+			)
 			BEGIN
-
-				SET @Tarifario = (SELECT RbcIdRate FROM DeliveryBackOffice.dbo.RatebyCustomer
-								  WHERE RbcIdCustomer = @IdCustomer AND RbcRowStatus = 1)
+				SET @Tarifario = (SELECT
+									  RbcIdRate
+								  FROM DeliveryBackOffice.dbo.RatebyCustomer
+								  WHERE RbcIdCustomer = @IdCustomer
+								  AND RbcRowStatus = 1
+								  AND RbcCodeOfReference = @IdSender)
+			END
+			ELSE
+			BEGIN
+				SET @Tarifario = (SELECT
+									  RbcIdRate
+								  FROM DeliveryBackOffice.dbo.RatebyCustomer
+								  WHERE RbcIdCustomer = @IdCustomer
+								  AND RbcRowStatus = 1
+								  AND RbcCodeOfReference = NULL)
 			END
 
-			SET @Attempts = (SELECT Attempt FROM DeliveryBackOffice.dbo.RateHeader
-							 WHERE RheId = @Tarifario)
+			SET @Attempts = (SELECT
+								Attempt
+							FROM DeliveryBackOffice.dbo.RateHeader
+							WHERE RheId = @Tarifario)
 
 		END
-
-		--Los demás clientes
-		ELSE
-		BEGIN
-
-			SET @Attempts = 2
-
-		END
-
-	END		
-
-	--Caso en el que exista IdCustomer pero no IdSender
-	ELSE IF ((@IdSender = 0 OR @IdSender IS NULL) AND (@IdCustomer != 0 AND @IdCustomer IS NOT NULL))
-	BEGIN
-
-		--Ya que no se cuenta con un IdSender, solo es posible tomar el tarifario del cliente
-
-		SET @Tarifario = (SELECT RbcIdRate FROM DeliveryBackOffice.dbo.RatebyCustomer
-								  WHERE RbcIdCustomer = @IdCustomer AND RbcRowStatus = 1 AND RbcCodeOfReference IS NULL)
 		
-		--Verifica que el cliente si cuente con tarifario
-		IF (@Tarifario IS NULL OR @Tarifario = 0)
-		BEGIN
-
-			SET @Attempts = (SELECT Attempt FROM DeliveryBackOffice.dbo.RateHeader
-							 WHERE RheId = @Tarifario)
-
-		END
-
-		--Valor por defecto
-		ELSE
+		ELSE--Los demás clientes
 		BEGIN
 
 			SET @Attempts = 2
