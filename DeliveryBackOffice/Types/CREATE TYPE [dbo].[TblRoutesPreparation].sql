daca356b51a390006eb628c9b1d4987c0cbac3b9
Cesarsazo09 <cesar.sazo@forzalatam.com>
2022-04-07 17:56:24 -0600

    Date: 2022-04-07 Author: César Sazo ### ADDED Se agrego un nuevo tipo de dato tabla para enviar información empaquetada desde la preparación de recolección para poder generar el manifiesto de forma transaccional
    ### CHANGED
    Se modifico el stored procedure sphd_set_AssigmentRouteService para crear los servicios de forma transaccional con el nuevo tipo de dato.
    Se midifico el stored procedure sphd_getPreparationRoutesReportInfo agregando la sentencia WITH NOLOCK para evitar bloqueos de las tablas.

---
+++ DeliveryBackOffice/Types/CREATE TYPE [dbo].[TblRoutesPreparation].sql
@@ -0,0 +1,15 @@
+USE [DeliveryBackOffice]
+GO
+
+CREATE TYPE [dbo].[TblRoutesPreparation] AS TABLE(
+	[rwNumber] [INT] NULL,
+	[StartDate] [DATETIME] NULL,
+	[EndDate] [DATETIME] NULL,
+	[CodeOfReference] [INT] NULL,
+	[nameSender] [VARCHAR](200) NULL,
+	[phoneSender] [VARCHAR](50) NULL,
+	[addressPickUp] [VARCHAR](500) NULL,
+	[idTownship] [INT] NULL,
+	[OrderP] [SMALLINT] NULL
+)
+GO
