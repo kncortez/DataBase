8689162e71d438f09a23522fb23fc14193549108
mespinoza <michael.espinoza@forzalatam.com>
2022-05-11 10:32:51 -0600

    [FDAPI-433-20220511]
    Modificaciones:
    - Correciones sp de configuracion de modulos concesionarios

--- DeliveryBackOffice/Script/INSERT INTO DeliveryBackOffice.dbo.RolByModuleBySystem.sql
+++ DeliveryBackOffice/Script/INSERT INTO DeliveryBackOffice.dbo.RolByModuleBySystem.sql
@@ -1,5 +1,36 @@
 DECLARE @rol BIGINT = (SELECT RolIdRol FROM DeliveryBackOffice.dbo.CatRol WHERE RolName='Concesionario');
 
+--RmsIdSystem numero 1 es Portal Web
+
+BEGIN TRANSACTION
+
+BEGIN TRY
+
+DECLARE @modules TABLE (
+		idModule INT
+	)
+
+INSERT INTO @modules
+(idModule)
+SELECT cm.ModIdModule FROM 
+DeliveryBackOffice.dbo.CatModule cm WHERE cm.ModName 
+IN(
+'Login',
+'Mi perfil',
+'Mis Envíos',
+'Crear Guías',
+'Rastreo',
+'Mis Datos',
+'Cartera Clientes',
+'Cierres',
+'Reportes',
+'Servicios',
+'Recepción',
+'Entrega',
+'Devolución',
+'Traslados'
+)
+AND cm.ModRowStatus=1 AND cm.ModVisible=1
 
 INSERT INTO DeliveryBackOffice.dbo.RolByModuleBySystem 
 (
@@ -10,65 +41,43 @@ RmsRowStatus,
 RmsTokenCreated,
 RmsDateCreated
 )
-VALUES
-(
-@rol,--RmsIdRol,
-1,--RmsIdSystem,
-1,--RmsIdModule,
-1,--RmsRowStatus,
-'SYS-MESPINOZA',--RmsTokenCreated,
-GETDATE()--RmsDateCreated
-),
-(
-@rol,--RmsIdRol,
-1,--RmsIdSystem,
-2,--RmsIdModule,
-1,--RmsRowStatus,
-'SYS-MESPINOZA',--RmsTokenCreated,
-GETDATE()--RmsDateCreated
-),
-(
-@rol,--RmsIdRol,
-1,--RmsIdSystem,
-6,--RmsIdModule,
-1,--RmsRowStatus,
-'SYS-MESPINOZA',--RmsTokenCreated,
-GETDATE()--RmsDateCreated
-),
-(
-@rol,--RmsIdRol,
-1,--RmsIdSystem,
-7,--RmsIdModule,
-1,--RmsRowStatus,
-'SYS-MESPINOZA',--RmsTokenCreated,
-GETDATE()--RmsDateCreated
-),
-(
-@rol,--RmsIdRol,
-1,--RmsIdSystem,
-8,--RmsIdModule,
-1,--RmsRowStatus,
-'SYS-MESPINOZA',--RmsTokenCreated,
-GETDATE()--RmsDateCreated
-),
-(
-@rol,--RmsIdRol,
-1,--RmsIdSystem,
-10,--RmsIdModule,
-1,--RmsRowStatus,
-'SYS-MESPINOZA',--RmsTokenCreated,
-GETDATE()--RmsDateCreated
-),
-(
-@rol,--RmsIdRol,
-1,--RmsIdSystem,
-14,--RmsIdModule,
-1,--RmsRowStatus,
-'SYS-MESPINOZA',--RmsTokenCreated,
-GETDATE()--RmsDateCreated
-)
+SELECT 
+@rol, --RmsIdRol,
+1, --RmsIdSystem,
+mdl.idModule, --RmsIdModule,
+1, --RmsRowStatus,
+'SYS-MESPINOZA', --RmsTokenCreated,
+GETDATE() --RmsDateCreated
+FROM @modules mdl
 
+END TRY
 
-SELECT cm.ModName, RmsIdModule FROM DeliveryBackOffice.dbo.RolByModuleBySystem 
+BEGIN CATCH
+
+SELECT 'Error al Configurar Modulos' AS message,
+				'FALSE'	blnResult,
+				CAST(-1 AS VARCHAR(5)) IdResult,
+				CAST(500 AS VARCHAR(5)) StatusResult,
+				CAST(ERROR_NUMBER() AS VARCHAR) AS ErrorNumber,
+				CAST(ERROR_SEVERITY() AS VARCHAR) AS ErrorSeverity,
+				CAST(ERROR_STATE() AS VARCHAR) AS ErrorState,
+				CAST(ERROR_PROCEDURE() AS VARCHAR) AS ErrorProcedure,
+				CAST(ERROR_LINE() AS VARCHAR) AS ErrorLine,
+				CAST(ERROR_MESSAGE() AS VARCHAR(MAX)) AS ResultMessage;
+
+ROLLBACK TRANSACTION;
+
+END CATCH
+
+IF @@TRANCOUNT > 0 BEGIN
+
+COMMIT TRANSACTION;
+
+---- Script para confirmar los modulos que tiene asignado el rol
+SELECT cm.ModName, RmsIdModule, cm.ModPath FROM DeliveryBackOffice.dbo.RolByModuleBySystem 
 JOIN DeliveryBackOffice.dbo.CatModule cm ON cm.ModIdModule=RmsIdModule 
 WHERE RmsIdRol=@rol AND RmsRowStatus=1
+
+END
+
+
