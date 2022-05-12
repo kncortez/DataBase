5ef4bb31ad3f4f07dce1a794e48ed4dc798e9451
Oscar Morales <oscar.morales@forzalatam.com>
2022-05-12 09:24:50 -0600

    Date: 2022-05-12 Author: Oscar Morales
    ### ADDED
    * Se agregó sp que actualiza el orden de un servicio en pantalla de asignación de servicios a demanda.
    
    ### CHANGED
    * Se modificaron sp para poder mostrar los ids de los servicios y en los servicios asignados el orden de estos.
    * Se modificaron sp del manifiesto de recolecciones para agregar el total de serviciios y la columna orden.

---
+++ DeliveryBackOffice/StoreProcedures/dbo.UpdateOrderOfServiceManagement.sql
@@ -0,0 +1,71 @@
+USE [DeliveryBackOffice]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <2022-05-11>
+-- Description:	<Actualiza el orden de un servicio>
+-- =============================================
+CREATE PROCEDURE UpdateOrderOfServiceManagement 
+	@IdServiceManagement INT,
+	@Order SMALLINT,
+	@Token VARCHAR(50)
+AS
+BEGIN
+	DECLARE @RModified INT = 0
+
+	BEGIN TRANSACTION
+
+		BEGIN TRY
+
+			UPDATE ServiceManagement
+			SET [Order] = @Order
+			   ,TokenUpdated = @Token
+			   ,DateUpdated = GETDATE()
+			WHERE IdServiceManagement = @IdServiceManagement
+
+			SET @RModified = @@ROWCOUNT
+
+		END TRY
+		BEGIN CATCH
+			ROLLBACK TRANSACTION
+			
+			SELECT 
+				0 AS 'StatusCode', 
+				ERROR_MESSAGE() AS 'Description', 
+				CONVERT(BIGINT, 0) AS 'NumTransferID'
+			
+		END CATCH;
+
+	IF (@@TRANCOUNT > 0)
+	BEGIN
+		IF (@RModified > 0)
+		BEGIN
+			COMMIT TRANSACTION;
+			SELECT			  
+				200 AS 'StatusCode',
+				'Registro actualizado correctamente' AS 'Description', 
+				@@TRANCOUNT AS 'NumTransferID'
+		END
+		ELSE
+		BEGIN
+			ROLLBACK TRANSACTION
+			SELECT			  
+				0 AS 'StatusCode',
+				'Registros no actualizado' AS 'Description', 
+				0 AS 'NumTransferID'
+			
+		END
+	END
+	ELSE
+	BEGIN
+		SELECT 
+			0 AS 'StatusCode', 
+			ERROR_MESSAGE() AS 'Description', 
+			CONVERT(BIGINT, 0) AS 'NumTransferID'
+	END
+END;
---
+++ DeliveryBackOffice/StoreProcedures/dbo.sphd_deliveryorder_settlement_guides_pickup_header.sql
@@ -0,0 +1,62 @@
+USE [DeliveryBackOffice]
+GO
+/****** Object:  StoredProcedure [dbo].[sphd_deliveryorder_settlement_guides_pickup_header]    Script Date: 11/05/2022 19:41:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- ==========================================j=
+-- Author:		<Alberto,,Ixchop>
+-- Create date: <2022-03-24>
+-- Description:	<Obtiene los datos generales del manifiesto de recolecciones>
+-- ============================================= 
+ALTER PROCEDURE [dbo].[sphd_deliveryorder_settlement_guides_pickup_header]
+	-- Add the parameters for the stored procedure here
+		@idRoute AS int,
+		@dateRoute AS date = ''
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+  
+	SELECT 
+		rat.IdRouteAssigment,
+		CONCAT(snr.First_Name,' ',snr.Last_Name) as Courier,		
+		rat.DateOfRoute,
+		--count(IIF(dor.Guide_Number is not null and  dor.Guide_Serie is not  null,1,null)) guide_count,
+		sum(smt.guide_count)guide_count,
+		--sum(ISNULL(dor.Pieces_Cold,0)) cold_count,
+		sum(smt.cold_count) cold_count,
+		--sum(ISNULL(dor.Pieces_Dry,0)) dry_count,
+		sum(smt.dry_count) dry_count,
+		sum(smt.Amount) total
+		--,smt.IdServiceManagement
+		--,smt.IdPuRouteAssigment
+		--,smt.IdSchedulePickup
+		 --,*
+  from 
+  [DeliveryBackOffice].[dbo].[RouteAssigment] as rat WITH(NOLOCK) 	
+	join 
+	(
+		select  IdServiceManagement,IdPuRouteAssigment,IdSchedulePickup, (ISNULL(sm.Amount,0)) Amount,  
+		count(IIF(dor.Guide_Number is not null and  dor.Guide_Serie is not  null,1,null)) guide_count,
+		sum(ISNULL(dor.Pieces_Cold,0)) cold_count,
+		sum(ISNULL(dor.Pieces_Dry,0)) dry_count
+		from [DeliveryBackOffice].[dbo].[ServiceManagement]sm
+		join [DeliveryBackOffice].[dbo].[SchedulePickup] as spu WITH(NOLOCK) on spu.SchedulePickupId = IdSchedulePickup 
+		left join [DeliveryBackOffice].[dbo].[DeliveryOrderPaymentDetail] as dop WITH(NOLOCK) ON sm.IdSchedulePickup=dop.IdHeaderRecolection
+		left join[DeliveryBackOffice].[dbo].[DeliveryOrder] AS dor WITH(NOLOCK) ON dor.Guide_Number = dop.GuideNumber AND dor.Guide_Serie = dop.GuideSerie
+		group by sm.Amount,IdServiceManagement,IdSchedulePickup,IdPuRouteAssigment
+	)smt on smt.IdPuRouteAssigment = rat.IdRouteAssigment
+	left join [DeliveryBackOffice].[dbo].[SenderReceiver] as snr WITH(NOLOCK) on rat.IdCurrierMan = snr.ID
+	WHERE rat.IdRoute=@idRoute AND rat.DateOfRoute = @dateRoute
+  
+   GROUP BY 
+	rat.IdRouteAssigment
+	,rat.DateOfRoute
+	,snr.First_Name
+	,snr.Last_Name
+
+
+END
