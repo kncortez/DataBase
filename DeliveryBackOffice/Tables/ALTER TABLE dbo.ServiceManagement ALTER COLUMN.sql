53dc70c970312ef1c5f58015a5367108ce6b004d
Oscar Morales <oscar.morales@forzalatam.com>
2022-06-13 13:07:10 -0600

    Date: 2022-06-13 Author: Oscar Morales
    ### CHANGED
    * Se agregó descripción a las columnas CiPuDate y CoPuDate de la tabla ServiceManagement.

--- DeliveryBackOffice/Tables/ALTER TABLE dbo.ServiceManagement ALTER COLUMN.sql
+++ DeliveryBackOffice/Tables/ALTER TABLE dbo.ServiceManagement ALTER COLUMN.sql
@@ -3,3 +3,10 @@ GO
 
 ALTER TABLE ServiceManagement ALTER COLUMN CiPuDate DATETIME
 ALTER TABLE ServiceManagement ALTER COLUMN CoPuDate DATETIME
+
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fecha y hora en la que inicia un proceso de recolección en Courier.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ServiceManagement', @level2type=N'COLUMN',@level2name=N'CiPuDate'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fecha y hora en la que finaliza un proceso de recolección en Courier.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ServiceManagement', @level2type=N'COLUMN',@level2name=N'CoPuDate'
+GO
