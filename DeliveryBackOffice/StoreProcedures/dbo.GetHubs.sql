eb1f043f59a78dc79b7c3048725145cd9882e9ff
Oscar Morales <oscar.morales@forzalatam.com>
2022-06-20 17:47:08 -0600

    Date: 2022-06-20 Author: Oscar Morales
    ### ADDED
    * Se agregó sp GetHubs que obtiene todos los hubs.
    ### CHANGED
    * Se modificó sp spg_get_RoutePreparationPickUp para poder filtrar por hub en la búsqueda de servicios.

---
+++ DeliveryBackOffice/StoreProcedures/dbo.GetHubs.sql
@@ -0,0 +1,27 @@
+USE [DeliveryBackOffice]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <20/06/2022>
+-- Description:	<Obtiene el listado de Hubs>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetHubs]
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT
+		IdHubLogistic Id
+	   ,HubAbbreviation Hub
+	FROM HubLogistics
+END
+GO
