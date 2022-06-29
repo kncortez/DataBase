f1450356e042996f86ba791c89e00650b7bde818
Jerson Ochoa <jerson.ochoa@forzadelivery.com>
2022-06-29 14:58:17 -0600

    [ADD] SP SP_HM_LOGIN - Login for Hermes Mobile APP

---
+++ DeliveryBackOffice(DEV)/dbo/Inserts/APO-33/1. Sentencias (Inserción)/1. Insert CatSystem.sql
@@ -0,0 +1,3 @@
+-- CatSystem - Registro de sistema Hermes Mobile
+INSERT INTO CatSystem (SysNameSystem, SysPlataform, SysDescription, SysRowStatus, SysTokenCreated, SysDateCreated)
+VALUES ('Hermes Mobile', 'Forza Delivery Mobile', 'App Operaciones Mobile', 1, 'SYS-ADMIN', SYSDATETIME());
