ccf3c0168d18105fdceca2792964b7c60f688197
Jerson Ochoa <jerson.ochoa@forzadelivery.com>
2022-07-06 17:06:39 -0600

    Date: 06/07/2022 Author: Jerson Ochoa ### ADDED * [APO-16] Sp GetTypeContainerByStatus

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sp_HM_getTypeContainerByStatus.sql
@@ -0,0 +1,19 @@
+﻿-- =============================================
+-- Author:		<Ochoa, Jerson>
+-- Create date: <06-07-2022>
+-- Description:	<Get list of TypeContainer filter by rowstatus = 1>
+-- =============================================
+CREATE PROCEDURE sp_HM_getTypeContainerByStatus
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	SELECT	[CTC].[IdCatTypeContainer], 
+			[CTC].[TypeContainerName],
+			[CTC].[TypeContainerSerie],
+			[CTC].[TypeContainerDescription]
+	FROM [dbo].[CatTypeContainer] CTC
+	WHERE [CTC].[RowStatus] = 1;
+END
