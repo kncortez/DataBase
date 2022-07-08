1259fb53416c39973e48c511f781d9a281d78c51
Cesarsazo09 <cesar.sazo@forzalatam.com>
2022-07-08 09:31:13 -0600

    Date: 08/07/2022 Author: César Dionicio Sazo
    ### UPDATED
    * Se actualizó el repositorio de Desarrollo respecto al proyecto de Producción por la publicación del 08/07/2022

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetStations.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <2022-06-03>
+-- Description:	<Obtiene la información de las estaciones>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetStations]
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT IdStation [IdStation]
+		, StationName [StationName]
+		, CountryId [CountryId]
+		, StationType [StationType]
+		, HubLogisticId [HubLogisticId]
+	FROM CatStation
+	WHERE RowStatus = 1 AND StationType = 1
+END
