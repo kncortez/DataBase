206cde32a01d02f8ab2dbaba193558c16861632e
Oscar Morales <oscar.morales@forzalatam.com>
2022-08-05 16:57:21 -0600

    Date: 2022-08-05 Author: Oscar Morales
    ### ADDED
    * Sp spHM_GetGuidesPendingSettlement para el método de API /Mobile/HM_GetGuidesPendingSettlement

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetGuidesPendingSettlement.sql
@@ -0,0 +1,39 @@
+﻿-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <2022-08-04>
+-- Description:	<Obtiene el listado de guías pendientes de liquidación Linehaul>
+-- =============================================
+CREATE PROCEDURE [dbo].[spHM_GetGuidesPendingSettlement]
+	-- Add the parameters for the stored procedure here
+	@LinehaulRoutePreparationId INT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    SELECT
+	lrpcd.GuideSerie
+   ,lrpcd.GuideNumber
+   ,lrpcdp.PieceNumber
+	FROM LinehaulRoutePreparationContainer lrpc
+	INNER JOIN LinehaulRoutePreparationContainerDetail lrpcd
+		ON lrpcd.LinehaulRoutePreparationContainerId = lrpc.IdLinehaulRoutePreparationContainer
+	INNER JOIN LinehaulRoutePreparationContainerDetailPiece lrpcdp
+		ON lrpcdp.LinehaulRoutePreparationContainerDetailId = lrpcd.IdLinehaulRoutePreparationContainerDetail
+	WHERE lrpc.LinehaulRoutePreparationId = @LinehaulRoutePreparationId
+	EXCEPT
+	SELECT
+		lrscd.GuideSerie
+	   ,lrscd.GuideNumber
+	   ,lrscdp.PieceNumber
+	FROM LinehaulRouteSettlement lrs
+	INNER JOIN LinehaulRouteSettlementContainer lrsc
+		ON lrsc.LinehaulRouteSettlementId = lrs.IdLinehaulRouteSettlement
+	INNER JOIN LinehaulRouteSettlementContainerDetail lrscd
+		ON lrscd.LinehaulRouteSettlementContainerId = lrsc.IdLinehaulRouteSettlementContainer
+	INNER JOIN LinehaulRouteSettlementContainerDetailPiece lrscdp
+		ON lrscdp.LinehaulRouteSettlementContainerDetailId = lrscd.IdLinehaulRouteSettlementContainerDetail
+	WHERE lrs.LinehaulRoutePreparationId = @LinehaulRoutePreparationId
+
+END
