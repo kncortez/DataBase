4edd14245ab49c038717103f7a7c79b227077b12
Oscar Morales <oscar.morales@forzalatam.com>
2022-08-11 14:56:47 -0600

    Date: 2022-08-11 Author: Oscar Morales
    ### ADDED
    * Sp spHM_updateOpenProcessInLinehaulRouteSettlementContainerDetail para el método de API /Mobile/HM_UpdateOpenProcessInLinehaulRoutePreparationContainerDetail

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_updateOpenProcessInLinehaulRouteSettlementContainerDetail.sql
@@ -0,0 +1,57 @@
+﻿-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <2022-08-11>
+-- Description:	<Actualiza un proceso abierto en LinehaulRouteSetlementContainerDetail>
+-- =============================================
+CREATE PROCEDURE [dbo].[spHM_updateOpenProcessInLinehaulRouteSettlementContainerDetail]
+	@LinehaulRouteSettlementContainerId AS INT,
+	@GuideSerie AS NVARCHAR(25),
+	@GuideNumber AS NVARCHAR(50),
+	@OpenProcess AS INT,
+	@RowStatus AS INT,
+	@TknUser AS NVARCHAR(50)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION
+	BEGIN TRY
+		
+		UPDATE LinehaulRouteSettlementContainerDetail
+		SET IsOpenProcess = @OpenProcess
+		   ,TokenUpdated = @TknUser
+		   ,DateUpdated = GETDATE()
+		   ,RowStatus = @RowStatus
+		WHERE LinehaulRouteSettlementContainerId = @LinehaulRouteSettlementContainerId
+		AND GuideSerie = @GuideSerie
+		AND GuideNumber = @GuideNumber
+
+		IF (@@TRANCOUNT > 0)
+			COMMIT TRANSACTION;
+
+		SELECT 
+			IdLinehaulRouteSettlementContainerDetail
+			,LinehaulRouteSettlementContainerId
+			,GuideSerie
+			,GuideNumber
+			,IsOpenProcess
+		FROM LinehaulRouteSettlementContainerDetail
+		WHERE LinehaulRouteSettlementContainerId = @LinehaulRouteSettlementContainerId 
+			AND GuideSerie = @GuideSerie
+			AND GuideNumber = GuideNumber
+
+		
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+		
+		SELECT 0 [spResult],
+				ERROR_NUMBER() AS [ErrorNumber],
+				ERROR_SEVERITY() AS [ErrorSeverity],
+				ERROR_STATE() AS [ErrorState],
+				ERROR_PROCEDURE() AS [ErrorProcedure],
+				ERROR_LINE() AS [ErrorLine],
+				ERROR_MESSAGE() AS [ErrorMessage];
+	END CATCH
+END
