36e4d7ffb14be4b3dd66cdb49d639c37ff9a01f2
EdelmanV <edelman.vasquez@forzalatam.com>
2022-08-18 14:53:30 -0600

    date: 2022-08-18 author:edelman description: modificación para mostrar nombre del hub por el código en detalle del reporte linehauls

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_LinehaulRouteManifestReportDetailFloor.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_LinehaulRouteManifestReportDetailFloor.sql
@@ -16,14 +16,16 @@ BEGIN
 		 SUM(ISNULL(LRPC.GuideQuantity,0))      AS Totaldeguiasasignadasacontenedor,
 		 SUM(ISNULL(LRPC.ColdPieceQuantity,0))  AS TotaldePiezasFriasAsignadasaPiso,
 		 SUM(ISNULL(LRPC.DryPieceQuantity,0))   AS TotaldePiezasAsignadasaPiso,
-		 LRPC.HubDestinyId
+		  HL.HubName AS HubDestinyId
   FROM   LinehaulRoutePreparationContainer LRPC WITH (NOLOCK)
 		INNER JOIN LinehaulRoutePreparationContainerDetail LRPCD WITH (NOLOCK)
 				ON LRPC.IdLinehaulRoutePreparationContainer = LRPCD.LinehaulRoutePreparationContainerId
 		LEFT JOIN Container C WITH (NOLOCK)
 				ON LRPC.ContainerId = C.IdContainer
+		LEFT JOIN dbo.HubLogistics HL
+		       ON HL.IdHubLogistic = LRPC.HubDestinyId
   WHERE C.ContainerDescription  LIKE ('%lh%') AND LRPC.LinehaulRoutePreparationId = @IdLinehaulRoutePreparation
-  GROUP BY LRPC.HubDestinyId,C.ContainerDescription
+  GROUP BY LRPC.HubDestinyId,C.ContainerDescription, HL.HubName
 
 
 
--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_RoutemanifestReportLinehaulDetail.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_RoutemanifestReportLinehaulDetail.sql
@@ -13,17 +13,19 @@ BEGIN
  SELECT  LRPC.LinehaulRoutePreparationId,
          C.ContainerDescription,
 		sum(LRPC.GuideQuantity)                 AS Totaldeguiasasignadasacontenedor,
-		SUM( ISNULL(LRPC.ColdPieceQuantity,0))  AS TotaldePiezasFriasAsignadasaContenedor,
-		SUM( ISNULL(LRPC.DryPieceQuantity,0))   AS TotaldePiezasAsignadasContenedor,
-		 LRPC.HubDestinyId
+		SUM(ISNULL(LRPC.ColdPieceQuantity,0))   AS TotaldePiezasFriasAsignadasaContenedor,
+		SUM(ISNULL(LRPC.DryPieceQuantity,0))    AS TotaldePiezasAsignadasContenedor,
+		 HL.HubName AS HubDestinyId
   FROM   LinehaulRoutePreparationContainer LRPC WITH (NOLOCK)
 		INNER JOIN LinehaulRoutePreparationContainerDetail LRPCD WITH (NOLOCK)
 			ON LRPC.IdLinehaulRoutePreparationContainer = LRPCD.LinehaulRoutePreparationContainerId
 		LEFT JOIN Container C WITH (NOLOCK)
 			ON LRPC.ContainerId = C.IdContainer
+		LEFT JOIN dbo.HubLogistics HL
+		    ON HL.IdHubLogistic = LRPC.HubDestinyId
   WHERE C.ContainerDescription NOT LIKE ('%lh%') AND LRPC.LinehaulRoutePreparationId = @IdLinehaulRoutePreparation
 	  GROUP BY LRPC.LinehaulRoutePreparationId, 
 	              C.ContainerDescription, 
-			   LRPC.HubDestinyId
+			     HL.HubName
 
 END
