4a526d0758db93b0b925e8e8d44c8ee0cabe7bf4
AndresRuizPeer <andres.ruiz@forzalatam.com>
2022-08-22 10:54:01 -0600

    Date: 22/08/2022 Author: Jose Andres Ruiz Peer
    ### FIXED
    * Tildes del tipo de servicio

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-876 1. INSERT INTO [dbo].[CatTypeService] StandardService.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-876 1. INSERT INTO [dbo].[CatTypeService] StandardService.sql
@@ -1,19 +1,19 @@
-IF( NOT EXISTS(SELECT TOP 1 1 FROM [DeliveryBackOffice].[dbo].[CatTypeService] CTS WITH(NOLOCK) WHERE CTS.CtsName = 'Estandar' COLLATE Latin1_General_CI_AI) )
+IF( NOT EXISTS(SELECT TOP 1 1 FROM [DeliveryBackOffice].[dbo].[CatTypeService] CTS WITH(NOLOCK) WHERE CTS.CtsName = 'Estándar' COLLATE Latin1_General_CI_AI) )
 BEGIN
 	INSERT INTO
 		[DeliveryBackOffice].[dbo].[CatTypeService]
 		(CtsName, CtsShortName, CtsDescription, CtsRowStatus, CtsTokenCreated, CtsDateCreated, RateGroup)
 	VALUES
-		('Estandar', 'EST', 'Servicio de entrega estandar', 1, 'SYS-ARUIZ', GETDATE(), 1)
+		('Estándar', 'EST', 'Servicio de entrega estándar', 1, 'SYS-ARUIZ', GETDATE(), 1)
 END
 
-IF( NOT EXISTS(SELECT TOP 1 1 FROM [DeliveryBackOffice].[dbo].[CatTypeService] CTS WITH(NOLOCK) WHERE CTS.CtsName = 'Estandar CoD' COLLATE Latin1_General_CI_AI) )
+IF( NOT EXISTS(SELECT TOP 1 1 FROM [DeliveryBackOffice].[dbo].[CatTypeService] CTS WITH(NOLOCK) WHERE CTS.CtsName = 'Estándar CoD' COLLATE Latin1_General_CI_AI) )
 BEGIN
 	INSERT INTO
 		[DeliveryBackOffice].[dbo].[CatTypeService]
 		(CtsName, CtsShortName, CtsDescription, CtsRowStatus, CtsTokenCreated, CtsDateCreated, RateGroup)
 	VALUES
-		('Estandar CoD', 'ECOD', 'Servicio de entrega estandar con CoD', 1, 'SYS-ARUIZ', GETDATE(), 1)
+		('Estándar CoD', 'ECOD', 'Servicio de entrega estándar con CoD', 1, 'SYS-ARUIZ', GETDATE(), 1)
 END
 
 --SELECT * FROM [DeliveryBackOffice].[dbo].[CatTypeService]
