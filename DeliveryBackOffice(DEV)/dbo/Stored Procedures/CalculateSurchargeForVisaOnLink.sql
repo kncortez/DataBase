2bd814c03ce0a51b18a60c12ee0961ebd469a1a9
Oscar Morales <oscar.morales@forzalatam.com>
2022-08-29 15:55:30 -0600

    Date: 2022-08-29 Author: Oscar Morales
    ### ADDED
    * Tabla DeliveryOrderSurcarge para almacenar los cálculos de recargos por pagos con Visa On Link.
    * Sp CalculateSurchargeForVisaOnLink que se utiliza en el método de API Portal/CalculateSurchargeForVisaOnLink.
    
    Historia FDAPI-896

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/CalculateSurchargeForVisaOnLink.sql
@@ -0,0 +1,122 @@
+﻿-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <2022-08-24>
+-- Description:	<Cálcula y guarda los montos de los recargos por pago en VisaOnLink>
+-- =============================================
+CREATE PROCEDURE [dbo].[CalculateSurchargeForVisaOnLink]
+	-- Add the parameters for the stored procedure here
+	@TblGuides TblGuides READONLY,
+	@Token NVARCHAR(50)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION
+
+	BEGIN TRY
+
+		--Validar que sean del mismo cliente
+		IF ( SELECT
+				COUNT(1)
+			FROM (SELECT
+					IdCustomer
+				FROM DeliveryOrder do WITH (NOLOCK)
+				INNER JOIN @TblGuides tg
+					ON tg.Guide_Serie = do.Guide_Serie 
+					AND tg.Guide_Number = do.Guide_Number
+				GROUP BY do.IdCustomer) sub)
+		= 1
+		BEGIN
+			
+			--Deshabilitar registros anteriores
+			UPDATE dos
+			SET RowStatus = 0
+			   ,DateUpdated = GETDATE()
+			   ,TokenUpdated = @Token
+			FROM DeliveryOrderSurcharge dos
+			INNER JOIN @TblGuides tg
+				ON tg.Guide_Serie = dos.GuideSerie
+				AND tg.Guide_Number = dos.GuideNumber
+
+			--Cálcular e insertar en tabla
+			INSERT INTO DeliveryOrderSurcharge ([GuideSerie]
+			, [GuideNumber]
+			, [GuideOriginalPriceShipment]
+			, [GuideOriginalCollectOnDelivery]
+			, [TotalSurcharge]
+			, [InternalSurchargeAmount]
+			, [ExternalSurchargeAmount]
+			, [GuideSurchargedShipment]
+			, [GuideSurchargedCollectOnDelivery]
+			, [GuidePaymentLink]
+			, [RowStatus]
+			, [DateCreated]
+			, [TokenCreated]
+			, [DateUpdated]
+			, [TokenUpdated])
+				SELECT
+					tg.Guide_Serie
+				   ,tg.Guide_Number
+				   ,do.PriceShippment
+				   ,do.Collect_OnDelivery
+				   ,ROUND((do.PriceShippment + ISNULL(do.Collect_OnDelivery,0)) * 0.05,2)
+				   ,ROUND(ROUND((do.PriceShippment + ISNULL(do.Collect_OnDelivery,0)) * 0.05,2) - ROUND((do.PriceShippment + ISNULL(do.Collect_OnDelivery,0)) * 0.025,2),2)
+				   ,ROUND((do.PriceShippment + ISNULL(do.Collect_OnDelivery,0)) * 0.025,2)
+				   ,ROUND(do.PriceShippment * 1.05,2)
+				   ,ROUND(ISNULL(do.Collect_OnDelivery,0) * 1.05,2)
+				   ,NULL
+				   ,1
+				   ,GETDATE()
+				   ,@Token
+				   ,NULL
+				   ,NULL
+				FROM @TblGuides tg
+				INNER JOIN DeliveryOrder do WITH (NOLOCK)
+					ON do.Guide_Serie = tg.Guide_Serie
+						AND do.Guide_Number = tg.Guide_Number
+			SELECT
+			1 'ResponseCode'
+			,'Cálculo exitoso' 'Description'
+
+			SELECT dos.GuideSerie
+				,dos.GuideNumber
+				,dos.GuideSurchargedShipment
+				,dos.GuideSurchargedCollectOnDelivery
+				,dos.TotalSurcharge
+			FROM DeliveryOrderSurcharge dos
+			INNER JOIN @TblGuides tg
+				ON tg.Guide_Serie = dos.GuideSerie
+				AND tg.Guide_Number = dos.GuideNumber
+			WHERE dos.RowStatus = 1
+
+			IF @@TRANCOUNT > 0
+				COMMIT TRANSACTION
+		END
+		ELSE 
+		BEGIN
+			ROLLBACK TRANSACTION
+
+			SELECT
+				2 'ResponseCode'
+			   ,'Las guías no pertenecen al mismo cliente' 'Description'
+		END
+
+		
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+		
+		SELECT
+			0 'ResponseCode'
+		   ,ERROR_MESSAGE() 'Description'
+		   ,ERROR_NUMBER() 'ErrorNumber'
+		   ,ERROR_SEVERITY() 'ErrorSeverity'
+		   ,ERROR_STATE() 'ErrorState'
+		   ,ERROR_PROCEDURE() 'ErrorProcedure'
+		   ,ERROR_LINE() 'ErrorLine';
+
+		
+	END CATCH
+	
+END
---
+++ DeliveryBackOffice(DEV)/dbo/Tables/DeliveryOrderSurcharge.sql
@@ -0,0 +1,95 @@
+﻿CREATE TABLE [dbo].[DeliveryOrderSurcharge] (
+    [IdDeliveryOrderSurcharge]         INT             IDENTITY (1, 1) NOT NULL,
+    [GuideSerie]                       NVARCHAR (2)    NOT NULL,
+    [GuideNumber]                      INT             NOT NULL,
+    [GuideOriginalPriceShipment]       DECIMAL (14, 2) NOT NULL,
+    [GuideOriginalCollectOnDelivery]   DECIMAL (14, 2) NOT NULL,
+    [TotalSurcharge]                   DECIMAL (14, 2) NOT NULL,
+    [InternalSurchargeAmount]          DECIMAL (14, 2) NOT NULL,
+    [ExternalSurchargeAmount]          DECIMAL (14, 2) NOT NULL,
+    [GuideSurchargedShipment]          DECIMAL (14, 2) NOT NULL,
+    [GuideSurchargedCollectOnDelivery] DECIMAL (14, 2) NOT NULL,
+    [GuidePaymentLink]                 INT             NULL,
+    [RowStatus]                        BIT             CONSTRAINT [DF_DeliveryOrderSurcharge_RowStatus] DEFAULT ((1)) NOT NULL,
+    [DateCreated]                      DATETIME        NOT NULL,
+    [TokenCreated]                     NVARCHAR (50)   NOT NULL,
+    [DateUpdated]                      DATETIME        NULL,
+    [TokenUpdated]                     NVARCHAR (50)   NULL,
+    CONSTRAINT [PK_DeliveryOrderSurcharge] PRIMARY KEY CLUSTERED ([IdDeliveryOrderSurcharge] ASC),
+    CONSTRAINT [FK_DeliveryOrderSurcharge_DeliveryOrder] FOREIGN KEY ([GuideSerie], [GuideNumber]) REFERENCES [dbo].[DeliveryOrder] ([Guide_Serie], [Guide_Number]),
+    CONSTRAINT [FK_DeliveryOrderSurcharge_PaymentLinkHeader] FOREIGN KEY ([GuidePaymentLink]) REFERENCES [dbo].[PaymentLinkHeader] ([IdPaymentLinkHeader])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_DeliveryOrderSurcharge]
+    ON [dbo].[DeliveryOrderSurcharge]([GuideSerie] ASC, [GuideNumber] ASC);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Token que modificó la fila', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'TokenUpdated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha de modificación de la fila', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'DateUpdated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Token que creo la fila', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'TokenCreated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha de creación de la fila', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'DateCreated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Estado de la fila', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'RowStatus';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Foranea de la tabla PaymentLinkHeader', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'GuidePaymentLink';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'COD con el recargo aplicado', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'GuideSurchargedCollectOnDelivery';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Precio de la guía con el recargo aplicado', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'GuideSurchargedShipment';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Recargo para VisaOnLink 2.5%', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'ExternalSurchargeAmount';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Recargo que queda interno 2.5%', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'InternalSurchargeAmount';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Recargo total por pago por VisaOnLink (Servicio y COD) 5%', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'TotalSurcharge';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Precio original del COD sin recargos', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'GuideOriginalCollectOnDelivery';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Precio original de la guía sin recargos', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'GuideOriginalPriceShipment';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Número de la guía, foranea de DeliveryOrder', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'GuideNumber';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Serie de la guía, foranea de DeliveryOrder', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'GuideSerie';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Identificador de tabla DeliveryOrderSurcharge', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge', @level2type = N'COLUMN', @level2name = N'IdDeliveryOrderSurcharge';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Tabla para almacenar los cálculo de los recargos que se hagan por pago por VisaOnLink', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DeliveryOrderSurcharge';
+
