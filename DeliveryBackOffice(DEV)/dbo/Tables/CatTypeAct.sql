88ef2ec35c4833f60e344daeafc531e6c3e79a2b
EdelmanV <edelman.vasquez@forzalatam.com>
2022-08-31 08:41:21 -0600

    date:2022-08-30 author:edelman description:tablas para creaciòn de actas justificaciòn de piezas guìas rutas linehauls

---
+++ DeliveryBackOffice(DEV)/dbo/Tables/CatTypeAct.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[CatTypeAct] (
+    [IdCatTypeAct]   INT            IDENTITY (1, 1) NOT NULL,
+    [ActName]        NVARCHAR (50)  NOT NULL,
+    [ActDescription] NVARCHAR (200) NULL,
+    [RowStatus]      BIT            DEFAULT ((1)) NOT NULL,
+    [TokenCreated]   NVARCHAR (50)  NOT NULL,
+    [DateCreated]    DATETIME       NOT NULL,
+    [TokenUpdated]   NVARCHAR (50)  NULL,
+    [DateUpdated]    DATETIME       NULL,
+    PRIMARY KEY CLUSTERED ([IdCatTypeAct] ASC)
+);
+
