08f8b26f88f0237dfdcb20b30824d28ad296358c
AndresRuizPeer <andres.ruiz@forzalatam.com>
2022-09-06 10:21:08 -0600

    Date: 06/09/2022 Author: Jose Andres Ruiz Peer
    ### ADDED
    * Tutorial de inicio de nuevo portal web

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-928 1. INSERT INTO [DeliveryBackOffice].[dbo].[Tutorial].sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-928 1. INSERT INTO [DeliveryBackOffice].[dbo].[Tutorial].sql
@@ -7,3 +7,13 @@ BEGIN
 		('Creación de guías', 'Tutorial de creación de guías estándar y estándar COD en portal web', GETDATE(), 'SYS-ARUIZ')
 
 END
+
+IF (NOT EXISTS (SELECT TOP 1 1 FROM [DeliveryBackOffice].[dbo].[Tutorial] Tut WITH(NOLOCK) WHERE Tut.TutorialName = 'Guía de nuevo portal' COLLATE Latin1_General_CI_AI AND Tut.RowStatus = 1))
+BEGIN
+
+	INSERT INTO [DeliveryBackOffice].[dbo].[Tutorial]
+		(TutorialName, TutorialDescription, DateCreated,TokenCreated)
+	VALUES
+		('Guía de nuevo portal', 'Tutorial inicial de nuevo portal web', GETDATE(), 'SYS-ARUIZ')
+
+END
