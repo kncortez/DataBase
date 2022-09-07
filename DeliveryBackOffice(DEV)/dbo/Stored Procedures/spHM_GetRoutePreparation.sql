d41abdf80b6eef1d2ac1275e68c6408cfc93f9bd
Oscar Morales <oscar.morales@forzalatam.com>
2022-09-07 15:19:42 -0600

    Date: 2022-09-07 Author: Oscar Morales
    ### ADDED
    * Sp spHM_GetRoutePreparation que se utiliza en  el método api Logistics/HM_GetRoutePreparation.
    
    Historia APO-74

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetRoutePreparation.sql
@@ -0,0 +1,75 @@
+﻿-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <2022-09-06>
+-- Description:	<Obtiene información de la preparación de una ruta de entregas en móvil>
+-- =============================================
+CREATE PROCEDURE [dbo].[spHM_GetRoutePreparation]
+	-- Add the parameters for the stored procedure here
+	@RouteId INT,
+	@Date DATE
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	IF EXISTS (SELECT TOP 1 1 FROM RoutePreparation WHERE CatRouteId = @RouteId AND DateRoutePreparation = @DATE)
+	BEGIN
+		SELECT
+			1 'StatusCode'
+		   ,'Successfull' 'Description'
+
+		--TABLE 1 Información de la preparación de la ruta
+		SELECT
+			rp.IdRoutePreparation
+		   ,rp.GuidesQuantity
+		   ,rp.PiecesDry
+		   ,rp.PiecesCold
+		   ,rp.DeliveryOrderBySettlementId
+		FROM RoutePreparation rp
+		WHERE rp.CatRouteId = @RouteId
+		AND rp.DateRoutePreparation = @Date
+		AND rp.RowStatus = 1
+
+		--TABLE 2 Información de las guías en preparación de la ruta
+		SELECT
+			rpd.IdRoutePreparationDetail 'IdRoutePreparationDetail'
+		   ,rpd.Guide_Serie 'GuideSerie'
+		   ,rpd.Guide_Number 'GuideNumber'
+		   ,COUNT(1) 'Pieces'
+		   ,COALESCE(do.Pieces_Dry, 0) + COALESCE(do.Pieces_Cold, 0) 'PiecesTotal'
+		   ,do.Receiver_Department 'Department'
+		   ,do.Receiver_Town 'Town'
+		   ,do.Receiver_Address 'Address'
+		   ,rpd.GuideOrder 'GuideOrder'
+		FROM RoutePreparation rp
+		INNER JOIN RoutePreparationDetail rpd
+			ON rpd.RoutePreparationId = rp.IdRoutePreparation
+				AND rpd.RowStatus = 1
+		INNER JOIN RoutePreparationDetailPiece rpdp
+			ON rpdp.RoutePreparationDetailId = rpd.IdRoutePreparationDetail
+				AND rpdp.RowStatus = 1
+		INNER JOIN DeliveryOrder do
+			ON rpd.Guide_Serie = do.Guide_Serie
+				AND rpd.Guide_Number = do.Guide_Number
+		WHERE rp.CatRouteId = @RouteId
+		AND rp.DateRoutePreparation = @Date
+		AND rp.RowStatus = 1
+		GROUP BY IdRoutePreparationDetail
+				,rpd.Guide_Serie
+				,rpd.Guide_Number
+				,do.Pieces_Dry
+				,do.Pieces_Cold
+				,do.Receiver_Department
+				,do.Receiver_Town
+				,do.Receiver_Address
+				,rpd.GuideOrder
+		ORDER BY COALESCE(rpd.GuideOrder, 999999) ASC 
+	END
+	ELSE
+	BEGIN 
+		SELECT
+			2 'StatusCode'
+		   ,'Records not found' 'Description'
+	END
+END
