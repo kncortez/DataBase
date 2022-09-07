f4ad0d2a60d572ac6b7598de91be737eda459c22
Jerson Ochoa <jerson.ochoa@forzadelivery.com>
2022-09-07 14:46:29 -0600

    Date: 07-09-2022 Author: Jerson Ochoa ### ADDED * Validation for empty containers in sp SetInTransitToLinehaulRoutePreparation [APO-70]

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_setInTransitToLinehaulRoutePreparation.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_setInTransitToLinehaulRoutePreparation.sql
@@ -24,10 +24,11 @@ BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	DECLARE @EXISTING_LRP AS INT;	-- LinehaulRoutePreparation
-	DECLARE @EXISTING_LCM AS INT;	-- LinehaulRoutePreparationCustomsMark
-	DECLARE @EXISTING_SR AS INT;	-- Sender Receiver
-	DECLARE @EXISTING_TAG AS INT;	-- CustomMarks
+	DECLARE @EXISTING_LRP AS INT;				-- Linehaul Route Preparation
+	DECLARE @EXISTING_LCM AS INT;				-- Linehaul Route Preparation Customs Mark
+	DECLARE @EXISTING_SR AS INT;				-- Sender Receiver
+	DECLARE @EXISTING_TAG AS INT;				-- Custom Marks
+	DECLARE @EXISTING_EMPTY_CONTAINERS AS INT;	-- Linehaul Route Preparation Container Detail
 
 	-- Check if there is an active record in LinehaulRoutePreparation
 	SET @EXISTING_LRP = (SELECT	COUNT([LRP].[IdLinehaulRoutePreparation]) AS IdLinehaulRoutePreparation
@@ -62,6 +63,32 @@ BEGIN
 		-- No active record, it was already processed or it doesn't exist
 		BEGIN
 			SELECT 2 [spResult], 'Marchamo ya ha sido registrado en otro proceso de linehaul.' [spMessage];
+			RETURN;
+		END
+
+	SET @EXISTING_EMPTY_CONTAINERS = (SELECT	COUNT([LRPC].[IdLinehaulRoutePreparationContainer]) AS CONT
+									  FROM		[dbo].[LinehaulRoutePreparationContainer] LRPC
+									  WHERE		[LRPC].[GuideQuantity] = 0
+										AND		[LRPC].[LinehaulRoutePreparationId] = @IdLinehaulRoutePreparation
+										AND		[LRPC].[RowStatus] = 1);
+
+	IF (@EXISTING_EMPTY_CONTAINERS > 0)
+		BEGIN
+			SELECT		2 [spResult],
+						'Se encontraron contenedores vacíos en el despacho de linehaul, debe removerlos antes de finalizar el proceso.' [spMessage],
+						[LRPC].[IdLinehaulRoutePreparationContainer], 
+						[LRPC].[ContainerId],
+						[CTC].[TypeContainerSerie] AS ContainerSerie,
+						[C].[ContainerNumber]
+			FROM		[dbo].[LinehaulRoutePreparationContainer] LRPC
+			INNER JOIN	[dbo].[Container] C
+				ON		[LRPC].[ContainerId] = [C].[IdContainer]
+			INNER JOIN	[dbo].[CatTypeContainer] CTC
+				ON		[C].[CatTypeContainerId] = [CTC].[IdCatTypeContainer]
+			WHERE		[LRPC].[GuideQuantity] = 0
+					AND	[LRPC].[LinehaulRoutePreparationId] = 1
+					AND	[LRPC].[RowStatus] = 1;
+
 			RETURN;
 		END
 
@@ -142,7 +169,7 @@ BEGIN
 				ERROR_STATE() AS [ErrorState],
 				ERROR_PROCEDURE() AS [ErrorProcedure],
 				ERROR_LINE() AS [ErrorLine],
-				ERROR_MESSAGE() AS [ErrorMessage];
+				ERROR_MESSAGE() AS [spMessage];
 
 		ROLLBACK TRANSACTION
 	END CATCH
