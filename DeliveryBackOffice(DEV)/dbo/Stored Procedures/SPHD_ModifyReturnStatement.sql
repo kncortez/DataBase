e1f55a63d22ac8de70aaa2c9df0d14ec8ec58e60
EdelmanV <edelman.vasquez@forzalatam.com>
2022-09-12 16:18:14 -0600

    date:2022-09-12 author:edelman description:SP modificación de campo para declartación de devolución

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_ModifyReturnStatement.sql
@@ -0,0 +1,79 @@
+﻿-- =============================================
+-- Author:		<Edelman Vásquez>
+-- Create date: <2022-08-29>
+-- Description:	<SP para Modificar bandera de devolución (IsLastMileReturn)>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHD_ModifyReturnStatement] 
+	@TblListGuideActa TblListGuideActa READONLY,
+	@Token NVARCHAR(50)
+AS
+BEGIN	
+  
+	SET NOCOUNT ON;
+
+	BEGIN TRANSACTION
+	BEGIN TRY
+	
+	
+	DECLARE @Numero AS INT;
+	DECLARE @Serie  AS NVARCHAR(2);
+	DECLARE @STATUS AS INT; 
+	DECLARE @RevalueGuides AS TABLE(
+				GuideSerie NVARCHAR(2),
+				GuideNumber INT)
+	DECLARE @GuidesModify AS TABLE(
+				GuideSerie NVARCHAR(2),
+				GuideNumber INT)
+
+	INSERT INTO @RevalueGuides
+				SELECT
+					SUBSTRING(lg.NumberGuidePice,1,2)
+				    ,CAST(SUBSTRING(LTRIM(lg.NumberGuidePice), 3, CAST(LEN(NumberGuidePice) AS INT)-4) AS INT)
+				FROM @TblListGuideActa lg
+
+------------Modificar Bandera campo IsLastMileReturn ----------------------------
+WHILE EXISTS (SELECT TOP 1 1 FROM @RevalueGuides)
+BEGIN
+					SELECT TOP 1
+						@Serie  =  rg.GuideSerie,
+						@Numero =  rg.GuideNumber	
+				   FROM @RevalueGuides rg
+
+				   SELECT @STATUS = StatusOrderId
+				   FROM dbo.DeliveryOrderDetail
+
+                   IF (@STATUS NOT IN(22,5))
+				   BEGIN
+
+						UPDATE dbo.DeliveryOrder SET IsLastMileReturn = 1 WHERE Guide_Serie = @Serie AND Guide_Number = @Numero
+				   
+				        INSERT INTO @GuidesModify(GuideSerie,GuideNumber) VALUES (@Serie, @Numero)
+				   END
+
+	DELETE FROM @RevalueGuides
+	WHERE GuideSerie = @Serie AND GuideNumber = @Numero
+
+END
+			COMMIT TRANSACTION;
+			SELECT * FROM @GuidesModify 
+          
+	   END TRY
+			 BEGIN CATCH
+				ROLLBACK TRANSACTION
+				
+			SELECT
+			0 [blnResult]
+			,ERROR_MESSAGE() [Description]
+			,0 [NumTransferID]
+			,ERROR_NUMBER() [ErrorNumber]
+			,ERROR_SEVERITY() [ErrorSeverity]
+			,ERROR_STATE() [ErrorState]
+			,ERROR_PROCEDURE() [ErrorProcedure]
+			,ERROR_LINE() [ErrorLine]
+			,ERROR_MESSAGE() [ErrorMessage];
+	  END CATCH
+
+
+
+
+END
