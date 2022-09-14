2466b7f8d7a9816477b2d9480d974bee6adcf888
AndresRuizPeer <andres.ruiz@forzalatam.com>
2022-09-14 12:59:21 -0600

    Date: 14/09/2022 Author: Jose Andres Ruiz Peer
    ### CHANGED
    * Corrección de tilde

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetWidgetData.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetWidgetData.sql
@@ -99,7 +99,7 @@ BEGIN
 						ISNULL(TotalGuidePieces,0) 'TopValue',
 						'Piezas' 'TopText',
 						ISNULL(TotalGuide,0) 'BottomValue',
-						'Envios realizados' 'BottomText',
+						'Envíos realizados' 'BottomText',
 						'bi bi-box-seam fa-3x fa-lg float-right mr-4' 'WidgetIcon'
 					FROM
 						@ResponseTable
@@ -113,7 +113,7 @@ BEGIN
 						0 'TopValue',
 						'Piezas' 'TopText',
 						0 'BottomValue',
-						'Envios realizados' 'BottomText',
+						'Envíos realizados' 'BottomText',
 						'bi bi-box-seam fa-3x fa-lg float-right mr-4' 'WidgetIcon'
 				END
 
@@ -127,7 +127,7 @@ BEGIN
 					0 'TopValue',
 					'Piezas' 'TopText',
 					0 'BottomValue',
-					'Envios realizados' 'BottomText',
+					'Envíos realizados' 'BottomText',
 					'bi bi-box-seam fa-3x fa-lg float-right mr-4' 'WidgetIcon'
 			END
 		END
