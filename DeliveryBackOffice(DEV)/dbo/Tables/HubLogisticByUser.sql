a9138ade1fa5dd4f89f39eeb4afb0451aeedfc07
EdelmanV <edelman.vasquez@forzalatam.com>
2022-09-23 15:38:50 -0600

    date:2022-09-23 author:Edelman decription:tabla para asociar filtros de clientes internos

---
+++ DeliveryBackOffice(DEV)/dbo/Tables/HubLogisticByUser.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[HubLogisticByUser] (
+    [IdHubLogisticByUser] INT           IDENTITY (1, 1) NOT NULL,
+    [HubLogisticId]       INT           NOT NULL,
+    [UserId]              BIGINT        NOT NULL,
+    [RowStatus]           BIT           CONSTRAINT [DF_HubLogisticByUser_RowStatus] DEFAULT ((1)) NOT NULL,
+    [TokenCreated]        NVARCHAR (50) NOT NULL,
+    [DateCreated]         DATETIME      NOT NULL,
+    [TokenUpdated]        NVARCHAR (50) NULL,
+    [DateUpdated]         DATETIME      NULL,
+    CONSTRAINT [PK_HubLogisticByUser] PRIMARY KEY CLUSTERED ([IdHubLogisticByUser] ASC),
+    CONSTRAINT [FK_HubLogisticByUser_HubLogistic] FOREIGN KEY ([HubLogisticId]) REFERENCES [dbo].[HubLogistics] ([IdHubLogistic]),
+    CONSTRAINT [FK_HubLogisticByUser_RegisterUser] FOREIGN KEY ([UserId]) REFERENCES [dbo].[RegisterUser] ([UsrIdUser])
+);
+
