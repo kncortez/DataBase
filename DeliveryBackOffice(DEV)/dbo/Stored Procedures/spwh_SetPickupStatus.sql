dde0f2c1ce9fa15e8ba07355d4b5e50657a2986e
Alberto Ixchop <albertokanecixchop@gmail.com>
2022-09-23 11:59:34 -0600

    Date: 23-09-2022 Author: Alberto Ixchop
    ### MODIFIED
    * Corrección de método SetPickupStatus

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spwh_SetPickupStatus.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spwh_SetPickupStatus.sql
@@ -6,7 +6,8 @@
 CREATE PROCEDURE spwh_SetPickupStatus
 	@ServiceManagementId INT,
 	@Status BIT,
-	@Token NVARCHAR(50)
+	@Token NVARCHAR(50),
+	@Validate bit = 1--Activa o desactiva la validación de estados validos a ser activados/reactivados
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -35,7 +36,8 @@ BEGIN
 		FROM ServiceManagement sm
 		WHERE sm.IdServiceManagement = @ServiceManagementId
 
-	    IF @StatusOld is not null and @StatusOld IN (SELECT IdServiceStatus FROM DBO.CatServiceStatus WHERE Name IN ('Creado','Asignado a ruta','Cancelado'))
+	    IF @IdchedulePickup IS NOT NULL AND 
+			(@Validate =0 OR (@StatusOld is not null and @StatusOld IN (SELECT IdServiceStatus FROM DBO.CatServiceStatus WHERE Name IN ('Creado','Asignado a ruta','Cancelado'))))
 		BEGIN
 			IF @Status = 1
 			BEGIN
@@ -74,7 +76,7 @@ BEGIN
 		ELSE
 			SELECT			  
 				0 AS 'StatusCode',
-				'Estado inválido para ser cancelado/activado' AS 'Description', 
+				'Estado inválido para ser cancelado/activado o servicio inexistente' AS 'Description', 
 				@StatusOld 'Status';
 
 		IF @TranCounter = 0  
@@ -116,11 +118,11 @@ BEGIN
 		SELECT			  
 			1 'StatusCode',
 			'Registro actualizado correctamente' 'Description', 
-			@Status 'Status'
+			@Status 'Status';
 	END
 	ELSE
 		SELECT			  
 			0 AS 'StatusCode',
 			'Registro no encontrado' AS 'Description', 
-			@Status 'Status'
+			@Status 'Status';
 END
