2a3bed3f98b2b94bd1145370c59bc8893685328d
EdelmanV <edelman.vasquez@forzalatam.com>
2022-09-26 09:16:05 -0600

    date:2022-09-23 author:Edelman description: sp para catalogo de estaos de servicios

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/StateCatalogforServices.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		<Edelman Vásquez>
+-- Create date: <26/09/26>
+-- Description:	<>
+-- =============================================
+CREATE PROCEDURE [dbo].[StateCatalogforServices] 
+
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+   SELECT IdServiceStatus,
+          Name 
+   FROM dbo.CatServiceStatus
+END
