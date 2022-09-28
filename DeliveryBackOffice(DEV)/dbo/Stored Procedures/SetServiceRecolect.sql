ecab52bb8aad4ab179d70fde0f4a5e0b4faadb98
EdelmanV <edelman.vasquez@forzalatam.com>
2022-09-28 17:48:28 -0600

    date:2022-09-28 author:Edelman description: cambiar a borrado logico al campo active

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetServiceRecolect.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetServiceRecolect.sql
@@ -717,7 +717,7 @@ if(@ValidateFilter = 3 )
 
 				
 				--l momento de finalizar el proceso de devolución se debe realizar update en la tabla warehouse al campo Rack_Position, colocarlo como NULL
-				  UPDATE  dbo.warehouse SET Rack_Position=NULL 
+				  UPDATE  dbo.warehouse SET Active=0
 				  where Guide_Serie = @Guide_Serie AND 
                         Guide_Number = @Guide_Number
 
