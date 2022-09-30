e6150447d351730876ed146f79e357556f9eb1b3
Eduardo30 <vicente.lopez@forzalatam.com>
2022-09-30 12:29:29 -0600

    Date: 2022-09-30 Author: Eduardo López ### CHANGED * Mejorar rendimiento de SPS

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_creditNoteForza_Hd.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_creditNoteForza_Hd.sql
@@ -15,84 +15,103 @@ BEGIN
 	SET NOCOUNT ON;
 	declare @idNotaCredito as int = null;
 	declare @vpCodeOfReferences nvarchar(10);
-    -- Insert statements for procedure here
-	INSERT INTO [dbo].[invoiceHeader]
-           ([inv_vpCodeOfReferences]
-           ,[inv_cmp_nit]
-           ,[inv_cli_name]
-           ,[inv_cli_adress]
-           ,[inv_cli_nit]
-           ,[inv_cli_email]
-           ,[inv_date]
-           ,[inv_IVA]
-           ,[inv_amount]
-           ,[inv_status]
-           ,[inv_dateRegister]
-           ,[inv_tokenRegister]
-           ,[inv_type]
-		   ,inv_invoiceOfCreditNote
-		   ,inv_motiveCreditNote
-		   ,inv_dateOriginDocument
-		   ,inv_documentOriginFEL)
-		select [inv_vpCodeOfReferences]
-           ,[inv_cmp_nit]
-           ,[inv_cli_name]
-           ,[inv_cli_adress]
-           ,[inv_cli_nit]
-           ,[inv_cli_email]
-           ,GETDATE()
-           ,[inv_IVA]
-           ,[inv_amount]
-           ,[inv_status]
-           ,GETDATE()
-           ,@token
-           ,2
-		   ,@idInvoice
-		   ,@motivoNotaCredito
-		   ,inv_date
-		   ,inv_certificationFEL
-		from invoiceHeader
-		where inv_pk_id = @idInvoice
 
-		set @idNotaCredito = @@IDENTITY
+		BEGIN TRANSACTION
+	BEGIN TRY
+			-- Insert statements for procedure here
+			INSERT INTO [dbo].[invoiceHeader]
+				   ([inv_vpCodeOfReferences]
+				   ,[inv_cmp_nit]
+				   ,[inv_cli_name]
+				   ,[inv_cli_adress]
+				   ,[inv_cli_nit]
+				   ,[inv_cli_email]
+				   ,[inv_date]
+				   ,[inv_IVA]
+				   ,[inv_amount]
+				   ,[inv_status]
+				   ,[inv_dateRegister]
+				   ,[inv_tokenRegister]
+				   ,[inv_type]
+				   ,inv_invoiceOfCreditNote
+				   ,inv_motiveCreditNote
+				   ,inv_dateOriginDocument
+				   ,inv_documentOriginFEL)
+				select [inv_vpCodeOfReferences]
+				   ,[inv_cmp_nit]
+				   ,[inv_cli_name]
+				   ,[inv_cli_adress]
+				   ,[inv_cli_nit]
+				   ,[inv_cli_email]
+				   ,GETDATE()
+				   ,[inv_IVA]
+				   ,[inv_amount]
+				   ,[inv_status]
+				   ,GETDATE()
+				   ,@token
+				   ,2
+				   ,@idInvoice
+				   ,@motivoNotaCredito
+				   ,inv_date
+				   ,inv_certificationFEL
+				from invoiceHeader
+				where inv_pk_id = @idInvoice
 
-		INSERT INTO [dbo].[invoiceDetail]
-           ([dti_fk_header]
-           ,[dti_fk_orderSerie]
-           ,[dti_fk_orderNumber]
-           ,[dti_identification]
-           ,[dti_category]
-           ,[dti_quantity]
-           ,[dti_measurement]
-           ,[dti_priceUnit]
-           ,[dti_description]
-           ,[dti_IVA]
-           ,[dti_amount]
-           ,[dti_dateRegister]
-           ,[dti_tokenRegister])
-		SELECT @idNotaCredito
-           ,[dti_fk_orderSerie]
-           ,[dti_fk_orderNumber]
-           ,[dti_identification]
-           ,[dti_category]
-           ,[dti_quantity]
-           ,[dti_measurement]
-           ,[dti_priceUnit]
-           ,[dti_description]
-           ,[dti_IVA]
-           ,[dti_amount]
-           ,GETDATE()
-           ,@token
-		FROM [DeliveryBackOffice].[dbo].[invoiceDetail]
-		WHERE [dti_fk_header] = @idInvoice
+				set @idNotaCredito = @@IDENTITY
 
-		declare @detalles as int = @@rowcount
-		Set @vpCodeOfReferences =(Select inv_vpCodeOfReferences from invoiceHeader where inv_pk_id = @idNotaCredito)
+				INSERT INTO [dbo].[invoiceDetail]
+				   ([dti_fk_header]
+				   ,[dti_fk_orderSerie]
+				   ,[dti_fk_orderNumber]
+				   ,[dti_identification]
+				   ,[dti_category]
+				   ,[dti_quantity]
+				   ,[dti_measurement]
+				   ,[dti_priceUnit]
+				   ,[dti_description]
+				   ,[dti_IVA]
+				   ,[dti_amount]
+				   ,[dti_dateRegister]
+				   ,[dti_tokenRegister])
+				SELECT @idNotaCredito
+				   ,[dti_fk_orderSerie]
+				   ,[dti_fk_orderNumber]
+				   ,[dti_identification]
+				   ,[dti_category]
+				   ,[dti_quantity]
+				   ,[dti_measurement]
+				   ,[dti_priceUnit]
+				   ,[dti_description]
+				   ,[dti_IVA]
+				   ,[dti_amount]
+				   ,GETDATE()
+				   ,@token
+				FROM [DeliveryBackOffice].[dbo].[invoiceDetail]
+				WHERE [dti_fk_header] = @idInvoice
 
-		update invoiceHeader
-		set inv_creditNote = @idNotaCredito,
-		inv_status = -1
-		where inv_pk_id = @idInvoice
+				declare @detalles as int = @@rowcount
+				Set @vpCodeOfReferences =(Select inv_vpCodeOfReferences from invoiceHeader where inv_pk_id = @idNotaCredito)
 
-		select @idNotaCredito 'id',@@ROWCOUNT 'Detalles',@vpCodeOfReferences 'vpCodeOfReference'
+				update invoiceHeader
+				set inv_creditNote = @idNotaCredito,
+				inv_status = -1
+				where inv_pk_id = @idInvoice
+
+				select @idNotaCredito 'id',@@ROWCOUNT 'Detalles',@vpCodeOfReferences 'vpCodeOfReference'
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+	
+        SELECT 0 [blnResult],
+               ERROR_NUMBER() AS [ErrorNumber],
+               ERROR_SEVERITY() AS [ErrorSeverity],
+               ERROR_STATE() AS [ErrorState],
+               ERROR_PROCEDURE() AS [ErrorProcedure],
+               ERROR_LINE() AS [ErrorLine],
+               ERROR_MESSAGE() AS [ErrorMessage];
+
+		ROLLBACK TRANSACTION;
+
+	END CATCH
+
 END
