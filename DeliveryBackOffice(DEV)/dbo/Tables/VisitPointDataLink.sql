544a8526afd9e3e31f0da7277abcf8a955cc8e25
AndresRuizPeer <andres.ruiz@forzalatam.com>
2022-10-03 19:30:25 -0600

    Date: 03/10/2022 Author: Jose Andres Ruiz Peer
    ### FIXED
    * Corrección en versionamiento de tabla para links de recolección

--- DeliveryBackOffice(DEV)/dbo/Tables/VisitPointDataLink.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/VisitPointDataLink.sql
@@ -1,6 +1,5 @@
 ﻿CREATE TABLE [dbo].[VisitPointDataLink] (
     [IdVisitPointDataLink]   BIGINT        IDENTITY (1, 1) NOT NULL,
-    [CustomerId]             INT           NOT NULL,
     [VisitPointId]           INT           NOT NULL,
     [ServiceToken]           NVARCHAR (50) NOT NULL,
     [ServiceTokenExpiration] DATETIME      NULL,
@@ -10,9 +9,60 @@
     [DateCreated]            DATETIME      NOT NULL,
     [TokenUpdated]           NVARCHAR (50) NULL,
     [DateUPdated]            DATETIME      NULL,
+    [AccountId]              BIGINT        NULL,
     PRIMARY KEY CLUSTERED ([IdVisitPointDataLink] ASC),
-    CONSTRAINT [FK_VisitPointDataLink_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customer] ([IdCustomer]),
+    CONSTRAINT [FK_VisitPointDataLink_Account] FOREIGN KEY ([AccountId]) REFERENCES [dbo].[Account] ([AccIdAccount]),
     CONSTRAINT [FK_VisitPointDataLink_DataLinkStatus] FOREIGN KEY ([DataLinkStatusId]) REFERENCES [dbo].[CatDataLinkStatus] ([IdCatDataLinkStatus]),
     CONSTRAINT [FK_VisitPointDataLink_VisitPoint] FOREIGN KEY ([VisitPointId]) REFERENCES [dbo].[VisitPointClient] ([CodeOfReference])
 );
+
+
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Tabla de links para recolección.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Codigo de referencia del punto de visita de la tabla VisitPointClient.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink', @level2type = N'COLUMN', @level2name = N'VisitPointId';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Último token de actualización del registro.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink', @level2type = N'COLUMN', @level2name = N'TokenUpdated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Token de creación del registro.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink', @level2type = N'COLUMN', @level2name = N'TokenCreated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha de expiración del link de recolección, NULL indica que no tiene vencimiento.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink', @level2type = N'COLUMN', @level2name = N'ServiceTokenExpiration';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Token que identifica el link de recolección.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink', @level2type = N'COLUMN', @level2name = N'ServiceToken';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Estado lógico del registro.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink', @level2type = N'COLUMN', @level2name = N'RowStatus';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Identificador del registro.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink', @level2type = N'COLUMN', @level2name = N'IdVisitPointDataLink';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Última fecha de actualización del registro.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink', @level2type = N'COLUMN', @level2name = N'DateUPdated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha de creación del registro.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink', @level2type = N'COLUMN', @level2name = N'DateCreated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Estado del link de recolección.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink', @level2type = N'COLUMN', @level2name = N'DataLinkStatusId';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Identificador de la cuenta para poder generar servicio de recolección de la tabla AccountId.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'VisitPointDataLink', @level2type = N'COLUMN', @level2name = N'AccountId';
 
