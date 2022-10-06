30f8462852004304043f74d041c16c57f8d2ba43
AndresRuizPeer <andres.ruiz@forzalatam.com>
2022-10-06 08:16:17 -0600

    Date: 06/10/2022 Author: Jose Andres Ruiz Peer
    ### ADDED
    * Manejo de datos para enviar link tomando datos de punto de visita

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetClientFromVisitPointClientByID.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		<Andres,Ruiz>
+-- Create date: <2022-10-05>
+-- Description:	<Obtener datos de cliente basado en el codigo de referencia de un punto de visita>
+-- =============================================
+
+CREATE PROCEDURE [dbo].[GetClientFromVisitPointClientByID]
+	@CodeOfReference AS int
+AS
+BEGIN
+
+	SELECT 
+		IIF(LTRIM(RTRIM(ISNULL(VPC.Phone,''))) != '', LTRIM(RTRIM(VPC.Phone)), Cu.CustomerPhone) 'destinationPhone',
+		IIF(LTRIM(RTRIM(ISNULL(VPC.DescriptionOfClient,''))) != '', LTRIM(RTRIM(VPC.DescriptionOfClient)), ISNULL(Cu.CommercialName, Cu.[Name])) 'destinationClientName'
+	FROM 
+		[DeliveryBackOffice].[dbo].[VisitPointClient] VPC WITH(NOLOCK)
+		LEFT JOIN
+			[DeliveryBackOffice].[dbo].[Customer] Cu WITH(NOLOCK)
+			ON
+				VPC.CustomerID = Cu.IdCustomer
+	WHERE 
+		VPC.CodeOfReference = @CodeOfReference
+
+END
