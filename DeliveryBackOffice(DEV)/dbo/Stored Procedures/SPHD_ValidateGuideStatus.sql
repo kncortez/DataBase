8ca032f8040e1a4e526f284264c94bc41efaadfd
EdelmanV <edelman.vasquez@forzalatam.com>
2022-10-12 15:24:31 -0600

    date:2022-10-12 author:Edelman description:sp valida estados de una guía

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_ValidateGuideStatus.sql
@@ -0,0 +1,29 @@
+﻿-- =============================================
+-- Author:		<Edelman V>
+-- Create date: <2022-10-10>
+-- Description:	<SP>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHD_ValidateGuideStatus] 
+@Guide AS NVARCHAR(20)
+AS
+BEGIN
+		DECLARE @STATUS AS INT; 
+	SET NOCOUNT ON;
+
+  SELECT TOP 1 @STATUS = StatusOrderId
+				   FROM dbo.DeliveryOrderDetail WITH (NOLOCK)
+				   WHERE Guide_Serie+CAST(Guide_Number AS nvarchar) = @Guide
+				   ORDER BY DateCreated DESC
+
+ IF (@STATUS  IN(22,5,7))
+	BEGIN
+
+		SELECT Result = 1
+	END
+	ELSE
+	BEGIN
+
+		SELECT Result = 0
+	END
+
+END
