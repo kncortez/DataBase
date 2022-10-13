98d52c8c82e0dd7ee070d3b9f4a71a1dafacb05b
EdelmanV <edelman.vasquez@forzalatam.com>
2022-10-13 14:13:07 -0600

    Revert "date:2022-10-13"
    This reverts commit b1e4cff1a7b41de2b4c98972f769363d4793a01d.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_AssignmentCollectionServicesCourier.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_AssignmentCollectionServicesCourier.sql
@@ -1,7 +1,7 @@
 ﻿-- =============================================
 -- Author:		<Edelman>
 -- Create date: <2022-09-22>
--- Description:	<Sp para asignar servicio de recolección a Courierman>
+-- Description:	<Sp para asignar servicio de recolección a courierman>
 -- =============================================
 CREATE PROCEDURE [dbo].[spHW_AssignmentCollectionServicesCourier] 
 @IdCurrierMan AS INT,
