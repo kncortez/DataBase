f7c6219a6eee0b7697d53aac292530973e0ad4b2
EdelmanV <edelman.vasquez@forzalatam.com>
2022-10-12 21:39:23 -0600

    date:2022-10-12 author:Edelman description: SP para registrar recolecciones programadas

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_RecordScheduledCollections.sql
@@ -0,0 +1,77 @@
+﻿-- =============================================
+-- Author:		<Edelman Vásquez>
+-- Create date: <2022-10-10>
+-- Description:	<SP Nuevo método para registrar recolecciones programadas>
+-- =============================================
+CREATE PROCEDURE [dbo].[spHW_RecordScheduledCollections] 
+ @StartDate AS DATETIME,
+ @EndDate AS DATETIME,
+ @SenderID AS INT,
+ @SenderName AS NVARCHAR(200),
+ @SenderAddress AS NVARCHAR(600),
+ @SpecialInstructions AS NVARCHAR(600),
+ @EstimatedPackages AS INT,
+ @TypeVehicleId AS INT,
+ @HubLogisticsId AS INT,
+ @TokenAuthorized AS NVARCHAR(50),
+ @DateAuthorized AS DATETIME,
+ @RowStatus AS BIT,
+ @DateCreated AS DATETIME,
+ @TokenCreated AS  NVARCHAR(50)
+AS
+BEGIN
+	
+
+	SET NOCOUNT ON;
+
+BEGIN TRANSACTION
+BEGIN TRY
+   
+   INSERT INTO [DeliveryBackOffice].[dbo].[SchedulePickupToProcess]
+   (
+  	StartDate,
+	EndDate,
+	SenderID,
+	SenderName,
+	SenderAddress,
+	SpecialInstructions,
+	EstimatedPackages,
+	TypeVehicleId,
+	HubLogisticsId,
+	TokenAuthorized,
+	DateAuthorized,
+	RowStatus,
+	DateCreated,
+	TokenCreated
+
+
+   )
+   VALUES
+   (
+	 @StartDate,
+	 @EndDate,
+	 @SenderID,
+	 @SenderName,
+	 @SenderAddress,
+	 @SpecialInstructions,
+	 @EstimatedPackages,
+	 @TypeVehicleId,
+	 @HubLogisticsId,
+	 @TokenAuthorized,
+	 @DateAuthorized,
+	 @RowStatus,
+	 @DateCreated,
+	 @TokenCreated 
+   )
+
+COMMIT TRANSACTION
+
+SELECT Result = 1;
+END TRY
+	BEGIN CATCH
+
+		ROLLBACK TRANSACTION
+		SELECT Result = 0;
+
+	END CATCH
+END
