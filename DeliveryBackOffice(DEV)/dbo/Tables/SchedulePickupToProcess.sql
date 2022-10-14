226a1f5f53958196f749b8dd5255621e8da644f6
EdelmanV <edelman.vasquez@forzalatam.com>
2022-10-14 08:20:19 -0600

    date:2022-10-14 author:Edelman description: descripción de campos de la tabla

--- DeliveryBackOffice(DEV)/dbo/Tables/SchedulePickupToProcess.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/SchedulePickupToProcess.sql
@@ -19,3 +19,69 @@
     CONSTRAINT [PK_SchedulePickupToProcess] PRIMARY KEY CLUSTERED ([IdSchedulePickupToProcess] ASC)
 );
 
+
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'identificador de tipo de vehículo', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'TypeVehicleId';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'token de actualización', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'TokenUpdated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'token de creación', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'TokenCreated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Token de autorización', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'TokenAuthorized';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha de Inicio de recolección', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'StartDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'descripción especial', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'SpecialInstructions';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Nombre de persona que envía', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'SenderName';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'identiifcador del ciente que envía', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'SenderID';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'dirección de cliente que envía', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'SenderAddress';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'estado de registro', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'RowStatus';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'identificador de hub', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'HubLogisticsId';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'paquete estimado', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'EstimatedPackages';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'fecha de finalización', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'EndDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha de actualización', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'DateUpdated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha de creación', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'DateCreated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha de autorización', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SchedulePickupToProcess', @level2type = N'COLUMN', @level2name = N'DateAuthorized';
+
