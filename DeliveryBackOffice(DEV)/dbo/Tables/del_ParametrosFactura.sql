7223e4a9b898dc65f02ecae1fa6c34b7217ad102
Eduardo30 <vicente.lopez@forzalatam.com>
2022-10-21 16:41:37 -0600

    Date: 2022-10-21 Author: Eduardo López ### ADDED * agregar campos en tablas y modificaciones en SP spg_InformationInvoice para mandar codigo de almacen de EXP a SAP

--- DeliveryBackOffice(DEV)/dbo/Tables/del_ParametrosFactura.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/del_ParametrosFactura.sql
@@ -30,6 +30,13 @@
     [dpf_OcrCode]                    NVARCHAR (50)  NULL,
     [dpf_OcrCode2]                   NVARCHAR (50)  NULL,
     [dpf_StatusFACE]                 NVARCHAR (1)   NULL,
+    [dpf_AlmacenCode]                INT            NULL,
     CONSTRAINT [PK_del_ParametrosFactura] PRIMARY KEY CLUSTERED ([dpf_VpCodeOfReference] ASC)
 );
+
+
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Campo para almacenar codigo de almacen de Express Center.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'del_ParametrosFactura', @level2type = N'COLUMN', @level2name = N'dpf_AlmacenCode';
 
