db12527012a39f5eccf7a7d2a6e2bbd9fbdefb87
Alfredo Monroy <alfredo.monroy@forzalatam.com>
2022-11-02 12:40:30 -0600

    Adición de coma para corrección de sintaxis.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphw_GetPickupVehiclesByUserHub.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphw_GetPickupVehiclesByUserHub.sql
@@ -6,7 +6,7 @@
 CREATE PROCEDURE [dbo].[sphw_GetPickupVehiclesByUserHub]
 	-- Add the parameters for the stored procedure here
 	@IdUser BIGINT,
-	@UbicaCourierLocations TblCourierLocation READONLY
+	@UbicaCourierLocations TblCourierLocation READONLY,
 	@CourierLocations TblCourierLocation READONLY
 AS
 BEGIN
