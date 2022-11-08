bea70b7d5b17ae569243968200b1e16cb0f945cc
Oscar Morales <oscar.morales@forzalatam.com>
2022-11-08 12:41:00 -0600

    Date: 2022-11-08 Author: Oscar Morales
    ### ADDED
    * Se agrega campo NextSteps a tabla StatusOrder, para guardar los siguientes pasos de cada checkpoint.
    
    Historia: HDB-32

--- DeliveryBackOffice(DEV)/dbo/Tables/StatusOrder.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/StatusOrder.sql
@@ -3,10 +3,13 @@
     [OrderDescription]    NVARCHAR (100) NOT NULL,
     [CatCheckpointTypeId] TINYINT        DEFAULT ((2)) NOT NULL,
     [CatStatusTypeId]     INT            NULL,
+    [NextSteps]           NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_Table_1] PRIMARY KEY CLUSTERED ([StatusOrderId] ASC),
     CONSTRAINT [FK_StatusOrder_CatCheckpointType] FOREIGN KEY ([CatCheckpointTypeId]) REFERENCES [dbo].[CatCheckpointType] ([IdCatCheckpointType]),
     CONSTRAINT [FK_StatusOrder_StatusType] FOREIGN KEY ([CatStatusTypeId]) REFERENCES [dbo].[CatStatusType] ([IdCatStatusType])
 );
+
+
 
 
 
