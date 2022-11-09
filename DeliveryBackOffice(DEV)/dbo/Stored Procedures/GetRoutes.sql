2fa1468b7ed4a8450568df6a9d43c1ac0ebede30
AndresRuizPeer <andres.ruiz@forzalatam.com>
2022-11-09 09:34:20 -0600

    Date: 09/11/2022 Author: Jose Andres Ruiz Peer
    ### CHANGED
    * Manejo correcto para obtener rutas basado en caracter "identificador" del flujo operativo (L = rutas linehaul, U = rutas última milla, etc.)

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetRoutes.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetRoutes.sql
@@ -1,11 +1,36 @@
 ﻿
 CREATE PROCEDURE [dbo].[GetRoutes]
-@RouteChar AS NVARCHAR 
+	@RouteChar AS NVARCHAR 
 AS
 BEGIN
 
-	SELECT IdRoute IdRoute, CodeRoute  FROM CatRoute
-	WHERE LEFT(UPPER(CodeRoute),1) = @RouteChar 
+	DECLARE @RouteType INT;
+
+	BEGIN TRY
+
+		IF(@RouteChar = 'R')
+			SET @RouteType = (SELECT TOP 1 CTR.IdTypeRoute FROM [DeliveryBackOffice].[dbo].[CatTypeRoute] CTR WITH(NOLOCK) WHERE CTR.[Name] = 'Recolección' COLLATE Latin1_General_CI_AI);
+		ELSE IF(@RouteChar = 'L')
+			SET @RouteType = (SELECT TOP 1 CTR.IdTypeRoute FROM [DeliveryBackOffice].[dbo].[CatTypeRoute] CTR WITH(NOLOCK) WHERE CTR.[Name] = 'Linehaul' COLLATE Latin1_General_CI_AI);
+		ELSE IF(@RouteChar = 'D')
+			SET @RouteType = (SELECT TOP 1 CTR.IdTypeRoute FROM [DeliveryBackOffice].[dbo].[CatTypeRoute] CTR WITH(NOLOCK) WHERE CTR.[Name] = 'Devolución' COLLATE Latin1_General_CI_AI);
+		ELSE IF(@RouteChar = 'U')
+			SET @RouteType = (SELECT TOP 1 CTR.IdTypeRoute FROM [DeliveryBackOffice].[dbo].[CatTypeRoute] CTR WITH(NOLOCK) WHERE CTR.[Name] = 'Ultima Milla' COLLATE Latin1_General_CI_AI);
+		ELSE
+			SET @RouteType = -1;
+
+		IF (ISNULL(@RouteType,-1) > 0)
+			SELECT IdRoute IdRoute, CodeRoute  FROM [DeliveryBackOffice].[dbo].CatRoute CR WITH(NOLOCK)
+			WHERE CR.IdTypeRoute = @RouteType
+		ELSE
+			SELECT IdRoute IdRoute, CodeRoute  FROM [DeliveryBackOffice].[dbo].CatRoute
+			WHERE LEFT(UPPER(CodeRoute),1) = @RouteChar 
+
+	END TRY
+	BEGIN CATCH
+		SELECT IdRoute IdRoute, CodeRoute  FROM [DeliveryBackOffice].[dbo].CatRoute
+		WHERE LEFT(UPPER(CodeRoute),1) = @RouteChar 
+	END CATCH
 
 END
 
