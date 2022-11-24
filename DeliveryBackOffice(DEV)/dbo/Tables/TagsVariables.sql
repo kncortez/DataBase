fc6f6d08b3388f64f992c95044ed5021d10508f5
EdelmanV <edelman.vasquez@forzalatam.com>
2022-11-24 11:47:30 -0600

    Revert "date:2022-11-24"
    This reverts commit e080427a1ee8c8105d1660d2307c2742f4902099.

--- DeliveryBackOffice(DEV)/dbo/Tables/TagsVariables.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[TagsVariables] (
-    [IdTagsVariables] INT            IDENTITY (1, 1) NOT NULL,
-    [Tag]             NVARCHAR (50)  NOT NULL,
-    [Description]     NVARCHAR (250) NOT NULL,
-    CONSTRAINT [PK_TagsVaribles] PRIMARY KEY CLUSTERED ([IdTagsVariables] ASC)
-);
-
