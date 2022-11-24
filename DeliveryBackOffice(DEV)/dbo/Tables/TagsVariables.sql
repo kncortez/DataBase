40ab164f0db1fe9c7b1556d745e2c58c0b2d2c12
EdelmanV <edelman.vasquez@forzalatam.com>
2022-11-24 12:16:22 -0600

    date:2022-11-24 author:edelmam description:Tabla de tag

---
+++ DeliveryBackOffice(DEV)/dbo/Tables/TagsVariables.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[TagsVariables] (
+    [IdTagsVariables] INT            IDENTITY (1, 1) NOT NULL,
+    [Tag]             NVARCHAR (50)  NOT NULL,
+    [Description]     NVARCHAR (250) NOT NULL,
+    CONSTRAINT [PK_TagsVaribles] PRIMARY KEY CLUSTERED ([IdTagsVariables] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'DEscripción de etiqueta de variable', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TagsVariables', @level2type = N'COLUMN', @level2name = N'Description';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Etiqueta de variable', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TagsVariables', @level2type = N'COLUMN', @level2name = N'Tag';
+
