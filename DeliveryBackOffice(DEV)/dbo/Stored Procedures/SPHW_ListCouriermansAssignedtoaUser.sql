1322ed5499d9e4e5bdbd55116d22dadbaa4740e6
EdelmanV <edelman.vasquez@forzalatam.com>
2022-11-30 17:20:34 -0600

    DATE:2022-11-30 AUTHOR:EDELMAN DESCRIPTION: SP LISTADO DE COURIER ASIGNADO A UN USUARIO INTERNO

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_ListCouriermansAssignedtoaUser.sql
@@ -0,0 +1,39 @@
+﻿-- =============================================
+-- Author:		<Edelman>
+-- Create date: <2022-11-30>
+-- Description:	<SP listar courier asignados a cliente interno>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHW_ListCouriermansAssignedtoaUser] 
+@IdUser AS INT
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+BEGIN TRY
+
+	SELECT SR.ID, 
+		   SR.First_Name+' '+SR.Last_Name [NAME], 
+		   SR.CUI, 
+		   HL.HubName, 
+		   SR.Phone, 
+		   CTSR.TypeName 
+	FROM [dbo].[SenderReceiverByUser] SRU WITH (NOLOCK)
+		INNER JOIN [dbo].[SenderReceiver] SR WITH(NOLOCK)
+	ON SRU.SenderReceiverId = SR.ID
+	    LEFT JOIN [dbo].[HubLogistics] HL WITH(NOLOCK)
+    ON SR.HubLogisticId=HL.IdHubLogistic
+	    LEFT JOIN [dbo].[CatTypeSenderReceiver] CTSR WITH(NOLOCK)
+	ON SR.CatTypeSenderReceiverId  = CTSR.IdCatTypeSenderReceiver
+	WHERE SRU.UserId =@IdUser
+
+	 
+
+END TRY
+        BEGIN CATCH
+            	  SELECT  [blnResult]=0
+                   
+			
+        END CATCH
+
+END
