616417de42681f3c6fe5c5620d155b4e083f2106
EdelmanV <edelman.vasquez@forzalatam.com>
2022-11-30 12:54:57 -0600

    date:2022-11-30 author:Edelman description: sp para asignar courier a usuario interno

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_AssignCouriertoInternalUser.sql
@@ -0,0 +1,59 @@
+﻿-- =============================================
+-- Author:		<Edelman,Vásquez>
+-- Create date: <2022-11-30>
+-- Description:	<SP para asignar courier a usuario interno>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHW_AssignCouriertoInternalUser]
+@IdUser AS INT,
+@SenderReceiverId  AS INT,
+@Token AS nvarchar(50)
+
+
+AS
+BEGIN
+
+BEGIN TRANSACTION
+BEGIN TRY
+
+	IF(EXISTS(SELECT TOP  1 1 from dbo.RegisterUser where UsrIdUser=@IdUser))
+	BEGIN
+
+		INSERT INTO [dbo].[SenderReceiverbyUser]
+		(
+			SenderReceiverId,	
+			UserId,	
+			RowStatus,
+			TokenCreated,
+			DateCreated
+			
+		   ) 
+		VALUES
+		(
+		@SenderReceiverId,
+		@IdUser,
+		1,
+		@Token,
+		GETDATE()
+		
+		)
+
+		 
+		SELECT [blnResult] = 1
+	END
+	ELSE
+		BEGIN
+	
+			 SELECT [blnResult] = 2
+	
+		END
+		COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+          
+	ROLLBACK TRANSACTION
+			SELECT [blnResult] = 0
+                  
+			
+END CATCH
+
+END
