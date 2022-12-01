ad0aeaddcf275e1d97ff13c586f93134ab720765
EdelmanV <edelman.vasquez@forzalatam.com>
2022-12-01 12:16:30 -0600

    Date:2022-11-30 author:Edelman description: Tabla para reemplazar logica de registros de usuarios internos con courier por la tabla HubLogistic

---
+++ DeliveryBackOffice(DEV)/dbo/Tables/SenderReceiverByUser.sql
@@ -0,0 +1,44 @@
+﻿CREATE TABLE [dbo].[SenderReceiverByUser] (
+    [IdSenderReceiverbyUser] INT           IDENTITY (1, 1) NOT NULL,
+    [SenderReceiverId]       INT           NOT NULL,
+    [UserId]                 INT           NOT NULL,
+    [RowStatus]              BIT           CONSTRAINT [DF_SenderReceiverByUser_RowStatus] DEFAULT ((0)) NOT NULL,
+    [TokenCreated]           NVARCHAR (50) NOT NULL,
+    [DateCreated]            DATETIME      NOT NULL,
+    [TokenUpdated]           NVARCHAR (50) NULL,
+    [DateUpdated]            DATETIME      NULL,
+    CONSTRAINT [PK_SenderReceiverByUser] PRIMARY KEY CLUSTERED ([IdSenderReceiverbyUser] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'fecha de actualización de registros', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SenderReceiverByUser', @level2type = N'COLUMN', @level2name = N'DateUpdated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'token de persona que actualiza el registro', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SenderReceiverByUser', @level2type = N'COLUMN', @level2name = N'TokenUpdated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'fecha de creación de registro', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SenderReceiverByUser', @level2type = N'COLUMN', @level2name = N'DateCreated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Token de creador de registro', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SenderReceiverByUser', @level2type = N'COLUMN', @level2name = N'TokenCreated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Indica si el registro esta activo o inactivo', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SenderReceiverByUser', @level2type = N'COLUMN', @level2name = N'RowStatus';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Identificador de usuario interno', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SenderReceiverByUser', @level2type = N'COLUMN', @level2name = N'UserId';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Identificador de hub', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SenderReceiverByUser', @level2type = N'COLUMN', @level2name = N'SenderReceiverId';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Identificador de la tabla', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SenderReceiverByUser', @level2type = N'COLUMN', @level2name = N'IdSenderReceiverbyUser';
+
