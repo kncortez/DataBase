3abb2bc94c75c5da6eda6c43dda171bafea06136
AndresRuizPeer <andres.ruiz@forzalatam.com>
2022-12-02 07:53:20 -0600

    Date: 02/12/2022 Author: Jose Andres Ruiz Peer
    ### CHANGED
    * Manejo de expresión regular para escaner de guías de CourierApp

--- DeliveryBackOffice(DEV)/dbo/Scripts/CAPP-460 1. INSERT INTO [DeliveryBackOffice].[dbo].[ConfigParams].sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/CAPP-460 1. INSERT INTO [DeliveryBackOffice].[dbo].[ConfigParams].sql
@@ -5,6 +5,16 @@ BEGIN
 	INSERT INTO [DeliveryBackOffice].[dbo].[ConfigParams]
 		(Name, Description, Value, Status, CreateDate)
 	VALUES
-		('GuideRegex', 'Expresión regular de formato de guías actual', '^([0-9]{7,10})(-([0-9]{1,3}))?$', 1, GETDATE())
+		('GuideRegex', 'Expresión regular de formato de guías actual', '^([0-9]{7,7})(-([0-9]{1,}))?$', 1, GETDATE())
+
+END
+
+IF (NOT EXISTS (SELECT TOP 1 1 FROM [DeliveryBackOffice].[dbo].[ConfigParams] CP WITH(NOLOCK) WHERE CP.[Name] = 'GuideRegexScanner' COLLATE Latin1_General_CI_AI))
+BEGIN
+
+	INSERT INTO [DeliveryBackOffice].[dbo].[ConfigParams]
+		(Name, Description, Value, Status, CreateDate)
+	VALUES
+		('GuideRegexScanner', 'Expresión regular de formato de guías actual', '^(FD)([0-9]{7,7})(-([0-9]{1,}))?$', 1, GETDATE())
 
 END
