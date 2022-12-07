bfbf4a2904d3abda603f0e219d8c27a6b74e4dec
EdelmanV <edelman.vasquez@forzalatam.com>
2022-12-07 14:03:27 -0600

    date:2022-12-07 author:Edelman description:Asociar Usuario Interno con Courierman a travéz de tabla dbo.SenderReceiverbyUser

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphw_GetCouriersByUserHubs.sql
@@ -0,0 +1,34 @@
+﻿
+-- =============================================
+-- Author:		<Andres, Ruiz>
+-- Create date: <2022-10-21>
+-- Description:	<Devuelve todos los couriers basado en los hubs del usuario interno>
+-- =============================================
+-- =============================================
+-- Author:		<Edelman, Vásquez>
+-- Create date: <2022-12-07>
+-- Description:	<Asociar Usuario Interno con Courierman a travéz de tabla dbo.SenderReceiverbyUser>
+-- =============================================
+CREATE PROCEDURE [dbo].[sphw_GetCouriersByUserHubs]
+	@userId BIGINT = NULL
+
+AS
+BEGIN
+    SET ARITHABORT ON;
+
+	SELECT 200 'StatusCode', 
+			'Registros obtenidos'	'Description';
+	--INSERT INTO @tbl
+	SELECT 
+		SR.ID 'IdCourier',
+		LTRIM(RTRIM(CONCAT(SR.First_Name,' ',SR.Last_Name))) 'CourierName',
+		SR.CUI 'CourierCUI',
+		SR.Phone 'CourierPhone'
+	FROM dbo.SenderReceiver sr WITH(NOLOCK)
+		INNER JOIN dbo.HubLogisticByUser hlbu WITH(NOLOCK)
+			ON sr.HubLogisticId = hlbu.HubLogisticId
+				AND hlbu.UserId = @userId
+		INNER JOIN [DeliveryBackOffice].[dbo].[SenderReceiverByUser] sru WITH(NOLOCK)
+		    ON sr.ID=sru.SenderReceiverId
+
+END;
