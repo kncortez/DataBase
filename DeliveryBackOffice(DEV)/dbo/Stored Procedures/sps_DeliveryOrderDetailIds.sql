72e3c96bb19600761d703f8f54c4e16ddc7a3ebc
EdelmanV <edelman.vasquez@forzalatam.com>
2022-12-27 08:58:50 -0600

    Date:2022-12-27 author:Edelman Description: Modificar Sp para agregar campo Bandera que indica uso de membresia y subscripción

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_DeliveryOrderDetailIds.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_DeliveryOrderDetailIds.sql
@@ -16,7 +16,9 @@
 ,@ReceiverId						int = 0
 ,@OriginSenderId					int = 0
 ,@IsReturn							bit = 0
+,@IsCreditCardPayment				bit = 0
 ,@OrderUserCreated                  varchar(100) = ''
+,@UseMembership bit=0
 AS 
 BEGIN
 
@@ -75,12 +77,15 @@ BEGIN
 		AND
 		PC.GuideNumberDestination = @GuideNumber
 		AND
+		PC.FinalActiveDate >= GETDATE()
+		AND
 		PC.RowStatus = 1), 0)
 
 
 	DECLARE @RC INT;
-	IF @Price =0 AND @CouponApplied = 0
-	BEGIN
+	--SE COMENTA PARA CÁLCULAR MEMBRESÍAS
+	--IF @Price =0 AND @CouponApplied = 0 
+	--BEGIN
 		EXECUTE @RC = DeliveryBackOffice.dbo.spws_revalue_guide
 					@GuideSerie = @GuideSerie,
 					@GuideNumber = @GuideNumber,
@@ -90,8 +95,9 @@ BEGIN
 					@IdModule = 33,
 					@SetUpdate = 'true',
 					@Token = 'sps_DeliveryOrderDetailIds',
-					@IsReturn = 'false'
-	END
+					@IsReturn = 'false',
+					@ParIsCreditCard = @IsCreditCardPayment
+	--END
 
 	select 1;
 END
