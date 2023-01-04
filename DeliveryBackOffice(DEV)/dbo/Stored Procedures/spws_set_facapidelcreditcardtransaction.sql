e0b0ebe2d044cc1a427d8f6c9e186fe7b46445e2
AndresRuizPeer <andres.ruiz@forzalatam.com>
2023-01-04 15:30:13 -0600

    Date: 04/01/2023 Author: Jose Andres Ruiz Peer
    ### FIXED
    * Especificación de joins

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_set_facapidelcreditcardtransaction.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_set_facapidelcreditcardtransaction.sql
@@ -93,13 +93,13 @@ BEGIN
 	-- Punto de visita por cuenta ingresada
 	SET @VisitPointClientIdByUser = (
 		SELECT CodeOfReference FROM DeliveryBackOffice.dbo.VisitPointClient VPC
-		JOIN VisitPointByUser VPU
+		INNER JOIN VisitPointByUser VPU
 			ON VPC.IdVisitPointClient = VPU.IdVisitPointClient
 				AND VPU.RowStatus = 1
-		JOIN RegisterUser ru
+		INNER JOIN RegisterUser ru
 			ON VPU.RegisterUserID = ru.UsrIdUser
 				AND ru.UsrRowStatus = 1
-		JOIN [dbo].[RolByUserByAccount] rua
+		INNER JOIN [dbo].[RolByUserByAccount] rua
 			ON rua.RuaIdUser = ru.UsrIdUser
 		WHERE rua.RuaIdAccount = @AccountId
 	)
