fac9acf7db40d412321bdd9596056356e73b02b8
EdelmanV <edelman.vasquez@forzalatam.com>
2023-01-06 17:16:02 -0600

    Date:2023-01-06 author:Edelman description: cargar codeofreference correspondiente a cada express center

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetCodeOfReferenceForStation.sql
@@ -0,0 +1,26 @@
+﻿-- =============================================
+-- Author:		<Author,Edelman,Vásquez>
+-- Create date: <Create Date 2023-01-06>
+-- Description:	<Description, SP para obtener código de referencia de credenciales para certificación FEL segun Exc por Id de estación>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetCodeOfReferenceForStation]
+@IdStation AS INT	
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+BEGIN TRY
+    
+	SELECT 
+	      CodeOfReference 
+	FROM [dbo].[CatStation] WITH(NOLOCK)
+	WHERE IdStation = @IdStation
+
+END TRY
+BEGIN CATCH
+	 ROLLBACK
+
+	 SELECT CodeOfReference  = 999
+END CATCH
+END
