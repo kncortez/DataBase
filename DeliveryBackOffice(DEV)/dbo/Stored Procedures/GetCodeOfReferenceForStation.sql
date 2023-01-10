de4314dc34bfbc7acaacdb19ad02c6d51f554b1d
EdelmanV <edelman.vasquez@forzalatam.com>
2023-01-10 16:57:54 -0600

    date:2022-01-12 author:Edelman description: modificar join con parametros de certificador fel

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetCodeOfReferenceForStation.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetCodeOfReferenceForStation.sql
@@ -4,23 +4,47 @@
 -- Description:	<Description, SP para obtener código de referencia de credenciales para certificación FEL segun Exc por Id de estación>
 -- =============================================
 CREATE PROCEDURE [dbo].[GetCodeOfReferenceForStation]
-@IdStation AS INT	
+	@IdStation AS INT = NULL
 AS
 BEGIN
 	
 	SET NOCOUNT ON;
 
-BEGIN TRY
+	DECLARE @CodeOfReferenceStation INT;
+
+	BEGIN TRY
     
-	SELECT 
-	      CodeOfReference 
-	FROM [dbo].[CatStation] WITH(NOLOCK)
-	WHERE IdStation = @IdStation
+		SET @CodeOfReferenceStation = (
+			SELECT 
+				CS.CodeOfReference 
+			FROM 
+				[DeliveryBackOffice].[dbo].[CatStation] CS WITH(NOLOCK)
+				INNER JOIN 
+				[DeliveryBackOffice].[dbo].[del_ParametrosFactura] Dpf WITH(NOLOCK)
+			ON CS.CodeOfReference = Dpf.dpf_VpCodeOfReference
+			WHERE 
+				CS.IdStation = @IdStation
+		)
 
-END TRY
-BEGIN CATCH
-	 ROLLBACK
+		IF(ISNULL(@CodeOfReferenceStation,0) = 0)
+		BEGIN
 
-	 SELECT CodeOfReference  = 999
-END CATCH
+			SELECT
+				CodeOfRefrence = 999
+
+		END
+		ELSE
+		BEGIN
+
+			SELECT
+				CodeOfReference = @CodeOfReferenceStation
+
+		END
+
+	END TRY
+	BEGIN CATCH
+
+		 SELECT CodeOfReference  = 999
+
+	END CATCH
 END
