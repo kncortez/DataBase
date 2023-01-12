75a43179e0ae1b9d4a52d88c28fc1a245bb4174d
AndresRuizPeer <andres.ruiz@forzalatam.com>
2023-01-12 10:03:49 -0600

    Date: 12/01/2023 Author: Jose Andrés Ruíz Peer
    ### ADDED
    * Insert de nuevo parametro para verificar despliegue de banner en creación de guías

---
+++ DeliveryBackOffice(PRO)/dbo/Scripts/FDAPI-1268/INSERT INTO  [DeliveryBackOffice].[dbo].[ConfigParams] GuideServiceBanner.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO  [DeliveryBackOffice].[dbo].[ConfigParams]
+	([Name], [Description], [Value], [Status], [CreateDate])
+VALUES
+	('GuideServiceBanner', 'Identificador si se debe desplegar banner en creación de guías', '0', 1, GETDATE())
