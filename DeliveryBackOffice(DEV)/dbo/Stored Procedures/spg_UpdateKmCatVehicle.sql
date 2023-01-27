6b371a644dcfb0ba5af69e504ffa3d2642e58a68
EdelmanV <edelman.vasquez@forzalatam.com>
2023-01-27 14:30:06 -0600

    Date:2022-01-27 author:Edelman description: sp para actualziar km

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_UpdateKmCatVehicle.sql
@@ -0,0 +1,28 @@
+﻿-- =============================================
+-- Author:		<Author,Edelman>
+-- Create date: <Create Date,2022-01-27>
+-- Description:	<Description,SP actualziar km de camión>
+-- =============================================
+CREATE PROCEDURE [dbo].[spg_UpdateKmCatVehicle] 
+@Unidad AS varchar(10),
+@kms AS INT
+
+AS
+BEGIN
+
+ BEGIN TRY
+
+
+      UPDATE CatVehicle 
+	  SET Kms = @kms 
+	  WHERE UnitNumber = @Unidad
+	  
+	  SELECT RESULT = 1 ,'Transacción Exitosa' AS [DESCRIPTION]
+ END TRY
+BEGIN CATCH
+
+ SELECT RESULT = 0 ,ERROR_MESSAGE() AS [DESCRIPTION]
+
+END CATCH
+
+END
