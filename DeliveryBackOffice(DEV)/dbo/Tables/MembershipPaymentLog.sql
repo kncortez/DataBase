9395412550662e2b15f9203b639cfbb8471e15a1
AndresRuizPeer <andres.ruiz@forzalatam.com>
2023-02-07 10:16:11 -0600

    Date: 07/02/2023 Author: Jose Andrés Ruíz Peer
    ### ADDED
    * Manejo de imagen para venta de membresías desde portal de telemercadeo

--- DeliveryBackOffice(DEV)/dbo/Tables/MembershipPaymentLog.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/MembershipPaymentLog.sql
@@ -9,6 +9,7 @@
     [TokenUpdated]           NVARCHAR (50) NULL,
     [DateUpdated]            DATETIME      NULL,
     [TransactionOrder]       NVARCHAR (50) NULL,
+    [PaymentImageURL]        NVARCHAR (600) NULL,
     CONSTRAINT [PK_MembershipPaymentLog] PRIMARY KEY CLUSTERED ([IdMembershipPaymentLog] ASC),
     CONSTRAINT [FK_MembershipPaymentLog_Membership] FOREIGN KEY ([MembershipId]) REFERENCES [dbo].[Membership] ([IdMembership]),
     CONSTRAINT [FK_MembershipPaymentLog_PaymentType] FOREIGN KEY ([TypeOfInOutOfMoneyId]) REFERENCES [dbo].[ctgTypeOfInOutOfMoney] ([tio_pk_id])
@@ -16,3 +17,4 @@
 
 
 
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Imagen de pago que se realizo, de ser posible', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MembershipPaymentLog', @level2type = N'COLUMN', @level2name = N'PaymentImageURL';
--- DeliveryBackOffice(DEV)/dbo/Tables/SubscriptionPaymentLog.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/SubscriptionPaymentLog.sql
@@ -9,6 +9,8 @@
     [TokenUpdated]             NVARCHAR (50) NULL,
     [DateUpdated]              DATETIME      NULL,
     [TransactionOrder]         NVARCHAR (50) NULL,
+    [PaymentImageURL]          NVARCHAR (600) NULL,
     CONSTRAINT [PK_SubscriptionPaymentLog] PRIMARY KEY CLUSTERED ([IdSubscriptionPaymentLog] ASC)
 );
 
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Imagen de pago que se realizo, de ser posible', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SubscriptionPaymentLog', @level2type = N'COLUMN', @level2name = N'PaymentImageURL';
