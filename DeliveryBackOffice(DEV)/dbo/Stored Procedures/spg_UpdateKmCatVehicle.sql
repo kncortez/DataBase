945190dae48a019bcecfbd7da2e162747a81c748
EdelmanV <edelman.vasquez@forzalatam.com>
2023-02-07 12:32:05 -0600

    Date:2023-02-07 author:Edelman description SP para guardar la actualización de km y bitacora

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_UpdateKmCatVehicle.sql
@@ -0,0 +1,44 @@
+﻿-- =============================================
+-- Author:		<Author,Edelman>
+-- Create date: <Create Date,2022-01-27>
+-- Description:	<Description,SP actualziar km de camión y registro de bitacora>
+-- =============================================
+CREATE PROCEDURE [dbo].[spg_UpdateKmCatVehicle] 
+@Unidad AS varchar(10),
+@kms AS INT,
+@Observacion AS nvarchar(500),
+@Token AS nvarchar(50)
+
+AS
+BEGIN
+
+ BEGIN TRY
+
+
+      UPDATE [dbo].[CatVehicle] 
+	  SET Kms = @kms 
+	  WHERE UnitNumber = @Unidad
+
+	  INSERT INTO [dbo].[VehicleLog] (Unidad,
+	                                  Kms, 
+	                                  Observacion, 
+									  TokenCreate, 
+									  DateCreate
+									  )
+				Values(@Unidad,
+				       @kms,
+					   @Observacion,
+					   @Token,
+					   GETDATE()
+				
+				      )
+	  
+	  SELECT RESULT = 1 ,'Transacción Exitosa' AS [DESCRIPTION]
+ END TRY
+BEGIN CATCH
+
+ SELECT RESULT = 0 ,ERROR_MESSAGE() AS [DESCRIPTION]
+
+END CATCH
+
+END
