2828ced374958c6c8705e6537e4337f56bfbd34b
Alberto Ixchop <albertokanecixchop@gmail.com>
2023-02-07 11:13:09 -0600

    corrección de instrucción go en catmembership

--- DeliveryBackOffice(DEV)/dbo/Tables/CatMembership.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/CatMembership.sql
@@ -75,5 +75,7 @@ EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Tabla de ca
 
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Campo de ícono configurable', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'CatMembership', @level2type = N'COLUMN', @level2name = N'Icon';
+
+GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Nombre de banner a desplegar cuando servicios de monto fijo esten proximos a acabarse', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'CatMembership', @level2type = N'COLUMN', @level2name = N'NextSalesPackageBanner';
 
