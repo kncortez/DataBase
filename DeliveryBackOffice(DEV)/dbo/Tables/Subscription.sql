1197d3d4bd99ee011d17e5898fff8579c9d2ef3b
Alberto Ixchop <albertokanecixchop@gmail.com>
2023-02-08 17:32:27 -0600

    Se elimina linea repetida de tabla subscription

--- DeliveryBackOffice(DEV)/dbo/Tables/Subscription.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/Subscription.sql
@@ -44,6 +44,3 @@ EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Tarifario a 
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Tarifario alterno a utilizar cuando se usa suscripción', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Subscription', @level2type = N'COLUMN', @level2name = N'AlternativeRateHeaderId';
 
-GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Tarifario alterno a utilizar cuando se usa suscripción', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Subscription', @level2type = N'COLUMN', @level2name = N'AlternativeRateHeaderId';
-
