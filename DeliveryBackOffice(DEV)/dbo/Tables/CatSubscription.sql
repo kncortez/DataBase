35b9c4d6b7b3c87d7def54d7657fc7ec24519852
Alfredo Monroy <alfredo.monroy@forzalatam.com>
2023-02-13 11:06:55 -0600

    Date: 13-02-2023 Author: Alfredo Monroy * Corrección de objetos inválidos en la definición del sqlproj * Eliminación de descripciones de columnas repetidas en tabla CatSubscription

--- DeliveryBackOffice(DEV)/dbo/Tables/CatSubscription.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/CatSubscription.sql
@@ -27,18 +27,11 @@ CREATE TABLE [dbo].[CatSubscription] (
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Campo de ícono configurable', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'CatSubscription', @level2type = N'COLUMN', @level2name = N'Icon';
-
-GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Tarifario a utilizar cuando se usa suscripción', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'CatSubscription', @level2type = N'COLUMN', @level2name = N'RateHeaderId';
 
 
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Campo de ícono configurable', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'CatSubscription', @level2type = N'COLUMN', @level2name = N'Icon';
-
-
-GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Tarifario a utilizar cuando se usa suscripción', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'CatSubscription', @level2type = N'COLUMN', @level2name = N'RateHeaderId';
 
 
 GO
