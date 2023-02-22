cf1a3f387b86ac577e5e82b88bfaee2ad0b75c63
EdelmanV <edelman.vasquez@forzalatam.com>
2023-02-22 15:48:53 -0600

    Date: 2023.02.22 author: Edelman description: actualziar mensaje de incidencia

---
+++ DeliveryBackOffice(DEV)/dbo/Scripts/Update [dbo].[ConfigExternalPlatform].sql
@@ -0,0 +1,3 @@
+Update [dbo].[ConfigExternalPlatform] SET ConfigParameterValue='Hola <Nombre> hemos intentado entregar este paquete <Guia> ingresa al siguiente link' Where IdConfigExternalPlatform=16 
+
+
