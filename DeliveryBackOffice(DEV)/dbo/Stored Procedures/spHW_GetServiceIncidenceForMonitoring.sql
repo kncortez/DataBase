a31883a5fd1932010c575aad480e72e5d4379ce7
AndresRuizPeer <andres.ruiz@forzalatam.com>
2023-02-22 10:25:28 -0600

    Date: 22/02/2023 Author: Jose Andrés Ruíz Peer
    ### ADDED
    * Campos adicionales para mejor manejo y despliegue de datos

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_GetServiceIncidenceForMonitoring.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_GetServiceIncidenceForMonitoring.sql
@@ -98,6 +98,20 @@ BEGIN
 			) 'ServiceAddress'
 			,(
 				CASE
+					WHEN DO.IsLastMileReturn = 1 THEN DO.Sender_Town
+					WHEN ISNULL(DO.IsLastMileReturn, 0) = 0 THEN DO.Receiver_Town
+					ELSE DO.Receiver_Town
+				END
+			) 'ServiceTownship'
+			,(
+				CASE
+					WHEN DO.IsLastMileReturn = 1 THEN DO.Sender_Department
+					WHEN ISNULL(DO.IsLastMileReturn, 0) = 0 THEN DO.Receiver_Department
+					ELSE DO.Receiver_Department
+				END
+			) 'ServiceProvince'
+			,(
+				CASE
 					WHEN DO.IsLastMileReturn = 1 THEN DO.Sender_Phone
 					WHEN ISNULL(DO.IsLastMileReturn, 0) = 0 THEN DO.Receiver_Phone
 					ELSE DO.Receiver_Phone
@@ -242,6 +256,7 @@ BEGIN
 			,DA.Guide_Serie 'GuideSerie'
 			,DA.Guide_Number 'GuideNumber'
 			,ISNULL(COI.IsActionIssued, 0) 'IsActionIssued'
+			,COI.IdConfirmationOfIncidence 'ConfirmationOfIncidenceId'
 		FROM
 			[DeliveryBackOffice].[dbo].[ConfirmationOfIncidence] COI WITH(NOLOCK)
 			INNER JOIN
