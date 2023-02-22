268e357567a3534d256b6d493d5f830a6d7c435e
EdelmanV <edelman.vasquez@forzalatam.com>
2023-02-22 09:24:08 -0600

    Date:2023-02-22 author:Edelman description: quitar filtro de estado paquete abandonado, para que muestre en cabecera datos aun que no hayan guías asignadas a inspección

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/RptManifestofGuidesinInspection.sql
@@ -0,0 +1,49 @@
+﻿-- =============================================
+-- Author:		<Author,Edelman Vásquez>
+-- Create date: <Create Date,2023-02-20>
+-- Description:	<Description, SP para  Manifiesto de guías en estado de inspección tras liquidación de entregas y devoluciones>
+-- =============================================
+CREATE PROCEDURE [dbo].[RptManifestofGuidesinInspection]
+@IdManifest INT
+			
+AS
+BEGIN
+	
+	DECLARE @GuideCount INT
+	DECLARE @IdStatus INT =(Select StatusOrderId From dbo.StatusOrder where OrderDescription ='Paquete abandonado')
+	SET NOCOUNT ON;
+
+	SET @GuideCount = (
+		      Select COUNT(DO.Guide_Number)
+				 From [dbo].[DeliveryOrderBySettlement] DOS 
+                Inner Join
+              [dbo].[DeliverySettlementDetail] DSD
+                ON DOS.ID = DSD.ID_DeliveryOrderBySettlement
+				Inner Join 
+			 [dbo].[DeliveryOrder] DO
+			   ON DSD.Guide_Serie = DO.Guide_Serie AND DSD.Guide_Number = DO.Guide_Number
+ Where  DO.StatusOrderId=@IdStatus and DSD.ID_DeliveryOrderBySettlement=@IdManifest
+		
+	)
+
+
+		     Select Distinct
+			        DOS.ID, 
+			        DOS.Date_Received,
+					@GuideCount as Guides_Received,
+			       isnull(sr.First_Name,'') + ' ' + isnull(sr.Last_Name,'') as Courier_Name,
+				   DOS.Route_Received
+			 From [dbo].[DeliveryOrderBySettlement] DOS 
+					 Inner Join
+                  [dbo].[DeliverySettlementDetail] DSD
+                ON DOS.ID = DSD.ID_DeliveryOrderBySettlement
+					Inner Join 
+				  [dbo].[DeliveryOrder] DO
+			   ON DSD.Guide_Serie = DO.Guide_Serie AND DSD.Guide_Number = DO.Guide_Number
+					Inner JOIN [DeliveryBackOffice].[dbo].[SenderReceiver] sr 
+		       ON sr.ID = DOS.ID_Courier
+			 Where   DSD.ID_DeliveryOrderBySettlement=@IdManifest
+
+		
+  
+END
