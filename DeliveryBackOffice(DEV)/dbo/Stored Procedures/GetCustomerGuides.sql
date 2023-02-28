6cd3c75dfa669f4e6749e2e566bcb4aa0b47765c
EdelmanV <edelman.vasquez@forzalatam.com>
2023-02-28 16:36:46 -0600

    Date:2023-02-28 author:Edelman description: agregar filtro en usuario individual para mostrar ultimo extado externo guías de mis envíos

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetCustomerGuides.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetCustomerGuides.sql
@@ -4,7 +4,11 @@
 -- Create date: <2022-11-03>
 -- Description:	< Obtener guías por rango de fechas de un cliente, vajo distintos filtros>
 -- =============================================
-
+-- =============================================
+-- Author:		<Edelman, Vásquez>
+-- Create date: <2022-11-03>
+-- Description:	< filtro para usuarios individuales para que meustre último estado externo>
+-- =============================================
 CREATE PROCEDURE [dbo].[GetCustomerGuides]
 	@AccountId INT, 
 	@StartDate DATETIME = NULL,
@@ -35,6 +39,8 @@ BEGIN
 
 	DECLARE @CanceledStatusOrderId INT = (SELECT TOP 1 SO.StatusOrderId FROM [DeliveryBackOffice].[dbo].[StatusOrder] SO WITH(NOLOCK) WHERE SO.OrderDescription = 'Anulado' COLLATE Latin1_General_CI_AI);
 
+	DECLARE @InmediatePaymentTime INT = (SELECT TOP 1 CPT.TimePlaId FROM [DeliveryBackOffice].[dbo].[CatPaymentTime] CPT WITH(NOLOCK) WHERE CPT.TimePlaName = 'Ahora' COLLATE Latin1_General_CI_AI)
+
 	-- Configuraciones generales
 	DECLARE @OffsetRegistries BIGINT = @DisplayPage * @DisplayRegistries;
 	DECLARE @TotalServices BIGINT = 0;
@@ -48,6 +54,15 @@ BEGIN
 		StatusOrderId INT
 	);
 	
+		DECLARE @ExternalTypeId INT = (
+				SELECT
+					TOP 1
+						CST.IdCatStatusType
+				FROM
+					[DeliveryBackOffice].[dbo].[CatStatusType] CST WITH (NOLOCK)
+				WHERE
+					CST.StatusType = 'Externo' COLLATE Latin1_General_CI_AI
+			)
 	-- Obtener datos de usuario
 	SELECT
 		@CustomerId = Cu.IdCustomer
@@ -108,7 +123,7 @@ BEGIN
 			GuideSerie NVARCHAR(2),
 			GuideNumber INT,
 			StatusOrderId INT,
-			Pieces_Dry INT,
+			Pieces_Dry INT,               
 			Pieces_Cold INT,
 			Ticket_Number NVARCHAR(50),
 			Receiver_FirstName NVARCHAR(100),
@@ -148,7 +163,7 @@ BEGIN
 				DISTINCT
 					DO.Guide_Serie
 					,DO.Guide_Number
-					,DO.StatusOrderId
+					,LastExternalStatus.StatusOrderId
 					,DO.Pieces_Dry
 					,DO.Pieces_Cold
 					,DO.Ticket_Number
@@ -162,10 +177,32 @@ BEGIN
 					,DO.DateCreated
 			FROM
 				[DeliveryBackOffice].[dbo].[DeliveryOrder] DO WITH(NOLOCK)
-				INNER JOIN
+				OUTER APPLY (
+					SELECT
+						TOP (1)
+							SO.[StatusOrderId]
+					FROM
+						[DeliveryBackOffice].[dbo].[DeliveryOrderDetail] DOD  WITH(NOLOCK) 
+						INNER JOIN
+							[DeliveryBackOffice].[dbo].[StatusOrder] SO  WITH(NOLOCK) 
+							ON
+								[SO].[StatusOrderId] = [DOD].[StatusOrderId]
+								AND
+								[SO].[CatStatusTypeId] = @ExternalTypeId
+					WHERE
+						DOD.[Guide_Serie] = DO.[Guide_Serie]
+						AND
+						DOD.[Guide_Number] = DO.[Guide_Number]
+						AND
+						DOD.[RowStatus] = 1
+					ORDER BY
+						DOD.[DateCreated] DESC
+				) LastExternalStatus
+				INNER  JOIN
 					@FilteredStatus FS
 					ON
-						DO.StatusOrderId = FS.StatusOrderId
+					LastExternalStatus.StatusOrderId = FS.StatusOrderId 
+
 			WHERE
 				-- Área de filtros
 				(
@@ -175,6 +212,7 @@ BEGIN
 				( (@StartDate IS NULL AND @EndDate IS NULL) OR DO.DateCreated BETWEEN @StartDate AND @EndDate )
 				AND
 				( @GuideFilter IS NULL OR CONCAT(DO.Guide_Serie, DO.Guide_Number) LIKE '%'+LTRIM(RTRIM(@GuideFilter))+'%' )
+				
 
 		END
 		ELSE IF (@CustomerTypeId = 2)
@@ -298,7 +336,10 @@ BEGIN
 				ISNULL(DO.Ticket_Number,'') 'Reference',
 				UPPER(LTRIM(RTRIM(CONCAT(DO.Receiver_FirstName,' ',DO.Receiver_LastName)))) 'ReceiverName',
 				ISNULL(DO.Receiver_Phone, '') 'ReceiverPhone',
-				ISNULL(DO.IsCollect, 0) 'IsCollect',
+				(CASE
+					WHEN PBSL.IdPointsByServiceLog IS NOT NULL THEN 0
+					ELSE ISNULL(DO.IsCollect, 0)
+				END) 'IsCollect',
 				CAST(CAST(ISNULL(DO.PriceShippment, 0) AS MONEY) AS NVARCHAR) 'PriceService',
 				CAST(CAST(ISNULL(DO.Collect_OnDelivery, 0) AS MONEY) AS NVARCHAR) 'CollectOnDelivery',
 				SO.StatusOrderId 'IdStatus',
@@ -307,6 +348,7 @@ BEGIN
 				ISNULL((
 					CASE
 						WHEN ISNULL(DOPD.ShipmentCompleted, 0) = 0 THEN 'PENDIENTE'
+						WHEN PBSL.IdPointsByServiceLog IS NOT NULL THEN 'PUNTOS'
 						WHEN DOPD.TimePlaId = 8 THEN 'CREDITO'
 						WHEN DO.IsCollect = 1 THEN 'COLLECT'
 						ELSE UPPER(CPType.PayTypeName)
@@ -315,13 +357,17 @@ BEGIN
 				ISNULL((
 					CASE
 						WHEN ISNULL(DOPD.ShipmentCompleted, 0) = 0 THEN UPPER('pendiente de pago')
+						WHEN PBSL.IdPointsByServiceLog IS NOT NULL THEN UPPER('Pago con puntos forza')
 						WHEN DOPD.TypeofInOutMoneyId = 6 THEN UPPER('pago con tarjeta')
 						WHEN DOPD.TypeofInOutMoneyId = 8 THEN UPPER('pago al crédito')
 						ELSE UPPER(IOOMT.tio_pk_name)
 					END
 				), UPPER('pago en efectivo')) 'TypePayment',
 				UPPER(ISNULL(DO.TypeService, '')) 'TypeService',
-				ISNULL(CONVERT(VARCHAR, DOPD.TimePlaId), '') 'TimePayment',
+				(CASE 
+					WHEN PBSL.IdPointsByServiceLog IS NOT NULL THEN CONVERT(VARCHAR, @InmediatePaymentTime)
+					ELSE ISNULL(CONVERT(VARCHAR, DOPD.TimePlaId), '') 
+				END) 'TimePayment',
 				ISNULL(CPTime.TimePlaName, '') 'TimePaymentDescription',
 				'Q.' 'CurrencySymbol'
 			FROM
@@ -348,6 +394,16 @@ BEGIN
 					[DeliveryBackOffice].[dbo].[ctgTypeOfInOutOfMoney] IOOMT WITH(NOLOCK)
 					ON
 						DOPD.TypeofInOutMoneyId = IOOMT.tio_pk_id
+				LEFT JOIN
+					[DeliveryBackOffice].[dbo].[PointsByServiceLog] PBSL WITH(NOLOCK)
+					ON
+						DO.GuideSerie = PBSL.GuideSerie
+						AND
+						DO.GuideNumber = PBSL.GuideNumber
+						AND
+						PBSL.PointsConsumed > 0
+						AND
+						PBSL.PointsReceived = 0
 				ORDER BY
 					DO.DateCreated DESC
 				OFFSET @OffsetRegistries ROWS FETCH NEXT @DisplayRegistries ROWS ONLY
