82c84c5b84710c931d08cfad9beb5717f4e38f5e
EdelmanV <edelman.vasquez@forzalatam.com>
2023-03-06 16:15:15 -0600

    date:2023-03-06 author:edelman description:modificar código por descripción

--- DeliveryBackOffice(DEV)/dbo/Scripts/Update [dbo].[ConfigExternalPlatform].sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/Update [dbo].[ConfigExternalPlatform].sql
@@ -1,1 +1,1 @@
-Update [dbo].[ConfigExternalPlatform] SET ConfigParameterValue='Hola <Nombre> hubo un inconveniente al entregar el paquete <Guia> ingrese al siguiente link' Where IdConfigExternalPlatform=16 
+Update [dbo].[ConfigExternalPlatform] SET ConfigParameterValue='Hola <Nombre> hubo un inconveniente al entregar el paquete <Guia> ingrese al siguiente link' Where ConfigParameterName='MsmMessageIncidence'
