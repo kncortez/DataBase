09973ba2b2d18aaa2e7e051e823a748ccf521026
Jerson Ochoa <jerson.ochoa@forzadelivery.com>
2023-03-08 11:23:50 -0600

    Date: 08-03-2023 Author: Jerson Ochoa ### ADDED * DataTime field for end time in linehaul dispatch and settlement [APO-158]

--- DeliveryBackOffice(DEV)/dbo/Tables/LinehaulRouteSettlement.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/LinehaulRouteSettlement.sql
@@ -1,25 +1,29 @@
 ﻿CREATE TABLE [dbo].[LinehaulRouteSettlement] (
-    [IdLinehaulRouteSettlement]  INT           IDENTITY (1, 1) NOT NULL,
-    [LinehaulRoutePreparationId] INT           NOT NULL,
-    [UserReceived]               NVARCHAR (50) NOT NULL,
-    [DateReceived]               DATETIME      NOT NULL,
-    [ContainersReceived]         INT           NULL,
-    [ToolsReceived]              INT           NULL,
-    [GuidesReceived]             INT           NULL,
-    [GuidePiecesReceived]        INT           NULL,
-    [GuidePiecesMissing]         INT           NULL,
-    [RowStatus]                  BIT           DEFAULT ((1)) NOT NULL,
-    [TokenCreated]               NVARCHAR (50) NOT NULL,
-    [DateCreated]                DATETIME      NOT NULL,
-    [TokenUpdated]               NVARCHAR (50) NULL,
-    [DateUpdated]                DATETIME      NULL,
-    [HubID]                      INT           NULL,
-    [CatLinehaulStatusId]        INT           NOT NULL,
-    PRIMARY KEY CLUSTERED ([IdLinehaulRouteSettlement] ASC),
+    [IdLinehaulRouteSettlement]      INT           IDENTITY (1, 1) NOT NULL,
+    [LinehaulRoutePreparationId]     INT           NOT NULL,
+    [HubID]                          INT           NULL,
+    [CatLinehaulStatusId]            INT           NOT NULL,
+    [UserReceived]                   NVARCHAR (50) NOT NULL,
+    [DateReceived]                   DATETIME      NOT NULL,
+    [ContainersReceived]             INT           NULL,
+    [ToolsReceived]                  INT           NULL,
+    [GuidesReceived]                 INT           NULL,
+    [GuidePiecesReceived]            INT           NULL,
+    [GuidePiecesMissing]             INT           NULL,
+    [VehicleKms]                     INT           CONSTRAINT [DF_LinehaulRouteSettlement_VehicleKms] DEFAULT ((0)) NULL,
+    [EndDateLinehaulRouteSettlement] DATETIME      NULL,
+    [RowStatus]                      BIT           CONSTRAINT [DF__LinehaulR__RowSt__02732F85] DEFAULT ((1)) NOT NULL,
+    [TokenCreated]                   NVARCHAR (50) NOT NULL,
+    [DateCreated]                    DATETIME      NOT NULL,
+    [TokenUpdated]                   NVARCHAR (50) NULL,
+    [DateUpdated]                    DATETIME      NULL,
+    CONSTRAINT [PK__Linehaul__6EB79E05F17E1868] PRIMARY KEY CLUSTERED ([IdLinehaulRouteSettlement] ASC),
     CONSTRAINT [FK_LinehaulRouteSettlement_CatLinehaulStatus] FOREIGN KEY ([CatLinehaulStatusId]) REFERENCES [dbo].[CatLinehaulStatus] ([IdCatLinehaulStatus]),
     CONSTRAINT [FK_LinehaulRouteSettlement_Hub] FOREIGN KEY ([HubID]) REFERENCES [dbo].[HubLogistics] ([IdHubLogistic]),
     CONSTRAINT [FK_LinehaulRouteSettlement_LinehaulRoutePreparation] FOREIGN KEY ([LinehaulRoutePreparationId]) REFERENCES [dbo].[LinehaulRoutePreparation] ([IdLinehaulRoutePreparation])
 );
+
+
 
 
 
@@ -114,4 +118,12 @@ EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'ID de HUB l
 
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'ID de estado | Tabla CatLinehaulStatus', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'LinehaulRouteSettlement', @level2type = N'COLUMN', @level2name = N'CatLinehaulStatusId';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Kilometraje de vehículo asociado', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'LinehaulRouteSettlement', @level2type = N'COLUMN', @level2name = N'VehicleKms';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha de cierre de liquidación', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'LinehaulRouteSettlement', @level2type = N'COLUMN', @level2name = N'EndDateLinehaulRouteSettlement';
 
