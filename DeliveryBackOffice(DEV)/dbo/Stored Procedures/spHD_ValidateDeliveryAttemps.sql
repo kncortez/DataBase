14bea906f0c4561458cb070c8f3810302890021b
Alfredo Monroy <alfredo.monroy@forzalatam.com>
2023-03-14 12:47:43 -0600

    Se elimina rollback que se hizo cuando se publicó por primera vez el tema de reversión de datos para devoluciones.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHD_ValidateDeliveryAttemps.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHD_ValidateDeliveryAttemps.sql
@@ -20,105 +20,74 @@ BEGIN
         FlowGuide TINYINT
     );
 
-    DECLARE @StatusOrderDestroyed INT =
-            (
-                SELECT so.StatusOrderId
-                FROM StatusOrder so
-                WHERE so.OrderDescription = 'Paquete destruido'
-            );
 	DECLARE @STATUSDECLAREDRETURNED_DO INT = (SELECT TOP 1 SO.StatusOrderId FROM DBO.StatusOrder SO WITH(NOLOCK) WHERE OrderDescription = 'Declarado para Devolución');
 
-    DECLARE @Active BIT = 'false';
+    BEGIN TRANSACTION;
+    BEGIN TRY
+        INSERT INTO @TblGuides
+        SELECT DISTINCT
+            dsd.Guide_Serie
+            ,dsd.Guide_Number
+            ,CASE
+                WHEN do.IsLastMileReturn IS NULL OR do.IsLastMileReturn = 0 THEN CASE
+                        WHEN doad.GuideDeliveryAttemptCount >= doad.GuideDeliveryMaxAttemptCount OR coi.ClientConfirmsReturn = 1 THEN 2
+                        ELSE 1
+                    END
+                ELSE 1
+            END
+            FlowGuide
+        FROM DeliverySettlementDetail dsd WITH (NOLOCK)
+        INNER JOIN DeliveryOrder do WITH (NOLOCK)
+            ON dsd.Guide_Serie = do.Guide_Serie
+                AND dsd.Guide_Number = do.Guide_Number
+        INNER JOIN DeliveryOrderAttemptData doad WITH (NOLOCK)
+            ON dsd.Guide_Serie = doad.GuideSerie
+                AND dsd.Guide_Number = doad.GuideNumber
+                AND doad.RowStatus = 1
+        INNER JOIN DeliveryAttempt da WITH(NOLOCK)
+        ON dsd.Guide_Serie = da.Guide_Serie
+                AND dsd.Guide_Number = da.Guide_Number
+                AND da.ID_DeliveryOrderBySettlement = dsd.ID_DeliveryOrderBySettlement
+        LEFT JOIN ConfirmationOfIncidence coi WITH(NOLOCK)
+        ON da.ConfirmationOfIncidenceId = coi.IdConfirmationOfIncidence
+        WHERE dsd.ID_DeliveryOrderBySettlement = @DeliveryOrderBySettlementId
+        AND dsd.RowStatus = 1
+        AND dsd.Guide_Returned = 1
 
 
+        -- Marcar las que ya no tienen intentos de entrega disponibles como devolución
+        UPDATE do
+        SET do.IsLastMileReturn = 1,
+            do.StatusOrderId = @STATUSDECLAREDRETURNED_DO
+        FROM DeliveryOrder do
+            INNER JOIN @TblGuides tg
+                ON do.Guide_Serie = tg.GuideSerie
+                    AND do.Guide_Number = tg.GuideNumber
+        WHERE tg.FlowGuide = 2;
 
-    IF @Active = 'true'
-    BEGIN
-        BEGIN TRANSACTION;
+        INSERT INTO [DeliveryBackOffice].[dbo].[DeliveryOrderDetail]
+            (Guide_Serie, Guide_Number, StatusOrderId, UserCreated, DateCreated, DateCreatedInSystem, RowStatus)
+        SELECT
+            DISTINCT
+                tg.GuideSerie, tg.GuideNumber, @STATUSDECLAREDRETURNED_DO, 'spHD_ValidateDeliveryAttemps', GETDATE(), GETDATE(), 1
+        FROM @TblGuides tg
+        WHERE tg.FlowGuide = 2;
 
-        BEGIN TRY
-            INSERT INTO @TblGuides
-            SELECT DISTINCT
-				dsd.Guide_Serie
-			   ,dsd.Guide_Number
-			   ,CASE
-					WHEN do.IsLastMileReturn IS NULL OR do.IsLastMileReturn = 0 THEN CASE
-							WHEN doad.GuideDeliveryAttemptCount >= doad.GuideDeliveryMaxAttemptCount OR coi.ClientConfirmsReturn = 1 THEN 2
-							ELSE 1
-						END
-					ELSE 1
-				END
-				FlowGuide
-			FROM DeliverySettlementDetail dsd WITH (NOLOCK)
-			INNER JOIN DeliveryOrder do WITH (NOLOCK)
-				ON dsd.Guide_Serie = do.Guide_Serie
-					AND dsd.Guide_Number = do.Guide_Number
-			INNER JOIN DeliveryOrderAttemptData doad WITH (NOLOCK)
-				ON dsd.Guide_Serie = doad.GuideSerie
-					AND dsd.Guide_Number = doad.GuideNumber
-					AND doad.RowStatus = 1
-			INNER JOIN DeliveryAttempt da WITH(NOLOCK)
-			ON dsd.Guide_Serie = da.Guide_Serie
-					AND dsd.Guide_Number = da.Guide_Number
-					AND da.ID_DeliveryOrderBySettlement = dsd.ID_DeliveryOrderBySettlement
-			LEFT JOIN ConfirmationOfIncidence coi WITH(NOLOCK)
-			ON da.ConfirmationOfIncidenceId = coi.IdConfirmationOfIncidence
-			WHERE dsd.ID_DeliveryOrderBySettlement = @DeliveryOrderBySettlementId
-			AND dsd.RowStatus = 1
-			AND dsd.Guide_Returned = 1
-
-
-            -- Marcar las que ya no tienen intentos de entrega disponibles como devolución
-            UPDATE do
-            SET do.IsLastMileReturn = 1,
-				do.StatusOrderId = @STATUSDECLAREDRETURNED_DO
-            FROM DeliveryOrder do
-                INNER JOIN @TblGuides tg
-                    ON do.Guide_Serie = tg.GuideSerie
-                       AND do.Guide_Number = tg.GuideNumber
-            WHERE tg.FlowGuide = 2;
-
-			INSERT INTO [DeliveryBackOffice].[dbo].[DeliveryOrderDetail]
-				(Guide_Serie, Guide_Number, StatusOrderId, UserCreated, DateCreated, DateCreatedInSystem, RowStatus)
-			SELECT
-				DISTINCT
-					tg.GuideSerie, tg.GuideNumber, @STATUSDECLAREDRETURNED_DO, 'spHD_ValidateDeliveryAttemps', GETDATE(), GETDATE(), 1
-			FROM @TblGuides tg
-            WHERE tg.FlowGuide = 2;
-
-            COMMIT TRANSACTION;
-
-            SELECT 1 'StatusCode',
-                   'Registros obtenidos correctamente.' 'Description';
-
-            SELECT GuideSerie Guide_Serie,
-                   GuideNumber Guide_Number
-            FROM @TblGuides
-            WHERE FlowGuide = 2;
-
-        END TRY
-        BEGIN CATCH
-            ROLLBACK TRANSACTION;
-
-            SELECT 0 'StatusCode',
-                   ERROR_MESSAGE() 'Description';
-        END CATCH;
-
-    END;
-
-    ELSE
-    BEGIN
-
-
+        COMMIT TRANSACTION;
 
         SELECT 1 'StatusCode',
-               'Registros obtenidos correctamente.' 'Description';
+                'Registros obtenidos correctamente.' 'Description';
 
-			    SELECT GuideSerie Guide_Serie,
-                   GuideNumber Guide_Number
-            FROM @TblGuides
-            WHERE FlowGuide = 2;
-				 
-			 
-    END;
+        SELECT GuideSerie Guide_Serie,
+                GuideNumber Guide_Number
+        FROM @TblGuides
+        WHERE FlowGuide = 2;
+
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+
+        SELECT 0 'StatusCode',
+                ERROR_MESSAGE() 'Description';
+    END CATCH;
 END;
