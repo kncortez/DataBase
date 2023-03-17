0a071edd5b5d9cd24064953d1be6214972b614b2
AndresRuizPeer <andres.ruiz@forzalatam.com>
2023-03-17 15:20:57 -0600

    Date: 17/03/2023 Author: Jose Andrés Ruíz Peer
    ### FIXED
    * MAnejo de IsLastMileReturn en reportes

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_settlement_cod_guides.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_settlement_cod_guides.sql
@@ -19,22 +19,22 @@ BEGIN
 SELECT do.Guide_Serie + ISNULL(CONVERT(NVARCHAR, do.Guide_Number), '') Guide_Code,
        do.Pieces_Cold Pieces_Cold,
        do.Pieces_Dry Pieces_Dry,
-       ISNULL(do.Receiver_FirstName, '') + ' ' + ISNULL(do.Receiver_LastName, '') Receiver_Fullname,
-       IIF(cu.Name = 'FD EXPRESS CENTER', CONCAT(do.Sender_FirstName, ' ', do.Sender_LastName), cu.Name) AS Client,
-       do.Receiver_Town Receiver_Town,
-       do.Receiver_Department Receiver_Departament,
+       (CASE WHEN [do].[IsLastMileReturn] = 1 THEN IIF(cu.Name = 'FD EXPRESS CENTER', CONCAT(do.Sender_FirstName, ' ', do.Sender_LastName), cu.Name) ELSE ISNULL(do.Receiver_FirstName, '') + ' ' + ISNULL(do.Receiver_LastName, '') END) Receiver_Fullname,
+       (CASE WHEN do.[IsLastMileReturn] = 1 THEN ISNULL(do.Receiver_FirstName, '') + ' ' + ISNULL(do.Receiver_LastName, '') ELSE IIF(cu.Name = 'FD EXPRESS CENTER', CONCAT(do.Sender_FirstName, ' ', do.Sender_LastName), cu.Name) END) AS Client,
+       (CASE WHEN do.[IsLastMileReturn] = 1 THEN do.[Sender_Town] ELSE do.Receiver_Town END) Receiver_Town,
+       (CASE WHEN do.[IsLastMileReturn] = 1 THEN do.[Sender_Department] ELSE do.Receiver_Department END) Receiver_Departament,
        CONVERT(VARCHAR, do.Preparation_Date, 103) + ' ' + CONVERT(VARCHAR(5), do.Preparation_Date, 108) Preparation_Date,
        CONVERT(VARCHAR, do.Shipping_Date, 103) Shipping_Date,
        ISNULL(CONVERT(VARCHAR, do.Delivery_Max_Date, 103), '') Max_Date,
-       do.Receiver_Phone Receiver_Phone,
+       (CASE WHEN [do].[IsLastMileReturn] = 1 THEN do.[Sender_Phone] ELSE do.Receiver_Phone END) Receiver_Phone,
        (
            SELECT DeliveryBackOffice.dbo.fn_get_rackposition(do.Guide_Serie, do.Guide_Number)
        ) Rack_Position,
        CAST(IIF(do.IsCollect = 'TRUE', ISNULL(do.PriceShippment, 0), 0) AS DECIMAL(18, 2)) Price,
-       CAST(ISNULL(do.Collect_OnDelivery, 0) AS DECIMAL(18, 2)) Collect_on_Delivery,
+       CAST(ISNULL((CASE WHEN do.[IsLastMileReturn] = 1 THEN 0 ELSE do.Collect_OnDelivery END), 0) AS DECIMAL(18, 2)) Collect_on_Delivery,
        CAST(IIF(do.IsCollect = 'TRUE',
-                (ISNULL(do.Collect_OnDelivery, 0) + ISNULL(do.PriceShippment, 0)),
-                ISNULL(do.Collect_OnDelivery, 0)) AS DECIMAL(18, 2)) Total,
+                (ISNULL((CASE WHEN do.[IsLastMileReturn] = 1 THEN 0 ELSE do.Collect_OnDelivery END), 0) + ISNULL(do.PriceShippment, 0)),
+                ISNULL((CASE WHEN do.[IsLastMileReturn] = 1 THEN 0 ELSE do.Collect_OnDelivery END), 0)) AS DECIMAL(18, 2)) Total,
        do.Receiver_ID Receiver_ID
 FROM [DeliveryBackOffice].[dbo].DeliveryOrder do
     JOIN DeliveryBackOffice.dbo.DeliverySettlementDetail dsd
