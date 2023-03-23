cdbf40b967cf7d53a6abe51e45fae7b9727961c2
Jerson Ochoa <jerson.ochoa@forzadelivery.com>
2023-03-23 15:09:26 -0600

    Date: 23-03-2023 Author: Jerson Ochoa ### ADDED * Get Version Data for courier App [FDAPI-1480]

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spCA_GetVersionData.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		<Jerson Ochoa>
+-- Create date: <22-03-2023>
+-- Description:	<Get Courier App copyright and version>
+-- =============================================
+CREATE PROCEDURE [dbo].[spCA_GetVersionData] 
+	
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE @CAVersion NVARCHAR(50);
+	DECLARE @CACopyright NVARCHAR(50);
+    
+	SET @CAVersion = (	SELECT ISNULL([CP].[Value], '') 
+						FROM	[dbo].[ConfigParams] CP
+						WHERE	[CP].[Name] = 'CourierAppVersion');
+
+	SET @CACopyright = (SELECT CONCAT(YEAR(SYSDATETIME()), ' ', ISNULL([CP].[Value], ''))
+						FROM	[dbo].[ConfigParams] CP
+						WHERE	[CP].[Name] = 'CourierAppCopyright');
+
+	SELECT	@CAVersion [CAVersion],
+			@CACopyright [CACopyright];
+END
