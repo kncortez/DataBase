3e51e114d989b6e0b17164256bc3e833693908bf
Oscar Morales <oscar.morales@forzalatam.com>
2023-03-27 16:09:07 -0600

    Date: 2023-03-27 Author: Oscar Morales
    ### CHANGED
    * Se cambio la base de datos en la que se valida el login en sp SetGuideAbandoned.
    
    FDAPI-1560

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetGuideAbandoned.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetGuideAbandoned.sql
@@ -24,31 +24,34 @@ BEGIN
 			@Exists = 1
 		   ,@IsValidPassword =
 			CASE
-				WHEN ru.UsrLastPassword = @Password THEN 1
+				WHEN u.USR_Password = @Password THEN 1
 				ELSE 0
 			END
 		   ,@IsValidRol =
 			CASE
 				WHEN EXISTS (SELECT
-							1
-						FROM RolByUserBySystem rus
-						INNER JOIN CatRol cr
-							ON rus.RusIdRol = cr.RolIdRol
-						INNER JOIN CatSystem cs
-							ON rus.RusIdSystem = cs.SysIdSystem
-						WHERE ru.UsrIdUser = rus.RusIdUser
-						AND rus.RusRowStatus = 1
-						AND cr.RolName = 'Supervisor'
-						AND cs.SysNameSystem = @NameSystem) THEN 1
+								1
+							FROM InternalUser iu
+							INNER JOIN RegisterUser ru
+								ON iu.RegisterUserID = ru.UsrIdUser
+								AND ru.UsrRowStatus = 1
+							INNER JOIN RolByUserBySystem rus
+								ON ru.UsrIdUser = rus.RusIdUser
+							INNER JOIN CatRol cr
+								ON rus.RusIdRol = cr.RolIdRol
+							INNER JOIN CatSystem cs
+								ON rus.RusIdSystem = cs.SysIdSystem
+							WHERE iu.IdUser = @IdUser
+							AND iu.Username = @Username
+							AND iu.RowStatus = 1
+							AND rus.RusRowStatus = 1
+							AND cr.RolName = 'Supervisor'
+							AND cs.SysNameSystem = @NameSystem) THEN 1
 				ELSE 0
 			END
-		FROM InternalUser iu
-		INNER JOIN RegisterUser ru
-			ON iu.RegisterUserID = ru.UsrIdUser
-				AND ru.UsrRowStatus = 1
-		WHERE iu.IdUser = @IdUser
-		AND iu.Username = @Username
-		AND iu.RowStatus = 1
+		FROM DenariusUser_Dev.dbo.LGN_User u
+		WHERE u.USR_IdUser = @IdUser
+		AND u.USR_Username = @Username
 
 		IF (@Exists = 1)
 		BEGIN
