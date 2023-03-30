a12f6b9ca2d6fb67fe58e07d86f4215a35ef4539
AndresRuizPeer <andres.ruiz@forzalatam.com>
2023-03-30 11:47:54 -0600

    Date: 03/03/2023 Author: Jose Andrés Ruíz Peer
    ### ADDED
    * Manejo de cola de ejecución de servicio de facturacíón base

---
+++ DeliveryBackOffice(DEV)/dbo/User Defined Types/TblServiceExecutionProcess.sql
@@ -0,0 +1,8 @@
+﻿CREATE TYPE [dbo].[TblServiceExecutionProcess] AS TABLE (
+    [ServiceExecutionProcessId]           INT            NULL,
+    [ServiceExecutionProcessName]         NVARCHAR (100) NULL,
+    [ServiceExecutionProcessTime]         TIME (7)       NULL,
+    [ServiceExecutionProcessIsPending]    BIT            NULL,
+    [ServiceExecutionProcessHasStarted]   BIT            NULL,
+    [ServiceExecutionProcessHasCompleted] BIT            NULL);
+
