f6fccc2a20cd613f91020f9c438944f7659c26fc
EdelmanV <edelman.vasquez@forzalatam.com>
2023-03-31 18:32:20 -0600

    date:2023-03-31 author:Edelman description: valdiar si guía tiene factura con una nota de credito

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHDValidate.sql
@@ -0,0 +1,29 @@
+﻿-- =============================================
+-- Author:		<Edelman Vásquez>
+-- Create date: <2023-03-31>
+-- Description:	<vALIDAR SI FACTURA TIENE NOTA DE CREDITO>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHDValidate]
+@GuideSerie Nvarchar(2),
+@GuideNumber Int
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+   IF ( EXISTS(SELECT  1 FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
+                     INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
+					 ON IH.inv_pk_id = ID.dti_fk_header
+					 WHERE ID.dti_fk_orderSerie = @GuideSerie AND 
+					 ID.dti_fk_orderNumber = @GuideNumber AND IH.inv_invoiceOfCreditNote IS NULL))
+   BEGIN
+
+   Select 1 'HaveaCreditNote'
+
+   END
+   ELSE
+   BEGIN
+    Select 0 'HaveaCreditNote'
+   END
+
+   END
