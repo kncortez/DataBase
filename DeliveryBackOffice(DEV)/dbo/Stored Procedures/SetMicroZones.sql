e12be9c8d2222026f0008c6bece8c9e2801d0f10
AndresRuizPeer <andres.ruiz@forzalatam.com>
2023-04-03 18:26:15 -0600

    Date: 03/04/2023 Author: Jose Andrés Ruíz Peer
    ### FIXED
    * MAnejo correcto de ingreso de microzonas

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetMicroZones.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetMicroZones.sql
@@ -3,6 +3,7 @@
 -- Create date: <2023-02-23>
 -- Description:	<Registrar microzonas y puntos cargadas desde modulo Microzonas BackOffice>
 -- =============================================
+
 CREATE PROCEDURE SetMicroZones
 @TblMicroZones AS TblMicroZones READONLY,
 @Token AS VARCHAR(50)
@@ -13,35 +14,46 @@ BEGIN
 		 BEGIN TRANSACTION; 
 		   BEGIN TRY
 
-			UPDATE pt1
-			SET pt1.RowStatus = 0
-			FROM dbo.Point pt1 WITH (NOLOCK)
-				INNER JOIN GeofencePoint gfp1 WITH (NOLOCK)
-				 ON pt1.IdPoint = gfp1.IdPoint
-				 INNER JOIN Geofence gfce1 WITH (NOLOCK)
-				 ON gfp1.IdGeofence = gfce1.IdGeofence
-				INNER JOIN @TblMicroZones tbmz1 
-				ON gfce1.GeofenceDescription = tbmz1.NameCol
+			-- Quitar todos los puntos de geocercas "No perdurables"
+			UPDATE 
+				pt1
+			SET 
+				pt1.RowStatus = 0
+			FROM 
+				dbo.Point pt1 WITH (NOLOCK)
+				INNER JOIN 
+					GeofencePoint gfp1 WITH (NOLOCK)
+					ON 
+						pt1.IdPoint = gfp1.IdPoint
+				 INNER JOIN 
+					Geofence gfce1 WITH (NOLOCK)
+					ON 
+						gfp1.IdGeofence = gfce1.IdGeofence
 				WHERE pt1.RowStatus = 1 AND gfp1.RowStatus = 1 AND gfce1.RowStatus = 1 AND gfce1.UpdateFlag = 0
 	
-			UPDATE gpt
-			SET gpt.RowStatus = 0
-			FROM dbo.GeofencePoint gpt WITH (NOLOCK)
-				INNER JOIN Geofence gfce WITH (NOLOCK)
-				 ON gpt.IdGeofence = gfce.IdGeofence
-				INNER JOIN @TblMicroZones tbmz2
-				ON gfce.GeofenceDescription = tbmz2.NameCol
+			-- Quitar la asociación de puntos de geocercas "No perdurables"
+			UPDATE 
+				gpt
+			SET 
+				gpt.RowStatus = 0
+			FROM 
+				dbo.GeofencePoint gpt WITH (NOLOCK)
+				INNER JOIN 
+					Geofence gfce WITH (NOLOCK)
+					ON 
+						gpt.IdGeofence = gfce.IdGeofence
 				WHERE gpt.RowStatus = 1 AND gfce.RowStatus = 1 AND gfce.UpdateFlag = 0
 
-
-			UPDATE gfc
-			SET gfc.RowStatus = 0
-			FROM dbo.Geofence gfc WITH (NOLOCK)
-				INNER JOIN @TblMicroZones tbmz3
-				ON gfc.GeofenceDescription = tbmz3.NameCol
+			-- Quitar geocercas "No perdurables"
+			UPDATE 
+				gfc
+			SET 
+				gfc.RowStatus = 0
+			FROM 
+				dbo.Geofence gfc WITH (NOLOCK)
 				WHERE gfc.RowStatus = 1 AND gfc.UpdateFlag = 0
 
-
+			-- Ingresar nuevas microzonas
 			 INSERT INTO dbo.Geofence
 						(
 						 CountryId
@@ -52,13 +64,13 @@ BEGIN
 						)
 						SELECT DISTINCT
 						'GT'
-						,(SELECT SUBSTRING(tbm.NameMicroZones,1,len(tbm.NameMicroZones) - 1))
+						,[tbm].[NameCol]
 						,1
 						,@Token
 						,GETDATE()
 						FROM @TblMicroZones tbm 
 
-
+				-- Ingresar nuevos puntos de microzonas
 				INSERT INTO dbo.Point
 						(
 						 PointDescription
@@ -70,20 +82,21 @@ BEGIN
 						,TokenUpdated
 						,DateUpdated
 						)
-						SELECT DISTINCT
-						tbmz.NameMicroZones 	
+						SELECT
+						CONCAT(tbmz.[NameCol],'-',[tbmz].[NumCol]) 	
 						,(SELECT CAST ((SUBSTRING(tbmz.MicroZones, CHARINDEX(' ',tbmz.MicroZones)+1, CHARINDEX(' ',tbmz.MicroZones))) AS DECIMAL(15,7)))
 						,(SELECT CAST ((SUBSTRING(tbmz.MicroZones, 1, CHARINDEX(' ',tbmz.MicroZones))) AS DECIMAL(15,7)))
-						--,(SELECT CAST ((SUBSTRING(tbmz.MicroZones,1,12)) AS DECIMAL(15,7)))
 						,1
 						,@Token
 						,GETDATE()
 						,null
 						,null
 						FROM @TblMicroZones tbmz
+						ORDER BY
+							[tbmz].[NameCol] ASC,
+							[tbmz].[NumCol] ASC
 
-				
-
+					-- Relacionar nuevos puntos de microzonas con geocercas
 					INSERT INTO dbo.GeofencePoint
 								(
 								IdGeofence
@@ -94,24 +107,28 @@ BEGIN
 								,DateCreated
 								)
 								SELECT
-								gf.IdGeofence
-								,IIF((SELECT Max(tb2.NumCol) FROM @TblMicroZones tb2 WHERE tb2.NameCol = tb.NameCol) = tb.NumCol,pt.IdPoint,pt.IdPoint)
-								--,IIF((Select Max(tb2.NumCol) from @TblMicroZones tb2 where tb2.NameCol = tb.NameCol) = tb.NumCol,(Select MIN(pt2.IdPoint) FROM Point pt2 where SUBSTRING(pt2.PointDescription,1,len(tb.NameMicroZones)-1) = SUBSTRING(pt.PointDescription,1,len(tb.NameMicroZones)-1)),pt.IdPoint)
-								,IIF((Count(SUBSTRING(tb.NameMicroZones,1,len(tb.NameMicroZones) - 1))) =(SUBSTRING(tb.NameMicroZones,len(tb.NameMicroZones),1)),(tb.NumCol),(tb.NumCol))
-								,1
-								,@Token
-								,GETDATE()
-								FROM Geofence gf WITH (NOLOCK)
-								INNER JOIN @TblMicroZones tb
-								ON gf.GeofenceDescription = (SELECT SUBSTRING(tb.NameMicroZones,1,len(tb.NameMicroZones) - 1))
-								AND gf.UpdateFlag = 0
-								INNER JOIN Point pt WITH (NOLOCK)
-								ON pt.PointDescription = tb.NameMicroZones
-								AND pt.PointFlag = 0
-								WHERE gf.RowStatus = 1 AND pt.RowStatus = 1
+									gf.IdGeofence
+									,pt.IdPoint
+									,tb.NumCol
+									,1
+									,@Token
+									,GETDATE()
+								FROM 
+									Geofence gf WITH (NOLOCK)
+									INNER JOIN 
+										@TblMicroZones tb
+										ON gf.GeofenceDescription = tb.[NameCol]
+									INNER JOIN 
+										Point pt WITH (NOLOCK)
+									ON 
+										pt.PointDescription = CONCAT(tb.[NameCol],'-',tb.[NumCol])
+										AND 
+										pt.PointFlag = 0
+								WHERE gf.UpdateFlag = 0 AND gf.RowStatus = 1 AND pt.RowStatus = 1
 								GROUP BY pt.IdPoint,pt.PointDescription,gf.IdGeofence, tb.NameMicroZones, tb.NumCol, tb.NameCol
 
-					INSERT INTO dbo.GeofencePoint
+						-- Finalizar poligono reingresando primer punto de microzona
+						INSERT INTO dbo.GeofencePoint
 								(
 								IdGeofence
 								,IdPoint
@@ -121,21 +138,46 @@ BEGIN
 								,DateCreated
 								)
 								SELECT
-								gf.IdGeofence
-								,IIF((SELECT Max(tb2.NumCol) FROM @TblMicroZones tb2 WHERE tb2.NameCol = tb.NameCol) = tb.NumCol,(SELECT MIN(pt2.IdPoint) FROM Point pt2 WITH (NOLOCK) WHERE SUBSTRING(pt2.PointDescription,1,len(tb.NameMicroZones)-1) = SUBSTRING(pt.PointDescription,1,len(tb.NameMicroZones)-1)AND pt2.RowStatus = 1 AND pt2.PointFlag = 0),pt.IdPoint)
-								,IIF((Count(SUBSTRING(tb.NameMicroZones,1,len(tb.NameMicroZones) - 1))) =(SUBSTRING(tb.NameMicroZones,len(tb.NameMicroZones),1)),(tb.NumCol+1),(tb.NumCol+1))
-								,1
-								,@Token
-								,GETDATE()
-								FROM Geofence gf WITH (NOLOCK)
-								INNER JOIN @TblMicroZones tb
-								ON gf.GeofenceDescription = (SELECT SUBSTRING(tb.NameMicroZones,1,len(tb.NameMicroZones) - 1))
-								AND gf.UpdateFlag = 0
-								INNER JOIN Point pt WITH (NOLOCK)
-								ON pt.PointDescription = tb.NameMicroZones
-								AND pt.PointFlag = 0
-								WHERE gf.RowStatus = 1 AND pt.RowStatus = 1 AND (SELECT MAX(tb2.NumCol) FROM @TblMicroZones tb2 WHERE tb2.NameCol = tb.NameCol) = tb.NumCol
+									gf.IdGeofence
+									,pt.IdPoint
+									,
+									(
+										SELECT 
+											TOP (1) 
+												MAX(GP.[GeofencePointOrder]) + 1 
+										FROM 
+											[dbo].[GeofencePoint] GP  WITH(NOLOCK) 
+										WHERE
+											[GP].[IdGeofence] = [gf].[IdGeofence]
+									)
+									,1
+									,@Token
+									,GETDATE()
+								FROM 
+									Geofence gf WITH (NOLOCK)
+									INNER JOIN 
+										@TblMicroZones tb
+										ON gf.GeofenceDescription = tb.[NameCol]
+									INNER JOIN 
+										Point pt WITH (NOLOCK)
+									ON 
+										pt.PointDescription = CONCAT(tb.[NameCol],'-',tb.[NumCol])
+										AND 
+										pt.PointFlag = 0
+								WHERE 
+									gf.UpdateFlag = 0 
+									AND 
+									gf.RowStatus = 1 
+									AND 
+									pt.RowStatus = 1 
+									AND 
+									tb.[NumCol] = 1
 								GROUP BY pt.IdPoint,pt.PointDescription,gf.IdGeofence, tb.NameMicroZones, tb.NumCol, tb.NameCol
+
+
+				COMMIT TRANSACTION;
+				SELECT 'Datos cargados exitosamente' AS message
+				SELECT *FROM Geofence WITH (NOLOCK)
 
 
 			END TRY
@@ -147,15 +189,6 @@ BEGIN
 			ROLLBACK TRANSACTION;
 
 			END CATCH;
-
-		IF @@trancount > 0
-		BEGIN
-
-			COMMIT TRANSACTION;
-			SELECT 'Datos cargados exitosamente' AS message
-			SELECT IdGeofence,CountryId,GeofenceDescription, RowStatus FROM Geofence WITH (NOLOCK) WHERE RowStatus = 1 AND UpdateFlag = 0
-
-		END;
 
 					
 END
