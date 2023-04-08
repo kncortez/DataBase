85502883531453d9b183e05984c86b3d3db65fd2
AndresRuizPeer <andres.ruiz@forzalatam.com>
2023-04-08 11:22:21 -0600

    Date: 08/04/2023 Author: Jose Andrés Ruíz Peer
    ### ADDED
    * Indicativo si guía fue despachada en linehaul o fue adicionada desde liquidación (Guías no despachadas en ruta)

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetManifestLinehaulDetail.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetManifestLinehaulDetail.sql
@@ -102,55 +102,174 @@ BEGIN
 	AND lrp.RowStatus = 1
 
 	-- Table 1 Detalle manifiesto
-	SELECT
-		CONCAT(lrpcd.GuideSerie, lrpcd.GuideNumber) Guide
-	   ,CONCAT((lrpcd.DryPieceQuantity + lrpcd.ColdPieceQuantity), '/', (lrpcd.GuideDryPieceTotal + lrpcd.GuideColdPieceTotal)) Pieces
-	   ,c.ContainerDescription ContainerDescription
-	   ,hl.HubName
-	   ,hl.HubAbbreviation
-	   ,(SELECT TOP 1
-				p.ProvinceName
-			FROM DumpServiceCoverage dsc WITH (NOLOCK)
-			INNER JOIN Settlement s WITH (NOLOCK)
-				ON s.IdSettlement = dsc.IdSettlement
-			INNER JOIN Province p WITH (NOLOCK)
-				ON s.IdProvince = p.IdProvince
-			WHERE dsc.Hub = hl.HubAbbreviation)
-		Department
-	   ,IIF((lrpcd.DryPieceQuantity + lrpcd.ColdPieceQuantity) = (lrpcd.GuideDryPieceTotal + lrpcd.GuideColdPieceTotal), 0, 1) IsParcial
-	   ,so.OrderDescription StatusDescription
-	   ,ad.ActId Act
-	   ,CASE
-			WHEN ad.ActId IS NOT NULL THEN (SELECT
-						STUFF((SELECT
-								', ' + CAST(adp.PieceNumber AS VARCHAR)
-							FROM ActDetail ad2 WITH (NOLOCK)
-							INNER JOIN ActDetailPiece adp WITH (NOLOCK)
-								ON ad2.IdActDetail = adp.ActDetailId
-							WHERE ad2.IdActDetail = ad.ActId
-							FOR XML PATH (''))
-						, 1, 2, ''))
-			ELSE NULL
-		END ActPieces
-
-	FROM LinehaulRoutePreparationContainer lrpc WITH (NOLOCK)
-	INNER JOIN LinehaulRoutePreparationContainerDetail lrpcd WITH (NOLOCK)
-		ON lrpcd.LinehaulRoutePreparationContainerId = lrpc.IdLinehaulRoutePreparationContainer
-			AND lrpcd.RowStatus = 1
-	INNER JOIN Container c WITH (NOLOCK)
-		ON c.IdContainer = lrpc.ContainerId
-	INNER JOIN HubLogistics hl WITH (NOLOCK)
-		ON hl.IdHubLogistic = lrpc.HubDestinyId
-	INNER JOIN DeliveryOrder do WITH (NOLOCK)
-		ON lrpcd.GuideSerie = do.Guide_Serie
-			AND lrpcd.GuideNumber = do.Guide_Number
-	INNER JOIN StatusOrder so WITH (NOLOCK)
-		ON do.StatusOrderId = so.StatusOrderId
-	LEFT JOIN ActDetail ad WITH (NOLOCK)
-		ON lrpcd.GuideSerie = ad.GuideSerie
-			AND lrpcd.GuideNumber = ad.GuideNumber
-			AND ad.RowStatus = 1
-	WHERE lrpc.LinehaulRoutePreparationId = @IdLinehaulRoutePreparation
-	AND lrpc.RowStatus = 1
-	ORDER BY hl.HubName
+	SELECT 
+		[DispatchedLienahul].[Guide]
+		,[DispatchedLienahul].[Pieces]
+		,[DispatchedLienahul].[ContainerDescription]
+		,[DispatchedLienahul].[HubName]
+		,[DispatchedLienahul].[HubAbbreviation]
+		,[DispatchedLienahul].[Department]
+		,[DispatchedLienahul].[IsParcial]
+		,[DispatchedLienahul].[StatusDescription]
+		,[DispatchedLienahul].[Act]
+		,[DispatchedLienahul].[ActPieces]
+		,[DispatchedLienahul].[IsNotInManifest]
+	FROM
+		(
+			SELECT
+				TOP 100 PERCENT
+					CONCAT(lrpcd.GuideSerie, lrpcd.GuideNumber) Guide
+				   ,CONCAT((lrpcd.DryPieceQuantity + lrpcd.ColdPieceQuantity), '/', (lrpcd.GuideDryPieceTotal + lrpcd.GuideColdPieceTotal)) Pieces
+				   ,c.ContainerDescription ContainerDescription
+				   ,hl.HubName
+				   ,hl.HubAbbreviation
+				   ,(SELECT TOP 1
+							p.ProvinceName
+						FROM DumpServiceCoverage dsc WITH (NOLOCK)
+						INNER JOIN Settlement s WITH (NOLOCK)
+							ON s.IdSettlement = dsc.IdSettlement
+						INNER JOIN Province p WITH (NOLOCK)
+							ON s.IdProvince = p.IdProvince
+						WHERE dsc.Hub = hl.HubAbbreviation)
+					Department
+				   ,IIF((lrpcd.DryPieceQuantity + lrpcd.ColdPieceQuantity) = (lrpcd.GuideDryPieceTotal + lrpcd.GuideColdPieceTotal), 0, 1) IsParcial
+				   ,so.OrderDescription StatusDescription
+				   ,ad.ActId Act
+				   ,CASE
+						WHEN ad.ActId IS NOT NULL THEN (SELECT
+									STUFF((SELECT
+											', ' + CAST(adp.PieceNumber AS VARCHAR)
+										FROM ActDetail ad2 WITH (NOLOCK)
+										INNER JOIN ActDetailPiece adp WITH (NOLOCK)
+											ON ad2.IdActDetail = adp.ActDetailId
+										WHERE ad2.IdActDetail = ad.ActId
+										FOR XML PATH (''))
+									, 1, 2, ''))
+						ELSE NULL
+					END ActPieces
+					,0 [IsNotInManifest]
+			FROM LinehaulRoutePreparationContainer lrpc WITH (NOLOCK)
+			INNER JOIN LinehaulRoutePreparationContainerDetail lrpcd WITH (NOLOCK)
+				ON lrpcd.LinehaulRoutePreparationContainerId = lrpc.IdLinehaulRoutePreparationContainer
+					AND lrpcd.RowStatus = 1
+			INNER JOIN Container c WITH (NOLOCK)
+				ON c.IdContainer = lrpc.ContainerId
+			INNER JOIN HubLogistics hl WITH (NOLOCK)
+				ON hl.IdHubLogistic = lrpc.HubDestinyId
+			INNER JOIN DeliveryOrder do WITH (NOLOCK)
+				ON lrpcd.GuideSerie = do.Guide_Serie
+					AND lrpcd.GuideNumber = do.Guide_Number
+			INNER JOIN StatusOrder so WITH (NOLOCK)
+				ON do.StatusOrderId = so.StatusOrderId
+			LEFT JOIN ActDetail ad WITH (NOLOCK)
+				ON lrpcd.GuideSerie = ad.GuideSerie
+					AND lrpcd.GuideNumber = ad.GuideNumber
+					AND ad.RowStatus = 1
+			WHERE lrpc.LinehaulRoutePreparationId = @IdLinehaulRoutePreparation
+			AND lrpc.RowStatus = 1
+			ORDER BY hl.HubName
+		) DispatchedLienahul
+	UNION
+	SELECT 
+		 [ExtraGuidesLinehaul].[Guide]
+		 ,[ExtraGuidesLinehaul].[Pieces]
+		 ,[ExtraGuidesLinehaul].[ContainerDescription]
+		 ,[ExtraGuidesLinehaul].[HubName]
+		 ,[ExtraGuidesLinehaul].[HubAbbreviation]
+		 ,[ExtraGuidesLinehaul].[Department]
+		 ,[ExtraGuidesLinehaul].[IsParcial]
+		 ,[ExtraGuidesLinehaul].[StatusDescription]
+		 ,[ExtraGuidesLinehaul].[Act]
+		 ,[ExtraGuidesLinehaul].[ActPieces]
+		 ,[ExtraGuidesLinehaul].[IsNotInManifest]
+	FROM
+		(
+			SELECT 
+				TOP 100 PERCENT
+					CONCAT([LRSCD].GuideSerie, [LRSCD].GuideNumber) Guide
+					,CONCAT(([LRSCD].[PiecesReceived]), '/', (DO.[Pieces_Dry] + DO.[Pieces_Cold])) Pieces
+					,[Ctn].ContainerDescription ContainerDescription
+					,hl.HubName
+					,hl.HubAbbreviation
+					,(SELECT TOP 1
+							p.ProvinceName
+						FROM DumpServiceCoverage dsc WITH (NOLOCK)
+						INNER JOIN Settlement s WITH (NOLOCK)
+							ON s.IdSettlement = dsc.IdSettlement
+						INNER JOIN Province p WITH (NOLOCK)
+							ON s.IdProvince = p.IdProvince
+						WHERE dsc.Hub = hl.HubAbbreviation)
+					Department
+					,0 IsParcial
+					,so.OrderDescription StatusDescription
+					,ad.ActId Act
+					,CASE
+						WHEN ad.ActId IS NOT NULL THEN (SELECT
+									STUFF((SELECT
+											', ' + CAST(adp.PieceNumber AS VARCHAR)
+										FROM ActDetail ad2 WITH (NOLOCK)
+										INNER JOIN ActDetailPiece adp WITH (NOLOCK)
+											ON ad2.IdActDetail = adp.ActDetailId
+										WHERE ad2.IdActDetail = ad.ActId
+										FOR XML PATH (''))
+									, 1, 2, ''))
+						ELSE NULL
+					END ActPieces
+					,1 [IsNotInManifest]
+				FROM
+					-- Datos de despacho de linehaul
+					[dbo].[LinehaulRoutePreparation] LRP  WITH(NOLOCK) 
+					INNER JOIN
+						[dbo].[LinehaulRoutePreparationContainer] LRPC  WITH(NOLOCK) 
+						ON
+							[LRPC].[LinehaulRoutePreparationId] = [LRP].[IdLinehaulRoutePreparation]
+							AND
+							[LRPC].[RowStatus] = 1
+					-- Revisar liquidación por guías adicionales, se debe validar datos de despacho con datos de liquidación para solo mostrar datos adicionales
+					INNER JOIN
+						[dbo].[LinehaulRouteSettlement] LRS  WITH(NOLOCK) 
+						ON
+							[LRS].[LinehaulRoutePreparationId] = [LRP].[IdLinehaulRoutePreparation]
+							AND
+							[LRS].[RowStatus] = 1
+					INNER JOIN
+						[dbo].[LinehaulRouteSettlementContainer] LRSC  WITH(NOLOCK) 
+						ON
+							[LRSC].[LinehaulRouteSettlementId] = [LRS].[IdLinehaulRouteSettlement]
+							AND
+							[LRSC].[ContainerId] <> [LRPC].[ContainerId]
+							AND
+							[LRSC].[RowStatus] = 1
+					INNER JOIN
+						[dbo].[LinehaulRouteSettlementContainerDetail] LRSCD  WITH(NOLOCK) 
+						ON
+							[LRSCD].[LinehaulRouteSettlementContainerId] = [LRSC].[IdLinehaulRouteSettlementContainer]
+							AND
+							[LRSCD].[RowStatus] = 1
+					INNER JOIN
+						[dbo].[Container] Ctn  WITH(NOLOCK) 
+						ON
+							[Ctn].[IdContainer] = [LRSC].[ContainerId]
+					INNER JOIN
+						[dbo].[HubLogistics] HL  WITH(NOLOCK) 
+						ON
+							[HL].[IdHubLogistic] = [LRSC].[HubId]
+					INNER JOIN
+						[dbo].[DeliveryOrder] DO  WITH(NOLOCK) 
+						ON
+							[DO].[Guide_Serie] = [LRSCD].[GuideSerie] AND [DO].[Guide_Number] = [LRSCD].[GuideNumber]
+					INNER JOIN
+						[dbo].[StatusOrder] SO  WITH(NOLOCK) 
+						ON
+							[SO].[StatusOrderId] = [DO].[StatusOrderId]
+					LEFT JOIN
+						[dbo].[ActDetail] AD  WITH(NOLOCK) 
+						ON
+							[AD].[GuideSerie] = [LRSCD].[GuideSerie] AND [AD].[GuideNumber] = [LRSCD].[GuideNumber]
+							AND
+							[AD].[RowStatus] = 1
+				WHERE
+					[LRP].[IdLinehaulRoutePreparation] = @IdLinehaulRoutePreparation
+				ORDER BY HL.HubName
+		) ExtraGuidesLinehaul
 END
