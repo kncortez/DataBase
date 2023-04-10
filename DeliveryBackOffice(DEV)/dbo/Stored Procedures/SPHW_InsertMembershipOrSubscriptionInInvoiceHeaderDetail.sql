b6391a3639be2dbba85f24da353e227a8631e109
Eduardo30 <vicente.lopez@forzalatam.com>
2023-04-10 17:31:46 -0600

    Date: 2023-04-10 Author: Eduardo López ### CHANGED * Realizar cambios en sp para almacenar tipo de factura membresias(Otros)

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_InsertMembershipOrSubscriptionInInvoiceHeaderDetail.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_InsertMembershipOrSubscriptionInInvoiceHeaderDetail.sql
@@ -209,6 +209,9 @@ BEGIN
 
         END;
 
+		DECLARE @IdTypeFac INT;
+		SET @IdTypeFac = (SELECT IdCatInvoiceType FROM CatInvoiceType WHERE NAME = 'Otros')
+
 
         INSERT INTO [dbo].[invoiceHeader]
         (
@@ -224,11 +227,12 @@ BEGIN
             inv_status,
             inv_dateRegister,
             inv_tokenRegister,
-            inv_type
+            inv_type,
+			CatInvoiceTypeId
         )
         VALUES
         (@inv_vpCodeOfReferences, @inv_cmp_nit, @inv_cli_name, @inv_cli_adress, @inv_cli_nit, @inv_cli_email,
-         @inv_date, @inv_IVA, @inv_amount, @inv_status, @inv_dateRegister, @inv_tokenRegister, 1);
+         @inv_date, @inv_IVA, @inv_amount, @inv_status, @inv_dateRegister, @inv_tokenRegister, 1,@IdTypeFac);
 
 
 
