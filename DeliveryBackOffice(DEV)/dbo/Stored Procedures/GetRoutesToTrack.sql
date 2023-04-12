565f783c8eb11982ca11a9af3c9a39605d40c710
AndresRuizPeer <andres.ruiz@forzalatam.com>
2023-04-12 16:00:29 -0600

    Date: 12/04/2023 Author: Jose Andrés Ruíz Peer
    ### ADDED
    * Fila de suma de totales en monitoreo de rutas de entrega

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetRoutesToTrack.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetRoutesToTrack.sql
@@ -5,9 +5,9 @@
 -- Description:	< Retorna las rutas de un día tomando en cuenta el hub donde se genero el manifiesto >
 -- =============================================
 CREATE PROCEDURE [dbo].[GetRoutesToTrack]
-    @Date DATE,
-    @Hub TblExtPlatTextParameterList READONLY,
-    @ServiceType BIGINT = 2
+	@Date DATE,
+	@Hub TblExtPlatTextParameterList READONLY,
+	@ServiceType BIGINT = 2
 AS
 BEGIN
 
@@ -37,7 +37,8 @@ BEGIN
                       ELSE
                           ISNULL(DO.Collect_OnDelivery, 0)
                   END
-              ) 'TotalToCharge'
+              ) 'TotalToCharge',
+			  0 'OrderWeight'
     FROM [DeliveryBackOffice].[dbo].[DeliveryOrderBySettlement] DOBS WITH (NOLOCK)
         LEFT JOIN [DeliveryBackOffice].[dbo].[DeliverySettlementDetail] DSD WITH (NOLOCK)
             ON DOBS.ID = DSD.ID_DeliveryOrderBySettlement
@@ -69,21 +70,76 @@ BEGIN
                AND DSD.Guide_Number = DO2.Guide_Number
                AND DO2.StatusOrderId IN ( 5, 24, 25 ) -- Entregado|COD Liquidado|COD Pagado
     WHERE CAST(DOBS.Date_Dispatched AS DATE) = @Date
-          AND REPLACE(ISNULL(HL.HubAbbreviation, 'N/A'), ' ', '')IN
+          AND REPLACE(ISNULL(HL.HubAbbreviation, 'N/A'), ' ', '') IN
               (
                   SELECT REPLACE(TextParameter, ' ', '')FROM @Hub
-              ) /*
-	AND
-	(
-		REPLACE(ISNULL(DO.Courier_Route,'N/A'),' ','') = 'N/A'
-		OR
-		LEFT(REPLACE(ISNULL(DO.Courier_Route,'N/A'),' ',''),1) = 'U'
-	)*/
+              )
     GROUP BY SR.First_Name,
              SR.Last_Name,
              HL.HubAbbreviation,
-             --,DO.Courier_Route
-             DOBS.ID;
+             DOBS.ID
+	UNION
+	SELECT '---' 'Courier',
+           '---' 'Hub',
+           '---' 'Route',
+           0 'Manifest',
+           COUNT(DISTINCT DO.Guide_Number) 'TotalServices',
+           SUM(ISNULL(DOA.TotalCount, 0)) 'TotalAlerts',
+           COUNT(DISTINCT DO2.Guide_Number) 'TotalSuccessfulDeliveries',
+           SUM(   CASE
+                      WHEN DO2.IsCollect = 1 THEN
+                          ISNULL(DO2.PriceShippment, 0) + ISNULL(DO2.Collect_OnDelivery, 0)
+                      ELSE
+                          ISNULL(DO2.Collect_OnDelivery, 0)
+                  END
+              ) 'TotalConfirmedCharge',
+           SUM(   CASE
+                      WHEN DO.IsCollect = 1 THEN
+                          ISNULL(DO.PriceShippment, 0) + ISNULL(DO.Collect_OnDelivery, 0)
+                      ELSE
+                          ISNULL(DO.Collect_OnDelivery, 0)
+                  END
+              ) 'TotalToCharge',
+			  1 'OrderWeight'
+    FROM [DeliveryBackOffice].[dbo].[DeliveryOrderBySettlement] DOBS WITH (NOLOCK)
+        LEFT JOIN [DeliveryBackOffice].[dbo].[DeliverySettlementDetail] DSD WITH (NOLOCK)
+            ON DOBS.ID = DSD.ID_DeliveryOrderBySettlement
+               AND DSD.RowStatus = 1
+        LEFT JOIN [DeliveryBackOffice].[dbo].[SenderReceiver] SR WITH (NOLOCK)
+            ON DOBS.ID_Courier = SR.ID
+        INNER JOIN [DeliveryBackOffice].[dbo].[DeliveryOrder] DO WITH (NOLOCK)
+            ON DSD.Guide_Serie = DO.Guide_Serie
+               AND DSD.Guide_Number = DO.Guide_Number
+        LEFT JOIN [DeliveryBackOffice].[dbo].[CatStation] CS WITH (NOLOCK)
+            ON DOBS.DispatchedStationId = CS.IdStation
+        LEFT JOIN [DeliveryBackOffice].[dbo].[HubLogistics] HL WITH (NOLOCK)
+            ON CS.HubLogisticId = HL.IdHubLogistic
+        LEFT JOIN
+        (
+            SELECT DOAA.GuideSerie,
+                   DOAA.GuideNumber,
+                   COUNT(DISTINCT DOAA.IdDeliveryOrderAlert) 'TotalCount'
+            FROM [DeliveryBackOffice].[dbo].[DeliveryOrderAlert] DOAA WITH (NOLOCK)
+            WHERE DOAA.ServiceTypeId = 2 -- Entrega
+                  AND DOAA.RowStatus = 1
+            GROUP BY DOAA.GuideSerie,
+                     DOAA.GuideNumber
+        ) DOA
+            ON DSD.Guide_Serie = DOA.GuideSerie
+               AND DSD.Guide_Number = DOA.GuideNumber
+        LEFT JOIN [DeliveryBackOffice].[dbo].[DeliveryOrder] DO2 WITH (NOLOCK) -- Guías las cuales se confirma su entrega y cobro
+            ON DSD.Guide_Serie = DO2.Guide_Serie
+               AND DSD.Guide_Number = DO2.Guide_Number
+               AND DO2.StatusOrderId IN ( 5, 24, 25 ) -- Entregado|COD Liquidado|COD Pagado
+    WHERE CAST(DOBS.Date_Dispatched AS DATE) = @Date
+          AND REPLACE(ISNULL(HL.HubAbbreviation, 'N/A'), ' ', '') IN
+              (
+                  SELECT REPLACE(TextParameter, ' ', '')FROM @Hub
+              )
+	ORDER BY
+		[OrderWeight] ASC,
+		[Courier] ASC,
+		[Hub] ASC,
+		[Route] ASC;
 
-
-END;
+END
