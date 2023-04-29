8c1d7e55d284d1659354c032cf5dbe58290f83b8
Oscar Morales <oscar.morales@forzalatam.com>
2023-04-28 23:44:42 -0600

    Date: 2023-04-28 Author: Oscar Morales
    ### ADDED
    * Agregar a servicio el proceso de actualización de información de facturas ya procesadas.
    
    Historia: FDAPI-1623

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetInvoiceProcessedToUpdate.sql
@@ -0,0 +1,29 @@
+﻿-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <2023-04-28>
+-- Description:	<Obtiene las facturas para el proceso UpdateInvoiceProcessed en el servicio HermesInvoiceHelper para actualizar datos de facturas procesadas>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetInvoiceProcessedToUpdate]
+AS
+BEGIN
+	BEGIN TRY
+		DECLARE @DaysFrom INT = 4 --Días desde donde se obtienen las guías.
+
+		SELECT
+			'1' 'StatusCode'
+		   ,'Datos obtenidos correctamente.' 'Description'
+
+		SELECT
+			inv_pk_id InvoiceId
+		FROM invoiceHeader WITH (NOLOCK)
+		WHERE inv_descriptionFEL = 'Fallo la ejecucion del comando: [POST_DOCUMENTGT], TrCode: [9], description: [Ya existe el Documento con el NIT, codigo establecimiento, tipo de documento y IDInterno, no se puede insertar un documento duplicado]'
+		AND CAST(inv_dateRegister AS DATE) >= CAST(GETDATE() - @DaysFrom AS DATE)
+
+	END TRY
+	BEGIN CATCH
+		
+		SELECT
+			'-1' 'StatusCode'
+		   ,ERROR_MESSAGE() 'Description'
+	END CATCH
+END
---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetInvoiceProcessedToUpdate.sql
@@ -0,0 +1,37 @@
+﻿-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <2023-04-28>
+-- Description:	<Guarda información de facturas que ya se han procesado>
+-- =============================================
+CREATE PROCEDURE [dbo].[SetInvoiceProcessedToUpdate]
+	@id bigint,
+	@certificationFEL varchar(200),
+	@serieFEL varchar(200),
+	@numberFEL varchar(50),
+	@descriptionFEL varchar(500),
+	@status int
+AS
+BEGIN
+	BEGIN TRY
+
+			UPDATE DeliveryBackOffice.dbo.invoiceHeader
+			SET inv_certificationFEL = @certificationFEL
+			   ,inv_serieFEL = @serieFEL
+			   ,inv_numberFEL = @numberFEL
+			   ,inv_descriptionFEL = @descriptionFEL
+			   ,inv_dateUpdate = GETDATE()
+			   ,inv_dateFEL = GETDATE(),
+			   inv_status = @status
+			WHERE inv_pk_id = @id
+
+			SELECT
+			'1' 'StatusCode'
+		   ,'Datos almacenados correctamente.' 'Description'
+	END TRY
+	BEGIN CATCH
+		
+		SELECT
+			'-1' 'StatusCode'
+		   ,ERROR_MESSAGE() 'Description'
+	END CATCH
+END
