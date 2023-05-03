c2186a970d1488453087a295ed89463fff7b9641
Jerson Ochoa <jerson.ochoa@forzadelivery.com>
2023-05-03 13:10:40 -0600

    Date: 03-05-2023 Author: Jerson Ochoa ### ADDED * sp GetRouteData [FDAPI-1398]

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetRouteData.sql
@@ -0,0 +1,28 @@
+﻿-- =============================================
+-- Author:		<Jerson Ochoa>
+-- Create date: <12-04-2023>
+-- Description:	<Get route and CatTypeRoute data>
+-- =============================================
+CREATE PROCEDURE [dbo].[spHM_GetRouteData]
+	@CodeRoute AS NVARCHAR(15)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    SELECT		[CR].[IdRoute],
+				[CR].[CodeRoute],
+				[CR].[Description],
+				[CR].[IdTownship],
+				[T].[TownshipName],
+				[CR].[IdTypeRoute],
+				[CTR].[Name] [Type]
+	FROM		[dbo].[CatRoute] CR
+	INNER JOIN	[dbo].[Township] T
+		ON		[CR].[IdTownship] = [T].[IdTownship]
+	INNER JOIN	[dbo].[CatTypeRoute] CTR
+		ON		[CR].[IdTypeRoute] = [CTR].[IdTypeRoute]
+	WHERE	[CR].[CodeRoute] = @CodeRoute 
+		AND [CR].[RowStatus] = 1;
+END
