b53c853f348eebf03220819237396a9e004c5ecf
EdelmanV <edelman.vasquez@forzalatam.com>
2023-05-04 15:01:56 -0600

    Date:2023-05-04 author:Edelman description: Método para registro de ubicación de Courier

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/CouriermanLocationRecord.sql
@@ -0,0 +1,59 @@
+﻿-- =============================================
+-- Author:		<Author,Edelman>
+-- Create date: <Create Date, 2023-04-28>
+-- Description:	<Description, Método para registro de ubicación de Courier>
+-- =============================================
+CREATE PROCEDURE [dbo].[CouriermanLocationRecord] 
+	@CourierIdentifier   AS INT,
+    @CourierLatitude AS VARCHAR,
+    @CourierLength AS INT,
+    @LocationAccuracy  AS VARCHAR
+AS
+BEGIN
+	
+
+	BEGIN TRANSACTION
+	BEGIN TRY
+
+			INSERT INTO [dbo].[SenderReceiverLocationLog]
+			(
+			 SenderReceiverId,
+			 CourierLatitude,
+			 CourierLongitude,
+			 LocationAccuracy,
+			 LocationDate,
+			 LocationTime,
+			 RowStatus,
+			 DateCreated,
+			 TokenCreated
+	
+			 )
+			 Values
+			 (
+			 @CourierIdentifier,
+			 @CourierLatitude,
+			 @CourierLength,
+			 @LocationAccuracy,
+			 CONVERT (date, GETDATE()),
+			 CONVERT(VARCHAR, getdate(), 108),
+			 1,
+			 GETDATE(),
+			 'Courierman Location Record'
+			 )
+
+			 SELECT 1 AS 'Result' 
+
+	COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+
+	ROLLBACK TRANSACTION
+
+		 SELECT 1 AS 'Result' 
+
+    END CATCH
+
+
+
+
+END
