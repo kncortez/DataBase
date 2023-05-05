0dd3d698e6d90fa15f60d11f4fdd83354022b7b8
EdelmanV <edelman.vasquez@forzalatam.com>
2023-05-04 21:14:03 -0600

    Date:2023-05-04 author:Edelman description: Nueva estructura de carrito de compras para nuevo portal web de express center

---
+++ DeliveryBackOffice(DEV)/dbo/Tables/ExpressAccountServiceCart.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ExpressAccountServiceCart] (
+    [IdExpressAccountServiceCart] BIGINT        IDENTITY (1, 1) NOT NULL,
+    [AccountId]                   BIGINT        NOT NULL,
+    [CustomerId]                  INT           NULL,
+    [CustomerPortfolioId]         INT           NULL,
+    [IsPending]                   BIT           NOT NULL,
+    [RowStatus]                   BIT           NOT NULL,
+    [DateCreated]                 DATETIME      NOT NULL,
+    [TokenCreated]                NVARCHAR (50) NOT NULL,
+    [DateUpdated]                 DATETIME      NULL,
+    [TokenUpdated]                NVARCHAR (50) NULL,
+    CONSTRAINT [PK_ExpressAccountServiceCart] PRIMARY KEY CLUSTERED ([IdExpressAccountServiceCart] ASC)
+);
+
