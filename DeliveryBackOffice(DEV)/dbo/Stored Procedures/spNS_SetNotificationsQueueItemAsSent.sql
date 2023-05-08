0e72924c379e5c14a6483c3adad1c4742c09d4bc
Jerson Ochoa <jerson.ochoa@forzadelivery.com>
2023-05-08 12:41:45 -0600

    Date: 08-05-2023 Author: Jerson Ochoa ### ADDED * Notifications service [FDAPI-1598]

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spNS_SetNotificationsQueueItemAsSent.sql
@@ -0,0 +1,36 @@
+﻿-- =============================================
+-- Author:		<Jerson Ochoa>
+-- Create date: <04-05-2023>
+-- Description:	<Set a notification queue item as sent>
+-- =============================================
+CREATE PROCEDURE [dbo].[spNS_SetNotificationsQueueItemAsSent]
+	@NotificationQueueId AS INT, 
+	@Token AS NVARCHAR(100)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION
+	BEGIN TRY
+		
+		UPDATE	[dbo].[NotificationQueue]
+		SET		[IsSent] = 1,
+				[TokenUpdated] = @Token,
+				[DateUpdated] = SYSDATETIME()
+		WHERE	[IdNotificationQueue] = @NotificationQueueId;
+
+		IF @@TRANCOUNT > 0 COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+
+		SELECT  
+			ERROR_NUMBER() AS spResult  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS spMessage;
+		
+		ROLLBACK TRANSACTION;
+
+	END CATCH
+END
