459ce59a4058316891adef549cf889d55d80439b
Alberto Ixchop <albertokanecixchop@gmail.com>
2023-05-10 17:42:42 -0600

    Merged feature/FDD-1038-Integration

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphdGetCustomer.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphdGetCustomer.sql
@@ -17,11 +17,11 @@ BEGIN
     BEGIN
         --First Catalog UI MgtCustomer
         SELECT CAST(cst.IdCustomer AS NVARCHAR) [IdValue],
-               UPPER(cst.Name) + ' ' + '[' + cst.Abbreviation + ']' [NameValue],
+               IIF(cst.RowSatus = 0, '[INACTIVO] ', '') + UPPER(cst.Name) + ' ' + '[' + cst.Abbreviation + ']' [NameValue],
                cst.CountryID [IdFilter]
         FROM Customer cst
         WHERE cst.IdCustomerType != 3 --todos excepto el portal 3
-              AND cst.RowSatus = 'TRUE'
+              --AND cst.RowSatus = 'TRUE'
               AND
               (
                   @IdCustomer = -1
@@ -31,11 +31,11 @@ BEGIN
 
         --Second Catalog UI MgtCustomer
         SELECT cst.SAPCardCode [IdValue],
-               cst.Name + ' ' + '[' + cst.Abbreviation + ']' [NameValue],
+               IIF(cst.RowSatus = 0, '[INACTIVO] ', '') + cst.Name + ' ' + '[' + cst.Abbreviation + ']' [NameValue],
                cst.CountryID [IdFilter]
         FROM Customer cst
         WHERE cst.IdCustomerType != 3 --todos excepto el portal 3
-              AND cst.RowSatus = 'TRUE'
+              --AND cst.RowSatus = 'TRUE'
               AND
               (
                   @IdCustomer = -1
@@ -98,7 +98,7 @@ BEGIN
 			   cst.[ExcludeCommissionCOD]
         FROM Customer cst
         WHERE cst.IdCustomerType != 3 --todos excepto el portal 3
-              AND cst.RowSatus = 'TRUE'
+              --AND cst.RowSatus = 'TRUE'
               AND
               (
                   @IdCustomer = -1
@@ -111,7 +111,7 @@ BEGIN
     BEGIN
 	--First Catalog UI MgtCustomer
         SELECT CAST(cst.IdCustomer AS NVARCHAR) [IdValue],
-               UPPER(cst.Name) + ' ' + '[' + cst.Abbreviation + ']' [NameValue],
+               IIF(cst.RowSatus = 0, '[INACTIVO] ', '') + UPPER(cst.Name) + ' ' + '[' + cst.Abbreviation + ']' [NameValue],
                cst.CountryID [IdFilter]
         FROM Customer cst
         WHERE cst.IdCustomerType != 3 --todos excepto el portal 3
@@ -120,7 +120,7 @@ BEGIN
 
         --Second Catalog UI MgtCustomer
         SELECT cst.SAPCardCode [IdValue],
-               cst.Name + ' ' + '[' + cst.Abbreviation + ']' [NameValue],
+               IIF(cst.RowSatus = 0, '[INACTIVO] ', '') + cst.Name + ' ' + '[' + cst.Abbreviation + ']' [NameValue],
                cst.CountryID [IdFilter]
         FROM Customer cst
         WHERE cst.IdCustomerType != 3 --todos excepto el portal 3
