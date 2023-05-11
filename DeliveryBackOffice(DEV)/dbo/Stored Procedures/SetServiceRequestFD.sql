201f953943111eb14d8086caa878486872eedc3e
Alberto Ixchop <albertokanecixchop@gmail.com>
2023-05-11 09:24:12 -0600

    Cambios aplicados a sps SetSErviceRequest y SetSErviceRequestFD respecto a nuevo formato de guía

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetServiceRequestFD.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetServiceRequestFD.sql
@@ -544,6 +544,17 @@ BEGIN
 		WHERE
 			[KOVPC].[KindOfVPName] = 'Concesionario'  COLLATE Latin1_General_CI_AI 
 	)
+	DECLARE @ExpressVisitPointTypeId INT = 
+	(
+		SELECT 
+			TOP (1) 
+				[KOVPC].[IdKindOfVPClient] 
+		FROM
+			[DeliveryBackOffice].[dbo].[KindOfVPClient] KOVPC  WITH(NOLOCK) 
+		WHERE
+			[KOVPC].[KindOfVPName] = 'Express Center'  COLLATE Latin1_General_CI_AI 
+	)
+
 	DECLARE @IndividualWebSys INT =
 	(
 		SELECT 
@@ -629,7 +640,7 @@ BEGIN
 					''
 				END
 			)'Icon',
-			(FORMAT((Select DeliveryETA from DeliveryOrder WITH(NOLOCK) where Manifest_Number = @ManifestNumber), 'ddMM'))'DeliveryETA',
+			(FORMAT(ISNULL([D].[DeliveryETA], DATEADD(DAY,5,GETDATE())), 'ddMM'))'DeliveryETA',
 				(
 					CASE
 						WHEN [DOPD].[TimePlaId] = 1 THEN 'PREPAGO'
@@ -643,10 +654,12 @@ BEGIN
 			(
 					CASE
 						WHEN [vpct].[IdKindOfVPClient] = @FranchiseVisitPointTypeId THEN 'CNC'
+						WHEN [vpct].[IdKindOfVPClient] = @ExpressVisitPointTypeId THEN 'EXC'
+						WHEN [vpcti].[IdKindOfVPClient] = @ExpressVisitPointTypeId THEN 'EXC'
 						WHEN [D].[CatSystemId] = @IndividualWebSys THEN 'WEB'
 						WHEN [D].[CatSystemId] = @ExpressWebSys THEN 'EXC'
-						WHEN [D].[CatSystemId] = @CorporateWebSys THEN 'CRP'
-						WHEN [D].[CatSystemId] = @ParserSys THEN 'CRP'
+						WHEN [D].[CatSystemId] = @CorporateWebSys THEN 'COR'
+						WHEN [D].[CatSystemId] = @ParserSys THEN 'PAR'
 						WHEN [D].[CatSystemId] IS NULL THEN 'API'
 						ELSE 'API'
 					END
@@ -665,6 +678,8 @@ BEGIN
 			ON DOPD.GuideNumber = D.Guide_Number
 		LEFT JOIN VisitPointClient vpct WITH (NOLOCK)
             ON vpct.CodeOfReference = D.Sender_ID
+		LEFT JOIN [DeliveryBackOffice].[dbo].[VisitPointClient] vpcti  WITH(NOLOCK) 
+		    ON [vpcti].[CodeOfReference] = [D].[OriginSenderId]
 		WHERE D.Guide_Serie = @GuideSerie AND D.Guide_Number IN (SELECT CT.Guide_Number FROM @CorrelativeTable CT)
 	END
 END
