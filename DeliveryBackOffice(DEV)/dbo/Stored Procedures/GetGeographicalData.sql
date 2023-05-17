fdae66f859277d4e8914c9d6a268ca98efb34f86
Oscar Morales <oscar.morales@forzalatam.com>
2023-05-17 08:27:26 -0600

    Date: 2023-05-17 Author: Oscar Morales
    ### ADDED
    * Sp GetArticleByCustomerByCodeOfReference para obtener información de artículos para validar existencia en parser.
    * Sp GetGeographicalData para obtener la información geográfica y validar existencia en parser.
    
    ### CHANGED
    * Sp SetServiceRequest para no hacer validaciones de artículos y geográficas en base de datos.
    
    Historia: FP-206

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetGeographicalData.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <2023-05-11>
+-- Description:	<Obtiene información de Departamentos y Municipios para el Parser>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetGeographicalData]
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	SELECT p.ProvinceName, t.TownshipName
+	FROM Province p WITH(NOLOCK)
+	INNER JOIN Township t WITH(NOLOCK)
+	ON p.IdProvince = t.IdProvince
+	WHERE p.ProvinceStatus = 1
+	AND t.TownshipStatus = 1
+	ORDER BY p.ProvinceName
+
+END
