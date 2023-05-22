ac1ef85a5e6dcfba15fc8e781f5365b21834ab02
caquino <cesar.aquino@forzalatam.com>
2023-05-22 12:14:20 -0600

    Actualizacion 22 May

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SupportSetPaymentGuide.sql
@@ -0,0 +1,36 @@
+﻿
+create procedure [dbo].[SupportSetPaymentGuide]
+    @GuideSerie [varchar](2)
+  , @GuideNumber int
+  , @Amount decimal(12, 2)
+  , @Token nvarchar(50)
+as
+begin
+
+    begin try
+        begin transaction;
+
+        update dbo.Cost
+        set TotalAmountPaid = @Amount
+          , TokenUpdated = @Token
+          , DateUpdated = getdate()
+        where GuideSerie = @GuideSerie
+              and GuideNumber = @GuideNumber;
+
+       
+
+		
+		 commit;
+
+		 SELECT * FROM dbo.Cost
+		 where GuideSerie =@GuideSerie  and GuideNumber = @GuideNumber
+
+    end try
+    begin catch
+        rollback transaction;
+
+	SELECT 'Error'
+
+    end catch;
+
+end;
