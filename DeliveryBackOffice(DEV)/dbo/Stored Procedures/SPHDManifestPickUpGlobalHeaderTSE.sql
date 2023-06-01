8bb5d129025d2b8ad5451814d54c479086df7755
EdelmanV <edelman.vasquez@forzalatam.com>
2023-06-01 09:20:04 -0600

    Date:2023-06-01 Author:Edelman Description: Cabecera yd etalle de reporte de manifiesto global de recolección

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHDManifestPickUpGlobalHeaderTSE.sql
@@ -0,0 +1,39 @@
+﻿-- =============================================
+-- Author:		<Author,Edelman>
+-- Create date: <Create Date,2023-05-31>
+-- Description:	<Description, Cabecera de manifiesto global de recolección>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHDManifestPickUpGlobalHeaderTSE] 
+
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+	Select 
+      COUNT(B.GuideNumber) TotalGuide,
+	  0 Totalpiece
+
+	  
+	From [DeliveryBackOffice].[dbo].[TSERoutePreparationHeader] A  WITH(NOLOCK) 
+		 Inner Join 
+		 [DeliveryBackOffice].[dbo].[TSERoutePreparationDetail] B  WITH(NOLOCK) 
+		 ON A.IDTSERoutePreparationHeader = B.TSERoutePreparationHeaderID
+	WHERE  A.HasFirstPickupProcess =1 And B.RowStatus =1
+	UNION ALL
+	Select 
+          0 TotalGuide,
+		  COUNT(C.Detail) Totalpiece
+	  
+	From [DeliveryBackOffice].[dbo].[TSERoutePreparationHeader] A  WITH(NOLOCK) 
+		 Inner Join 
+		 [DeliveryBackOffice].[dbo].[TSERoutePreparationDetail] B  WITH(NOLOCK) 
+		 ON A.IDTSERoutePreparationHeader = B.TSERoutePreparationHeaderID
+		 Inner Join 
+		 [DeliveryBackOffice].[dbo].[DeliveryOrderPiece] C  WITH(NOLOCK) 
+		 ON
+		 B.GuideSerie = C.GuideSerie And B.GuideNumber = C.GuideNumber
+	WHERE  A.HasFirstPickupProcess =1 And B.RowStatus =1
+ 
+   
+END
