20c48319238dd088ed95607a67aae2edca11e91d
EdelmanV <edelman.vasquez@forzalatam.com>
2023-06-09 10:11:25 -0600

    Date:2023-06-09 Author:Edelman Description: SP de catalogos de ventana de preparación y ventana de evidencia de recolección

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_Courier_Dispatched_Status_PickUp.sql
@@ -0,0 +1,60 @@
+﻿
+
+
+-- =============================================
+-- Author:		 <Edelman Vasquez>
+-- Create date:  <08/Junio/2023>
+-- Description:	 <Listado de afiliados y su status actual sobre recolección>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHD_Courier_Dispatched_Status_PickUp]
+	@DispatchedDate DATE
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+
+SELECT SR.ID AS ID_Courier,
+       SR.First_Name + ' ' + SR.Last_Name AS Nombre_Courier,
+	     COUNT(DISTINCT SM.IdServiceManagement) AS Cantidad_de_Servicios,
+		 COUNT(DISTINCT   CASE
+                        WHEN SM.ServiceStatusId = 3 THEN
+                            SM.IdServiceManagement
+                        ELSE
+                            NULL
+                    END
+                ) AS Servicios_Recolectados,
+		 COUNT(DISTINCT   CASE
+                        WHEN SM.ServiceStatusId = 3 THEN
+                            DOPD.GuideNumber
+                        ELSE
+                            NULL
+                    END
+                ) AS Guías_Recolectadas,
+
+			 COUNT(DISTINCT   CASE
+                        WHEN SM.ServiceStatusId = 4 THEN
+                            SM.IdServiceManagement
+                        ELSE
+                            NULL
+                    END
+                ) AS Servicio_con_Incidencias,
+				 COUNT(DOPaux.Detail) AS Piezas_Recolectadas
+				FROM [DeliveryBackOffice].[dbo].[ServiceManagement] SM WITH (NOLOCK)
+				INNER JOIN [DeliveryBackOffice].[dbo].[SchedulePickup] SP WITH (NOLOCK)
+				   ON SP.SchedulePickupId = SM.IdSchedulePickup 
+				INNER JOIN [DeliveryBackOffice].[dbo].[DeliveryOrderPaymentDetail] DOPD WITH (NOLOCK)
+				   ON SP.SchedulePickupId = DOPD.IdHeaderRecolection
+				INNER JOIN [DeliveryBackOffice].[DBO].[SenderReceiver] SR WITH (NOLOCK)
+				 ON SM.IdPuCourrier = SR.ID
+				INNER JOIN [DeliveryBackOffice].[dbo].[DeliveryOrderPiece] DOPaux WITH (NOLOCK)
+				 ON                [DOPaux].[GuideSerie] = [DOPD].[GuideSerie]
+									AND
+									[DOPaux].[GuideNumber] = [DOPD].[GuideNumber]
+				WHERE CONVERT(DATE,SM.DateCreated)= @DispatchedDate
+				GROUP BY 
+				SR.ID,
+				SR.First_Name,
+				SR.Last_Name,
+				SM.ServiceStatusId ,
+				SM.IdServiceManagement
+END
---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SpCatRouteClusterEspecial.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:		<Author,Edelman>
+-- Create date: <Create Date,2023-05-19>
+-- Description:	<Description, Catálogo de couster de rutas especiales TSE>
+-- =============================================
+CREATE PROCEDURE [dbo].[SpCatRouteClusterEspecial]
+
+
+AS
+BEGIN	SET NOCOUNT ON;
+
+	Select Distinct
+		RC.IdCatRouteCluster,
+	    Upper(RC.ClusterName) ClusterName
+	From [dbo].[CatRouteCluster] RC WITH (NOLOCK)
+	Where  RowStatus = 1
+  
+END
