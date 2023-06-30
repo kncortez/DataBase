a2e339d10aaf7942e340c1fd34efe40f3c90c37e
Oscar Morales <oscar.morales@forzalatam.com>
2023-06-30 09:43:14 -0600

    Date: 2023-06-30 Author: Oscar Morales
    ### ADDED
    * SP para agregar incidencia a sobres en Entrega de rutas Especaiales.
    ### CHANGED
    * SPs de Manifiestos de rutas Especiales.
    
    Historia: FDD-1265

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetSpecialRouteDeliveryConfirmationIncidence.sql
@@ -0,0 +1,40 @@
+﻿-- =============================================
+-- Author:		<Oscar Morales>
+-- Create date: <2023-06-30>
+-- Description:	<Guarda la descripción de la incidencia para sobres>
+-- =============================================
+CREATE PROCEDURE [dbo].[SetSpecialRouteDeliveryConfirmationIncidence]
+	-- Add the parameters for the stored procedure here
+	@IDTSERoutePreparationHeader INT,
+	@GuideSerie NVARCHAR(2),
+	@GuideNumber INT,
+	@Observation NVARCHAR(200),
+	@Token NVARCHAR(50)
+AS
+BEGIN
+	BEGIN TRANSACTION
+	BEGIN TRY
+
+		UPDATE TSERoutePreparationDetail
+		SET Observation = @Observation
+		   ,TokenUpdated = @Token
+		   ,DateUpdated = GETDATE()
+		WHERE TSERoutePreparationHeaderID = @IDTSERoutePreparationHeader
+		AND GuideSerie = @GuideSerie
+		AND GuideNumber = @GuideNumber
+		AND RowStatus = 1
+
+		COMMIT TRANSACTION
+		
+		SELECT
+			'1' 'ResultCode'
+		   ,'Registros guardados correctamente.' 'Description'
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION
+
+		SELECT
+			'-1' 'ResultCode'
+		   ,ERROR_MESSAGE() 'Description'
+	END CATCH
+END
