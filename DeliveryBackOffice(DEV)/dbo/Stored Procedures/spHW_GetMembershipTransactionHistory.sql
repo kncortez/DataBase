6f37af94fd734cada8173da9a93b3b26f9c700f5
caquino <cesar.aquino@forzalatam.com>
2023-06-30 12:06:55 -0600

    Merge branch 'BD_2000'
    # Conflicts:
    #	DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetWebhookDataForJSON.sql
    #	DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetServiceRequestFD.sql

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_GetMembershipTransactionHistory.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_GetMembershipTransactionHistory.sql
@@ -55,7 +55,7 @@ BEGIN
 				[M].[CustomerId],
 				[CM].[MembershipName],
 				IIF(([M].[MembershipMaxServiceFixedValue] - [M].[ActualServiceCount]) < 0, 0, ([M].[MembershipMaxServiceFixedValue] - [M].[ActualServiceCount])) [MembershipRemainingUses],
-				IIF((([M].[ActualServiceCount] * 100) / IIF([M].[MembershipMaxServiceFixedValue] = 0, 1, [M].[MembershipMaxServiceFixedValue])) > 100, 100, (([M].[ActualServiceCount] * 100) / IIF([M].[MembershipMaxServiceFixedValue] = 0, 1, [M].[MembershipMaxServiceFixedValue]))) [MembershipUsagePercentage],
+				IIF((([M].[ActualServiceCount] * 100) / (case when [M].[MembershipMaxServiceFixedValue] = 0 then 1 else [M].[MembershipMaxServiceFixedValue] end)) > 100, 100, (([M].[ActualServiceCount] * 100) / IIF([M].[MembershipMaxServiceFixedValue] = 0, 1, [M].[MembershipMaxServiceFixedValue]))) [MembershipUsagePercentage],
 				[M].[IsAutoRenewable],
 				ISNULL([CM].[Icon], '') [Icon],
 				[M].[DateCreated],
