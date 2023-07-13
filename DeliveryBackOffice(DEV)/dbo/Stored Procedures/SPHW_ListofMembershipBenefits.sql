78e35948d45c6b0c57762e2cd4488ddc0962e8ed
EdelmanV <edelman.vasquez@forzalatam.com>
2023-07-13 14:55:54 -0600

    Date:2023-07-13 Author:Edelman Descripción: listado de beneficios de membresías

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_ListofMembershipBenefits.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		<Author,Edelman>
+-- Create date: <Create Date,2023-07-07>
+-- Description:	<Description, Descripciones de los beneficios de membresias para notificaciones>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHW_ListofMembershipBenefits] 
+@IdMembership INT 	
+AS
+BEGIN
+
+
+
+     DECLARE @CatMembershipId INT=(Select CatMembershipId From [dbo].[Membership] WITH (NOLOCK) Where IdMembership = @IdMembership);
+	
+	SET NOCOUNT ON;
+
+		Select ISNULL(MembershipAttributeDescription,'') AS 'Benefits'
+			From [dbo].[CatMembershipAttribute] WITH (NOLOCK) 
+		Where CatMembershipId = @CatMembershipId 
+				And RowStatus = 1
+
+
+END
