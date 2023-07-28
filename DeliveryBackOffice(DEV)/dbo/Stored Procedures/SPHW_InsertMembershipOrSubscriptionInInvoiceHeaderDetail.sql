db08e9d90502d5bea74b39d938a45cdef6a482c9
Alberto Ixchop <alberto.ixchop@forzadelivery.com>
2023-07-27 21:10:20 -0600

    Correción de versionamiento

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_InsertMembershipOrSubscriptionInInvoiceHeaderDetail.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_InsertMembershipOrSubscriptionInInvoiceHeaderDetail.sql
@@ -25,7 +25,8 @@ begin
 
     -- Datos cliente Cabecera de factura   
 
-    DECLARE @inv_vpCodeOfReferences AS INT = (SELECT TOP 1 [VPC].[CodeOfReference] FROM [DeliveryBackOffice].[dbo].[VisitPointClient] VPC  WITH(NOLOCK) WHERE VPC.[DescriptionOfClient] = 'EXPRESS CENTER CLUBFORZA'  COLLATE Latin1_General_CI_AI  AND VPC.[StatusClient] = 1);
+    
+    declare @inv_vpCodeOfReferences as int =
             (
                 select top 1
                        [VPC].[CodeOfReference]
