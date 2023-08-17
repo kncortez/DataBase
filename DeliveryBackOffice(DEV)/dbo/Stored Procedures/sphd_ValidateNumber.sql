f8a705c9edc228a973dfbdad14f6dda3dfe6a9e2
EdelmanV <edelman.vasquez@forzalatam.com>
2023-08-16 22:46:00 -0600

    Date:2023-08-16 Author:Edelman Description:  validación cuando tiene que actualizar data d ecourier

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphd_ValidateNumber.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphd_ValidateNumber.sql
@@ -3,17 +3,37 @@
 -- Create date: <2023-07-31>
 -- Description:	<Validar si existe número>
 -- =============================================
-CREATE procedure [dbo].[sphd_ValidateNumber]
-@Phone nvarchar(50)
+ALTER procedure [dbo].[sphd_ValidateNumber]
+@Phone nvarchar(50),
+@UniqueCode nvarchar(50)
 AS
 BEGIN
-		    
-		IF EXISTS(SELECT TOP 1 1   FROM [DeliveryBackOffice].[dbo].[SenderReceiver] WHERE Phone = @Phone)
-		BEGIN 
-
-		  SELECT 1 AS 'StatusCode'
 		
-		END
-		ELSE
-		   SELECT 0 AS 'StatusCode'
-		END
+	BEGIN TRY	
+
+			IF (@UniqueCode='0')	   
+			BEGIN
+						IF EXISTS(SELECT TOP 1 1   FROM [DeliveryBackOffice].[dbo].[SenderReceiver] WHERE Phone = @Phone )
+						BEGIN 
+						  SELECT 1 AS 'StatusCode'
+		
+						END
+						ELSE
+						BEGIN
+						   SELECT 0 AS 'StatusCode'
+						END
+			END
+			ELSE
+			BEGIN
+			 SELECT 0 AS 'StatusCode'
+			END
+		
+
+	END TRY
+
+	BEGIN CATCH
+    -- Bloque de código donde se manejan las excepciones
+    SELECT ERROR_MESSAGE() AS ErrorMessage, ERROR_NUMBER() AS ErrorNumber;
+   END CATCH
+
+END
