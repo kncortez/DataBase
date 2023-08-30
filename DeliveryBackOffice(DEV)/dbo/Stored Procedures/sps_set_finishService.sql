3734533c8cddfafa6782d31ca78c7978b4e32986
EdelmanV <edelman.vasquez@forzalatam.com>
2023-08-30 17:47:15 -0600

    Date:2023-08-23 Author:Edelman Description: modificaciones puntos club forza caje y acumulación

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_set_finishService.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_set_finishService.sql
@@ -1086,7 +1086,8 @@ BEGIN
 													SET @PointsGenerated = ISNULL((SELECT	SUM([PSL].[PointsReceived])
 																			FROM	[dbo].[PointsByServiceLog] PSL
 																			WHERE	[PSL].[GuideSerie] = @AuxGuideSerie
-																				AND [PSL].[GuideNumber] = @AuxGuideNumber), 0);
+																				AND [PSL].[GuideNumber] = @AuxGuideNumber
+                                                                                AND [PSL].[GuidePrice]>0), 0);
 					
 													UPDATE	[dbo].[Membership] 
 															SET		[AccumulatedPoints] = ISNULL([AccumulatedPoints], 0) + (@PointsGenerated),
