9d9a8926102ac105d9ba64ac1888e4d20741920a
EdelmanV <edelman.vasquez@forzalatam.com>
2023-09-28 14:19:30 -0600

    Date:2023-09-28 Author:Edelman Description: agregar validación cuando la opción de entrega es en express center o en casa, tambien agregar estados de incidencia correctos

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetDataforIncidentRegistration.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetDataforIncidentRegistration.sql
@@ -9,23 +9,31 @@ CREATE PROCEDURE [dbo].[SPHW_GetDataforIncidentRegistration]
  @GuideNumber INT	
 AS
 BEGIN
+	 @GuideSerie NVARCHAR(2) = 'FD',
+ @GuideNumber INT	
+AS
+BEGIN
 	
 	SET NOCOUNT ON;
 
 
 	Select TOP 1 
 	       ISNULL(DO.Sender_FirstName +' '+ DO.Sender_LastName,'') AS Sender,
-	       SO.OrderDescription,
+	       SO.OrderDescription AS [Status],
 		   DO.Sender_Phone,
-		   ISNULL(DO.Receiver_FirstName +' '+DO.Receiver_LastName,'') AS Receiver,
+		   ISNULL(DO.Receiver_FirstName,'') +' '+ISNULL(DO.Receiver_LastName,'') AS Receiver,
 		   DO.Receiver_Phone,
-		   DO.Receiver_Address,
+		  ( CASE 
+		       WHEN DO.IdDeliveryOption = 1 THEN DO.Receiver_Address ELSE
+			   VPC.DescriptionOfClient
+			   END 
+			   
+			        ) Receiver_Address,
+		   DO.Receiver_Id,
 		   DO.Receiver_Department,
-		   DO.Receiver_Town,
-	       DP.Path_Incident, 
+		   DO.Receiver_Town, 
 	       DA.Latitude, 
 		   DA.Longitude,
-		   DO.StatusOrderId,
 		    (CASE
                  WHEN CTI.NameIncidence IS NULL THEN
                      NULL
@@ -39,21 +47,50 @@ BEGIN
             ) [Attempts],
 			CTI.NameIncidence,
 			DA.Date_Created,
-			CTI.IdIncidenceType,
-		ISNULL(CIS.IncidenceTypeName, 'N/A') [TypeOfIncident]
-	From [dbo].[DeliveryOrder] DO 
-	    INNER JOIN  [dbo].[DeliveryProof] DP
+		ISNULL(CIS.IncidenceTypeName, 'N/A') [TypeOfIncident],
+		 (CASE
+                     WHEN cfi.IsConfirmed IS NULL THEN
+                         NULL
+                     WHEN cfi.IsConfirmed = 0 THEN
+                         'Pendiente'
+                     ELSE
+                (CASE
+                     WHEN cfi.IsValid = 1 THEN
+                         'Real'
+                     ELSE
+                         'Falsa'
+                 END
+                )
+                 END
+                ) [StatusOfIncident]
+	From [dbo].[DeliveryOrder] DO WITH (NOLOCK)
+	    INNER JOIN  [dbo].[DeliveryProof] DP WITH (NOLOCK)
 			ON DO.Guide_Serie = DP.Guide_Serie AND DO.Guide_Number = DP.Guide_Number 
-	    INNER JOIN  [dbo].[DeliveryAttempt] DA
+	    INNER JOIN  [dbo].[DeliveryAttempt] DA WITH (NOLOCK)
 			ON DP.Guide_Serie = DA.Guide_Serie AND DP.Guide_Number = DA.Guide_Number
-		INNER JOIN [dbo].[StatusOrder] SO 
+		INNER JOIN [dbo].[StatusOrder] SO WITH (NOLOCK)
 		    ON DO.StatusOrderId = SO.StatusOrderId
-	   INNER JOIN [dbo].[DeliveryOrderAttemptData] ATD
+	   INNER JOIN [dbo].[DeliveryOrderAttemptData] ATD WITH (NOLOCK)
 	        ON DA.Guide_Serie = ATD.GuideSerie AND DA.Guide_Number = ATD.GuideNumber
 	  LEFT JOIN [dbo].[CatTypeIncidence] cti WITH (NOLOCK)
                 ON cti.IdIncidenceType = DA.ID_Incident
-	 LEFT JOIN [dbo].[CatIncidenceClasification] CIS
+	 LEFT JOIN [dbo].[CatIncidenceClasification] CIS WITH (NOLOCK)
 		   ON CTI.IncidenceClasificationId = CIS.IdCatIncidenceClasification
+	LEFT JOIN [dbo].[VisitPointClient] VPC WITH (NOLOCK)
+	      ON DO.Receiver_ID = VPC.CodeOfReference
+   LEFT JOIN dbo.ConfirmationOfIncidence cfi WITH (NOLOCK)
+         ON cfi.IdConfirmationOfIncidence = DA.ConfirmationOfIncidenceId
+	Where  DO.Guide_Number = @GuideNumber  And DO.Guide_Number = @GuideNumber
+
+
+	Select  TOP 1
+	       DP.Path_Incident
+	     
+	From [dbo].[DeliveryOrder] DO WITH (NOLOCK)
+	    INNER JOIN  [dbo].[DeliveryProof] DP WITH (NOLOCK)
+			ON DO.Guide_Serie = DP.Guide_Serie AND DO.Guide_Number = DP.Guide_Number 
+	    INNER JOIN  [dbo].[DeliveryAttempt] DA WITH (NOLOCK)
+			ON DP.Guide_Serie = DA.Guide_Serie AND DP.Guide_Number = DA.Guide_Number
 	Where  DO.Guide_Number = @GuideNumber  And DO.Guide_Number = @GuideNumber
 	       
 
