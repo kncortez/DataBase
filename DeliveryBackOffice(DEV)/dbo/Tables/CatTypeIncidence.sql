de803871bb455b633796f9064a5b78a8e66ce808
Eduardo30 <vicente.lopez@forzalatam.com>
2023-10-04 16:46:25 -0600

    04-10-2023 Eduardo López ADD Cambios para obtener descripcion publica para incidencias operativas

--- DeliveryBackOffice(DEV)/dbo/Tables/CatTypeIncidence.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/CatTypeIncidence.sql
@@ -18,58 +18,12 @@
     PRIMARY KEY CLUSTERED ([IdIncidenceType] ASC)
 );
 
-
-
-
-
-
-
-
-
+GO
+ALTER TABLE [DeliveryBackOffice].[dbo].[CatTypeIncidence]
+ADD NameIncidencePublic VARCHAR(50) NULL
 
 GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Mensaje a mostrar en incidencias operativas para tracking publico', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'CatTypeIncidence', @level2type = N'COLUMN', @level2name = N'NameIncidencePublic';
 
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Indicativo si la incidencia esta forzada a ser incidencia en ruta.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'CatTypeIncidence', @level2type = N'COLUMN', @level2name = N'IsForcedIncidence';
