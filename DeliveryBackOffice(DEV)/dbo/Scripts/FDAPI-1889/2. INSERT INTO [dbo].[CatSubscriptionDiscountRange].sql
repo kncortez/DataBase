9c7daaa3df92067973354ea66ad8e2c4cfc75321
Eduardo30 <vicente.lopez@forzalatam.com>
2023-10-09 18:10:27 -0600

    09-10-2023 Eduardo Lopez ADD Configuraciones para creación de producto PAQUETE PETIT

---
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-1889/2. INSERT INTO [dbo].[CatSubscriptionDiscountRange].sql
@@ -0,0 +1,2 @@
+Insert into CatSubscriptionDiscountRange (CatSubscriptionId,DiscountLowServiceRange,ValueTypeId,DiscountValue,RowStatus, TokenCreated, DateCreated)
+values(18,25,1,0,1,'ELOPEZ',GETDATE())
