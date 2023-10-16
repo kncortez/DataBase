501c225c90eb952e98d94ddfffb84eaae2d9e942
caquino <cesar.aquino@forzalatam.com>
2023-10-16 10:06:59 -0600

    cambios de if a case

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_GetMembershipTransactionHistory.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_GetMembershipTransactionHistory.sql
@@ -78,16 +78,16 @@ BEGIN
     SELECT [M].[IdMembership]
          , [M].[CustomerId]
          , [CM].[MembershipName]
-         , IIF(([M].[MembershipMaxServiceFixedValue] - [M].[ActualServiceCount]) < 0
-             , 0
-             , ([M].[MembershipMaxServiceFixedValue] - [M].[ActualServiceCount])) [MembershipRemainingUses]
-         , IIF((([M].[ActualServiceCount] * 100)
-                / IIF([M].[MembershipMaxServiceFixedValue] = 0, 1, [M].[MembershipMaxServiceFixedValue])
+         , (case when([M].[MembershipMaxServiceFixedValue] - [M].[ActualServiceCount]) < 0
+              then 0
+              else ([M].[MembershipMaxServiceFixedValue] - [M].[ActualServiceCount]) end ) [MembershipRemainingUses]
+         , (case when (([M].[ActualServiceCount] * 100)
+                / (case when[M].[MembershipMaxServiceFixedValue] = 0 then 1 else [M].[MembershipMaxServiceFixedValue] end)
                ) > 100
-             , 100
-             , (([M].[ActualServiceCount] * 100)
-                / IIF([M].[MembershipMaxServiceFixedValue] = 0, 1, [M].[MembershipMaxServiceFixedValue])
-               ))                                                                 [MembershipUsagePercentage]
+             then 100
+             else  (([M].[ActualServiceCount] * 100)
+                / (case when[M].[MembershipMaxServiceFixedValue] = 0 then 1 else  [M].[MembershipMaxServiceFixedValue] end )
+               ) end )                                                                 [MembershipUsagePercentage]
          , [M].[IsAutoRenewable]
          , ISNULL([CM].[Icon], '')                                                [Icon]
          , [M].[DateCreated]
