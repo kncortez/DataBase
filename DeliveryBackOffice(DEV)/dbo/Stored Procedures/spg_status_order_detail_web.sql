c574b64b8028e5dafc889fab64113aea44c6e42e
Alberto Ixchop <alberto.ixchop@forzadelivery.com>
2023-10-27 09:59:10 -0600

    Corrección de sp spg_status_order_detail_web

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_status_order_detail_web.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_status_order_detail_web.sql
@@ -365,7 +365,7 @@ BEGIN
             INNER JOIN [dbo].[CatCheckpointType] CCT
                 ON [so].[CatCheckpointTypeId] = [CCT].[IdCatCheckpointType]
             LEFT  JOIN [dbo].[DeliveryAttempt] da WITH(NOLOCK)
-			    ON   dod.Guide_Serie = da.Guide_Serie And dod.Guide_Number = da.Guide_Number  
+			    ON   da.ID=dod.DeliveryAttemptId
 			LEFT JOIN [dbo].[ConfirmationOfIncidence] COI WITH(NOLOCK) 
 			    ON da.ConfirmationOfIncidenceId = COI.IdConfirmationOfIncidence
         WHERE dod.Guide_Serie = @Guide_Serie
