1c6d4064afa953812e31230326ea54ea32b304cc
Eduardo30 <vicente.lopez@forzalatam.com>
2023-11-16 10:36:15 -0600

    16-11-2023 Eduardo Lòpez ADD Cambios para revolver inconveniente en resumen de reporte que aparecian montos de guìas de otros usuarios

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/ReportClosure.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/ReportClosure.sql
@@ -301,7 +301,7 @@ begin
 			LEFT JOIN DeliveryBackOffice.dbo.RegisterUser REU 
 				ON REU.UsrIdUser = ACH.UserId
 		WHERE CONVERT(DATE, DOPD.DateCreated) BETWEEN  CONVERT(DATE, @StartDate) AND CONVERT(DATE, @EndDate)
-			AND (VPC.CodeOfReference = @VisitPointId OR DOPD.AccountId = @IdAccount)
+			AND (DOPD.AccountId = @IdAccount)
 			AND (CTS.IdTypeService NOT IN (5,23))
 			 AND DOPD.[TypeofInOutMoneyId] != 8
 		ORDER BY ACD.AccountingClosuresHeaderId, DOPD.DateCreated ASC
