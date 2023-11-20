b70bc7f7c0e2b39c4d4241ce33380ba20976a5c9
EdelmanV <edelman.vasquez@forzalatam.com>
2023-11-20 16:43:34 -0600

    #cambio de nombre del reporte

--- DeliveryBackOffice(DEV)/dbo/Scripts/Insert Into ConfigParams.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/Insert Into ConfigParams.sql
@@ -1,7 +1,7 @@
 Insert Into ConfigParams ([Name],	[Description],	[Value],	[Status],	CreateDate)
-			VALUES('HistorialdeValidacionesdeControldeCalidad',
+			VALUES('CCValidationHistory',
 			       'Historial de validación de incidencias de control de calidad',
-				   'http://192.168.31.235/ReportServer/Pages/ReportViewer?/ForzaDelivery%2fHistorialdeValidacionesdeControldeCalidad',
+				   'http://192.168.31.235/ReportServer/Pages/ReportViewer?/ForzaDelivery%2fCCValidationHistory',
 				   1,	
 				   GETDATE()
 				   )
