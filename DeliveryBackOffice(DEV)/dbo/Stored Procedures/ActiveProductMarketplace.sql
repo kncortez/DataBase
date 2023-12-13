8b27f43b966a840733eb439719aeb54af99f5494
Eduardo30 <vicente.lopez@forzalatam.com>
2023-12-13 14:40:14 -0600

    13-12-2023 Eduardo López ADD Mejoras para metodo de activación de productos

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/ActiveProductMarketplace.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/ActiveProductMarketplace.sql
@@ -23,8 +23,10 @@ BEGIN
 			SET RowStatus = 1
 			WHERE ActivationCode = @Code
 
-		SELECT CatProductName,
-		'EL producto ha sido activado correctamente' AS Message
+		SELECT cp.CatProductName,
+		IIF(cp.CatProductName = 'Gift Card',('Felicidades..! has activado la '+' '+cp.CatProductName), 
+		(IIF(cp.CatProductName = 'Club Forza',('Felicidades..! has activado la membresía'+' '+cp.CatProductName),('Felicidades..! has activado el'+' '+cp.CatProductName)))) AS Message,
+		CONVERT(VARCHAR(10),pt.ProductExpirationDate,105) AS DateExpiration
 		from CatProduct cp WITH(NOLOCK)
 		  INNER JOIN Product pt WITH(NOLOCK)
 		  ON cp.IdCatProduct = pt.CatProductId
