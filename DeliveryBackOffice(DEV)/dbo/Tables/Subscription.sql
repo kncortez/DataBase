eec2321606392a043977342f8676df64c07269fa
EdelmanV <edelman.vasquez@forzalatam.com>
2024-02-13 16:47:59 -0600

    #agregar campos a tablas

--- DeliveryBackOffice(DEV)/dbo/Tables/Subscription.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/Subscription.sql
@@ -1,62 +1,79 @@
-﻿CREATE TABLE [dbo].[Subscription] (
-    [IdSubscription]                   INT             IDENTITY (1, 1) NOT NULL,
-    [MembershipId]                     INT             NULL,
-    [CatSubscriptionId]                INT             NOT NULL,
-    [CatSubscriptionStatusId]          INT             NOT NULL,
-    [SubscriptionCode]                 NVARCHAR (50)   NULL,
-    [SubscriptionCost]                 DECIMAL (18, 2) NOT NULL,
-    [CustomerId]                       INT             NULL,
-    [AccountId]                        BIGINT          NULL,
-    [VisitPointClientId]               INT             NULL,
-    [CustomerPaymentId]                INT             NULL,
-    [IsAutoRenewable]                  BIT             CONSTRAINT [DF_Subscription_IsAutoRenewable] DEFAULT ((0)) NOT NULL,
-    [SubscriptionFixedValue]           INT             NOT NULL,
-    [SubscriptionMaxServiceFixedValue] INT             NOT NULL,
-    [ActualServiceCount]               INT             NOT NULL,
-    [ExpirationDate]                   DATETIME        NOT NULL,
-    [RowStatus]                        BIT             CONSTRAINT [DF_Subscription_RowStatus] DEFAULT ((1)) NOT NULL,
-    [TokenCreated]                     NVARCHAR (50)   NOT NULL,
-    [DateCreated]                      DATETIME        NOT NULL,
-    [TokenUpdated]                     NVARCHAR (50)   NULL,
-    [DateUpdated]                      DATETIME        NULL,
-    [LastPaymentDate]                  DATETIME        NULL,
-    [RenewalFixedDay]                  INT             NULL,
-    [RateHeaderId]                     INT             NULL,
-    [AlternativeRateHeaderId]          INT             NULL,
-    [CatTypeSubscriptionId]            INT             NULL,
-    CONSTRAINT [PK_Subscription] PRIMARY KEY CLUSTERED ([IdSubscription] ASC),
-    CONSTRAINT [FK_Subscription_AlternativeRate] FOREIGN KEY ([AlternativeRateHeaderId]) REFERENCES [dbo].[RateHeader] ([RheId]),
-    CONSTRAINT [FK_Subscription_CatTypeSubscription] FOREIGN KEY ([CatTypeSubscriptionId]) REFERENCES [dbo].[CatTypeSubscription] ([IdCatTypeSubscription]),
-    CONSTRAINT [FK_Subscription_Rate] FOREIGN KEY ([RateHeaderId]) REFERENCES [dbo].[RateHeader] ([RheId])
-);
-
-
-
-
-
-
-
-
-
-
-
-
-GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Día  el cual se desea poder renovar la suscripción.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Subscription', @level2type = N'COLUMN', @level2name = N'RenewalFixedDay';
-
-
+﻿CREATE TABLE [dbo].[Subscription](
+	[IdSubscription] [int] IDENTITY(1,1) NOT NULL,
+	[MembershipId] [int] NULL,
+	[CatSubscriptionId] [int] NOT NULL,
+	[CatSubscriptionStatusId] [int] NOT NULL,
+	[SubscriptionCode] [nvarchar](50) NULL,
+	[SubscriptionCost] [decimal](18, 2) NOT NULL,
+	[CustomerId] [int] NULL,
+	[AccountId] [bigint] NULL,
+	[VisitPointClientId] [int] NULL,
+	[CustomerPaymentId] [int] NULL,
+	[IsAutoRenewable] [bit] NOT NULL,
+	[SubscriptionFixedValue] [int] NOT NULL,
+	[SubscriptionMaxServiceFixedValue] [int] NOT NULL,
+	[ActualServiceCount] [int] NOT NULL,
+	[ExpirationDate] [datetime] NOT NULL,
+	[RowStatus] [bit] NOT NULL,
+	[TokenCreated] [nvarchar](50) NOT NULL,
+	[DateCreated] [datetime] NOT NULL,
+	[TokenUpdated] [nvarchar](50) NULL,
+	[DateUpdated] [datetime] NULL,
+	[LastPaymentDate] [datetime] NULL,
+	[RenewalFixedDay] [int] NULL,
+	[RateHeaderId] [int] NULL,
+	[AlternativeRateHeaderId] [int] NULL,
+	[CatTypeSubscriptionId] [int] NULL,
+	[ActivationCode] [nvarchar](50) NULL,
+	[ProductGiftShippingEmail] [nvarchar](100) NULL,
+	[ActivationDate] [datetime] NULL,
+ CONSTRAINT [PK_Subscription] PRIMARY KEY CLUSTERED 
+(
+	[IdSubscription] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
-
-
+ALTER TABLE [dbo].[Subscription] ADD  CONSTRAINT [DF_Subscription_IsAutoRenewable]  DEFAULT ((0)) FOR [IsAutoRenewable]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Tarifario a utilizar cuando se usa suscripción', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Subscription', @level2type = N'COLUMN', @level2name = N'RateHeaderId';
 
-
+ALTER TABLE [dbo].[Subscription] ADD  CONSTRAINT [DF_Subscription_RowStatus]  DEFAULT ((1)) FOR [RowStatus]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = 'Tarifario alterno a utilizar cuando se usa suscripción', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Subscription', @level2type = N'COLUMN', @level2name = N'AlternativeRateHeaderId';
 
-
+ALTER TABLE [dbo].[Subscription]  WITH CHECK ADD  CONSTRAINT [FK_Subscription_AlternativeRate] FOREIGN KEY([AlternativeRateHeaderId])
+REFERENCES [dbo].[RateHeader] ([RheId])
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Id relacion con tabla CatTypeSubscription', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Subscription', @level2type = N'COLUMN', @level2name = N'CatTypeSubscriptionId';
+
+ALTER TABLE [dbo].[Subscription] CHECK CONSTRAINT [FK_Subscription_AlternativeRate]
+GO
+
+ALTER TABLE [dbo].[Subscription]  WITH CHECK ADD  CONSTRAINT [FK_Subscription_CatTypeSubscription] FOREIGN KEY([CatTypeSubscriptionId])
+REFERENCES [dbo].[CatTypeSubscription] ([IdCatTypeSubscription])
+GO
+
+ALTER TABLE [dbo].[Subscription] CHECK CONSTRAINT [FK_Subscription_CatTypeSubscription]
+GO
+
+ALTER TABLE [dbo].[Subscription]  WITH CHECK ADD  CONSTRAINT [FK_Subscription_Rate] FOREIGN KEY([RateHeaderId])
+REFERENCES [dbo].[RateHeader] ([RheId])
+GO
+
+ALTER TABLE [dbo].[Subscription] CHECK CONSTRAINT [FK_Subscription_Rate]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Día  el cual se desea poder renovar la suscripción.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Subscription', @level2type=N'COLUMN',@level2name=N'RenewalFixedDay'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Tarifario a utilizar cuando se usa suscripción' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Subscription', @level2type=N'COLUMN',@level2name=N'RateHeaderId'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Tarifario alterno a utilizar cuando se usa suscripción' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Subscription', @level2type=N'COLUMN',@level2name=N'AlternativeRateHeaderId'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Id relacion con tabla CatTypeSubscription' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Subscription', @level2type=N'COLUMN',@level2name=N'CatTypeSubscriptionId'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fecha de activación del producto' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Subscription', @level2type=N'COLUMN',@level2name=N'ActivationDate'
+GO
+
 
