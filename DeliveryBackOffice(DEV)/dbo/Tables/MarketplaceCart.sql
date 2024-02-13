a007cd8efddf500d42beabee25afd84436bb9577
EdelmanV <edelman.vasquez@forzalatam.com>
2024-02-13 15:46:57 -0600

    #tabla de cabecera de carrito

---
+++ DeliveryBackOffice(DEV)/dbo/Tables/MarketplaceCart.sql
@@ -0,0 +1,42 @@
+
+CREATE TABLE [dbo].[MarketplaceCart](
+	[IdMarketplaceCart] [int] IDENTITY(1,1) NOT NULL,
+	[AccountId] [bigint] NOT NULL,
+	[RowStatus] [bit] NOT NULL,
+	[TokenCreated] [nvarchar](50) NOT NULL,
+	[DateCreated] [datetime] NOT NULL,
+	[TokenUpdated] [nvarchar](50) NULL,
+	[DateUpdated] [datetime] NULL,
+ CONSTRAINT [PK_MarketplaceCart] PRIMARY KEY CLUSTERED 
+(
+	[IdMarketplaceCart] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[MarketplaceCart]  WITH CHECK ADD  CONSTRAINT [FK_MarketplaceCart_Account] FOREIGN KEY([AccountId])
+REFERENCES [dbo].[Account] ([AccIdAccount])
+GO
+
+ALTER TABLE [dbo].[MarketplaceCart] CHECK CONSTRAINT [FK_MarketplaceCart_Account]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Estado del registro' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'MarketplaceCart', @level2type=N'COLUMN',@level2name=N'RowStatus'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Usuario de creación' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'MarketplaceCart', @level2type=N'COLUMN',@level2name=N'TokenCreated'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fecha de creación' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'MarketplaceCart', @level2type=N'COLUMN',@level2name=N'DateCreated'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Usuario de modificación' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'MarketplaceCart', @level2type=N'COLUMN',@level2name=N'TokenUpdated'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fecha de modificación' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'MarketplaceCart', @level2type=N'COLUMN',@level2name=N'DateUpdated'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Carrito de productos' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'MarketplaceCart'
+GO
+
+
