b5e0948b00b5369d2a837ecd5fdc73e57ff7ae1c
EdelmanV <edelman.vasquez@forzalatam.com>
2024-02-13 14:36:50 -0600

    #método para obtener estado de una transacción

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetResponseFacTransactionProcessStatus.sql
@@ -0,0 +1,26 @@
+-- =============================================
+-- Author:		<Author,Edelman>
+-- Create date: <Create Date,2023-09-12>
+-- Description:	<Description,Validar estado de una transacción en proceso de pago>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHW_GetResponseFacTransactionProcessStatus] 
+	@OrderNumber VARCHAR(100) = NULL, --Guide, membresía o suscripción
+	@AccountId INT = 0,
+	@Token NVARCHAR(50) = '',
+	@SystemId INT = NULL
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+	  SELECT Top 1       
+		 CTC.ReasonCode, 
+         CTC.ReasonDescription,
+		 CTC.StatusSend,
+		 CTC.ReferenceNumber
+	    FROM [DeliveryBackOffice].[dbo].[CreditCardTransactionByCustomer] CTC WITH(NOLOCK)
+							WHERE
+							OrderNumber = @OrderNumber
+
+   
+END
