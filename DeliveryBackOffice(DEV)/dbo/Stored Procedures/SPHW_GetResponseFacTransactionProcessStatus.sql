1b6f9b3990b31cf034841bfcd899fa2df9bbacc7
EdelmanV <edelman.vasquez@forzalatam.com>
2024-02-13 18:26:58 -0600

    #modificar sp

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetResponseFacTransactionProcessStatus.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetResponseFacTransactionProcessStatus.sql
@@ -4,7 +4,10 @@
 -- Description:	<Description,Validar estado de una transacción en proceso de pago>
 -- =============================================
 CREATE PROCEDURE [dbo].[SPHW_GetResponseFacTransactionProcessStatus] 
-@OrderNumber VARCHAR(100)
+	@OrderNumber VARCHAR(100) = NULL, 
+	@AccountId INT = 0,
+	@Token NVARCHAR(50) = '',
+	@SystemId INT = NULL
 AS
 BEGIN
 
