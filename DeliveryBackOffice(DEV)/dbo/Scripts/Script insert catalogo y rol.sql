cd9711cc02beceb4c31ad634986c26a02a0f077c
Alberto Ixchop <alberto.ixchop@forzadelivery.com>
2024-02-19 19:00:53 -0600

    Fix

--- DeliveryBackOffice(DEV)/dbo/Scripts/Script insert catalogo y rol.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/Script insert catalogo y rol.sql
@@ -3,14 +3,15 @@
 GO
 INSERT [dbo].[CatModule] ( [ModName], [ModIdModuleParent], [ModPath], [ModDescription], [ModOrder], [ModMetadata], [ModVisible], [ModRowStatus], [ModTokenCreated], [ModDateCreated], [ModTokenUpdated], [ModDateUpdated], [ModGroup]) VALUES (N'Centro de Canje', NULL, N'/individual/centro-canje', N'Centro de Canje', 100, N'fa bi bi-shop-window fa-1x', 1, 1, N'evasquez', CAST(N'2022-12-22T13:23:21.510' AS DateTime), NULL, NULL, 0)
 GO
-INSERT [dbo].[CatModule] ( [ModName], [ModIdModuleParent], [ModPath], [ModDescription], [ModOrder], [ModMetadata], [ModVisible], [ModRowStatus], [ModTokenCreated], [ModDateCreated], [ModTokenUpdated], [ModDateUpdated], [ModGroup]) VALUES (N'Centro de Canje', NULL, N'/express/centro-canje', N'Centro de canje express centrer ', 45, N'fa bi bi-shop-window fa-1x', 1, 1, N'evasquez', CAST(N'2024-01-06T00:39:59.723' AS DateTime), NULL, NULL, 1)
+INSERT [dbo].[CatModule] ( [ModName], [ModIdModuleParent], [ModPath], [ModDescription], [ModOrder], [ModMetadata], [ModVisible], [ModRowStatus], [ModTokenCreated], [ModDateCreated], [ModTokenUpdated], [ModDateUpdated], [ModGroup]) VALUES (N'Centro de Canje', NULL, N'/express/centro-canje', N'Centro de canje express center ', 45, N'fa bi bi-shop-window fa-1x', 1, 1, N'evasquez', CAST(N'2024-01-06T00:39:59.723' AS DateTime), NULL, NULL, 1)
 GO
 
 
 --- rol aquí tiene que ir en este campo: [RmsIdModule] que se insertan  en la tabla CatModulo
 
-GO
-INSERT [dbo].[RolByModuleBySystem] ([RmsIdRol], [RmsIdSystem], [RmsIdModule], [RmsRowStatus], [RmsTokenCreated], [RmsDateCreated], [RmsTokenUpdated], [RmsDateUpdated], [RmsModuleMenu], [RmsHasNewFunction]) VALUES (5, 1, 145, 1, N'evasquez', CAST(N'2024-01-05T19:01:07.360' AS DateTime), NULL, NULL, NULL, NULL)
+DECLARE @IDMODULE INT =(select * from dbo.catmodule WHERE ModDescription='Centro de Canje' and ModPath='/individual/centro-canje');
+DECLARE @IDMODULEEXC INT =(select modidmodule from  dbo.catmodule WHERE ModDescription='Centro de canje express center ' and ModPath='/express/centro-canje');
 
-INSERT [dbo].[RolByModuleBySystem] ([RmsIdRol], [RmsIdSystem], [RmsIdModule], [RmsRowStatus], [RmsTokenCreated], [RmsDateCreated], [RmsTokenUpdated], [RmsDateUpdated], [RmsModuleMenu], [RmsHasNewFunction]) VALUES (28, 1, 144, 1, N'evasquez', CAST(N'2024-01-31T00:00:00.000' AS DateTime), NULL, NULL, 1, NULL)
-GO
+INSERT [dbo].[RolByModuleBySystem] ([RmsIdRol], [RmsIdSystem], [RmsIdModule], [RmsRowStatus], [RmsTokenCreated], [RmsDateCreated], [RmsTokenUpdated], [RmsDateUpdated], [RmsModuleMenu], [RmsHasNewFunction]) VALUES (5, 1,@IDMODULEEXC, 1, N'evasquez', CAST(N'2024-01-05T19:01:07.360' AS DateTime), NULL, NULL, NULL, NULL)
+
+INSERT [dbo].[RolByModuleBySystem] ([RmsIdRol], [RmsIdSystem], [RmsIdModule], [RmsRowStatus], [RmsTokenCreated], [RmsDateCreated], [RmsTokenUpdated], [RmsDateUpdated], [RmsModuleMenu], [RmsHasNewFunction]) VALUES (28, 1, @IDMODULE, 1, N'evasquez', CAST(N'2024-01-31T00:00:00.000' AS DateTime), NULL, NULL, 1, NULL)
