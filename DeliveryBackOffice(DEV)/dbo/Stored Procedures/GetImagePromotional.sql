a2555c7b15f275406150680870b24902e29951f7
EdelmanV <edelman.vasquez@forzalatam.com>
2024-02-20 18:34:47 -0600

    #agregar campo de hipervinculo

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetImagePromotional.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetImagePromotional.sql
@@ -3,6 +3,11 @@
 -- Create date: <2023-08-21>
 -- Description:	< Obtener imagenes y datos para Carousel superior de marketplace>
 -- =============================================
+-- =============================================
+-- Author:		<Edelman>
+-- Create date: <2024-16-2>
+-- Description:	< agregar parámetro de hipervinculo para imagenes>
+-- =============================================
 CREATE PROCEDURE [dbo].[GetImagePromotional]
 
 AS
@@ -14,6 +19,7 @@ BEGIN
         ,[XLImageURL]
         ,[MDImageURL]
         ,[XSImageURL]
+        ,[HyperlinkURL]
   FROM [DeliveryBackOffice].[dbo].[MarketplaceCarouselImage]
   WHERE [RowStatus] = 1
 
