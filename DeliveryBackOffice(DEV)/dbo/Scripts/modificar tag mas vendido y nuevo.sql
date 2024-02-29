d3069c79e39463283c87cc534a8e9e8aabffee6f
EdelmanV <edelman.vasquez@forzalatam.com>
2024-02-29 11:31:49 -0600

    #modificación de tag

--- DeliveryBackOffice(DEV)/dbo/Scripts/modificar tag mas vendido y nuevo.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/modificar tag mas vendido y nuevo.sql
@@ -1,18 +1,31 @@
--- quitar cintillo del mas vendido
+--  incluir el cintillo Más Vendido
 UPDATE [dbo].[CatSubscription]
-SET Tag=NULL
-where SubscriptionName='Paquete Plus'
+SET Tag='MÁS VENDIDO'
+where SubscriptionName='Plan Amigo'
+AND RowStatus=1
+
 --  quitar cintillo Nuevo e incluir el cintillo de Mas vendido
 UPDATE [dbo].[CatSubscription]
 SET Tag='MÁS VENDIDO'
 where SubscriptionName='Paquete Básico'
+AND RowStatus=1
 
 --  incluir el cintillo de Nuevo
 UPDATE [dbo].[CatSubscription]
-SET Tag='NUEVO'
+SET Tag='MÁS VENDIDO'
 where SubscriptionName='Paquete Petit'
+AND RowStatus=1
 
 -- , incluir el cintillo de Nuevo
 UPDATE [dbo].[CatSubscription]
-SET Tag='NUEVO'
+SET Tag='NOVEDADES'
 where SubscriptionName='Paquete Platino'
+AND RowStatus=1
+
+
+
+-- quitar cintillo del mas vendido
+UPDATE [dbo].[CatSubscription]
+SET Tag=NULL
+where SubscriptionName='Paquete Plus'
+AND RowStatus=1
