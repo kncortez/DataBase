6db9a06cceba3805efe56b972c175c4343fc9e81
Eduardo30 <vicente.lopez@forzalatam.com>
2024-03-07 16:14:45 -0600

    07-03-2024 Eduardo Lopez ADD Agregar dos columnas mas solicitados a la tabla

--- DeliveryBackOffice(DEV)/dbo/Tables/WebhookTrackingQueueDetailForSFTP.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/WebhookTrackingQueueDetailForSFTP.sql
@@ -13,6 +13,8 @@ CREATE TABLE [dbo].[WebhookTrackingQueueDetailForSFTP](
 	[TokenCreated] [NVARCHAR](50) NOT NULL,
 	[DateUpdated] [DATETIME] NULL,
 	[TokenUpdated] [NVARCHAR](50) NULL,
+	[DeliveryAttemptId] [bigint] NULL,
+	[NewDeliveryDate] [datetime] NULL,
  CONSTRAINT [PK_IdWebhookTrackingQueueDetailForSFTP] PRIMARY KEY CLUSTERED 
 (
 	[IdWebhookTrackingQueueDetailForSFTP] ASC
@@ -29,6 +31,9 @@ ALTER TABLE [dbo].[WebhookTrackingQueueDetailForSFTP]  WITH CHECK ADD  CONSTRAIN
 REFERENCES [dbo].[StatusOrder] ([StatusOrderId])
 GO
 ALTER TABLE [dbo].[WebhookTrackingQueueDetailForSFTP] ADD CONSTRAINT UQ_1 UNIQUE ([CustomerId],[ExternalNumber],[ExternalPieceId])
+GO
+ALTER TABLE [dbo].[WebhookTrackingQueueDetailForSFTP]  WITH CHECK ADD  CONSTRAINT [FK_WebhookTrackingQueueDetailForSFTP_DeliveryAttempt] FOREIGN KEY([DeliveryAttemptId])
+REFERENCES [dbo].[DeliveryAttempt] ([ID])
 GO
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'ID de la tabla WebhookTrackingQueueDetailForSFTP' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WebhookTrackingQueueDetailForSFTP', @level2type=N'COLUMN',@level2name=N'IdWebhookTrackingQueueDetailForSFTP'
 GO
@@ -58,4 +63,7 @@ EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Usuario que ac
 GO
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fecha que actualiza' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WebhookTrackingQueueDetailForSFTP', @level2type=N'COLUMN',@level2name=N'DateUpdated'
 GO
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Si es un estado de incidencia registrar el detalle de incidencia' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WebhookTrackingQueueDetailForSFTP', @level2type=N'COLUMN',@level2name=N'DeliveryAttemptId'
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Nueva fecha de entrega, Incidencia (destino solicita cambio de fecha de entrega)' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'WebhookTrackingQueueDetailForSFTP', @level2type=N'COLUMN',@level2name=N'NewDeliveryDate'
 
