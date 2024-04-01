4ba81b0557a3ba99fea2cea8b6259fb68e5824ef
FLG\rene.rodriguez <oscar.vasquez@forzadelivery.com>
2024-04-01 15:55:34 -0600

    2024-04-01 Oscar Rodriguez ADD Se agrego a SP validacion para TODOS los departamentos. Descripcion Se agrego con estado '-1' el estado de TODOS los departamentos solicitados para recolecciones ya sea a demanda o programadas dentro del SP de consulta de informacion

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_get_schedule_demand_pickup.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_get_schedule_demand_pickup.sql
@@ -1,6 +1,6 @@
 -- =============================================
 -- Author:		<Oscar Rodriguez>
--- Update date: <2024-03-21>
+-- Update date: <2024-04-t01>
 -- Description:	<Obtiene la informacion sobre recolecciones a demanda y programadas, para integración con DispatchTrack>
 -- =============================================
 CREATE PROCEDURE [dbo].[spg_get_schedule_demand_pickup]
@@ -12,83 +12,161 @@ CREATE PROCEDURE [dbo].[spg_get_schedule_demand_pickup]
 AS
 BEGIN
 
-DECLARE @tblCollectionProvinceId 
-TABLE (ProvinceId INT)
+IF @CollectionProvince = '-1' BEGIN
 
-INSERT INTO @tblCollectionProvinceId (ProvinceId)
-SELECT SUBSTRING(Item, 1, LEN(Item)) ItemNumber
-FROM DeliveryBackOffice.dbo.SplitUnlimited(@CollectionProvince, ',')
+	IF @TypePickup = 1 BEGIN
 
-IF @TypePickup = 1 BEGIN
+	SELECT	IIF(sp.IsScheduled = 0, CONCAT('RDG', sm.IdServiceManagement), CONCAT('RPG', sm.IdServiceManagement) ) [ORDEN DE RECOLECCION], 
+			'' [RUTA], 
+			IIF(ctv.Name = 'Camión', 'Paquete grande', IIF(ctv.Name = 'Panel', 'Paquete mediano', IIF(ctv.Name = 'Motocicleta', 'Paquete pequeño', ''))) [DESCRIPCION DEL PAQUETE], 
+			IIF(sp.QuantityRegularPackages is null, '', sp.QuantityRegularPackages) [CANTIDAD DE PIEZAS], 
+			'' [CODIGO DE ITEM], sp.SenderId [CODIGO DE PUNTO DE VISITA], 
+			vpc.DescriptionOfClient [NOMBRE DE REMITENTE], 
+			IIF(vpc.Phone is null, '', vpc.Phone ) [TELEFONO DE REMITENTE], 
+			IIF(sp.AccountId is null, IIF(vpc.Email is null, '', vpc.Email), ru.UsrEmail ) [EMAIL REMITENTE], 
+			IIF(vpc.Address is null, '', vpc.Address) [DIRECCION REMITENTE], 
+			IIF(vpc.Latitude is null, '', vpc.Latitude) [LATITUD], 
+			IIF(vpc.Longitude is null, '', vpc.Longitude ) [LONGITUD], 
+			sm.DateCreated [FECHA Y HORA MINIMA DE RECOLECCION], 
+			DATEADD(HOUR, 2, sm.DateCreated) [FECHA Y HORA MAXIMA DE RECOLECCION], 
+			'' [CT DESTINO], 
+			'Solo Recogida' [MODO]
+	FROM		[DeliveryBackOffice].dbo.ServiceManagement	sm		WITH (NOLOCK)
+	INNER  JOIN	[DeliveryBackOffice].dbo.SchedulePickup		sp		WITH (NOLOCK) ON sm.IdSchedulePickup = sp.SchedulePickupId
+	INNER  JOIN	[DeliveryBackOffice].dbo.VisitPointClient	vpc		WITH (NOLOCK) ON vpc.CodeOfReference = sp.SenderId
+	INNER  JOIN	[DeliveryBackOffice].dbo.Township			tw		WITH (NOLOCK) ON tw.IdTownShip = sp.TownshipId 
+	LEFT   JOIN	[DeliveryBackOffice].dbo.CatTypeVehicle		ctv		WITH (NOLOCK) ON ctv.IdTypeVehicle = sp.TypeVehicleId
+	LEFT   JOIN	[DeliveryBackOffice].dbo.RolByUserByAccount rbuba	WITH (NOLOCK) ON rbuba.RuaIdAccount = sp.AccountId
+	LEFT   JOIN	[DeliveryBackOffice].dbo.RegisterUser		ru		WITH (NOLOCK) ON ru.UsrIdUser = rbuba.RuaIdUser
+	LEFT   JOIN	[DeliveryBackOffice].dbo.Province			pv		WITH (NOLOCK) ON pv.IdProvince = tw.IdProvince
+	INNER  JOIN	[DeliveryBackOffice].dbo.RouteAssigment		ra		WITH (NOLOCK) ON ra.IdRouteAssigment = sm.IdPuRouteAssigment
+	WHERE	sp.IsScheduled = 0
+	AND		sm.IdPuCourrier is not null 
+	AND		ra.IdVehicle is not null
+	AND		CAST(sm.DateCreated AS DATE) = @AssignDate
+	AND		CAST(sm.DateCreated AS TIME) BETWEEN @StartDate AND @EndDate
+	ORDER BY [ORDEN DE RECOLECCION] DESC;
 
-SELECT	IIF(sp.IsScheduled = 0, CONCAT('RDG', sm.IdServiceManagement), CONCAT('RPG', sm.IdServiceManagement) ) [ORDEN DE RECOLECCION], 
-		'' [RUTA], 
-		IIF(ctv.Name = 'Camión', 'Paquete grande', IIF(ctv.Name = 'Panel', 'Paquete mediano', IIF(ctv.Name = 'Motocicleta', 'Paquete pequeño', ''))) [DESCRIPCION DEL PAQUETE], 
-		IIF(sp.QuantityRegularPackages is null, '', sp.QuantityRegularPackages) [CANTIDAD DE PIEZAS], 
-		'' [CODIGO DE ITEM], sp.SenderId [CODIGO DE PUNTO DE VISITA], 
-		vpc.DescriptionOfClient [NOMBRE DE REMITENTE], 
-		IIF(vpc.Phone is null, '', vpc.Phone ) [TELEFONO DE REMITENTE], 
-		IIF(sp.AccountId is null, IIF(vpc.Email is null, '', vpc.Email), ru.UsrEmail ) [EMAIL REMITENTE], 
-		IIF(vpc.Address is null, '', vpc.Address) [DIRECCION REMITENTE], 
-		IIF(vpc.Latitude is null, '', vpc.Latitude) [LATITUD], 
-		IIF(vpc.Longitude is null, '', vpc.Longitude ) [LONGITUD], 
-		sm.DateCreated [FECHA Y HORA MINIMA DE RECOLECCION], 
-		DATEADD(HOUR, 2, sm.DateCreated) [FECHA Y HORA MAXIMA DE RECOLECCION], 
-		'' [CT DESTINO], 
-		'Solo Recogida' [MODO]
-FROM		[DeliveryBackOffice].dbo.ServiceManagement	sm		WITH (NOLOCK)
-INNER  JOIN	[DeliveryBackOffice].dbo.SchedulePickup		sp		WITH (NOLOCK) ON sm.IdSchedulePickup = sp.SchedulePickupId
-INNER  JOIN	[DeliveryBackOffice].dbo.VisitPointClient	vpc		WITH (NOLOCK) ON vpc.CodeOfReference = sp.SenderId
-INNER  JOIN	[DeliveryBackOffice].dbo.Township			tw		WITH (NOLOCK) ON tw.IdTownShip = sp.TownshipId 
-LEFT   JOIN	[DeliveryBackOffice].dbo.CatTypeVehicle		ctv		WITH (NOLOCK) ON ctv.IdTypeVehicle = sp.TypeVehicleId
-LEFT   JOIN	[DeliveryBackOffice].dbo.RolByUserByAccount rbuba	WITH (NOLOCK) ON rbuba.RuaIdAccount = sp.AccountId
-LEFT   JOIN	[DeliveryBackOffice].dbo.RegisterUser		ru		WITH (NOLOCK) ON ru.UsrIdUser = rbuba.RuaIdUser
-LEFT   JOIN	[DeliveryBackOffice].dbo.Province			pv		WITH (NOLOCK) ON pv.IdProvince = tw.IdProvince
-INNER  JOIN	[DeliveryBackOffice].dbo.RouteAssigment		ra		WITH (NOLOCK) ON ra.IdRouteAssigment = sm.IdPuRouteAssigment
-WHERE	pv.IdProvince IN (SELECT ProvinceId FROM @tblCollectionProvinceId)
-AND		sp.IsScheduled = 0
-AND		sm.IdPuCourrier is not null 
-AND		ra.IdVehicle is not null
-AND		CAST(sm.DateCreated AS DATE) = @AssignDate
-AND		CAST(sm.DateCreated AS TIME) BETWEEN @StartDate AND @EndDate
-ORDER BY [ORDEN DE RECOLECCION] DESC;
+	END
+
+	ELSE BEGIN
+
+	SELECT	IIF(sp.IsScheduled = 0, CONCAT('RDG', sm.IdServiceManagement), CONCAT('RPG', sm.IdServiceManagement) ) [ORDEN DE RECOLECCION], 
+			IIF(cr.CodeRoute is null, '',cr.CodeRoute) [RUTA],		
+			'Caja' [DESCRIPCION DEL PAQUETE], 
+			'' [CANTIDAD DE PIEZAS], 
+			'' [CODIGO DE ITEM], 
+			sp.SenderId [CODIGO DE PUNTO DE VISITA], 
+			vpc.DescriptionOfClient [NOMBRE DE REMITENTE], 
+			IIF(vpc.Phone is null, '', vpc.Phone ) [TELEFONO DE REMITENTE], 
+			IIF(sp.AccountId is null, IIF(vpc.Email is null, '', vpc.Email), ru.UsrEmail ) [EMAIL REMITENTE], 
+			IIF(vpc.Address is null, '', vpc.Address) [DIRECCION REMITENTE], 
+			IIF(vpc.Latitude is null, '', vpc.Latitude) [LATITUD], 
+			IIF(vpc.Longitude is null, '', vpc.Longitude ) [LONGITUD], 
+			sp.StartDate [FECHA Y HORA MINIMA DE RECOLECCION], 
+			IIF(sp.EndDate is null, '', sp.EndDate) [FECHA Y HORA MAXIMA DE RECOLECCION], 
+			'' [CT DESTINO], 
+			'Solo Recogida' [MODO]
+	FROM		[DeliveryBackOffice].dbo.ServiceManagement	sm		WITH (NOLOCK)
+	INNER  JOIN	[DeliveryBackOffice].dbo.SchedulePickup		sp		WITH (NOLOCK) ON sm.IdSchedulePickup = sp.SchedulePickupId
+	INNER  JOIN	[DeliveryBackOffice].dbo.VisitPointClient	vpc		WITH (NOLOCK) ON vpc.CodeOfReference = sp.SenderId
+	INNER  JOIN	[DeliveryBackOffice].dbo.Township			tw		WITH (NOLOCK) ON tw.IdTownShip = sp.TownshipId 
+	LEFT   JOIN	[DeliveryBackOffice].dbo.CatTypeVehicle		ctv		WITH (NOLOCK) ON ctv.IdTypeVehicle = sp.TypeVehicleId
+	LEFT   JOIN	[DeliveryBackOffice].dbo.RolByUserByAccount rbuba	WITH (NOLOCK) ON rbuba.RuaIdAccount = sp.AccountId
+	LEFT   JOIN	[DeliveryBackOffice].dbo.RegisterUser		ru		WITH (NOLOCK) ON ru.UsrIdUser = rbuba.RuaIdUser
+	LEFT   JOIN	[DeliveryBackOffice].dbo.Province			pv		WITH (NOLOCK) ON pv.IdProvince = tw.IdProvince
+	LEFT   JOIN	[DeliveryBackOffice].dbo.RouteAssigment		ra		WITH (NOLOCK) ON ra.IdRouteAssigment = sm.IdPuRouteAssigment
+	LEFT   JOIN	[DeliveryBackOffice].dbo.CatRoute			cr		WITH (NOLOCK) ON cr.IdRoute = ra.IdRoute
+	WHERE	sp.IsScheduled = 1
+	AND		sm.IdPuCourrier is not null 
+	AND		ra.IdVehicle is not null
+	AND		CAST(sp.StartDate AS DATE) = @AssignDate
+	ORDER BY [ORDEN DE RECOLECCION] DESC;
+
+	END
 
 END
 
 ELSE BEGIN
 
-SELECT	IIF(sp.IsScheduled = 0, CONCAT('RDG', sm.IdServiceManagement), CONCAT('RPG', sm.IdServiceManagement) ) [ORDEN DE RECOLECCION], 
-		IIF(cr.CodeRoute is null, '',cr.CodeRoute) [RUTA],		
-		'Caja' [DESCRIPCION DEL PAQUETE], 
-		'' [CANTIDAD DE PIEZAS], 
-		'' [CODIGO DE ITEM], 
-		sp.SenderId [CODIGO DE PUNTO DE VISITA], 
-		vpc.DescriptionOfClient [NOMBRE DE REMITENTE], 
-		IIF(vpc.Phone is null, '', vpc.Phone ) [TELEFONO DE REMITENTE], 
-		IIF(sp.AccountId is null, IIF(vpc.Email is null, '', vpc.Email), ru.UsrEmail ) [EMAIL REMITENTE], 
-		IIF(vpc.Address is null, '', vpc.Address) [DIRECCION REMITENTE], 
-		IIF(vpc.Latitude is null, '', vpc.Latitude) [LATITUD], 
-		IIF(vpc.Longitude is null, '', vpc.Longitude ) [LONGITUD], 
-		sp.StartDate [FECHA Y HORA MINIMA DE RECOLECCION], 
-		IIF(sp.EndDate is null, '', sp.EndDate) [FECHA Y HORA MAXIMA DE RECOLECCION], 
-		'' [CT DESTINO], 
-		'Solo Recogida' [MODO]
-FROM		[DeliveryBackOffice].dbo.ServiceManagement	sm		WITH (NOLOCK)
-INNER  JOIN	[DeliveryBackOffice].dbo.SchedulePickup		sp		WITH (NOLOCK) ON sm.IdSchedulePickup = sp.SchedulePickupId
-INNER  JOIN	[DeliveryBackOffice].dbo.VisitPointClient	vpc		WITH (NOLOCK) ON vpc.CodeOfReference = sp.SenderId
-INNER  JOIN	[DeliveryBackOffice].dbo.Township			tw		WITH (NOLOCK) ON tw.IdTownShip = sp.TownshipId 
-LEFT   JOIN	[DeliveryBackOffice].dbo.CatTypeVehicle		ctv		WITH (NOLOCK) ON ctv.IdTypeVehicle = sp.TypeVehicleId
-LEFT   JOIN	[DeliveryBackOffice].dbo.RolByUserByAccount rbuba	WITH (NOLOCK) ON rbuba.RuaIdAccount = sp.AccountId
-LEFT   JOIN	[DeliveryBackOffice].dbo.RegisterUser		ru		WITH (NOLOCK) ON ru.UsrIdUser = rbuba.RuaIdUser
-LEFT   JOIN	[DeliveryBackOffice].dbo.Province			pv		WITH (NOLOCK) ON pv.IdProvince = tw.IdProvince
-LEFT   JOIN	[DeliveryBackOffice].dbo.RouteAssigment		ra		WITH (NOLOCK) ON ra.IdRouteAssigment = sm.IdPuRouteAssigment
-LEFT   JOIN	[DeliveryBackOffice].dbo.CatRoute			cr		WITH (NOLOCK) ON cr.IdRoute = ra.IdRoute
-WHERE	pv.IdProvince IN (SELECT ProvinceId FROM @tblCollectionProvinceId)
-AND		sp.IsScheduled = 1
-AND		sm.IdPuCourrier is not null 
-AND		ra.IdVehicle is not null
-AND		CAST(sp.StartDate AS DATE) = @AssignDate
-ORDER BY [ORDEN DE RECOLECCION] DESC;
+	DECLARE @tblCollectionProvinceId 
+	TABLE (ProvinceId INT)
 
+	INSERT INTO @tblCollectionProvinceId (ProvinceId)
+	SELECT SUBSTRING(Item, 1, LEN(Item)) ItemNumber
+	FROM DeliveryBackOffice.dbo.SplitUnlimited(@CollectionProvince, ',')
+
+	IF @TypePickup = 1 BEGIN
+
+	SELECT	IIF(sp.IsScheduled = 0, CONCAT('RDG', sm.IdServiceManagement), CONCAT('RPG', sm.IdServiceManagement) ) [ORDEN DE RECOLECCION], 
+			'' [RUTA], 
+			IIF(ctv.Name = 'Camión', 'Paquete grande', IIF(ctv.Name = 'Panel', 'Paquete mediano', IIF(ctv.Name = 'Motocicleta', 'Paquete pequeño', ''))) [DESCRIPCION DEL PAQUETE], 
+			IIF(sp.QuantityRegularPackages is null, '', sp.QuantityRegularPackages) [CANTIDAD DE PIEZAS], 
+			'' [CODIGO DE ITEM], sp.SenderId [CODIGO DE PUNTO DE VISITA], 
+			vpc.DescriptionOfClient [NOMBRE DE REMITENTE], 
+			IIF(vpc.Phone is null, '', vpc.Phone ) [TELEFONO DE REMITENTE], 
+			IIF(sp.AccountId is null, IIF(vpc.Email is null, '', vpc.Email), ru.UsrEmail ) [EMAIL REMITENTE], 
+			IIF(vpc.Address is null, '', vpc.Address) [DIRECCION REMITENTE], 
+			IIF(vpc.Latitude is null, '', vpc.Latitude) [LATITUD], 
+			IIF(vpc.Longitude is null, '', vpc.Longitude ) [LONGITUD], 
+			sm.DateCreated [FECHA Y HORA MINIMA DE RECOLECCION], 
+			DATEADD(HOUR, 2, sm.DateCreated) [FECHA Y HORA MAXIMA DE RECOLECCION], 
+			'' [CT DESTINO], 
+			'Solo Recogida' [MODO]
+	FROM		[DeliveryBackOffice].dbo.ServiceManagement	sm		WITH (NOLOCK)
+	INNER  JOIN	[DeliveryBackOffice].dbo.SchedulePickup		sp		WITH (NOLOCK) ON sm.IdSchedulePickup = sp.SchedulePickupId
+	INNER  JOIN	[DeliveryBackOffice].dbo.VisitPointClient	vpc		WITH (NOLOCK) ON vpc.CodeOfReference = sp.SenderId
+	INNER  JOIN	[DeliveryBackOffice].dbo.Township			tw		WITH (NOLOCK) ON tw.IdTownShip = sp.TownshipId 
+	LEFT   JOIN	[DeliveryBackOffice].dbo.CatTypeVehicle		ctv		WITH (NOLOCK) ON ctv.IdTypeVehicle = sp.TypeVehicleId
+	LEFT   JOIN	[DeliveryBackOffice].dbo.RolByUserByAccount rbuba	WITH (NOLOCK) ON rbuba.RuaIdAccount = sp.AccountId
+	LEFT   JOIN	[DeliveryBackOffice].dbo.RegisterUser		ru		WITH (NOLOCK) ON ru.UsrIdUser = rbuba.RuaIdUser
+	LEFT   JOIN	[DeliveryBackOffice].dbo.Province			pv		WITH (NOLOCK) ON pv.IdProvince = tw.IdProvince
+	INNER  JOIN	[DeliveryBackOffice].dbo.RouteAssigment		ra		WITH (NOLOCK) ON ra.IdRouteAssigment = sm.IdPuRouteAssigment
+	WHERE	pv.IdProvince IN (SELECT ProvinceId FROM @tblCollectionProvinceId)
+	AND		sp.IsScheduled = 0
+	AND		sm.IdPuCourrier is not null 
+	AND		ra.IdVehicle is not null
+	AND		CAST(sm.DateCreated AS DATE) = @AssignDate
+	AND		CAST(sm.DateCreated AS TIME) BETWEEN @StartDate AND @EndDate
+	ORDER BY [ORDEN DE RECOLECCION] DESC;
+
+	END
+
+	ELSE BEGIN
+
+	SELECT	IIF(sp.IsScheduled = 0, CONCAT('RDG', sm.IdServiceManagement), CONCAT('RPG', sm.IdServiceManagement) ) [ORDEN DE RECOLECCION], 
+			IIF(cr.CodeRoute is null, '',cr.CodeRoute) [RUTA],		
+			'Caja' [DESCRIPCION DEL PAQUETE], 
+			'' [CANTIDAD DE PIEZAS], 
+			'' [CODIGO DE ITEM], 
+			sp.SenderId [CODIGO DE PUNTO DE VISITA], 
+			vpc.DescriptionOfClient [NOMBRE DE REMITENTE], 
+			IIF(vpc.Phone is null, '', vpc.Phone ) [TELEFONO DE REMITENTE], 
+			IIF(sp.AccountId is null, IIF(vpc.Email is null, '', vpc.Email), ru.UsrEmail ) [EMAIL REMITENTE], 
+			IIF(vpc.Address is null, '', vpc.Address) [DIRECCION REMITENTE], 
+			IIF(vpc.Latitude is null, '', vpc.Latitude) [LATITUD], 
+			IIF(vpc.Longitude is null, '', vpc.Longitude ) [LONGITUD], 
+			sp.StartDate [FECHA Y HORA MINIMA DE RECOLECCION], 
+			IIF(sp.EndDate is null, '', sp.EndDate) [FECHA Y HORA MAXIMA DE RECOLECCION], 
+			'' [CT DESTINO], 
+			'Solo Recogida' [MODO]
+	FROM		[DeliveryBackOffice].dbo.ServiceManagement	sm		WITH (NOLOCK)
+	INNER  JOIN	[DeliveryBackOffice].dbo.SchedulePickup		sp		WITH (NOLOCK) ON sm.IdSchedulePickup = sp.SchedulePickupId
+	INNER  JOIN	[DeliveryBackOffice].dbo.VisitPointClient	vpc		WITH (NOLOCK) ON vpc.CodeOfReference = sp.SenderId
+	INNER  JOIN	[DeliveryBackOffice].dbo.Township			tw		WITH (NOLOCK) ON tw.IdTownShip = sp.TownshipId 
+	LEFT   JOIN	[DeliveryBackOffice].dbo.CatTypeVehicle		ctv		WITH (NOLOCK) ON ctv.IdTypeVehicle = sp.TypeVehicleId
+	LEFT   JOIN	[DeliveryBackOffice].dbo.RolByUserByAccount rbuba	WITH (NOLOCK) ON rbuba.RuaIdAccount = sp.AccountId
+	LEFT   JOIN	[DeliveryBackOffice].dbo.RegisterUser		ru		WITH (NOLOCK) ON ru.UsrIdUser = rbuba.RuaIdUser
+	LEFT   JOIN	[DeliveryBackOffice].dbo.Province			pv		WITH (NOLOCK) ON pv.IdProvince = tw.IdProvince
+	LEFT   JOIN	[DeliveryBackOffice].dbo.RouteAssigment		ra		WITH (NOLOCK) ON ra.IdRouteAssigment = sm.IdPuRouteAssigment
+	LEFT   JOIN	[DeliveryBackOffice].dbo.CatRoute			cr		WITH (NOLOCK) ON cr.IdRoute = ra.IdRoute
+	WHERE	pv.IdProvince IN (SELECT ProvinceId FROM @tblCollectionProvinceId)
+	AND		sp.IsScheduled = 1
+	AND		sm.IdPuCourrier is not null 
+	AND		ra.IdVehicle is not null
+	AND		CAST(sp.StartDate AS DATE) = @AssignDate
+	ORDER BY [ORDEN DE RECOLECCION] DESC;
+
+	END
 END
 END;
