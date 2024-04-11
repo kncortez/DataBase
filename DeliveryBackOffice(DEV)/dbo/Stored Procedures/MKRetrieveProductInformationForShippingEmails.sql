95158e4d64f3384a3365bd6e435ee350ffbdc268
EdelmanV <edelman.vasquez@forzalatam.com>
2024-04-11 08:52:03 -0600

    #modificar URL de usuarios con cuenta activa

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/MKRetrieveProductInformationForShippingEmails.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/MKRetrieveProductInformationForShippingEmails.sql
@@ -11,8 +11,9 @@ AS
 
 BEGIN
 
-     DECLARE  @URL_USER_LOGIN VARCHAR (200) = 'https://develop.portal.forzadelivery.com/notification/gift-create-account';
-	 DECLARE  @URL_USER_NOT_LOGIN VARCHAR (200)='https://develop.portal.forzadelivery.com';
+
+	DECLARE  @URL_USER_LOGIN VARCHAR (200) = 'https://develop.portal.forzadelivery.com';
+	DECLARE  @URL_USER_NOT_LOGIN VARCHAR (200)= 'https://develop.portal.forzadelivery.com/notification/gift-create-account';
 
 	
 		IF(@Telemarketing = 0)
