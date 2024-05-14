093b77dc909ccd50a0e5da381ef0e2afe7d4c2ca
FLG\rene.rodriguez <oscar.vasquez@forzadelivery.com>
2024-05-14 08:41:04 -0600

    2024-05-14 Oscar Rodriguez ADD Se agrego estructura para Creacion y modificacion de tablas

--- DeliveryBackOffice(DEV)/dbo/Tables/Customer.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/Customer.sql
@@ -1,177 +1,107 @@
-﻿CREATE TABLE [dbo].[Customer](
-	[IdCustomer] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [nvarchar](100) NOT NULL,
-	[Description] [nvarchar](100) NULL,
-	[Domain] [nvarchar](50) NOT NULL,
-	[RegexSubject] [nvarchar](100) NOT NULL,
-	[RegexEmail] [nvarchar](100) NOT NULL,
-	[RegexFilename] [nvarchar](100) NOT NULL,
-	[Abbreviation] [nvarchar](25) NOT NULL,
-	[IdCustomerType] [int] NULL,
-	[ConditionOfPaymentID] [int] NULL,
-	[CountryID] [varchar](2) NULL,
-	[CommercialName] [nvarchar](50) NULL,
-	[CustomerPhone] [nvarchar](50) NULL,
-	[WebsiteURI] [nvarchar](50) NULL,
-	[ContactName] [nvarchar](50) NULL,
-	[ContactEmail] [nvarchar](50) NULL,
-	[NotificationAddress] [nvarchar](200) NULL,
-	[SaleAdvisorID] [int] NULL,
-	[DateUpService] [datetime] NULL,
-	[DateDownService] [datetime] NULL,
-	[TypeOfBusinessID] [int] NULL,
-	[BusinessSegmentID] [int] NULL,
-	[BusinessActivityID] [int] NULL,
-	[CommercialSegmentID] [int] NULL,
-	[OperationContactName] [nvarchar](50) NULL,
-	[OperationContactPhone] [nvarchar](50) NULL,
-	[OperationContactEmail] [nvarchar](50) NULL,
-	[LegalSponsorName] [nvarchar](50) NULL,
-	[LegalSponsorLastName] [nvarchar](50) NULL,
-	[LegalSponsorDPI] [nvarchar](50) NULL,
-	[HasAgreement] [bit] NULL,
-	[AgreementNumber] [nvarchar](50) NULL,
-	[AgreementDateStart] [datetime] NULL,
-	[AgreementDateEnd] [datetime] NULL,
-	[InvoiceName] [nvarchar](100) NULL,
-	[TaxIdentificationNumber] [nvarchar](50) NULL,
-	[FiscalAddress] [nvarchar](200) NULL,
-	[InvoiceEmail] [nvarchar](50) NULL,
-	[InvoiceContactName] [nvarchar](50) NULL,
-	[InvoiceContactPhone] [nvarchar](50) NULL,
-	[InvoiceContactEmail] [nvarchar](50) NULL,
-	[CODAccountBankID] [int] NULL,
-	[CODAccountNumber] [nvarchar](50) NULL,
-	[CODAccountName] [nvarchar](50) NULL,
-	[CODAccountTypeID] [int] NULL,
-	[CODCurrencyID] [int] NULL,
-	[CODContactName] [nvarchar](50) NULL,
-	[CODContactPhone] [nvarchar](50) NULL,
-	[CODContactEmail] [nvarchar](50) NULL,
-	[RowSatus] [bit] NULL,
-	[TokenCreated] [nvarchar](50) NULL,
-	[DateCreated] [datetime] NULL,
-	[TokenUpdated] [nvarchar](50) NULL,
-	[DateUpdated] [datetime] NULL,
-	[SAPCardCode] [nvarchar](50) NULL,
-	[ExcludePriceShippingCOD] [bit] NULL,
-	[ExcludeCommissionCOD] [bit] NULL,
-	[CatBatchTypeCODId] [bigint] NULL,
-	[CatBatchFrequencyCODId] [bigint] NULL,
-	[CatTMSalesPersonId] [int] NULL,
-	[CutOffDate] [datetime] NULL,
-	[UpgradeDate] [datetime] NULL,
-	[CustomerGoalQuantity] [int] NULL,
-	[CatBillingTimeId] [int] NULL,
-	[CatBillingVolumeId] [int] NULL,
-	[BillingCut_offDate] [date] NULL,
-	[NumImgEvidence] [int] NULL,
-	[IsCOD] [int] NULL,
- CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED 
-(
-	[IdCustomer] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+﻿CREATE TABLE [dbo].[Customer] (
+    [IdCustomer]              INT            IDENTITY (1, 1) NOT NULL,
+    [Name]                    NVARCHAR (100) NOT NULL,
+    [Description]             NVARCHAR (100) NULL,
+    [Domain]                  NVARCHAR (50)  NOT NULL,
+    [RegexSubject]            NVARCHAR (100) NOT NULL,
+    [RegexEmail]              NVARCHAR (100) NOT NULL,
+    [RegexFilename]           NVARCHAR (100) NOT NULL,
+    [Abbreviation]            NVARCHAR (25)  NOT NULL,
+    [IdCustomerType]          INT            NULL,
+    [ConditionOfPaymentID]    INT            NULL,
+    [CountryID]               VARCHAR (2)    NULL,
+    [CommercialName]          NVARCHAR (50)  NULL,
+    [CustomerPhone]           NVARCHAR (50)  NULL,
+    [WebsiteURI]              NVARCHAR (50)  NULL,
+    [ContactName]             NVARCHAR (50)  NULL,
+    [ContactEmail]            NVARCHAR (50)  NULL,
+    [NotificationAddress]     NVARCHAR (200) NULL,
+    [SaleAdvisorID]           INT            NULL,
+    [DateUpService]           DATETIME       NULL,
+    [DateDownService]         DATETIME       NULL,
+    [TypeOfBusinessID]        INT            NULL,
+    [BusinessSegmentID]       INT            NULL,
+    [BusinessActivityID]      INT            NULL,
+    [CommercialSegmentID]     INT            NULL,
+    [OperationContactName]    NVARCHAR (50)  NULL,
+    [OperationContactPhone]   NVARCHAR (50)  NULL,
+    [OperationContactEmail]   NVARCHAR (50)  NULL,
+    [LegalSponsorName]        NVARCHAR (50)  NULL,
+    [LegalSponsorLastName]    NVARCHAR (50)  NULL,
+    [LegalSponsorDPI]         NVARCHAR (50)  NULL,
+    [HasAgreement]            BIT            NULL,
+    [AgreementNumber]         NVARCHAR (50)  NULL,
+    [AgreementDateStart]      DATETIME       NULL,
+    [AgreementDateEnd]        DATETIME       NULL,
+    [InvoiceName]             NVARCHAR (100) NULL,
+    [TaxIdentificationNumber] NVARCHAR (50)  NULL,
+    [FiscalAddress]           NVARCHAR (200) NULL,
+    [InvoiceEmail]            NVARCHAR (50)  NULL,
+    [InvoiceContactName]      NVARCHAR (50)  NULL,
+    [InvoiceContactPhone]     NVARCHAR (50)  NULL,
+    [InvoiceContactEmail]     NVARCHAR (50)  NULL,
+    [CODAccountBankID]        INT            NULL,
+    [CODAccountNumber]        NVARCHAR (50)  NULL,
+    [CODAccountName]          NVARCHAR (50)  NULL,
+    [CODAccountTypeID]        INT            NULL,
+    [CODCurrencyID]           INT            NULL,
+    [CODContactName]          NVARCHAR (50)  NULL,
+    [CODContactPhone]         NVARCHAR (50)  NULL,
+    [CODContactEmail]         NVARCHAR (50)  NULL,
+    [RowSatus]                BIT            NULL,
+    [TokenCreated]            NVARCHAR (50)  NULL,
+    [DateCreated]             DATETIME       NULL,
+    [TokenUpdated]            NVARCHAR (50)  NULL,
+    [DateUpdated]             DATETIME       NULL,
+    [SAPCardCode]             NVARCHAR (50)  NULL,
+    [ExcludePriceShippingCOD] BIT            NULL,
+    [ExcludeCommissionCOD]    BIT            NULL,
+    [CatBatchTypeCODId]       BIGINT         NULL,
+    [CatBatchFrequencyCODId]  BIGINT         NULL,
+    [CatTMSalesPersonId]      INT            NULL,
+    [CutOffDate]              DATETIME       NULL,
+    [UpgradeDate]             DATETIME       NULL,
+    [CustomerGoalQuantity]    INT            NULL,
+    [CatBillingTimeId]        INT            NULL,
+    [CatBillingVolumeId]      INT            NULL,
+    [BillingCut_offDate]      DATE           NULL,
+    [NumImgEvidence]          INT            NULL,
+	[IsCOD]                   INT            NULL,
+    CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED ([IdCustomer] ASC),
+    CONSTRAINT [FK_Customer_CatBankAccountType] FOREIGN KEY ([CODAccountTypeID]) REFERENCES [dbo].[CatBankAccountType] ([IdBankAccountType]),
+    CONSTRAINT [FK_Customer_CatBillingTime] FOREIGN KEY ([CatBillingTimeId]) REFERENCES [dbo].[CatBillingTime] ([IdCatBillingTime]),
+    CONSTRAINT [FK_Customer_CatBillingVolume] FOREIGN KEY ([CatBillingVolumeId]) REFERENCES [dbo].[CatBillingVolume] ([IdCatBillingVolume]),
+    CONSTRAINT [FK_Customer_CatBusinessActivity] FOREIGN KEY ([BusinessActivityID]) REFERENCES [dbo].[CatBusinessActivity] ([IdBusinessActivity]),
+    CONSTRAINT [FK_Customer_CatBusinessSegment] FOREIGN KEY ([BusinessSegmentID]) REFERENCES [dbo].[CatBusinessSegment] ([IdBusinessSegment]),
+    CONSTRAINT [FK_Customer_CatCommercialSegment] FOREIGN KEY ([CommercialSegmentID]) REFERENCES [dbo].[CatCommercialSegment] ([IdCommercialSegment]),
+    CONSTRAINT [FK_Customer_CatConditionOfPayment] FOREIGN KEY ([ConditionOfPaymentID]) REFERENCES [dbo].[CatConditionOfPayment] ([IdConditionOfPayment]),
+    CONSTRAINT [FK_Customer_CatCountry] FOREIGN KEY ([CountryID]) REFERENCES [dbo].[CatCountry] ([IdCountry]),
+    CONSTRAINT [FK_Customer_CatSaleAdvisor] FOREIGN KEY ([SaleAdvisorID]) REFERENCES [dbo].[CatSaleAdvisor] ([IdSaleAdvisor]),
+    CONSTRAINT [FK_Customer_CatTMSalesPerson] FOREIGN KEY ([CatTMSalesPersonId]) REFERENCES [dbo].[CatTMSalesPerson] ([IdCatTMSalesPerson]),
+    CONSTRAINT [FK_Customer_CatTypeOfBusiness] FOREIGN KEY ([TypeOfBusinessID]) REFERENCES [dbo].[CatTypeOfBusiness] ([IdTypeOfBusiness]),
+    CONSTRAINT [FK_Customer_CustomerType] FOREIGN KEY ([IdCustomerType]) REFERENCES [dbo].[CustomerType] ([IdCustomerType]),
+    CONSTRAINT [FK_Customer_DeliveryBank] FOREIGN KEY ([CODAccountBankID]) REFERENCES [dbo].[DeliveryBank] ([Id_bank]),
+    CONSTRAINT [FK_Customer_DeliveryCurrency] FOREIGN KEY ([CODCurrencyID]) REFERENCES [dbo].[DeliveryCurrency] ([Currency_Id])
+);
 
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_CatBankAccountType] FOREIGN KEY([CODAccountTypeID])
-REFERENCES [dbo].[CatBankAccountType] ([IdBankAccountType])
-GO
 
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CatBankAccountType]
-GO
 
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_CatBillingTime] FOREIGN KEY([CatBillingTimeId])
-REFERENCES [dbo].[CatBillingTime] ([IdCatBillingTime])
-GO
 
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CatBillingTime]
-GO
 
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_CatBillingVolume] FOREIGN KEY([CatBillingVolumeId])
-REFERENCES [dbo].[CatBillingVolume] ([IdCatBillingVolume])
 GO
+CREATE NONCLUSTERED INDEX [idx_IdCustomerType_RowSatus]
+ON [dbo].[Customer]([IdCustomerType] ASC, [RowSatus] ASC)
+INCLUDE([Name], [ConditionOfPaymentID], [CustomerPhone], [ContactEmail], [InvoiceName], [TaxIdentificationNumber], [FiscalAddress], [InvoiceEmail], [CODAccountBankID], [CODAccountNumber], [CODAccountName], [CODAccountTypeID]);
 
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CatBillingVolume]
 GO
+CREATE NONCLUSTERED INDEX [IDX_Customer_CODContactEmail]
+ON [dbo].[Customer]([CODContactEmail] ASC, [RegexEmail] ASC);
 
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_CatBusinessActivity] FOREIGN KEY([BusinessActivityID])
-REFERENCES [dbo].[CatBusinessActivity] ([IdBusinessActivity])
+
+
+
+
 GO
-
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CatBusinessActivity]
-GO
-
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_CatBusinessSegment] FOREIGN KEY([BusinessSegmentID])
-REFERENCES [dbo].[CatBusinessSegment] ([IdBusinessSegment])
-GO
-
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CatBusinessSegment]
-GO
-
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_CatCommercialSegment] FOREIGN KEY([CommercialSegmentID])
-REFERENCES [dbo].[CatCommercialSegment] ([IdCommercialSegment])
-GO
-
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CatCommercialSegment]
-GO
-
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_CatConditionOfPayment] FOREIGN KEY([ConditionOfPaymentID])
-REFERENCES [dbo].[CatConditionOfPayment] ([IdConditionOfPayment])
-GO
-
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CatConditionOfPayment]
-GO
-
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_CatCountry] FOREIGN KEY([CountryID])
-REFERENCES [dbo].[CatCountry] ([IdCountry])
-GO
-
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CatCountry]
-GO
-
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_CatSaleAdvisor] FOREIGN KEY([SaleAdvisorID])
-REFERENCES [dbo].[CatSaleAdvisor] ([IdSaleAdvisor])
-GO
-
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CatSaleAdvisor]
-GO
-
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_CatTMSalesPerson] FOREIGN KEY([CatTMSalesPersonId])
-REFERENCES [dbo].[CatTMSalesPerson] ([IdCatTMSalesPerson])
-GO
-
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CatTMSalesPerson]
-GO
-
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_CatTypeOfBusiness] FOREIGN KEY([TypeOfBusinessID])
-REFERENCES [dbo].[CatTypeOfBusiness] ([IdTypeOfBusiness])
-GO
-
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CatTypeOfBusiness]
-GO
-
-ALTER TABLE [dbo].[Customer]  WITH NOCHECK ADD  CONSTRAINT [FK_Customer_CustomerType] FOREIGN KEY([IdCustomerType])
-REFERENCES [dbo].[CustomerType] ([IdCustomerType])
-GO
-
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_CustomerType]
-GO
-
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_DeliveryBank] FOREIGN KEY([CODAccountBankID])
-REFERENCES [dbo].[DeliveryBank] ([Id_bank])
-GO
-
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_DeliveryBank]
-GO
-
-ALTER TABLE [dbo].[Customer]  WITH CHECK ADD  CONSTRAINT [FK_Customer_DeliveryCurrency] FOREIGN KEY([CODCurrencyID])
-REFERENCES [dbo].[DeliveryCurrency] ([Currency_Id])
-GO
-
-ALTER TABLE [dbo].[Customer] CHECK CONSTRAINT [FK_Customer_DeliveryCurrency]
-GO
-
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Id para la tabla Customer ' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Customer', @level2type=N'COLUMN',@level2name=N'IdCustomer'
 GO
 
@@ -378,5 +308,3 @@ GO
 
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'La tabla Cliente almacena informacon relacionada con los clientes de la empresa Forza Delivery' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Customer'
 GO
-
-
