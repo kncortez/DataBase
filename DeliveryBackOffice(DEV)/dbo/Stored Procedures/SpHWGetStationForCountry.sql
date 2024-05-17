80d73c9adc1c7427616d313ffaae1371fc712ec1
brandon-p <brandon.pedroza@forzadelivery.com>
2024-05-17 09:05:56 -0600

    #creacion de sp para obtener pais y estacion del usuario

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SpHWGetStationForCountry.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[SpHWGetStationForCountry]
+	@IdUser INT,
+	@Username NVARCHAR(50),
+	@IdCountry NVARCHAR(2)
+-- =============================================
+-- Author:		<Brandon Pedroza>
+-- Create date: <2024-05-16>
+-- Description:	<Obtener estaciones por pais asignadas al usuario en login Hermes Desktop>
+-- =============================================
+AS
+SELECT DISTINCT STA.IdStation	[IdStation],
+				STA.StationName [StationName]
+FROM InternalUser IU
+INNER JOIN RolByUserBySystem RUS
+ON IU.RegisterUserID = RUS.RusIdUser
+INNER JOIN CatStation STA
+ON RUS.StationId = STA.IdStation
+WHERE IU.IdUser = @IdUser
+AND IU.Username = @Username
+AND RUS.StationId IS NOT NULL
+AND RUS.RusIdSystem = 2
+AND STA.CountryId = @IdCountry
---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spGetCountries.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spGetCountries]
+	@IdUser INT,
+	@Username NVARCHAR(50)
+-- =============================================
+-- Author:		<Brandon Pedroza>
+-- Create date: <2024-05-15>
+-- Description:	<Obtener pais que tiene asignado un usuario en login Hermes Desktop>
+-- =============================================
+
+AS
+SELECT DISTINCT C.IdCountry		[IdCountry], 
+			    C.CountryNameES [Name],
+				CASE C.IdCountry WHEN 'HN' THEN '-2' ELSE '-1' END AS[Station]
+FROM DenariusUser_Dev.dbo.LGN_RolByUserByRegion R
+INNER JOIN DeliveryBackOffice.dbo.CatCountry C on  R.RUR_IdCountry = C.IdCountry
+WHERE R.RUR_IdUser =@IdUser AND R.RUR_Username = @Username
