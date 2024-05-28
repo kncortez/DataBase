73a1d5ee8235fcb9e942f5e88401dea2e46ec88d
Azurdia <cristian.azurdia@forzadelivery.com>
2024-05-28 11:04:46 -0600

    -Integración de catalogo dinamico sin creación de JSon desde BD

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetDynamicCatalog_old.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[GetDynamicCatalog_old]
+    @TypeMethod VARCHAR(100) = 'GetTypePayment',
+    @IdAccount INT = 1,
+    @Token VARCHAR(100) = '0BE2F8F3BD53652635746ACD069954B5',
+    @GuideSerie VARCHAR(2) = 'FD',
+    @GuideNumber VARCHAR(100) = '12345',
+    @Others VARCHAR(500) = ''
+AS
+BEGIN
+    IF @IdAccount = '' 
+	BEGIN
+		SET @IdAccount = NULL;
+	END
+
+    IF (@TypeMethod = 'GetCustomerType')
+    BEGIN
+
+		SELECT [idCustomerType], [Description], [CustomerTypeStatus] FROM dbo.CustomerType
+        
+    END;
+    ELSE 
+    BEGIN
+
+		 SELECT '404', 'CONSULTA NO ENCONTRADA' ;
+       
+    END;
+
+END;
