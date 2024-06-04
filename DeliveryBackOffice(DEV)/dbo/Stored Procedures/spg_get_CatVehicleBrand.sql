bdbd1e516d480e2909987c49e6817c0de93389e2
JuanRamirezFD <juan.ramirez@forzadelivery.com>
2024-06-04 13:01:06 -0600

    FDAPI-2408: Ajustes a procedimientos y tablas para obtener información por pais

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_get_CatVehicleBrand.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_get_CatVehicleBrand.sql
@@ -4,10 +4,16 @@
 -- Create date: <2020-03-02>
 -- Description:	<Devuelve todos los vehiculos disponibles>
 -- =============================================
+-- Author:      <Daniel, Ramirez>
+-- Create date: <2024-06-04>
+-- Description: <Se agrega filtro para filtrar por pais, por defecto GT>
+-- =============================================
 CREATE PROCEDURE [dbo].[spg_get_CatVehicleBrand]
-@value bit = 1
+@value bit = 1,
+@IdCountry VARCHAR(2) = 'GT'
 AS
 BEGIN
 	SELECT IdCatVehicleBrand, Name FROM CatVehicleBrand
 	where RowStatus = 1
+      AND IIF(IdCountry IS NULL,'GT',IdCountry) = @IdCountry
 END
