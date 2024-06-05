7177a691b1ce422aaa7b022b4bd829e3c9419a6f
Alberto Ixchop <alberto.ixchop@forzadelivery.com>
2024-06-05 08:28:53 -0600

    Se modifica sp sps_getReprintGuie.sql para manejar el filtro por pais en las tablas catalogo KindOfVPClient y CatBusinessSegment

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_getReprintGuie.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_getReprintGuie.sql
@@ -110,6 +110,7 @@ begin
 			[DeliveryBackOffice].[dbo].[KindOfVPClient] KOVPC  WITH(NOLOCK) 
 		WHERE
 			[KOVPC].[KindOfVPName] = 'Express Center'  --COLLATE Latin1_General_CI_AI 
+		AND ISNULL(IdCountry,'GT')=@CountryThatConsults
 	);
 
     DECLARE @DaysToExpiration INT =
@@ -151,7 +152,7 @@ begin
                                        END;
     DECLARE @ExpressName VARCHAR(50) = '';
 
-	DECLARE @IDCatBusinessB2B INT = (SELECT IdBusinessSegment FROM DBO.CatBusinessSegment WHERE BusinessSegmentName='B2B');
+	DECLARE @IDCatBusinessB2B INT = (SELECT IdBusinessSegment FROM DBO.CatBusinessSegment WHERE BusinessSegmentName='B2B'AND ISNULL(IdCountry,'GT')=@CountryThatConsults);
 
     IF (@Impersonate = 'TRUE')
     BEGIN
