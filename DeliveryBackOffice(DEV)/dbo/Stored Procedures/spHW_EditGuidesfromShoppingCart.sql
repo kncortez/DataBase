590aaa74302804e1314b41688f31773b2796a055
Alberto Ixchop <alberto.ixchop@forzadelivery.com>
2024-06-05 16:14:17 -0600

    FDAPI-2467 Se quita la revalorización para guías que consumen algun beneficio de club forza

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_EditGuidesfromShoppingCart.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_EditGuidesfromShoppingCart.sql
@@ -63,17 +63,28 @@ BEGIN
 				Guide_Number = @GuideNumber  
 			END
 
-			EXEC [dbo].[spws_revalue_guide]
-				@GuideSerie   = @GuideSerie
-				,@GuideNumber = @GuideNumber
-				,@CodeApp = ''
-				,@Format =''
-				,@CalculateTaxes = 'false' -- Dado a nuevas tarifas, no cálcular impuestos
-				,@IdModule = 1
-				,@SetUpdate = 'true' -- Actualizar registros
-				,@Token = @Token
-				,@ParIsCreditCard = @IsTCCPaid
-				,@UseMembership = @GuideUsedMembership
+			--Inicio fix 20240605 para evitar revaorizar guías que hayan aplicado algun beneficio de clubforza
+			DECLARE @useclubforzabenefit bit =0;
+			SELECT @useclubforzabenefit='TRUE'
+			FROM membershipsubscriptionlog
+			WHERE LogguideNumber=@GuideNumber
+			AND LogguideSerie=@GuideSerie;
+
+			IF @useclubforzabenefit='FALSE'
+			BEGIN
+				EXEC [dbo].[spws_revalue_guide]
+					@GuideSerie   = @GuideSerie
+					,@GuideNumber = @GuideNumber
+					,@CodeApp = ''
+					,@Format =''
+					,@CalculateTaxes = 'false' -- Dado a nuevas tarifas, no cálcular impuestos
+					,@IdModule = 1
+					,@SetUpdate = 'true' -- Actualizar registros
+					,@Token = @Token
+					,@ParIsCreditCard = @IsTCCPaid
+					,@UseMembership = 0
+			END
+			--Fin fix 20240605
 
 			COMMIT TRANSACTION
 				SELECT 
