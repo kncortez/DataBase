c1657c84e556ba5a5400459b42878157d6a090a1
EdelmanV <edelman.vasquez@forzalatam.com>
2024-06-13 17:50:11 -0600

    #filtro por país para pilotos

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_getSenderReceiverList.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_getSenderReceiverList.sql
@@ -4,6 +4,7 @@
 -- Description:	<Get complete information list of sender receiver>
 -- =============================================
 CREATE PROCEDURE [dbo].[spHM_getSenderReceiverList] 
+ @IdCountry  NVARCHAR(2) = 'GT'
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -21,5 +22,6 @@ BEGIN
 				[SR].[CUI]
 	FROM		[dbo].[SenderReceiver] SR
 	WHERE		[SR].[Estatus] = 1
+	 AND ISNULL([SR].IdCountry,'GT') =  @IdCountry
 	ORDER BY	[SR].[Last_Name];
 END
