6b87a1616280778d860eca4840c02f2a2985bd61
EdelmanV <edelman.vasquez@forzalatam.com>
2024-06-13 16:44:18 -0600

    #Filtro por país

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_get_CatVehicle.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_get_CatVehicle.sql
@@ -4,11 +4,20 @@
 -- Create date: <2020-02-15>
 -- Description:	<Devuelve todos los vehiculos disponibles>
 -- =============================================
+-- =============================================
+-- Author:		<Edelman>
+-- Create date: <2024-06-13>
+-- Description:	<filtro de vehiculos por país>
+-- =============================================
 CREATE PROCEDURE [dbo].[spg_get_CatVehicle]
+(
+  @IdCountry  NVARCHAR(2) = 'GT'
+)
 AS
 BEGIN
 	SELECT cv.IdVehicle,
 	cv.UnitNumber	
 	FROM [DeliveryBackOffice].[dbo].[CatVehicle] as cv
 	where cv.RowStatus = 1
+	AND ISNULL(cv.IdCountry,'GT') = @IdCountry;
 END
