b327ee4d34aa82ffe308f83e7ca25138f1fd75a1
JuanRamirezFD <juan.ramirez@forzadelivery.com>
2024-06-17 18:53:45 -0600

    FDAPI-2404: datos de tipo de cambio

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-2241/Valores[CurrencyExchangeRates].sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-2241/Valores[CurrencyExchangeRates].sql
@@ -19,7 +19,11 @@
           (15, GETDATE() + 7, 'GT', 1, 2, 7.76241),
           (16, GETDATE() + 7, 'HN', 4, 2, 0.04035),
           (17, GETDATE() + 8, 'GT', 1, 2, 7.76241),
-          (18, GETDATE() + 8, 'HN', 4, 2, 0.04030)
+          (18, GETDATE() + 8, 'HN', 4, 2, 0.04030),
+          (19, GETDATE(), 'GT', 2, 1, 7.752943),
+          (20, GETDATE(), 'HN', 2, 4, 24.672196),
+          (21, GETDATE() + 1, 'GT', 2, 1, 7.753243),
+          (22, GETDATE() + 1, 'HN', 2, 4, 24.692196)
          ) AS T (IdExchange,ExchangeDate,IdCountry,SourceCurrency,TargetCurrency,ExchangeRate)
 
 )
