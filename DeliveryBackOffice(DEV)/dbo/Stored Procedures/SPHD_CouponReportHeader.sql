ed937ead6fd0b289d0656421f6c61e9cf49b7f3e
brandon-p <brandon.pedroza@forzadelivery.com>
2024-06-18 12:36:00 -0600

    FDAPI-2425: #se agrega parametro para filtrar guias por pais de origen. se agrega una columna que indica el codigo ISO de la moneda por defecto que se utiliza segun el parametro idCountry

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_CouponReportHeader.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_CouponReportHeader.sql
@@ -8,11 +8,17 @@
 -- Create date: <Create Date,2022-06-09>
 -- Description:	<Description, MODIFICACIONES, AGREGAR COLUMNA DE EXC Y FILTRO, RESUMEN DE TOTALES DE GENERACIONES DE CUPONES Y DE CANJE>
 -- =============================================
+-- =============================================
+-- Modified:	<Brandon Pedroza>
+-- Create date: <2024-06-18>
+-- Description:	<Se agrega parametro para filtrar guias por pais de origen>
+-- =============================================
 CREATE PROCEDURE [dbo].[SPHD_CouponReportHeader] 
 	-- Add the parameters for the stored procedure here
 	@DateOf       as DATE,
 	@DateTo       as DATE,
-	@IdStation    as INT
+	@IdStation    as INT,
+	@IdCountry	  as NVARCHAR(2) = 'GT'
 AS
 BEGIN
 
@@ -70,8 +76,14 @@ SUM( Isnull(DO.PriceShippment,0)
 	 ) 
 		                                TotalMontoGuideOrigin,
 		FORMAT(@DateOf, 'yyyy-MM-dd')  DateOf,
-		FORMAT(@DateTo, 'yyyy-MM-dd')  DateTo 
-
+		FORMAT(@DateTo, 'yyyy-MM-dd')  DateTo,
+(SELECT CC.CodeISO 
+		FROM DeliveryCurrency DC WITH(NOLOCK)
+		INNER JOIN CatCurrencyCOD CC WITH(NOLOCK)
+		ON DC.IdCurrencyCOD = CC.IdCatCurrencyCOD
+		WHERE DC.Currency_IdCountry = @IdCountry
+		AND DC.DefaultPerCountry = 1
+		) ISOCodeCurrency
 FROM  dbo.PromoCoupon PC         WITH (NOLOCK)
 LEFT JOIN  dbo.DeliveryOrder DO WITH (NOLOCK)
 ON PC.GuideSerieOrigin  = DO.Guide_Serie AND  
@@ -79,6 +91,6 @@ ON PC.GuideSerieOrigin  = DO.Guide_Serie AND
 WHERE PC.DateCreated BETWEEN FORMAT(@DateOf, 'yyyy-MM-dd 00:00:00') 
                          AND FORMAT(@DateTo, 'yyyy-MM-dd 23:59:59')
 						-- AND PC.VisitPointClientOrigin = @CodeOfReference
-						
+						AND ISNULL(DO.SenderCountryId, 'GT') = @IdCountry
 	
 END
