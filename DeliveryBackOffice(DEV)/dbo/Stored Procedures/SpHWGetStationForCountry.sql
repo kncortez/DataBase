0f3b48629ea184d89441557a025681b5c0c55184
EdelmanV <edelman.vasquez@forzalatam.com>
2024-06-19 10:09:18 -0600

    #FDAPI-2553 Filtrar estaciones por sistema

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SpHWGetStationForCountry.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SpHWGetStationForCountry.sql
@@ -1,22 +1,30 @@
-﻿CREATE PROCEDURE [dbo].[SpHWGetStationForCountry]
-	@IdUser INT,
-	@Username NVARCHAR(50),
-	@IdCountry NVARCHAR(2)
--- =============================================
+﻿-- =============================================
 -- Author:		<Brandon Pedroza>
 -- Create date: <2024-05-16>
 -- Description:	<Obtener estaciones por pais asignadas al usuario en login Hermes Desktop>
 -- =============================================
+-- =============================================
+-- Author:		<Edelman>
+-- Create date: <2024-06-19>
+-- Description:	<Agregar filtro de sistema Hermes Mobile Linehaul>
+-- =============================================
+ALTER PROCEDURE [dbo].[SpHWGetStationForCountry]
+	@IdUser INT,
+	@Username NVARCHAR(50),
+	@IdCountry NVARCHAR(2),
+	@IdSystem INT = 2
+
 AS
-SELECT DISTINCT STA.IdStation	[IdStation],
-				STA.StationName [StationName]
+
+SET NOCOUNT ON;
+
+SELECT DISTINCT STA.IdStation [IdStation],
+                STA.StationName [StationName]
 FROM InternalUser IU WITH(NOLOCK)
-INNER JOIN RolByUserBySystem RUS WITH(NOLOCK)
-ON IU.RegisterUserID = RUS.RusIdUser
-INNER JOIN CatStation STA WITH(NOLOCK)
-ON RUS.StationId = STA.IdStation
+INNER JOIN RolByUserBySystem RUS WITH(NOLOCK) ON IU.RegisterUserID = RUS.RusIdUser
+INNER JOIN CatStation STA WITH(NOLOCK) ON RUS.StationId = STA.IdStation
 WHERE IU.IdUser = @IdUser
-AND IU.Username = @Username
-AND RUS.StationId IS NOT NULL
-AND RUS.RusIdSystem = 2
-AND STA.CountryId = @IdCountry
+  AND IU.Username = @Username
+  AND RUS.StationId IS NOT NULL
+  AND RUS.RusIdSystem = @IdSystem
+  AND STA.CountryId = @IdCountry
