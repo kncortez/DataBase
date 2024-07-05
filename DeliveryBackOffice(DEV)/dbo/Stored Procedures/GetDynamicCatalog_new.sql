1a52de85d21dc1f475dfd8b818d5941dbc0440b3
brandon-p <brandon.pedroza@forzadelivery.com>
2024-07-05 16:40:11 -0600

    FDAPI-2616: # se agrega filtro por pais a sp utilizados en modulo control de calidad, logistica inversa

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetDynamicCatalog_new.sql
@@ -0,0 +1,151 @@
+﻿-- =============================================
+-- Modified:	<Brandon, Pedroza >
+-- Create date: <2024-07-05>
+-- Description:	<Se agrega parametro para filtrar por pais, GT por defecto>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetDynamicCatalog_new]
+    @TypeMethod VARCHAR(100) = 'GetCustomerType'
+  , @IdAccount INT = null
+  , @Token VARCHAR(100) = '0BE2F8F3BD53652635746ACD069954B5'
+  , @GuideSerie VARCHAR(2) = 'FD'
+  , @GuideNumber VARCHAR(100) = '12345'
+  , @Others VARCHAR(500) = ''
+  , @IdCountry NVARCHAR(2) = 'GT'
+AS
+BEGIN
+
+	IF @IdAccount = '' 
+	BEGIN
+		SET @IdAccount = NULL;
+	END
+
+    IF (@TypeMethod = 'GetCustomerType')
+    BEGIN
+
+		SELECT [idCustomerType], [Description], [CustomerTypeStatus] 
+		FROM [DeliveryBackOffice].[dbo].[CustomerType] WITH (NOLOCK)
+        
+    END;
+	ELSE IF (@TypeMethod = 'GetClientActive')
+    BEGIN
+		
+			IF @Others = ''
+			BEGIN
+				SELECT  [idCustomer], 
+						CONCAT(dbo.fnt_String_Escape([Name],'json'), ' ', dbo.fnt_String_Escape([Description],'json')) AS [Customer]
+				FROM
+					[DeliveryBackOffice].[dbo].[Customer] WITH (NOLOCK)
+				WHERE RowSatus = 1
+					AND IdCustomerType = 1
+					AND ISNULL(CountryID, 'GT')= @IdCountry
+				UNION 
+				SELECT 81 [idCustomer], 'FDC EXPRESS CENTER' [Customer]
+			END
+			ELSE
+			BEGIN
+				IF EXISTS (SELECT Item FROM dbo.SplitUnlimited(@Others, ',') WHERE Item = 2 )
+				BEGIN
+					SELECT [idCustomer], 
+							CONCAT(dbo.fnt_String_Escape([Name],'json'), ' ', dbo.fnt_String_Escape([Description],'json')) AS [Customer]
+					FROM [DeliveryBackOffice].[dbo].[Customer] WITH (NOLOCK)
+					WHERE	RowSatus = 1
+							AND IdCustomerType IN (
+													SELECT Item 
+													FROM dbo.SplitUnlimited(@Others, ',') 
+													WHERE Item <> 2 -- Omite Redistribuidores
+												   )
+							AND ISNULL(CountryID, 'GT')= @IdCountry
+					UNION 
+						SELECT 81 [idCustomer], 'FD EXPRESS CENTER' [Customer]
+				END
+				ELSE
+				BEGIN
+					SELECT [idCustomer], 
+							CONCAT(dbo.fnt_String_Escape([Name],'json'), ' ', dbo.fnt_String_Escape([Description],'json')) AS [Customer]
+					FROM [DeliveryBackOffice].[dbo].[Customer] WITH (NOLOCK)
+					WHERE	RowSatus = 1
+							AND IdCustomerType IN ( SELECT Item FROM dbo.SplitUnlimited(@Others, ',') )
+							AND ISNULL(CountryID, 'GT')= @IdCountry
+				END
+			END
+    END;
+	ELSE IF (@TypeMethod = 'GetActiveVisitPointByClient')
+    BEGIN
+		
+		IF @Others = ''
+		BEGIN
+				SELECT vpc.[CodeOfReference], CONCAT( dbo.fnt_String_Escape(c.[Description],'json'), ' ',  dbo.fnt_String_Escape(vpc.[DescriptionOfClient],'json') ) ClientVisitPoint
+				FROM [DeliveryBackOffice].[dbo].[Customer]					c   WITH (NOLOCK)
+					INNER JOIN [DeliveryBackOffice].[dbo].[VisitPointClient] vpc WITH (NOLOCK)
+						ON c.IdCustomer = vpc.CustomerID
+				WHERE  IdCustomerType = 1
+					  AND c.RowSatus = 1
+					  AND vpc.StatusClient = 1
+					  AND ISNULL(vpc.CountryId, 'GT')= @IdCountry
+				UNION
+				SELECT A2.[CodeOfReference], CONCAT(A1.[Description] , ' ' , A2.[DescriptionOfClient])
+				FROM [DeliveryBackOffice].[dbo].[Customer] A1
+					INNER JOIN [DeliveryBackOffice].[dbo].[VisitPointClient] A2
+					ON A2.CustomerID = A1.IdCustomer
+				WHERE	  IdCustomerType = 2 
+					AND A2.StatusClient = 1
+					AND A2.IdKindOfVPClient = 1	
+					AND ISNULL(A2.CountryId, 'GT')= @IdCountry
+		END
+		ELSE
+		BEGIN
+			IF EXISTS (SELECT Item FROM dbo.SplitUnlimited(@Others, ',') WHERE Item = 81 )
+			BEGIN
+					SELECT vpc.[CodeOfReference], CONCAT( dbo.fnt_String_Escape(c.[Description],'json'), ' ',  dbo.fnt_String_Escape(vpc.[DescriptionOfClient],'json') ) ClientVisitPoint
+					FROM [DeliveryBackOffice].[dbo].[Customer]					c   WITH (NOLOCK)
+						INNER JOIN [DeliveryBackOffice].[dbo].[VisitPointClient] vpc WITH (NOLOCK)
+							ON c.IdCustomer = vpc.CustomerID
+					WHERE c.IdCustomer  IN (SELECT Item FROM dbo.SplitUnlimited(@Others, ',') WHERE Item <> 2339 )-- Omite Redistribuidores
+						  AND IdCustomerType = 1
+						  AND c.RowSatus = 1
+						  AND vpc.StatusClient = 1
+						  AND ISNULL(vpc.CountryId, 'GT')= @IdCountry
+					UNION
+					SELECT A2.[CodeOfReference], CONCAT(A1.[Description] , ' ' , A2.[DescriptionOfClient])
+					FROM [DeliveryBackOffice].[dbo].[Customer] A1
+						INNER JOIN [DeliveryBackOffice].[dbo].[VisitPointClient] A2
+						ON A2.CustomerID = A1.IdCustomer
+					WHERE	  IdCustomerType = 2 
+						AND A2.StatusClient = 1
+						AND A2.IdKindOfVPClient = 1	
+						AND ISNULL(A2.CountryId, 'GT')= @IdCountry
+			END
+			ELSE
+			BEGIN
+
+					SELECT vpc.[CodeOfReference], CONCAT( dbo.fnt_String_Escape(c.[Description],'json'), ' ',  dbo.fnt_String_Escape(vpc.[DescriptionOfClient],'json') ) ClientVisitPoint
+					FROM [DeliveryBackOffice].[dbo].[Customer]					c   WITH (NOLOCK)
+						INNER JOIN [DeliveryBackOffice].[dbo].[VisitPointClient] vpc WITH (NOLOCK)
+							ON c.IdCustomer = vpc.CustomerID
+					WHERE c.IdCustomer  IN (SELECT Item FROM dbo.SplitUnlimited(@Others, ',')) 
+						  AND IdCustomerType = 1
+						  AND c.RowSatus = 1
+						  AND vpc.StatusClient = 1
+						  AND ISNULL(vpc.CountryId, 'GT')= @IdCountry
+			END
+		END
+		
+    END;
+	ELSE IF (@TypeMethod = 'GetActiveTypeIncidenceDelivery')
+    BEGIN
+
+		SELECT [IdIncidenceType], [NameIncidence]
+		FROM [DeliveryBackOffice].[dbo].CatTypeIncidence
+		WHERE ServiceType = 'DELIVERY'
+			  AND RowStatus = 1
+			  AND ISNULL(CountryID, 'GT')= @IdCountry
+
+    END;
+    ELSE 
+    BEGIN
+
+		 SELECT '404', 'CONSULTA NO ENCONTRADA' ;
+       
+    END;
+
+END;
--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphwGetCatCountry.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphwGetCatCountry.sql
@@ -1,23 +1,15 @@
-﻿
+﻿-- =============================================
+-- Author:		<Brandon, Pedroza >
+-- Create date: <2024-07-04>
+-- Description:	<Obtiene el listado de paises>
 -- =============================================
--- Author:		<Alberto, Ixchop>
--- Create date: <22-09-2022>
--- Description:	<Método que obitene el catálogo de hubs activos>
--- =============================================
--- Modified:	<Brandon, Pedroza >
--- Create date: <2024-07-05>
--- Description:	<Se agrega parametro para filtrar por pais, GT por defecto>
--- =============================================
-CREATE PROCEDURE [dbo].[sphw_GetCatHubs]
-	-- Add the parameters for the stored procedure here
-	@IdCountry AS NVARCHAR(2) = 'GT'
+CREATE PROCEDURE [dbo].[sphwGetCatCountry] 
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
-    DECLARE @TranCounter INT;  
+	    DECLARE @TranCounter INT;  
     SET @TranCounter = @@TRANCOUNT;  
     IF @TranCounter > 0  
         SAVE TRANSACTION SPGetPendingRecolectionServices;  
@@ -28,15 +20,11 @@ BEGIN
 		SELECT 
 			1 AS 'StatusCode', 
 			'Registros obtenidos' AS 'Description';
-		SELECT 
-			IdHubLogistic,
-			HubName,
-			HubAbbreviation,
-			IdStation,
-			IdCountry
-		FROM DBO.HubLogistics 
-			WHERE HubStatus =1
-			AND ISNULL(IdCountry,'GT') = @IdCountry;
+		SELECT   [IdCountry]  [IdCountry]
+			,UPPER([CountryNameES])  [CountryName]
+		FROM [DeliveryBackOffice].[dbo].[CatCountry]
+			WHERE CountryRowStatus  = 'TRUE'
+				  AND IdCountry in ('GT','HN') 
 
 		IF @TranCounter = 0  
             COMMIT TRANSACTION;  
