23c1ae931961d81170b74626bf6d9ddbb5ca9889
brandon-p <brandon.pedroza@forzadelivery.com>
2024-07-08 12:52:20 -0600

    FDAPI-2617: # se versiona el sp GetQualityControlData y sus tipos de tabla, ya que no se encontraba actualizado en la rama actual. se le agrega 2 campos que indican el simbolo de la moneda para el precio de envio y COD

---
+++ DeliveryBackOffice(DEV)/dbo/User Defined Types/TblCustomer.sql
@@ -0,0 +1,7 @@
+﻿CREATE TYPE [dbo].[TblCustomer] AS TABLE(
+	[IdCustomer] [int] NOT NULL,
+	PRIMARY KEY CLUSTERED 
+(
+	[IdCustomer] ASC
+)WITH (IGNORE_DUP_KEY = OFF)
+)
---
+++ DeliveryBackOffice(DEV)/dbo/User Defined Types/TblVisitPointClient.sql
@@ -0,0 +1,7 @@
+﻿CREATE TYPE [dbo].[TblVisitPointClient] AS TABLE(
+	[IdVisitPointClient] [int] NOT NULL,
+	PRIMARY KEY CLUSTERED 
+(
+	[IdVisitPointClient] ASC
+)WITH (IGNORE_DUP_KEY = OFF)
+)
