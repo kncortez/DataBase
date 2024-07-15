96c7fcc2dcf7417b5213e99400bbc018304c759f
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2024-07-15 12:20:59 -0600

    FDAPI-2678: Se modifico el sp LoadProductCategory para agregar la URL de imagen por categoría.

--- DeliveryBackOffice(DEV)/dbo/Tables/CatProductCategory.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/CatProductCategory.sql
@@ -10,6 +10,7 @@ CREATE TABLE [dbo].[CatProductCategory](
 	[TokenUpdated] [nvarchar](50) NULL,
 	[DateUpdated] [datetime] NULL,
     [TechnicalDescription] NVARCHAR(50) NULL, 
+    [ImageURL] NVARCHAR(255) NULL, 
     CONSTRAINT [PK_CatProductCategory] PRIMARY KEY CLUSTERED ([IdCatProductCategory] ASC)
 )
 GO
