7168b211386a6c07c2898026de4d1329e60acb4d
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2024-07-16 17:59:37 -0600

    FDAPI-2695 Se agrega a la consulta el id y el nombre de la clasificación de la incidencia

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetDynamicCatalog_new.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetDynamicCatalog_new.sql
@@ -120,10 +120,15 @@ BEGIN
 	ELSE IF (@TypeMethod = 'GetActiveTypeIncidenceDelivery')
     BEGIN
 
-		SELECT [IdIncidenceType], [NameIncidence]
-		FROM [DeliveryBackOffice].[dbo].CatTypeIncidence
-		WHERE ServiceType = 'DELIVERY'
-			  AND RowStatus = 1
+		SELECT cti.IdIncidenceType									[IncidenceId],
+				cti.NameIncidence									[IncidenceName],
+				cti.IncidenceClasificationId						[IncidenceClasificationId],
+				cic.IncidenceTypeName								[IncidenceClasificationName]
+		FROM [DeliveryBackOffice].[dbo].CatTypeIncidence AS cti WITH(NOLOCK)
+			INNER JOIN [DeliveryBackOffice].[dbo].[CatIncidenceClasification] AS cic WITH(NOLOCK) 
+								ON cti.IncidenceClasificationId = cic.IdCatIncidenceClasification
+		WHERE cti.ServiceType = 'DELIVERY'
+			  AND cti.RowStatus = 1
 
     END;
     ELSE 
