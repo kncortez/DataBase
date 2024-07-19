4d4264185ca94371f199784fd3665efcaf6075ec
Azurdia <cristian.azurdia@forzadelivery.com>
2024-07-19 10:35:11 -0600

    FDAPI-2744 Optimización en los tipos de datos de las tablas que se utilzan en las consultas Optimización de Inner Joins Indice para el manejo de la Tabla SettlementPickupStation

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetServicesOfRabbitPhone.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetServicesOfRabbitPhone.sql
@@ -1,6 +1,4 @@
 ﻿
-
-
 -- =============================================
 -- Author:		<Andres,Ruiz>
 -- Create date: <2022-02-16>
@@ -13,7 +11,9 @@
 -- =============================================
 
 CREATE PROCEDURE [dbo].[GetServicesOfRabbitPhone]
-    @Phone NVARCHAR(50) = 'Guatemala'
+    @Phone NVARCHAR(50) = 'Guatemala',
+    @IdCountry NVARCHAR(2) = 'GT'
+
 AS
 BEGIN
 
@@ -29,58 +29,43 @@ BEGIN
 		INSERT INTO @Guides
 			(Guide_Serie, Guide_Number, Guide_Settled)
 		SELECT 
-			DISTINCT
+				DISTINCT
 				dop.GuideSerie
 				, dop.GuideNumber
 				, IIF(spd.SettlementDate IS NULL, 0 , 1 )
 		FROM 
 			dbo.SenderReceiver sr WITH(NOLOCK)
-			INNER JOIN 
-				dbo.SettlementPickupStation sps WITH(NOLOCK)
-				ON 
-					sps.CouriermanId = sr.ID 
-					AND 
-					sps.TransactionDate  = CONVERT(DATE,GETDATE())
-					AND
-					sps.RowStatus = 1
-			INNER JOIN 
-				dbo.SettlementPickupStationDetail spd WITH(NOLOCK)
-				ON 
-					spd.SettlementPickupStationId = sps.IdSettlementPickupStation
-					AND
-					spd.RowStatus = 1
-			INNER JOIN 
-				dbo.ServiceManagement srv WITH(NOLOCK)
-				ON 
-					srv.IdServiceManagement = spd.ServiceManagementId
-			INNER JOIN 
-				dbo.SchedulePickup scp WITH(NOLOCK)
-				ON 
-					scp.SchedulePickupId = srv.IdSchedulePickup
-			INNER JOIN 
-				dbo.DeliveryOrderPaymentDetail dop WITH(NOLOCK)
-				ON 
-					dop.IdHeaderRecolection = scp.SchedulePickupId
-			INNER JOIN 
-				dbo.DeliveryOrder ord WITH(NOLOCK)
-				ON 
-					ord.Guide_Serie = dop.GuideSerie
-					AND 
-					ord.Guide_Number = dop.GuideNumber
-			LEFT JOIN
-				[DeliveryBackOffice].[dbo].[CatServiceStatus] CSS WITH(NOLOCK)
-				ON
-					srv.ServiceStatusId = CSS.IdServiceStatus
-		WHERE (sr.Phone LIKE '%' + @Phone + '%'
-				OR
-			  [sr].[UniqueCode] = @Phone)
-
+			INNER JOIN  dbo.SettlementPickupStation sps WITH(NOLOCK)
+				ON  sps.CouriermanId = CONVERT(NVARCHAR(50),sr.ID) 
+				AND sps.TransactionDate  = CONVERT(DATE,GETDATE())
+			INNER JOIN dbo.SettlementPickupStationDetail spd WITH(NOLOCK)
+				ON  spd.SettlementPickupStationId = sps.IdSettlementPickupStation
+			INNER JOIN dbo.ServiceManagement srv WITH(NOLOCK)
+				ON CONVERT(BIGINT,srv.IdServiceManagement) = spd.ServiceManagementId
+			INNER JOIN  dbo.SchedulePickup scp WITH(NOLOCK)
+				ON  scp.SchedulePickupId = srv.IdSchedulePickup
+			INNER JOIN  dbo.DeliveryOrderPaymentDetail dop WITH(NOLOCK)
+				ON  CONVERT(BIGINT,dop.IdHeaderRecolection) = scp.SchedulePickupId
+			INNER JOIN dbo.DeliveryOrder ord WITH(NOLOCK)
+				ON  ord.Guide_Serie = dop.GuideSerie
+				AND ord.Guide_Number = dop.GuideNumber
+			LEFT JOIN [DeliveryBackOffice].[dbo].[CatServiceStatus] CSS WITH(NOLOCK)
+				ON srv.ServiceStatusId = CSS.IdServiceStatus
+		WHERE sps.RowStatus = 1
+			AND spd.RowStatus = 1
+			AND (sr.Phone LIKE '%' + @Phone + '%'
+			OR
+			[sr].[UniqueCode] = @Phone)
+			AND IIF(ord.SenderCountryId IS NULL, 'GT', ord.SenderCountryId) = @IdCountry
+			   
 		-- TABLAS A DEVOLVER
 		-- DATOS DE COURIER
-		SELECT SR.ID , CONCAT( SR.First_Name, ' ',SR.Last_Name) Courier FROM dbo.SenderReceiver sr WITH(NOLOCK)
+		SELECT SR.ID , CONCAT( SR.First_Name, ' ',SR.Last_Name) Courier 
+		FROM dbo.SenderReceiver sr WITH(NOLOCK)
 		WHERE (sr.Phone LIKE '%' + @Phone + '%'
 				OR
 			  [sr].[UniqueCode] = @Phone)
+          AND IIF(sr.IdCountry IS NULL, 'GT', sr.IdCountry) = @IdCountry
 
 		-- DATOS DE SERVICIOS
 		SELECT
@@ -93,47 +78,29 @@ BEGIN
 			, IIF(srv.ServiceStatusId = 3,MAX(spd.Price),0) Price
 		FROM 
 			dbo.SenderReceiver sr WITH(NOLOCK)
-			INNER JOIN 
-				dbo.SettlementPickupStation sps WITH(NOLOCK)
-				ON 
-					sps.CouriermanId = sr.ID 
-					AND 
-					sps.TransactionDate  = CONVERT(DATE,GETDATE())
-					AND
-					sps.RowStatus = 1
-			LEFT JOIN 
-				dbo.SettlementPickupStationDetail spd WITH(NOLOCK)
-				ON 
-					spd.SettlementPickupStationId = sps.IdSettlementPickupStation
-					AND
-					spd.RowStatus = 1
-			LEFT JOIN 
-				dbo.ServiceManagement srv WITH(NOLOCK)
-				ON 
-					srv.IdServiceManagement = spd.ServiceManagementId
-			LEFT JOIN 
-				dbo.SchedulePickup scp WITH(NOLOCK)
-				ON 
-					scp.SchedulePickupId = srv.IdSchedulePickup
-			LEFT JOIN 
-				dbo.DeliveryOrderPaymentDetail dop WITH(NOLOCK)
-				ON 
-					dop.IdHeaderRecolection = scp.SchedulePickupId
-			LEFT JOIN 
-				dbo.DeliveryOrder ord WITH(NOLOCK)
-				ON 
-					ord.Guide_Serie = dop.GuideSerie
-					AND 
-					ord.Guide_Number = dop.GuideNumber
-					AND
-					ord.StatusOrderId = 2
-			LEFT JOIN
-				[DeliveryBackOffice].[dbo].[CatServiceStatus] CSS WITH(NOLOCK)
-				ON
-					srv.ServiceStatusId = CSS.IdServiceStatus
-		WHERE (sr.Phone LIKE '%' + @Phone + '%'
+			INNER JOIN dbo.SettlementPickupStation sps WITH(NOLOCK)
+				ON  sps.CouriermanId = CONVERT(NVARCHAR(50), sr.ID)
+				AND sps.TransactionDate  = CONVERT(DATE,GETDATE())
+			LEFT JOIN dbo.SettlementPickupStationDetail spd WITH(NOLOCK)
+				ON  spd.SettlementPickupStationId = sps.IdSettlementPickupStation		
+			LEFT JOIN  dbo.ServiceManagement srv WITH(NOLOCK)
+				ON CONVERT(BIGINT,srv.IdServiceManagement) = spd.ServiceManagementId
+			LEFT JOIN dbo.SchedulePickup scp WITH(NOLOCK)
+				ON scp.SchedulePickupId = srv.IdSchedulePickup
+			LEFT JOIN dbo.DeliveryOrderPaymentDetail dop WITH(NOLOCK)
+				ON CONVERT(BIGINT, dop.IdHeaderRecolection) = scp.SchedulePickupId
+			LEFT JOIN dbo.DeliveryOrder ord WITH(NOLOCK)
+				ON  ord.Guide_Serie = dop.GuideSerie
+				AND ord.Guide_Number = dop.GuideNumber
+				AND ord.StatusOrderId = 2
+			LEFT JOIN [DeliveryBackOffice].[dbo].[CatServiceStatus] CSS WITH(NOLOCK)
+				ON srv.ServiceStatusId = CSS.IdServiceStatus
+		WHERE sps.RowStatus = 1
+			  AND spd.RowStatus = 1
+			  AND (sr.Phone LIKE '%' + @Phone + '%'
 				OR
 			  [sr].[UniqueCode] = @Phone)
+              AND IIF(sr.IdCountry IS NULL, 'GT', sr.IdCountry) = @IdCountry
 		AND spd.SettlementDate IS NULL
 		GROUP BY
 			srv.IdServiceManagement
@@ -169,115 +136,75 @@ BEGIN
 			, srv.IdServiceManagement
 		FROM 
 			dbo.SenderReceiver sr WITH(NOLOCK)
-			INNER JOIN 
-				dbo.SettlementPickupStation sps WITH(NOLOCK)
-				ON 
-					sps.CouriermanId = sr.ID 
-					AND 
-					sps.TransactionDate  = CONVERT(DATE,GETDATE())
-					AND
-					sps.RowStatus = 1
-			INNER JOIN 
-				dbo.SettlementPickupStationDetail spd WITH(NOLOCK)
-				ON 
-					spd.SettlementPickupStationId = sps.IdSettlementPickupStation
-					AND
-					spd.RowStatus = 1
-			INNER JOIN 
-				dbo.ServiceManagement srv WITH(NOLOCK)
-				ON 
-					srv.IdServiceManagement = spd.ServiceManagementId
-			INNER JOIN 
-				dbo.SchedulePickup scp WITH(NOLOCK)
-				ON 
-					scp.SchedulePickupId = srv.IdSchedulePickup
-			INNER JOIN 
-				dbo.DeliveryOrderPaymentDetail dop WITH(NOLOCK)
-				ON 
-					dop.IdHeaderRecolection = scp.SchedulePickupId
-			INNER JOIN 
-				dbo.DeliveryOrder ord WITH(NOLOCK)
-				ON 
-					ord.Guide_Serie = dop.GuideSerie
-					AND 
-					ord.Guide_Number = dop.GuideNumber
-			LEFT JOIN 
-				DeliveryOrderPiece ordp WITH(NOLOCK)
-				ON 
-					ord.Guide_Number = ordp.GuideNumber
-					AND 
-					ord.Guide_Serie = ordp.GuideSerie
-			LEFT JOIN
-				[DeliveryBackOffice].[dbo].[CatServiceStatus] CSS WITH(NOLOCK)
-				ON
-					srv.ServiceStatusId = CSS.IdServiceStatus
-		WHERE (sr.Phone LIKE '%' + @Phone + '%'
+			INNER JOIN dbo.SettlementPickupStation sps WITH(NOLOCK)
+				ON sps.CouriermanId = CONVERT(NVARCHAR(50), sr.ID) 
+				AND sps.TransactionDate  = CONVERT(DATE,GETDATE())
+				
+			INNER JOIN dbo.SettlementPickupStationDetail spd WITH(NOLOCK)
+				ON  spd.SettlementPickupStationId = sps.IdSettlementPickupStation
+				
+			INNER JOIN dbo.ServiceManagement srv WITH(NOLOCK)
+				ON CONVERT(BIGINT,srv.IdServiceManagement) = spd.ServiceManagementId
+			INNER JOIN dbo.SchedulePickup scp WITH(NOLOCK)
+				ON scp.SchedulePickupId = srv.IdSchedulePickup
+			INNER JOIN dbo.DeliveryOrderPaymentDetail dop WITH(NOLOCK)
+				ON CONVERT(BIGINT, dop.IdHeaderRecolection) = scp.SchedulePickupId
+			INNER JOIN dbo.DeliveryOrder ord WITH(NOLOCK)
+				ON  ord.Guide_Serie = dop.GuideSerie
+				AND ord.Guide_Number = dop.GuideNumber
+			LEFT JOIN DeliveryOrderPiece ordp WITH(NOLOCK)
+				ON  ord.Guide_Number = ordp.GuideNumber
+				AND ord.Guide_Serie = ordp.GuideSerie
+			LEFT JOIN [DeliveryBackOffice].[dbo].[CatServiceStatus] CSS WITH(NOLOCK)
+				ON srv.ServiceStatusId = CSS.IdServiceStatus
+		WHERE sps.RowStatus = 1
+		  AND spd.RowStatus = 1
+		  AND (sr.Phone LIKE '%' + @Phone + '%'
 				OR
-			  [sr].[UniqueCode] = @Phone)
-		and
-		spd.SettlementDate is null
+				[sr].[UniqueCode] = @Phone)
+		  AND	spd.SettlementDate is null
+		  AND IIF(sr.IdCountry IS NULL, 'GT', sr.IdCountry) = @IdCountry
 
 		-- MANIFIESTO
 		SELECT
 			sps.IdSettlementPickupStation idManifest
-		FROM 
-			dbo.SenderReceiver sr WITH(NOLOCK)
-			INNER JOIN 
-				dbo.SettlementPickupStation sps WITH(NOLOCK) 
-				ON 
-					sps.CouriermanId = sr.ID 
-					AND 
-					sps.TransactionDate  = CONVERT(DATE,GETDATE())
-					AND
-					sps.RowStatus = 1
-		WHERE (sr.Phone LIKE '%' + @Phone + '%'
+		FROM dbo.SenderReceiver sr WITH(NOLOCK)
+			INNER JOIN dbo.SettlementPickupStation sps WITH(NOLOCK) 
+				ON  sps.CouriermanId = CONVERT(NVARCHAR(50), sr.ID)
+				AND sps.TransactionDate  = CONVERT(DATE,GETDATE())	
+		WHERE sps.RowStatus = 1 
+		 AND (sr.Phone LIKE '%' + @Phone + '%'
 				OR
 			  [sr].[UniqueCode] = @Phone)
+          AND IIF(sr.IdCountry IS NULL, 'GT', sr.IdCountry) = @IdCountry
 
 		-- Totales de piezas por guía las cuales no han sido liquidadas
 		SELECT DISTINCT
 			dop.GuideSerie
 			, dop.GuideNumber
 			, (ord.Pieces_Dry + ord.Pieces_Cold) 'Pieces'
-		FROM 
-			dbo.SenderReceiver sr WITH(NOLOCK)
-			INNER JOIN 
-				dbo.SettlementPickupStation sps WITH(NOLOCK)
-				ON 
-					sps.CouriermanId = sr.ID 
-					AND 
-					sps.TransactionDate  = CONVERT(DATE,GETDATE())
-					AND
-					sps.RowStatus = 1
-			INNER JOIN 
-				dbo.SettlementPickupStationDetail spd WITH(NOLOCK)
-				ON 
-					spd.SettlementPickupStationId = sps.IdSettlementPickupStation
-					AND
-					spd.RowStatus = 1
-			INNER JOIN 
-				dbo.ServiceManagement srv WITH(NOLOCK)
-				ON 
-					srv.IdServiceManagement = spd.ServiceManagementId
-			INNER JOIN 
-				dbo.SchedulePickup scp WITH(NOLOCK)
-				ON 
-					scp.SchedulePickupId = srv.IdSchedulePickup
-			INNER JOIN 
-				dbo.DeliveryOrderPaymentDetail dop WITH(NOLOCK)
-				ON 
-					dop.IdHeaderRecolection = scp.SchedulePickupId
-			INNER JOIN 
-				dbo.DeliveryOrder ord WITH(NOLOCK)
-				ON 
-					ord.Guide_Serie = dop.GuideSerie
-					AND 
-					ord.Guide_Number = dop.GuideNumber
-		WHERE (sr.Phone LIKE '%' + @Phone + '%'
+		FROM dbo.SenderReceiver sr WITH(NOLOCK)
+			INNER JOIN dbo.SettlementPickupStation sps WITH(NOLOCK)
+				ON  sps.CouriermanId = CONVERT(NVARCHAR(50), sr.ID)
+				AND sps.TransactionDate  = CONVERT(DATE,GETDATE())
+			INNER JOIN dbo.SettlementPickupStationDetail spd WITH(NOLOCK)
+				ON spd.SettlementPickupStationId = sps.IdSettlementPickupStation
+			INNER JOIN dbo.ServiceManagement srv WITH(NOLOCK)
+				ON CONVERT(BIGINT, srv.IdServiceManagement) = spd.ServiceManagementId
+			INNER JOIN dbo.SchedulePickup scp WITH(NOLOCK)
+				ON  scp.SchedulePickupId = srv.IdSchedulePickup
+			INNER JOIN dbo.DeliveryOrderPaymentDetail dop WITH(NOLOCK)
+				ON CONVERT(BIGINT, dop.IdHeaderRecolection) = scp.SchedulePickupId
+			INNER JOIN dbo.DeliveryOrder ord WITH(NOLOCK)
+				ON  ord.Guide_Serie = dop.GuideSerie
+				AND ord.Guide_Number = dop.GuideNumber
+		WHERE sps.RowStatus = 1
+			AND spd.RowStatus = 1
+			AND (sr.Phone LIKE '%' + @Phone + '%'
 				OR
 			  [sr].[UniqueCode] = @Phone)
-		and
-		spd.SettlementDate is null
+			AND spd.SettlementDate is null
+			AND IIF(sr.IdCountry IS NULL, 'GT', sr.IdCountry) = @IdCountry
 
 	END TRY
 	BEGIN CATCH
