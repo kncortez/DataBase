67b80ef91fb623f2e7ca61e47f2c5152d5dd5db4
csuazo11 <cristian.suazo@forzadelivery.com>
2024-07-18 18:02:01 -0600

    FDAPI-2729: Se agrega informacion para multipais

---
+++ DeliveryBackOffice(DEV)/dbo/Inserts/FDAPI-2729/INSERT MarketplaceTagsByProduct.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO MarketplaceTagsByProduct (MarketplaceProductTagsId,RowStatus,TokenCreated,DateCreated, CatSubscriptionId, CatMembershipId, Position)
+VALUES(1,1,'SYS-BHERRERA',GETDATE(),13,NULL,3),	
+	  (1,1,'SYS-BHERRERA',GETDATE(),14,NULL,6),
+	  (1,1,'SYS-BHERRERA',GETDATE(),17,NULL,2),
+	  (4,1,'SYS-BHERRERA',GETDATE(),NULL,3,3)
