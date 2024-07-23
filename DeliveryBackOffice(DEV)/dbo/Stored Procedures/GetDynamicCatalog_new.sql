bf2f96b84e8cad0129d2e5147e64bcd56c7a59e5
JuanRamirezFD <juan.ramirez@forzadelivery.com>
2024-07-22 19:17:45 -0600

    FDAPI-2727: Se agrega catalogo y filtro para obtener por pais los vehiculos para portal corporativo

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetDynamicCatalog_new.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetDynamicCatalog_new.sql
@@ -3,6 +3,10 @@
 -- Create date: <2024-07-05>
 -- Description:	<Se agrega parametro para filtrar por pais, GT por defecto>
 -- =============================================
+-- Modified:    <Daniel, Ramirez>
+-- Create date: <2024-22-07>
+-- Description: <Se agrega catalogo y filtro para obtener por pais los vehiculos para portal corporativo>
+-- =============================================
 CREATE PROCEDURE [dbo].[GetDynamicCatalog_new]
     @TypeMethod VARCHAR(100) = 'GetCustomerType'
   , @IdAccount INT = null
@@ -166,6 +170,16 @@ BEGIN
 				AND RowStatus = 1
 				AND ISNULL(CountryID, 'GT')= @IdCountry
 
+    END;
+    ELSE IF (@TypeMethod = 'GetTypeVehicle')
+    BEGIN
+        SELECT IdTypeVehicle AS [Id],
+               [Name] AS [Name],
+               [Description] AS [Description]
+          FROM CatTypeVehicle
+         WHERE RowStatus = 1
+           AND Name IN ('Camión','Panel','Motocicleta')
+           AND IIF(IdCountry IS NULL, 'GT', IdCountry) = @IdCountry
     END;
     ELSE 
     BEGIN
