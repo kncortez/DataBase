6fe18612cff2ff1acefa20cff1dfe35f92058593
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2024-08-05 15:19:46 -0600

    FDAPI-2829: Se modifico el sp spg_extern_order_detail_status para obtener el Receiver_Phone sin NirPhone.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_extern_order_detail_status.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_extern_order_detail_status.sql
@@ -106,7 +106,7 @@ BEGIN
          , DO.Manifest_Number
          , DO.NameOfReceiver
          , DO.Delivery_Max_Date
-         , LTRIM(RTRIM(DO.Receiver_Phone))
+         , RIGHT(LTRIM(RTRIM(DO.Receiver_Phone)), 8)
          , DO.PriceShippment
          , DO.Collect_OnDelivery
     FROM [DeliveryBackOffice].[dbo].[DeliveryOrder] DO WITH (NOLOCK)
