8e0121cad41139bf71a7071105579cdca4cbd7ae
csuazo11 <cristian.suazo@forzadelivery.com>
2024-08-12 18:13:42 -0600

    FDAPI-2432: Se trabaja en facturacion para multipais

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_IVE_InfWbSrvFELG4S_Delivery.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_IVE_InfWbSrvFELG4S_Delivery.sql
@@ -7,6 +7,10 @@
 -- Create date: 2024/06/28
 -- Description: Retorna valor de configuracion para porcentaje de impuesto segun el pais de uso
 -- =============================================
+-- Author:     Cristian Suazo
+-- Create date: 2024/08/12
+-- Description: Se agrega la direccion del emisor del punto de visita
+-- =============================================
 CREATE PROCEDURE [dbo].[spg_IVE_InfWbSrvFELG4S_Delivery]
 	@VpCodeOfReference as varchar(100)
 AS
@@ -15,10 +19,12 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-    DECLARE @country AS VARCHAR(2),
-            @taxes   AS VARCHAR(50);
+	DECLARE @country AS VARCHAR(2),
+            @taxes   AS VARCHAR(50),
+			@Address AS NVARCHAR(25);
 
-	select @country = CountryId
+	select @country = CountryId,
+		   @Address = Address
 	from VisitPointClient WITH(NOLOCK)
 	where CodeOfReference = @VpCodeOfReference
 
@@ -31,7 +37,8 @@ BEGIN
 				@correoCCO as varchar(200)
 
 				select *, 
-                       @taxes AS [TaxPercentage]
+                       @taxes AS [TaxPercentage],
+					   @Address AS AddressEmisor
 				from del_ParametrosFactura WITH(NOLOCK)
 				where dpf_VpCodeOfReference = @VpCodeOfReference	
 END
