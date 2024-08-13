6a65d8324f9081b7c961b73d73b8418307efa96d
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2024-08-12 19:12:03 -0600

    FDAPI-2888: Se modifico el sp spGetCatalogTypePieceMC para agregar nuevos parámetros del portal individual.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spGetCatalogTypePieceMC.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spGetCatalogTypePieceMC.sql
@@ -9,16 +9,41 @@ CREATE PROCEDURE [dbo].[spGetCatalogTypePieceMC]
 	@pIdAccount INT = 1,
     @pToken VARCHAR(100) = '0BE2F8F3BD53652635746ACD069954B5',
     @pCountryId NVARCHAR(3) = 'GT',
-    @pType NVARCHAR(30) = 'GetCorporateArticles' -- GetCorporateArticles o GetTypePiece
+    @pType NVARCHAR(30) = 'GetCorporateArticles' -- GetCorporateArticles o GetTypePiece 
 AS
 BEGIN
-	
+
+	--DESCRIPCIÓN DE ARTICULOS IMPORTANTES PARA PORTALES INDIVIDUAL, EXPRESS CENTER y CORPORATIVO
+	DECLARE @DescriptionAritcle TABLE (
+    Id NVARCHAR(50),
+    Description NVARCHAR(200),
+    Country NVARCHAR(3),
+	Main bit, --campo para saber el paquete estandar del país
+	Label NVARCHAR(20),
+	Width2 NVARCHAR(5),
+	Dim NVARCHAR(50),
+	IsOversized bit
+);
+
+INSERT INTO @DescriptionAritcle (Id, Description, Country, Main,Label,Width2,Dim,IsOversized)
+VALUES
+    ('Paquete pequeño', 'Si el lado más largo es menor o igual a 28 cm - Peso: 1 a 10 lbs.', 'HN',1,'Pequeño','60%','Máx: 28cm o 10lbs',0),
+    ('Paquete mediano', 'Si el lado más largo mide entre 28.1 y 36 cm - Peso: 10.1 a 20 lbs.', 'HN',0,'Mediano','70%','Máx: 36cm o 20lbs',0),
+    ('Paquete grande', 'Si el lado más largo mide entre 36.1 y 47 cm - Peso: 20.1 a 40 lbs.', 'HN',0,'Grande','80%','Máx: 47cm o 40lbs',0),
+    ('Paquete extra grande', 'Si el lado más largo mide entre 47.1 y 51 cm - Peso: 40.1 a 59 lbs.', 'HN',0,'Extra Grande','90%','Máx: 51cm o 59lbs',0),
+    ('Paquete sobredimensionado', 'Si el lado más largo es mayor a 51 cm - Peso: 60 lbs en adelante.', 'HN',0,'Sobredimensionado','100%','Min: 60lbs',1),
+	('Paquete pequeño', 'Si el lado más largo es menor o igual a 28 cm - Peso: 1 a 10 lbs.', 'GT',1,'Pequeño','60%','Máx: 28cm o 10lbs',0),
+    ('Paquete mediano', 'Si el lado más largo mide entre 28.1 y 36 cm - Peso: 10.1 a 20 lbs.', 'GT',0,'Mediano','70%','Máx: 36cm o 20lbs',0),
+    ('Paquete grande', 'Si el lado más largo mide entre 36.1 y 47 cm - Peso: 20.1 a 40 lbs.', 'GT',0,'Grande','80%','Máx: 47cm o 40lbs',0),
+    ('Paquete extra grande', 'Si el lado más largo mide entre 47.1 y 51 cm - Peso: 40.1 a 59 lbs.', 'GT',0,'Extra Grande','90%','Máx: 51cm o 59lbs',0),
+    ('Paquete sobredimensionado', 'Si el lado más largo es mayor a 51 cm - Peso: 60 lbs en adelante.', 'GT',0,'Sobredimensionado','100%','Min: 60lbs',1);
+
 	IF (@pType = 'GetTypePiece') --CARGA DE ARTICULOS
 	BEGIN
 
-		DECLARE @NewMainRates INT = (SELECT TOP 1 RH.RheId FROM [DeliveryBackOffice].[dbo].[RateHeader] RH WITH(NOLOCK) WHERE RH.RheName = 'Tarifario de servicio estandar' COLLATE Latin1_General_CI_AI);
-		DECLARE @NewAlternativeRates INT = (SELECT TOP 1 RH.RheId FROM [DeliveryBackOffice].[dbo].[RateHeader] RH WITH(NOLOCK) WHERE RH.RheName = 'Tarifario destinos express center' COLLATE Latin1_General_CI_AI);
-		DECLARE @NewAutoSalesMainRates INT = (SELECT TOP 1 RH.RheId FROM [DeliveryBackOffice].[dbo].[RateHeader] RH WITH(NOLOCK) WHERE RH.RheName = 'Tarifario de servicio estandar autoventas' COLLATE Latin1_General_CI_AI);
+		DECLARE @NewMainRates INT = (SELECT TOP 1 RH.RheId FROM [DeliveryBackOffice].[dbo].[RateHeader] RH WITH(NOLOCK) WHERE RH.RheName = 'Tarifario de servicio estandar' AND RH.CountryId = @pCountryId COLLATE Latin1_General_CI_AI);
+		DECLARE @NewAlternativeRates INT = (SELECT TOP 1 RH.RheId FROM [DeliveryBackOffice].[dbo].[RateHeader] RH WITH(NOLOCK) WHERE RH.RheName = 'Tarifario destinos express center' AND RH.CountryId = @pCountryId COLLATE Latin1_General_CI_AI);
+		DECLARE @NewAutoSalesMainRates INT = (SELECT TOP 1 RH.RheId FROM [DeliveryBackOffice].[dbo].[RateHeader] RH WITH(NOLOCK) WHERE RH.RheName = 'Tarifario de servicio estandar autoventas' AND RH.CountryId = @pCountryId COLLATE Latin1_General_CI_AI);
 
 		SELECT
 			CONVERT(NVARCHAR, ISNULL(cd.[AbcId], 0)) AS 'Id',
@@ -31,6 +56,12 @@ BEGIN
 			CONVERT(NVARCHAR, ISNULL(cd.[Width], 30)) AS 'Width',
 			CONVERT(NVARCHAR, ISNULL(cd.[Length], 30)) AS 'Length',
 			CONVERT(NVARCHAR, ISNULL(cd.MassWeight, 1)) AS 'Weight',
+			ISNULL(cd.Description,'') AS 'DescriptionLog',
+			ISNULL(cd.Main,0) AS 'IsMainSTD',
+			ISNULL(cd.Label,'') AS 'Label',
+			ISNULL(cd.Width2,'') AS 'Width2',
+			ISNULL(cd.Dim,'') AS 'Dim',
+			ISNULL(cd.IsOversized,0) AS 'IsOversized',
 			IIF(cd.IsMainPackage IS NOT NULL,IIF(cd.IsMainPackage = 1, 'true', 'false'), '') AS 'IsMainPackage'
 		FROM (
 			SELECT DISTINCT
@@ -42,6 +73,12 @@ BEGIN
 				abc.Width,
 				abc.Length,
 				abc.MassWeight,
+				d.Description,
+				d.Main,
+				d.Label,
+				d.Width2,
+				d.Dim,
+				d.IsOversized,
 				IIF(ra.RateId IN (@NewMainRates, @NewAlternativeRates, @NewAutoSalesMainRates), IIF(ra.TypeServiceId IS NOT NULL AND ra.TypeSegmentId IS NOT NULL, 1, 0), NULL) AS IsMainPackage
 			FROM dbo.CatArticle art WITH(NOLOCK)
 			INNER JOIN dbo.ArticleByCustomer abc WITH(NOLOCK)
@@ -54,6 +91,8 @@ BEGIN
 				ON rbc.RbcIdRate = ra.RateId
 			INNER JOIN Account ac WITH(NOLOCK)
 				ON ac.IdCustomer = rbc.RbcIdCustomer
+			LEFT JOIN @DescriptionAritcle d
+				ON art.ArtName = d.Id AND d.Country = @pCountryId
 			WHERE ac.AccIdAccount = @pIdAccount
 			AND (art.IdCountry = @pCountryId OR (art.IdCountry IS NULL AND @pCountryId = 'GT'))
 			AND art.ArtRowStatus	= 'TRUE'
