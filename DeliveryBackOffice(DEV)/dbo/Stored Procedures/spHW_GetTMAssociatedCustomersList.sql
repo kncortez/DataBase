c9f90396cbda48f1ff1899def4c456c6513c20de
brandon-p <brandon.pedroza@forzadelivery.com>
2024-08-16 12:52:56 -0600

    FDAPI-2901: #se agrega parametro para filtrar clientes por pais

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_GetTMAssociatedCustomersList.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHW_GetTMAssociatedCustomersList.sql
@@ -7,8 +7,13 @@
 -- Modified:	<14-08-2024>
 -- Description:	<Concat nirphone in Phone number>
 -- =============================================
+-- Author:		<Brandon Pedroza>
+-- Modified:	<16-08-2024>
+-- Description:	<Added idcontry parameter>
+-- =============================================
 CREATE PROCEDURE [dbo].[spHW_GetTMAssociatedCustomersList]
-	@RegisterUserId INT
+	@RegisterUserId INT,
+	@IdCountry AS NVARCHAR(2) = 'GT'
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -24,7 +29,8 @@ BEGIN
     SELECT		[P].[PerFirstName] [FirstName],
 				[P].[PerLastName] [LastName],
 				[RU].[UsrEmail] [Email],
-				CONCAT(ISNULL([RU].[PrefixCallingCode],'+502'),[RU].[Phone]) [Phone],
+				ISNULL([RU].[PrefixCallingCode],'+502') [NirPhone],
+				[RU].[Phone] [Phone],				
 				[RU].[UsrDateCreated] [DateCreated],
 				[C].[CutOffDate] [CutOffDate],
 				[C].[CustomerGoalQuantity] [CustomerGoalQuantity],
@@ -63,11 +69,13 @@ BEGIN
 			DO.IdCustomer
 	) GuideAmountBeforeCut
 	WHERE	[C].[CatTMSalesPersonId] = @CatTMSalesPersonId
+	AND ISNULL([P].[PerCountryOrigin], 'GT') = @IdCountry
 	UNION
 	SELECT		[P].[PerFirstName] [FirstName],
 				[P].[PerLastName] [LastName],
 				[RU].[UsrEmail] [Email],
-				CONCAT(ISNULL([RU].[PrefixCallingCode], '+502'),[RU].[Phone]) [Phone],
+				ISNULL([RU].[PrefixCallingCode], '+502') [NirPhone],
+				[RU].[Phone] [Phone],
 				[RU].[UsrDateCreated] [DateCreated],
 				[RU].[UsrDateCreated] [CutOffDate],
 				0 [CustomerGoalQuantity],
@@ -90,6 +98,7 @@ BEGIN
 		AND		[M].[ExpirationDate] >= SYSDATETIME()
 	LEFT JOIN	[dbo].[CatMembership] CM  WITH(NOLOCK) 
 		ON		[M].[CatMembershipId] = [CM].[IdCatMembership]
-	WHERE	[M].[CatTMSalesPersonId] = @CatTMSalesPersonId;
+	WHERE	[M].[CatTMSalesPersonId] = @CatTMSalesPersonId
+	AND ISNULL([P].[PerCountryOrigin], 'GT') = @IdCountry;
 
 END
