c22ad795156aabcd5098ed74b3d163d67b8c8afa
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2024-08-19 18:03:52 -0600

    FDAPI-2646 Se agrega un SP para obtener el path de la imagen de firma hecha por el cliente en la entrega

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetSignaturePathByGuide.sql
@@ -0,0 +1,19 @@
+-- =============================================
+-- Author:		<Tito García>
+-- Create date: <19/08/2024>
+-- Description:	<Retorna el path de la firma>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetSignaturePathByGuide]
+	@GuideSerie VARCHAR(2),
+	@GuideNumber INT
+AS
+BEGIN
+
+	SELECT TOP 1 dp.PathSignature 
+	FROM [dbo].[DeliveryProof] dp WITH (NOLOCK)
+	WHERE dp.Guide_Serie = @GuideSerie
+		AND dp.Guide_Number = @GuideNumber
+		AND dp.PathSignature IS NOT NULL
+	ORDER BY dp.ID DESC
+
+END
