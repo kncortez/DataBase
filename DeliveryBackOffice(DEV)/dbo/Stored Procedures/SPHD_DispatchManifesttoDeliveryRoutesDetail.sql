ddd39b7ea6e84383a91907ccee2482ad5fa9dd20
EdelmanV <edelman.vasquez@forzalatam.com>
2024-09-05 17:28:26 -0600

    #corrección para quitar campos multipaís

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_DispatchManifesttoDeliveryRoutesDetail.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_DispatchManifesttoDeliveryRoutesDetail.sql
@@ -80,10 +80,10 @@ BEGIN
         HaveCredit NVARCHAR(50) NULL,
         CollectCOD NVARCHAR(50) NULL,
         ReturnRate DECIMAL(14, 2) NULL,
-		CurrencyPrice_CODCodeISO NVARCHAR(8),
-	  	CurrencyPrice_CODSymbol  NVARCHAR(8),
-	    CurrencyPriceCodeISO     NVARCHAR(8),
-	    CurrencyPriceSymbol      NVARCHAR(8),
+		-- CurrencyPrice_CODCodeISO NVARCHAR(8),
+	  	-- CurrencyPrice_CODSymbol  NVARCHAR(8),
+	    -- CurrencyPriceCodeISO     NVARCHAR(8),
+	    -- CurrencyPriceSymbol      NVARCHAR(8),
         AmountToPay DECIMAL(14, 2) NULL,
         CODAmount DECIMAL(14, 2) NULL,
         ReturnRates DECIMAL(14, 2) NULL,
@@ -109,10 +109,10 @@ BEGIN
         HaveCredit,
         CollectCOD,
         ReturnRate,
-		CurrencyPrice_CODCodeISO,
-	  	CurrencyPrice_CODSymbol,
-	    CurrencyPriceCodeISO,
-	    CurrencyPriceSymbol,
+		-- CurrencyPrice_CODCodeISO,
+	  	-- CurrencyPrice_CODSymbol,
+	    -- CurrencyPriceCodeISO,
+	    -- CurrencyPriceSymbol,
         AmountToPay,
         CODAmount,
         ReturnRates
@@ -147,7 +147,8 @@ BEGIN
 		Rack_Position nvarchar(MAX),
 		Price decimal(16,2),
 		Collect_on_Delivery decimal(16,2),
-		Total decimal(16,2)
+		Total decimal(16,2),
+		ReceiverCountry NVARCHAR(2)
 		
 
 	)
@@ -183,6 +184,7 @@ BEGIN
 		ISNULL((CASE WHEN [do].[IsLastMileReturn] = 1 THEN 0 ELSE do.Collect_OnDelivery END), 0)
 		END
 		) AS  Total
+		,'GT'
 	from 
 		[DeliveryBackOffice].[dbo].DeliveryOrder do WITH(NOLOCK)
 	INNER JOIN 
@@ -221,6 +223,7 @@ BEGIN
 		,Price
 		,Collect_on_Delivery
 		,Total
+		,ReceiverCountry
 	FROM 
 		@temp tmp
 	ORDER BY 
