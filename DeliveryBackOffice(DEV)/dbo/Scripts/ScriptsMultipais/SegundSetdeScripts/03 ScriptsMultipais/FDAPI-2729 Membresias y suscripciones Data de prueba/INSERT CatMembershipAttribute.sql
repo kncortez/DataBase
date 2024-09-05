90f0750dfc5c6a3068a1457d1634910ed9c2b980
csuazo11 <cristian.suazo@forzadelivery.com>
2024-09-05 17:47:44 -0600

    correccion de script insert

--- DeliveryBackOffice(DEV)/dbo/Scripts/ScriptsMultipais/SegundSetdeScripts/03 ScriptsMultipais/FDAPI-2729 Membresias y suscripciones Data de prueba/INSERT CatMembershipAttribute.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/ScriptsMultipais/SegundSetdeScripts/03 ScriptsMultipais/FDAPI-2729 Membresias y suscripciones Data de prueba/INSERT CatMembershipAttribute.sql
@@ -1,4 +1,5 @@
 --pendiente
+DECLARE @CatMembershipId INT = (SELECT IdCatMembership FROM CatMembership WHERE MembershipName = 'Club Forza' AND IdCountry = 'HN')
 
 INSERT INTO CatMembershipAttribute (CatMembershipId,
 								    CatAttributeId,
@@ -10,8 +11,8 @@ INSERT INTO CatMembershipAttribute (CatMembershipId,
 									DateCreated,
 									MembershipAttributeDescriptionLong,
 									CatMembershipAttributeIcon)
-		VALUES(3,1,1,'Acumulaci�n de puntos para env�os gratis',1,1,'SYS-ARUIZ',GETDATE(),'1 a�o de vigencia','fa fa-truck fa-2x'),
-			  (3,1,1,'Promociones en comercios Afiliados',4,1,'SYS-ARUIZ',GETDATE(),'10% de descuento en las tarifas bases vigentes de acuerdo al tipo de destino para servicios Est�ndar o COD.','fa fa-shopping-cart fa-2x'),
-			  (3,1,1,'Recolecciones SIN COSTO',5,1,'SYS-BHERRERA',GETDATE(),'Descuento desde la primer gu�a, sin tener que comprar saldo, ideal para clientes que env�an de 10 a 15 env�os mensuales.','fa fa-check-circle fa-2x'),
-			  (3,1,1,'Env�o de devoluciones SIN COSTO',6,1,'SYS-BHERRERA',GETDATE(),'El env�o�lo puede pagar el remitente o puedes solicitar el�servicio cobrar al destinatario (collect + L 12.77).','fa fa-check-circle fa-2x'),
-			  (3,1,1,'Pagos de reclamo en 24 horas',7,1,'SYS-BHERRERA',GETDATE(),'Generas tu gu�a desde el portal web y puedes llevar tu env�o a cualquiera de las Agencias Express Center o solicitar una recolecci�n.','fa fa-check-circle fa-2x')
+		VALUES(@CatMembershipId,1,1,'Acumulación de puntos para envíos gratis',1,1,'SYS-ARUIZ',GETDATE(),'1 año de vigencia','fa fa-truck fa-2x'),
+			  (@CatMembershipId,1,1,'Promociones en comercios Afiliados',4,1,'SYS-ARUIZ',GETDATE(),'10% de descuento en las tarifas bases vigentes de acuerdo al tipo de destino para servicios Estándar o COD.','fa fa-shopping-cart fa-2x'),
+			  (@CatMembershipId,1,1,'Recolecciones SIN COSTO',5,1,'SYS-BHERRERA',GETDATE(),'Descuento desde la primer guía, sin tener que comprar saldo, ideal para clientes que envían de 10 a 15 envíos mensuales.','fa fa-check-circle fa-2x'),
+			  (@CatMembershipId,1,1,'Envío de devoluciones SIN COSTO',6,1,'SYS-BHERRERA',GETDATE(),'El envío lo puede pagar el remitente o puedes solicitar el servicio cobrar al destinatario (collect + L 12.77).','fa fa-check-circle fa-2x'),
+			  (@CatMembershipId,1,1,'Pagos de reclamo en 24 horas',7,1,'SYS-BHERRERA',GETDATE(),'Generas tu guÍa desde el portal web y puedes llevar tu envÍo a cualquiera de las Agencias Express Center o solicitar una recolección.','fa fa-check-circle fa-2x')
--- DeliveryBackOffice(DEV)/dbo/Scripts/ScriptsMultipais/SegundSetdeScripts/03 ScriptsMultipais/FDAPI-2729 Membresias y suscripciones Data de prueba/INSERT MarketplaceTagsByProduct.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/ScriptsMultipais/SegundSetdeScripts/03 ScriptsMultipais/FDAPI-2729 Membresias y suscripciones Data de prueba/INSERT MarketplaceTagsByProduct.sql
@@ -1,14 +1,39 @@
 --pendiente
+--7
+DECLARE @MarketProduct INT = (SELECT IdMarketplaceProductTags FROM MarketplaceProductTags WHERE MarketplaceProductTagsName = 'TODOS LOS PRODUCTOS' AND IdCountry = 'HN')
+--5
+DECLARE @MarketProduct2 INT = (SELECT IdMarketplaceProductTags FROM MarketplaceProductTags WHERE MarketplaceProductTagsName = 'LO MÁS VENDIDO' AND IdCountry = 'HN')
+--6
+DECLARE @MarketProduct3 INT = (SELECT IdMarketplaceProductTags FROM MarketplaceProductTags WHERE MarketplaceProductTagsName = 'NOVEDADES' AND IdCountry = 'HN')
+
+--Suscripciones
+--13
+DECLARE @CatSubscriptionId INT = (SELECT IdCatSubscription FROM CatSubscription WHERE SubscriptionName = 'Paquete Básico' AND IdCountry = 'HN')
+--14
+DECLARE @CatSubscriptionId2 INT = (SELECT IdCatSubscription FROM CatSubscription WHERE SubscriptionName = 'Paquete Plus' AND IdCountry = 'HN')
+--17
+DECLARE @CatSubscriptionId3 INT = (SELECT IdCatSubscription FROM CatSubscription WHERE SubscriptionName = 'Paquete Petit' AND IdCountry = 'HN')
+--15
+DECLARE @CatSubscriptionId4 INT = (SELECT IdCatSubscription FROM CatSubscription WHERE SubscriptionName = 'Paquete Gold' AND IdCountry = 'HN')
+--18
+DECLARE @CatSubscriptionId5 INT = (SELECT IdCatSubscription FROM CatSubscription WHERE SubscriptionName = 'Paquete Platino' AND IdCountry = 'HN')
+--16
+DECLARE @CatSubscriptionId6 INT = (SELECT IdCatSubscription FROM CatSubscription WHERE SubscriptionName = 'Plan Amigo' AND IdCountry = 'HN')
+
+
+--Membresias
+DECLARE @CatMembershipId INT = (SELECT IdCatMembership FROM CatMembership WHERE MembershipName = 'Club Forza' AND IdCountry = 'HN')
+
 
 INSERT INTO MarketplaceTagsByProduct (MarketplaceProductTagsId,RowStatus,TokenCreated,DateCreated, CatSubscriptionId, CatMembershipId, Position)
-VALUES(4,1,'SYS-BHERRERA',GETDATE(),13,NULL,3),	
-	  (5,1,'SYS-BHERRERA',GETDATE(),14,NULL,6),
-	  (5,1,'SYS-BHERRERA',GETDATE(),17,NULL,2),
-	  (7,1,'SYS-BHERRERA',GETDATE(),NULL,3,3),
-	  (7,1,'SYS-BHERRERA',GETDATE(),13,NULL,3),
-	  (7,1,'SYS-BHERRERA',GETDATE(),14,NULL,6),
-	  (7,1,'SYS-BHERRERA',GETDATE(),15,NULL,4),
-	  (6,1,'SYS-BHERRERA',GETDATE(),18,NULL,5),
-	  (7,1,'SYS-BHERRERA',GETDATE(),16,NULL,6),
-      (7,1,'SYS-BHERRERA',GETDATE(),17,NULL,2),
-	  (7,1,'SYS-BHERRERA',GETDATE(),18,NULL,5)
+VALUES(@MarketProduct,1,'SYS-BHERRERA',GETDATE(),@CatSubscriptionId,NULL,3),	
+	  (@MarketProduct2,1,'SYS-BHERRERA',GETDATE(),@CatSubscriptionId2,NULL,6),
+	  (@MarketProduct2,1,'SYS-BHERRERA',GETDATE(),@CatSubscriptionId3,NULL,2),
+	  (@MarketProduct,1,'SYS-BHERRERA',GETDATE(),NULL,@CatMembershipId,3),
+	  (@MarketProduct,1,'SYS-BHERRERA',GETDATE(),@CatSubscriptionId,NULL,3),
+	  (@MarketProduct,1,'SYS-BHERRERA',GETDATE(),@CatSubscriptionId2,NULL,6),
+	  (@MarketProduct,1,'SYS-BHERRERA',GETDATE(),@CatSubscriptionId4,NULL,4),
+	  (@MarketProduct3,1,'SYS-BHERRERA',GETDATE(),@CatSubscriptionId5,NULL,5),
+	  (@MarketProduct,1,'SYS-BHERRERA',GETDATE(),@CatSubscriptionId6,NULL,6),
+      (@MarketProduct,1,'SYS-BHERRERA',GETDATE(),@CatSubscriptionId3,NULL,2),
+	  (@MarketProduct,1,'SYS-BHERRERA',GETDATE(),@CatSubscriptionId5,NULL,5)
