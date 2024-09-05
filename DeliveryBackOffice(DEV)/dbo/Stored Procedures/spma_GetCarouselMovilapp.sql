9712a35487df2ed81b7ab3df6a010c41eee888b3
FLG\rene.rodriguez <oscar.vasquez@forzadelivery.com>
2024-09-05 11:11:28 -0600

    FDAPI-3006 Metodo para carga imagenes 2024-09-05 Oscar Rodriguez ADD Se agregaron sp, tabla y script para el funcionamiento de metodo de api para carga de imagenes en carousel para nueva movil app

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spma_GetCarouselMovilapp.sql
@@ -0,0 +1,28 @@
+-- =============================================
+-- Author:		<Oscar,Rodriguez>
+-- Create date: <2024-09-05>
+-- Description:	<Devuelve el listado de imagenes por pais para el carrusel de aplicacion movil>
+-- =============================================
+CREATE PROCEDURE [dbo].[spma_GetCarouselMovilapp]
+    -- Add the parameters for the stored procedure here
+    @IdCountry AS VARCHAR(2) = 'GT'
+AS
+BEGIN
+    -- SET NOCOUNT ON added to prevent extra result sets from
+    -- interfering with SELECT statements.
+    SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT		mai.IdCarouselImage [id],
+				mai.SDImageURL		[SmallDimension],
+				mai.MDImageURL		[MediumDimension],
+				mai.LDImageURL		[LargeDimension],
+				mai.ImageOrder		[Order],
+				mai.HyperlinkURL	[HyperLink],
+				mai.IdCountry		[Country]
+	FROM		DeliveryBackOffice.dbo.MovilAppCarouselImage mai WITH(NOLOCK)
+	WHERE		ISNULL(mai.IdCountry,'GT') = @IdCountry
+	AND			mai.RowStatus = 1
+	ORDER BY	mai.ImageOrder asc;
+END;
+
