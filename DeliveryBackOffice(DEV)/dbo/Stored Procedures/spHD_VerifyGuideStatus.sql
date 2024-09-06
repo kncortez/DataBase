d27dd2b25d321f0b1663d785caf067c9f62fc865
brandon-p <brandon.pedroza@forzadelivery.com>
2024-09-05 19:33:51 -0600

    multipais: WAVE 8 ==> cambios reportados miercoles 04/09 Sep, bloque uno

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHD_VerifyGuideStatus.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHD_VerifyGuideStatus.sql
@@ -34,7 +34,7 @@ BEGIN
 			FROM
 				[DeliveryBackOffice].[dbo].[CatCheckpointType] CCT  WITH(NOLOCK) 
 			WHERE
-				[CCT].[CheckpointTypeDescription] = 'Checkpoint final'  COLLATE Latin1_General_CI_AI 
+				[CCT].[CheckpointTypeDescription] = 'Checkpoint final'
 		);
 		DECLARE @ActualGuideStatus INT;
 		DECLARE @TerminalStatus TABLE
