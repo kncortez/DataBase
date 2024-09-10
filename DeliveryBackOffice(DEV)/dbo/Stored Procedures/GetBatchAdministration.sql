0ae0697e21907cfd626613baf90a301f14605826
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2024-09-09 19:10:22 -0600

    FDAPI-2986: Se agrega nuevo sp GetBatchAdministration para cargar lotes de facturación de Honduras para portal interno.

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetBatchAdministration.sql
@@ -0,0 +1,36 @@
+﻿-- =============================================
+-- Author:		<Walter Orozco>
+-- Create date: <2024-09-05>
+-- Description:	<Administración de lotes - Consulta de lotes>
+-- =============================================
+
+CREATE PROCEDURE [dbo].[GetBatchAdministration]
+@RTN NVARCHAR(100) = NULL
+AS
+BEGIN
+    BEGIN TRY
+
+	SELECT 
+		Id_Lote					AS 'IdLote'
+		,RTN
+		,CAI
+		,LimitDateEmision		AS 'Vigente hasta'
+		,Establishment			AS 'Establecimiento'
+		,Emision_Point			AS 'Punto de emisión'
+		,TypeDocument			AS 'Tipo de documento'
+		,Status					AS 'Estado'
+		,AmountGranted			AS 'Cantidad otorgada'
+		,DaysLeftNotifycation	AS 'Disponible'
+	FROM DeliveryBackOffice.dbo.InvoiceBatchHeader
+	WHERE RTN = @RTN OR @RTN IS NULL
+		
+    END TRY
+    BEGIN CATCH
+
+		-- Manejo de errores con PRINT
+		DECLARE @ErrorMessage NVARCHAR(4000);
+		SELECT @ErrorMessage = ERROR_MESSAGE();
+    
+		PRINT 'Error: ' + @ErrorMessage;
+	END CATCH;
+END;
