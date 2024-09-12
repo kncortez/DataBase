04995d5af873c6fcdb3966711882efc6baeeb7b6
Alberto Ixchop <alberto.ixchop@forzadelivery.com>
2024-09-11 18:09:58 -0600

    Se agrega trucante de la variable tabla @TBSUBMODULES2

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_GetCorporateLogIn.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_GetCorporateLogIn.sql
@@ -267,7 +267,7 @@ BEGIN
                           , ModIdModuleDAD INT
                           , ModIdModuleCHILD INT
                         );
-
+                        TRUNCATE TABLE @TBSUBMODULES2
                         --	IF (@VERIFYUSER  > 0 )
                         --BEGIN
 
