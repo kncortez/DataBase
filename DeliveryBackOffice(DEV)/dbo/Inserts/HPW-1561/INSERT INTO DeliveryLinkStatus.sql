f40f65a88cd7b46b121d22128164e47de625e2f5
csuazo11 <cristian.suazo@forzadelivery.com>
2024-09-25 15:15:30 -0600

    HPW-1561: Se agrega estado anulado

--- DeliveryBackOffice(DEV)/dbo/Inserts/HPW-1561/INSERT INTO DeliveryLinkStatus.sql
+++ DeliveryBackOffice(DEV)/dbo/Inserts/HPW-1561/INSERT INTO DeliveryLinkStatus.sql
@@ -1,4 +1,5 @@
 INSERT INTO DeliveryLinkStatus (Name, RowStatus, UserCreated, DateCreated) 
 	   VALUES('Envíado',1,'SYSTEM',GETDATE()),
 			 ('Envío realizado',1,'SYSTEM',GETDATE()),
-			 ('Completado',1,'SYSTEM',GETDATE())
+			 ('Completado',1,'SYSTEM',GETDATE()),
+			 ('Anulado',1,'SYSTEM',GETDATE())
