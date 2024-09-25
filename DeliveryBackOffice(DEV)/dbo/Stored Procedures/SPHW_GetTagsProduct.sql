1deb29d09bc82c6b0aa38043331c1ef38245d8b1
brandon-p <brandon.pedroza@forzadelivery.com>
2024-09-25 12:14:58 -0600

    HPW-1555: #se agrega nuevo procedimiento para obtener tags de productos para link de entrega

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetTagsProduct.sql
@@ -0,0 +1,43 @@
+﻿-- =============================================
+-- Author:		<Brandon, Pedroza >
+-- Create date: <2024-09-25>
+-- Description:	<Obtiene lista de tags de productos para link de entrega>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHW_GetTagsProduct]
+AS
+BEGIN
+    -- SET NOCOUNT ON added to prevent extra result sets from
+    -- interfering with SELECT statements.
+    SET NOCOUNT ON;
+    DECLARE @TranCounter INT;
+    SET @TranCounter = @@TRANCOUNT;
+    IF @TranCounter > 0
+        SAVE TRANSACTION spgetSubcategories;
+    ELSE
+        BEGIN TRANSACTION;
+    BEGIN TRY
+
+        SELECT 200 AS 'StatusCode',
+               'Registros obtenidos' AS 'Description';
+
+        SELECT C.[IdCatProductTag] [Id],
+               C.[Description] [Description]
+        FROM [dbo].[CatProductTag] C
+        WHERE C.[RowStatus] = 'TRUE'
+
+
+
+        IF @TranCounter = 0
+            COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        IF @TranCounter = 0
+            ROLLBACK TRANSACTION;
+        ELSE IF XACT_STATE() <> -1
+            ROLLBACK TRANSACTION spgetSubcategories;
+        SELECT 0 AS 'StatusCode',
+               ERROR_MESSAGE() AS 'Description';
+    END CATCH
+
+END
+
