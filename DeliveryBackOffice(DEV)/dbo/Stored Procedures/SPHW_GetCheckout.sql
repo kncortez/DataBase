08ab264a52518dbb02af11eba0dd0c73575a2ab0
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2024-09-27 16:35:02 -0600

    HPW-1565: Se modifico el sp SPHW_GetCheckout para que se pueda usar el listado de productos en Mis Links y se agregaron datos para el cotizador.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetCheckout.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetCheckout.sql
@@ -5,43 +5,11 @@
 -- =============================================
 
 CREATE PROCEDURE [dbo].[SPHW_GetCheckout]
-@IdDeliveryLink NVARCHAR(100)
+@IdDeliveryLink INT,
+@OnlyProducts INT --0 checkout, 1 mis links
 AS
 BEGIN
 BEGIN TRY
-
-	--RESUMEN DE ENVIO
-	SELECT 
-		--DE
-		  C.Name AS 'Nombre remitente'
-		, VPC.Phone AS 'Telefono remitente'
-		, T2.TownshipName + ' , ' + P2.ProvinceName  AS 'Direccion remitente'
-		, VPC.Email AS 'Correo remitente'
-		--PARA
-		, DL.ReceiverName AS 'Nombre destinatario'
-		, DL.ReceiverPhone AS 'Telefono destinatario'
-		, T.TownshipName + ' , ' + P.ProvinceName AS 'Direccion destinatario'
-		, DL.ReceiverEmail AS 'Correo destinatario'
-	FROM DeliveryBackOffice.dbo.DeliveryLink DL WITH(NOLOCK)
-	LEFT JOIN DeliveryBackOffice.dbo.Settlement S WITH(NOLOCK)
-		ON DL.ReceiverSettlementId = S.IdSettlement
-	LEFT JOIN DeliveryBackOffice.dbo.Province P WITH(NOLOCK)
-		ON S.IdProvince = P.IdProvince
-	LEFT JOIN DeliveryBackOffice.dbo.Township T WITH(NOLOCK)
-		ON S.IdTownship = T.IdTownship
-	INNER JOIN DeliveryBackOffice.dbo.VisitPointClient VPC WITH(NOLOCK)
-		ON DL.OriginCodeOfReference = VPC.CodeOfReference
-	LEFT JOIN DeliveryBackOffice.dbo.Settlement S2 WITH(NOLOCK)
-		ON VPC.IdSettlement = S2.IdSettlement
-	LEFT JOIN DeliveryBackOffice.dbo.Province P2 WITH(NOLOCK)
-		ON S2.IdProvince = P2.IdProvince
-	LEFT JOIN DeliveryBackOffice.dbo.Township T2 WITH(NOLOCK)
-		ON S2.IdTownship = T2.IdTownship
-	INNER JOIN DeliveryBackOffice.dbo.Account A WITH(NOLOCK)
-		ON DL.AccountId = A.AccIdAccount
-	INNER JOIN DeliveryBackOffice.dbo.Customer C WITH(NOLOCK)
-		ON A.IdCustomer = C.IdCustomer
-	WHERE DL.IdDeliveryLink = @IdDeliveryLink
 
 	--PRODUCTOS
 	SELECT
@@ -61,6 +29,47 @@ BEGIN TRY
 		ON P.IdProduct = P2.ProductId
 	WHERE DeliveryLinkId = @IdDeliveryLink AND P.RowStatus = 1 
 		AND P2.Position = 1 AND P2.RowStatus = 1
+
+	IF(@OnlyProducts = 0)
+	BEGIN
+		--RESUMEN DE ENVIO
+		SELECT 
+			--DE
+			  C.Name AS 'Nombre remitente'
+			, VPC.Phone AS 'Telefono remitente'
+			, T2.TownshipName + ' , ' + P2.ProvinceName  AS 'Direccion remitente'
+			, VPC.Email AS 'Correo remitente'
+			--PARA
+			, DL.ReceiverName AS 'Nombre destinatario'
+			, DL.ReceiverPhone AS 'Telefono destinatario'
+			, T.TownshipName + ' , ' + P.ProvinceName AS 'Direccion destinatario'
+			, DL.ReceiverEmail AS 'Correo destinatario'
+			--DATOS PARA COTIZADOR
+			, ISNULL(DL.OriginCodeOfReference,'0') AS 'CodeOfReferenceSource'
+			, ISNULL(DL.DestinyCodeOfReference,'0') AS 'CodeOfReferenceDestiny'
+			,ISNULL(T2.HeaderCode,'0') 'HeaderCodeSource'
+			,ISNULL(T.HeaderCode,'0') 'HeaderCodeDestiny'
+		FROM DeliveryBackOffice.dbo.DeliveryLink DL WITH(NOLOCK)
+		LEFT JOIN DeliveryBackOffice.dbo.Settlement S WITH(NOLOCK)
+			ON DL.ReceiverSettlementId = S.IdSettlement
+		LEFT JOIN DeliveryBackOffice.dbo.Province P WITH(NOLOCK)
+			ON S.IdProvince = P.IdProvince
+		LEFT JOIN DeliveryBackOffice.dbo.Township T WITH(NOLOCK)
+			ON S.IdTownship = T.IdTownship
+		INNER JOIN DeliveryBackOffice.dbo.VisitPointClient VPC WITH(NOLOCK)
+			ON DL.OriginCodeOfReference = VPC.CodeOfReference
+		LEFT JOIN DeliveryBackOffice.dbo.Settlement S2 WITH(NOLOCK)
+			ON VPC.IdSettlement = S2.IdSettlement
+		LEFT JOIN DeliveryBackOffice.dbo.Province P2 WITH(NOLOCK)
+			ON S2.IdProvince = P2.IdProvince
+		LEFT JOIN DeliveryBackOffice.dbo.Township T2 WITH(NOLOCK)
+			ON S2.IdTownship = T2.IdTownship
+		INNER JOIN DeliveryBackOffice.dbo.Account A WITH(NOLOCK)
+			ON DL.AccountId = A.AccIdAccount
+		INNER JOIN DeliveryBackOffice.dbo.Customer C WITH(NOLOCK)
+			ON A.IdCustomer = C.IdCustomer
+		WHERE DL.IdDeliveryLink = @IdDeliveryLink
+	END;
 
 END TRY
 BEGIN CATCH
