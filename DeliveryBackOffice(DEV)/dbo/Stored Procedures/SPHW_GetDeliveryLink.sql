54ca7b757fd55d32dd0a4a7c880219f33a16f690
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2024-09-27 12:55:09 -0600

    HPW-1572: Se modifico el sp SPHW_GetDeliveryLink cambiándole nombre y agregando el proceso para actualizar los caducados.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetDeliveryLink.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetDeliveryLink.sql
@@ -3,13 +3,26 @@
 -- Create date: <2024-09-25>
 -- Description:	<Carga la informacion de los links por cuenta y pais>
 -- =============================================
-CREATE PROCEDURE sp_GetDeliveryLink
+-- =============================================
+-- Author:		<Walter Orozco>
+-- Create date: <2024-09-27>
+-- Description:	<Comprobar caducidad de links y cambio de estado a Caducado.>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHW_GetDeliveryLink]
 				 @AccountId INT,
 				 @IdCountry NVARCHAR(2) = 'GT'
 AS
 
 BEGIN
 	BEGIN TRY
+	BEGIN TRANSACTION 
+
+	DECLARE @Status INT = (SELECT IdDeliveryLinkStatus FROM DeliveryLinkStatus WHERE Name = 'Caducado')
+	DECLARE @StatusAcepted INT = (SELECT IdDeliveryLinkStatus FROM DeliveryLinkStatus WHERE Name = 'Enviado')
+	--Procedimiento para saber si su link esta caducado
+	UPDATE DeliveryBackOffice.dbo.DeliveryLink
+	SET DeliveryLinkStatusId = @Status
+	WHERE AccountId = @AccountId AND ExpirationDate < GETDATE() AND DeliveryLinkStatusId = @StatusAcepted
 
 	SELECT 1 AS StatusCode,
 		   'Registros encontrados' AS Description
@@ -30,8 +43,14 @@ BEGIN
 			ON DLS.IdDeliveryLinkStatus = DL.DeliveryLinkStatusId
 	WHERE VP.CountryId = @IdCountry
 		  AND DL.AccountId = @AccountId
+
+	COMMIT TRANSACTION;
+
 	END TRY
 	BEGIN CATCH
+		IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+
 		SELECT 0 AS StatusCode,
 			   ERROR_MESSAGE() AS Description,
 			   ERROR_LINE() AS ErrorLine
