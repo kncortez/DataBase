ef9eacd9f6a934daf3f1cb972511b3fe56217817
FLG\rene.rodriguez <oscar.vasquez@forzadelivery.com>
2024-10-07 13:02:40 -0600

    FDAPI-3006 2024-10-07 Oscar Rodriguez ADD Se agrego validacion de SP para insercion de imagenes en tabla para carrousel

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3006/1) SCRIPT Insert Imagenes para Carousel Movil App.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3006/1) SCRIPT Insert Imagenes para Carousel Movil App.sql
@@ -1,10 +1,17 @@
 -- SCRIP PARA INSERTAR DATOS DE IMAGENES POR PAIS EN TABLA PARA CAROSUSEL DE IMAGENES MOVIL APP
-INSERT INTO DeliveryBackOffice.dbo.MovilAppCarouselImage 
-(SDImageURL,MDImageURL,LDImageURL,ImageOrder,RowStatus,IdCountry,TokenCreated,DateCreated) 
-VALUES
-('https://forzadelivery.com/images/MovilApp/slider01-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider01-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider01-2560-1440.jpg',1,1,'GT','ORODRIGUEZ-SYS',GETDATE()),
-('https://forzadelivery.com/images/MovilApp/slider02-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider02-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider02-2560-1440.jpg',2,1,'GT','ORODRIGUEZ-SYS',GETDATE()),
-('https://forzadelivery.com/images/MovilApp/slider03-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider03-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider03-2560-1440.jpg',3,1,'GT','ORODRIGUEZ-SYS',GETDATE()),
-('https://forzadelivery.com/images/MovilApp/slider01-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider01-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider01-2560-1440.jpg',1,1,'HN','ORODRIGUEZ-SYS',GETDATE()),
-('https://forzadelivery.com/images/MovilApp/slider02-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider02-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider02-2560-1440.jpg',2,1,'HN','ORODRIGUEZ-SYS',GETDATE()),
-('https://forzadelivery.com/images/MovilApp/slider03-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider03-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider03-2560-1440.jpg',3,1,'HN','ORODRIGUEZ-SYS',GETDATE())
+BEGIN TRY
+    BEGIN TRANSACTION;
+		INSERT INTO DeliveryBackOffice.dbo.MovilAppCarouselImage 
+		(SDImageURL,MDImageURL,LDImageURL,ImageOrder,RowStatus,IdCountry,TokenCreated,DateCreated) 
+		VALUES
+		('https://forzadelivery.com/images/MovilApp/slider01-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider01-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider01-2560-1440.jpg',1,1,'GT','ORODRIGUEZ-SYS',GETDATE()),
+		('https://forzadelivery.com/images/MovilApp/slider02-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider02-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider02-2560-1440.jpg',2,1,'GT','ORODRIGUEZ-SYS',GETDATE()),
+		('https://forzadelivery.com/images/MovilApp/slider03-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider03-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider03-2560-1440.jpg',3,1,'GT','ORODRIGUEZ-SYS',GETDATE()),
+		('https://forzadelivery.com/images/MovilApp/slider01-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider01-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider01-2560-1440.jpg',1,1,'HN','ORODRIGUEZ-SYS',GETDATE()),
+		('https://forzadelivery.com/images/MovilApp/slider02-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider02-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider02-2560-1440.jpg',2,1,'HN','ORODRIGUEZ-SYS',GETDATE()),
+		('https://forzadelivery.com/images/MovilApp/slider03-1280-720.jpg','https://forzadelivery.com/images/MovilApp/slider03-1920-1080.jpg','https://forzadelivery.com/images/MovilApp/slider03-2560-1440.jpg',3,1,'HN','ORODRIGUEZ-SYS',GETDATE())
+	COMMIT TRANSACTION 
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+END CATCH
