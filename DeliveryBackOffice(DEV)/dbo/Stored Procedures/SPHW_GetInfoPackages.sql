97b9e54392f6a82436040d3e30a6d202d16cb4ee
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2024-10-23 15:52:41 -0600

    HPW-1624: Se modifico el sp GetInfoPackages para mejora en la validación del teléfono.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetInfoPackages.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetInfoPackages.sql
@@ -15,11 +15,25 @@ BEGIN TRY
 	DECLARE @Receiver_Phone NVARCHAR(200) = (SELECT RIGHT(LTRIM(RTRIM(Receiver_Phone)), 8) FROM DeliveryBackOffice.dbo.DeliveryOrder WITH(NOLOCK)
 											 WHERE Guide_Serie = @GuideSerie AND Guide_Number = @GuideNumber)
 
-    DECLARE @CountryId NVARCHAR(3) = (SELECT ISNULL(ReceiverCountryId,'GT') FROM DeliveryBackOffice.dbo.DeliveryOrder WITH(NOLOCK)
-											 WHERE Guide_Serie = @GuideSerie AND Guide_Number = @GuideNumber)
+    DECLARE @CodeArea NVARCHAR(5) = (
+		SELECT 
+			CASE 
+				-- Caso 1: El número comienza con '+' y tiene al menos 11 dígitos (ej. +50244444444)
+				WHEN LEFT(DO.Receiver_Phone, 1) = '+' AND LEN(DO.Receiver_Phone) >= 11 THEN 
+					SUBSTRING(DO.Receiver_Phone, 2, 3)
 
-	DECLARE @CodeArea NVARCHAR(5) = (SELECT [Value] FROM DeliveryBackOffice.dbo.ConfigParams WITH(NOLOCK)
-											WHERE [Name] = 'AreaCode' AND IdCountry = @CountryId)
+				-- Caso 2: El número comienza con un código de área sin '+' y tiene al menos 10 dígitos (ej. 50244444444)
+				WHEN LEN(DO.Receiver_Phone) >= 10 AND ISNUMERIC(LEFT(DO.Receiver_Phone, 3)) = 1 THEN 
+					LEFT(DO.Receiver_Phone, 3)
+
+				-- Caso 3: Si no tiene código de área válido, devuelve NULL (ej. 2345-6789)
+				ELSE ISNULL(C.[Value],'502')
+			END AS 'AreaCode'
+		FROM DeliveryBackOffice.dbo.DeliveryOrder DO WITH(NOLOCK)
+		LEFT JOIN DeliveryBackOffice.dbo.ConfigParams C WITH(NOLOCK)
+			ON C.[Name] = 'AreaCode' AND ISNULL(DO.ReceiverCountryId,'GT') = C.IdCountry
+		WHERE DO.Guide_Serie = @GuideSerie AND DO.Guide_Number = @GuideNumber
+	)
 	
 	DECLARE @StatusIncVal INT  = (
 		SELECT StatusOrderId FROM DeliveryBackOffice.dbo.StatusOrder
