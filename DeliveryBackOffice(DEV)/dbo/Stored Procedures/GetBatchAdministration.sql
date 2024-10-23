2fac103f66878b2150e9b9e9c2ba650e5664cadb
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2024-10-22 18:52:41 -0600

    FDAPI-3105: Se modificaron los sp de carga/editar lotes para agregar nombre del tipo de documento en ambos lados.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetBatchAdministration.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetBatchAdministration.sql
@@ -11,23 +11,26 @@ BEGIN
     BEGIN TRY
 
 	SELECT 
-		Id_Lote					AS 'IdLote'
-		,RTN
-		,CAI
-		,LimitDateEmision		AS 'Vigente hasta'
-		,Establishment			AS 'Establecimiento'
-		,Emision_Point			AS 'Punto de emisión'
-		,TypeDocument			AS 'Tipo de documento'
-		,Status					AS 'Estado'
-		,Enable					AS 'Detenido'
-		,AmountGranted			AS 'Cantidad otorgada'
+		IBH.Id_Lote					AS 'IdLote'
+		,IBH.RTN
+		,IBH.CAI
+		,IBH.LimitDateEmision		AS 'Vigente hasta'
+		,IBH.Establishment			AS 'Establecimiento'
+		,IBH.Emision_Point			AS 'Punto de emisión'
+		,IBH.TypeDocument			AS 'Tipo de documento'
+		,ISNULL(CTD.Name,'')		AS 'Nombre de tipo de documento'
+		,IBH.Status					AS 'Estado'
+		,IBH.Enable					AS 'Detenido'
+		,IBH.AmountGranted			AS 'Cantidad otorgada'
 		,CASE 
-			WHEN Last_Process > InitialRange 
-			THEN (FinalRange - InitialRange) - (Last_Process - InitialRange)	
-			ELSE (FinalRange - InitialRange)
+			WHEN IBH.Last_Process > IBH.InitialRange 
+			THEN (IBH.FinalRange - IBH.InitialRange) - (IBH.Last_Process - IBH.InitialRange)	
+			ELSE (IBH.FinalRange - IBH.InitialRange)
 		END AS 'Disponible'
-	FROM DeliveryBackOffice.dbo.InvoiceBatchHeader
-	WHERE RTN = @RTN OR @RTN IS NULL
+	FROM DeliveryBackOffice.dbo.InvoiceBatchHeader IBH WITH(NOLOCK)
+	LEFT JOIN DeliveryBackOffice.dbo.CatTypeDocument CTD WITH(NOLOCK)
+		ON IBH.TypeDocument = CTD.IdTypeDocument
+	WHERE IBH.RTN = @RTN OR @RTN IS NULL
 		
     END TRY
     BEGIN CATCH
--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetBatchAdministrationSpecific.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetBatchAdministrationSpecific.sql
@@ -12,28 +12,31 @@ BEGIN
 
 	--Devuelva toda la información de un Lote especifico
 	SELECT 
-	   [Id_Lote]							AS [Id_Lote]
-      ,ISNULL([RTN],'')						AS [RTN]
-      ,[NoDeclaracion]						AS [NoDeclaracion]
-      ,[CAI]								AS [CAI]
-      ,[LimitDateEmision]					AS [LimitDateEmision]
-      ,[Establishment]						AS [Establishment]
-      ,[Emision_Point]						AS [Emision_Point]
-      ,[TypeDocument]						AS [TypeDocument]
-      ,[RecepcionDate]						AS [RecepcionDate]
-      ,[Administration_Code]				AS [Administration_Code]
-      ,[Status]								AS [Status]
-      ,[Enable]								AS [Enable]
-      ,[InitialRange]						AS [InitialRange]
-      ,[FinalRange]							AS [FinalRange]
-      ,[Last_Process]						AS [Last_Process]
-      ,[AmountGranted]						AS [AmountGranted]
-      ,[EmailNotification]					AS [EmailNotification]
-      ,[DaysLeftNotifycation]				AS [DaysLeftNotifycation]
-      ,[PercentInvoiceLeftNotifycation]		AS [PercentInvoiceLeftNotifycation]
-      ,ISNULL([AmountRequested],0)			AS [AmountRequested]
-      ,[RowStatus]							AS [RowStatus]
-	FROM [dbo].[InvoiceBatchHeader] WITH (NOLOCK)
+	   [IBH].[Id_Lote]								AS [Id_Lote]
+      ,ISNULL([IBH].[RTN],'')						AS [RTN]
+      ,[IBH].[NoDeclaracion]						AS [NoDeclaracion]
+      ,[IBH].[CAI]									AS [CAI]
+      ,[IBH].[LimitDateEmision]						AS [LimitDateEmision]
+      ,[IBH].[Establishment]						AS [Establishment]
+      ,[IBH].[Emision_Point]						AS [Emision_Point]
+      ,[IBH].[TypeDocument]							AS [TypeDocument]
+      ,[CTD].[Name]									AS [NameTypeDocument]
+      ,[IBH].[RecepcionDate]						AS [RecepcionDate]
+      ,[IBH].[Administration_Code]					AS [Administration_Code]
+      ,[IBH].[Status]								AS [Status]
+      ,[IBH].[Enable]								AS [Enable]
+      ,[IBH].[InitialRange]							AS [InitialRange]
+      ,[IBH].[FinalRange]							AS [FinalRange]
+      ,[IBH].[Last_Process]							AS [Last_Process]
+      ,[IBH].[AmountGranted]						AS [AmountGranted]
+      ,[IBH].[EmailNotification]					AS [EmailNotification]
+      ,[IBH].[DaysLeftNotifycation]					AS [DaysLeftNotifycation]
+      ,[IBH].[PercentInvoiceLeftNotifycation]		AS [PercentInvoiceLeftNotifycation]
+      ,ISNULL([IBH].[AmountRequested],0)			AS [AmountRequested]
+      ,[IBH].[RowStatus]							AS [RowStatus]
+	FROM [dbo].[InvoiceBatchHeader] IBH WITH (NOLOCK)
+	LEFT JOIN [dbo].[CatTypeDocument] CTD WITH(NOLOCK)
+		ON [IBH].[TypeDocument] = [CTD].[IdTypeDocument]
 	WHERE [Id_Lote] = @IdLote
 
 	--Devuelve toda la información de los puntos de ventas relacionado a este Lote
@@ -51,11 +54,13 @@ BEGIN
 		ON IBD.inv_pk_id = IH.inv_pk_id
 	INNER JOIN dbo.VisitPointClient VPC WITH(NOLOCK)
 		ON IH.inv_vpCodeOfReferences = VPC.CodeOfReference
+	INNER JOIN dbo.InvoiceBatchRelationships IBR WITH(NOLOCK)
+		ON IBH.Id_Lote = IBR.Id_Lote and VPC.CodeOfReference = IBR.CodeOfReference
 	INNER JOIN dbo.CatCurrencyCOD CCC WITH(NOLOCK)
 		ON IH.IdCurrency = CCC.IdCatCurrencyCOD
 	LEFT JOIN dbo.del_ParametrosFactura PF WITH(NOLOCK)
 		ON VPC.CodeOfReference = PF.dpf_VpCodeOfReference
-	WHERE IBH.Id_Lote = @IdLote
+	WHERE IBH.Id_Lote = @IdLote and IBR.RowStatus = 1
 	GROUP BY IBD.Id_Lote , VPC.DescriptionOfClient, VPC.CodeOfReference, CCC.Symbol, PF.dpf_SAPcardCode
 	ORDER BY VPC.DescriptionOfClient
 		
