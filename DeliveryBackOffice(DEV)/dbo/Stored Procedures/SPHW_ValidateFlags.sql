057100bb958ecc1a25a58f2a715960e503ba61fa
aylinnerecinos <aylinne.recinos@forzadelivery.com>
2024-10-24 15:59:29 -0600

    HPW-1622 Actualización de validación en flags

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_ValidateFlags.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_ValidateFlags.sql
@@ -15,40 +15,50 @@ BEGIN TRY
         SELECT StatusOrderId FROM DeliveryBackOffice.dbo.StatusOrder  
         WHERE OrderDescription = 'Incidencia Validada'  
         )  
-        
-		DECLARE @f1 NVARCHAR(10) = (  
-			SELECT  
-			CASE  
-				WHEN   
-				IsLastMileReturn = 1 OR  
-				(SELECT COUNT(CI.IdConfirmationOfIncidence) FROM DeliveryBackOffice.dbo.DeliveryAttempt DA WITH(NOLOCK)  
-				INNER JOIN DeliveryBackOffice.dbo.ConfirmationOfIncidence CI WITH(NOLOCK)  
-				ON DA.ConfirmationOfIncidenceId = CI.IdConfirmationOfIncidence  
-				WHERE DA.Guide_Serie = @GuideSerie AND DA.Guide_Number = @GuideNumber   
-				AND CI.StatusOrderId = @StatusIncVal AND CI.IsConfirmed = 1 AND CI.IsDenied = 0) > 1 --INTENTO DEVOLUCIONES  
-				THEN 'false'  
-				ELSE 'true'  
-			END AS 'flagRescheduleDelivery'  
-			FROM DeliveryBackOffice.dbo.DeliveryOrder WITH(NOLOCK)  
-			WHERE Guide_Serie = @GuideSerie AND Guide_Number = @GuideNumber  
-			);  
 
-		 DECLARE @f2 NVARCHAR(10) = (  
-			SELECT  
-			CASE  
-				WHEN   
-				IsLastMileReturn = 1 OR  
-				(SELECT COUNT(CI.IdConfirmationOfIncidence) FROM DeliveryBackOffice.dbo.DeliveryAttempt DA WITH(NOLOCK)  
-				INNER JOIN DeliveryBackOffice.dbo.ConfirmationOfIncidence CI WITH(NOLOCK)  
-				ON DA.ConfirmationOfIncidenceId = CI.IdConfirmationOfIncidence  
-				WHERE DA.Guide_Serie = @GuideSerie AND DA.Guide_Number = @GuideNumber   
-				AND CI.StatusOrderId = @StatusIncVal AND CI.IsConfirmed = 1 AND CI.IsDenied = 0) > 1 --INTENTO DEVOLUCIONES  
-				THEN 'false'  
-				ELSE 'true'  
-			END AS 'flagChangeAdress'  
-			FROM DeliveryBackOffice.dbo.DeliveryOrder WITH(NOLOCK)  
-			WHERE Guide_Serie = @GuideSerie AND Guide_Number = @GuideNumber  
-			);  
+		DECLARE @StatusProcessFinal INT = (
+		SELECT IdStatusProcess FROM DeliveryBackOffice.dbo.CatStatusProcess
+		WHERE NameStatusProcess = 'Entregado'
+		)
+        DECLARE @f1 NVARCHAR(10) = (
+			SELECT
+				CASE
+					WHEN 
+						IsLastMileReturn = 1 OR
+						(SELECT COUNT(CI.IdConfirmationOfIncidence) FROM DeliveryBackOffice.dbo.DeliveryAttempt DA WITH(NOLOCK)
+						INNER JOIN DeliveryBackOffice.dbo.ConfirmationOfIncidence CI WITH(NOLOCK)
+							ON DA.ConfirmationOfIncidenceId = CI.IdConfirmationOfIncidence
+						WHERE DA.Guide_Serie = @GuideSerie AND DA.Guide_Number = @GuideNumber 
+							AND CI.StatusOrderId = @StatusIncVal AND CI.IsConfirmed = 1 AND CI.IsDenied = 0) > 1 --INTENTO DEVOLUCIONES
+						OR SO.CatStatusProcessId = @StatusProcessFinal --LA GU�A SE ENCUENTRA EN UN ESTADO ENTREGADO
+					THEN 'false'
+					ELSE 'true'
+				END AS 'flagRescheduleDelivery'
+			FROM DeliveryBackOffice.dbo.DeliveryOrder DO WITH(NOLOCK)
+			INNER JOIN DeliveryBackOffice.dbo.StatusOrder SO WITH(NOLOCK)
+				ON DO.StatusOrderId = SO.StatusOrderId
+			WHERE Guide_Serie = @GuideSerie AND Guide_Number = @GuideNumber
+			);
+
+		DECLARE @f2 NVARCHAR(10) = (
+			SELECT
+				CASE
+					WHEN 
+						IsLastMileReturn = 1 OR
+						(SELECT COUNT(CI.IdConfirmationOfIncidence) FROM DeliveryBackOffice.dbo.DeliveryAttempt DA WITH(NOLOCK)
+						INNER JOIN DeliveryBackOffice.dbo.ConfirmationOfIncidence CI WITH(NOLOCK)
+							ON DA.ConfirmationOfIncidenceId = CI.IdConfirmationOfIncidence
+						WHERE DA.Guide_Serie = @GuideSerie AND DA.Guide_Number = @GuideNumber 
+							AND CI.StatusOrderId = @StatusIncVal AND CI.IsConfirmed = 1 AND CI.IsDenied = 0) > 1 --INTENTO DEVOLUCIONES
+						OR SO.CatStatusProcessId = @StatusProcessFinal --LA GU�A SE ENCUENTRA EN UN ESTADO ENTREGADO
+					THEN 'false'
+					ELSE 'true'
+				END AS 'flagChangeAdress'
+			FROM DeliveryBackOffice.dbo.DeliveryOrder DO WITH(NOLOCK)
+			INNER JOIN DeliveryBackOffice.dbo.StatusOrder SO WITH(NOLOCK)
+				ON DO.StatusOrderId = SO.StatusOrderId
+			WHERE Guide_Serie = @GuideSerie AND Guide_Number = @GuideNumber
+			); 
 	--Validación del telefono
 	 SELECT   
 		ISNULL(@f1,'false') AS 'flagRescheduleDelivery', 
