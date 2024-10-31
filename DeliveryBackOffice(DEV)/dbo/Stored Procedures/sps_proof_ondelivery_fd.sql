3ada3bd62b223d3b0adc6d7bf9e1eb120a07fda8
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2024-10-31 14:47:40 -0600

    Stagingprod  Se agrega la coma faltante

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_proof_ondelivery_fd.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_proof_ondelivery_fd.sql
@@ -40,7 +40,7 @@ CREATE PROCEDURE [dbo].[sps_proof_ondelivery_fd]
     @ImageCold VARCHAR(300),
     @CODPayment DECIMAL(12, 2) = 0,
     @ExcludeCODPyament BIT = 'false',
-	@IdCountry NVARCHAR(8) = 'GT'
+	@IdCountry NVARCHAR(8) = 'GT',
     @Receiver_CUI NVARCHAR(25) = ''
 AS
 BEGIN
