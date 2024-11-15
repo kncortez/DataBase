edf3f578e47a792fd06b2029976f9b6a4cb85bd9
FLG\rene.rodriguez <oscar.vasquez@forzadelivery.com>
2024-11-15 14:18:32 -0600

    HPW-1736 IdSettlement express center y creacion de guias 2024-11-15 Oscar Rodriguez ADD Se agrego tipo de dato para validacion de creacion de guias con nuevo campo SenderIdSettlement en DeliveryOrder

--- DeliveryBackOffice(DEV)/dbo/User Defined Types/TblDeliveryOrdersFD.sql
+++ DeliveryBackOffice(DEV)/dbo/User Defined Types/TblDeliveryOrdersFD.sql
@@ -50,7 +50,8 @@
     [SenderIdTownship]                     INT             NULL,
     [ReceiverIdTownship]                   INT             NULL,
     [ReceiverLatitude]                     NVARCHAR (25)   NULL,
-    [ReceiverLongitude]                    NVARCHAR (25)   NULL);
+    [ReceiverLongitude]                    NVARCHAR (25)   NULL,
+	[SenderIdSettlement]                   BIGINT          NULL);
 
 
 
