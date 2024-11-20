99cc839172e2f393bcb57b997bb4d91d8846259c
EdelmanV <edelman.vasquez@forzalatam.com>
2024-11-20 15:34:10 -0600

    Script para configuración en generación de plantillas

---
+++ DeliveryBackOffice(DEV)/dbo/Scripts/Script #1 Configuración de Bancos para generación de plantillas.sql
@@ -0,0 +1,13 @@
+-- PROMERICA
+UPDATE [DeliveryBackOffice].[dbo].DeliveryBank
+SET PayingBank = 118
+WHERE Id_bank = 118
+
+--ATLANTIDA
+UPDATE [DeliveryBackOffice].[dbo].DeliveryBank
+SET PayingBank = 53
+WHERE Id_bank = 53
+
+--OCCIDENTE
+UPDATE [DeliveryBackOffice].[dbo].DeliveryBank
+SET PayingBank = 5
