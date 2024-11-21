a6607f483eb976b041fc720eb642a05550aeb626
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2024-11-21 17:37:15 -0600

    HPW-1751: Se agrego el sp SPHW_ConfirmAddress para agregar flujo de confirmación de dirección.

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_ConfirmAddress.sql
@@ -0,0 +1,31 @@
+﻿-- =============================================
+-- Author:		<Walter Orozco>
+-- Create date: <2024-11-21>
+-- Description:	<Delivery Tracking - Método para confirmar dirección del destinatario.>
+-- =============================================
+CREATE PROCEDURE [dbo].[SPHW_ConfirmAddress]
+@GuideSerie NVARCHAR(4),
+@GuideNumber INT,
+@Address NVARCHAR(200),
+@Longitude NVARCHAR(200),
+@Latitude NVARCHAR(200),
+@Token NVARCHAR(200)
+AS
+BEGIN
+BEGIN TRY
+	BEGIN TRANSACTION;
+    
+    SELECT
+		  '1' AS StatusCode
+		, 'Tu dirección ha sido confirmada exitosamente.' AS MessageResponse
+    
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION;
+
+    DECLARE @ErrorMessage NVARCHAR(4000);
+    SELECT @ErrorMessage = ERROR_MESSAGE();
+    PRINT 'Error: ' + @ErrorMessage;
+END CATCH;
+END;
