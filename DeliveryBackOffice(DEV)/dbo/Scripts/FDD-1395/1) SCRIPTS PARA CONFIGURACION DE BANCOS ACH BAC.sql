223025409e6409adac1c4d1617d690477619ccae
FLG\rene.rodriguez <oscar.vasquez@forzadelivery.com>
2024-11-27 18:04:03 -0600

    FDD-1395 Plantilla BAC Honduras 2024-11-27 Oscar Rodriguez ADD Se agregaron validaciones para plantillas BAC HN

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDD-1395/1) SCRIPTS PARA CONFIGURACION DE BANCOS ACH BAC.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDD-1395/1) SCRIPTS PARA CONFIGURACION DE BANCOS ACH BAC.sql
@@ -1,37 +1,28 @@
 -- SCRIP PARA INSERTAR NUEVO REGISTRO DE BANCO BANPAIS HN
 BEGIN TRY
     BEGIN TRANSACTION;
-		--REGISTRO DE BANCOS NUEVOS CUSCATLAN Y CREDIQ
-		INSERT INTO dbo.DeliveryBank 
-		(Id_bank,Name,Acronym,Description,create_date,Id_status,Id_country,ACHCode,PayingBank)
-		VALUES 
-		(124,'BANCO CUSCATLAN','CUSCATLAN','BANCO CUSCATLAN',GETDATE(),1,'HN',6,109),
-		(136,'BANCO CREDIQ','CREDIQ','BANCO CREDIQ',GETDATE(),1,'HN',52,109)
-
-		--QUITAR BANCOS QUE NO SE USARAN PARA HONDURAS
+		--ACTUALIZAR BANCO CUSCATLAN
 		UPDATE dbo.DeliveryBank
-		SET Id_status = 0
-		WHERE Id_bank IN (120, 115, 50, 51)
+		SET ACHCode = 6,
+		PayingBank = 109,
+		WHERE Id_bank = 124
+
+		--ACTUALIZAR BANCO CREDIQ
+		UPDATE dbo.DeliveryBank
+		SET ACHCode = 52,
+		PayingBank = 109
+		WHERE Id_bank = 136
 
 		--ACTUALIZAR BANCO CENTRAL DE HONDURAS
 		UPDATE dbo.DeliveryBank
 		SET ACHCode = 1,
-		PayingBank = 109,
-		Acronym = 'BCH'
+		PayingBank = 109
 		WHERE Id_bank = 114
-
-		--ACTUALIZAR BANCO ATLANTIDA S.A. DE C.V.
-		UPDATE dbo.DeliveryBank
-		SET PayingBank = 53
-		WHERE Id_bank = 53
 
 		--ACTUALIZAR BANCO DE OCCIDENTE S.A.
 		UPDATE dbo.DeliveryBank
 		SET ACHCode = 7,
-		PayingBank = 109,
-		Name = 'BANCO DE OCCIDENTE S.A.',
-		Description = 'BANCO DE OCCIDENTE S.A.',
-		Acronym = 'OCCIDENTE'
+		PayingBank = 109
 		WHERE Id_bank = 52
 
 		--ACTUALIZAR BANCO DE HONDURAS
@@ -49,8 +40,7 @@ BEGIN TRY
 		--ACTUALIZAR BANCO LAFISE
 		UPDATE dbo.DeliveryBank
 		SET ACHCode = 17,
-		PayingBank = 109,
-		Description = 'BANCO LAFISE'
+		PayingBank = 109
 		WHERE Id_bank = 116
 
 		--ACTUALIZAR BANCO FINANCIERA CENTROAMERICANA S.A.
@@ -65,25 +55,11 @@ BEGIN TRY
 		PayingBank = 109
 		WHERE Id_bank = 109
 
-		--ACTUALIZAR BANCO PROMERICA
-		UPDATE dbo.DeliveryBank
-		SET ACHCode = 25,
-		PayingBank = 109
-		WHERE Id_bank = 118
-
 		--ACTUALIZAR BANCO FICOHSA
 		UPDATE dbo.DeliveryBank
 		SET ACHCode = 28,
-		PayingBank = 109,
-		Name = 'BANCO FICOHSA',
-		Description = 'BANCO FICOHSA',
-		Acronym = 'FICOHSA'
+		PayingBank = 109
 		WHERE Id_bank = 108
-
-		--ACTUALIZAR BANCO DAVIVIENDA HONDURAS S.A.
-		UPDATE dbo.DeliveryBank
-		SET PayingBank = 27
-		WHERE Id_bank = 27
 
 		--ACTUALIZAR BANCO DE DESARROLLO RURAL
 		UPDATE dbo.DeliveryBank
@@ -94,10 +70,7 @@ BEGIN TRY
 		--ACTUALIZAR BANCO AZTECA
 		UPDATE dbo.DeliveryBank
 		SET ACHCode = 32,
-		PayingBank = 109,
-		Name = 'BANCO AZTECA',
-		Description = 'BANCO AZTECA',
-		Acronym = 'AZTECA'
+		PayingBank = 109
 		WHERE Id_bank = 17
 
 		--ACTUALIZAR BANCO NACIONAL DE DESARROLLO AGRICOLA
