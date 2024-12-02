27dea68a847712e4b326802d2513da3d81b8b6ae
EdelmanV <edelman.vasquez@forzalatam.com>
2024-12-02 12:04:05 -0600

    Cambiar nombre del sistema de generación de link desde usuario individual

--- DeliveryBackOffice(DEV)/dbo/Scripts/Script #1 Insert CatSystem.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/Script #1 Insert CatSystem.sql
@@ -6,4 +6,4 @@ INSERT [dbo].[CatSystem] ( [SysNameSystem], [SysPlataform], [SysDescription], [S
 VALUES ( N'One stop Link', N'Hermes Delivery Link', N'Link de envíos', 1, N'SYS-EVASQUEZ', CAST(N'2024-11-30' AS DateTime), NULL, NULL)
 
 INSERT [dbo].[CatSystem] ( [SysNameSystem], [SysPlataform], [SysDescription], [SysRowStatus], [SysTokenCreated], [SysDateCreated], [SysTokenUpdated], [SysDateUpdated])
-VALUES ( N'Portal web con Link', N'Hermes Delivery Link', N'Link de envíos', 1, N'SYS-EVASQUEZ', CAST(N'2024-11-30' AS DateTime), NULL, NULL)
+VALUES ( N'Portal web con Link', N'Web Delivery Link', N'Link de envíos', 1, N'SYS-EVASQUEZ', CAST(N'2024-11-30' AS DateTime), NULL, NULL)
