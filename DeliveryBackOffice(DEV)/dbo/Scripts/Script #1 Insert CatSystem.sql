489b52768c7215286dcbce55d20912698b1ea616
EdelmanV <edelman.vasquez@forzalatam.com>
2024-12-02 10:52:06 -0600

    #corrección de nombre de sistema

--- DeliveryBackOffice(DEV)/dbo/Scripts/Script #1 Insert CatSystem.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/Script #1 Insert CatSystem.sql
@@ -3,8 +3,7 @@ INSERT [dbo].[CatSystem] ([SysNameSystem], [SysPlataform], [SysDescription], [Sy
 VALUES ( N'One stop shot', N'Hermes Delivery Link', N'Link de envíos', 1, N'SYS-EVASQUEZ', CAST(N'2024-11-30' AS DateTime), NULL, NULL)
 
 INSERT [dbo].[CatSystem] ( [SysNameSystem], [SysPlataform], [SysDescription], [SysRowStatus], [SysTokenCreated], [SysDateCreated], [SysTokenUpdated], [SysDateUpdated]) 
-VALUES ( N'ne stop Link', N'Hermes Delivery Link', N'Link de envíos', 1, N'SYS-EVASQUEZ', CAST(N'2024-11-30' AS DateTime), NULL, NULL)
+VALUES ( N'One stop Link', N'Hermes Delivery Link', N'Link de envíos', 1, N'SYS-EVASQUEZ', CAST(N'2024-11-30' AS DateTime), NULL, NULL)
 
 INSERT [dbo].[CatSystem] ( [SysNameSystem], [SysPlataform], [SysDescription], [SysRowStatus], [SysTokenCreated], [SysDateCreated], [SysTokenUpdated], [SysDateUpdated])
 VALUES ( N'Portal web con Link', N'Hermes Delivery Link', N'Link de envíos', 1, N'SYS-EVASQUEZ', CAST(N'2024-11-30' AS DateTime), NULL, NULL)
-
