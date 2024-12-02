b5ffb7ab3bbc7a47028b0bf429f8ebeeb6470a44
EdelmanV <edelman.vasquez@forzalatam.com>
2024-12-01 21:28:03 -0600

    #Script para insert en tabla CatSystem nuevos sistemas

---
+++ DeliveryBackOffice(DEV)/dbo/Scripts/Script #1 Insert CatSystem.sql
@@ -0,0 +1,10 @@
+
+INSERT [dbo].[CatSystem] ([SysNameSystem], [SysPlataform], [SysDescription], [SysRowStatus], [SysTokenCreated], [SysDateCreated], [SysTokenUpdated], [SysDateUpdated])
+VALUES ( N'One stop shot', N'Hermes Delivery Link', N'Link de envíos', 1, N'SYS-EVASQUEZ', CAST(N'2024-11-30' AS DateTime), NULL, NULL)
+
+INSERT [dbo].[CatSystem] ( [SysNameSystem], [SysPlataform], [SysDescription], [SysRowStatus], [SysTokenCreated], [SysDateCreated], [SysTokenUpdated], [SysDateUpdated]) 
+VALUES ( N'ne stop Link', N'Hermes Delivery Link', N'Link de envíos', 1, N'SYS-EVASQUEZ', CAST(N'2024-11-30' AS DateTime), NULL, NULL)
+
+INSERT [dbo].[CatSystem] ( [SysNameSystem], [SysPlataform], [SysDescription], [SysRowStatus], [SysTokenCreated], [SysDateCreated], [SysTokenUpdated], [SysDateUpdated])
+VALUES ( N'Portal web con Link', N'Hermes Delivery Link', N'Link de envíos', 1, N'SYS-EVASQUEZ', CAST(N'2024-11-30' AS DateTime), NULL, NULL)
+
