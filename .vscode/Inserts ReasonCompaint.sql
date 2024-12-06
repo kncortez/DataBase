bd1b95d26780a2d5a5655dd922e29c51eabe4863
aylinnerecinos <aylinne.recinos@forzadelivery.com>
2024-12-06 08:59:02 -0600

    FDAPI-3213 Agrega modificaciones y consulta de catalogo reclamos

--- .vscode/Inserts ReasonCompaint.sql
+++ .vscode/Inserts ReasonCompaint.sql
@@ -1,11 +1,23 @@
 INSERT INTO [dbo].[ReasonComplaint] 
-                ([Description],[RowStatus],[TokenCreated],[DateCreated])
-		VALUES  ('Extravío', 1, 'SYS-ADMIN-MOVIL-APP', SYSDATETIME());
+                ([Description],[RowStatus],[TokenCreated],[DateCreated],[CountryId])
+		VALUES  ('Extravío', 1, 'SYS-ADMIN-MOVIL-APP', SYSDATETIME(), 'GT');
 
 INSERT INTO [dbo].[ReasonComplaint] 
-                ([Description],[RowStatus],[TokenCreated],[DateCreated])
-		VALUES  ('Daño', 1, 'SYS-ADMIN-MOVIL-APP', SYSDATETIME());
+                ([Description],[RowStatus],[TokenCreated],[DateCreated],[CountryId])
+		VALUES  ('Daño', 1, 'SYS-ADMIN-MOVIL-APP', SYSDATETIME(), 'GT');
 
 INSERT INTO [dbo].[ReasonComplaint] 
-                ([Description],[RowStatus],[TokenCreated],[DateCreated])
-		VALUES  ('Incompleto', 1, 'SYS-ADMIN-MOVIL-APP', SYSDATETIME());
+                ([Description],[RowStatus],[TokenCreated],[DateCreated],[CountryId])
+		VALUES  ('Incompleto', 1, 'SYS-ADMIN-MOVIL-APP', SYSDATETIME(), 'GT');
+
+INSERT INTO [dbo].[ReasonComplaint] 
+                ([Description],[RowStatus],[TokenCreated],[DateCreated],[CountryId])
+		VALUES  ('Extravío', 1, 'SYS-ADMIN-MOVIL-APP', SYSDATETIME(), 'HN');
+
+INSERT INTO [dbo].[ReasonComplaint] 
+                ([Description],[RowStatus],[TokenCreated],[DateCreated],[CountryId])
+		VALUES  ('Daño', 1, 'SYS-ADMIN-MOVIL-APP', SYSDATETIME(), 'HN');
+
+INSERT INTO [dbo].[ReasonComplaint] 
+                ([Description],[RowStatus],[TokenCreated],[DateCreated],[CountryId])
+		VALUES  ('Incompleto', 1, 'SYS-ADMIN-MOVIL-APP', SYSDATETIME(), 'HN');
--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3106/CREATE TABLE ReasonComplaint.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3106/CREATE TABLE ReasonComplaint.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[ReasonComplaint] (
     [ReasonComplaintId]    INT              IDENTITY (1, 1) NOT NULL,
     [Description]          NVARCHAR (80)    NULL,
+    [CountryId]            NVARCHAR (2)    NULL,
     [RowStatus]            BIT              CONSTRAINT [DF_ReasonComplaint_RowStatus] DEFAULT ((1)) NOT NULL,
     [TokenCreated]         NVARCHAR (50)    NOT NULL,
     [DateCreated]          DATETIME         NOT NULL,
