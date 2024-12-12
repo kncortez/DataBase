f2f24180cec774047fa6b2ecb869239f2c3b6c3c
FLG\rene.rodriguez <oscar.vasquez@forzadelivery.com>
2024-12-12 12:43:47 -0600

    HPW-1762 COD Anticipado 2024-12-12 Oscar Rodriguez ADD Se agrego validacion para actualizar estado pagado de guia cod anticipado

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_settlement_guide_COD.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_settlement_guide_COD.sql
@@ -4,6 +4,10 @@
 -- Create date: <2020-11-25>
 -- Description:	<Registrar transacción de liquidación (cobro) de guías en área de COD>
 -- =============================================
+-- Author:		<Oscar, Rodriguez>
+-- Create date: <2020-12-12>
+-- Description:	<Se agrego actualizacion de estado PAGADO para guias COD Anticipado>
+-- =============================================
 CREATE PROCEDURE [dbo].[sps_settlement_guide_COD]
     @GuideSerie NVARCHAR(2),
     @GuideNumbers NVARCHAR(MAX),
@@ -250,7 +254,37 @@ BEGIN
                 VALUES
                 (@GuideSerie, @GuideNumber, 24, @Token, GETDATE());
 
-				END;
+				-- Actualizamos guia liquidada cod anticipado a estado de balance PAGADO
+				IF EXISTS
+				(
+					SELECT 1
+					FROM DeliveryBackOffice.dbo.AnticipatedCODDetail acd WITH(NOLOCK)
+					WHERE	acd.GuideNumber = @GuideNumber
+						AND acd.RowStatus = 1
+				)
+				BEGIN
+					UPDATE acd
+					SET acd.BalanceStatus = 'PAGADO'
+					FROM DeliveryBackOffice.dbo.AnticipatedCODDetail acd WITH(NOLOCK)
+					WHERE	acd.GuideNumber = @GuideNumber
+						AND acd.RowStatus = 1
+
+					DECLARE @TempData TblAnticipatedCODCustomerBalance
+					INSERT INTO @TempData
+					(
+						CustomerId,
+						PortfolioId
+					)
+					SELECT ach.CustomerId, ach.PortfolioId
+					FROM DeliveryBackOffice.dbo.AnticipatedCODDetail acd WITH(NOLOCK)
+					INNER JOIN DeliveryBackOffice.dbo.AnticipatedCODHeader ach WITH(NOLOCK) 
+						ON ach.IdAnticipatedCODHeader = acd.AnticipatedCODHeaderId
+					WHERE	acd.GuideNumber = @GuideNumber
+						AND acd.RowStatus = 1
+
+					EXEC spUpdateBalanceByIdClient @TempData
+				END
+			END;
 
             END;
 
