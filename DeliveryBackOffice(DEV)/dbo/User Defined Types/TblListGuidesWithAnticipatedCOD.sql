68820e88c22c51a8aae163b5576131d3db45b659
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2024-12-16 16:37:40 -0600

    HPW-1762 Se modifica el tipo de campo

--- DeliveryBackOffice(DEV)/dbo/User Defined Types/TblListGuidesWithAnticipatedCOD.sql
+++ DeliveryBackOffice(DEV)/dbo/User Defined Types/TblListGuidesWithAnticipatedCOD.sql
@@ -1,5 +1,5 @@
 CREATE TYPE [dbo].[TblListGuidesWithAnticipatedCOD] AS TABLE (
-    [Guide_Serie]       VARCHAR (2) NULL,
+    [Guide_Serie]       NVARCHAR (2) NULL,
     [Guide_Number]      INT         NULL,
     [ExcludeCOD]        BIT         NULL,
     [IsAnticipatedCOD]  BIT         NULL
