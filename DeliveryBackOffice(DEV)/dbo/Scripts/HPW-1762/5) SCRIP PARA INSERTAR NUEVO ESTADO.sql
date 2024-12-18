23c649fa673e455a937f60a2dc154d3f10bfa1de
FLG\rene.rodriguez <oscar.vasquez@forzadelivery.com>
2024-12-18 16:18:32 -0600

    HPW-1762 COD Anticipado 2024-12-18 Oscar Rodriguez ADD Se agrego validacion para actualizacion de estado nuevo de COD Anticipado

--- DeliveryBackOffice(DEV)/dbo/Scripts/HPW-1762/5) SCRIP PARA INSERTAR NUEVO ESTADO.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/HPW-1762/5) SCRIP PARA INSERTAR NUEVO ESTADO.sql
@@ -3,7 +3,7 @@ BEGIN TRY
     BEGIN TRANSACTION;
 		
 		INSERT INTO DeliveryBackOffice.dbo.StatusOrder (OrderDescription,CatCheckpointTypeId,CatStatusTypeId,StatusMessage,StatusOrderTrackingDescription,RowStatus,TokenCreated,DateCreated, CatstatusProcessId)
-		VALUES ('Recepcionado en Express Center COD Anticipado',2,2,'La guia de COD Anticipado fue Recibida en Express Center','Guia COD Anticipado Recepcionada en Express Center de manera Exitosa',1,'SYS-ORODRIGUEZ',GETDATE(),2)
+		VALUES ('COD Anticipado',1,2,'La guia de COD Anticipado fue procesada exitosamente','La guia de COD Anticipado fue procesada exitosamente',1,'SYS-ORODRIGUEZ',GETDATE(),1)
 	
 	COMMIT TRANSACTION 
 END TRY
