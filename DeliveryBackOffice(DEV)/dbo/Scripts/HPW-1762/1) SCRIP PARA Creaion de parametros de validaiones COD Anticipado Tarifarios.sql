c236b8f54a5019b49ad0e38ee3837871a4ff9efb
FLG\rene.rodriguez <oscar.vasquez@forzadelivery.com>
2024-12-26 10:50:30 -0600

    HPW-1762 COD Anticipado 2024-12-26 Oscar Rodriguez ADD Se agregaron validaciones para separacion de estados de COD Pagado y COD Pagado Anticipado, junto con ajustes de parametrizaciones de valores HN

--- DeliveryBackOffice(DEV)/dbo/Scripts/HPW-1762/1) SCRIP PARA Creaion de parametros de validaiones COD Anticipado Tarifarios.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/HPW-1762/1) SCRIP PARA Creaion de parametros de validaiones COD Anticipado Tarifarios.sql
@@ -2,16 +2,24 @@
 BEGIN TRY
     BEGIN TRANSACTION;
 		ALTER TABLE DeliveryBackOffice.dbo.RateHeader
-		ADD GuideAmountCOD DECIMAL(18,2) DEFAULT 800,
-		ReturnPercent INT DEFAULT 4,
-		IsOldest INT DEFAULT 90,
-		MinGuidesPerMonth INT DEFAULT 25;
+		ADD GuideAmountCOD DECIMAL(18,2),
+		ReturnPercent INT,
+		IsOldest INT,
+		MinGuidesPerMonth INT;
 
 		--UPDATE DeliveryBackOffice.dbo.RateHeader
 		--SET GuideAmountCOD = 800,
 		--ReturnPercent = 4,
 		--IsOldest = 30,
-		--MinGuidesPerMonth = 25;
+		--MinGuidesPerMonth = 25
+		--WHERE IdCountry = 'GT';
+
+		--UPDATE DeliveryBackOffice.dbo.RateHeader
+		--SET GuideAmountCOD = 2400,
+		--ReturnPercent = 4,
+		--IsOldest = 30,
+		--MinGuidesPerMonth = 25
+		--WHERE IdCountry = 'HN';
 	COMMIT TRANSACTION 
 END TRY
 BEGIN CATCH
--- DeliveryBackOffice(DEV)/dbo/Scripts/HPW-1762/5) SCRIP PARA INSERTAR NUEVO ESTADO.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/HPW-1762/5) SCRIP PARA INSERTAR NUEVO ESTADO.sql
@@ -3,7 +3,9 @@ BEGIN TRY
     BEGIN TRANSACTION;
 		
 		INSERT INTO DeliveryBackOffice.dbo.StatusOrder (OrderDescription,CatCheckpointTypeId,CatStatusTypeId,StatusMessage,StatusOrderTrackingDescription,RowStatus,TokenCreated,DateCreated, CatstatusProcessId)
-		VALUES ('COD Anticipado',1,2,'La guia de COD Anticipado fue procesada exitosamente','La guia de COD Anticipado fue procesada exitosamente',1,'SYS-ORODRIGUEZ',GETDATE(),1)
+		VALUES 
+		('COD Anticipado',1,2,'La guia de COD Anticipado fue procesada exitosamente','La guia de COD Anticipado fue procesada exitosamente',1,'SYS-ORODRIGUEZ',GETDATE(),1),
+		('COD Pagado Anticipado',1,2,'La guia de COD Anticipado fue Pagada exitosamente','La guia de COD Anticipado fue Pagada exitosamente',1,'SYS-ORODRIGUEZ',GETDATE(),1)
 	
 	COMMIT TRANSACTION 
 END TRY
