0226ca8fd79c12a5f9d8710f9a994d3082c7166c
aylinnerecinos <aylinne.recinos@forzadelivery.com>
2024-12-31 11:36:22 -0600

    FDAPI-3261 Metodo para listar poblados

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_get_list_settlement_by_header_code.sql
@@ -0,0 +1,25 @@
+-- =============================================
+-- Author:		<Aylinne Recinos>
+-- Create date: <2024-12-30>
+-- Description:	<Consulta del listado de poblados>
+-- =============================================
+ALTER PROCEDURE [dbo].[spws_get_list_settlement_by_header_code]
+	-- Add the parameters for the stored procedure here
+	@HeaderCodeTownship as nvarchar(10)  = '-1', --all
+	@HeaderCode as  nvarchar(2) = '-1', --all
+	@CountryId as nvarchar(2) = NULL
+AS
+BEGIN
+	SELECT sett.Settlement, sett.IdSettlement, ISNULL(sett.IdCountry, @CountryId), sett.IsSpecial FROM [dbo].[Settlement] sett
+	INNER JOIN 
+			DeliveryBackOffice.dbo.Province depto ON sett.IdProvince = depto.IdProvince 
+	INNER JOIN 
+			DeliveryBackOffice.dbo.Township mun ON sett.IdTownship = mun.IdTownship
+	WHERE 
+		sett.SettlementSatus = 1
+		AND depto.ProvinceStatus = 1 
+		AND mun.TownshipStatus = 1 
+		AND ISNULL(NULLIF(@CountryId,''), depto.IdCountry) = depto.IdCountry
+		AND (@HeaderCodeTownship = '-1' OR mun.HeaderCode = @HeaderCodeTownship)
+		AND (@HeaderCode = '-1' OR depto.LocalCode = @HeaderCode)
+END
