139288f899f598237b5f2ba306b8bc57445dc65b
aylinnerecinos <aylinne.recinos@forzadelivery.com>
2024-12-31 12:34:05 -0600

    FDAPI-3261 Modificacion datos devueltos

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_get_list_settlement_by_header_code.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_get_list_settlement_by_header_code.sql
@@ -3,14 +3,18 @@
 -- Create date: <2024-12-30>
 -- Description:	<Consulta del listado de poblados>
 -- =============================================
-ALTER PROCEDURE [dbo].[spws_get_list_settlement_by_header_code]
+CREATE PROCEDURE [dbo].[spws_get_list_settlement_by_header_code]
 	-- Add the parameters for the stored procedure here
 	@HeaderCodeTownship as nvarchar(10)  = '-1', --all
 	@HeaderCode as  nvarchar(2) = '-1', --all
 	@CountryId as nvarchar(2) = NULL
 AS
 BEGIN
-	SELECT sett.Settlement, sett.IdSettlement, ISNULL(sett.IdCountry, @CountryId), sett.IsSpecial FROM [dbo].[Settlement] sett
+	SELECT 	sett.Settlement AS [Settlement], 
+			sett.IdSettlement AS [IdSettlement], 	
+			ISNULL(sett.IdCountry, @CountryId) AS [IdCountry], 
+			sett.IsSpecial AS [IsSpecial] 
+	FROM [dbo].[Settlement] sett
 	INNER JOIN 
 			DeliveryBackOffice.dbo.Province depto ON sett.IdProvince = depto.IdProvince 
 	INNER JOIN 
