8ecd4a9d393c7aace44875a87d5c6e881d0adf04
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2025-01-08 14:52:00 -0600

    FDD-1459 Se agrega filtro para obtener solo las guias que no estan en estados terminales

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphd_getGuidesFromReferenceNumber.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphd_getGuidesFromReferenceNumber.sql
@@ -3,6 +3,10 @@
 -- Create date: <2024-10-29>
 -- Description:	<Se obtienen las guías según el número de referencia>
 -- =============================================
+-- Author:		<Tito García>
+-- Updated date: <2025-01-08>
+-- Description:	<Se agrega filtro para obtner las guias que no estan en estados terminales>
+-- =============================================
 create PROCEDURE [dbo].[sphd_getGuidesFromReferenceNumber]
 	@ReferenceNumber NVARCHAR(150),
 	@IdCountry VARCHAR(2) = 'GT'
@@ -14,9 +18,13 @@ BEGIN
 	
 		SELECT TOP 10 CONCAT(Guide_Serie, Guide_Number) AS Guide
 		FROM [dbo].[DeliveryOrder] do WITH (NOLOCK)
-		WHERE do.Ticket_Number = @ReferenceNumber
-			AND [do].SenderCountryId = @IdCountry
-		ORDER BY DateCreated DESC
+            INNER JOIN [dbo].[StatusOrder] [so] WITH (NOLOCK)
+                ON [do].[StatusOrderId] = [so].[StatusOrderId]
+		WHERE so.CatCheckpointTypeId <> 3
+			AND so.RowStatus = 1
+			AND do.Ticket_Number = @ReferenceNumber
+			AND do.SenderCountryId = @IdCountry
+		ORDER BY do.DateCreated DESC
 
     END TRY 
 	BEGIN CATCH
