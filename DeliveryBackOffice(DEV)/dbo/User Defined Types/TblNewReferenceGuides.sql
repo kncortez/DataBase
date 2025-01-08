7bd19842c6bd55b96093fe54851d585edcfe89c2
aylinnerecinos <aylinne.recinos@forzadelivery.com>
2025-01-08 09:12:38 -0600

    FDAPI-3267 Mueve sp de historia

--- DeliveryBackOffice(DEV)/dbo/User Defined Types/TblNewReferenceGuides.sql
+++
@@ -1,5 +0,0 @@
-CREATE TYPE [dbo].[TblNewReferenceGuides] AS TABLE (
-    [GuideSerie]                            NVARCHAR (2)    NOT NULL,
-    [GuideNumber]                           INT             NOT NULL,
-    [TicketNumber]                          NVARCHAR (150)  NOT NULL
-);
