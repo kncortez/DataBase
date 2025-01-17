51e68819babd9c506ebff9fe9b3b047e9f8046be
brandon-p <brandon.pedroza@forzadelivery.com>
2025-01-17 16:34:09 -0600

    FDAPI-3271: #se agrega validacion para tomar en cuenta referencia o numero de contenedor al escanear en recolecciones

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetGuidesByContainerByReference.sql
@@ -0,0 +1,57 @@
+﻿CREATE PROCEDURE [dbo].[GetGuidesByContainerByReference]
+	@Container TblContainerList READONLY,
+	@References TblReferencesList READONLY,
+	@IdCountry NVARCHAR(2) = 'GT'
+AS
+BEGIN
+	DECLARE @StatusContainerPickUp INT;
+
+	SET @StatusContainerPickUp = (
+			SELECT IdCatStatus
+			FROM CatShipContainerStatus WITH (NOLOCK)
+			WHERE [Name] = 'Creado'
+			);
+            -- Consulta 2 
+     WITH CTE_Ranked AS (    
+          SELECT  DOP.GuideSerie,
+                  DOP.GuideNumber
+                  ,DOP.NoPiece
+		          ,DO.Ticket_Number
+		          ,ROW_NUMBER() OVER (
+			                          PARTITION BY DO.Ticket_Number ORDER BY DOP.GuideSerie DESC
+				                      ,DOP.GuideNumber DESC
+			                         ) AS RowNum
+		          ,DO.DateCreated
+	        FROM DeliveryOrder DO WITH (NOLOCK)
+	             INNER JOIN DeliveryOrderPiece DOP WITH(NOLOCK)
+				 ON DO.Guide_Serie = DOP.GuideSerie
+	                AND DO.Guide_Number = DOP.GuideNumber
+	       WHERE DO.Ticket_Number IN
+                 (
+	              SELECT ReferenceGuide
+	                FROM @References
+	             )
+            AND ISNULL(DO.SenderCountryId, 'GT') = @IdCountry 
+       )
+	SELECT GuideSerie
+		   ,GuideNumber
+		   ,NoPiece
+	FROM CTE_Ranked
+	WHERE RowNum = 1
+	UNION
+	SELECT DOP.GuideSerie
+	       ,DOP.GuideNumber
+	       ,DOP.NoPiece
+	  FROM ShippingContainer CT WITH (NOLOCK)
+	       INNER JOIN ShippingContainerDetail CTD WITH (NOLOCK) 
+              ON CT.IdContainer = CTD.IdContainer
+	       INNER JOIN DeliveryOrderPiece DOP WITH (NOLOCK)
+		   ON CTD.GuideSerie = DOP.GuideSerie
+	   AND CTD.GuideNumber = DOP.GuideNumber
+	 WHERE CT.IdStatusContainer = @StatusContainerPickUp
+	   AND CTD.RowStatus = 1
+	   AND CT.ReferenceContainer IN (
+			SELECT ContainerReference
+			FROM @Container
+			)
+END
