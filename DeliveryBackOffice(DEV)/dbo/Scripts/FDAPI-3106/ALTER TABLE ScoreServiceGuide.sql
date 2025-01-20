7b5c0937e9d0646a7b7cf23a35ee0222a6a0b774
aylinnerecinos <aylinne.recinos@forzadelivery.com>
2025-01-20 11:35:05 -0600

    FDAPI-3321 Agrega metodo de evaluación de destinatario

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3106/ALTER TABLE ScoreServiceGuide.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3106/ALTER TABLE ScoreServiceGuide.sql
@@ -7,6 +7,18 @@ ADD IsGoodConditions BIT NULL;
 ALTER TABLE [dbo].[ScoreServiceGuide]
 ADD IsGoodSender BIT NULL;
 
+ALTER TABLE [dbo].[ScoreServiceGuide]
+ADD IsGoodCommunication BIT NULL;
+
+ALTER TABLE [dbo].[ScoreServiceGuide]
+ADD IsGoodReceiver BIT NULL;
+
+ALTER TABLE [dbo].[ScoreServiceGuide]
+ADD ScoreReceiver DECIMAL(5,2) NULL;
+
+ALTER TABLE [dbo].[ScoreServiceGuide]
+ALTER COLUMN Score DECIMAL(5,2) NULL;
+
 -- Agregar descripciones para cada columna
 EXEC sp_addextendedproperty 
     @name = N'MS_Description'
@@ -28,3 +40,24 @@ EXEC sp_addextendedproperty
 	, @level0type = N'SCHEMA', @level0name = 'dbo'
 	, @level1type = N'TABLE', @level1name = 'ScoreServiceGuide'
 	, @level2type = N'COLUMN', @level2name = 'IsGoodSender';
+
+EXEC sp_addextendedproperty 
+    @name = N'MS_Description'
+    , @value = N'Valida si la comunicación con el destinatario fue clara y rápida'
+	, @level0type = N'SCHEMA', @level0name = 'dbo'
+	, @level1type = N'TABLE', @level1name = 'ScoreServiceGuide'
+	, @level2type = N'COLUMN', @level2name = 'IsGoodCommunication';
+
+EXEC sp_addextendedproperty 
+    @name = N'MS_Description'
+    , @value = N'Valida si repetiría el envío a este destinatario'
+	, @level0type = N'SCHEMA', @level0name = 'dbo'
+	, @level1type = N'TABLE', @level1name = 'ScoreServiceGuide'
+	, @level2type = N'COLUMN', @level2name = 'IsGoodReceiver';
+
+EXEC sp_addextendedproperty 
+    @name = N'MS_Description'
+    , @value = N'Calificación del destinatario'
+	, @level0type = N'SCHEMA', @level0name = 'dbo'
+	, @level1type = N'TABLE', @level1name = 'ScoreServiceGuide'
+	, @level2type = N'COLUMN', @level2name = 'ScoreReceiver';
