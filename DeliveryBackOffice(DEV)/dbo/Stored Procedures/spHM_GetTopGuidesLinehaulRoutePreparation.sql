d50134a53c7c988d22a7daa1bb9879e95ee288b7
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2025-01-22 10:17:03 -0600

    FDAPI-3324: Se modifico el SP spHM_GetTopGuidesLinehaulRoutePreparation para obtener código de referencia si la guía cuenta con uno.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetTopGuidesLinehaulRoutePreparation.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetTopGuidesLinehaulRoutePreparation.sql
@@ -3,6 +3,11 @@
 -- Create date: <12-09-2022>
 -- Description:	<Get TOP 20 guides in LinehaulRoutePreparationContainerDetail>
 -- =============================================
+-- =============================================
+-- Author:		<Walter Orozco>
+-- Create date: <20-01-2025>
+-- Description:	<Se agrego ticket number para guías que tengan código de referencia>
+-- =============================================
 CREATE PROCEDURE [dbo].[spHM_GetTopGuidesLinehaulRoutePreparation]
 	@LinehaulRoutePreparationId AS INT
 AS
@@ -14,18 +19,21 @@ BEGIN
 	SELECT		TOP 40
 				[LRPCD].[GuideSerie],
 				[LRPCD].[GuideNumber],
+				ISNULL([SCD].[TicketNumber],'') AS 'TicketNumber',
 				[LRPCD].[DryPieceQuantity],
 				[LRPCD].[ColdPieceQuantity],
 				[CTC].[TypeContainerSerie],
 				[C].[ContainerNumber]
-	FROM		[dbo].[LinehaulRoutePreparationContainerDetail] LRPCD
-	INNER JOIN	[dbo].[LinehaulRoutePreparationContainer] LRPC
+	FROM		[dbo].[LinehaulRoutePreparationContainerDetail] LRPCD  WITH(NOLOCK)
+	INNER JOIN	[dbo].[LinehaulRoutePreparationContainer] LRPC WITH(NOLOCK)
 		ON		[LRPCD].[LinehaulRoutePreparationContainerId] = [LRPC].[IdLinehaulRoutePreparationContainer]
 		AND		[LRPC].[LinehaulRoutePreparationId] = @LinehaulRoutePreparationId
-	INNER JOIN	[dbo].[Container] C
+	INNER JOIN	[dbo].[Container] C WITH(NOLOCK)
 		ON		[LRPC].[ContainerId] = [C].[IdContainer]
-	INNER JOIN	[dbo].[CatTypeContainer] CTC
+	INNER JOIN	[dbo].[CatTypeContainer] CTC WITH(NOLOCK)
 		ON		[C].[CatTypeContainerId] = [CTC].[IdCatTypeContainer]
+	LEFT JOIN	[dbo].[ShippingContainerDetail] SCD WITH(NOLOCK)
+		ON		[LRPCD].[GuideSerie] = [SCD].[GuideSerie] AND [LRPCD].[GuideNumber] = [SCD].[GuideNumber]
 	WHERE		[LRPCD].[RowStatus] = 1
 	AND			[LRPCD].[IsOpenProcess] = 0
 	ORDER BY	[LRPCD].[DateCreated] DESC;
