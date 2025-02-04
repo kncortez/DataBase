de6726c9f3ecc2dd7ecc22a46c7c45b77f09be43
EdelmanV <edelman.vasquez@forzalatam.com>
2025-02-03 23:05:06 -0600

    #Actualizar nombre de variables de paquetes

--- DeliveryBackOffice(DEV)/dbo/Scripts/Actualizar  No #2 Suscripciones y Paquetes de Tienda Virtual.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/Actualizar  No #2 Suscripciones y Paquetes de Tienda Virtual.sql
@@ -8,10 +8,10 @@
 
 DECLARE @IdCatSubscriptionPETIT INT =(SELECT IdCatSubscription FROM [dbo].[CatSubscription] WHERE SubscriptionName='Paquete Petit' AND IdCountry='GT')
 DECLARE @IdCatSubscriptionBASICO INT =(SELECT IdCatSubscription FROM [dbo].[CatSubscription] WHERE SubscriptionName='Paquete Básico' AND IdCountry='GT')
-DECLARE @IdCatSubscriptionPLUS INT =(SELECT IdCatSubscription FROM [dbo].[CatSubscription] WHERE SubscriptionName='Paquete Petit' AND IdCountry='GT')
-DECLARE @IdCatSubscriptionGOLD INT =(SELECT IdCatSubscription FROM [dbo].[CatSubscription] WHERE SubscriptionName='Paquete Básico' AND IdCountry='GT')
-DECLARE @IdCatSubscriptionPLATINO INT =(SELECT IdCatSubscription FROM [dbo].[CatSubscription] WHERE SubscriptionName='Paquete Petit' AND IdCountry='GT')
-DECLARE @IdCatSubscriptionPRO INT =(SELECT IdCatSubscription FROM [dbo].[CatSubscription] WHERE SubscriptionName='Paquete Básico' AND IdCountry='GT')
+DECLARE @IdCatSubscriptionPLUS INT =(SELECT IdCatSubscription FROM [dbo].[CatSubscription] WHERE SubscriptionName='Paquete Plus' AND IdCountry='GT')
+DECLARE @IdCatSubscriptionGOLD INT =(SELECT IdCatSubscription FROM [dbo].[CatSubscription] WHERE SubscriptionName='Paquete Gold' AND IdCountry='GT')
+DECLARE @IdCatSubscriptionPLATINO INT =(SELECT IdCatSubscription FROM [dbo].[CatSubscription] WHERE SubscriptionName='Paquete Platino' AND IdCountry='GT')
+DECLARE @IdCatSubscriptionPRO INT =(SELECT IdCatSubscription FROM [dbo].[CatSubscription] WHERE SubscriptionName='Paquete Pro' AND IdCountry='GT')
 
 /*Actualizar Precio en Catálogo*/
 
@@ -45,6 +45,14 @@ UPDATE [dbo].[CatSubscription]
 SET SubscriptionDescription ='500 guias a Q22 C/U',
     SubscriptionCost=11000.00
 WHERE  IdCatSubscription = @IdCatSubscriptionPRO  AND  IdCountry='GT'
+
+/*Actualizar rango de descuento*/
+
+UPDATE  [dbo].[CatSubscriptionDiscountRange]
+SET DiscountLowServiceRange =500
+WHERE  CatSubscriptionId = @IdCatSubscriptionPRO
+
+/*Actualizar Suscripción*/
 
 
 
