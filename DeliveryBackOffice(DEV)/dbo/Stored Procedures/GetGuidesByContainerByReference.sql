c943327aba94c6211960752e82915cca7aeb2ad2
brandon-p <brandon.pedroza@forzadelivery.com>
2025-02-05 16:31:22 -0600

    FDAPI-3395: # se agregan validacion para aceptar guias del cliente de recoleccion en app de recolecciones

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetGuidesByContainerByReference.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetGuidesByContainerByReference.sql
@@ -3,19 +3,33 @@
 -- Create date: <2025-01-13>  
 -- Description: <Contenerizacion guias - obtiene las guias por referencia y por contenedor>  
 -- =============================================  
+-- Author:		<Brandon, Pedroza>  
+-- Modifie:		<2025-02-04>  
+-- Description: <Contenerizacion guias - aceptar paquete unicamente del cliente asignado a la recoleccion>  
+-- =============================================   
 CREATE PROCEDURE [dbo].[GetGuidesByContainerByReference]
 	@Container TblContainerList READONLY,
 	@References TblReferencesList READONLY,
-	@IdCountry NVARCHAR(2) = 'GT'
+	@IdCountry NVARCHAR(2) = 'GT',
+	@IdPickup INT = NULL
 AS
 BEGIN
-	DECLARE @StatusContainerPickUp INT;
+	DECLARE @StatusContainerCreated INT,@IdCustomerPickup INT;
 
-	SET @StatusContainerPickUp = (
+	SET @StatusContainerCreated = (
 			SELECT IdCatStatus
 			FROM CatShipContainerStatus WITH (NOLOCK)
 			WHERE [Name] = 'Creado'
 			);
+
+	SET @IdCustomerPickup =(
+		SELECT TOP 1 VPC.CustomerID
+			FROM SchedulePickup SP WITH(NOLOCK)
+						INNER JOIN VisitPointClient VPC WITH(NOLOCK)
+						ON SP.SenderId = VPC.CodeOfReference
+						WHERE SP.SchedulePickupStatus = 1
+						AND SP.SchedulePickupId =@IdPickup
+	);
             -- Consulta 2 
      WITH CTE_Ranked AS (    
           SELECT  DOP.GuideSerie,
@@ -38,12 +52,14 @@ BEGIN
 					WHERE ReferenceGuide NOT IN ('','0')
 	             )
             AND ISNULL(DO.SenderCountryId, 'GT') = @IdCountry 
+			AND DO.StatusOrderId IN (1,15)
+			AND DO.IdCustomer =@IdCustomerPickup
        )
 	SELECT GuideSerie
 		   ,GuideNumber
 		   ,NoPiece
 	FROM CTE_Ranked
-	WHERE RowNum = 1
+	--WHERE RowNum = 1
 	UNION
 	SELECT DOP.GuideSerie
 	       ,DOP.GuideNumber
@@ -54,10 +70,11 @@ BEGIN
 	       INNER JOIN DeliveryOrderPiece DOP WITH (NOLOCK)
 		   ON CTD.GuideSerie = DOP.GuideSerie
 	   AND CTD.GuideNumber = DOP.GuideNumber
-	 WHERE CT.IdStatusContainer = @StatusContainerPickUp
+	 WHERE CT.IdStatusContainer = @StatusContainerCreated
 	   AND CTD.RowStatus = 1
 	   AND CT.ReferenceContainer IN (
 			SELECT ContainerReference
 			FROM @Container
 			)
+		AND CT.IdCustomer = @IdCustomerPickup
 END
