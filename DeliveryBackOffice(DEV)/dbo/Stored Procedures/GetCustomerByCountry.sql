601339cc74ae1afe3dc29a139a4757da196c8d4a
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2025-02-21 09:02:53 -0600

    HPW-1778 Se realizan ajustes en SPs

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetCustomerByCountry.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetCustomerByCountry.sql
@@ -12,26 +12,30 @@ BEGIN
 	BEGIN TRY
 		DECLARE @CorporativeCustomerID INT
 
-		SELECT @CorporativeCustomerID = idCustomerType 
+		SELECT TOP 1 @CorporativeCustomerID = idCustomerType 
 		FROM customerType WITH (NOLOCK)
 		WHERE CustomerTypeStatus = 1
-			AND Description = 'CORPORATIVO'
+			AND Description = 'CORPORATIVO';
+
+		IF @CorporativeCustomerID IS NULL
+        BEGIN
+            SELECT 0 AS 'StatusCode', 'No se encontró tipo de cliente corporativo' AS Description;
+            RETURN;
+        END;
 
 		SELECT IdCustomer AS Id, Name
 		FROM Customer WITH (NOLOCK)
 		WHERE idCustomerType = @CorporativeCustomerID
 			AND CountryID = @CountryId
 			AND RowSatus = 1
-		ORDER BY Name ASC
+		ORDER BY Name ASC;
 
-		SELECT 1 AS 'StatusCode', 
-        'SUCCESS' AS 'Description' 
+		SELECT 1 AS 'StatusCode', 'SUCCESS' AS 'Description' 
 
     END TRY 
 	BEGIN CATCH
 
-        SELECT 0 AS 'StatusCode', 
-                ERROR_MESSAGE() AS 'Description' 
+        SELECT 0 AS 'StatusCode', ERROR_MESSAGE() AS 'Description' 
 	
 	END CATCH
 END;
