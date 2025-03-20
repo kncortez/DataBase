a67634da52456612df15b1d591e29f7a0141e237
EdelmanV <edelman.vasquez@forzalatam.com>
2025-03-20 10:42:25 -0600

    #Correcciones de compatibilidad para sps del método BillingRequest

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_InformationInvoice.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_InformationInvoice.sql
@@ -20,11 +20,80 @@ BEGIN
         WHERE inv_pk_id = @idInvoice;
 
     -- Insert statements for procedure here
-    SELECT *
+    SELECT  
+	     inv_pk_id,
+		inv_vpCodeOfReferences,
+		inv_cmp_name,
+		inv_cmp_nameComercial,
+		inv_cmp_adress,
+		inv_cmp_nit,
+		inv_cli_name,
+		inv_cli_adress,
+		inv_cli_nit,
+		inv_cli_email,
+		inv_date,
+		inv_documentSend,
+		inv_documentRecieved,
+		inv_certificationFEL,
+		inv_serieFEL,
+		inv_numberFEL,
+		inv_descriptionFEL,
+		inv_RequestorFEL,
+		inv_TransactionFEL,
+		inv_CountryFEL,
+		inv_EntityFEL,
+		inv_UserFEL,
+		inv_UserName,
+		inv_Data1FEL,
+		inv_Data3FEL,
+		inv_MailSendFEL,
+		inv_subjectFEL,
+		inv_IVA,
+		inv_amount,
+		inv_status,
+		inv_dateRegister,
+		inv_tokenRegister,
+		inv_dateUpdate,
+		inv_tokenUpdate,
+		inv_type,
+		inv_invoiceOfCreditNote,
+		inv_motiveCreditNote,
+		inv_dateOriginDocument,
+		inv_documentOriginFEL,
+		inv_creditNote,
+		inv_establecimientoFEL,
+		inv_cmp_nameFEL,
+		inv_FechaHoraFEL,
+		inv_SAPDocEntry,
+		inv_SAPError,
+		systemOperation,
+		IsManualInvoice,
+		inv_dateFEL,
+		CatInvoiceTypeId,
+		Retries,
+		IdCurrency,
+		IdCountry
     FROM [dbo].[invoiceHeader] WITH (NOLOCK)
-    WHERE inv_pk_id = @idInvoice;
+    WHERE inv_pk_id =  @idInvoice;
 
-    SELECT *
+    SELECT 
+	        dti_fk_header,
+			dti_fk_orderSerie,
+			dti_fk_orderNumber,
+			dti_identification,
+			dti_category,
+			dti_quantity,
+			dti_measurement,
+			dti_priceUnit,
+			dti_description,
+			dti_IVA,
+			dti_amount,
+			dti_dateRegister,
+			dti_tokenRegister,
+			SAPCode,
+			SendToInvoice,
+			MembershipId,
+			SubscriptionId
     FROM [dbo].[invoiceDetail] WITH (NOLOCK)
     WHERE dti_fk_header = @idInvoice
     ORDER BY dti_dateRegister;
