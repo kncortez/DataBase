3178f45db3cc19ef36173c825e9d1434874436be
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2025-04-02 14:49:42 -0600

    FDAPI-3279 Se modifica consulta en SP

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_GetRouteHubBySettlement.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_GetRouteHubBySettlement.sql
@@ -9,21 +9,17 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	SELECT 
-		cr.CodeRoute, 
-		cr.IdRoute, 
-		hub.IdHubLogistic, 
+	SELECT dsc.RouteCode,
 		hub.HubAbbreviation
 	FROM dbo.DumpServiceCoverage dsc  WITH (NOLOCK)
 		INNER JOIN dbo.Settlement setl    WITH (NOLOCK)
 			ON setl.IdSettlement=dsc.IdSettlement 
-		INNER JOIN dbo.CatRoute cr WITH (NOLOCK)
-			ON dsc.RouteCode = cr.CodeRoute
 		INNER JOIN DeliveryBackOffice.dbo.HubLogistics hub WITH(NOLOCK)  
 			ON RTRIM(LTRIM(hub.HubAbbreviation)) = RTRIM(LTRIM(dsc.Hub))
 	WHERE dsc.RowStatus=1
 		AND setl.SettlementSatus = 1
 		AND hub.HubStatus = 1
-		AND cr.RowStatus = 1
-		AND setl.IdSettlement = @IdSettlement;
+		AND setl.IdSettlement = @IdSettlement
+		AND dsc.RouteCode IS NOT NULL;
+
 END;
