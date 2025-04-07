c05b95ca8431a2fab269c883d313023807d8c953
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2025-04-07 09:45:14 -0600

    FDAPI-3730: Se agrego script para multipais para configuraciones de COD anticipado SV.

--- DeliveryBackOffice(DEV)/dbo/Scripts/ScriptsMultipaisSV/01 Script agregar ConfigParams SV.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/ScriptsMultipaisSV/01 Script agregar ConfigParams SV.sql
@@ -109,6 +109,150 @@ BEGIN TRY
 		,'SV'
 		,NULL)
 
+	--SCRIPT PARA INSERTAR EL VALOR DE MinRangeCODComisison1Param DE SV EN ConfigParams
+	INSERT INTO [dbo].[ConfigParams]
+		([Name]
+		,[Description]
+		,[Value]
+		,[Status]
+		,[CreateDate]
+		,[IdCountry]
+		,[IdCurrencyCOD])
+	VALUES
+		('MinRangeCODComisison1Param'
+		,'Minimo de rango para comision COD Anticipado rango 1 - 300'
+		,'1'
+		,1
+		,GETDATE()
+		,'SV'
+		,NULL)
+
+	--SCRIPT PARA INSERTAR EL VALOR DE MinRangeCODComisison2Param DE SV EN ConfigParams
+	INSERT INTO [dbo].[ConfigParams]
+		([Name]
+		,[Description]
+		,[Value]
+		,[Status]
+		,[CreateDate]
+		,[IdCountry]
+		,[IdCurrencyCOD])
+	VALUES
+		('MinRangeCODComisison2Param'
+		,'Minimo de rango para comision COD Anticipado rango 301 - 600'
+		,'38'
+		,1
+		,GETDATE()
+		,'SV'
+		,NULL)
+
+	--SCRIPT PARA INSERTAR EL VALOR DE MaxRangeCODComisison1Param DE SV EN ConfigParams
+	INSERT INTO [dbo].[ConfigParams]
+		([Name]
+		,[Description]
+		,[Value]
+		,[Status]
+		,[CreateDate]
+		,[IdCountry]
+		,[IdCurrencyCOD])
+	VALUES
+		('MaxRangeCODComisison1Param'
+		,'Maximo de rango para comision COD Anticipado rango 1 - 300'
+		,'300'
+		,1
+		,GETDATE()
+		,'SV'
+		,NULL)
+
+	--SCRIPT PARA INSERTAR EL VALOR DE MaxRangeCODComisison2Param DE SV EN ConfigParams
+	INSERT INTO [dbo].[ConfigParams]
+		([Name]
+		,[Description]
+		,[Value]
+		,[Status]
+		,[CreateDate]
+		,[IdCountry]
+		,[IdCurrencyCOD])
+	VALUES
+		('MaxRangeCODComisison2Param'
+		,'Maximo de rango para comision COD Anticipado rango 301 - 600'
+		,'75'
+		,1
+		,GETDATE()
+		,'SV'
+		,NULL)
+
+	--SCRIPT PARA INSERTAR EL VALOR DE ValueCODComisison1Param DE SV EN ConfigParams
+	INSERT INTO [dbo].[ConfigParams]
+		([Name]
+		,[Description]
+		,[Value]
+		,[Status]
+		,[CreateDate]
+		,[IdCountry]
+		,[IdCurrencyCOD])
+	VALUES
+		('ValueCODComisison1Param'
+		,'Valor de comision COD Anticipado rango 1 - 300'
+		,'1'
+		,1
+		,GETDATE()
+		,'SV'
+		,NULL)
+
+	--SCRIPT PARA INSERTAR EL VALOR DE ValueCODComisison2Param DE SV EN ConfigParams
+	INSERT INTO [dbo].[ConfigParams]
+		([Name]
+		,[Description]
+		,[Value]
+		,[Status]
+		,[CreateDate]
+		,[IdCountry]
+		,[IdCurrencyCOD])
+	VALUES
+		('ValueCODComisison2Param'
+		,'Valor de comision COD Anticipado rango 301 - 600'
+		,'1'
+		,1
+		,GETDATE()
+		,'SV'
+		,NULL)
+
+	--SCRIPT PARA INSERTAR EL VALOR DE ValueCODComisison3Param DE SV EN ConfigParams
+	INSERT INTO [dbo].[ConfigParams]
+		([Name]
+		,[Description]
+		,[Value]
+		,[Status]
+		,[CreateDate]
+		,[IdCountry]
+		,[IdCurrencyCOD])
+	VALUES
+		('ValueCODComisison3Param'
+		,'Valor de comision COD Anticipado rango 601 - 800'
+		,'1'
+		,1
+		,GETDATE()
+		,'SV'
+		,NULL)
+
+	--SCRIPT PARA INSERTAR EL VALOR DE GuideAmountCODAnticipatedParam DE SV EN ConfigParams
+	INSERT INTO [dbo].[ConfigParams]
+		([Name]
+		,[Description]
+		,[Value]
+		,[Status]
+		,[CreateDate]
+		,[IdCountry]
+		,[IdCurrencyCOD])
+	VALUES
+		('GuideAmountCODAnticipatedParam'
+		,'Valor maximo por guia para validacion general de COD Anticipado'
+		,'100'
+		,1
+		,GETDATE()
+		,'SV'
+		,NULL)
+
 	COMMIT TRANSACTION;
 END TRY
 BEGIN CATCH
