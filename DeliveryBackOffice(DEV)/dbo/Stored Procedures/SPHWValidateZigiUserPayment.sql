262cb7554890b84dcec8df3bdf6772292f2f9218
brandon-p <brandon.pedroza@forzadelivery.com>
2025-04-11 21:18:01 -0600

    FDAPI-3778: # se agrega script para envio de link pago zigi y actualizacion de estado de link

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHWValidateZigiUserPayment.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHWValidateZigiUserPayment.sql
@@ -9,53 +9,80 @@ CREATE PROCEDURE [dbo].[SPHWValidateZigiUserPayment]
 AS
 BEGIN
 	--verificar guia collect
-	IF NOT EXISTS (SELECT 1 FROM DeliveryOrder WHERE Guide_Number = @GuideNumber AND Guide_Serie = @GuideSerie AND IsCollect = 1 )
+	IF NOT EXISTS (SELECT 1 FROM DeliveryOrder WITH(NOLOCK) WHERE Guide_Number = @GuideNumber AND Guide_Serie = @GuideSerie AND IsCollect = 1 )
 	BEGIN
 		SELECT 400 [IdResult],
 			'la guia no es collect' AS [Message],
 			0				AS [PaidZigi],
 			@GuideNumber	AS GuideNumber,
-			@GuideSerie		AS GuideSerie;
+			@GuideSerie		AS GuideSerie,
+			0				AS [LinkCreated],
+			0				AS [IsPay],
+			''				AS [LinkZigi];
 		RETURN;
 	END
 	
-	IF NOT EXISTS (SELECT 1 FROM PaymentZigi WHERE GuideNumber = @GuideNumber AND GuideSerie = @GuideSerie)
+	IF NOT EXISTS (SELECT 1 FROM PaymentZigi WITH(NOLOCK) WHERE GuideNumber = @GuideNumber AND GuideSerie = @GuideSerie)
 	BEGIN
 		SELECT 200 [IdResult],
 			'Guia no tiene link asociado ' AS [Message],
 			1				AS [PaidZigi],
 			@GuideNumber	AS GuideNumber,
-			@GuideSerie		AS GuideSerie;
+			@GuideSerie		AS GuideSerie,
+			0				AS [LinkCreated],
+			0				AS [IsPay],
+			''				AS [LinkZigi];
 		RETURN;
 	END
 
-	IF EXISTS (SELECT 1 FROM PaymentZigi WHERE GuideNumber = @GuideNumber AND GuideSerie = @GuideSerie AND ZigiLinkStatus = 'CREATED')
+	IF EXISTS (SELECT 1 FROM PaymentZigi WITH(NOLOCK) WHERE GuideNumber = @GuideNumber AND GuideSerie = @GuideSerie AND ZigiLinkStatus = 'CREATED')
 	BEGIN
 		SELECT 200 [IdResult],
 			'Guia link creado' AS [Message],
 			1				AS [PaidZigi],
 			@GuideNumber	AS GuideNumber,
-			@GuideSerie		AS GuideSerie;
+			@GuideSerie		AS GuideSerie,
+			1				AS [LinkCreated],
+			0				AS [IsPay],
+			ZigiLink		AS [LinkZigi]
+		FROM PaymentZigi WITH(NOLOCK)
+		WHERE GuideNumber = @GuideNumber 
+		AND GuideSerie = @GuideSerie 
+		AND ZigiLinkStatus = 'CREATED';
 		RETURN;
 	END
 
-	IF EXISTS (SELECT 1 FROM PaymentZigi WHERE GuideNumber = @GuideNumber AND GuideSerie = @GuideSerie AND ZigiLinkStatus = 'PAID')
+	IF EXISTS (SELECT 1 FROM PaymentZigi WITH(NOLOCK) WHERE GuideNumber = @GuideNumber AND GuideSerie = @GuideSerie AND ZigiLinkStatus = 'PAID')
 	BEGIN
 		SELECT 400 AS [IdResult],
 			'Link de Guia ha sido pagado'	AS [Message],
 			0				AS [PaidZigi],
 			@GuideNumber	AS GuideNumber,
-			@GuideSerie		AS GuideSerie;
+			@GuideSerie		AS GuideSerie,
+			1				AS [LinkCreated],
+			1				AS [IsPay],
+			ZigiLink		AS [LinkZigi]
+		FROM PaymentZigi WITH(NOLOCK) 
+		WHERE GuideNumber = @GuideNumber 
+		AND GuideSerie = @GuideSerie 
+		AND ZigiLinkStatus = 'PAID'
 		RETURN;
 	END
 
-	IF EXISTS (SELECT 1 FROM PaymentZigi WHERE GuideNumber = @GuideNumber AND GuideSerie = @GuideSerie AND ZigiLinkStatus = 'CANCELED')
+	IF EXISTS (SELECT 1 FROM PaymentZigi WITH(NOLOCK) WHERE GuideNumber = @GuideNumber AND GuideSerie = @GuideSerie AND ZigiLinkStatus = 'CANCELED')
 	BEGIN
 		SELECT 1 AS [IdResult],
 			'Link HA SIDO DESHABILITADO'	AS [Message],
 			0				AS [PaidZigi],
 			@GuideNumber	AS GuideNumber,
-			@GuideSerie		AS GuideSerie;
+			@GuideSerie		AS GuideSerie,
+			1				AS [LinkCreated],
+			0				AS [IsPay],
+			ZigiLink		AS [LinkZigi]
+			FROM PaymentZigi WITH(NOLOCK) 
+			WHERE GuideNumber = @GuideNumber 
+			AND GuideSerie = @GuideSerie 
+			AND ZigiLinkStatus = 'CANCELED'
 		RETURN;
 	END
 	ELSE
@@ -64,7 +91,10 @@ BEGIN
 			'ERROR'	AS [Message],
 			0				AS [PaidZigi],
 			@GuideNumber	AS GuideNumber,
-			@GuideSerie		AS GuideSerie;
+			@GuideSerie		AS GuideSerie,
+			0				AS [LinkCreated],
+			0				AS [IsPay],
+			''				AS [LinkZigi]
 		RETURN;
 	END
 END
