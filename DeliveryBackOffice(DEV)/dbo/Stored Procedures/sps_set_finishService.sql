91bcc0467886ac663db97c967b021d122a443c1d
JuanRamirezFD <juan.ramirez@forzadelivery.com>
2025-04-23 09:20:43 -0600

    FDAPI-3837: Cambio de llave primaria por indice

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_set_finishService.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sps_set_finishService.sql
@@ -134,8 +134,14 @@ BEGIN
         (
            IdCustomer INT NOT NULL,
            PortfolioId INT NOT NULL,
-           CONSTRAINT PK_TempDataClient PRIMARY KEY (IdCustomer, PortfolioId)
+           --CONSTRAINT PK_TempDataClient PRIMARY KEY (IdCustomer, PortfolioId)
         );
+
+        CREATE NONCLUSTERED INDEX IDX_PK_TempDataClient
+        ON #TempDataClient (
+                             IdCustomer
+                           , PortfolioId
+                           );
 
         CREATE TABLE #TempData
         (
