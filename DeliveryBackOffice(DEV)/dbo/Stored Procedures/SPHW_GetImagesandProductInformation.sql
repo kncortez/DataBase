8ae6acf4a8fa4931358248bc435f95b36f784221
Azurdia <cristian.azurdia@forzadelivery.com>
2025-05-06 12:14:13 -0600

    FDAPI-3805 Correción de moneda al cargar elementos de MarketPlace (membresias y/o suscripciones)

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetImagesandProductInformation.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_GetImagesandProductInformation.sql
@@ -4,19 +4,22 @@
 -- Create date: <Create Date,2023-30-11>
 -- Description:	<Description,carga de imagenes e información de un producto>
 -- =============================================
--- =============================================
 -- Author:		<Cristian Suazo>
 -- Create date: <Create Date,2024-07-25>
 -- Description:	<DSe agrega la moneda correspondiente al pais>
 -- =============================================
+-- Author:		<Cristian Azurdia>
+-- Create date: <Create Date,2025-05-06>
+-- Description:	<Manejo de moneda multipais>
+-- =============================================
+
 CREATE PROCEDURE [SPHW_GetImagesandProductInformation]
-@IdCatProduct INT, 
-@ProductName NVARCHAR(300)
+	@IdCatProduct INT, 
+	@ProductName NVARCHAR(300)
 AS
 BEGIN
 
-
-IF (EXISTS( Select Top 1 1
+    IF (EXISTS( Select Top 1 1
 	 From [dbo].[CatSubscription] CP WITH (NOLOCK)
 	WHERE CP.[IdCatSubscription] = @IdCatProduct AND CP.RowStatus=1)
 	OR 
@@ -31,113 +34,120 @@ IF (EXISTS( Select Top 1 1
 	END 
 	ELSE
 	BEGIN
+
 	SELECT 201 [StatusCode], 'Sin Registros' [Description]
+
 	END
-	Select  
-	        CP.IdCatSubscription   [IdCatProduct],
+
+	Select  CP.IdCatSubscription   [IdCatProduct],
 			CP.SubscriptionName [CatProductName],
 			CP.SubscriptionDescription   [CatProductDescription],
 			CP.SubscriptionCost  [CatProductCost],
-			CASE WHEN ISNULL(CP.IdCountry,'GT') = 'GT' THEN 'Q.' ELSE 'L.' END AS CurrencySymbol,
+			ccc.Symbol [CurrencySymbol],
 			CP.SubscriptionFixedValue [CatProductDiscountValue],
 			CP.[CatProductCategoryId],
 			CP.Tag,
 			CAST(CP.SubscriptionValidity AS nvarchar) +' '+ 'Meses' [Validity]
-From DBO.[CatSubscription] CP WITH(NOLOCK) 
-WHERE CP.IdCatSubscription = @IdCatProduct
-      AND   CP.RowStatus=1 AND CP.SubscriptionName = @ProductName
-UNION ALL 
-
-Select  
-	        CP.IdCatMembership   [IdCatProduct],
-			CP.MembershipName [CatProductName],
-			CP.MembershipDescription   [CatProductDescription],
-			CP.MembershipCost  [CatProductCost],
-			CASE WHEN ISNULL(CP.IdCountry,'GT') = 'GT' THEN 'Q.' ELSE 'L.' END AS CurrencySymbol,
-			CP.MembershipFixedValue [CatProductDiscountValue],
-			CP.[CatProductCategoryId],
-			CP.Tag,
-			CAST(CP.MembershipValidity AS nvarchar) +' '+ 'Meses' [Validity]
-From [dbo].[CatMembership] CP WITH(NOLOCK) 
-WHERE CP.IdCatMembership = @IdCatProduct
-      AND   CP.RowStatus=1 AND CP.MembershipName  = @ProductName
-
-
- SELECT 
-			CPI.[CatProductImageSmallImageURL],
-			CPI.[CatProductImageLargeImageURL],
-			ISNULL(CPI.CatSubscriptionId,CPI.CatMembershipId) [CatProductId],
-			CPI.[CatProductImageBigImageURL]
-	FROM DeliveryBackOffice.dbo.CatProductImage  CPI WITH(NOLOCK)
-	  LEFT JOIN DeliveryBackOffice.dbo.CatMembership CM WITH(NOLOCK)
-	  ON CPI.CatMembershipId = CM.IdCatMembership 
-	WHERE CPI.RowStatus = 1 
-	 AND CM.MembershipName = @ProductName
-	  AND CM.IdCatMembership = @IdCatProduct
-	UNION ALL
-	SELECT 
-			CPI.[CatProductImageSmallImageURL],
-			CPI.[CatProductImageLargeImageURL],
-			ISNULL(CPI.CatSubscriptionId,CPI.CatMembershipId) [CatProductId],
-			CPI.[CatProductImageBigImageURL]
-	FROM DeliveryBackOffice.dbo.CatProductImage  CPI WITH(NOLOCK)
-	    LEFT JOIN DeliveryBackOffice.dbo.CatSubscription CS WITH(NOLOCK)
-	  ON CS.IdCatSubscription = CPI.CatSubscriptionId
-	WHERE CPI.RowStatus = 1 
-	AND CS.SubscriptionName = @ProductName
-		 AND CS.IdCatSubscription = @IdCatProduct
-
-select 
-       CPA.SubscriptionAttributeDescription [CatProductAttributeDescription],
-	   ISNULL(CPA.SubscriptionAttributeDescriptionLong,'N/D') [CatProductAttributeDescriptionLong],
-	   CPA.CatSubscriptionAttributeIcon   [CatProductAttributeIcon],
-	   CPA.SubscriptionAttributePosition [CatProductAtributeOrder],
-	   CPA.CatSubscriptionId [CatProductId]
-From [dbo].[CatSubscriptionAtribute] CPA WITH(NOLOCK)
-      LEFT JOIN [dbo].[CatSubscription] CS WITH(NOLOCK)
-	  ON CPA.CatSubscriptionId = CS.IdCatSubscription
-WHERE CPA.CatSubscriptionId = @IdCatProduct
-       AND   CPA.RowStatus=1
-	   AND CS.SubscriptionName = @ProductName
-	   UNION ALL
-	   select 
-       CPA.MembershipAttributeDescription [CatProductAttributeDescription],
-	   ISNULL(CPA.MembershipAttributeDescriptionLong,'N/D') [CatProductAttributeDescriptionLong],
-	   CPA. CatMembershipAttributeIcon   [CatProductAttributeIcon],
-	   CPA.MembershipAttributePosition [CatProductAtributeOrder],
-	   CPA.CatMembershipId [CatProductId]
-From [dbo].[CatMembershipAttribute] CPA WITH(NOLOCK)
-      LEFT JOIN [dbo].[CatMembership] CS WITH(NOLOCK)
-	  ON CPA.CatMembershipId = CS.IdCatMembership
-WHERE CPA.CatMembershipId = @IdCatProduct
-       AND   CPA.RowStatus=1
-	   AND CS.MembershipName = @ProductName
-ORDER BY CPA.SubscriptionAttributePosition ASC
-
-					
-select  CPD.Title [CatProductDescriptionTitle],
-		CPD.[Description] [CatProductDescription],
-		CPD.Position [CatProductDescriptionOrder],
-		CPD.CatSubscriptionId [CatProductId]
-From CatSubscriptionDescription CPD WITH(NOLOCK)
-   INNER JOIN CatSubscription CS WITH(NOLOCK)
-   ON CPD.CatSubscriptionId = CS.IdCatSubscription
-Where CPD.CatSubscriptionId = @IdCatProduct
- AND   CPD.RowStatus=1
-    AND CS.SubscriptionName = @ProductName 
- UNION ALL
- select  CPD.Title [CatProductDescriptionTitle],
-		CPD.[Description] [CatProductDescription],
-		CPD.Position [CatProductDescriptionOrder],
-		CPD.IdCatMembershipDescription [CatProductId]
-From CatMembershipDescription CPD WITH(NOLOCK)
-INNER JOIN CatMembership CM WITH(NOLOCK)
-ON CPD.CatMembershipId = CM.IdCatMembership
-Where CPD.CatMembershipId = @IdCatProduct
- AND   CPD.RowStatus=1
- AND CM.MembershipName = CM.MembershipName
+	From DBO.[CatSubscription] CP WITH(NOLOCK)
+	LEFT JOIN DeliveryCurrency dc WITH(NOLOCK)
+		ON dc.Currency_IdCountry = CP.IdCountry
+		AND dc.DefaultPerCountry = 1
+	LEFT JOIN CatCurrencyCOD ccc  WITH(NOLOCK)
+		ON ccc.IdCatCurrencyCOD = dc.IdCurrencyCOD
+	WHERE CP.IdCatSubscription = @IdCatProduct
+		AND   CP.RowStatus=1 
+		AND CP.SubscriptionName = @ProductName
+	UNION ALL 
+	Select      CP.IdCatMembership   [IdCatProduct],
+				CP.MembershipName [CatProductName],
+				CP.MembershipDescription   [CatProductDescription],
+				CP.MembershipCost  [CatProductCost],
+				ccc.Symbol [CurrencySymbol],
+				CP.MembershipFixedValue [CatProductDiscountValue],
+				CP.[CatProductCategoryId],
+				CP.Tag,
+				CAST(CP.MembershipValidity AS nvarchar) +' '+ 'Meses' [Validity]
+	From [dbo].[CatMembership] CP WITH(NOLOCK)
+	LEFT JOIN DeliveryCurrency dc WITH(NOLOCK)
+		ON dc.Currency_IdCountry = CP.IdCountry
+		AND dc.DefaultPerCountry = 1
+	LEFT JOIN CatCurrencyCOD ccc  WITH(NOLOCK)
+		ON ccc.IdCatCurrencyCOD = dc.IdCurrencyCOD
+	WHERE CP.IdCatMembership = @IdCatProduct
+			AND   CP.RowStatus=1 AND CP.MembershipName  = @ProductName
 
 
+		SELECT 
+				CPI.[CatProductImageSmallImageURL],
+				CPI.[CatProductImageLargeImageURL],
+				ISNULL(CPI.CatSubscriptionId,CPI.CatMembershipId) [CatProductId],
+				CPI.[CatProductImageBigImageURL]
+		FROM DeliveryBackOffice.dbo.CatProductImage  CPI WITH(NOLOCK)
+			LEFT JOIN DeliveryBackOffice.dbo.CatMembership CM WITH(NOLOCK)
+			ON CPI.CatMembershipId = CM.IdCatMembership 
+		WHERE CPI.RowStatus = 1 
+			AND CM.MembershipName = @ProductName
+			AND CM.IdCatMembership = @IdCatProduct
+		UNION ALL
+		SELECT 
+				CPI.[CatProductImageSmallImageURL],
+				CPI.[CatProductImageLargeImageURL],
+				ISNULL(CPI.CatSubscriptionId,CPI.CatMembershipId) [CatProductId],
+				CPI.[CatProductImageBigImageURL]
+		FROM DeliveryBackOffice.dbo.CatProductImage  CPI WITH(NOLOCK)
+			LEFT JOIN DeliveryBackOffice.dbo.CatSubscription CS WITH(NOLOCK)
+			ON CS.IdCatSubscription = CPI.CatSubscriptionId
+		WHERE CPI.RowStatus = 1 
+		AND CS.SubscriptionName = @ProductName
+				AND CS.IdCatSubscription = @IdCatProduct
 
+	select 
+			CPA.SubscriptionAttributeDescription [CatProductAttributeDescription],
+			ISNULL(CPA.SubscriptionAttributeDescriptionLong,'N/D') [CatProductAttributeDescriptionLong],
+			CPA.CatSubscriptionAttributeIcon   [CatProductAttributeIcon],
+			CPA.SubscriptionAttributePosition [CatProductAtributeOrder],
+			CPA.CatSubscriptionId [CatProductId]
+	From [dbo].[CatSubscriptionAtribute] CPA WITH(NOLOCK)
+			LEFT JOIN [dbo].[CatSubscription] CS WITH(NOLOCK)
+			ON CPA.CatSubscriptionId = CS.IdCatSubscription
+	WHERE CPA.CatSubscriptionId = @IdCatProduct
+			AND   CPA.RowStatus=1
+			AND CS.SubscriptionName = @ProductName
+			UNION ALL
+			select 
+			CPA.MembershipAttributeDescription [CatProductAttributeDescription],
+			ISNULL(CPA.MembershipAttributeDescriptionLong,'N/D') [CatProductAttributeDescriptionLong],
+			CPA. CatMembershipAttributeIcon   [CatProductAttributeIcon],
+			CPA.MembershipAttributePosition [CatProductAtributeOrder],
+			CPA.CatMembershipId [CatProductId]
+	From [dbo].[CatMembershipAttribute] CPA WITH(NOLOCK)
+			LEFT JOIN [dbo].[CatMembership] CS WITH(NOLOCK)
+			ON CPA.CatMembershipId = CS.IdCatMembership
+	WHERE CPA.CatMembershipId = @IdCatProduct
+			AND   CPA.RowStatus=1
+			AND CS.MembershipName = @ProductName
+	ORDER BY CPA.SubscriptionAttributePosition ASC
+
+	select  CPD.Title [CatProductDescriptionTitle],
+			CPD.[Description] [CatProductDescription],
+			CPD.Position [CatProductDescriptionOrder],
+			CPD.CatSubscriptionId [CatProductId]
+	From CatSubscriptionDescription CPD WITH(NOLOCK)
+		INNER JOIN CatSubscription CS WITH(NOLOCK)
+		ON CPD.CatSubscriptionId = CS.IdCatSubscription
+	Where CPD.CatSubscriptionId = @IdCatProduct
+		AND   CPD.RowStatus=1
+		AND CS.SubscriptionName = @ProductName 
+		UNION ALL
+		select  CPD.Title [CatProductDescriptionTitle],
+			CPD.[Description] [CatProductDescription],
+			CPD.Position [CatProductDescriptionOrder],
+			CPD.IdCatMembershipDescription [CatProductId]
+	From CatMembershipDescription CPD WITH(NOLOCK)
+	INNER JOIN CatMembership CM WITH(NOLOCK)
+	ON CPD.CatMembershipId = CM.IdCatMembership
+	Where CPD.CatMembershipId = @IdCatProduct
+		AND   CPD.RowStatus=1
+		AND CM.MembershipName = CM.MembershipName
 
 END
