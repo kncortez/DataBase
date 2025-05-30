b8d23a9d0bea63a0fe27563fb4fbcdef28411621
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2025-05-30 11:21:47 -0600

    FDD-1459 Se agregan validaciones a la referencia y se manejan mensajes de error

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphd_getGuidesFromReferenceNumber.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphd_getGuidesFromReferenceNumber.sql
@@ -5,9 +5,9 @@
 -- =============================================
 -- Author:		<Tito García>
 -- Updated date: <2025-01-08>
--- Description:	<Se agrega filtro para obtner las guias que no estan en estados terminales>
+-- Description:	<Se agrega filtro para obtner las guias que no estan en estados terminales y se valida la referencia si existe en el país>
 -- =============================================
-create PROCEDURE [dbo].[sphd_getGuidesFromReferenceNumber]
+CREATE PROCEDURE [dbo].[sphd_getGuidesFromReferenceNumber]
 	@ReferenceNumber NVARCHAR(150),
 	@IdCountry VARCHAR(2) = 'GT'
 AS
@@ -15,22 +15,55 @@ BEGIN
 	SET NOCOUNT ON;
 	
 	BEGIN TRY
-	
-		SELECT TOP 10 CONCAT(Guide_Serie, Guide_Number) AS Guide
-		FROM [dbo].[DeliveryOrder] do WITH (NOLOCK)
-            INNER JOIN [dbo].[StatusOrder] [so] WITH (NOLOCK)
-                ON [do].[StatusOrderId] = [so].[StatusOrderId]
-		WHERE so.CatCheckpointTypeId <> 3
-			AND so.RowStatus = 1
-			AND do.Ticket_Number = @ReferenceNumber
-			AND do.SenderCountryId = @IdCountry
-		ORDER BY do.DateCreated DESC
+
+        -- Verificamos si existen guías en el país solicitado
+        IF EXISTS (
+            SELECT 1
+            FROM [dbo].[DeliveryOrder] DO WITH (NOLOCK)
+            WHERE DO.Ticket_Number = @ReferenceNumber
+              AND DO.SenderCountryId = @IdCountry
+              AND ISNULL(DO.GuideType, 'DOM') = 'DOM'
+        )
+        BEGIN
+		    SELECT 1 AS StatusCode,
+                   'Exito' AS Description;
+
+            SELECT TOP 10 
+                CONCAT(Guide_Serie, Guide_Number) AS Guide
+            FROM [dbo].[DeliveryOrder] DO WITH (NOLOCK)
+                INNER JOIN [dbo].[StatusOrder] SO WITH (NOLOCK)
+                    ON DO.StatusOrderId = SO.StatusOrderId
+            WHERE SO.CatCheckpointTypeId <> 3
+              AND SO.RowStatus = 1
+              AND DO.Ticket_Number = @ReferenceNumber
+              AND DO.SenderCountryId = @IdCountry
+            ORDER BY DO.DateCreated DESC;
+
+        END
+        ELSE IF EXISTS (
+            SELECT 1
+            FROM [dbo].[DeliveryOrder] DO WITH (NOLOCK)
+            WHERE DO.Ticket_Number = @ReferenceNumber
+              AND DO.SenderCountryId <> @IdCountry
+              AND ISNULL(DO.GuideType, 'DOM') = 'DOM'
+        )
+        BEGIN
+
+            SELECT 2 AS StatusCode,
+                   'La referencia que intentas procesar pertenece a otro país' AS Description;
+        END
+        ELSE
+        BEGIN
+
+            SELECT 3 AS StatusCode,
+                   'No se encontraron guías con la referencia proporcionada' AS Description;
+        END
 
     END TRY 
 	BEGIN CATCH
 
         SELECT 0 AS 'StatusCode', 
-                ERROR_MESSAGE() AS 'Description' 
+                'No se pudieron procesar los datos, por favor intente de nuevo o comuníquese con el administrador del sistema. \n' + ERROR_MESSAGE() AS 'Description' 
 	
 	END CATCH
 END
