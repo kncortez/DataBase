4ea8e9a291af8412f54e2ac5e90e1b149af50f93
Azurdia <cristian.azurdia@forzadelivery.com>
2025-06-05 15:25:55 -0600

    FDAPI-4017 Generación de tablas para manejo de token de autentificación así como de configuración en del_ParametrosFActura de usuario y contraseña para obtención del token

---
+++ DeliveryBackOffice(DEV)/dbo/Tables/InvoiceAuthorizationHeader.sql
@@ -0,0 +1,50 @@
+CREATE TABLE [dbo].[InvoiceAuthorizationHeader](
+    [IdInvoiceAuthorizationHeader]        [int] IDENTITY(1,1) NOT NULL,
+    [Authorization]                 [nvarchar](512) NOT NULL,
+    [StartDate]                     [date] NOT NULL,
+    [EndDate]                       [date] NOT NULL,
+    [RowStatus]                     [bit] NOT NULL,
+    [TokenCreated]                  [nvarchar](100) NOT NULL,
+    [DateCreated]                   [datetime] NOT NULL,
+    [TokenUpdated]                  [nvarchar](100) NULL,
+    [DateUpdated]                   [datetime] NULL,
+    CONSTRAINT [PK_InvoiceAuthorizationHeader] PRIMARY KEY CLUSTERED ([IdInvoiceAuthorizationHeader] ASC)
+);
+GO
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Llave primaria de la tabla', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'InvoiceAuthorizationHeader', @level2type = N'COLUMN', @level2name = N'IdInvoiceAuthorizationHeader';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Token JWT generado por Digifact para autenticar la transacciones realizadas, tiene 30 días duración', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'InvoiceAuthorizationHeader', @level2type = N'COLUMN', @level2name = N'Authorization';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha de generación del token', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'InvoiceAuthorizationHeader', @level2type = N'COLUMN', @level2name = N'StartDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha de vencimiento del token', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'InvoiceAuthorizationHeader', @level2type = N'COLUMN', @level2name = N'EndDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Estado de la fila, TRUE o FALSE.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'InvoiceAuthorizationHeader', @level2type = N'COLUMN', @level2name = N'RowStatus';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Token que creó la fila.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'InvoiceAuthorizationHeader', @level2type = N'COLUMN', @level2name = N'TokenCreated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha y hora en la que se creo la fila.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'InvoiceAuthorizationHeader', @level2type = N'COLUMN', @level2name = N'DateCreated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Token que modificó la fila.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'InvoiceAuthorizationHeader', @level2type = N'COLUMN', @level2name = N'TokenUpdated';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha y hora en la que se creo la fila.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'InvoiceAuthorizationHeader', @level2type = N'COLUMN', @level2name = N'DateUpdated';
+
+
