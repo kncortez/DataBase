a570a6ecc57a2045d7801c769fd20f9f3c0b6b09
Azurdia <cristian.azurdia@forzadelivery.com>
2025-06-11 12:16:24 -0600

    FDAPI-4023 Correción en el flujo de entrega positva así como actualización de estado para renovación de Token

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/ValidateAuthorizationInvoice.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/ValidateAuthorizationInvoice.sql
@@ -48,13 +48,33 @@ BEGIN
                    AND iar.[RowStatus] = 1
                    AND (
                         ISNULL(iah.[Authorization],'') = ''
-                        OR ISNULL(iah.[DateCreated], '') = ''
+                   )
+               )
+     BEGIN
+          SELECT @Code = 2,
+                 @Message = 'No existe autorización para el codeOfReference'
+
+          SELECT @Code AS code,
+                 @Message AS [Message];
+          RETURN;
+     END
+
+     IF EXISTS (
+                SELECT TOP 1 1
+                  FROM InvoiceAuthorizationHeader iah WITH(NOLOCK)
+                       INNER JOIN InvoiceAuthorizationRelationships iar WITH(NOLOCK)
+                         ON iah.IdInvoiceAuthorizationHeader = iar.InvoiceAuthorizationHeaderId
+                 WHERE iar.CodeOfReference = @CodeOfReference
+                   AND iah.[RowStatus] = 1
+                   AND iar.[RowStatus] = 1
+                   AND (
+                        ISNULL(iah.[DateCreated], '') = ''
                         OR ISNULL(iah.[EndDate],'') = ''
                    )
                )
      BEGIN
           SELECT @Code = 0,
-                 @Message = 'La autorización tiene campo pendientes de configurar'
+                 @Message = 'La autorización tiene pendientes las fechas por configurar'
 
           SELECT @Code AS code,
                  @Message AS [Message];
@@ -72,13 +92,19 @@ BEGIN
                        AND GETDATE() <= iah.EndDate
                    )
      BEGIN
-          SELECT @Code = 0,
-                 @Message = 'La fecha actual excede la fecha limite de facturación para la autorización'
+          SELECT @Code = 2,
+                 @Message = 'La fecha actual excede la fecha límite de facturación para la autorización'
 
           SELECT @Code AS code,
                  @Message AS [Message];
 
           RETURN;
      END
+
+     SELECT @Code = 1,
+            @Message = 'Si existe Autorización Activa y Válida'
+
+     SELECT @Code AS code,
+            @Message AS [Message];
 
 END;
