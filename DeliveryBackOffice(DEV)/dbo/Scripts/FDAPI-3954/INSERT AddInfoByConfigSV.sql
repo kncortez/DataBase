00e40e4748bd5c6959c6de52b23bfd3544593428
JuanRamirezFD <juan.ramirez@forzadelivery.com>
2025-06-11 15:12:08 -0600

    FDAPI-4038: Cambios para construir el builder y generar json para facturar.

---
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3954/INSERT AddInfoByConfigSV.sql
@@ -0,0 +1,33 @@
+-- Insertar registros de información adicional para CodeOfReference 1162393 - Seller
+BEGIN TRY
+    BEGIN TRANSACTION;
+    
+    USE DeliveryBackOffice;
+
+    INSERT INTO AddInfoByConfigSV ([Node], [Name], [Data], [Value], RowStatus, DateCreated, TokenCreated)
+    VALUES 
+    ('Items', 'UnitOfMeasure'                                    , NULL, '99', 1, GETDATE(), 'JRAMIREZ-SYS'),
+    ('Items', 'AdditionalInfo'                                   , 'PrecioSugeridoVenta', NULL, 1, GETDATE(), 'JRAMIREZ-SYS'),
+    ('Header','AdditionalIssueType'                              , NULL, '00', 1, GETDATE(), 'JRAMIREZ-SYS'),
+    ('Totals', 'TotalTaxes.TotalTax'                             , '20','Impuesto al valor Agregado 13%', 1, GETDATE(), 'JRAMIREZ-SYS'),
+    ('Version', 'Version'                                        , NULL,'3', 1, GETDATE(), 'JRAMIREZ-SYS'),
+    ('Header.AdditionalIssueDocInfo', 'Secuencial'               , NULL,'000900030000045', 1, GETDATE(), 'JRAMIREZ-SYS'),
+    ('Header.AdditionalIssueDocInfo', 'CodEstPuntoV'             , NULL,'1234M010', 1, GETDATE(), 'JRAMIREZ-SYS'),
+    ('Header.AdditionalIssueDocInfo', 'TipoModelo'               , NULL,'1', 1, GETDATE(), 'JRAMIREZ-SYS'),
+    ('Header.AdditionalIssueDocInfo', 'TipoOperacion'            , NULL,'1', 1, GETDATE(), 'JRAMIREZ-SYS'),
+    ('Taxes.Tax', 'Code'                                         , NULL,'20', 1, GETDATE(), 'JRAMIREZ-SYS');
+
+    -- Consulta para verificar los datos
+    SELECT * FROM AddInfoByConfigSV;
+
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    ROLLBACK TRANSACTION;
+
+    -- Manejo de errores con PRINT
+    DECLARE @ErrorMessage NVARCHAR(4000);
+    SELECT @ErrorMessage = ERROR_MESSAGE();
+    
+    PRINT 'Error: ' + @ErrorMessage;
+END CATCH;
--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetInvoicePaymentDetailDeliveryCorp.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetInvoicePaymentDetailDeliveryCorp.sql
@@ -5,16 +5,18 @@
 -- =============================================
 CREATE PROCEDURE GetInvoicePaymentDetailDeliveryCorp
 (
-  @LstVisitPointClient NVARCHAR(MAX) = '',
-  @CutOffDate          DATETIME,
-  @IdCountry           NVARCHAR(2) = 'GT'
+ @LstVisitPointClient NVARCHAR(MAX) = '',
+ @CutOffDate          DATETIME,
+ @IdCountry           NVARCHAR(2) = 'GT',
+ @Option              TINYINT = 0
 )
 AS
 BEGIN
   --     Inicia el bloque de manejo de excepciones
     BEGIN TRY
         --Variables locales
-        DECLARE @XmlVisitPointClient XML; 
+        DECLARE @XmlVisitPointClient XML,
+                @TypeService         NVARCHAR(3);
 
         DECLARE @IdCatInvoiceType INT =
                 (
@@ -24,6 +26,12 @@ BEGIN
                   WHERE [Name] = 'Envío'
                     AND RowStatus = 1
                 );
+
+        SELECT @TypeService = CASE
+                                 WHEN @Option = 1 THEN 'STD'
+                                 WHEN @Option = 3 THEN 'COD'
+                                 ELSE 'STD'
+                              END
 
         -- Verificar si la tabla existe y eliminarla si es necesario
         IF OBJECT_ID('tempdb..#InvoiceByVisitPointDetails') IS NOT NULL
@@ -54,7 +62,7 @@ BEGIN
         DECLARE @TempVisitPointClient TblBillingGuideDetail;
 
         INSERT INTO @TempVisitPointClient (IdVisitPointClient, GuideSerie, GuideNumber)
-        SELECT vst.IdVisitPointClient,
+        SELECT TOP 3 vst.IdVisitPointClient,
                do.Guide_Serie,
                do.Guide_Number
           FROM DeliveryBackOffice.dbo.DeliveryOrder do WITH(NOLOCK)
@@ -75,9 +83,10 @@ BEGIN
                                            SELECT v.value('.', 'NVARCHAR(MAX)') AS Valor
                                              FROM @XmlVisitPointClient.nodes('/LstVisitPointClient/PointClient') AS x(v)
                                           )
-           AND CAST(do.Preparation_Date AS DATE) <= CAST(@CutOffDate AS DATE)
+           AND CAST(do.Preparation_Date AS DATE) >= CAST(@CutOffDate AS DATE)
            AND do.IsCollect = 0
            AND do.SenderCountryId = @IdCountry
+           AND do.TypeService = @TypeService
            AND EXISTS
                      (
                       SELECT TOP 1 1
@@ -121,7 +130,7 @@ BEGIN
         -- Deshace la transacción en caso de error
         IF @@TRANCOUNT > 0
             ROLLBACK TRANSACTION;
-
+            PRINT ERROR_MESSAGE()
             SELECT 0 AS StatusCode, 
                    'Ha ocurrido un error en el proceso' AS StatusMessage
     END CATCH
