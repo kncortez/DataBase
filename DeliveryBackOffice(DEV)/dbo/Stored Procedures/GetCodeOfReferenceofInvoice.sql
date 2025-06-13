356d35581d0e64edeaab992a3f837e11638a29ab
csuazo11 <cristian.suazo@forzadelivery.com>
2025-06-12 19:02:30 -0600

    FDAPI-4062: Se agrega nuewvo parametro

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetCodeOfReferenceofInvoice.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetCodeOfReferenceofInvoice.sql
@@ -4,12 +4,12 @@
 -- Create date: <2024-08-22>
 -- Description:	<TRAE EL CODIGO DE REFERENCIA REGISTRADO EN LA FACTURA>
 -- =============================================   
-CREATE PROCEDURE [dbo].[GetCodeOfReferenceofInvoice] @NumberFel NVARCHAR(50)
+CREATE PROCEDURE [dbo].[GetCodeOfReferenceofInvoice] @NumberFel NVARCHAR(50), @SerieFel NVARCHAR(50)
 AS
 BEGIN
     SELECT inv_vpCodeOfReferences AS CodeOfReference
          , inv_pk_id              AS IdInvoice
          , inv_numberFEL          AS CorrelativeInvoice
-    FROM invoiceHeader
-    WHERE inv_certificationFEL = @NumberFel;
+    FROM invoiceHeader WITH(NOLOCK)
+    WHERE inv_certificationFEL = @NumberFel AND inv_serieFEL = @SerieFel
 END;
