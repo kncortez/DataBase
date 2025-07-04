b971a085921239bd5307f1ce2e350f9d2fa0405b
brandon-p <brandon.pedroza@forzadelivery.com>
2025-07-04 16:54:47 -0600

    FDAPI-4173: #se crea sp para obtener parametros por pais

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetConfigParams.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetConfigParams.sql
@@ -3,13 +3,8 @@
 -- Create date: <19-10-2022>
 -- Description:	<Obtener valores de configParams>
 -- =============================================
--- Author:		<Brandon Pedroza>
--- Modified:	<02-07-2025>
--- Description:	<Se agrega parametro para filtrar por país>
--- =============================================
 CREATE PROCEDURE spHM_GetConfigParams 
-	@Name AS VARCHAR(50),
-	@IdCountry AS NVARCHAR(2) = 'GT'
+	@Name AS VARCHAR(50)
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -18,8 +13,7 @@ BEGIN
 			[CP].[Name],
 			[CP].[Description],
 			[CP].[Value]
-	FROM	[dbo].[ConfigParams] CP WITH(NOLOCK)
+	FROM	[dbo].[ConfigParams] CP
 	WHERE	[CP].[Name] = @Name
-		AND [CP].[Status] = 1
-		AND [CP].[IdCountry] = @IdCountry;
+		AND [CP].[Status] = 1;
 END
---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetConfigParamsByCountry.sql
@@ -0,0 +1,22 @@
+﻿
+-- =============================================
+-- Author:  <Brandon Pedroza>  
+-- Create date: <02-07-2025>  
+-- Description: <Se agrega parametro para filtrar por país>  
+-- =============================================  
+CREATE PROCEDURE [dbo].[spHM_GetConfigParamsByCountry]
+	@Name AS VARCHAR(50),
+	@IdCountry AS NVARCHAR(2) = 'GT'
+AS  
+BEGIN  
+ SET NOCOUNT ON;  
+  
+    SELECT [CP].[ConfigParamsId],  
+           [CP].[Name],  
+           [CP].[Description],  
+           [CP].[Value]  
+    FROM [dbo].[ConfigParams] CP  WITH(NOLOCK)
+    WHERE [CP].[Name] = @Name  
+      AND [CP].[Status] = 1  
+      AND ISNULL([CP].[IdCountry],'GT') = @IdCountry;
+END  
