8e88eb471922c310f38b60478ac8d3ba1d4b1d92
EdelmanV <edelman.vasquez@forzalatam.com>
2025-07-08 08:56:57 -0600

    #quitar log cuando no son usuarios Ultraentregas

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/RecordofGuidesCreatedWebhookTrackingQueueEU.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/RecordofGuidesCreatedWebhookTrackingQueueEU.sql
@@ -81,12 +81,7 @@ BEGIN
 				         
 						 SELECT 0 as 'Code' , 'Registro fallido' as 'Result'
 
-				      	 EXEC [dbo].[SetIntegrationForzaUELog]
-							@GuideSerie = @GuideSerie,
-							@GuideNumber = @GuideNumber,
-							@Description = 'Registro fallido, cliente no esta configurado para UltraEntregas',
-							@System = N'WebhookService',
-							@Token = N'SYS-HERMESWEBHOOKS'
+				      	 
 
 				   END
 
