7cfafe17f289564ac41f94ce09e9402282d3d775
brandon-p <brandon.pedroza@forzadelivery.com>
2025-07-30 17:00:12 -0600

    FDAPI-4159: #se agregan cambios para nota de credito en sv

---
+++ DeliveryBackOffice(DEV)/dbo/User Defined Types/TblBuyerInfo.sql
@@ -0,0 +1,11 @@
+﻿CREATE TYPE [dbo].[TblBuyerInfo] AS TABLE
+(
+	DistrictCode		varchar (100) NULL,
+	StateCode			varchar (100) NULL,
+	ActivityCode		varchar (100) NULL,
+	ActivityDescription	varchar (500) NULL,
+	NRC					varchar (20) NULL,
+	TypeDocument		varchar (100) NULL,
+	IdDocument			varchar (100) NULL,
+	Phone				varchar (100) NULL
+)
