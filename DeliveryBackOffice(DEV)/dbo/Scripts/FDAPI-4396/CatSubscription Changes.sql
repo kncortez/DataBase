33d7c3ad9cd2081affc673c2a2bd08f74fac7972
csuazo11 <cristian.suazo@forzadelivery.com>
2025-08-12 17:57:09 -0600

    FDAPI-4396: Se agregan los cambios de tienda virtual

---
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-4396/CatSubscription Changes.sql
@@ -0,0 +1,15 @@
+
+
+DECLARE @IdCountry NVARCHAR(3) = 'GT'
+
+DECLARE @CatSubcription INT = ( SELECT IdCatSubscription FROM CatSubscription WHERE SubscriptionName = 'Paquete Básico' AND IdCountry = @IdCountry AND RowStatus = 1)
+
+--ACTUALIZAMOS EL PRECIO
+UPDATE CatSubscription SET SubscriptionDescription = '50 guías a Q25.00 c/u.', SubscriptionCost = '1250.00' WHERE IdCatSubscription = @CatSubcription
+
+--ACTUALIZAMOS DECRIPCIONES
+
+UPDATE CatSubscriptionDescription SET Description = 'Compra en la tienda virtual y recibe las guías en tu correo electrónico.  Prepara tus paquetes, completa la información de envío y entrégalos en las  +100 agencias express center o puedes solicitar la recolección a tu casa u  oficina. Rastrea el progreso del envío con el número de guía proporcionado  para una experiencia sin complicaciones. Ahorra tiempo y esfuerzo al  centrarte en hacer crecer tu negocio mientras nosotros nos encargamos de  los detalles logísticos. Adquiere tu Paquete Básico y podrás obtener tus  guías prepagadas de 50 envíos con tarifa única a todo el país a Q25.00  c/u.' WHERE CatSubscriptionId = @CatSubcription AND RowStatus = 1 AND Title = '¿Cómo Funciona?'
+
+-- SE DA DE BAJA EL PAQUETE FLEXI
+UPDATE CatSubscription SET RowStatus = 0 WHERE SubscriptionName = 'Paquete FLEXI'
