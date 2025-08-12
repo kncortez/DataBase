ea5daa7feb560b51eff10a71a258e03806ba3b77
Azurdia <cristian.azurdia@forzadelivery.com>
2025-08-12 14:45:14 -0600

    FDAPI-4143 Correcciones solicitadas en el PR prt2

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3954/14 - ALTER CodeOfReferenceForInvoice.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3954/14 - ALTER CodeOfReferenceForInvoice.sql
@@ -2,9 +2,11 @@ BEGIN TRANSACTION
 
 BEGIN TRY
 --Script
-ALTER TABLE DefaultValuesPerCountry
-  ADD CodeOfReferenceCorpForInvoice NVARCHAR(10) NULL
+    ALTER TABLE DefaultValuesPerCountry
+    ADD CodeOfReferenceCorpForInvoice NVARCHAR(10) NULL
 
+    EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Code Of Reference que se usa por defecto para facturas corporativas.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'DefaultValuesPerCountry', @level2type = N'COLUMN', @level2name = N'CodeOfReferenceCorpForInvoice';
+    
 	COMMIT TRANSACTION
 	print 'Columna agregada exitosamente'
 
