8b6258b9d18fa75d045127b3d3ef1c8fff44fd97
EdelmanV <edelman.vasquez@forzalatam.com>
2025-08-12 19:17:28 -0600

    #Procedimiento para actualizar la transacción con doble validación de pasarela de pago PAyWayOne SV

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHW_UpdateCustomerTransactionPWO.sql
@@ -0,0 +1,87 @@
+
+-- =============================================
+-- Author:		<Author,Edelman>
+-- Create date: <Create Date,12/08/2025>
+-- Description:	<Description,Actualizar el estado de la transacción de pasarela de pago PayWayOne SV>
+-- =============================================
+ALTER PROCEDURE SPHW_UpdateCustomerTransactionPWO 
+    @Type AS INT = -1
+  , @System AS INT = 1
+  , @CardNumber AS NVARCHAR(50)
+  , @Signature AS NVARCHAR(100) = NULL
+  , @ReferenceNumber AS VARCHAR(50) = ''
+  , @TransactionStain AS VARCHAR(50) = ''
+  , @ReasonCode AS NVARCHAR(50) = NULL
+  , @ReasonDescription AS NVARCHAR(100) = NULL
+  , @StatusSend AS INT = NULL
+  , @TokenUpdated AS NVARCHAR(50) = NULL
+
+AS
+BEGIN
+
+        DECLARE @IdTransaction BIGINT = 0;
+		DECLARE @Code INT=0;
+		DECLARE @Description NVARCHAR(50)='ERROR';
+    -- Transacción para ingreso de proceso con tarjeta
+        BEGIN TRANSACTION LogTransactionTypeOne;
+        BEGIN TRY
+
+
+            -- Flujo normal de spws_set_facapidelcreditcardtransaction
+            SELECT @IdTransaction = ISNULL([IdTransaction], 0)
+            FROM DeliveryBackOffice.dbo.CreditCardTransactionByCustomer CCTBC WITH (NOLOCK)
+            WHERE [CCTBC].TransactionStain = @TransactionStain
+                  
+      
+             IF (@IdTransaction > 0)
+             BEGIN
+
+			      IF(@CardNumber<>NULL)
+				   BEGIN
+						UPDATE DeliveryBackOffice.dbo.CreditCardTransactionByCustomer
+						SET [System] = @System
+						  , CardNumber = @CardNumber
+						  , [Signature] = @Signature
+						  , ReferenceNumber = @ReferenceNumber
+						  , TransactionStain = @TransactionStain
+						  , ReasonCode = @ReasonCode
+						  , ReasonDescription = @ReasonDescription
+						  , StatusSend = @StatusSend
+						  , TokenUpdated = @TokenUpdated
+						  , DateUpdated = GETDATE()
+						WHERE IdTransaction = @IdTransaction
+							  AND TransactionStain = @TransactionStain
+					END
+					   ELSE
+					   BEGIN
+					        UPDATE DeliveryBackOffice.dbo.CreditCardTransactionByCustomer
+							SET 
+							    ReasonCode = @ReasonCode
+							  , ReasonDescription = @ReasonDescription
+							  , TokenUpdated = @TokenUpdated
+							  , DateUpdated = GETDATE()
+							WHERE IdTransaction = @IdTransaction
+								  AND TransactionStain = @TransactionStain
+					   END
+                      
+                 SET @Code = 1
+				 SET @Description = 'Success';
+
+            END
+		    
+			 SELECT @Code    AS   'Code',
+                    @Description AS [Description]
+
+            COMMIT TRANSACTION LogTransactionTypeOne;
+        END TRY
+        BEGIN CATCH
+            ROLLBACK TRANSACTION LogTransactionTypeOne;
+
+			SELECT 0    AS   'Code'
+             , 'Error' AS [Description]
+
+        END CATCH;
+
+END
+
+
