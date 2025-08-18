67dcef20492c8d679d08d6eab859a332698b41b3
Josue <vn595s5@mx.wal-mart.com>
2025-08-18 17:20:03 -0600

    Guide_Serie on Join

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_status_order_detail_wbs.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_status_order_detail_wbs.sql
@@ -215,7 +215,7 @@ DECLARE @DeliveryAttempt TABLE (
 			'' as Longitude,
 			CommentOnIncident
 		FROM @DeliveryOrderDetail DOD
-		LEFT JOIN @DeliveryAttempt DA ON DOD.Guide_Number = DA.Guide_Number
+		LEFT JOIN @DeliveryAttempt DA ON DOD.Guide_Number = DA.Guide_Number AND DOD.Guide_Serie = DA.Guide_Serie
 		) RES
 		ORDER BY RES.[EventID], RES.[StageDate] ASC	
 END
