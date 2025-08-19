f8cbfda255607cf4672a94c9871315df4a0de53d
brandon-p <brandon.pedroza@forzadelivery.com>
2025-08-18 20:30:31 -0600

    FDAPI-4159: #se realizan correcciones sugeridas por dba

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHDValidate.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHDValidate.sql
@@ -48,7 +48,7 @@ If (@Guide IS NOT NULL)
 Begin
 
 
-     Select Top 1 @dti_fk_header = dti_fk_header From [dbo].[invoiceDetail]
+     Select Top 1 @dti_fk_header = dti_fk_header From [dbo].[invoiceDetail] WITH(NOLOCK)
 								Where dti_fk_orderSerie + Cast(dti_fk_orderNumber as varchar)  = @Guide
 								
 
