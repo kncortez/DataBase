e7464f5649162d5fa9530dfc495b4ab342520551
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2025-08-20 18:13:31 -0600

    FDAPI-3975 Se agrega estructura de transacción y se valida si ya existe el registro para no insertar duplicados

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3975/Insert Param.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3975/Insert Param.sql
@@ -1,20 +1,51 @@
 USE [DeliveryBackOffice]
 GO
 
-INSERT INTO [dbo].[ConfigParams]
-           ([Name]
-           ,[Description]
-           ,[Value]
-           ,[Status]
-           ,[CreateDate]
-           ,[IdCountry]
-           ,[IdCurrencyCOD])
-     VALUES
-           ('BaseURL'
-           ,'Base para URL que muestra el comprobante de entrega escaneado'
-           ,'https://tracking.forzadelivery.com'
-           ,1
-           ,GETDATE()
-           ,NULL
-           ,NULL)
+BEGIN TRY
+    BEGIN TRANSACTION
+    
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM [dbo].[ConfigParams] 
+        WHERE [Name] = 'BaseURL'
+        AND [Value] = 'https://tracking.forzadelivery.com'
+    )
+    BEGIN
+        INSERT INTO [dbo].[ConfigParams]
+                   ([Name]
+                   ,[Description]
+                   ,[Value]
+                   ,[Status]
+                   ,[CreateDate]
+                   ,[IdCountry]
+                   ,[IdCurrencyCOD])
+             VALUES
+                   ('BaseURL'
+                   ,'Base para URL que muestra el comprobante de entrega escaneado'
+                   ,'https://tracking.forzadelivery.com'
+                   ,1
+                   ,GETDATE()
+                   ,NULL
+                   ,NULL)
+        
+        PRINT 'Registro insertado exitosamente.'
+    END
+    ELSE
+    BEGIN
+        PRINT 'El registro ya existe. No se realizó la inserción.'
+    END
+    
+    COMMIT TRANSACTION
+    
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION
+    
+    DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE()
+    DECLARE @ErrorSeverity INT = ERROR_SEVERITY()
+    DECLARE @ErrorState INT = ERROR_STATE()
+    
+    RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState)
+END CATCH
 GO
