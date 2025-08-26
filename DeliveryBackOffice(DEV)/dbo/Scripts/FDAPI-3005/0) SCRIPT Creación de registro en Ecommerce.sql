5f5e8aab512e773b849e76f99d3c702d52f2c924
aylinnerecinos <aylinne.recinos@forzadelivery.com>
2025-08-26 16:38:24 -0600

    Agrega script de creación de registro en Ecommerce

---
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-3005/0) SCRIPT Creación de registro en Ecommerce.sql
@@ -0,0 +1,19 @@
+INSERT INTO Ecommerce 
+(EcomerceName, 
+EcommerceDescription, 
+IsPaymentGateway, 
+IdCountry, 
+ApiWSEndPoint,
+ApiWSPort,
+ApiWSResource,
+ApiWSController,
+ApiWSMethod,
+UserKey,
+Passkey,
+SecretKey,
+CertSourceKey,
+EcommerceStatus,
+TokenCreated,
+DateCreated,
+IdCustomer)
+VALUES ('NABENIK','App de clientes',0,'GT','forza.systems','','','','','SICOMLAXAPIECOM104930237589','','n4IapUw4C49ehE+S6YCvJdpxvnh9XPScSLTCCNcE6epnri1ohPmBhSAkXv7DhJEm','',1,'ARECINOS',GETDATE(),84489)
