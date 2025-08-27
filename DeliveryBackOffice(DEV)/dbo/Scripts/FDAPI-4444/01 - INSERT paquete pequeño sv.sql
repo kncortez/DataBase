0c97f68bf622b52951278356f994604c4d7466d0
brandon-p <brandon.pedroza@forzadelivery.com>
2025-08-26 18:55:35 -0600

    FDAPI-4444: #se agregan script para nueva tarifa de El Salvador para portal web y exc

---
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-4444/01 - INSERT paquete pequeño sv.sql
@@ -0,0 +1,74 @@
+--SELECT * FROM DeliveryBackOffice.dbo.ArticleByCustomer
+--WHERE  AbcIdArticle = (SELECT ArtId FROM DeliveryBackOffice.dbo.CatArticle
+--WHERE  ArtName = 'Paquete pequeño' AND IdCountry IS NULL) --EJEMPLO DEVELOP
+
+DECLARE @IdArticle INT;
+DECLARE @IdArticleGT INT;
+DECLARE @IdCurrency INT;
+DECLARE @NewArticle INT;
+DECLARE @IdRate INT;
+
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+	IF NOT EXISTS(SELECT ArtId  FROM DeliveryBackOffice.dbo.CatArticle WITH(NOLOCK) WHERE ArtName = 'Paquete pequeño' AND  IdCountry = 'SV')
+	BEGIN
+		SELECT @IdArticle = ArtId  FROM DeliveryBackOffice.dbo.CatArticle WITH(NOLOCK)
+		WHERE ArtName = 'Paquete pequeño' AND  IdCountry = 'SV'
+	
+		SELECT @IdCurrency = IdCatCurrencyCOD FROM DeliveryBackOffice.dbo.CatCurrencyCOD WITH(NOLOCK)
+		WHERE [Name] = 'DOLAR ESTADOUNIDENSE'
+
+		--SCRIPT AGREGAR ARTICULO POR CLIENTE DE EN ArticleByCustomer Paquete pequeño
+		INSERT INTO [dbo].[ArticleByCustomer]
+			([AbcIdArticle]
+			,[AbcIdCustomer]
+			,[AbcRowStatus]
+			,[AbcTokenCreated]
+			,[AbcDateCreated]
+			,[AbcTokenUpdated]
+			,[AbcDateUpdated]
+			,[Code]
+			,[PriceDefault]
+			,[Height]
+			,[Width]
+			,[Length]
+			,[MassWeight]
+			,[VolumetricWeight]
+			,[ShowDefault]
+			,[IdCurrency])
+		VALUES
+			(@IdArticle
+			,NULL
+			,1
+			,''
+			,GETDATE()
+			,NULL
+			,NULL
+			,'EXPSV076'
+			,0.0
+			,28.00
+			,28.00
+			,28.00
+			,10.00
+			,NULL
+			,NULL
+			,@IdCurrency)
+
+		PRINT('Articulo pequeño insertado correctamente')
+	END
+	ELSE
+		BEGIN
+		PRINT('El registro ya existe')
+	END
+	COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    ROLLBACK TRANSACTION;
+
+    -- Manejo de errores con PRINT
+    DECLARE @ErrorMessage NVARCHAR(4000);
+    SELECT @ErrorMessage = ERROR_MESSAGE();
+    
+    PRINT 'Error: ' + @ErrorMessage;
+END CATCH;
