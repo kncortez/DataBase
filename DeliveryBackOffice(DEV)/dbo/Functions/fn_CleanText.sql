acaa96d4cd8f3715027766097b6f9a01c90e3430
csuazo11 <cristian.suazo@forzadelivery.com>
2025-08-29 10:03:58 -0600

    FDAPI-4353: Se agrega funcion para quitar caracteres especiales

---
+++ DeliveryBackOffice(DEV)/dbo/Functions/fn_CleanText.sql
@@ -0,0 +1,22 @@
+
+ALTER FUNCTION dbo.fn_CleanText(@Texto NVARCHAR(MAX))
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+    -- Solo caracteres permitidos
+    DECLARE @Resultado NVARCHAR(MAX) = @Texto;
+
+    -- Quitar caracteres no deseados
+    SET @Resultado = REPLACE(@Resultado, '/', '');
+    SET @Resultado = REPLACE(@Resultado, '"', '');
+    SET @Resultado = REPLACE(@Resultado, '\', '');
+    SET @Resultado = REPLACE(@Resultado, '''', '');
+	SET @Resultado = REPLACE(@Resultado, ',', '');
+    SET @Resultado = REPLACE(@Resultado, CHAR(9), ''); -- Tab
+    SET @Resultado = REPLACE(@Resultado, CHAR(10), ''); -- Salto de línea
+    SET @Resultado = REPLACE(@Resultado, CHAR(13), ''); -- Carriage return
+
+    RETURN @Resultado;
+
+    RETURN @Resultado;
+END
