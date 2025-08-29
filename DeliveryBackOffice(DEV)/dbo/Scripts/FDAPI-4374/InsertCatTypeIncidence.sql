566ef095a96559b6707fdbe59f80dcdd75a5107a
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2025-08-29 15:13:36 -0600

    FDAPI-4374 Script que inserta los 3 nuevos tipos de incidencias que se utilizaran en homolagación Forza-Aeropost

---
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-4374/InsertCatTypeIncidence.sql
@@ -0,0 +1,67 @@
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM [DeliveryBackOffice].[dbo].[CatTypeIncidence] 
+        WHERE [NameIncidence] = 'El destinatario cambio de residencia'
+        AND [CountryId] = 'HN'
+    )
+    BEGIN
+        INSERT INTO [DeliveryBackOffice].[dbo].[CatTypeIncidence]
+            ([NameIncidence],[DescriptionIncidence],[RowStatus],[TokenCreated],[DateCreated],
+                [TokenUpdated],[DateUpdated],[ServiceType],[OrderId],[Code],[IncidenceClasificationId],
+                [IsForcedIncidence],[ValidatesLocation],[HasConfirmationProcess],[NotifiesOrigin],
+                [NameIncidencePublic],[EvidenceRequirement],[CourierInstructions],[CountryId])
+        VALUES
+            ('El destinatario cambio de residencia','El destinatario cambio de residencia',1,'SYS-TGARCIA',GETDATE(),
+                NULL,NULL,'DELIVERY',1,NULL,1,1,0,1,1,NULL,1,NULL,'HN');
+    END;
+
+
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM [DeliveryBackOffice].[dbo].[CatTypeIncidence] 
+        WHERE [NameIncidence] = 'Zona roja o inaccecible'
+        AND [CountryId] = 'HN'
+    )
+    BEGIN
+        INSERT INTO [DeliveryBackOffice].[dbo].[CatTypeIncidence]
+            ([NameIncidence],[DescriptionIncidence],[RowStatus],[TokenCreated],[DateCreated],
+                [TokenUpdated],[DateUpdated],[ServiceType],[OrderId],[Code],[IncidenceClasificationId],
+                [IsForcedIncidence],[ValidatesLocation],[HasConfirmationProcess],[NotifiesOrigin],
+                [NameIncidencePublic],[EvidenceRequirement],[CourierInstructions],[CountryId])
+        VALUES
+            ('Zona roja o inaccecible','Zona roja o inaccecible',1,'SYS-TGARCIA',GETDATE(),
+                NULL,NULL,'DELIVERY',1,NULL,1,1,0,1,1,NULL,1,NULL,'HN');
+    END;
+
+
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM [DeliveryBackOffice].[dbo].[CatTypeIncidence] 
+        WHERE [NameIncidence] = 'Destinatario no localizado'
+        AND [CountryId] = 'HN'
+    )
+    BEGIN
+        INSERT INTO [DeliveryBackOffice].[dbo].[CatTypeIncidence]
+            ([NameIncidence],[DescriptionIncidence],[RowStatus],[TokenCreated],[DateCreated],
+                [TokenUpdated],[DateUpdated],[ServiceType],[OrderId],[Code],[IncidenceClasificationId],
+                [IsForcedIncidence],[ValidatesLocation],[HasConfirmationProcess],[NotifiesOrigin],
+                [NameIncidencePublic],[EvidenceRequirement],[CourierInstructions],[CountryId])
+        VALUES
+            ('Destinatario no localizado','Destinatario no localizado',1,'SYS-TGARCIA',GETDATE(),
+                NULL,NULL,'DELIVERY',1,NULL,1,1,0,1,1,NULL,1,NULL,'HN');
+    END;
+
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    ROLLBACK TRANSACTION;
+
+    -- Manejo de errores con PRINT
+    DECLARE @ErrorMessage NVARCHAR(4000);
+    SELECT @ErrorMessage = ERROR_MESSAGE();
+    
+    PRINT 'Error: ' + @ErrorMessage;
+END CATCH;
