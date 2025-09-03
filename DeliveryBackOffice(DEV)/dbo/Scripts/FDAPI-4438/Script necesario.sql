fd254d9ed4e717635607c58ca4ed3a0d07ece357
Bilkar Morataya <bilkar.morataya@forzadelivery.com>
2025-09-03 11:19:22 -0600

    FDAPI-4325: Se agrega nuevo campo a tabla PaymentZigi

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-4438/Script necesario.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-4438/Script necesario.sql
@@ -1,3 +1,4 @@
+--  Modificación a tabla DefaultValuesPerCountry --
 alter table dbo.DefaultValuesPerCountry
     add RegxMovilPhone NVARCHAR(50)
 go
@@ -14,6 +15,23 @@ exec sp_addextendedproperty 'MS_Description',
      N'Número asignado de Forza al país para enviar mensajes, debe estar registrado en META', 'SCHEMA', 'dbo', 'TABLE',
      'DefaultValuesPerCountry', 'COLUMN', 'WhatsappNumber'
 go
+
+
+-- Modificación a tabla PaymentZigi --
+
+alter table dbo.PaymentZigi
+    add PhoneNumber NVARCHAR(20)
+go
+
+exec sp_addextendedproperty 'MS_Description',
+     N'En cao que se requira enviar a otro númer de tléfono y no necesariamente asignado a la Guía, sirve mucho pra multiguía',
+     'SCHEMA', 'dbo', 'TABLE', 'PaymentZigi', 'COLUMN', 'PhoneNumber'
+go
+
+
+
+-- Modificación a tabla DefaultValuesPerCountry --
+
 
 UPDATE DeliveryBackOffice.dbo.DefaultValuesPerCountry SET RegxMovilPhone = N'^502[3-7]\d{7}$', WhatsappNumber = N'50223775302' WHERE IdCountry = N'GT';
 
