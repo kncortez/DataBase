2da0d58601a29e610a4e7505eff1f2f36ae05216
Azurdia <cristian.azurdia@forzadelivery.com>
2025-10-25 11:30:43 -0600

    FDAPI-4454 Correcion de tipo de dato de usaurio faltante

--- DeliveryBackOffice(DEV)/dbo/User Defined Types/TblBillingList.sql
+++ DeliveryBackOffice(DEV)/dbo/User Defined Types/TblBillingList.sql
@@ -1,11 +1,18 @@
-﻿CREATE TYPE [dbo].[TblBillingList] AS TABLE (
-    [RowNumber]                     INT            NOT NULL,
-    [IdBilling]                     INT            NULL,
-    [IdAccount]                     INT            NULL,
-    [Name]                          NVARCHAR (100) NULL,
-    [Address]                       NVARCHAR (600) NULL,
-    [TaxId]                         NVARCHAR (100) NULL,
-    [Status]                        INT            NULL,
-    [Token]                         NVARCHAR (150) NULL,
-    [IdVisitPointByClientPortfolio] INT            NULL);
-
+CREATE TYPE [dbo].[TblBillingList] AS TABLE(
+	[RowNumber] [int] NOT NULL,
+	[IdBilling] [int] NULL,
+	[IdAccount] [int] NULL,
+	[Name] [nvarchar](100) NULL,
+	[Address] [nvarchar](600) NULL,
+	[TaxId] [nvarchar](100) NULL,
+	[NRC] [nvarchar](200) NULL,
+	[TypeIdentificationDocumentCode] [nvarchar](100) NULL,
+	[IdDocument] [nvarchar](20) NULL,
+	[DistrictId] [int] NULL,
+	[StateId] [int] NULL,
+	[ActivityCode] [nvarchar](100) NULL,
+	[Inv_type] [int] NULL,
+	[Status] [int] NULL,
+	[Token] [nvarchar](150) NULL,
+	[IdVisitPointByClientPortfolio] [int] NULL
+);
