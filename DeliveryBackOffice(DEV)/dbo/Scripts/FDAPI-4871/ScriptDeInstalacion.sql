ab1e47fe40c33394e5da7e684516642ddeb164a7
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2025-10-29 18:41:41 -0600

    FDAPI-4871 Se agrega creacion de la tabla CatPartyResponsible

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-4871/ScriptDeInstalacion.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-4871/ScriptDeInstalacion.sql
@@ -1,6 +1,90 @@
 SET NOCOUNT ON;
 
 BEGIN TRY
+    IF EXISTS (SELECT 1 FROM sys.tables WHERE name = 'CatPartyResponsible' AND schema_id = SCHEMA_ID('dbo'))
+    BEGIN
+		PRINT 'La tabla dbo.CatPartyResponsible ya existe.';
+	END
+	ELSE
+	BEGIN
+
+		CREATE TABLE [dbo].[CatPartyResponsible] (
+			[IdCatPartyResponsible]       INT           IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
+			[PartyResponsibleName]        NVARCHAR (50) NOT NULL,
+			[RowStatus]                   BIT           NOT NULL,
+			[TokenCreated]                NVARCHAR (50) NOT NULL,
+			[DateCreated]                 DATETIME      NOT NULL,
+			[DateUpdated]                 DATETIME      NULL,
+			[TokenUpdated]                NVARCHAR (50) NULL,
+			CONSTRAINT [PK_CatPartyResponsible] PRIMARY KEY CLUSTERED ([IdCatPartyResponsible] ASC)
+		);
+
+		EXEC sp_addextendedproperty @name = N'MS_Description',
+			@value = N'Catalogo de parte responsable de la incidencia, Forza, cliente',
+			@level0type = N'SCHEMA',
+			@level0name = N'dbo',
+			@level1type = N'TABLE',
+			@level1name = N'CatPartyResponsible',
+			@level2type = NULL,
+			@level2name = NULL
+		EXEC sp_addextendedproperty @name = N'MS_Description',
+			@value = N'Identificador del catalago',
+			@level0type = N'SCHEMA',
+			@level0name = N'dbo',
+			@level1type = N'TABLE',
+			@level1name = N'CatPartyResponsible',
+			@level2type = N'COLUMN',
+			@level2name = N'IdCatPartyResponsible'
+		EXEC sp_addextendedproperty @name = N'MS_Description',
+			@value = N'Nombre de la parte responsable de la incidencia',
+			@level0type = N'SCHEMA',
+			@level0name = N'dbo',
+			@level1type = N'TABLE',
+			@level1name = N'CatPartyResponsible',
+			@level2type = N'COLUMN',
+			@level2name = N'PartyResponsibleName'
+		EXEC sp_addextendedproperty @name = N'MS_Description',
+			@value = N'Estado(1 Activo, 0 Inactivo)',
+			@level0type = N'SCHEMA',
+			@level0name = N'dbo',
+			@level1type = N'TABLE',
+			@level1name = N'CatPartyResponsible',
+			@level2type = N'COLUMN',
+			@level2name = N'RowStatus'
+		EXEC sp_addextendedproperty @name = N'MS_Description',
+			@value = N'Codigo de quien creo el registro',
+			@level0type = N'SCHEMA',
+			@level0name = N'dbo',
+			@level1type = N'TABLE',
+			@level1name = N'CatPartyResponsible',
+			@level2type = N'COLUMN',
+			@level2name = N'TokenCreated'
+		EXEC sp_addextendedproperty @name = N'MS_Description',
+			@value = N'Fecha de creacion',
+			@level0type = N'SCHEMA',
+			@level0name = N'dbo',
+			@level1type = N'TABLE',
+			@level1name = N'CatPartyResponsible',
+			@level2type = N'COLUMN',
+			@level2name = N'DateCreated'
+		EXEC sp_addextendedproperty @name = N'MS_Description',
+			@value = N'Codigo de quien modifico el registro',
+			@level0type = N'SCHEMA',
+			@level0name = N'dbo',
+			@level1type = N'TABLE',
+			@level1name = N'CatPartyResponsible',
+			@level2type = N'COLUMN',
+			@level2name = N'TokenUpdated'
+		EXEC sp_addextendedproperty @name = N'MS_Description',
+			@value = N'Fecha de modificacion',
+			@level0type = N'SCHEMA',
+			@level0name = N'dbo',
+			@level1type = N'TABLE',
+			@level1name = N'CatPartyResponsible',
+			@level2type = N'COLUMN',
+			@level2name = N'DateUpdated'
+	END;
+
     IF COL_LENGTH('dbo.CatTypeIncidence', 'CatPartyResponsibleId') IS NULL
     BEGIN
         ALTER TABLE dbo.CatTypeIncidence 
