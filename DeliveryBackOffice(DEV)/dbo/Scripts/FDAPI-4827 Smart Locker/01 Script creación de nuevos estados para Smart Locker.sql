7d68514cb40623901420314b72ce532a895eb655
FLG\walter.orozco <walter.orozco@forzadelivery.com>
2025-11-14 16:49:37 -0600

    FDAPI-4834: Creación del sp SLGuideReturnPickUp encargado de registrar el retiro de un paquete para devolución de guías en Smart Locker.

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-4827 Smart Locker/01 Script creación de nuevos estados para Smart Locker.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-4827 Smart Locker/01 Script creación de nuevos estados para Smart Locker.sql
@@ -2,7 +2,7 @@
    Script:    Creación de nuevos estados.
    Propósito: Se encargara en crear nuevos estados para los smart lockers.
    Autor:     Walter Orozco
-   Historia:  FDAPI-4827 [FDAPI-4831],[FDAPI-4832]
+   Historia:  FDAPI-4827 [FDAPI-4831],[FDAPI-4832],[FDAPI-4834]
    Fecha:     2025-10-23
 =================================================*/
 
@@ -61,6 +61,37 @@ BEGIN TRY
 			,2 --Externo
 			,'Estimado cliente, el paquete fue entregado en Smart Locker'
 			,'Guía entregada a travez de Smart Locker'
+			,1
+			,'SYS-WOROZCO'
+			,GETDATE()
+			,NULL
+			,NULL
+			,NULL
+			,5 --Entregado
+			)
+	END
+
+	IF NOT EXISTS (SELECT 1 FROM DeliveryBackOffice.dbo.StatusOrder WITH(NOLOCK) WHERE OrderDescription = 'Retirado en Smart Locker')
+	BEGIN
+		INSERT INTO [dbo].[StatusOrder]
+           ([OrderDescription]
+           ,[CatCheckpointTypeId]
+           ,[CatStatusTypeId]
+           ,[StatusMessage]
+           ,[StatusOrderTrackingDescription]
+           ,[RowStatus]
+           ,[TokenCreated]
+           ,[DateCreated]
+           ,[TokenUpdated]
+           ,[DateUpdated]
+           ,[NextSteps]
+           ,[CatStatusProcessId])
+		 VALUES
+			('Retirado en Smart Locker'
+			,3 --Checkpoint final
+			,2 --Externo
+			,'Estimado cliente, el paquete fue devuelto en Smart Locker'
+			,'Guía entregada por devolución a travez de Smart Locker'
 			,1
 			,'SYS-WOROZCO'
 			,GETDATE()
