574a6d85e992e31ea6a1d73189582cc3f38b8f68
csuazo11 <cristian.suazo@forzadelivery.com>
2025-11-25 15:28:51 -0600

    FDAPI-5100: Se pasan de Json a entidades

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetCourierInfoWithoutTokenLog.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetCourierInfoWithoutTokenLog.sql
@@ -6,6 +6,7 @@
    Fecha:     2024-04-08
 ============================================
 === CHANGELOG ================================
+-- 2025-11-25 | Historia/épica: FDAPI-5100 | Autor: Cristian Suazo |
 -- 2025-11-17 | Historia/épica: FDAPI-4976 | Autor: Cristian Suazo |
 =========================================== */
 
@@ -120,121 +121,74 @@ begin
                         from dbo.ConfigParams cf
                         where cf.Name = 'PickUpManifestEmailCAPP'
                     );
+			IF EXISTS 
+			(
+				SELECT TOP 1 1
+				from LogTokenPOD   pod with (nolock)
+                inner join SenderReceiver    sr with (nolock)
+                    on (sr.ID = pod.IdCourierman)
+                left join dbo.RouteAssigment ras with (nolock)
+                    on ras.IdCurrierMan = sr.ID
+                        and DateOfRoute = convert(date, getdate())
+                left join dbo.CatVehicle     vh with (nolock)
+                    on vh.IdVehicle = ras.IdVehicle
+                left join dbo.CatRoute       cr with (nolock)
+                    on cr.IdRoute = ras.IdRoute
+				where pod.LogTokenPOD = @Token
+					AND pod.RowStatus = 1
+			)
+			BEGIN
+				SELECT TOP 1 pod.IdCourierman AS IdCourier,
+						pod.DateCreated AS DateToken,
+						sr.First_Name AS FirstName,
+						sr.Last_Name AS LastName,
+						vh.Plate AS Vehicle,
+						 cr.CodeRoute AS Route,
+						@GuideRegexData AS GuideRegex,
+						 @GuideRegexScannerData AS GuideRegexEscaner,
+						 @DefaultEmail AS BillingEmail,
+						 @DefaultPickupManifestEmail AS PickUpManifestEmail,
+						 LogTokenPOD AS Token,
+						 @StationId AS StationId
+				from LogTokenPOD   pod with (nolock)
+                inner join SenderReceiver    sr with (nolock)
+                    on (sr.ID = pod.IdCourierman)
+                left join dbo.RouteAssigment ras with (nolock)
+                    on ras.IdCurrierMan = sr.ID
+                        and DateOfRoute = convert(date, getdate())
+                left join dbo.CatVehicle     vh with (nolock)
+                    on vh.IdVehicle = ras.IdVehicle
+                left join dbo.CatRoute       cr with (nolock)
+                    on cr.IdRoute = ras.IdRoute
+				where pod.LogTokenPOD = @Token
+					AND pod.RowStatus = 1
 
-            set @jsonResult1 =
-            (
-                select stuff(
-                                (
-                                    select top 1
-                                           ',{"IdCourier":"'
-                                           + convert(varchar, isnull(convert(varchar(10), pod.IdCourierman), 'N/A'))
-                                           + '",' + '"DateToken":"' + isnull(convert(varchar, pod.DateCreated, 23), 'N/A')
-                                           + '",' + '"FirstName":"' + isnull(convert(varchar, sr.First_Name), 'N/A')
-                                           + '",' + '"LastName":"' + isnull(convert(varchar, sr.Last_Name), 'N/A') + '",'
-                                           + '"Vehicle":"' + isnull(convert(varchar, vh.Plate), 'N/A') + '",'
-                                           + '"Route":"' + isnull(convert(varchar, cr.CodeRoute), 'N/A') + '",'
-                                           + '"GuideRegex":"' + isnull(convert(varchar(500), @GuideRegexData), '')
-                                           + '",' -- Para validar solo los digitos de la guía
-                                           + '"GuideRegexEscaner":"'
-                                           + isnull(convert(varchar(500), @GuideRegexScannerData), '')
-                                           + '",' -- Para el input del escaner de la courier
-                                           + '"BillingEmail":"' + isnull(convert(varchar(50), @DefaultEmail), 'N/A')
-                                           + '",' + '"PickUpManifestEmail":"'
-                                           + isnull(convert(varchar(50), @DefaultPickupManifestEmail), 'N/A') + '",'
-                                           + '"Token":"' + isnull(LogTokenPOD, '') + +'",'
-										   + '"StationId":"'+ ISNULL(CONVERT(NVARCHAR(5), @StationId),'N/A') + '"}'
-                                    from LogTokenPOD                 pod with (nolock)
-                                        inner join SenderReceiver    sr with (nolock)
-                                            on (sr.ID = pod.IdCourierman)
-                                        left join dbo.RouteAssigment ras with (nolock)
-                                            on ras.IdCurrierMan = sr.ID
-                                               and DateOfRoute = convert(date, getdate())
-                                        left join dbo.CatVehicle     vh with (nolock)
-                                            on vh.IdVehicle = ras.IdVehicle
-                                        left join dbo.CatRoute       cr with (nolock)
-                                            on cr.IdRoute = ras.IdRoute
-                                    where pod.LogTokenPOD = @Token
-									  AND pod.RowStatus = 1
-                                    order by 1 desc
-                                    for xml path(''), type
-                                ).value('.', 'varchar(max)')
-                              , 1
-                              , 1
-                              , ''
-                            )
-            );
-            print 'ingresa2';
-            print @jsonResult;
+				SELECT IdResult,
+						Message
+				FROM @responsemessage
+				WHERE Id = 'OK'
+			END
+			ELSE
+			BEGIN
+				SELECT 204 AS IdResult,
+					   'No se encontraron registros' AS Message
+			END
 
-        -- retornar resultado en formato json
-        if @jsonResult1 is null
-        begin
-
-            set @jsonResult1 =
-            (
-                select stuff((
-                                 select ',{"IdResult":204,' + '"Message":" No se encontraron registros"}'
-                                 for xml path(''), type
-                             ).value('.', 'varchar(max)')
-                           , 1
-                           , 1
-                           , ''
-                            )
-            );
-        end;
-
-        select ('[' + @jsonResult1 + ']') jsonResult1;
-
-
-
-        set @jsonResult =
-        (
-            select stuff((
-                             select ',{"IdResult":' + convert(varchar, IdResult) + ',' + '"Message":"' + Message + '"}'
-                             from @responsemessage
-                             where Id = 'OK'
-                             for xml path(''), type
-                         ).value('.', 'varchar(max)')
-                       , 1
-                       , 1
-                       , ''
-                        )
-        );
-
+        
     end try
     begin catch
         rollback transaction;
         select error_message();
         -- retornar mensaje de error
-        set @jsonResult =
-        (
-            select stuff(
-                            (
-                                select '"IdResult":' + convert(varchar, IdResult) + ',' + '"Message":"'
-                                       + convert(nvarchar(max), error_message()) + '"}'
-                                from @responsemessage
-                                where Id = 'Invalid'
-                                for xml path(''), type
-                            ).value('.', 'varchar(max)')
-                          , 1
-                          , 1
-                          , ''
-                        )
-        );
+		SELECT IdResult,
+			   ERROR_MESSAGE() AS Message
+		FROM @responsemessage
+
     end catch;
     if @@trancount > 0
     begin
         commit transaction;
 
     end;
-
-    -- destruir tablas temporales
-
-    if object_id('tempdb.dbo.#listGuides', 'U') is not null
-        drop table #listGuides;
-
-    -- retornar resultado en formato json
-
-    select ('[' + @jsonResult + ']') jsonResult;
 
 end;
