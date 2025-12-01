0d66f3403a8d32aa12e6e47e7d8e415c7a361f8c
csuazo11 <cristian.suazo@forzadelivery.com>
2025-12-01 16:04:52 -0600

    FDAPI-4607:

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetStatusOrderValid.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetStatusOrderValid.sql
@@ -20,14 +20,14 @@ BEGIN
                       AND do.Guide_Number = @GuideNumber
             );
 
-	DECLARE @StatusDelivery TINYINT = 5  
-	DECLARE @StatusDeliveryExpress TINYINT = 22 
-	DECLARE @StatusCODSettlement TINYINT = 24 
-	DECLARE @StatusCODPaid TINYINT = 25 
-	DECLARE @StatusReturn TINYINT = 14 
-	DECLARE @StatusReturnExpress TINYINT = 23 
-	DECLARE @StatusCancelled TINYINT = 7 
-	DECLARE @StatusReview TINYINT = 13 
+	DECLARE @StatusDelivery TINYINT = (SELECT so.StatusOrderId FROM StatusOrder so WHERE so.OrderDescription = 'Entregado')
+	DECLARE @StatusDeliveryExpress TINYINT = (SELECT so.StatusOrderId FROM StatusOrder so WHERE so.OrderDescription = 'Entregado en Express center')
+	DECLARE @StatusCODSettlement TINYINT = (SELECT so.StatusOrderId FROM StatusOrder so WHERE so.OrderDescription = 'COD Liquidado')
+	DECLARE @StatusCODPaid TINYINT = (SELECT so.StatusOrderId FROM StatusOrder so WHERE so.OrderDescription = 'COD Pagado')
+	DECLARE @StatusReturn TINYINT = (SELECT so.StatusOrderId FROM StatusOrder so WHERE so.OrderDescription = 'Devuelto')
+	DECLARE @StatusReturnExpress TINYINT = (SELECT so.StatusOrderId FROM StatusOrder so WHERE so.OrderDescription = 'Devuelto en Express center')
+	DECLARE @StatusCancelled TINYINT = (SELECT so.StatusOrderId FROM StatusOrder so WHERE so.OrderDescription = 'Anulado')
+	DECLARE @StatusReview TINYINT = (SELECT so.StatusOrderId FROM StatusOrder so WHERE so.OrderDescription = 'En Revisión')
 
     --Estados de finalización (Entregado, Entregado en Express center)
     IF @GuideStatusOrderId = @StatusDelivery
