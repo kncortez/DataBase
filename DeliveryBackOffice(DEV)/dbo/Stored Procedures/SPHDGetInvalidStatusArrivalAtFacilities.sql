963b27405c84c4384d38922e80908519bff78fab
brandon-p <brandon.pedroza@forzadelivery.com>
2026-01-08 21:06:21 -0600

    FDAPI-5348: #se agrega sp para obtener estados que deben excluirse en arribo a instalaciones

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHDGetInvalidStatusArrivalAtFacilities.sql
@@ -0,0 +1,26 @@
+﻿/* =================================================
+   SP:        SPHDGetInvalidStatusArrivalAtFacilities
+   Propósito: Obtiene los estados que no deben permitir el modulo de arribo a instalaciones
+   Autor:     Brandon Pedroza
+   Historia:  FDAPI-5348
+   Fecha:     2026-01-08
+
+=== CHANGELOG ============================
+
+=========================================== */
+
+CREATE PROCEDURE [dbo].[SPHDGetInvalidStatusArrivalAtFacilities]
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT StatusOrderId
+    FROM DeliveryBackOffice.dbo.StatusOrder WITH(NOLOCK)
+    WHERE CatCheckpointTypeId = 3
+      AND RowStatus = 1
+
+    UNION
+
+    SELECT 4 AS StatusOrderId; -- EN RUTA
+END
+GO
