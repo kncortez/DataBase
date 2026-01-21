2c447dd25dee7ba9607c3e4a775be1069f1d5932
Azurdia <cristian.azurdia@forzadelivery.com>
2026-01-21 08:11:44 -0600

    FDAPI-2982 Correción Solicitada por revisor de código

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetClientFromVisitPointClientByID.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetClientFromVisitPointClientByID.sql
@@ -1,25 +1,31 @@
-﻿
--- =============================================
--- Author:		<Andres,Ruiz>
--- Create date: <2022-10-05>
--- Description:	<Obtener datos de cliente basado en el codigo de referencia de un punto de visita>
--- =============================================
+﻿/* =================================================
+   SP:        [dbo].[ValidateBatchInvoice]
+   Propósito: <Obtener datos de cliente basado en el codigo de referencia de un punto de visita>
+   Autor:     <Andres Ruiz>
+   Historia:  <>
+   Fecha:     2022-10-05
+============================================
+=== CHANGELOG ================================
+-- 2025-01-21 | Historia/épica: FDAPI-2982 | Autor: Cristian Azurdia |
+-- 2022-10-05 | Historia/épica:  | Autor: Andres Ruiz |
+=========================================== */
 
 CREATE PROCEDURE [dbo].[GetClientFromVisitPointClientByID]
 	@CodeOfReference AS int
 AS
 BEGIN
 
-	SELECT 
-		IIF(LTRIM(RTRIM(ISNULL(VPC.Phone,''))) != '', LTRIM(RTRIM(VPC.Phone)), Cu.CustomerPhone) 'destinationPhone',
-		IIF(LTRIM(RTRIM(ISNULL(VPC.DescriptionOfClient,''))) != '', LTRIM(RTRIM(VPC.DescriptionOfClient)), ISNULL(Cu.CommercialName, Cu.[Name])) 'destinationClientName',
-		LTRIM(RTRIM(VPC.[Address])) 'AddressPickup'
-	FROM 
-		[DeliveryBackOffice].[dbo].[VisitPointClient] VPC WITH(NOLOCK)
-		LEFT JOIN
-			[DeliveryBackOffice].[dbo].[Customer] Cu WITH(NOLOCK)
-			ON
-				VPC.CustomerID = Cu.IdCustomer
+    SELECT 
+    	ISNULL(dvpc.PrefixNumber, '') 'PrefixNumber',
+    	IIF(LTRIM(RTRIM(ISNULL(VPC.Phone,''))) != '', LTRIM(RTRIM(VPC.Phone)), Cu.CustomerPhone) 'destinationPhone',
+    	IIF(LTRIM(RTRIM(ISNULL(VPC.DescriptionOfClient,''))) != '', LTRIM(RTRIM(VPC.DescriptionOfClient)), ISNULL(Cu.CommercialName, Cu.[Name])) 'destinationClientName',
+    	LTRIM(RTRIM(VPC.[Address])) 'AddressPickup'
+    FROM 
+    	[DeliveryBackOffice].[dbo].[VisitPointClient] VPC WITH(NOLOCK)
+    	LEFT JOIN [DeliveryBackOffice].[dbo].[Customer] Cu WITH(NOLOCK)
+    		ON VPC.CustomerID = Cu.IdCustomer
+    	LEFT JOIN [DeliveryBackOffice].[dbo].[DefaultValuesPerCountry] dvpc WITH(NOLOCK)
+    		ON cu.CountryID = dvpc.IdCountry
 	WHERE 
 		VPC.CodeOfReference = @CodeOfReference
 
