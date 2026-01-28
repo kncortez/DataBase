03d384ff131bd4e3f55a88e59a8fbf8bb273fc49
EdelmanV <edelman.vasquez@forzalatam.com>
2026-01-28 17:42:57 -0600

    #Cambair order de campos en Join

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetSettlementCODUnifiedRoute.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetSettlementCODUnifiedRoute.sql
@@ -65,8 +65,8 @@ BEGIN
             INNER JOIN DeliveryBackOffice.dbo.DeliverySettlementDetail dsd WITH (NOLOCK)
                 ON dsd.ID_DeliveryOrderBySettlement = dbs.ID
             INNER JOIN dbo.DeliveryOrder DOR WITH (NOLOCK)
-                ON DOR.Guide_Number = dsd.Guide_Number
-                   AND DOR.Guide_Serie = dsd.Guide_Serie
+                ON DOR.Guide_Serie = dsd.Guide_Serie AND 
+                   DOR.Guide_Number = dsd.Guide_Number
             LEFT JOIN DeliveryBackOffice.dbo.ManifestSettlementIncidence msi WITH (NOLOCK)
                 ON dbs.ID = msi.ManifestNumber
             LEFT JOIN DeliveryBackOffice.dbo.CatStation cs
@@ -106,8 +106,8 @@ BEGIN
             INNER JOIN DeliveryBackOffice.dbo.DeliverySettlementDetail dsd WITH (NOLOCK)
                 ON dsd.ID_DeliveryOrderBySettlement = dbs.ID
             INNER JOIN dbo.DeliveryOrder DOR WITH (NOLOCK)
-                ON DOR.Guide_Number = dsd.Guide_Number
-                   AND DOR.Guide_Serie = dsd.Guide_Serie
+                ON DOR.Guide_Serie = dsd.Guide_Serie 
+                   AND DOR.Guide_Number = dsd.Guide_Number
             LEFT JOIN DeliveryBackOffice.dbo.ManifestSettlementIncidence msi WITH (NOLOCK)
                 ON dbs.ID = msi.ManifestNumber
             LEFT JOIN DeliveryBackOffice.dbo.CatStation cs
