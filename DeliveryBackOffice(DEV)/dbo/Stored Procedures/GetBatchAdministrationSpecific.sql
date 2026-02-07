6581600a4830f0c19874d4f49ff0f9ba0a51816a
Azurdia <cristian.azurdia@forzadelivery.com>
2026-02-06 18:53:38 -0600

    FDAPI-2982 Correción en rendimiento en sp para desplegar documentos relacionados a un lote

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetBatchAdministrationSpecific.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetBatchAdministrationSpecific.sql
@@ -17,9 +17,9 @@ AS
 BEGIN
     BEGIN TRY
 
-	--Devuelva toda la información de un Lote especifico
-	SELECT 
-	   [IBH].[Id_Lote]								AS [Id_Lote]
+    --Devuelva toda la información de un Lote especifico
+    SELECT 
+       [IBH].[Id_Lote]								AS [Id_Lote]
       ,ISNULL([IBH].[RTN],'')						AS [RTN]
       ,[IBH].[NoDeclaracion]						AS [NoDeclaracion]
       ,[IBH].[CAI]									AS [CAI]
@@ -41,46 +41,42 @@ BEGIN
       ,[IBH].[PercentInvoiceLeftNotifycation]		AS [PercentInvoiceLeftNotifycation]
       ,ISNULL([IBH].[AmountRequested],0)			AS [AmountRequested]
       ,[IBH].[RowStatus]							AS [RowStatus]
-	FROM [dbo].[InvoiceBatchHeader] IBH WITH (NOLOCK)
-	LEFT JOIN [dbo].[CatTypeDocument] CTD WITH(NOLOCK)
-		ON [IBH].[TypeDocument] = [CTD].[IdTypeDocument]
-	WHERE [Id_Lote] = @IdLote
+    FROM [dbo].[InvoiceBatchHeader] IBH WITH (NOLOCK)
+    INNER JOIN [dbo].[CatTypeDocument] CTD WITH(NOLOCK)
+       ON [IBH].[TypeDocument] = [CTD].[IdTypeDocument]
+    WHERE [Id_Lote] = @IdLote
 
-	--Devuelve toda la información de los puntos de ventas relacionado a este Lote
-	SELECT 
-		F.[Code],
-		VPC.[DescriptionOfClient]          [Punto de venta],
-		IBR.[CodeOfReference]              [CodeOfReference],
-		ISNULL(F.[Facturas generadas],0)   [Facturas generadas],
-		ISNULL(F.[Moneda], 'L')            [Moneda],
-		ISNULL(F.[Monto facturado], 0.00)  [Monto facturado],
-		ISNULL(IBR.RowStatus,0)            [RowStatus]
-	FROM dbo.InvoiceBatchHeader IBH WITH (NOLOCK)
-	LEFT JOIN dbo.InvoiceBatchRelationships IBR WITH(NOLOCK)
-		ON IBR.Id_Lote = IBH.Id_Lote 
-	INNER JOIN dbo.VisitPointClient VPC WITH(NOLOCK)
-		ON  VPC.CodeOfReference = IBR.CodeOfReference
-	OUTER APPLY(
-		SELECT ISNULL(PF.dpf_SAPcardCode,'')        [Code]
-			  ,ISNULL(COUNT(IBD.Id_Lote),0)			[Facturas generadas]
-			  ,ISNULL(CCC.Symbol,'L')				[Moneda]
-			  ,ISNULL(SUM(IH.inv_amount),0)			[Monto facturado]
-		FROM dbo.InvoiceHeader IH WITH (NOLOCK)
-		INNER JOIN dbo.InvoiceBatchDetail IBD WITH (NOLOCK)
-			ON IBD.inv_pk_id = IH.inv_pk_id
-		INNER JOIN dbo.CatCurrencyCOD CCC WITH(NOLOCK)
-			ON IH.IdCurrency = CCC.IdCatCurrencyCOD
-		LEFT JOIN dbo.del_ParametrosFactura PF WITH(NOLOCK)
-			ON VPC.CodeOfReference = PF.dpf_VpCodeOfReference
-		WHERE IdCountry = 'HN'
-		  AND IBD.Id_Lote = IBH.Id_Lote
-		  AND inv_vpCodeOfReferences =IBR.CodeOfReference
-		GROUP BY PF.dpf_SAPcardCode,
-				 IBD.Id_Lote,
-				 CCC.Symbol
-	) F
-	WHERE IBH.Id_Lote = @IdLote --and IBR.RowStatus = 1
-	ORDER BY VPC.[DescriptionOfClient]
+    --Devuelve toda la información de los puntos de ventas relacionado a este Lote
+    SELECT IBR.CodeOfReference             [ID],
+        PF.dpf_SAPcardCode                 [Code],
+        VPC.[DescriptionOfClient]          [Punto de venta],
+        IBR.[CodeOfReference]              [CodeOfReference],
+        ISNULL(F.[Facturas generadas],0)   [Facturas generadas],
+        ISNULL(F.[Moneda],'L')             [Moneda],
+        ISNULL(F.[Monto facturado], 0.00)  [Monto facturado],
+        IBR.RowStatus                      [RowStatus]
+    FROM dbo.InvoiceBatchHeader IBH WITH (NOLOCK)
+    LEFT JOIN dbo.InvoiceBatchRelationships IBR WITH(NOLOCK)
+        ON IBR.Id_Lote = IBH.Id_Lote
+    INNER JOIN dbo.VisitPointClient VPC         WITH(NOLOCK)
+        ON VPC.CodeOfReference = IBR.CodeOfReference
+    INNER JOIN dbo.del_ParametrosFactura PF     WITH(NOLOCK)
+        ON PF.dpf_VpCodeOfReference = IBR.CodeOfReference
+    OUTER APPLY(
+        SELECT COUNT(IBD.Id_Lote)                 [Facturas generadas]
+             , MAX(CCC.Symbol)                    [Moneda]
+             , SUM(ISNULL(IH.inv_amount,0))       [Monto facturado]
+        FROM dbo.InvoiceBatchDetail IBD WITH (NOLOCK)
+        INNER JOIN dbo.InvoiceHeader IH WITH (NOLOCK)
+            ON IBD.inv_pk_id = IH.inv_pk_id
+        INNER JOIN dbo.CatCurrencyCOD CCC WITH(NOLOCK)
+            ON CCC.IdCatCurrencyCOD = IH.IdCurrency
+        WHERE inv_vpCodeOfReferences = IBR.CodeOfReference
+          AND IH.inv_type = @TypeDocument
+          AND IBD.Id_Lote = IBH.Id_Lote
+    ) F
+    WHERE IBH.Id_Lote = @IdLote
+    ORDER BY VPC.[DescriptionOfClient]
 		
     END TRY
     BEGIN CATCH
