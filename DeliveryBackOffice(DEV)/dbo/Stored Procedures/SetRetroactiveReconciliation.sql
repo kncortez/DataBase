d8a017269f4af893717fe344f6267b7a60d8563d
Azurdia <cristian.azurdia@forzadelivery.com>
2026-02-12 16:39:38 -0600

    FDAPI-5383 SP para generación de datos de reconciliación retroactiva

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SetRetroactiveReconciliation.sql
@@ -0,0 +1,87 @@
+/* =================================================
+   SP:        [dbo].[SetRetroactiveReconciliation]
+   Propósito: <Datos para reportes de reconciliación retroactiva>
+   Autor:     <Cristian Azurdia>
+   Historia:  <FDAPI-5383>
+   Fecha:     2026-02-02
+============================================
+=== CHANGELOG ================================
+-- 2026-02-02 | Historia/épica: FDAPI-5383 | Autor: Cristian Azurdia |
+=========================================== */
+
+ALTER PROCEDURE [dbo].[SetRetroactiveReconciliation]
+
+AS
+BEGIN
+    BEGIN TRY
+
+    DECLARE @StatusOrderFinish TABLE
+    (
+        StatusOrderId INT PRIMARY KEY,
+        StatusOrderDescription NVARCHAR(100) NOT NULL,
+        StatusMessage NVARCHAR(512) NOT NULL
+    );
+
+    INSERT INTO @StatusOrderFinish (StatusOrderId, StatusOrderDescription, StatusMessage)
+    SELECT StatusOrderId, OrderDescription, StatusMessage 
+    FROM StatusOrder WITH (NOLOCK)
+    WHERE CatCheckpointTypeId = 3
+      AND RowStatus = 1;
+
+    INSERT INTO RetroactiveReconciliation
+    SELECT do.dateCreated                                       [Guidedate]
+         , concat(do.Guide_Serie,'-',do.Guide_Number)           [Guide]
+         , cs.[IdCustomer]                                      [CustomerId]
+         , cs.[Name]                                            [CustomerName]
+         , cts.CtsId                                            [TypeSaleId]
+         , cts.CtsName                                          [TypeSale]
+         , so.StatusOrderId                                     [StatusId]
+         , so.OrderDescription                                  [Status]
+         , ISNULL(cas.SysIdSystem,-1)                           [SystemId]
+         , ISNULL(cas.SysNameSystem,'No Reconocido')            [NameSystem]
+         , Concat(do.Sender_FirstName, ' ', do.Sender_LastName) [Sender]
+         , IIF(ISNULL(cs.ConditionOfPaymentID, 1) = 1 , 'CONTADO', 'CREDITO') [Payment_method]
+         , ISNULL(inv.invoice,'Pendiente')                      [Invoice]
+         , att.attempt                                          [Attempt]
+         , ISNULL(bop.Amount,0)                                 [Overweight]
+         , do.SenderCountryId                                   [CountryId]
+    FROM DeliveryOrder do     WITH (NOLOCK)
+    INNER JOIN StatusOrder so WITH (NOLOCK)
+        ON so.StatusOrderId = do.StatusOrderId
+    INNER JOIN dbo.cost c     WITH(NOLOCK)
+        ON c.GuideSerie = do.Guide_serie
+       AND c.GuideNumber = do.Guide_Number
+    LEFT JOIN dbo.BreakdownOfPayment bop WITH(NOLOCK)
+        ON c.IdCost = bop.IdCost
+       AND bop.[Description] = 'Recargo por Peso'
+    INNER JOIN dbo.Customer cs WITH(NOLOCK)
+        ON cs.IdCustomer = do.IdCustomer
+    LEFT JOIN CatTypeService cts WITH (NOLOCK)
+        ON do.TypeService = cts.CtsShortName
+    LEFT JOIN CatSystem cas WITH (NOLOCK)
+        ON cas.SysIdSystem = do.CatSystemId
+    OUTER APPLY(
+                 SELECT IIF( IH.IdCountry = 'SV', IH.inv_NumberFEL, IH.inv_certificationFEL) [invoice]
+                      , ID.dti_fk_orderSerie    [Guide_Serie]
+                      , ID.dti_fk_orderNumber   [Guide_number]
+                 FROM invoiceDetail ID WITH(NOLOCK)
+                 INNER JOIN invoiceHeader IH WITH(NOLOCK)
+                   ON IH.inv_pk_id = ID.dti_fk_header
+                 WHERE ID.dti_fk_orderSerie = do.Guide_Serie
+                   AND ID.dti_fk_orderNumber = do.Guide_number
+               ) inv
+    OUTER APPLY(
+                 SELECT count(*) [attempt]
+                 FROM DeliveryAttempt da WITH(NOLOCK)
+                 WHERE da.Guide_Serie = do.Guide_Serie
+                   AND da.Guide_Number = do.Guide_Number
+               ) att
+    WHERE do.DateCreated = DATEADD(DAY,-1,GETDATE())
+
+    END TRY
+    BEGIN CATCH
+            SELECT 0 AS StatusCode, 
+                   'Ha ocurrido un error en el proceso' AS StatusMessage
+    END CATCH
+
+END
---
+++ DeliveryBackOffice(DEV)/dbo/Tables/RetroactiveReconciliation.sql
@@ -0,0 +1,208 @@
+﻿CREATE TABLE [dbo].[RetroactiveReconciliation](
+	[IdRetroactiveReconciliation] [int] IDENTITY(1,1) NOT NULL,
+	[GuideDate] [datetime] NOT NULL,
+	[Guide] [nvarchar](25) NOT NULL,
+	[CustomerId] [int] NOT NULL,
+	[CustomerName] [nvarchar](64) NOT NULL,
+	[TypeSaleId] [int] NOT NULL,
+	[TypeSale] [nvarchar](64) NOT NULL,
+	[StatusId] [int] NOT NULL,
+	[Status] [nvarchar](64) NOT NULL,
+	[SystemId] [int] NOT NULL,
+	[NameSystem] [nvarchar](64) NOT NULL,
+	[Sender] [nvarchar](128) NOT NULL,
+	[Payment_method] [nvarchar](50) NOT NULL,
+	[Invoice] [nvarchar](128) NOT NULL,
+	[Attempt] [int] NOT NULL,
+	[Overweight] [decimal](16, 2) NOT NULL,
+	[CountryId] [nvarchar](4) NOT NULL,
+
+    CONSTRAINT [PK_RetroactiveReconciliation] PRIMARY KEY CLUSTERED ([IdRetroactiveReconciliation] ASC)
+);
+
+GO
+EXECUTE sp_addextendedproperty 
+        @name = N'MS_Description', 
+        @value = N'Fecha de emisión de la guía', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'GuideDate';
+
+GO
+EXECUTE sp_addextendedproperty 
+        @name = N'MS_Description', 
+        @value = N'Serie y Correlativo de guía', 
+        @level0type = N'SCHEMA', 
+        @level0name = N'dbo', 
+        @level1type = N'TABLE', 
+        @level1name = N'RetroactiveReconciliation', 
+        @level2type = N'COLUMN', 
+        @level2name = N'Guide';
+
+GO
+EXECUTE sp_addextendedproperty 
+        @name = N'MS_Description', 
+		@value = N'Código de cliente', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'CustomerId';
+
+GO
+EXECUTE sp_addextendedproperty 
+        @name = N'MS_Description', 
+		@value = N'Nombre del cliente', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'CustomerName';
+
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Código de tipo de venta', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'TypeSaleId';
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Tipo de Venta', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'TypeSale';
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Id del estado de guía', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'StatusId';
+
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Estado de la guía, estado terminal', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'Status';
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Id del sistema donde se emitió la guía', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'SystemId';
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Sistema donde se emite la guía', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'System';
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Nombre de quien envia el paquete', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'Sender';
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Metodo de pago de la guía', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'Payment_method';
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Factura asociada a la guía', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'Invoice';
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Intentos de envío de la guía', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'Attempt';
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Pago por sobrepeso', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'Overweight';
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'País de origen de la guía', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation', 
+		@level2type = N'COLUMN', 
+		@level2name = N'CountryId';
+
+GO
+EXECUTE sp_addextendedproperty 
+		@name = N'MS_Description', 
+		@value = N'Datos para reporte de Reconciliación Retro Activa', 
+		@level0type = N'SCHEMA', 
+		@level0name = N'dbo', 
+		@level1type = N'TABLE', 
+		@level1name = N'RetroactiveReconciliation';
