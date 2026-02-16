248ec7f203614c728e414f32bc5fb6faf45f0b23
EdelmanV <edelman.vasquez@forzalatam.com>
2026-02-16 10:06:07 -0600

    #Separar logica de pagos de envíos y de COD

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_GetTransactionBySettlementId.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_GetTransactionBySettlementId.sql
@@ -1,9 +1,16 @@
+USE [DeliveryBackOffice]
+GO
+/****** Object:  StoredProcedure [dbo].[SPHD_GetTransactionBySettlementId]    Script Date: 15/02/2026 23:01:40 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 -- =============================================
 -- Author:		<Edelman>
 -- Create date: <2026-01-16>
 -- Description:	<obtener guías pagadas por transferencia en entrega POD>
 -- =============================================
-CREATE PROCEDURE SPHD_GetTransactionBySettlementId
+CREATE PROCEDURE [dbo].[SPHD_GetTransactionBySettlementId]
 (
     @ID_DeliveryOrderBySettlement INT
 )
@@ -11,14 +18,46 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT
+    SELECT 
         CD.Voucher + '-' + C.ProductNumber AS Transaccion,
-         CASE 
+		 CASE 
 		    WHEN CD.IdTypeOfMoney = 11  THEN CD.Voucher
 			WHEN CD.IdTypeOfMoney = 10  THEN PZ.ZigiTransactionId
 			ELSE CD.Voucher
         END AS Voucher,
-          ISNULL(DO.Collect_OnDelivery,0)+ISNULL(DO.PriceShippment,0) AS 'TotalAmount',
+		 CASE
+		     WHEN CD.IdTypeOfMoneyCOD = 10 AND CD.IdTypeOfMoneyCollect = 10 THEN (ISNULL(DO.priceShippment,0) + ISNULL(DO.Collect_OnDelivery,0))
+			 WHEN CD.IdTypeOfMoneyCOD = 11 AND CD.IdTypeOfMoneyCollect = 11 THEN (ISNULL(DO.PriceShippment,0) + ISNULL(DO.Collect_OnDelivery,0))
+			 ELSE ISNULL(DO.Collect_OnDelivery,0) END
+			 AS 'TotalAmount',
+		 CASE 
+		    WHEN CD.IdTypeOfMoneyCOD = 11  THEN 'Transferencia'
+			WHEN CD.IdTypeOfMoneyCOD = 10  THEN 'Zigi'
+			ELSE 'N/A'
+        END AS 'PaymentType'
+    FROM [DeliveryBackOffice].[dbo].[DeliverySettlementDetail] DSD WITH (NOLOCK)
+    INNER JOIN [DeliveryBackOffice].[dbo].[Cost] C WITH (NOLOCK)
+        ON C.GuideSerie = DSD.Guide_Serie AND C.GuideNumber = DSD.Guide_Number
+    LEFT JOIN [DeliveryBackOffice].[dbo].[CostDetail] CD WITH (NOLOCK)
+        ON CD.IdCost = C.IdCost
+    LEFT JOIN [DeliveryBackOffice].[dbo].[PaymentZigi] PZ WITH (NOLOCK)
+        ON  PZ.GuideSerie  = DSD.Guide_Serie AND PZ.GuideNumber = DSD.Guide_Number
+    LEFT JOIN [DeliveryBackOffice].[dbo].[PaymentZigiMulti] PZM WITH (NOLOCK)
+        ON  PZM.Id_PaymentZigi  = PZ.ZigiPaymentId 
+	LEFT JOIN [DeliveryBackOffice].[dbo].[DeliveryOrder] DO WITH (NOLOCK)
+	    ON DO.Guide_Serie = DSD.Guide_Serie AND DO.Guide_Number = DSD.Guide_Number
+    WHERE
+        DSD.ID_DeliveryOrderBySettlement = @ID_DeliveryOrderBySettlement
+        AND CD.IdTypeOfMoneyCOD IN (10,11)
+	UNION ALL
+	   SELECT 
+        CD.Voucher + '-' + C.ProductNumber AS Transaccion,
+		 CASE 
+		    WHEN CD.IdTypeOfMoney = 11  THEN CD.Voucher
+			WHEN CD.IdTypeOfMoney = 10  THEN PZ.ZigiTransactionId
+			ELSE CD.Voucher
+        END AS Voucher,
+         ISNULL(DO.PriceShippment,0) AS 'TotalAmount',
 		 CASE 
 		    WHEN CD.IdTypeOfMoneyCOD = 11  THEN 'Transferencia'
 			WHEN CD.IdTypeOfMoneyCOD = 10  THEN 'Zigi'
@@ -28,15 +67,16 @@ BEGIN
     FROM [DeliveryBackOffice].[dbo].[DeliverySettlementDetail] DSD WITH (NOLOCK)
     INNER JOIN [DeliveryBackOffice].[dbo].[Cost] C WITH (NOLOCK)
         ON C.GuideSerie = DSD.Guide_Serie AND C.GuideNumber = DSD.Guide_Number
-    INNER JOIN [DeliveryBackOffice].[dbo].[CostDetail] CD WITH (NOLOCK)
+    LEFT JOIN [DeliveryBackOffice].[dbo].[CostDetail] CD WITH (NOLOCK)
         ON CD.IdCost = C.IdCost
     LEFT JOIN [DeliveryBackOffice].[dbo].[PaymentZigi] PZ WITH (NOLOCK)
-        ON  PZ.GuideSerie  = DSD.Guide_Serie
-    AND PZ.GuideNumber = DSD.Guide_Number
+        ON  PZ.GuideSerie  = DSD.Guide_Serie AND PZ.GuideNumber = DSD.Guide_Number
     LEFT JOIN [DeliveryBackOffice].[dbo].[PaymentZigiMulti] PZM WITH (NOLOCK)
         ON  PZM.Id_PaymentZigi  = PZ.ZigiPaymentId 
+	LEFT JOIN [DeliveryBackOffice].[dbo].[DeliveryOrder] DO WITH (NOLOCK)
+	    ON DO.Guide_Serie = DSD.Guide_Serie AND DO.Guide_Number = DSD.Guide_Number
     WHERE
         DSD.ID_DeliveryOrderBySettlement = @ID_DeliveryOrderBySettlement
-        AND CD.IdTypeOfMoney IN (2,11,10);
+        AND CD.IdTypeOfMoneyCollect IN (2)
+	
 END
-GO
