5dddddadd54136b90057c582f1007f211b6c6f04
EdelmanV <edelman.vasquez@forzalatam.com>
2026-02-16 11:37:51 -0600

    #Agregar vacio cuando no aplique ningun metodo de mago registrado

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphd_LastMileSettlementsReport.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/sphd_LastMileSettlementsReport.sql
@@ -42,14 +42,14 @@ BEGIN
 			  WHEN cd.IdTypeOfMoneyCollect = 1  THEN 'Efectivo'
 			  WHEN cd.IdTypeOfMoneyCollect = 2  THEN 'Pago con Tarjeta'
 			  WHEN cd.IdTypeOfMoneyCollect = 10 THEN 'Zigi'
-	          ELSE 'Pago preautorizado'
+	          ELSE ''
 		  END AS PaymentMethodCollect,
            CASE 
 		      WHEN cd.IdTypeOfMoneyCOD = 11 THEN 'Transferencia'
 			  WHEN cd.IdTypeOfMoneyCOD = 1  THEN 'Efectivo'
 			  WHEN cd.IdTypeOfMoneyCOD = 2  THEN 'Pago con Tarjeta'
 			  WHEN cd.IdTypeOfMoneyCOD = 10 THEN 'Zigi'
-	          ELSE 'Pago preautorizado'
+	          ELSE ''
 		  END AS PaymentMethodCOD
     FROM dbo.DeliveryOrderBySettlement dst
         INNER JOIN dbo.DeliverySettlementDetail dsd
