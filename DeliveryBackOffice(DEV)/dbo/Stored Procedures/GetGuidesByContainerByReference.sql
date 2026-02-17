feb7a726dc6d9a2a6d6d549f8685c686dfc5c9c1
Erick Hernandez <erick.hernandez@forzadelivery.com>
2026-02-17 12:52:12 -0600

    FDAPI-5552 Se agrega validacion de checkpoint = 50 para realizar la recoleccion.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetGuidesByContainerByReference.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetGuidesByContainerByReference.sql
@@ -1,12 +1,13 @@
-﻿-- =============================================  
--- Author:    <Brandon, Pedroza>  
--- Create date: <2025-01-13>  
--- Description: <Contenerizacion guias - obtiene las guias por referencia y por contenedor>  
--- =============================================  
--- Author:		<Brandon, Pedroza>  
--- Modifie:		<2025-02-04>  
--- Description: <Contenerizacion guias - aceptar paquete unicamente del cliente asignado a la recoleccion>  
--- =============================================   
+﻿/* =================================================
+   SP:        [dbo].[GetGuidesByContainerByReference]
+   Propósito: <Contenerizacion guias - obtiene las guias por referencia y por contenedor>
+   Autor:     Brandon Pedroza
+   Historia:  <>
+   Fecha:     <2025-01-13>
+   === CHANGELOG ============================
+2025-02-04 | Historia/épica: <> | Autor: Brandon Pedroza | <Contenerizacion guias - aceptar paquete unicamente del cliente asignado a la recoleccion>  
+2026-02-17 | Historia/épica: <FDAPI-5552> | Autor: Erick Hernandez | 
+=========================================== */
 CREATE PROCEDURE [dbo].[GetGuidesByContainerByReference]
 	@Container TblContainerList READONLY,
 	@References TblReferencesList READONLY,
@@ -18,14 +19,14 @@ BEGIN
 
 	SET @StatusContainerCreated = (
 			SELECT IdCatStatus
-			FROM CatShipContainerStatus WITH (NOLOCK)
+			FROM DeliveryBackOffice.dbo.CatShipContainerStatus WITH (NOLOCK)
 			WHERE [Name] = 'Creado'
 			);
 
 	SET @IdCustomerPickup =(
 		SELECT TOP 1 VPC.CustomerID
-			FROM SchedulePickup SP WITH(NOLOCK)
-						INNER JOIN VisitPointClient VPC WITH(NOLOCK)
+			FROM DeliveryBackOffice.dbo.SchedulePickup SP WITH(NOLOCK)
+						INNER JOIN DeliveryBackOffice.dbo.VisitPointClient VPC WITH(NOLOCK)
 						ON SP.SenderId = VPC.CodeOfReference
 						WHERE SP.SchedulePickupStatus = 1
 						AND SP.SchedulePickupId =@IdPickup
@@ -41,8 +42,8 @@ BEGIN
 				                      ,DOP.GuideNumber DESC
 			                         ) AS RowNum
 		          ,DO.DateCreated
-	        FROM DeliveryOrder DO WITH (NOLOCK)
-	             INNER JOIN DeliveryOrderPiece DOP WITH(NOLOCK)
+	        FROM DeliveryBackOffice.dbo.DeliveryOrder DO WITH (NOLOCK)
+	             INNER JOIN DeliveryBackOffice.dbo.DeliveryOrderPiece DOP WITH(NOLOCK)
 				 ON DO.Guide_Serie = DOP.GuideSerie
 	                AND DO.Guide_Number = DOP.GuideNumber
 	       WHERE DO.Ticket_Number IN
@@ -51,8 +52,8 @@ BEGIN
 	                FROM @References
 					WHERE ReferenceGuide NOT IN ('','0')
 	             )
-            AND ISNULL(DO.SenderCountryId, 'GT') = @IdCountry 
-			AND DO.StatusOrderId IN (1,15)
+            AND DO.SenderCountryId = @IdCountry 
+			AND DO.StatusOrderId IN (1,15,50)
 			AND DO.IdCustomer =@IdCustomerPickup
        )
 	SELECT GuideSerie
@@ -64,10 +65,10 @@ BEGIN
 	SELECT DOP.GuideSerie
 	       ,DOP.GuideNumber
 	       ,DOP.NoPiece
-	  FROM ShippingContainer CT WITH (NOLOCK)
-	       INNER JOIN ShippingContainerDetail CTD WITH (NOLOCK) 
+	  FROM DeliveryBackOffice.dbo.ShippingContainer CT WITH (NOLOCK)
+	       INNER JOIN DeliveryBackOffice.dbo.ShippingContainerDetail CTD WITH (NOLOCK) 
               ON CT.IdContainer = CTD.IdContainer
-	       INNER JOIN DeliveryOrderPiece DOP WITH (NOLOCK)
+	       INNER JOIN DeliveryBackOffice.dbo.DeliveryOrderPiece DOP WITH (NOLOCK)
 		   ON CTD.GuideSerie = DOP.GuideSerie
 	   AND CTD.GuideNumber = DOP.GuideNumber
 	 WHERE CT.IdStatusContainer = @StatusContainerCreated
--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_get_validate_guides_pickup.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_get_validate_guides_pickup.sql
@@ -1,21 +1,14 @@
-﻿
--- =============================================
--- Author:		<César,Aquino>
--- Create date: <2021-02-15>
--- Description:	<Verifica si existen guias
---				 si estan en estado 15 (generado) o 1(solicitado)
---               Si no estan asignadas a otra recolección (IdPickup) >
--- =============================================
--- =============================================
--- Author:		<Edelman, Vásquez>
--- Create date: <2023-03-10>
--- Description:	<Al procesar guías en proceso de recolección desde la CourierApp, si durante el proceso de verificación de montos se detecta una guía en estado terminal, debe impedir el proceso indicando las guías y los estados de estas.>
--- =============================================
--- =============================================  
--- Mofified:    <Brandon, Pedroza>  
--- Create date: <2025-01-13>  
--- Description: <Contenerizacion guias - se agrega parametro para buscar guias de un contenedor asociado o referencia>  
--- =============================================  
+﻿/* =================================================
+   SP:        [dbo].[spws_get_validate_guides_pickup]
+   Propósito: <Verifica si existen guias si estan en estado 15 (generado) o 1(solicitado) Si no estan asignadas a otra recolección (IdPickup) >
+   Autor:     Cesar Aquino
+   Historia:  <>
+   Fecha:     <2021-02-15>
+   === CHANGELOG ============================
+2023-03-10 | Historia/épica: <> | Autor: Edelman Vasquez | <Al procesar guías en proceso de recolección desde la CourierApp, si durante el proceso de verificación de montos se detecta una guía en estado terminal, debe impedir el proceso indicando las guías y los estados de estas.>
+2025-01-13 | Historia/épica: <> | Autor: Brandon Pedroza | <Contenerizacion guias - se agrega parametro para buscar guias de un contenedor asociado o referencia>  
+2026-02-17 | Historia/épica: <FDAPI-5552> | Autor: Erick Hernandez | 
+=========================================== */
 CREATE PROCEDURE [dbo].[spws_get_validate_guides_pickup]
     -- Add the parameters for the stored procedure here
     @InGuides NVARCHAR(MAX) = 'FD138515,FD138513,FD13852,FD138514,FD138545,FD135539',
@@ -28,16 +21,6 @@ AS
 BEGIN
 
     SET NOCOUNT ON;
-	
-	--DECLARE @CountryFind TABLE (
-	--	IdCountry varchar(2)
-	--);
-	--INSERT INTO @CountryFind
-	--exec GetCountryOfPickupService @IdPickup,@Token
-	--DECLARE @IDCOUNTRYSERVICE varchar(2) =(SELECT IdCountry FROM @CountryFind)
-	
-	
-
 
     BEGIN TRY
 
@@ -49,7 +32,7 @@ BEGIN
         DECLARE @StatusContainerPickUp INT;
 
 	    SET @StatusContainerPickUp = (SELECT IdCatStatus
-									    FROM CatShipContainerStatus WITH(NOLOCK)
+									    FROM DeliveryBackOffice.dbo.CatShipContainerStatus WITH(NOLOCK)
 									    WHERE [Name]= 'Creado');
 
 			 CREATE TABLE #listGuides
@@ -68,8 +51,8 @@ BEGIN
 			            DO.Ticket_Number,
 			            ROW_NUMBER() OVER (PARTITION BY DO.Ticket_Number ORDER BY DOP.GuideSerie DESC, DOP.GuideNumber DESC) AS RowNum,
 			            DO.DateCreated
-		        FROM DeliveryOrder DO WITH (NOLOCK)
-		        INNER JOIN DeliveryOrderPiece DOP WITH(NOLOCK)
+		        FROM DeliveryBackOffice.dbo.DeliveryOrder DO WITH (NOLOCK)
+		        INNER JOIN DeliveryBackOffice.dbo.DeliveryOrderPiece DOP WITH(NOLOCK)
 			        ON DO.Guide_Serie = DOP.GuideSerie
 			        AND DO.Guide_Number = DOP.GuideNumber
 		        WHERE DO.Ticket_Number IN (SELECT ReferenceGuide FROM @ReferencesGuide WHERE ReferenceGuide NOT IN ('','0'))
@@ -96,10 +79,10 @@ BEGIN
 			    DOP.NoPiece, 
 			    '',
 			    1
-	    FROM ShippingContainer CT WITH (NOLOCK)
-	    INNER JOIN ShippingContainerDetail CTD WITH (NOLOCK)
+	    FROM DeliveryBackOffice.dbo.ShippingContainer CT WITH (NOLOCK)
+	    INNER JOIN DeliveryBackOffice.dbo.ShippingContainerDetail CTD WITH (NOLOCK)
 		    ON CT.IdContainer = CTD.IdContainer
-	    INNER JOIN DeliveryOrderPiece DOP WITH (NOLOCK)
+	    INNER JOIN DeliveryBackOffice.dbo.DeliveryOrderPiece DOP WITH (NOLOCK)
 		    ON CTD.GuideSerie = DOP.GuideSerie
 		    AND CTD.GuideNumber = DOP.GuideNumber
 	    WHERE CT.IdStatusContainer = @StatusContainerPickUp
@@ -124,11 +107,8 @@ BEGIN
 		CREATE NONCLUSTERED INDEX IX_listGuides_Pickup
             ON #listGuides (ItemSerie, ItemNumber);
 
-	
-        -- select * from #listGuides
-
         --Se inserta log de cambio de recolección a un servicio
-        INSERT INTO ServicePickupLog
+        INSERT INTO DeliveryBackOffice.dbo.ServicePickupLog
         (
             GuideSerie,
             GuideNumber,
@@ -151,34 +131,34 @@ BEGIN
                NULL,
                NULL
         FROM #listGuides g
-            INNER JOIN DeliveryOrderPaymentDetail dopd WITH(NOLOCK)
+            INNER JOIN DeliveryBackOffice.dbo.DeliveryOrderPaymentDetail dopd WITH(NOLOCK)
                 ON dopd.GuideSerie = g.ItemSerie
                    AND dopd.GuideNumber = g.ItemNumber
-            INNER JOIN DeliveryOrder do WITH(NOLOCK)
+            INNER JOIN DeliveryBackOffice.dbo.DeliveryOrder do WITH(NOLOCK)
                 ON do.Guide_Serie = g.ItemSerie
                    AND do.Guide_Number = g.ItemNumber
         WHERE COALESCE(dopd.IdHeaderRecolection, 0) <> @IdPickup
-              AND do.StatusOrderId IN ( 16, 15, 1,21,20, 10 );
+              AND do.StatusOrderId IN ( 16, 15, 1,21,20, 10, 50);
 
         --Se asignan los servicios a la nueva recolección
         UPDATE dopd
         SET dopd.IdHeaderRecolection = @IdPickup
-        FROM DeliveryOrderPaymentDetail dopd WITH(NOLOCK)
+        FROM DeliveryBackOffice.dbo.DeliveryOrderPaymentDetail dopd WITH(NOLOCK)
             INNER JOIN #listGuides g
                 ON g.ItemSerie = dopd.GuideSerie
                    AND g.ItemNumber = dopd.GuideNumber
-            INNER JOIN DeliveryOrder do WITH(NOLOCK)
+            INNER JOIN DeliveryBackOffice.dbo.DeliveryOrder do WITH(NOLOCK)
                 ON do.Guide_Serie = g.ItemSerie
                    AND do.Guide_Number = g.ItemNumber
         WHERE COALESCE(dopd.IdHeaderRecolection, 0) <> @IdPickup
-              AND do.StatusOrderId IN ( 16, 15, 1,21,20, 10 );
+              AND do.StatusOrderId IN ( 16, 15, 1,21,20, 10, 50);
 
         SELECT DISTINCT
                lst.ItemSerie,
                lst.ItemNumber,
                ISNULL(dr.Guide_Number, 0) exist,
                IIF(ISNULL(pyt.IdHeaderRecolection, 0) = @IdPickup, 1, IIF(ISNULL(pyt.IdHeaderRecolection, 0) = 0, 1, 0)) pik,
-               IIF(dr.StatusOrderId IN ( 16, 15, 1,21,20, 10 ), 1, 0) status,
+               IIF(dr.StatusOrderId IN ( 16, 15, 1,21,20, 10, 50), 1, 0) status,
                st.OrderDescription,
 			   --IIF(DR.SenderCountryId=@IDCOUNTRYSERVICE,1,0) samecountry,
                1 samecountry,
@@ -187,19 +167,19 @@ BEGIN
         --, pyt.IdHeaderRecolection
         INTO #ErrorGuides
         FROM #listGuides lst
-            LEFT JOIN dbo.DeliveryOrder dr WITH(NOLOCK)
+            LEFT JOIN DeliveryBackOffice.dbo.DeliveryOrder dr WITH(NOLOCK)
                 ON dr.Guide_Serie = lst.ItemSerie
                    AND dr.Guide_Number = lst.ItemNumber
-            LEFT JOIN dbo.DeliveryOrderPaymentDetail pyt WITH(NOLOCK)
+            LEFT JOIN DeliveryBackOffice.dbo.DeliveryOrderPaymentDetail pyt WITH(NOLOCK)
                 ON pyt.GuideSerie = dr.Guide_Serie
                    AND pyt.GuideNumber = dr.Guide_Number
-            LEFT JOIN dbo.StatusOrder st WITH(NOLOCK)
+            LEFT JOIN DeliveryBackOffice.dbo.StatusOrder st WITH(NOLOCK)
                 ON st.StatusOrderId = dr.StatusOrderId
 				WHERE
 	      dr.StatusOrderId IN ( SELECT
 									SO.[StatusOrderId]
 								FROM
-									[dbo].[StatusOrder] SO  WITH(NOLOCK)
+									[DeliveryBackOffice].[dbo].[StatusOrder] SO  WITH(NOLOCK)
 								WHERE
 									[CatCheckpointTypeId] = 3 And SO.RowStatus =1
 							) 
@@ -210,8 +190,6 @@ BEGIN
 
 		CREATE NONCLUSTERED INDEX IX_ErrorGuides_status
             ON #ErrorGuides (status);
-
-        --select * from #ErrorGuides
 
         SELECT CONCAT(er.ItemSerie, er.ItemNumber) Guide,
 			(
