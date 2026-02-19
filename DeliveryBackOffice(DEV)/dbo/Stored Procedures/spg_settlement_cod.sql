c086288d9c2108de30f3ea9a77632d357882a219
EdelmanV <edelman.vasquez@forzalatam.com>
2026-02-19 09:21:29 -0600

    #Agregar diferentes métodos de pagos

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_settlement_cod.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_settlement_cod.sql
@@ -11,6 +11,11 @@
 -- Create date: <2025-10-17>
 -- Description:	<Se agrega campo TotalVouchers para mostrar el total de vouchers Efectibox aplicados al manifiesto de liquidación.>
 -- =============================================
+-- =============================================
+-- Author:		<Edelman>
+-- Create date: <2026-02-19>
+-- Description:	<Agregar flujo de diferentes medios de pagos>
+-- =============================================
 CREATE PROCEDURE [dbo].[spg_settlement_cod]
 		@IdManifest INT
 AS
@@ -21,6 +26,7 @@ BEGIN
 	DECLARE @AmountDeposit DECIMAL(8,2) = 0;
 	DECLARE @AmountTotal DECIMAL(8,2) = 0;
 	DECLARE @TotalVouchers DECIMAL(19,4) = 0;
+	DECLARE @TotalCardCollect DECIMAL(19,4) = 0;
 	DECLARE @TotalTransfer DECIMAL(19,4) = 0;
 	DECLARE @TotalZigi DECIMAL(19,4) = 0;
 
@@ -37,9 +43,37 @@ BEGIN
 		AND dsd.Guide_Discharged = 1  -- guía liquidada vía COD
 		AND dsd.RowStatus = 1
 	)
---Monto liquidado con Transferencia
+	--Monto liquidado con tarjeta
+	SET @TotalCardCollect =(SELECT
+								 SUM(
+									CASE 
+										WHEN E.IdTypeOfMoneyCOLLECT = 2 AND E.IdTypeOfMoneyCOD IS NULL
+											THEN ISNULL(C.PriceShippment,0)
+										ELSE 0 
+									END
+								) 
+							  + SUM(
+									CASE 
+										WHEN E.IdTypeOfMoneyCOD <> 2 AND E.IdTypeOfMoneyCOLLECT = 2 
+											THEN ISNULL(C.PriceShippment,0)
+										ELSE 0
+									END
+								) 
+								AS Total
+							FROM  [DeliveryBackOffice].[dbo].[DeliveryOrderBySettlement] A WITH (NOLOCK)
+							INNER JOIN [DeliveryBackOffice].[dbo].[DeliverySettlementDetail] B WITH (NOLOCK)
+								ON A.ID = B.ID_DeliveryOrderBySettlement
+							INNER JOIN [DeliveryBackOffice].[dbo].[DeliveryOrder] C WITH(NOLOCK)
+								ON B.Guide_Serie = C.Guide_Serie AND B.Guide_number = C.Guide_number
+							INNER JOIN [DeliveryBackOffice].[dbo].[Cost] D WITH(NOLOCK)
+								ON D.GuideSerie = C.Guide_Serie AND D.GuideNumber = C.Guide_number
+							INNER JOIN [DeliveryBackOffice].[dbo].[CostDetail] E WITH(NOLOCK)
+								ON D.IdCost = E.IdCost
+							WHERE A.ID  = @IdManifest
+	);
+	--Monto liquidado con Transferencia
 	SET @TotalTransfer =(
-				SELECT 
+				SELECT
 					SUM(
 						CASE 
 							WHEN E.IdTypeOfMoneyCOD = 11 AND E.IdTypeOfMoneyCOLLECT = 11
@@ -119,8 +153,7 @@ BEGIN
 						  FROM DeliveryBackOffice.dbo.RelDepositManifest WITH(NOLOCK)
 						  WHERE DeliveryOrderBySettlementId = @IdManifest AND RowStatus = 1);
 	--Monto Total
-	SET @AmountTotal = ISNULL(@AmountMoney,0) + ISNULL(@AmountDeposit,0) + ISNULL(@TotalTransfer,0) + ISNULL(@TotalZigi,0);
-	
+	SET @AmountTotal = @AmountMoney + @AmountDeposit + @TotalTransfer + @TotalZigi + @TotalCardCollect;
 
 	-- Calcular el total de vouchers Efectibox aplicados al manifiesto
 	SET @TotalVouchers = (
