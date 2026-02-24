045b7cfe49e332e85c48f0b15a90941ab494646e
erickguerrafd <erick.guerra@forzadelivery.com>
2026-02-24 11:57:22 -0600

    FDAPI-5609 Worker 2026-02-24 Erick Guerra UPDATE Actualización estructura de tabla ZPLFilesStatusLog

--- DeliveryBackOffice(DEV)/dbo/Tables/ZPLFilesStatusLog.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/ZPLFilesStatusLog.sql
@@ -5,10 +5,13 @@ CREATE TABLE ZPLFilesStatusLog (
     [FileName]              NVARCHAR(100) NOT NULL,
     [IsDeleted]             BIT NOT NULL DEFAULT 0,
     [IsProcessed]           BIT NOT NULL DEFAULT 0,
-    [TokenCreated]          NVARCHAR(50) NOT NULL,
+    [JobId]                 NVARCHAR(75) NOT NULL DEFAULT '',
+    [Guide]                 NVARCHAR(75) NOT NULL DEFAULT '',
+    [DateCreatedQueue]      DATETIME NOT NULL DEFAULT SYSDATETIME(),
+    [TokenCreated]          NVARCHAR(50) NOT NULL DEFAULT '',
     [DateCreated]           DATETIME NOT NULL DEFAULT SYSDATETIME(),
     [TokenUpdated]          NVARCHAR(50) NULL,
-    [DateUpdated]           DATETIME2(0) NULL,
+    [DateUpdated]           DATETIME NULL,
     CONSTRAINT FK_ZPLFilesStatusLog_Customer
         FOREIGN KEY (IdCustomer)
         REFERENCES Customer(IdCustomer)
@@ -31,6 +34,15 @@ EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Fue eliminado 
 GO
 
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Ya fue procesado en S3' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ZPLFilesStatusLog', @level2type=N'COLUMN',@level2name=N'IsProcessed'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Identificador de Job' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ZPLFilesStatusLog', @level2type=N'COLUMN',@level2name=N'JobId'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Número de guía' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ZPLFilesStatusLog', @level2type=N'COLUMN',@level2name=N'Guide'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Ingreso a cola SQS' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ZPLFilesStatusLog', @level2type=N'COLUMN',@level2name=N'DateCreatedQueue'
 GO
 
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Token de creación' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ZPLFilesStatusLog', @level2type=N'COLUMN',@level2name=N'TokenCreated'
