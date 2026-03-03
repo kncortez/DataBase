7d11d2ac19940fc8d30ba49e68dd1f688fbd01e0
JuanRamirezFD <juan.ramirez@forzadelivery.com>
2026-03-03 11:28:33 -0600

    FDAPI-5719: Remover funcion de doble condicion

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/get_TownshipByProvince.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/get_TownshipByProvince.sql
@@ -3,15 +3,17 @@
 -- Create date: <2025-07-28>
 -- Description: <Se agrega filtro por pais, por defecto GT>
 -- =============================================
+-- =============================================
+-- Author:      <Daniel, Ramirez>
+-- Create date: <2026-03-03>
+-- Description: <Remover función doble condición>
+-- =============================================
 CREATE PROCEDURE [dbo].[get_TownshipByProvince]
 (
- @IdCountry  AS NVARCHAR(2) = 'GT',
- @IdProvince AS INT = 0
+ @IdCountry  AS NVARCHAR(2) = 'GT'
 )
 AS
 BEGIN
-    IF(@IdProvince = 0)
-    BEGIN
         SELECT ts.IdTownship,
                ts.TownshipName,
                ts.IdProvince 
@@ -21,18 +23,4 @@ BEGIN
          WHERE TownshipStatus = 1
            AND pv.ProvinceStatus = 1
            AND pv.IdCountry = @IdCountry
-    END
-    ELSE
-    BEGIN
-        SELECT ts.IdTownship,
-               ts.TownshipName,
-               ts.IdProvince 
-          FROM Township ts WITH(NOLOCK)
-               INNER JOIN Province pv WITH(NOLOCK)
-               ON pv.IdProvince = ts.IdProvince
-         WHERE TownshipStatus = 1
-           AND pv.ProvinceStatus = 1
-           AND pv.IdCountry = @IdCountry
-           AND ts.IdProvince = @IdProvince
-    END
 END
