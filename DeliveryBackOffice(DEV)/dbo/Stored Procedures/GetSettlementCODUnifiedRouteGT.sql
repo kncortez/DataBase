e48af3da700dabfd16c379db06765ca19d3eff5d
EdelmanV <edelman.vasquez@forzalatam.com>
2026-03-03 18:10:25 -0600

    #Flujos para pises diferentes a GT

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetSettlementCODUnifiedRouteGT.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetSettlementCODUnifiedRouteGT.sql
@@ -12,7 +12,7 @@
 -- Create date: <2025-10-02>
 -- Description:	<Obtener DPI del piloto y filtrar por día actual los manifiestos liquidados y pendientes>
 -- =============================================
-CREATE PROCEDURE [dbo].[GetSettlementCODUnifiedRoute] @IdRoute INT
+CREATE PROCEDURE [dbo].[GetSettlementCODUnifiedRouteGT] @IdRoute INT
 AS
 BEGIN
     SET NOCOUNT ON;
@@ -65,8 +65,8 @@ BEGIN
             INNER JOIN DeliveryBackOffice.dbo.DeliverySettlementDetail dsd WITH (NOLOCK)
                 ON dsd.ID_DeliveryOrderBySettlement = dbs.ID
             INNER JOIN dbo.DeliveryOrder DOR WITH (NOLOCK)
-                ON DOR.Guide_Serie = dsd.Guide_Serie
-                   AND DOR.Guide_Number = dsd.Guide_Number
+                ON DOR.Guide_Serie = dsd.Guide_Serie AND 
+                   DOR.Guide_Number = dsd.Guide_Number
             LEFT JOIN DeliveryBackOffice.dbo.ManifestSettlementIncidence msi WITH (NOLOCK)
                 ON dbs.ID = msi.ManifestNumber
             LEFT JOIN DeliveryBackOffice.dbo.CatStation cs
@@ -106,7 +106,7 @@ BEGIN
             INNER JOIN DeliveryBackOffice.dbo.DeliverySettlementDetail dsd WITH (NOLOCK)
                 ON dsd.ID_DeliveryOrderBySettlement = dbs.ID
             INNER JOIN dbo.DeliveryOrder DOR WITH (NOLOCK)
-                ON DOR.Guide_Serie = dsd.Guide_Serie
+                ON DOR.Guide_Serie = dsd.Guide_Serie 
                    AND DOR.Guide_Number = dsd.Guide_Number
             LEFT JOIN DeliveryBackOffice.dbo.ManifestSettlementIncidence msi WITH (NOLOCK)
                 ON dbs.ID = msi.ManifestNumber
@@ -128,16 +128,6 @@ BEGIN
                   )
               )
     ) AS s;
-
-	--quitar guias que hayan sido pagadas con zigi
-	DELETE GF
-	FROM @GuidesFound GF
-	LEFT JOIN PaymentZigi PZ
-	ON PZ.GuideSerie = GF.Guide_Serie
-		  AND PZ.GuideNumber = GF.Guide_Number
-		WHERE PZ.GuideSerie = GF.Guide_Serie
-		  AND PZ.GuideNumber = GF.Guide_Number
-		  AND (PZ.ZigiLinkStatus = 'PAID'OR PZ.AuthorizationNumberByUser IS NOT NULL)
 
     SELECT DISTINCT
            dbs.ID,
@@ -189,20 +179,21 @@ BEGIN
             0
                   )
            ) AS Delivered,
-           CAST(IIF(do.IsCollect = 'TRUE',
-                    IIF(do.IsLastMileReturn = 1,
-                        ISNULL(   CASE
-                                      WHEN ISNULL(cdp.IdConditionOfPayment, 1) > 1 THEN
-                                          0
-                                      ELSE
+           --CAST(IIF(do.IsCollect = 'TRUE',
+           --         IIF(do.IsLastMileReturn = 1,
+           --             ISNULL(   CASE
+           --                           WHEN ISNULL(cdp.IdConditionOfPayment, 1) > 1 THEN
+           --                               0
+           --                           ELSE
                                           do.PriceShippment
-                                  END,
-                                  0
-                              ),
+                              --    END,
+                              --    0
+                              --),
 
                         --sino es una devolución que hago?
-                        IIF(A1.ReasonCode = '00', 0, do.PriceShippment)),
-                    0) AS DECIMAL(18, 2)) AS Price,
+                    --    IIF(A1.ReasonCode = '00', do.PriceShippment, do.PriceShippment)),
+                    --0) AS DECIMAL(18, 2))
+					AS Price,
            CAST(ISNULL(
                           (CASE
                                WHEN [do].[IsLastMileReturn] = 1 THEN
@@ -317,7 +308,7 @@ BEGIN
                        0
                    ELSE
                        do.Collect_OnDelivery
-               END > 0
+               END >= 0
               )
               OR
               (
@@ -337,7 +328,7 @@ BEGIN
                                                                                      0
                                                                                  ELSE
                                                                                      do.Collect_OnDelivery
-                                                                             END > 0
+                                                                             END >= 0
               )
           )
           AND
@@ -384,7 +375,7 @@ BEGIN
 
     END;
 
-     SELECT SUM(Total) - ISNULL(rdm.TotalApplied,0) AS COD_Manifest
+     SELECT  SUM(Total) - ISNULL(rdm.TotalApplied,0) AS COD_Manifest
     FROM @GuidesDetail gd
 	LEFT JOIN (
 			SELECT 
@@ -422,24 +413,28 @@ BEGIN
 				ON gd.id = r.DeliveryOrderBySettlementId
 		)
 		SELECT
-			id,
-			Guide,
-			GuideSerie,
-			GuideNumber,
-			Delivered,
-			Price,
-			COD,
+			A.id,
+			A.Guide,
+			A.GuideSerie,
+			A.GuideNumber,
+			A.Delivered,
+			A.Price,
+			A.COD,
 			CASE
-				WHEN TotalApplied <= ISNULL(RunningBefore, 0) THEN Total
+				WHEN TotalApplied <= ISNULL(RunningBefore, 0)   THEN (ISNULL(Price,0) + ISNULL(COD,0))
 				WHEN TotalApplied >= RunningTotal THEN 0
 				ELSE (RunningTotal - TotalApplied)
 			END AS Total,
-			FEL,
-			StatusOrderId,
-			OrderDescription,
-			StatusOrderValid,
-			DescriptionStatusOrderValid
-		FROM Applied
+			A.FEL,
+			A.StatusOrderId,
+			A.OrderDescription,
+			A.StatusOrderValid,
+			A.DescriptionStatusOrderValid
+		FROM Applied A
+		INNER JOIN [DeliveryBackOffice].[dbo].[Cost] C WITH (NOLOCK)
+        ON C.GuideSerie = A.GuideSerie AND C.GuideNumber = A.GuideNumber
+        LEFT JOIN [DeliveryBackOffice].[dbo].[CostDetail] CD WITH (NOLOCK)
+        ON CD.IdCost = C.IdCost
 		ORDER BY id ASC;
 
 END;
--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_GetTransactionBySettlementId.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_GetTransactionBySettlementId.sql
@@ -1,99 +1,133 @@
 -- =============================================
--- Author:		<Edelman>
--- Create date: <2026-01-16>
--- Description:	<obtener guías pagadas por transferencia en entrega POD>
+-- Author:        <Edelman>
+-- Create date:   <2026-01-16>
+-- Description:   obtener guías pagadas por transferencia en entrega POD
 -- =============================================
 CREATE PROCEDURE [dbo].[SPHD_GetTransactionBySettlementId]
 (
-@ID_DeliveryOrderBySettlement INT
+    @ID_DeliveryOrderBySettlement INT,
+    @Country VARCHAR(5)
 )
 AS
 BEGIN
-SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
-;WITH BaseData AS
-(
+    --Si no es GT, devolver estructura vacía
+    IF @Country <> 'GT'
+    BEGIN
+        SELECT 
+            CAST(NULL AS VARCHAR(20))  AS PaymentType,
+            CAST(NULL AS VARCHAR(100)) AS Transaccion,
+            CAST(NULL AS VARCHAR(50))  AS Voucher,
+            CAST(NULL AS DECIMAL(18,2)) AS TotalAmount
+        WHERE 1 = 0;
+
+        RETURN;
+    END;
+
+    ;WITH BaseData AS
+    (
+        SELECT
+            CD.Voucher,
+            C.ProductNumber,
+            CD.IdTypeOfMoneyCOD,
+            CD.IdTypeOfMoneyCollect,
+            PZ.ZigiTransactionId,
+            ISNULL(DO.PriceShippment,0)      AS PriceShippment,
+            ISNULL(DO.Collect_OnDelivery,0)  AS Collect_OnDelivery,
+            DSD.ID_DeliveryOrderBySettlement
+        FROM [DeliveryBackOffice].[dbo].[DeliverySettlementDetail] DSD WITH (NOLOCK)
+        INNER JOIN [DeliveryBackOffice].[dbo].[Cost] C WITH (NOLOCK)
+            ON C.GuideSerie = DSD.Guide_Serie 
+           AND C.GuideNumber = DSD.Guide_Number
+        LEFT JOIN [DeliveryBackOffice].[dbo].[CostDetail] CD WITH (NOLOCK)
+            ON CD.IdCost = C.IdCost
+        LEFT JOIN [DeliveryBackOffice].[dbo].[PaymentZigi] PZ WITH (NOLOCK)
+            ON PZ.GuideSerie  = DSD.Guide_Serie 
+           AND PZ.GuideNumber = DSD.Guide_Number
+        LEFT JOIN [DeliveryBackOffice].[dbo].[PaymentZigiMulti] PZM WITH (NOLOCK)
+            ON PZM.Id_PaymentZigi = PZ.ZigiPaymentId 
+        LEFT JOIN [DeliveryBackOffice].[dbo].[DeliveryOrder] DO WITH (NOLOCK)
+            ON DO.Guide_Serie = DSD.Guide_Serie 
+           AND DO.Guide_Number = DSD.Guide_Number
+        WHERE DSD.ID_DeliveryOrderBySettlement = @ID_DeliveryOrderBySettlement
+        AND DO.IsLastMileReturn = 0
+    )
+
+    -- BLOQUE 1: COD (Transferencia / Zigi)
     SELECT
-        CD.Voucher,
-        C.ProductNumber,
-        CD.IdTypeOfMoneyCOD,
-        CD.IdTypeOfMoneyCollect,
-        PZ.ZigiTransactionId,
-        ISNULL(DO.PriceShippment,0)      AS PriceShippment,
-        ISNULL(DO.Collect_OnDelivery,0)  AS Collect_OnDelivery,
-        DSD.ID_DeliveryOrderBySettlement
-    FROM [DeliveryBackOffice].[dbo].[DeliverySettlementDetail] DSD WITH (NOLOCK)
-    INNER JOIN [DeliveryBackOffice].[dbo].[Cost] C WITH (NOLOCK)
-        ON C.GuideSerie = DSD.Guide_Serie 
-       AND C.GuideNumber = DSD.Guide_Number
-    LEFT JOIN [DeliveryBackOffice].[dbo].[CostDetail] CD WITH (NOLOCK)
-        ON CD.IdCost = C.IdCost
-    LEFT JOIN [DeliveryBackOffice].[dbo].[PaymentZigi] PZ WITH (NOLOCK)
-        ON PZ.GuideSerie  = DSD.Guide_Serie 
-       AND PZ.GuideNumber = DSD.Guide_Number
-    LEFT JOIN [DeliveryBackOffice].[dbo].[PaymentZigiMulti] PZM WITH (NOLOCK)
-        ON  PZM.Id_PaymentZigi  = PZ.ZigiPaymentId 
-    LEFT JOIN [DeliveryBackOffice].[dbo].[DeliveryOrder] DO WITH (NOLOCK)
-        ON DO.Guide_Serie = DSD.Guide_Serie 
-       AND DO.Guide_Number = DSD.Guide_Number
-    WHERE DSD.ID_DeliveryOrderBySettlement = @ID_DeliveryOrderBySettlement
-    AND DO.IsLastMileReturn = 0
-)
+        CASE 
+            WHEN IdTypeOfMoneyCOD = 11 THEN 'Transferencia'
+            WHEN IdTypeOfMoneyCOD = 10 THEN 'Zigi'
+            WHEN IdTypeOfMoneyCollect = 11 THEN 'Transferencia'
+            WHEN IdTypeOfMoneyCollect = 10 THEN 'Zigi'
+        END AS PaymentType,
 
---BLOQUE 1: COD (Transferencia / Zigi)
-					SELECT
-					  CASE 
-							WHEN IdTypeOfMoneyCOD = 11 THEN 'Transferencia'
-							WHEN IdTypeOfMoneyCOD = 10 THEN 'Zigi'
-							WHEN IdTypeOfMoneyCollect = 11 THEN 'Transferencia'
-							WHEN IdTypeOfMoneyCollect = 10 THEN 'Zigi'
-						END AS PaymentType,
-						Voucher + ProductNumber AS Transaccion,
-						CASE 
-							WHEN IdTypeOfMoneyCOD = 11 THEN Voucher
-							WHEN IdTypeOfMoneyCOD = 10 THEN ZigiTransactionId
-							WHEN IdTypeOfMoneyCollect = 11 THEN Voucher
-							WHEN IdTypeOfMoneyCollect = 10 THEN ZigiTransactionId
-							ELSE ''
-						END AS Voucher,
-						CASE
-							WHEN IdTypeOfMoneyCOD = 10 AND IdTypeOfMoneyCollect = 10 
-								THEN ISNULL(PriceShippment,0) + ISNULL(Collect_OnDelivery,0)
-							WHEN IdTypeOfMoneyCOD = 11 AND IdTypeOfMoneyCollect = 11 
-								THEN ISNULL(PriceShippment,0) + ISNULL(Collect_OnDelivery,0)
-							WHEN IdTypeOfMoneyCOD = 11  AND IdTypeOfMoneyCollect IS NULL
-								THEN ISNULL(PriceShippment,0) + ISNULL(Collect_OnDelivery,0)
-							WHEN IdTypeOfMoneyCOD = 10 AND IdTypeOfMoneyCollect IS NULL
-								THEN ISNULL(PriceShippment,0) + ISNULL(Collect_OnDelivery,0)
-							WHEN IdTypeOfMoneyCollect = 11  AND IdTypeOfMoneyCOD IS NULL
-								THEN ISNULL(PriceShippment,0) + ISNULL(Collect_OnDelivery,0)
-							WHEN IdTypeOfMoneyCollect = 10 AND IdTypeOfMoneyCOD IS NULL
-								THEN ISNULL(PriceShippment,0) + ISNULL(Collect_OnDelivery,0)
-							WHEN IdTypeOfMoneyCOD = 11  AND IdTypeOfMoneyCollect <> 11
-							    THEN ISNULL(Collect_OnDelivery,0)
-							WHEN IdTypeOfMoneyCOD <> 11  AND IdTypeOfMoneyCollect = 11
-							    THEN ISNULL(PriceShippment,0) 
-						END AS TotalAmount
-					FROM BaseData
-					WHERE IdTypeOfMoneyCOD IN (10,11)
-					OR IdTypeOfMoneyCollect  IN (10,11)
-			UNION ALL
-			-- BLOQUE 2: Tarjeta (Collect = 2)
-			SELECT
-			    'Tarjeta',
-				Voucher + ProductNumber,
-				'',
-				CASE 
-				   WHEN  IdTypeOfMoneyCOD = 2 AND IdTypeOfMoneyCollect = 2  THEN
-				   ISNULL(PriceShippment,0) + ISNULL(Collect_OnDelivery,0)
-				   WHEN  IdTypeOfMoneyCOD = 2 AND IdTypeOfMoneyCollect <> 2  THEN
-				     ISNULL(Collect_OnDelivery,0)
-				   WHEN  IdTypeOfMoneyCOD <> 2 AND IdTypeOfMoneyCollect = 2  THEN
-					 ISNULL(PriceShippment,0)
-				    WHEN  IdTypeOfMoneyCOD IS NULL AND IdTypeOfMoneyCollect = 2  THEN
-					 ISNULL(PriceShippment,0)
-				END 
-			FROM BaseData
-			WHERE IdTypeOfMoneyCollect = 2
-			
+        Voucher + ProductNumber AS Transaccion,
+
+        CASE 
+            WHEN IdTypeOfMoneyCOD = 11 THEN Voucher
+            WHEN IdTypeOfMoneyCOD = 10 THEN ZigiTransactionId
+            WHEN IdTypeOfMoneyCollect = 11 THEN Voucher
+            WHEN IdTypeOfMoneyCollect = 10 THEN ZigiTransactionId
+            ELSE ''
+        END AS Voucher,
+
+        CASE
+            WHEN IdTypeOfMoneyCOD IN (10,11) 
+                 AND IdTypeOfMoneyCollect IN (10,11)
+                THEN ISNULL(PriceShippment,0) + ISNULL(Collect_OnDelivery,0)
+
+            WHEN IdTypeOfMoneyCOD IN (10,11) 
+                 AND IdTypeOfMoneyCollect IS NULL
+                THEN ISNULL(PriceShippment,0) + ISNULL(Collect_OnDelivery,0)
+
+            WHEN IdTypeOfMoneyCollect IN (10,11) 
+                 AND IdTypeOfMoneyCOD IS NULL
+                THEN ISNULL(PriceShippment,0) + ISNULL(Collect_OnDelivery,0)
+
+            WHEN IdTypeOfMoneyCOD IN (10,11) 
+                 AND IdTypeOfMoneyCollect NOT IN (10,11)
+                THEN ISNULL(Collect_OnDelivery,0)
+
+            WHEN IdTypeOfMoneyCollect IN (10,11) 
+                 AND IdTypeOfMoneyCOD NOT IN (10,11)
+                THEN ISNULL(PriceShippment,0)
+        END AS TotalAmount
+
+    FROM BaseData
+    WHERE IdTypeOfMoneyCOD IN (10,11)
+       OR IdTypeOfMoneyCollect IN (10,11)
+
+    UNION ALL
+
+    -- BLOQUE 2: Tarjeta
+    SELECT
+        'Tarjeta',
+
+        Voucher + ProductNumber,
+
+        '',
+
+        CASE 
+            WHEN IdTypeOfMoneyCOD = 2 
+                 AND IdTypeOfMoneyCollect = 2
+                THEN ISNULL(PriceShippment,0) + ISNULL(Collect_OnDelivery,0)
+
+            WHEN IdTypeOfMoneyCOD = 2 
+                 AND IdTypeOfMoneyCollect <> 2
+                THEN ISNULL(Collect_OnDelivery,0)
+
+            WHEN IdTypeOfMoneyCollect = 2 
+                 AND IdTypeOfMoneyCOD <> 2
+                THEN ISNULL(PriceShippment,0)
+
+            WHEN IdTypeOfMoneyCOD IS NULL 
+                 AND IdTypeOfMoneyCollect = 2
+                THEN ISNULL(PriceShippment,0)
+        END
+
+    FROM BaseData
+    WHERE IdTypeOfMoneyCollect = 2
+
 END
