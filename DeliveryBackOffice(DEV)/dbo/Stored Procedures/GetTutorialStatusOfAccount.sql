6b97a3cac3233744e6e4259a817a72aa336b1895
FLG\brenda.nova <brenda.nova@forzadelivery.com>
2026-03-10 10:09:30 -0600

    FDAPI-5759-Trasladar condiciones fijas del INNER JOIN al WHERE

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetTutorialStatusOfAccount.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetTutorialStatusOfAccount.sql
@@ -4,6 +4,10 @@
 -- Create date: <2022-09-05>
 -- Description:	< Obtener si a usuario se le debe desplegar algun tutorial >
 -- =============================================
+-- Modified:    <Brenda, Echeverria>
+-- Create date: <2026-03-04>
+-- Description: < Se paso el filtro del inner join al where >
+-- =============================================
 CREATE PROCEDURE [dbo].[GetTutorialStatusOfAccount]
 	@AccountId BIGINT
 AS
@@ -12,23 +16,23 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-	SELECT
+SELECT
 		Tut.IdTutorial,
 		Tut.TutorialName,
 		Tut.TutorialDescription
 	FROM
 		[DeliveryBackOffice].[dbo].[TutorialByAccount] TBA WITH(NOLOCK)
-		INNER JOIN
-			[DeliveryBackOffice].[dbo].[Tutorial] Tut WITH(NOLOCK)
-			ON
-				TBA.TutorialId = Tut.IdTutorial
-				AND
-				Tut.RowStatus = 1
+	INNER JOIN
+		[DeliveryBackOffice].[dbo].[Tutorial] Tut WITH(NOLOCK)
+		ON
+			TBA.TutorialId = Tut.IdTutorial				
 	WHERE
 		TBA.AccountId = @AccountId
 		AND
 		TBA.ToDisplay = 1
 		AND
-		TBA.RowStatus = 1;
+		TBA.RowStatus = 1
+		AND
+		Tut.RowStatus = 1;
 
 END
