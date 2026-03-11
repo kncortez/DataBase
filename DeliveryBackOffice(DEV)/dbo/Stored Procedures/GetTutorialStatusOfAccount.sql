527f4957ad8596897a485b7962a61b1c27490590
FLG\brenda.nova <brenda.nova@forzadelivery.com>
2026-03-10 20:40:57 -0600

    FDAPI-5759-ajustar formato SQL, remover comentarios innecesarios, actualizar encaezado segun lineamientos. Por revision.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetTutorialStatusOfAccount.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetTutorialStatusOfAccount.sql
@@ -1,38 +1,32 @@
 ﻿
--- =============================================
--- Author:		<Andres,Ruiz>
--- Create date: <2022-09-05>
--- Description:	< Obtener si a usuario se le debe desplegar algun tutorial >
--- =============================================
--- Modified:    <Brenda, Echeverria>
--- Create date: <2026-03-04>
--- Description: < Se paso el filtro del inner join al where >
--- =============================================
+/* =================================================
+   SP:        [dbo].[GetTutorialStatusOfAccount]
+   Propósito: Obtener si se le debe desplegar algun tutorial al usuario 
+   Autor:     Andres Ruiz
+   Historia:  
+   Fecha:     2022-09-05
+============================================
+=== CHANGELOG ================================
+2025-09-01 | Historia/épica: FDAPI-5759 | Autor: Brenda Echeverria |
+
+=========================================== */
 CREATE PROCEDURE [dbo].[GetTutorialStatusOfAccount]
 	@AccountId BIGINT
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-SELECT
+	SELECT
 		Tut.IdTutorial,
 		Tut.TutorialName,
 		Tut.TutorialDescription
 	FROM
 		[DeliveryBackOffice].[dbo].[TutorialByAccount] TBA WITH(NOLOCK)
-	INNER JOIN
-		[DeliveryBackOffice].[dbo].[Tutorial] Tut WITH(NOLOCK)
-		ON
-			TBA.TutorialId = Tut.IdTutorial				
-	WHERE
-		TBA.AccountId = @AccountId
-		AND
-		TBA.ToDisplay = 1
-		AND
-		TBA.RowStatus = 1
-		AND
-		Tut.RowStatus = 1;
+		INNER JOIN [DeliveryBackOffice].[dbo].[Tutorial] Tut WITH(NOLOCK)
+				ON TBA.TutorialId = Tut.IdTutorial				
+	WHERE TBA.AccountId = @AccountId
+		AND TBA.ToDisplay = 1
+		AND TBA.RowStatus = 1
+		AND Tut.RowStatus = 1;
 
 END
