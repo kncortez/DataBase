709737f4d822b2b759661c12948fe7bed054a96f
hespinoza <hanss.espinoza@forzadelivery.com>
2026-03-13 15:19:49 -0600

    FDAPI-5860: Se crea SP para obtener el idTownship y HeaderCode segun el codigo o nombre enviado por el cliente

---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spws_get_township_by_external_mapping.sql
@@ -0,0 +1,59 @@
+/* =================================================
+   SP:        [dbo].[spws_get_township_by_external_mapping]
+   Propósito: Obtiene el IdTownship y HeaderCode de Forza mapeando el código o nombre enviado por el cliente.
+   Autor:     Hanss Espinoza
+   Historia:  FDAPI-5860
+   Fecha:     2026-03-13
+============================================
+=== CHANGELOG ================================
+
+=========================================== */
+
+CREATE PROCEDURE [dbo].[spws_get_township_by_external_mapping]
+    @pIdCustomer INT,
+    @pExternalId NVARCHAR(200) = NULL,
+    @pExternalName NVARCHAR(200) = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF @pExternalId IS NULL AND @pExternalName IS NULL
+    BEGIN
+        RAISERROR('Debe proporcionar al menos un ID externo o un Nombre externo.', 16, 1);
+        RETURN;
+    END
+
+    -- Bloque 1: Búsqueda por ID Externo (Uso de índice IX_CustomerTownshipMapping_Customer_ExternalId)
+    IF @pExternalId IS NOT NULL
+    BEGIN
+        SELECT
+            t.HeaderCode,
+            CONVERT(VARCHAR, t.IdTownship) AS 'IdTownship',
+            dbo.fnt_String_Escape(ISNULL(t.TownshipName, ''), 'json') AS 'TownshipName'
+        FROM [dbo].[CustomerTownshipMapping] ctm WITH (NOLOCK)
+        INNER JOIN [dbo].[Township] t WITH (NOLOCK)
+            ON t.IdTownship = ctm.IdTownship
+        WHERE ctm.IdCustomer = @pIdCustomer
+          AND ctm.ExternalTownshipId = @pExternalId
+          AND ctm.RowStatus = 1;
+    END
+    -- Bloque 2: Búsqueda por Nombre Externo (Uso de índice IX_CustomerTownshipMapping_Customer_ExternalName)
+    ELSE IF @pExternalName IS NOT NULL
+    BEGIN
+        SELECT
+            t.HeaderCode,
+            CONVERT(VARCHAR, t.IdTownship) AS 'IdTownship',
+            dbo.fnt_String_Escape(ISNULL(t.TownshipName, ''), 'json') AS 'TownshipName'
+        FROM [dbo].[CustomerTownshipMapping] ctm WITH (NOLOCK)
+        INNER JOIN [dbo].[Township] t WITH (NOLOCK)
+            ON t.IdTownship = ctm.IdTownship
+        WHERE ctm.IdCustomer = @pIdCustomer
+          AND ctm.ExternalTownshipName = @pExternalName
+          AND ctm.RowStatus = 1;
+    END
+END
+GO
+
+-- Descripciones Extendidas para el diccionario de datos de SQL Server
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Obtiene el IdTownship y HeaderCode de Forza mapeando el código o nombre enviado por el cliente.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'PROCEDURE', @level1name = N'spws_get_township_by_external_mapping';
+GO
