bdd27ca4b98cbd164544e86f5ffad28e38a3e7f2
erickguerrafd <erick.guerra@forzadelivery.com>
2026-04-01 08:32:52 -0600

    FDAPI-5985 Migración 2026-04-01 Erick Guerra UPDATE Se modifica el Script de actualización sobre Person para incluir Transacción y ejecución de Rollback en caso de error.

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-5985/Script.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-5985/Script.sql
@@ -8,9 +8,24 @@
 *
 * TABLAS AFECTADAS:
 *   - Person
+*
+* NOTA:
+*   - El script incluye transacción automática con ROLLBACK en caso de error
 ***************************************************************************************************/
 
-USE DeliveryBackOffice;
-UPDATE Person
-	SET [PerCountryOrigin] = 'GT'
-	WHERE [PerCountryOrigin] IS NULL;
+BEGIN TRY
+	BEGIN TRANSACTION;
+
+	USE DeliveryBackOffice;
+	UPDATE Person
+		SET [PerCountryOrigin] = 'GT'
+		WHERE [PerCountryOrigin] IS NULL;
+	PRINT 'Cambios aplicados permanentemente.';
+	COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION;
+	PRINT 'Error detectado - Los cambios han sido rechazados.';
+END CATCH;
+
+GO
