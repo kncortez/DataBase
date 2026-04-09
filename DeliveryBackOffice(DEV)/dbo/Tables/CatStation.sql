0e063c1ea59bc342e869a7973b8514f51523ef79
brandon-p <brandon.pedroza@forzadelivery.com>
2026-04-09 10:05:56 -0600

    FDAPI-6028: #se agrega cambio para insertar en inventario cuando la guia es creada en express center se agrega cambio para quitar de inventario en proceso de recoleccion

--- DeliveryBackOffice(DEV)/dbo/Tables/CatStation.sql
+++ DeliveryBackOffice(DEV)/dbo/Tables/CatStation.sql
@@ -10,6 +10,7 @@
     [DateCreated]     DATETIME       NULL,
     [TokenUpdated]    NVARCHAR (50)  NULL,
     [DateUpdated]     DATETIME       NULL,
+    [RackPositionDefault] NVARCHAR(60) NULL, 
     CONSTRAINT [PK_CatStation] PRIMARY KEY CLUSTERED ([IdStation] ASC),
     CONSTRAINT [FK_CatStation_CatCountry] FOREIGN KEY ([CountryId]) REFERENCES [dbo].[CatCountry] ([IdCountry]),
     CONSTRAINT [FK_CatStation_HubLogistics] FOREIGN KEY ([HubLogisticId]) REFERENCES [dbo].[HubLogistics] ([IdHubLogistic]),
@@ -66,3 +67,13 @@ EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Usuario act
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fecha actualización', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'CatStation', @level2type = N'COLUMN', @level2name = N'DateUpdated';
 
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Posición de rack por defecto de la estación',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'CatStation',
+    @level2type = N'COLUMN',
+    @level2name = N'RackPositionDefault'
