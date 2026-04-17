d7ab36ef2284a6726242f57f0fae72cb50899760
Mario Herrarte <mario.herrarte@forzadelivery.com>
2026-04-17 17:06:24 -0600

    FDAPI-6087: Mejoras recomendadas en PR aplicadas

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_DispatchManifesttoDeliveryRoutesDetail.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHD_DispatchManifesttoDeliveryRoutesDetail.sql
@@ -34,20 +34,13 @@ BEGIN
 		DISTINCT
 			do.Guide_Serie,
 			do.Guide_Number
-    FROM 
-		[DeliveryBackOffice].[dbo].DeliveryOrder do WITH(NOLOCK)
-	INNER JOIN 
-		DeliverySettlementDetail dsd WITH(NOLOCK)
-		ON 
-			do.Guide_Serie = dsd.Guide_Serie 
-			AND 
-			do.Guide_Number = dsd.Guide_Number
-			AND
-			do.[IsLastMileReturn] = 1
-			AND 
-			dsd.ID_DeliveryOrderBySettlement = @IdManifest 
-			AND 
-			dsd.RowStatus = 1
+    FROM [DeliveryBackOffice].[dbo].DeliveryOrder do WITH(NOLOCK)
+	INNER JOIN DeliverySettlementDetail dsd WITH(NOLOCK)
+		ON do.Guide_Serie = dsd.Guide_Serie 
+			AND do.Guide_Number = dsd.Guide_Number			
+			AND dsd.ID_DeliveryOrderBySettlement = @IdManifest 
+	WHERE do.[IsLastMileReturn] = 1
+		AND dsd.RowStatus = 1;
 
     DECLARE @ConcatReturnGuides NVARCHAR(MAX) = (
         SELECT STUFF
@@ -190,28 +183,20 @@ BEGIN
 		,do.ReceiverCountryId AS ReceiverCountry
 		,CCU.Symbol
 		,do.Ticket_Number
-	from 
-		[DeliveryBackOffice].[dbo].DeliveryOrder do WITH(NOLOCK)
-	INNER JOIN 
-		DeliverySettlementDetail dsd WITH(NOLOCK)
-		ON 
-			do.Guide_Serie = dsd.Guide_Serie 
-			AND 
-			do.Guide_Number = dsd.Guide_Number
-			AND 
-			dsd.ID_DeliveryOrderBySettlement = @IdManifest 
-			AND 
-			dsd.RowStatus = 1
+	from [DeliveryBackOffice].[dbo].DeliveryOrder do WITH(NOLOCK)
+	INNER JOIN DeliverySettlementDetail dsd WITH(NOLOCK)
+		ON do.Guide_Serie = dsd.Guide_Serie 
+			AND do.Guide_Number = dsd.Guide_Number
+			AND dsd.ID_DeliveryOrderBySettlement = @IdManifest 
 	INNER JOIN Cost CO WITH (NOLOCK)
-	    ON do.Guide_Serie = CO.GuideSerie AND do.Guide_Number = CO.GuideNumber
+	    ON do.Guide_Serie = CO.GuideSerie 
+			AND do.Guide_Number = CO.GuideNumber
 	INNER JOIN CatCurrencyCOD CCU WITH (NOLOCK)
 	    ON ISNULL(CO.ShippingCurrency,@CurrencyGT)= CCU.IdCatCurrencyCOD
-	LEFT JOIN
-		@TempReturnPrice TRP
-		ON
-			TRP.[GuideSerie] = do.[Guide_Serie]
-			AND
-			TRP.[GuideNumber] = do.[Guide_Number]
+	LEFT JOIN @TempReturnPrice TRP
+		ON TRP.[GuideSerie] = do.[Guide_Serie]
+			AND TRP.[GuideNumber] = do.[Guide_Number]
+	WHERE dsd.RowStatus = 1;
 
 	SELECT 
 		GuideOrder
