35dfa181111ec0b2d44081508e60d5be47204ed9
pedromacajol <pedro.macajol@forzadelivery.com>
2026-04-21 14:43:23 -0600

    FDAPI-6057 : AJUSTES DE SP

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_IVE_InfWbSrvFELG4S_Delivery.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spg_IVE_InfWbSrvFELG4S_Delivery.sql
@@ -2,41 +2,38 @@
    SP: spg_IVE_InfWbSrvFELG4S_Delivery
    Propósito: Retorna información de factura para consumo de web service FEL G4S,
               incluyendo impuestos, dirección del emisor y centro de costo dinámico
+   Autor:Luis Fernando Coti Itzep
    Historia:
-   Fecha: 
+   Fecha: 2020-10-06
 ================================================= */
-
 /* === CHANGELOG ============================
 2026-04-13 | Historia/épica: FDAPI-6057  | Autor: Pedro Macajol     | Correccion de centro de costo dinamico.
 2024-08-12 | Historia/épica: (pendiente) | Autor: Cristian Suazo    | Se agrega la dirección del emisor del punto de visita
 2024-06-28 | Historia/épica: (pendiente) | Autor: Daniel Ramirez    | Retorna valor de configuracion para porcentaje de impuesto segun el pais de uso
-2020-10-06 | Historia/épica: (pendiente) | Luis Fernando Coti Itzep | Creación inicial - credenciales consumo web service FEL G4S
 =========================================== */
 
 CREATE PROCEDURE [dbo].[spg_IVE_InfWbSrvFELG4S_Delivery]
 	@VpCodeOfReference as varchar(100)
 AS
 BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
 	DECLARE @country AS VARCHAR(2),
             @taxes   AS VARCHAR(50),
 			@Address AS NVARCHAR(25);
 
-	select @country = CountryId,
+	SELECT @country = CountryId,
 		   @Address = Address
-	from VisitPointClient WITH(NOLOCK)
-	where CodeOfReference = @VpCodeOfReference
+	FROM DeliveryBackOffice.dbo.VisitPointClient WITH(NOLOCK)
+	WHERE CodeOfReference = @VpCodeOfReference
 
     SELECT @taxes = [value]
-      FROM ConfigParams WITH(NOLOCK)
+      FROM DeliveryBackOffice.dbo.ConfigParams WITH(NOLOCK)
      WHERE [name] = 'TaxPercentage'
        AND IdCountry = @country
 
-				DECLARE @establecimiento as varchar(15),
-					@correoCCO as varchar(200)
+	DECLARE @establecimiento as varchar(15),
+			@correoCCO as varchar(200)
 	
 	SELECT 
 		dpf.dpf_VpCodeOfReference,
@@ -68,29 +65,22 @@ BEGIN
 		dpf.dpf_SAPvendor,
 		dpf.dpf_SAPcreditCard,
 		dpf.dpf_OcrCode,
-
 		--Reemplazo seguro sin duplicar filas
 		ISNULL(csc.OcrCode2, dpf.dpf_OcrCode2) AS dpf_OcrCode2,
-
 		dpf.dpf_StatusFACE,
 		dpf.dpf_WarehouseCode,
 		dpf.inv_cmp_name,
 		dpf.inv_cmp_nameComercial,
 		dpf.KioskCode,
-
 		@taxes AS TaxPercentage,
 		@Address AS AddressEmisor
-
-	FROM del_ParametrosFactura dpf WITH(NOLOCK)
-
-	OUTER APPLY (
-		SELECT TOP 1 csc.OcrCode2
-		FROM CatSAPCodeCentroCosto csc WITH(NOLOCK)
-		WHERE csc.SAPCode   = dpf.dpf_SAParticulo
-		  AND csc.IdCountry = @country
-		  AND csc.RowStatus = 1
-	) csc
-
+	FROM DeliveryBackOffice.dbo.del_ParametrosFactura dpf WITH(NOLOCK)
+		OUTER APPLY (
+			SELECT TOP 1 csc.OcrCode2
+			FROM DeliveryBackOffice.dbo.CatSAPCodeCentroCosto csc WITH(NOLOCK)
+			WHERE csc.SAPCode   = dpf.dpf_SAParticulo
+			  AND csc.IdCountry = @country
+			  AND csc.RowStatus = 1
+		) csc
 	WHERE dpf.dpf_VpCodeOfReference = @VpCodeOfReference;
-
 END
