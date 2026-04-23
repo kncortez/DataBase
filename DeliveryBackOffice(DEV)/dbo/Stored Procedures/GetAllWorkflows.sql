8d6af8a7cac20886f82352489e82c63fe2563349
Erick Hernandez <erick.hernandez@forzadelivery.com>
2026-04-23 10:58:33 -0600

    FDAPI-6127 Se agrega parámetro a SP para filtrar por país.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetAllWorkflows.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/GetAllWorkflows.sql
@@ -6,35 +6,27 @@
    Fecha:     2026-03-05
 
 === CHANGELOG ============================
+2026-04-22 | Historia/épica: FDAPI-6127 | Autor: Erick Hernandez | Se agrega parámetro de código del país
 =========================================== */
 CREATE PROCEDURE [dbo].[GetAllWorkflows]
+	@CountryId VARCHAR(2)
 AS
 BEGIN
-	SELECT 
-    W.WorkflowId,
-    W.Name AS Workflow,
-	ISNULL(
-		'(' + CAST(
-				(
-					SELECT COUNT(*)
-					FROM WorkflowStatusMap WSM WITH (NOLOCK)
-					WHERE WSM.WorkflowId = W.WorkflowId
-					AND WSM.RowStatus = 1
-				) AS NVARCHAR(10)
-			) + 
-		') ' +
-		STUFF(
-			(
-				SELECT ', ' + SO.OrderDescription
-				FROM DeliveryBackOffice.dbo.WorkflowStatusMap WSM WITH (NOLOCK)
-				INNER JOIN DeliveryBackOffice.dbo.StatusOrder SO WITH (NOLOCK)
-					ON SO.StatusOrderId = WSM.StatusOrderId
-				WHERE WSM.WorkflowId = W.WorkflowId
-				AND WSM.RowStatus = 1
-				FOR XML PATH(''), TYPE
-			).value('.', 'NVARCHAR(MAX)')
-		,1,2,''), '(0)') AS Statuses
-	FROM DeliveryBackOffice.dbo.Workflow W WITH(NOLOCK)
-	WHERE W.RowStatus = 1
-	ORDER BY ISNULL(W.DateUpdated, W.DateCreated) DESC;
+	DECLARE @RowStatus_Active INT = 1;
+
+	IF EXISTS (SELECT 1 FROM DeliveryBackOffice.dbo.CatCountry WITH (NOLOCK) WHERE IdCountry = @CountryId AND CountryRowStatus = @RowStatus_Active)
+	BEGIN
+		SELECT W.WorkflowId, W.Name AS Workflow, 
+			CONCAT('(', COUNT(WSM.StatusOrderId), ') ', STRING_AGG(SO.OrderDescription, ', ')) AS Statuses
+		FROM DeliveryBackOffice.dbo.Workflow W WITH(NOLOCK)
+		LEFT JOIN DeliveryBackOffice.dbo.WorkflowStatusMap WSM WITH (NOLOCK)
+			ON WSM.WorkflowId = W.WorkflowId 
+			AND WSM.RowStatus = @RowStatus_Active AND WSM.CountryId = @CountryId
+		LEFT JOIN DeliveryBackOffice.dbo.StatusOrder SO WITH (NOLOCK)
+			ON SO.StatusOrderId = WSM.StatusOrderId 
+			AND SO.RowStatus = @RowStatus_Active
+		WHERE W.RowStatus = @RowStatus_Active
+		GROUP BY W.WorkflowId, W.Name, W.DateUpdated, W.DateCreated
+		ORDER BY ISNULL(W.DateUpdated, W.DateCreated) DESC;
+	END
 END;
