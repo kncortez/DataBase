544d3d538cc7c324c487ca32181b3f0f5f2850de
brandon-p <brandon.pedroza@forzadelivery.com>
2026-04-24 15:30:35 -0600

    FDAPI-5662: #se agrega script para insertar rackposition por defecto a las estaciones tipo 2 que son cnc y express center faltantes

---
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-5662/06 - UPDATE Rackpositiondefault EXC faltantes.sql
@@ -0,0 +1,116 @@
+--*****************************
+--*****************************
+-- NOTA: ID's de estaciones fueron consultados en produccion
+--*****************************
+--*****************************
+
+BEGIN TRY
+UPDATE CS SET CS.RackpositionDefault = '10#CNC001#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 290
+UPDATE CS SET CS.RackpositionDefault = '10#CNC003#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 294
+UPDATE CS SET CS.RackpositionDefault = '10#CNC004#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 292
+UPDATE CS SET CS.RackpositionDefault = '10#CNC005#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 287
+UPDATE CS SET CS.RackpositionDefault = '10#CNC007#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 317
+UPDATE CS SET CS.RackpositionDefault = '10#CNC008#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 321
+UPDATE CS SET CS.RackpositionDefault = '10#CNC009#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 320
+UPDATE CS SET CS.RackpositionDefault = '10#CNC010#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 319
+UPDATE CS SET CS.RackpositionDefault = '10#CNC011#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 323
+UPDATE CS SET CS.RackpositionDefault = '10#CNC012#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 325
+UPDATE CS SET CS.RackpositionDefault = '10#CNC013#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 316
+UPDATE CS SET CS.RackpositionDefault = '10#CNC014#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 315
+UPDATE CS SET CS.RackpositionDefault = '10#CNC015#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 324
+UPDATE CS SET CS.RackpositionDefault = '10#CNC016#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 332
+UPDATE CS SET CS.RackpositionDefault = '10#CNC017#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 330
+UPDATE CS SET CS.RackpositionDefault = '10#CNC018#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 338
+UPDATE CS SET CS.RackpositionDefault = '10#CNC019#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 333
+UPDATE CS SET CS.RackpositionDefault = '10#CNC020#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 336
+UPDATE CS SET CS.RackpositionDefault = '10#CNC021#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 331
+UPDATE CS SET CS.RackpositionDefault = '10#CNC022#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 337
+UPDATE CS SET CS.RackpositionDefault = '10#CNC024#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 335
+UPDATE CS SET CS.RackpositionDefault = '10#CNC025#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 329
+UPDATE CS SET CS.RackpositionDefault = '10#CNC026#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 342
+UPDATE CS SET CS.RackpositionDefault = '10#CNC027#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 344
+UPDATE CS SET CS.RackpositionDefault = '10#CNC028#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 346
+UPDATE CS SET CS.RackpositionDefault = '10#CNC029#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 345
+UPDATE CS SET CS.RackpositionDefault = '10#CNC030#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 347
+UPDATE CS SET CS.RackpositionDefault = '10#CNC031#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 348
+UPDATE CS SET CS.RackpositionDefault = '10#CNC032#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 350
+UPDATE CS SET CS.RackpositionDefault = '10#CNC033#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 349
+UPDATE CS SET CS.RackpositionDefault = '10#CNC034#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 355
+UPDATE CS SET CS.RackpositionDefault = '10#CNC035#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 362
+UPDATE CS SET CS.RackpositionDefault = '10#CNC036#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 364
+UPDATE CS SET CS.RackpositionDefault = '10#CNC037#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 361
+UPDATE CS SET CS.RackpositionDefault = '10#CNC038#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 366
+UPDATE CS SET CS.RackpositionDefault = '10#CNC039#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 373
+UPDATE CS SET CS.RackpositionDefault = '10#CNC040#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 374
+UPDATE CS SET CS.RackpositionDefault = '10#CNC041#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 371
+UPDATE CS SET CS.RackpositionDefault = '10#CNC042#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 372
+UPDATE CS SET CS.RackpositionDefault = '10#CNC043#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 377
+UPDATE CS SET CS.RackpositionDefault = '10#CNC044#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 376
+UPDATE CS SET CS.RackpositionDefault = '10#CNC045#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 375
+UPDATE CS SET CS.RackpositionDefault = '10#CNC046#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 378
+UPDATE CS SET CS.RackpositionDefault = '10#CNC047#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 380
+UPDATE CS SET CS.RackpositionDefault = '10#CNC048#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 382
+UPDATE CS SET CS.RackpositionDefault = '10#CNC049#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 387
+UPDATE CS SET CS.RackpositionDefault = '10#CNC050#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 385
+UPDATE CS SET CS.RackpositionDefault = '10#CNC051#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 386
+UPDATE CS SET CS.RackpositionDefault = '10#CNC052#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 388
+UPDATE CS SET CS.RackpositionDefault = '10#CNC053#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 389
+UPDATE CS SET CS.RackpositionDefault = '10#CNC054#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 390
+UPDATE CS SET CS.RackpositionDefault = '10#CNC055#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 394
+UPDATE CS SET CS.RackpositionDefault = '10#CNC056#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 392
+UPDATE CS SET CS.RackpositionDefault = '10#CNC057#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 398
+UPDATE CS SET CS.RackpositionDefault = '10#CNC058#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 400
+UPDATE CS SET CS.RackpositionDefault = '10#CNC059#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 399
+UPDATE CS SET CS.RackpositionDefault = '10#CNC060#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 353
+UPDATE CS SET CS.RackpositionDefault = '10#CNC061#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 328
+UPDATE CS SET CS.RackpositionDefault = '10#CNC062#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 404
+UPDATE CS SET CS.RackpositionDefault = '10#CNC063#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 406
+UPDATE CS SET CS.RackpositionDefault = '10#CNC064#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 408
+UPDATE CS SET CS.RackpositionDefault = '10#CNC065#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 407
+UPDATE CS SET CS.RackpositionDefault = '10#CNC066#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 409
+UPDATE CS SET CS.RackpositionDefault = '10#CNC067#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 410
+UPDATE CS SET CS.RackpositionDefault = '10#CNC068#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 414
+UPDATE CS SET CS.RackpositionDefault = '10#CNC069#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 413
+UPDATE CS SET CS.RackpositionDefault = '10#CNC070#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 415
+UPDATE CS SET CS.RackpositionDefault = '10#CNC071#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 351
+UPDATE CS SET CS.RackpositionDefault = '10#CNC072#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 422
+UPDATE CS SET CS.RackpositionDefault = '10#CNC073#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 423
+UPDATE CS SET CS.RackpositionDefault = '10#CNC074#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 418
+UPDATE CS SET CS.RackpositionDefault = '10#CNC075#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 419
+UPDATE CS SET CS.RackpositionDefault = '10#CNC076#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 421
+UPDATE CS SET CS.RackpositionDefault = '10#CNC077#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 420
+UPDATE CS SET CS.RackpositionDefault = '10#CNC078#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 424
+UPDATE CS SET CS.RackpositionDefault = '10#CNC079#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 430
+UPDATE CS SET CS.RackpositionDefault = '10#CNC080#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 429
+UPDATE CS SET CS.RackpositionDefault = '10#CNC081#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 425
+UPDATE CS SET CS.RackpositionDefault = '10#CNC082#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 426
+UPDATE CS SET CS.RackpositionDefault = '10#CNC083#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 334
+UPDATE CS SET CS.RackpositionDefault = '10#CNC084#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 427
+UPDATE CS SET CS.RackpositionDefault = '10#CNC085#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 431
+UPDATE CS SET CS.RackpositionDefault = '10#CNC086#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 432
+UPDATE CS SET CS.RackpositionDefault = '10#CNC087#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 433
+UPDATE CS SET CS.RackpositionDefault = '10#CNC088#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 428
+UPDATE CS SET CS.RackpositionDefault = '10#CNC089#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 441
+UPDATE CS SET CS.RackpositionDefault = '10#CNC090#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 442
+UPDATE CS SET CS.RackpositionDefault = '10#CNC091#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 443
+UPDATE CS SET CS.RackpositionDefault = '10#CNC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 67
+UPDATE CS SET CS.RackpositionDefault = '10#CNC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 164
+UPDATE CS SET CS.RackpositionDefault = '10#CNC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 68
+UPDATE CS SET CS.RackpositionDefault = '10#CNC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 165
+UPDATE CS SET CS.RackpositionDefault = '10#CNC023#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 295
+UPDATE CS SET CS.RackpositionDefault = '10#CNC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 367
+UPDATE CS SET CS.RackpositionDefault = '10#CNC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 293
+UPDATE CS SET CS.RackpositionDefault = '10#CNC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 393
+UPDATE CS SET CS.RackpositionDefault = '10#EXC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 276
+UPDATE CS SET CS.RackpositionDefault = '10#EXC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 170
+UPDATE CS SET CS.RackpositionDefault = '10#EXC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 75
+UPDATE CS SET CS.RackpositionDefault = '10#EXC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 172
+UPDATE CS SET CS.RackpositionDefault = '10#EXC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 97
+UPDATE CS SET CS.RackpositionDefault = '10#EXC000#PAL001' FROM DeliveryBackOffice.dbo.CatStation CS WITH(NOLOCK) WHERE CS.IdStation = 194
+
+PRINT 'ACTUALIZACION REALIZADA CORRECTAMENTE'
+END TRY
+BEGIN CATCH
+	PRINT 'ERROR: ' + ERROR_MESSAGE();
+	THROW;
+END CATCH
