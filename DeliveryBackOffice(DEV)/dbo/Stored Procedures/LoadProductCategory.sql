87c5703a7dfcf97a00486b5d211ad7d919214e71
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2026-05-04 10:51:15 -0600

    FDAPI-5807 Se modifica estructura y codificación

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/LoadProductCategory.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/LoadProductCategory.sql
@@ -10,18 +10,15 @@
 2026-03-18 | Historia/épica: FDAPI-5843  | Autor: Pedro Macajol  | Corrección de filtro por país y adición de SV.
 2024-07-17 | Historia/épica: (pendiente) | Autor: Cristian Suazo | Se agrega filtro por país.
 =========================================== */
-CREATE PROCEDURE [dbo].[LoadProductCategory]
-    @IdCountry NVARCHAR(2) = 'GT'
+CREATE PROCEDURE [dbo].[LoadProductCategory] @IdCountry NVARCHAR(2) = 'GT'
 AS
 BEGIN
-    SELECT 
-
-        IdCatProductCategory,
-        CatProductCategoryName,
-        CatProductCategoryDescription,
-        CatProductCategoryOrder,
-        ImageURL
+    SELECT IdCatProductCategory,
+           CatProductCategoryName,
+           CatProductCategoryDescription,
+           CatProductCategoryOrder,
+           ImageURL
     FROM DeliveryBackOffice.dbo.CatProductCategory
-    WHERE RowStatus = 1 
-    AND IdCountry = @IdCountry 
-END
+    WHERE RowStatus = 1
+          AND IdCountry = @IdCountry;
+END;
