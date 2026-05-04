495409f5530819fbf7f212c82f4de215ac32e0ab
pedromacajol <pedro.macajol@forzadelivery.com>
2026-05-04 09:39:41 -0600

    FDAPI-5807: Correcion de codificacion

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/LoadProductCategory.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/LoadProductCategory.sql
@@ -7,8 +7,8 @@
    Fecha:     2024-07-17
 ================================================= */
 /* === CHANGELOG ============================
-2026-03-18 | Historia/épica: FDAPI-5843  | Autor: Pedro Macajol  | Corrección de filtro por país y adición de SV
-2024-07-17 | Historia/épica: (pendiente) | Autor: Cristian Suazo | Se agrega filtro por país
+2026-03-18 | Historia/épica: FDAPI-5843  | Autor: Pedro Macajol  | Corrección de filtro por país y adición de SV.
+2024-07-17 | Historia/épica: (pendiente) | Autor: Cristian Suazo | Se agrega filtro por país.
 =========================================== */
 CREATE PROCEDURE [dbo].[LoadProductCategory]
     @IdCountry NVARCHAR(2) = 'GT'
