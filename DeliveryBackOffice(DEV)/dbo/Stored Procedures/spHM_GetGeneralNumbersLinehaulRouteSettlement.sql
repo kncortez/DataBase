89c56b3ce192c6eb1a4e288d22c7f50e12735a9a
brandon-bash <brandon.pedroza@forzadelivery.com>
2026-05-08 18:50:12 -0600

    FDAPI-6096: #se agregan sps para obtener resumen para realizar preparacion automatica en linehaul

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetGeneralNumbersLinehaulRouteSettlement.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetGeneralNumbersLinehaulRouteSettlement.sql
@@ -1,8 +1,15 @@
-﻿-- =============================================
--- Author:		<Ochoa, Jerson>
--- Create date: <14-09-2022>
--- Description:	<Get general numbers from Linehaul Route Settlement>
--- =============================================
+﻿/* =================================================
+   SP:        spHM_GetGeneralNumbersLinehaulRouteSettlement
+   Propósito: Obtener métricas generales de Linehaul Route Settlement
+   Autor:     Jerson Ochoa
+   Historia:  ---
+   Fecha:     2022-09-14
+
+=== CHANGELOG ============================
+
+2026-05-08 | Historia/épica: FDAPI-6096        | Autor: Brandon Pedroza      | Se obtiene numero de contenedores en escala
+
+=========================================== */
 CREATE PROCEDURE [dbo].[spHM_GetGeneralNumbersLinehaulRouteSettlement]
 	@LinehaulRouteSettlementId AS INT
 AS
@@ -10,29 +17,50 @@ BEGIN
 	SET NOCOUNT ON;
 	DECLARE @LRP_ID AS INT;
 	DECLARE @TOTAL_PIECES AS INT;
+	DECLARE @CONTAINER_IN_STOPOVER AS INT = 0;
+	DECLARE @STATUS_STOPOVER AS INT = 4;
+
 
 	SET @LRP_ID = (SELECT	[LRS].[LinehaulRoutePreparationId]
-					FROM	[dbo].[LinehaulRouteSettlement] LRS
+					FROM	[dbo].[LinehaulRouteSettlement] LRS WITH (NOLOCK)
 					WHERE	[LRS].[IdLinehaulRouteSettlement] = @LinehaulRouteSettlementId);
 
-	SET @TOTAL_PIECES = (SELECT		COUNT([LRPCD].[GuideSerie]) AS TOTAL
-						FROM		[dbo].[LinehaulRoutePreparationContainerDetailPiece] LRPCDP
-						INNER JOIN	[dbo].[LinehaulRoutePreparationContainerDetail] LRPCD
-							ON		[LRPCDP].[LinehaulRoutePreparationContainerDetailId] = [LRPCD].[IdLinehaulRoutePreparationContainerDetail]
-							AND		[LRPCD].[RowStatus] = 1
-						INNER JOIN	[dbo].[LinehaulRoutePreparationContainer] LRPC
-							ON		[LRPCD].[LinehaulRoutePreparationContainerId] = [LRPC].[IdLinehaulRoutePreparationContainer]
-						INNER JOIN	[dbo].[LinehaulRoutePreparation] LRP
-							ON		[LRPC].[LinehaulRoutePreparationId] = [LRP].[IdLinehaulRoutePreparation]
-							AND		[LRP].[IdLinehaulRoutePreparation] = @LRP_ID
-						WHERE		[LRPCDP].[ActCode] IS NULL
-							AND		[LRPCDP].[RowStatus] = 1);
+
+	SELECT
+		@TOTAL_PIECES         = TOTAL_PIECES,
+		@CONTAINER_IN_STOPOVER = CONTAINER_IN_STOPOVER
+	FROM (
+		SELECT
+			(
+				SELECT      COUNT([LRPCD].[GuideSerie])
+				FROM        [dbo].[LinehaulRoutePreparationContainerDetailPiece] LRPCDP WITH (NOLOCK)
+				INNER JOIN  [dbo].[LinehaulRoutePreparationContainerDetail] LRPCD WITH (NOLOCK)
+					ON      [LRPCDP].[LinehaulRoutePreparationContainerDetailId] = [LRPCD].[IdLinehaulRoutePreparationContainerDetail]
+				INNER JOIN  [dbo].[LinehaulRoutePreparationContainer] LRPC WITH (NOLOCK)
+					ON      [LRPCD].[LinehaulRoutePreparationContainerId] = [LRPC].[IdLinehaulRoutePreparationContainer]
+				INNER JOIN  [dbo].[LinehaulRoutePreparation] LRP WITH (NOLOCK)
+					ON      [LRPC].[LinehaulRoutePreparationId] = [LRP].[IdLinehaulRoutePreparation]
+				WHERE       [LRPCDP].[ActCode] IS NULL
+					AND     [LRPCDP].[RowStatus] = 1
+					AND     [LRP].[IdLinehaulRoutePreparation] = @LRP_ID
+					AND     [LRPCD].[RowStatus] = 1
+			) AS TOTAL_PIECES,
+			(
+				SELECT  COUNT(1)
+				FROM    [dbo].[LinehaulRoutePreparationContainer] WITH (NOLOCK)
+				WHERE   [LinehaulRoutePreparationId] = @LRP_ID
+					AND [CatLinehaulStatusId] = @STATUS_STOPOVER
+			) AS CONTAINER_IN_STOPOVER
+	) RESULTS;
+
+
 
     SELECT	[LRS].[GuidesReceived],
 			[LRS].[ContainersReceived],
 			[LRS].[GuidePiecesReceived],
 			[LRS].[GuidePiecesMissing],
-			@TOTAL_PIECES [TotalPieces]
+			@TOTAL_PIECES [TotalPieces],
+			@CONTAINER_IN_STOPOVER [ContainersInStopOver]
 	FROM	[dbo].[LinehaulRouteSettlement] LRS
 	WHERE	[LRS].[IdLinehaulRouteSettlement] = @LinehaulRouteSettlementId;
 END
---
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetSummaryRoutePreparationAutomatic.sql
@@ -0,0 +1,69 @@
+/* =================================================
+   SP:        [dbo].[spHMGetSummaryRoutePreparationAutomatic]
+   Propósito: Obtiene resumen de piezas para realizar preparacion de ruta automatica
+   Autor:     Brandon Pedroza
+   Historia:  FDAPI-6096
+   Fecha:     2026-05-08
+   === CHANGELOG ============================
+
+=========================================== */
+
+CREATE PROCEDURE [dbo].[spHM_GetSummaryRoutePreparationAutomatic]
+    @LinehaulRouteSettlementId  INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+
+    DECLARE @LRP_ID AS INT;
+    DECLARE @STATUS_LIQUID AS INT = 3; -- Liquidado <- CatLinehaulStatus 
+
+    SET @LRP_ID = (
+        SELECT  [LRS].[LinehaulRoutePreparationId]
+        FROM    [dbo].[LinehaulRouteSettlement] LRS WITH (NOLOCK)
+        WHERE   [LRS].[IdLinehaulRouteSettlement] = @LinehaulRouteSettlementId
+    );
+
+    IF OBJECT_ID('tempdb..#GuidesTmp') IS NOT NULL
+        DROP TABLE #GuidesTmp;
+
+    CREATE TABLE #GuidesTmp (
+        GuideSerie   NVARCHAR(2),
+        GuideNumber  INT,
+        GuidePiece   INT
+    );
+
+    INSERT INTO #GuidesTmp (GuideSerie, GuideNumber, GuidePiece)
+    SELECT
+        LRPCD.GuideSerie,
+        LRPCD.GuideNumber,
+        LRPCDP.PieceNumber
+    FROM        [dbo].[LinehaulRoutePreparationContainerDetailPiece]    LRPCDP  WITH (NOLOCK)
+    INNER JOIN  [dbo].[LinehaulRoutePreparationContainerDetail]         LRPCD   WITH (NOLOCK)
+        ON      [LRPCDP].[LinehaulRoutePreparationContainerDetailId]    = [LRPCD].[IdLinehaulRoutePreparationContainerDetail]
+    INNER JOIN  [dbo].[LinehaulRoutePreparationContainer]               LRPC    WITH (NOLOCK)
+        ON      [LRPCD].[LinehaulRoutePreparationContainerId]           = [LRPC].[IdLinehaulRoutePreparationContainer]
+    INNER JOIN  [dbo].[LinehaulRoutePreparation]                        LRP     WITH (NOLOCK)
+        ON      [LRPC].[LinehaulRoutePreparationId]                     = [LRP].[IdLinehaulRoutePreparation]
+    WHERE       [LRPCDP].[ActCode]                  IS NULL
+        AND     [LRPCDP].[RowStatus]                = 1
+        AND     [LRP].[IdLinehaulRoutePreparation]  = @LRP_ID
+        AND     [LRPCD].[RowStatus]                 = 1
+        AND     [LRPCDP].CatLinehaulStatusId        = @STATUS_LIQUID;
+
+    SELECT
+        COUNT(GT.GuideNumber)   AS [TotalPieces],
+        DSC.Hub,
+        DSC.RouteCode
+    FROM        DeliveryOrder           DO WITH (NOLOCK)
+    INNER JOIN  #GuidesTmp              GT
+        ON      DO.Guide_Serie          = GT.GuideSerie
+        AND     DO.Guide_Number         = GT.GuideNumber
+    INNER JOIN  DumpServiceCoverage     DSC WITH (NOLOCK)
+        ON      DO.ReceiverIdSettlement = DSC.IdSettlement
+    GROUP BY    DSC.IdSettlement, DSC.Hub, DSC.RouteCode;
+
+    IF OBJECT_ID('tempdb..#GuidesTmp') IS NOT NULL
+        DROP TABLE #GuidesTmp;
+
+END;
