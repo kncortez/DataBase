4fd8e5a2bba024c3a95c3b75c34dacbeab08f6b8
FLG\tito.garcia <tito.garcia@forzadelivery.com>
2026-05-20 17:24:42 -0600

    FDAPI-5807 Se agregan los registros en configParams de las redes sociales para SV

--- DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-5807/6 - Insert into CatproductCategory.sql
+++ DeliveryBackOffice(DEV)/dbo/Scripts/FDAPI-5807/6 - Insert into CatproductCategory.sql
@@ -18,6 +18,23 @@ BEGIN TRY
 		('Guías Prepago', 'Guías Prepago', 2, 1, 'SYS-BHERRERA', GETDATE(), NULL, NULL, 'Paquetes', NULL, 'SV'),
 		('Planes de Descuento', 'Planes de Descuento', 7, 1, 'SYS-BHERRERA', GETDATE(), NULL, NULL, 'Planes', NULL, 'SV');
 
+	INSERT INTO DeliveryBackOffice.dbo.ConfigParams
+	(
+		Name,
+		Description,
+		Value,
+		Status,
+		CreateDate,
+		IdCountry,
+		IdCurrencyCOD
+	)
+	VALUES
+	('UrlNetWork','Url de facebook','https://www.facebook.com/profile.php?id=61575901449458',1, GETDATE(),'SV', NULL),
+	('UrlNetWork','Url de instagram','https://www.instagram.com/forzadeliverysv?igsh=anFrMWJ2eWN5c2dn',1, GETDATE(),'SV', NULL),
+	('UrlNetWork','Url de Linkedin','https://www.linkedin.com/company/107254935/admin/dashboard/',1, GETDATE(),'SV', NULL),
+	('UrlNetWork','Url de Whatsapp','',1, GETDATE(),'SV', NULL),
+	('PBX','Numero de telefono','2535-1818',1, GETDATE(),'SV', NULL)
+
 	COMMIT TRANSACTION;
 END TRY
 BEGIN CATCH
