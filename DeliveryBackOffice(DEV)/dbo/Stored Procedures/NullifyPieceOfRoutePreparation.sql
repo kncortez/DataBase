ffd0bc73d1d4049e3f66051e2c366da88f2a97fc
caleb_loarca <caleb.loarca@forzadelivery.com>
2026-05-27 17:29:40 -0600

    FDAPI-6116: Se agrega actualización en estado de guía a "En Inventario" en las tablas DeliveryOrder, DeliveryOrderDetail y DeliveryOrderPiece.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/NullifyPieceOfRoutePreparation.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/NullifyPieceOfRoutePreparation.sql
@@ -1,15 +1,22 @@
-﻿
--- =============================================
--- Author:		<Andres,Ruiz>
--- Create date: <2022-01-19>
--- Description:	< Extracción de pieza de RoutePreparation .>
--- =============================================
+﻿/* =================================================
+   SP:        [dbo].[NullifyPieceOfRoutePreparation]
+   Propósito: Extracción de pieza de RoutePreparation
+   Autor:     Andres Ruiz
+   Historia:  ---
+   Fecha:     2022-01-19
+
+=== CHANGELOG ============================
+
+2026-05-25 | Historia/épica: FDAPI-6115   | Autor: Caleb Loarca    | Hacer actualización de estado de guía a Inventario.
+
+=========================================== */
 
 CREATE PROCEDURE [dbo].[NullifyPieceOfRoutePreparation]
 	@IdRoutePreparation INT,
 	@GuideSerie NVARCHAR(2),
 	@GuideNumber INT,
-	@Token NVARCHAR(50)
+	@Token NVARCHAR(50),
+	@StationId INT
 AS
 BEGIN
 	--- Conteo para verificar cantidad correcta de validaciones
@@ -20,6 +27,7 @@ BEGIN
 	
 	--- Variables para manejo de piezas
 	DECLARE @GuidePieceExists BIT;
+	DECLARE @PieceCount INT = 0;
 
 	--- Variables para despliegue de errores
 	DECLARE @FatalError INT = 0;
@@ -59,8 +67,9 @@ BEGIN
 					,DateUpdated = GETDATE()
 				WHERE
 					RoutePreparationDetailId = @IdRoutePreparationDetail
-					
-				IF COALESCE(@@ROWCOUNT,0) > 0
+
+				SET @PieceCount = COALESCE(@@ROWCOUNT, 0)
+				IF @PieceCount > 0
 					SET @RModified = @RModified + 1
 
 				--- Actualizar el detalle de la preparación de ruta
@@ -74,6 +83,63 @@ BEGIN
 					
 				IF COALESCE(@@ROWCOUNT,0) > 0
 					SET @RModified = @RModified + 1
+
+				--- Actualiza la guia para volver a inventario
+				UPDATE [DeliveryBackOffice].[dbo].[DeliveryOrder]
+                SET StatusOrderId = 10	---En Inventario
+                WHERE Guide_Serie = @GuideSerie
+                AND Guide_Number = @GuideNumber;
+
+				IF COALESCE(@@ROWCOUNT,0) > 0
+					SET @RModified = @RModified + 1
+
+				--- Insertar el nuevo estado a bitácora (una fila por cada pieza anulada)
+				;WITH Tally (n) AS (
+					SELECT 1
+					UNION ALL
+					SELECT n + 1 FROM Tally WHERE n < @PieceCount
+				)
+				INSERT [DeliveryBackOffice].[dbo].[DeliveryOrderDetail]
+				(
+					[Guide_Serie],
+					[Guide_Number],
+					[StatusOrderId],
+					[UserCreated],
+					[DateCreated],
+					[DateCreatedInSystem],
+					[StationId]
+				)
+				SELECT @GuideSerie,
+					   @GuideNumber,
+					   10, --En Inventario
+					   @Token,
+					   GETDATE(),
+					   GETDATE(),
+					   @StationId
+				FROM Tally
+				WHERE EXISTS
+				(
+					SELECT 1
+					FROM RoutePreparationDetail WITH (NOLOCK)
+					WHERE RoutePreparationId = @IdRoutePreparation
+						  AND Guide_Serie = @GuideSerie
+						  AND Guide_Number = @GuideNumber
+						  AND CAST(DateCreated AS DATE) = CAST(GETDATE() AS DATE)
+				)
+				OPTION (MAXRECURSION 1000);
+
+						SET @RModified = @RModified + COALESCE(@@ROWCOUNT, 0)
+
+                        --- Actualziar el estado de las piezas
+                        UPDATE [DeliveryBackOffice].[dbo].[DeliveryOrderPiece]
+                        SET StatusOrderId = 10	---En Inventario
+                        WHERE GuideSerie = @GuideSerie
+                              AND GuideNumber = @GuideNumber;
+                              
+
+						IF COALESCE(@@ROWCOUNT,0) > 0
+						SET @RModified = @RModified + 1 
+	
 			END
 			ELSE
 			BEGIN
