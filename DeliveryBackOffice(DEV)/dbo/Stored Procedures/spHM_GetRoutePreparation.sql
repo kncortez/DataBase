dcc1a7239b8f9128a8d4e95a1ae1a236c0907130
caleb_loarca <caleb.loarca@forzadelivery.com>
2026-05-28 22:08:31 -0600

    FDAPI-6242: Se comento Inner join de tabla RoutePreparationDetailPiece, para obtener listado de guías incluyendo las que fueron reasignadas (estado Pendiente) y aún no están Confirmadas.

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetRoutePreparation.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/spHM_GetRoutePreparation.sql
@@ -1,13 +1,18 @@
 ﻿
--- =============================================
--- Author:		<Oscar Morales>
--- Create date: <2022-09-06>
--- Description:	<Obtiene información de la preparación de una ruta de entregas en móvil>
--- =============================================
--- Author:      <Edelman, Vasquez>
--- Create date: <22-04-2025>
--- Description: #Agregar campo al groupby
--- =============================================
+/* =================================================
+   SP:        spHM_GetRoutePreparation
+   Propósito: Obtener información de la preparación de una ruta de entregas en móvil
+   Autor:     Oscar Morales
+   Historia:  ---
+   Fecha:     2022-09-06
+
+=== CHANGELOG ============================
+
+2026-05-28 | Historia/épica: FDAPI-6242   | Autor: Caleb Loarca    | Obtener listado de guías incluyendo las que fueron reasignadas (estado Pendiente) y aún no están Confirmadas.
+2025-04-22 | Historia/épica: ---          | Autor: Edelman Vásquez | #Agregar campo al groupby
+
+=========================================== */
+
 CREATE PROCEDURE [dbo].[spHM_GetRoutePreparation]
 	-- Add the parameters for the stored procedure here
 	@RouteId INT,
@@ -51,8 +56,8 @@ BEGIN
 		FROM RoutePreparation rp WITH (NOLOCK)
 		INNER JOIN RoutePreparationDetail rpd WITH (NOLOCK)
 			ON rpd.RoutePreparationId = rp.IdRoutePreparation
-		INNER JOIN RoutePreparationDetailPiece rpdp WITH (NOLOCK)
-			ON rpdp.RoutePreparationDetailId = rpd.IdRoutePreparationDetail
+		--INNER JOIN RoutePreparationDetailPiece rpdp WITH (NOLOCK)
+		--	ON rpdp.RoutePreparationDetailId = rpd.IdRoutePreparationDetail
 		INNER JOIN DeliveryOrder do WITH (NOLOCK)
 			ON rpd.Guide_Serie = do.Guide_Serie
 				AND rpd.Guide_Number = do.Guide_Number
@@ -60,7 +65,7 @@ BEGIN
 		AND rp.DateRoutePreparation = @Date
 		AND rp.RowStatus = 1
 		AND rpd.RowStatus = 1
-		AND rpdp.RowStatus = 1
+		--AND rpdp.RowStatus = 1
 		GROUP BY IdRoutePreparationDetail
 				,rpd.Guide_Serie
 				,rpd.Guide_Number
