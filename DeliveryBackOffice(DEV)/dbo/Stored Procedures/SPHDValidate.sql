8a0e0241e7044415496102e9cc61c4241ae154c4
Azurdia <cristian.azurdia@forzadelivery.com>
2026-06-04 15:52:54 -0600

    FDAPI-5721 Correción de fix  facturación HN

--- DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHDValidate.sql
+++ DeliveryBackOffice(DEV)/dbo/Stored Procedures/SPHDValidate.sql
@@ -1,23 +1,18 @@
-﻿-- =============================================
--- Author:		<Edelman Vásquez>
--- Create date: <2023-03-31>
--- Description:	<VALIDAR SI FACTURA TIENE NOTA DE CREDITO>
--- =============================================
--- =============================================
--- Author:		<Cristian Suazo>
--- Create date: <2024-08-23>
--- Description:	<Se agrega el IdCountry de la factura en la respuesta>
--- =============================================
--- =============================================
--- Author:		<Brandon Pedroza>
--- Create date: <2025-07-18>
--- Description:	<Facturacion SV - se obtiene factura por numberfel para SV>
--- =============================================
--- =============================================
--- Author:		<Brandon Pedroza>
--- Create date: <2025-09-11>
--- Description:	<Facturacion SV - se quita validacion isnull al consultar tabla invoiceHeader>
--- =============================================
+﻿/* =================================================
+   SP:        [dbo].[SPHDValidate]
+   Propósito: <VALIDAR SI FACTURA EXISTE Y VER SUS DATOS>
+   Autor:     <Edelman Vasquez>
+   Historia:  <FDAPI-5460>
+   Fecha:     2023-03-31
+============================================
+=== CHANGELOG ================================
+-- 2023-03-31 | Historia/épica: FDAPI-5460 | Autor: Edelman Vásquez  | VALIDAR SI FACTURA TIENE NOTA DE CREDITO
+-- 2024-08-23 | Historia/épica: FDAPI-5460 | Autor: Cristian Suazo   | Se agrega el IdCountry de la factura en la respuesta
+-- 2025-07-18 | Historia/épica: FDAPI-5460 | Autor: Brandon Pedroza  | Facturacion SV - se obtiene factura por numberfel para SV
+-- 2025-09-11 | Historia/épica: FDAPI-5460 | Autor: Brandon Pedroza  | Facturacion SV - se quita validacion isnull al consultar tabla invoiceHeader
+-- 2026-06-03 | Historia/épica: FDAPI-5721 | Autor: Cristian Azurdia | Facturacion HN - obtención de datos nuevos de facturas con error 2025
+=========================================== */
+
 CREATE PROCEDURE [dbo].[SPHDValidate]
 @Guide     Nvarchar(25)=null,
 @DateOf    Datetime=null,
@@ -31,238 +26,241 @@ CREATE PROCEDURE [dbo].[SPHDValidate]
 AS
 BEGIN
 
-	SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
-	
+    DECLARE @Envio INT =0
+    DECLARE @ComisionCOD INT=0
+    DECLARE @dti_fk_header INT =0;
 
-	 DECLARE @Envio INT =0
-	 DECLARE @ComisionCOD INT=0
-	 DECLARE @dti_fk_header INT =0;
+    -- IF(@TipoEnvio > 0)
+    SET @Envio =(Select IdCatInvoiceType From [dbo].[CatInvoiceType] CIT  WHERE [Name]='Envío')
 
-	       -- IF(@TipoEnvio > 0)
-		       SET @Envio =(Select IdCatInvoiceType From [dbo].[CatInvoiceType] CIT  WHERE [Name]='Envío')
+    --IF(@TipoComisionCOD>0)
+    SET @ComisionCOD =(Select IdCatInvoiceType From [dbo].[CatInvoiceType] CIT  WHERE [Name]='Comisión COD')
 
-		    
-			--IF(@TipoComisionCOD>0)
-			SET @ComisionCOD =(Select IdCatInvoiceType From [dbo].[CatInvoiceType] CIT  WHERE [Name]='Comisión COD')
-
-		
-			
-
+    IF CHARINDEX('-', @Guide) > 0
+    BEGIN
+        SET  @Guide=  SUBSTRING(@Guide, 0, IIF(CHARINDEX('-', @Guide) = 0, (LEN(@Guide)), (CHARINDEX('-', @Guide) - 0)))
+    END
 
 
-		IF CHARINDEX('-', @Guide) > 0
-		BEGIN
-		   SET  @Guide=  SUBSTRING(@Guide, 0, IIF(CHARINDEX('-', @Guide) = 0, (LEN(@Guide)), (CHARINDEX('-', @Guide) - 0)))
-		END
-If (@Guide IS NOT NULL) 
-Begin
+    IF (@Guide IS NOT NULL) 
+    BEGIN
+
+        SELECT TOP 1 @dti_fk_header = dti_fk_header 
+        FROM [dbo].[invoiceDetail]	WITH(NOLOCK)
+        WHERE dti_fk_orderSerie + Cast(dti_fk_orderNumber as varchar)  = @Guide
 
 
-     Select Top 1 @dti_fk_header = dti_fk_header From [dbo].[invoiceDetail]	WITH(NOLOCK)
-								Where dti_fk_orderSerie + Cast(dti_fk_orderNumber as varchar)  = @Guide
-								
+        IF (@TipoEnvio = 1 And @TipoComisionCOD = 0)
+        BEGIN
 
+            SELECT
+                Top 1
+                ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote'
+                ,ID.dti_description
+                ,IH.inv_pk_id
+                ,IH.inv_serieFEL
+                ,IH.inv_numberFEL
+                ,IH.inv_certificationFEL
+                ,IH.inv_cli_name
+                ,IH.IdCountry
+            FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
+            INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
+                ON IH.inv_pk_id = ID.dti_fk_header
+            WHERE 
+                ID.dti_fk_header = @dti_fk_header
+                AND   IH.inv_invoiceOfCreditNote IS NULL
+                AND   inv_certificationFEL IS NOT NULL
+                AND (IH.CatInvoiceTypeId IS NULL OR IH.CatInvoiceTypeId IN (@Envio))
+                AND IH.inv_creditNote IS NULL
+                AND   IH.inv_invoiceOfCreditNote IS  NULL
+            ORDER BY IH.inv_pk_id DESC
 
+        END
+        ELSE IF (@TipoComisionCOD = 1 And @TipoEnvio=0)
+        BEGIN
 
-    IF (@TipoEnvio = 1 And @TipoComisionCOD = 0)
-	 BEGIN
+            SELECT
+                 TOP 1
+                 ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote'
+                ,ID.dti_description
+                ,IH.inv_pk_id
+                ,IH.inv_serieFEL
+                ,IH.inv_numberFEL
+                ,IH.inv_certificationFEL
+                ,IH.inv_cli_name
+                ,IH.IdCountry
+            FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
+            INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
+                ON IH.inv_pk_id = ID.dti_fk_header
+            WHERE 
+                ID.dti_fk_header = @dti_fk_header
+                AND IH.inv_invoiceOfCreditNote IS NULL
+                AND inv_certificationFEL IS NOT NULL
+                AND (IH.CatInvoiceTypeId IS NULL OR IH.CatInvoiceTypeId IN (@ComisionCOD))
+                AND IH.inv_creditNote IS NULL
+                AND IH.inv_invoiceOfCreditNote IS   NULL
+            ORDER BY IH.inv_pk_id DESC
 
-			Select 
-			    Top 1
-				ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote',
-				ID.dti_description,
-				IH.inv_pk_id,
-				IH.inv_serieFEL,
-				IH.inv_numberFEL,
-				IH.inv_certificationFEL,
-				IH.inv_cli_name,
-				IH.IdCountry
-						 FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
-							 INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
-							 ON IH.inv_pk_id = ID.dti_fk_header
-							 WHERE 
-							 ---ID.dti_fk_orderSerie + Cast(ID.dti_fk_orderNumber as varchar) = @Guide 
-							  ID.dti_fk_header = @dti_fk_header
-							 AND   IH.inv_invoiceOfCreditNote IS NULL
-							 AND   inv_certificationFEL IS NOT NULL
-							 AND (IH.CatInvoiceTypeId IS NULL OR IH.CatInvoiceTypeId IN (@Envio))
-							 AND IH.inv_creditNote IS NULL
-							 AND   IH.inv_invoiceOfCreditNote IS  NULL
-							 ORDER BY IH.inv_pk_id DESC
-         END
-		ELSE IF (@TipoComisionCOD = 1 And @TipoEnvio=0)
-		 BEGIN
+        END
+        ELSE IF (@TipoEnvio = 1  And @TipoComisionCOD =1)
+        BEGIN
 
-			Select Top 1 
-			    ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote',
-				ID.dti_description,
-				IH.inv_pk_id,
-				IH.inv_serieFEL,
-				IH.inv_numberFEL,
-				IH.inv_certificationFEL,
-				IH.inv_cli_name,
-				IH.IdCountry
-						 FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
-							 INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
-							 ON IH.inv_pk_id = ID.dti_fk_header
-							 WHERE 
-							 --ID.dti_fk_orderSerie + Cast(ID.dti_fk_orderNumber as varchar) = @Guide
-							  ID.dti_fk_header = @dti_fk_header
-							 AND   IH.inv_invoiceOfCreditNote IS NULL
-							 AND   inv_certificationFEL IS NOT NULL
-							 AND (IH.CatInvoiceTypeId IS NULL OR IH.CatInvoiceTypeId IN (@ComisionCOD))
-							 AND IH.inv_creditNote IS NULL
-							 AND  IH.inv_invoiceOfCreditNote IS   NULL
-							 ORDER BY IH.inv_pk_id DESC
-         END
-	ELSE IF (@TipoEnvio = 1  And @TipoComisionCOD =1)
-	 BEGIN
+            SELECT
+             ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote'
+            ,ID.dti_description
+            ,IH.inv_pk_id
+            ,IH.inv_serieFEL
+            ,IH.inv_numberFEL
+            ,IH.inv_certificationFEL
+            ,IH.inv_cli_name
+            ,IH.IdCountry
+            FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
+            INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
+                ON IH.inv_pk_id = ID.dti_fk_header
+            WHERE
+                ID.dti_fk_header = @dti_fk_header
+                AND   inv_certificationFEL IS NOT NULL
+                AND IH.inv_creditNote IS NULL
+                AND  IH.inv_invoiceOfCreditNote IS  NULL
 
-			Select
-				ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote',
-				ID.dti_description,
-				IH.inv_pk_id,
-				IH.inv_serieFEL,
-				IH.inv_numberFEL,
-				IH.inv_certificationFEL,
-				IH.inv_cli_name,
-				IH.IdCountry
-						 FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
-							 INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
-							 ON IH.inv_pk_id = ID.dti_fk_header
-							 WHERE
-							 --ID.dti_fk_orderSerie + Cast(ID.dti_fk_orderNumber as varchar) = @Guide  
-							  ID.dti_fk_header = @dti_fk_header
-							 AND   inv_certificationFEL IS NOT NULL
-							 AND IH.inv_creditNote IS NULL
-							 AND  IH.inv_invoiceOfCreditNote IS  NULL
+        END
+        ELSE
+        BEGIN
 
-         END
-	
-		 ELSE
-		 BEGIN
+            SELECT
+                ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote'
+                ,ID.dti_description
+                ,IH.inv_pk_id
+                ,IH.inv_serieFEL
+                ,IH.inv_numberFEL
+                ,IH.inv_certificationFEL
+                ,IH.inv_cli_name
+                ,IH.IdCountry
+            FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
+            INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
+                ON IH.inv_pk_id = ID.dti_fk_header
+            WHERE
+                ID.dti_fk_header = @dti_fk_header
+                AND   inv_certificationFEL IS NOT NULL
+                AND IH.inv_creditNote IS NULL
+                AND  IH.inv_invoiceOfCreditNote IS NULL
 
-	
-				Select
-				ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote',
-				ID.dti_description,
-				IH.inv_pk_id,
-				IH.inv_serieFEL,
-				IH.inv_numberFEL,
-				IH.inv_certificationFEL,
-				IH.inv_cli_name,
-				IH.IdCountry
-						 FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
-							 INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
-							 ON IH.inv_pk_id = ID.dti_fk_header
-							 WHERE 
-							 ---ID.dti_fk_orderSerie + Cast(ID.dti_fk_orderNumber as varchar) = @Guide 
-							  ID.dti_fk_header = @dti_fk_header
-							 AND   inv_certificationFEL IS NOT NULL
-							 AND IH.inv_creditNote IS NULL
-							 AND  IH.inv_invoiceOfCreditNote IS NULL
-							
+        END
 
-		 END
-   
+    END
+    ELSE IF (@NumberFel IS NOT NULL)
+    BEGIN
 
+        IF(@IdCountry ='SV')
+        BEGIN
 
+            DECLARE @TypeCF INT = (SELECT IdRegister FROM CatTypeDocument WITH(NOLOCK) WHERE [Name] = 'Comprobante Crédito Fiscal');
+            DECLARE @TypeInvoice INT = (SELECT IdRegister FROM CatTypeDocument WITH(NOLOCK) WHERE [Name] = 'Factura');
 
+            Select
+                 0 'HaveaCreditNote'
+                ,ID.dti_description
+                ,IH.inv_pk_id
+                ,IH.inv_serieFEL
+                ,IH.inv_numberFEL
+                ,IH.inv_certificationFEL
+                ,IH.inv_cli_name
+                ,IH.IdCountry
+            FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
+            INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
+                ON IH.inv_pk_id = ID.dti_fk_header
+            WHERE
+                inv_numberFEL = @NumberFel
+                AND IH.inv_type in (@TypeInvoice,@TypeCF)
+            ORDER BY IH.inv_pk_id DESC
+
+        END
+        ELSE
+        BEGIN
+
+            DECLARE @ResolvedFel VARCHAR(50) = COALESCE(
+                (SELECT TOP 1 inv_certificationFEL
+                 FROM dbo.invoiceHeader WITH (NOLOCK)
+                 WHERE inv_certificationFEL = @NumberFel),
+            
+                (SELECT TOP 1 inv_certificationFEL_new
+                 FROM dbo.InvoiceDataFixHN WITH (NOLOCK)
+                 WHERE inv_certificationFEL_old = @NumberFel),
+            
+                (SELECT TOP 1 inv_certificationFEL_new
+                 FROM dbo.InvoiceDataFixHn2 WITH (NOLOCK)
+                 WHERE inv_certificationFEL_old = @NumberFel)
+            );
+            
+            -- =====================================================================
+            -- Consulta final usando el FEL resuelto
+            -- =====================================================================
+            SELECT
+                 ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote'
+                ,ID.dti_description
+                ,IH.inv_pk_id
+                ,IH.inv_serieFEL
+                ,IH.inv_numberFEL
+                ,IH.inv_certificationFEL
+                ,IH.inv_cli_name
+                ,IH.IdCountry
+            FROM dbo.invoiceHeader IH WITH (NOLOCK)
+            INNER JOIN dbo.invoiceDetail ID WITH (NOLOCK)
+                ON IH.inv_pk_id = ID.dti_fk_header
+            WHERE IH.inv_certificationFEL    = @ResolvedFel
+              AND IH.inv_invoiceOfCreditNote IS NULL
+              AND IH.inv_creditNote          IS NULL
+            ORDER BY IH.inv_pk_id DESC;
+
+        END
+    END
+    ELSE IF (@Membership IS NOT NULL)
+    BEGIN
+
+        SELECT
+             ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote'
+            ,ID.dti_description
+            ,IH.inv_pk_id
+            ,IH.inv_serieFEL
+            ,IH.inv_numberFEL
+            ,IH.inv_certificationFEL
+            ,IH.inv_cli_name
+            ,IH.IdCountry
+        FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
+        INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
+            ON IH.inv_pk_id = ID.dti_fk_header
+        WHERE
+            IH.inv_invoiceOfCreditNote IS NULL AND ID.MembershipId = @Membership
+            AND IH.inv_dateFEL Between  @DateOf + ' 00:00:00'  AND @DateTo + ' 23:59:59'
+            AND inv_certificationFEL IS NOT NULL
+            AND IH.inv_creditNote IS NULL
+            AND IH.inv_invoiceOfCreditNote IS  NULL
+
+    END
+    ELSE IF (@Subscription IS NOT NULL)
+    BEGIN
+
+        SELECT
+             ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote'
+            ,ID.dti_description
+            ,IH.inv_pk_id
+            ,IH.inv_serieFEL
+            ,IH.inv_numberFEL
+            ,IH.inv_certificationFEL
+            ,IH.inv_cli_name
+            ,IH.IdCountry
+        FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
+        INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
+            ON IH.inv_pk_id = ID.dti_fk_header
+        WHERE
+            IH.inv_invoiceOfCreditNote IS NULL AND  ID.SubscriptionId = @Subscription
+            AND   IH.inv_dateFEL Between  @DateOf + ' 00:00:00'  AND @DateTo + ' 23:59:59'
+            AND   inv_certificationFEL IS NOT NULL
+            AND IH.inv_creditNote IS NULL
+            AND  IH.inv_invoiceOfCreditNote IS   NULL
+
+    END
 
 END
-ELSE IF (@NumberFel IS NOT NULL)
-BEGIN
-
-
-	IF(@IdCountry ='SV')
-	BEGIN
-		DECLARE @TypeCF INT = (SELECT IdRegister FROM CatTypeDocument WITH(NOLOCK) WHERE [Name] = 'Comprobante Crédito Fiscal');
-		DECLARE @TypeInvoice INT = (SELECT IdRegister FROM CatTypeDocument WITH(NOLOCK) WHERE [Name] = 'Factura');
-
-		Select  0 'HaveaCreditNote',  
-				ID.dti_description,  
-				IH.inv_pk_id,  
-				IH.inv_serieFEL,  
-				IH.inv_numberFEL,  
-				IH.inv_certificationFEL,  
-				IH.inv_cli_name,  
-				IH.IdCountry 
-		FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)  
-			INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)  
-			ON IH.inv_pk_id = ID.dti_fk_header  
-			WHERE   
-			inv_numberFEL = @NumberFel  
-			AND IH.inv_type in (@TypeInvoice,@TypeCF)
-			ORDER BY IH.inv_pk_id DESC  
-	END
-	ELSE
-	BEGIN
-		Select  ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote',
-			ID.dti_description,
-			IH.inv_pk_id,
-			IH.inv_serieFEL,
-			IH.inv_numberFEL,
-			IH.inv_certificationFEL,
-			IH.inv_cli_name,
-			IH.IdCountry
-					 FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
-						 INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
-						 ON IH.inv_pk_id = ID.dti_fk_header
-						 WHERE 
-						 IH.inv_invoiceOfCreditNote IS NULL AND inv_certificationFEL = @NumberFel
-						  AND IH.inv_creditNote IS NULL
-						  AND  IH.inv_invoiceOfCreditNote IS  NULL
-						 ORDER BY IH.inv_pk_id DESC
-	END
-
-END
-ELSE IF (@Membership IS NOT NULL)
-BEGIN
-	
-	Select ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote',
-			ID.dti_description,
-			IH.inv_pk_id,
-			IH.inv_serieFEL,
-			IH.inv_numberFEL,
-			IH.inv_certificationFEL,
-			IH.inv_cli_name,
-			IH.IdCountry
-					 FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
-						 INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
-						 ON IH.inv_pk_id = ID.dti_fk_header
-						 WHERE 
-						 IH.inv_invoiceOfCreditNote IS NULL AND ID.MembershipId = @Membership
-						 AND  IH.inv_dateFEL Between  @DateOf + ' 00:00:00'  AND @DateTo + ' 23:59:59'
-						 AND   inv_certificationFEL IS NOT NULL
-						 AND IH.inv_creditNote IS NULL
-						 AND  IH.inv_invoiceOfCreditNote IS  NULL
-
-	
-END
-ELSE IF (@Subscription IS NOT NULL)
-BEGIN
-
-
-
-Select ISNULL(IH.inv_creditNote,0) 'HaveaCreditNote',
-	    ID.dti_description,
-		IH.inv_pk_id,
-        IH.inv_serieFEL,
-		IH.inv_numberFEL,
-		IH.inv_certificationFEL,
-		IH.inv_cli_name,
-		IH.IdCountry
-		         FROM [dbo].[invoiceHeader] IH WITH (NOLOCK)
-                     INNER JOIN [dbo].[invoiceDetail] ID WITH (NOLOCK)
-					 ON IH.inv_pk_id = ID.dti_fk_header
-					 WHERE  
-					 IH.inv_invoiceOfCreditNote IS NULL AND  ID.SubscriptionId = @Subscription
-					 AND   IH.inv_dateFEL Between  @DateOf + ' 00:00:00'  AND @DateTo + ' 23:59:59'
-					 AND   inv_certificationFEL IS NOT NULL
-					 AND IH.inv_creditNote IS NULL
-					 AND  IH.inv_invoiceOfCreditNote IS   NULL
-
-END
-
-   END
